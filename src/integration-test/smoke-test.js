e785ee58df1c4236ed0d77b6049f1baebf862124
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-10-29 13:46:46 +0100

    lint issues

--- src/integration-test/smoke-test.js
+++ src/integration-test/smoke-test.js
@@ -5,12 +5,12 @@ const { app } = require('../main/app');
   
 // TODO: replace this sample test with proper smoke tests later 
 describe('Dummy Smoke test - Home page', () => {
-    describe('on GET', () => {
-      test('should return sample home page', async () => {
-        await request(app)
-          .get(process.env.TEST_URL)
-          .expect((res) => expect(res.status).to.equal(200));
-      });
+  describe('on GET', () => {
+    test('should return sample home page', async () => {
+      await request(app)
+        .get(process.env.TEST_URL)
+        .expect((res) => expect(res.status).to.equal(200));
     });
+  });
 });
   
