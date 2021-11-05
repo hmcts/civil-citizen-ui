a4407a066082793948f9624e9b54a634fb9ca296
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2021-11-05 08:51:52 +0000

    SSl key and self-signed certificates are created on the fly, instead of being stored in VCS

--- src/main/server.ts
+++ src/main/server.ts
@@ -10,12 +10,34 @@ const logger = Logger.getLogger('server');
 // TODO: set the right port for your application
 const port: number = parseInt(process.env.PORT, 10) || 3100;
 
-if (app.locals.ENV === 'development') {
-  const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl');
+const spawnSync = require('child_process').execSync;
+
+function buildSslOptions() {
+  const sslDirectory = path.join(__dirname, 'localhost-ssl');
+
+  spawnSync('mkdir ' + sslDirectory);
+
+  const keyFile = path.join(sslDirectory, 'localhost.key');
+  const certFile = path.join(sslDirectory, 'localhost.crt');
+
+  let command = 'openssl req -nodes -x509 -newkey rsa:4096 -keyout ' + keyFile + ' -out ' + certFile + ' -sha256 -days 365 -subj "/C=GB/ST=A/L=B/O=C/OU=D/CN=E"';
+
+  spawnSync(command);
+
   const sslOptions = {
-    cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt')),
-    key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
+    cert: fs.readFileSync(certFile, 'utf8'),
+    key: fs.readFileSync(keyFile, 'utf8')
   };
+
+  spawnSync('rm -rf ' + sslDirectory);
+
+  return sslOptions;
+}
+
+if (app.locals.ENV === 'development') {
+  logger.info('Building SSL options..');
+  const sslOptions = buildSslOptions();
+  logger.info('Building SSL options complete');
   const server = https.createServer(sslOptions, app);
   server.listen(port, () => {
     logger.info(`Application started: https://localhost:${port}`);
