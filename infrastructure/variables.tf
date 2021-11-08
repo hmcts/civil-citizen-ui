0226088500666aa81416167ef1562bcef5046797
leonardo palmeiro <leonardo.palmeiro@hmcts.net>
2021-11-08 12:53:00 +0000

    removed unsed variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -9,15 +9,15 @@ variable "location" {
   default = "UK South"
 }
 
-variable "env" { }
+variable "env" {}
 
-variable "ilbIp" { }
+variable "ilbIp" {}
 
 
 variable "subscription" {}
 
 variable "jenkins_AAD_objectId" {
-  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "tenant_id" {
@@ -30,7 +30,7 @@ variable "node_env" {
 
 variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default = ""
+  default     = ""
 }
 
 variable "common_tags" {
