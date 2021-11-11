ead9c510b4be10bbef28ee3b2c28f190b0b8b2ae
leonardo palmeiro <leonardo.palmeiro@hmcts.net>
2021-11-11 11:16:37 +0000

    created the access of exists vault.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,21 +2,9 @@ provider "azurerm" {
   features {}
 }
 
-
-locals {
-  vaultNameCivil = "civil-${var.env}"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = var.location
-
-  tags = var.common_tags
-}
-
 data "azurerm_key_vault" "civil_key_vault" {
-  name = "${local.vaultNameCivil}"
-  resource_group_name = azurerm_resource_group.rg.name
+  name = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-service-${var.env}"
 }
 
 
