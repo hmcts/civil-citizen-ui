02d0f6fa8be1ad9451626242b7bbe2f236398622
leonardo palmeiro <leonardo.palmeiro@hmcts.net>
2021-11-11 12:16:37 +0000

    removed unused variables

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.50"
+      version = "2.84.0"
     }
     random = {
       source = "hashicorp/random"
