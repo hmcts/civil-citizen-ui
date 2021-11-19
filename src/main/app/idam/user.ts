2f4f8ad8cc5102c9a4f90a5f7f0f75dfe6a75830
leonardo palmeiro <leonardo.palmeiro@hmcts.net>
2021-11-19 09:33:41 +0000

    added dependencies and copied claimEligibilityGuard.ts and draftClaim.ts

--- src/main/app/idam/user.ts
+++ src/main/app/idam/user.ts
@@ -8,23 +8,23 @@ export class User {
   bearerToken: string
 
   constructor (id: string,
-    email: string,
-    forename: string,
-    surname: string,
-    roles: string[],
-    group: string,
-    bearerToken: string) {
-    this.id = id;
-    this.email = email;
-    this.forename = forename;
-    this.surname = surname;
-    this.roles = roles;
-    this.group = group;
-    this.bearerToken = bearerToken;
+               email: string,
+               forename: string,
+               surname: string,
+               roles: string[],
+               group: string,
+               bearerToken: string) {
+    this.id = id
+    this.email = email
+    this.forename = forename
+    this.surname = surname
+    this.roles = roles
+    this.group = group
+    this.bearerToken = bearerToken
   }
 
   isInRoles (...requiredRoles: string[]): boolean {
-    return requiredRoles.every(requiredRole => this.roles.indexOf(requiredRole) > -1);
+    return requiredRoles.every(requiredRole => this.roles.indexOf(requiredRole) > -1)
   }
 
   getLetterHolderIdList (): string[] {
@@ -33,8 +33,8 @@ export class User {
         (role: string) =>
           role.startsWith('letter') &&
           role !== 'letter-holder' &&
-          !role.endsWith('loa1'),
+          !role.endsWith('loa1')
       )
-      .map(role => role.replace('letter-', ''));
+      .map(role => role.replace('letter-', ''))
   }
 }
