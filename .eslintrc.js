4a5557b7e8d02218674ca0dad94a35889e82269c
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2021-12-02 14:17:21 +0000

    added eslint configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,79 @@
 module.exports = {
-      "env": { "browser": true, "es6": true, "node": true, "jest": true},
-      "extends": ["eslint:recommended"],
-      "globals": { "Atomics": "readonly", "SharedArrayBuffer": "readonly" },
-      "parser": "babel-eslint",
-      "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+  root: true,
+  env: { browser: true, es6: true, node: true,  mocha: true},
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'mocha'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:mocha/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+  globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
+  rules: {
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
       },
-      "rules": {
-        "indent": ["error", 2, { "SwitchCase": 1 }],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "comma-dangle": ["error", "always-multiline"],
-        "semi": ["error", "always"]
+    ],
+    '@typescript-eslint/no-var-requires': 'off',
+    curly: 'error',
+    eqeqeq: 'error',
+    'import/no-duplicates': 'error',
+    'import/no-named-as-default': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          caseInsensitive: false,
+          order: 'asc',
+        },
+        'newlines-between': 'always',
       },
-      "overrides": [
-        {
-          "files": ["**/*.ts", "**/*.tsx"],
-          "env": { "browser": true, "es6": true, "node": true },
-          "extends": [
-            "eslint:recommended",
-            "plugin:@typescript-eslint/eslint-recommended",
-            "plugin:@typescript-eslint/recommended",
-          ],
-          "globals": { "Atomics": "readonly", "SharedArrayBuffer": "readonly" },
-          "parser": "@typescript-eslint/parser",
-          "parserOptions": {
-            "ecmaVersion": 2018,
-            "sourceType": "module",
-            "project": "./tsconfig.json"
-          },
-          "plugins": ["@typescript-eslint"],
-          "rules": {
-            "indent": ["error", 2, { "SwitchCase": 1 }],
-            "linebreak-style": ["error", "unix"],
-            "quotes": ["error", "single", { "avoidEscape": true }],
-            "comma-dangle": ["error", "always-multiline"],
-            "@typescript-eslint/no-var-requires": 0
-          },
-        }
-      ]
-
+    ],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 'warn',
+    'no-prototype-builtins': 'off',
+    'no-return-await': 'error',
+    'no-unneeded-ternary': [
+      'error',
+      {
+        defaultAssignment: false,
+      },
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'object-shorthand': ['error', 'properties'],
+    quotes: [
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: false,
+        avoidEscape: true,
+      },
+    ],
+    semi: ['error', 'always'],
+    'sort-imports': [
+      'error',
+      {
+        allowSeparatedGroups: false,
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+      },
+    ],
+  },
 };
