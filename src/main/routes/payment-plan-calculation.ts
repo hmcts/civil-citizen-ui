953c6a6ece935f2d4ff9d9803968eaf5872621e1
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2021-12-02 15:44:14 +0000

    fixed lint errors

--- src/main/routes/payment-plan-calculation.ts
+++ src/main/routes/payment-plan-calculation.ts
@@ -1,56 +1,56 @@
-import * as express from 'express'
-import * as HttpStatus from 'http-status-codes'
-import * as _ from 'lodash'
+import * as express from 'express';
+import * as HttpStatus from 'http-status-codes';
+import * as _ from 'lodash';
 
-import { Paths as AppPaths } from 'paths'
-import { PaymentPlan } from 'common/payment-plan/paymentPlan'
-import { Frequency } from 'common/frequency/frequency'
+import { Paths as AppPaths } from 'paths';
+import { PaymentPlan } from 'common/payment-plan/paymentPlan';
+import { Frequency } from 'common/frequency/frequency';
 
 /* tslint:disable:no-default-export */
 export default express.Router()
   .get(AppPaths.paymentPlanCalculation.uri, (req, res) => {
 
-    const totalAmount: string = req.query['total-amount'] as string
-    const instalmentAmount: string = req.query['instalment-amount'] as string
-    const frequencyInWeeks: string = req.query['frequency-in-weeks'] as string
+    const totalAmount: string = req.query['total-amount'] as string;
+    const instalmentAmount: string = req.query['instalment-amount'] as string;
+    const frequencyInWeeks: string = req.query['frequency-in-weeks'] as string;
 
-    const error: string = validate(totalAmount, instalmentAmount, frequencyInWeeks)
+    const error: string = validate(totalAmount, instalmentAmount, frequencyInWeeks);
 
     if (error) {
       return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
         error: {
           status: HttpStatus.UNPROCESSABLE_ENTITY,
-          message: error
-        }
-      })
+          message: error,
+        },
+      });
     }
 
-    const frequency: Frequency = Frequency.ofWeekly(Number(frequencyInWeeks))
-    const paymentPlan: PaymentPlan = PaymentPlan.create(Number(totalAmount), Number(instalmentAmount), frequency)
+    const frequency: Frequency = Frequency.ofWeekly(Number(frequencyInWeeks));
+    const paymentPlan: PaymentPlan = PaymentPlan.create(Number(totalAmount), Number(instalmentAmount), frequency);
 
     return res.status(HttpStatus.OK).json({
       paymentPlan: {
         paymentLength: paymentPlan.calculatePaymentLength(),
-        lastPaymentDate: paymentPlan.calculateLastPaymentDate().toJSON()
-      }
-    })
-  })
+        lastPaymentDate: paymentPlan.calculateLastPaymentDate().toJSON(),
+      },
+    });
+  });
 
-function validate (totalAmount: string, instalmentAmount: string, frequencyInWeeks: string) {
+function validate(totalAmount: string, instalmentAmount: string, frequencyInWeeks: string) {
   return validateThatIsPositiveNumber(totalAmount, 'total-amount')
-  || validateThatIsPositiveNumber(instalmentAmount, 'instalment-amount')
-  || validateThatIsPositiveNumber(frequencyInWeeks, 'frequency-in-weeks')
+    || validateThatIsPositiveNumber(instalmentAmount, 'instalment-amount')
+    || validateThatIsPositiveNumber(frequencyInWeeks, 'frequency-in-weeks');
 }
 
-function validateThatIsPositiveNumber (value: string, name: string) {
+function validateThatIsPositiveNumber(value: string, name: string) {
   if (_.isEmpty(value)) {
-    return `'${name}' not provided`
+    return `'${name}' not provided`;
   }
-  const convertedValue = Number(value)
+  const convertedValue = Number(value);
   if (isNaN(convertedValue)) {
-    return `'${name}' must be a number`
+    return `'${name}' must be a number`;
   }
   if (convertedValue < 1) {
-    return `'${name}' must be a positive number`
+    return `'${name}' must be a positive number`;
   }
 }
