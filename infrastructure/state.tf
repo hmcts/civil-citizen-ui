76ffc6c94260ac60564b803fbae6adefe398112e
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2021-12-16 08:53:49 +0000

    Add missing infrastructure files

---
+++ infrastructure/state.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.84.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,42 @@
+// Infrastructural variables
+variable "product" {}
+
+variable "raw_product" {
+  default = "cmc"
+}
+
+variable "microservice" {
+  default = "citizen-frontend"
+}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "env" { }
+
+variable "ilbIp" { }
+
+
+variable "subscription" {}
+
+variable "jenkins_AAD_objectId" {
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
+
+variable "node_env" {
+  default = "production"
+}
+
+variable "appinsights_instrumentation_key" {
+  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
+  default = ""
+}
+
+variable "common_tags" {
+  type = map(string)
+}
