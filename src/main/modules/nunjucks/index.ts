80ddc2582df8f45c9f1eef47907b38b20e958b18
RosDiaf <ros_diaf@hotmail.com>
2022-01-21 15:00:03 +0000

    fix numeral package for claim details

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -2,6 +2,11 @@ import { join } from 'path';
 import { Express } from 'express';
 import { configure } from 'nunjucks';
 import { InitOptions } from 'i18next';
+import { Paths as AppPaths } from '../../app/paths';
+import { NUMBER_FORMAT } from '../../app/utils/numberFormatter';
+const numeralFilter = require('nunjucks-numeral-filter');
+import * as numeral from 'numeral';
+import * as moment from 'moment';
 
 const packageDotJson = require('../../../../package.json');
 
@@ -50,10 +55,18 @@ export class Nunjucks {
       },
     );
 
+    require('numeral/locales/en-gb');
+    numeral.locale('en-gb');
+    numeral.defaultFormat(NUMBER_FORMAT);
+
+    moment.locale('en-gb');
+
     nunjucksEnv.addGlobal('asset_paths', appAssetPaths);
     nunjucksEnv.addGlobal('development', this.developmentMode);
     nunjucksEnv.addGlobal('govuk_template_version', packageDotJson.dependencies.govuk_template_jinja);
     nunjucksEnv.addGlobal('t', (key: string, options?: InitOptions): string => this.i18next.t(key, options));
+    nunjucksEnv.addFilter('numeral', numeralFilter);
+    nunjucksEnv.addGlobal('AppPaths', AppPaths);
 
     app.use((req, res, next) => {
       res.locals.pagePath = req.path;
