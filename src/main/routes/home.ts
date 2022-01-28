80604dca324750e319850afc1eb1ae24cf9d4cd4
laarmada <sergiosalmar@yahoo.es>
2022-01-28 10:47:38 +0000

    Idam setup

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -3,7 +3,8 @@ import * as express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home');
+  //res.render('home');
+  res.redirect('http://localhost:3333');
 });
 
 export default router;
