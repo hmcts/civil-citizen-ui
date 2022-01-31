497e9da018fde7f9af72ffc21dfcf696ac3e8432
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2022-01-31 15:21:35 +0000

    Rename Draft Storage to Draft Store

--- src/main/modules/draft-store/index.ts
+++ src/main/modules/draft-store/index.ts
@@ -2,15 +2,15 @@ import config from 'config';
 import {createClient} from 'redis';
 import {Application} from 'express';
 
-export class DraftStorageClient {
+export class DraftStoreClient {
   public enableFor(app: Application): void {
     const client = createClient({
       socket: {
-        host: config.get('draftStorage.redis.host'),
-        port: config.get('draftStorage.redis.port'),
+        host: config.get('draftStore.redis.host'),
+        port: config.get('draftStore.redis.port'),
       },
     });
 
-    client.connect().then(() => app.locals.draftStorageClient = client);
+    client.connect().then(() => app.locals.draftStoreClient = client);
   }
 }
