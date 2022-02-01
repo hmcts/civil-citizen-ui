c3cf7d7ca386ed88e8611a491cc8c9474789bd43
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2022-02-01 16:10:53 +0000

    Fix redirect and remove unnecessary code and

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -3,9 +3,7 @@ import * as express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  //res.render('home');
-  res.redirect('http://localhost:3333');
-  //res.redirect(AppPaths.receiver.uri);
+  res.render('home');
 });
 
 export default router;
