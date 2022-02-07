5a6a191b0669e454c0cea8fdf6ede5979ca28bb3
laarmada <sergiosalmar@yahoo.es>
2022-02-07 13:21:13 +0000

    add new parameter citizenRole

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -13,6 +13,7 @@ export class OidcMiddleware {
     const loginUrl: string = config.get('services.idam.authorizationURL');
     const clientId: string = config.get('services.idam.clientID');
     const redirectUri: string = config.get('services.idam.callbackURL');
+    const citizenRole: string = config.get('services.idam.citizenRole');
 
     app.get(SIGN_IN_URL, (req: Request, res) => {
       res.redirect(loginUrl + '?client_id=' + clientId + '&response_type=code&redirect_uri=' + encodeURI(redirectUri));
@@ -22,7 +23,7 @@ export class OidcMiddleware {
       if (typeof req.query.code === 'string') {
         req.session.user = await getUserDetails(redirectUri, req.query.code);
         req.session.save(() => {
-          if (req.session.user?.roles?.includes('civil-citizen')) {
+          if (req.session.user?.roles?.includes(citizenRole)) {
             return res.redirect(DASHBOARD_URL);
           }
           return res.render('unauthorised');
@@ -40,7 +41,7 @@ export class OidcMiddleware {
     app.use((req: AppRequest, res: Response, next: NextFunction) => {
 
       if (req.session.user) {
-        if (req.session?.user?.roles?.includes('civil-citizen')) {
+        if (req.session?.user?.roles?.includes(citizenRole)) {
           return next();
         }
         return res.redirect(UNAUTHORISED_URL);
