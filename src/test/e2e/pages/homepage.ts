981d9d118e8ba02332c64cf8ff6fb16686f0796a
laarmada <sergiosalmar@yahoo.es>
2022-02-08 09:10:58 +0000

    homepage.ts issues

--- src/test/e2e/pages/homepage.ts
+++ src/test/e2e/pages/homepage.ts
@@ -4,7 +4,7 @@ import {  config as testConfig } from '../../config';
 module.exports = {
   homepage(I): void {
     Given('Load Citizen UI homepage', async () => {
-      await I.amOnPage(testConfig.TestUrl);
+      await I.amOnPage('http://localhost:3001/home');
     });
 
     Then('I expect the page to have content {string}', (content: string) => {
