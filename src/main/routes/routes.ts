20711ef0dccfe19576b923cf087f7ad127c09f86
OsheaF <fergal.oshea@hmcts.net>
2022-02-09 11:04:57 +0000

    CIV-904 First commit to set up page

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -2,10 +2,12 @@ import homeRoute from './home';
 import unauthorisedRoute from './unauthorised';
 import dashboardRoute from './features/dashboard/index';
 import responseRoute from './features/response/index';
+import defendantRoute from './features/defendant/index';
 
 export default [
   homeRoute,
   dashboardRoute,
   unauthorisedRoute,
   responseRoute,
+  defendantRoute,
 ];
