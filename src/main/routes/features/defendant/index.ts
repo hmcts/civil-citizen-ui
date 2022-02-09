2858cd2827bf30eeea9bf028ccfe260ed463787c
OsheaF <fergal.oshea@hmcts.net>
2022-02-09 11:04:57 +0000

    CIV-904 First commit to set up page

--- src/main/routes/features/defendant/index.ts
+++ src/main/routes/features/defendant/index.ts
@@ -1,32 +1,9 @@
 import * as express from 'express';
-import {DefendantDetailsTelephoneNumber} from '../../../common/form/models/defendantDetailsTelephoneNumber';
-import {Form} from '../../../common/form/form';
-
-import {ValidationError, Validator} from 'class-validator';
 
 const router = express.Router();
-const defendantDetailsTelephoneNumber = new DefendantDetailsTelephoneNumber();
 
-function renderView(form: Form<DefendantDetailsTelephoneNumber>, res: express.Response): void {
-  res.render('features/defendant/citizen-phone', {form: form});
-}
-
-router.get('/citizen-phone', (req, res) => {
-  renderView(new Form<DefendantDetailsTelephoneNumber>(defendantDetailsTelephoneNumber), res);
+router.get('/features/defendant/citizen-phone', (req, res) => {
+  res.render('features/defendant/citizen-phone');
 });
-router.post('/citizen-phone',
-  (req, res) => {
-    const form: Form<DefendantDetailsTelephoneNumber> = new Form<DefendantDetailsTelephoneNumber>(Object.assign(new DefendantDetailsTelephoneNumber(), req.body));
-    const model: DefendantDetailsTelephoneNumber = form.model;
-    const validator = new Validator();
-    const errors: ValidationError[] = validator.validateSync(model);
-    if (errors && errors.length > 0){
-      const formWithErrors = new Form<DefendantDetailsTelephoneNumber>(model, errors);
-      renderView(formWithErrors, res);
-    } else {
-      // temporary to show error removed, should forward to next page in sequence
-      renderView(form, res);
-    }
-  });
 
 export default router;
