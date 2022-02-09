1f7db54e0fc4a3f63caf39a8296ba2df5fb3738e
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-02-09 16:31:00 +0000

    CIV-904 error validation

--- src/main/routes/features/defendant/index.ts
+++ src/main/routes/features/defendant/index.ts
@@ -1,9 +1,26 @@
 import * as express from 'express';
+import {DefendendDetailsTelephoneNumber} from '../../../common/form/models/defendendDetailsTelephoneNumber';
+import {Form} from '../../../common/form/form';
+import {FormValidator} from '../../../common/form/validation/formValidator';
 
 const router = express.Router();
+const defendantDetailsTelephoneNumber = new DefendendDetailsTelephoneNumber();
 
-router.get('/features/defendant/citizen-phone', (req, res) => {
-  res.render('features/defendant/citizen-phone');
+function renderView(form: Form<DefendendDetailsTelephoneNumber>, res: express.Response): void {
+  res.render('features/defendant/citizen-phone',{form:form});
+}
+
+router.get('/citizen-phone', (req, res) => {
+  renderView(new Form<DefendendDetailsTelephoneNumber>(defendantDetailsTelephoneNumber), res);
 });
+router.post('/citizen-phone', FormValidator.requestHandler(DefendendDetailsTelephoneNumber),
+  (req, res) => {
+    const form: Form<DefendendDetailsTelephoneNumber> = req.body;
+
+    if (form.hasErrors()) {
+      renderView(form, res);
+      console.log(form);
+    }
+  });
 
 export default router;
