13509410c4137c3bd5cddc01c257093ca923b6a7
OsheaF <fergal.oshea@hmcts.net>
2022-02-09 11:04:57 +0000

    CIV-904 First commit to set up page

--- src/main/routes/features/defendant/index.ts
+++ src/main/routes/features/defendant/index.ts
@@ -1,29 +1,9 @@
 import * as express from 'express';
-import {CitizenTelephoneNumber} from '../../../common/form/models/citizenTelephoneNumber';
-
-import {ValidationError, Validator} from 'class-validator';
 
 const router = express.Router();
-const defendantDetailsTelephoneNumber = new CitizenTelephoneNumber();
 
-function renderView(form: CitizenTelephoneNumber, res: express.Response): void {
-  res.render('features/defendant/citizen-phone', {form: form});
-}
-
-router.get('/citizen-phone', (req, res) => {
-  renderView(defendantDetailsTelephoneNumber, res);
+router.get('/features/defendant/citizen-phone', (req, res) => {
+  res.render('features/defendant/citizen-phone');
 });
-router.post('/citizen-phone',
-  (req, res) => {
-    const model: CitizenTelephoneNumber = Object.assign(new CitizenTelephoneNumber(), req.body);
-    const validator = new Validator();
-    const errors: ValidationError[] = validator.validateSync(model);
-    if (errors && errors.length > 0) {
-      model.error = errors[0];
-      console.log(model.getErrorMessage());
-    }
-    //temporary outside the if statement to show there are no errors.
-    renderView(model, res);
-  });
 
 export default router;
