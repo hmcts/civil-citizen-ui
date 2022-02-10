b653ab2a40150b8daaaf74120fac154a776a8965
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-02-10 11:11:53 +0000

    CIV-904 ensure that special characters are not allowed

--- src/main/common/form/models/defendendDetailsTelephoneNumber.ts
+++
@@ -1,8 +0,0 @@
-import {IsInt, ValidateIf} from 'class-validator';
-const NON_NUMERIC_VALUES_NOT_ALLOWED = 'There was a problem. Please enter numeric number';
-export class DefendendDetailsTelephoneNumber {
-
-  @ValidateIf(o => o.telephoneNumber !== undefined)
-  @IsInt({message:NON_NUMERIC_VALUES_NOT_ALLOWED})
-  telephoneNumber?: number
-}
