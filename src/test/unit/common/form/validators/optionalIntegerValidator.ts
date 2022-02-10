c78e83cc65bce870000a1d77431dff1de644d1c4
OsheaF <fergal.oshea@hmcts.net>
2022-02-10 12:40:45 +0000

    CIV-904 Adding unit test for OptionalIntegerValidator

--- src/test/unit/common/form/validators/optionalIntegerValidator.ts
+++ src/test/unit/common/form/validators/optionalIntegerValidator.ts
@@ -1,13 +1,23 @@
 import {OptionalIntegerValidator} from '../../../../../main/common/form/validators/optionalIntegerValidator';
+import {ValidationArguments} from 'class-validator';
+
+
 
 describe('OptionalIntegerValidator', () => {
+  class DummyClass implements ValidationArguments {
+    constraints: any[];
+    object: object;
+    property: string;
+    targetName: string;
+    value: any;
+  }
   it('should return true for numeric input', async () => {
     //Given a valid input
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = '1232134234';
-    //When input is validated
-    const result = optionalIntegerValidator.validate(validNumericInput);
-    //Then
+    //when input is validated
+    const result = optionalIntegerValidator.validate(validNumericInput, new DummyClass());
+    //then
     expect(result).toBeTruthy();
   });
 
@@ -15,9 +25,9 @@ describe('OptionalIntegerValidator', () => {
     //Given input with special characters
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = '+442342845452';
-    //When
-    const result = optionalIntegerValidator.validate(validNumericInput);
-    //Then
+    //when
+    const result = optionalIntegerValidator.validate(validNumericInput, new DummyClass());
+    //then
     expect(result).toBeFalsy();
   });
 
@@ -25,9 +35,9 @@ describe('OptionalIntegerValidator', () => {
     //Given alphanumeric input
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = 'sdfswsdfsdf';
-    //When
-    const result = optionalIntegerValidator.validate(validNumericInput);
-    //Then
+    //when
+    const result = optionalIntegerValidator.validate(validNumericInput, new DummyClass());
+    //then
     expect(result).toBeFalsy();
   });
 
@@ -36,7 +46,7 @@ describe('OptionalIntegerValidator', () => {
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = '';
     //when
-    const result = optionalIntegerValidator.validate(validNumericInput);
+    const result = optionalIntegerValidator.validate(validNumericInput, new DummyClass());
     //then
     expect(result).toBeTruthy();
   });
