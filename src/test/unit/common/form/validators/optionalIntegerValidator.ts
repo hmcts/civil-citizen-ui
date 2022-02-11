a7f0a387d27112e8ba9fe958d4c67c01a301098a
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-02-11 20:06:18 +0000

    CIV-904 refactor optionalIntegerValidator.ts

--- src/test/unit/common/form/validators/optionalIntegerValidator.ts
+++ src/test/unit/common/form/validators/optionalIntegerValidator.ts
@@ -1,29 +1,12 @@
 import {OptionalIntegerValidator} from '../../../../../main/common/form/validators/optionalIntegerValidator';
-import {ValidationArguments} from 'class-validator';
-
-
 
 describe('OptionalIntegerValidator', () => {
-  const mock = jest.fn<ValidationArguments, []>(() => {
-    const person = {
-      firstName: 'Tom',
-      lastName: 'Hanks',
-    };
-    return {
-      constraints : ['strict', 'lenient', 'none'],
-      object: person,
-      property: 'property',
-      targetName: 'targetName',
-      value: '',
-    };
-  });
-
   it('should return true for numeric input', async () => {
     //Given a valid input
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = '1232134234';
     //When input is validated
-    const result = optionalIntegerValidator.validate(validNumericInput, new mock());
+    const result = optionalIntegerValidator.validate(validNumericInput);
     //Then
     expect(result).toBeTruthy();
   });
@@ -33,7 +16,7 @@ describe('OptionalIntegerValidator', () => {
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = '+442342845452';
     //When
-    const result = optionalIntegerValidator.validate(validNumericInput, new mock());
+    const result = optionalIntegerValidator.validate(validNumericInput);
     //Then
     expect(result).toBeFalsy();
   });
@@ -43,7 +26,7 @@ describe('OptionalIntegerValidator', () => {
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = 'sdfswsdfsdf';
     //When
-    const result = optionalIntegerValidator.validate(validNumericInput, new mock());
+    const result = optionalIntegerValidator.validate(validNumericInput);
     //Then
     expect(result).toBeFalsy();
   });
@@ -53,7 +36,7 @@ describe('OptionalIntegerValidator', () => {
     const optionalIntegerValidator = new OptionalIntegerValidator();
     const validNumericInput  = '';
     //when
-    const result = optionalIntegerValidator.validate(validNumericInput, new mock());
+    const result = optionalIntegerValidator.validate(validNumericInput);
     //then
     expect(result).toBeTruthy();
   });
