9a7dd8fa11dc9b728a71f8f1b54c6f61f56e3244
RosDiaf <ros_diaf@hotmail.com>
2022-02-11 10:12:00 +0000

    Decimal format removed from process.env.PORT

--- src/main/server.ts
+++ src/main/server.ts
@@ -8,7 +8,7 @@ import { app } from './app';
 const logger = Logger.getLogger('server');
 
 // TODO: set the right port for your application
-const port: number = parseInt(process.env.PORT || '3001', 10);
+const port: number = parseInt(process.env.PORT) || 3001;
 
 if (app.locals.ENV === 'development') {
   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl');
