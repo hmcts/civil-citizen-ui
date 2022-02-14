ec77cd03b50951d98eb8c6ac722fe492b5b39537
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-02-14 16:09:10 +0000

    CIV-904 refactor

--- src/main/routes/features/defendant/index.ts
+++ src/main/routes/features/defendant/index.ts
@@ -1,32 +1,29 @@
 import * as express from 'express';
-import {DefendantDetailsTelephoneNumber} from '../../../common/form/models/defendantDetailsTelephoneNumber';
-import {Form} from '../../../common/form/form';
+import {CitizenTelephoneNumber} from '../../../common/form/models/citizenTelephoneNumber';
 
 import {ValidationError, Validator} from 'class-validator';
 
 const router = express.Router();
-const defendantDetailsTelephoneNumber = new DefendantDetailsTelephoneNumber();
+const defendantDetailsTelephoneNumber = new CitizenTelephoneNumber();
 
-function renderView(form: Form<DefendantDetailsTelephoneNumber>, res: express.Response): void {
+function renderView(form: CitizenTelephoneNumber, res: express.Response): void {
   res.render('features/defendant/citizen-phone', {form: form});
 }
 
 router.get('/citizen-phone', (req, res) => {
-  renderView(new Form<DefendantDetailsTelephoneNumber>(defendantDetailsTelephoneNumber), res);
+  renderView(defendantDetailsTelephoneNumber, res);
 });
 router.post('/citizen-phone',
   (req, res) => {
-    const form: Form<DefendantDetailsTelephoneNumber> = new Form<DefendantDetailsTelephoneNumber>(Object.assign(new DefendantDetailsTelephoneNumber(), req.body));
-    const model: DefendantDetailsTelephoneNumber = form.model;
+    const model: CitizenTelephoneNumber = Object.assign(new CitizenTelephoneNumber(), req.body);
     const validator = new Validator();
     const errors: ValidationError[] = validator.validateSync(model);
-    if (errors && errors.length > 0){
-      const formWithErrors = new Form<DefendantDetailsTelephoneNumber>(model, errors);
-      renderView(formWithErrors, res);
-    } else {
-      // temporary to show error removed, should forward to next page in sequence
-      renderView(form, res);
+    if (errors && errors.length > 0) {
+      model.error = errors[0];
+      console.log(model.getErrorMessage());
     }
+    //temporary outside the if statement to show there are no errors.
+    renderView(model, res);
   });
 
 export default router;
