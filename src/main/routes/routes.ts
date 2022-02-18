c3b9951275ae7f69ffe2a2a427ef129fb7093e6d
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-02-18 10:53:36 +0000

    CIV-904 refactor - change package name to be consistent

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -2,7 +2,7 @@ import homeRoute from './home';
 import unauthorisedRoute from './unauthorised';
 import dashboardRoute from './features/dashboard/index';
 import responseRoute from './features/response/index';
-import phoneDetailsRoute from './features/response/phone-details';
+import phoneDetailsRoute from './features/response/yourPhoneDetails';
 import responseDobRoute from './features/response/yourDob/index';
 
 export default [
