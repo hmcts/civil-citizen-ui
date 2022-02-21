863ba6a6fa3ab6da34deecd161e5f053e13bb0a2
laarmada <sergiosalmar@yahoo.es>
2022-02-21 13:40:18 +0000

    update validations

--- src/main/common/form/models/citizenDob.ts
+++ src/main/common/form/models/citizenDob.ts
@@ -1,12 +1,13 @@
-import {IsDate, Min, Max,MaxDate, ValidationError} from 'class-validator';
+import {Min, Max, ValidationError, Validate, IsDate} from 'class-validator';
 import {NON_FUTURE_VALUES_NOT_ALLOWED, VALID_DATE} from '../validationErrors/errorMessageConstants';
 import {Form} from './form';
 import {DateConverter} from '../../../common/utils/dateConverter';
+import {OptionalDateValidator} from 'common/form/validators/optionalDateValidator';
 
 export class CitizenDob extends Form{
 
   @IsDate({message: VALID_DATE})
-  @MaxDate(new Date(Date.now()),{message: NON_FUTURE_VALUES_NOT_ALLOWED})
+  @Validate(OptionalDateValidator, {message: NON_FUTURE_VALUES_NOT_ALLOWED})
   dateOfBirth?: Date
 
   @Min(1872)
---
+++ src/main/common/form/validators/optionalDateValidator.ts
@@ -0,0 +1,20 @@
+import {ValidatorConstraint, ValidatorConstraintInterface} from 'class-validator';
+import {NON_FUTURE_VALUES_NOT_ALLOWED} from '../validationErrors/errorMessageConstants';
+
+/**
+ * Validates that the input value is either empty or an integer with no special characters
+ */
+@ValidatorConstraint({name: 'customDate', async: false})
+export class OptionalDateValidator implements ValidatorConstraintInterface {
+
+  validate(inputDate: Date) {
+    if (inputDate !== null && (inputDate > (new Date(Date.now())))) {
+      return false;
+    }
+    return true;
+  }
+
+  defaultMessage() {
+    return NON_FUTURE_VALUES_NOT_ALLOWED;
+  }
+}
