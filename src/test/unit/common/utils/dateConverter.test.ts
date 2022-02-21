4a32a016ff6c1648f1138650d0dd15b0e48309ed
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-02-21 13:07:36 +0000

    CIV-904 date converter unit test

---
+++ src/test/unit/common/utils/dateConverter.test.ts
@@ -0,0 +1,29 @@
+import {DateConverter} from '../../../../main/common/utils/dateConverter';
+
+
+describe('Convert to valid Date', () => {
+  it('should convert to valid date successfully with valid input', () => {
+    //Given
+    const date = '1';
+    const month = '2';
+    const year = '2021';
+    //When
+    const result = DateConverter.convertToDate(year, month, date);
+    //Then
+    expect(result).not.toBeNull();
+    expect(result?.toString()).toBe('Mon Feb 01 2021 00:00:00 GMT+0000 (Greenwich Mean Time)');
+  });
+
+  it('should not convert to date with invalid input', () => {
+    //Given
+    //Given
+    const date = '1';
+    const month = '22';
+    const year = '2021';
+    //When
+    const result = DateConverter.convertToDate(year, month, date);
+    //Then
+    expect(result).toBeNull();
+  });
+
+});
