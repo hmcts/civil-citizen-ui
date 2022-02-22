a762bdb5163ce868a9dbb3c5f1815f610e8f1e64
laarmada <sergiosalmar@yahoo.es>
2022-02-22 08:28:11 +0000

    fix PR comment

--- src/main/common/utils/dateConverter.ts
+++ src/main/common/utils/dateConverter.ts
@@ -3,7 +3,7 @@ export class DateConverter {
    * Converts the string fields entered in the Date fields to a dd-mm-yyyy Date format if the fields entered correspond to a valid Date
    */
   static convertToDate(year: string, month: string, day: string) {
-    const date = new Date(year + '-' + month + '-' + day);
+    const date = new Date(Number(year) , Number(month) , Number(day));
     const givenDay = Number(day);
     if ((date.getDate() == givenDay)) {
       return date;
