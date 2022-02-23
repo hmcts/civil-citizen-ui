f568f313a3d77c248a45fc6226b653458a557562
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-02-23 16:48:53 +0000

    CIV-1107 formatting

--- src/main/common/utils/ageEligibilityVerification.ts
+++ src/main/common/utils/ageEligibilityVerification.ts
@@ -1,17 +1,17 @@
 export const AGE_THRESHOLD = 17;
+
 export class AgeEligibilityVerification {
   public static isOverEighteen(dob: Date) {
     return AgeEligibilityVerification.getAge(dob) > AGE_THRESHOLD;
   }
 
-  private static getAge(dob: Date){
+  private static getAge(dob: Date) {
     const today = new Date();
-    today.setHours(0,0,0);
+    today.setHours(0, 0, 0);
     let age = today.getFullYear() - dob.getFullYear();
     const monthDifference = today.getMonth() - dob.getMonth();
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dob.getDate()))
-    {
-      age = age-1;
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dob.getDate())) {
+      age = age - 1;
     }
     return age;
   }
