2e0f2c019cfc806572d84fbca3b6be1969762932
RosDiaf <ros_diaf@hotmail.com>
2022-02-24 13:01:07 +0000

    Check if link exist on page

--- src/main/assets/js/postcode-lookup.js
+++ src/main/assets/js/postcode-lookup.js
@@ -1,8 +1,11 @@
 (function () {
-  document.querySelector('#conditional-enter-address-manually')
-    .addEventListener('click', function (event) {
-      event.preventDefault();
-      this.classList.add('govuk-visually-hidden');
-      document.querySelector('#correspondence-address').classList.remove('govuk-visually-hidden');
-    });
+  const addressManuallyLink = document.querySelector('#conditional-enter-address-manually');
+  if (addressManuallyLink) {
+    document.querySelector('#conditional-enter-address-manually')
+      .addEventListener('click', function (event) {
+        event.preventDefault();
+        this.classList.add('govuk-visually-hidden');
+        document.querySelector('#correspondence-address').classList.remove('govuk-visually-hidden');
+      });
+  }
 })();
