3cc71b25d163f694787ef737fbca13e8057ad9a5
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2022-02-28 15:52:56 +0000

    Add test coverage for draft store client (quality gate failure)

--- src/main/modules/draft-store/index.ts
+++ src/main/modules/draft-store/index.ts
@@ -1,12 +1,12 @@
 import config from 'config';
 import {createClient} from 'redis';
 import {Application} from 'express';
-
-const {Logger} = require('@hmcts/nodejs-logging');
-
-const logger = Logger.getLogger('draftStoreClient');
+import {LoggerInstance} from 'winston';
 
 export class DraftStoreClient {
+
+  constructor(private readonly logger: LoggerInstance) {}
+
   public enableFor(app: Application): void {
     const client = createClient({
       socket: {
@@ -20,10 +20,10 @@ export class DraftStoreClient {
 
     client.connect()
       .then(() => {
-        logger.info('Connected to Redis instance successfully');
+        this.logger.info('Connected to Redis instance successfully');
       })
       .catch((error: Error) => {
-        logger.error('Error connecting to Redis instance', error);
+        this.logger.error('Error connecting to Redis instance', error);
       });
   }
 }
---
+++ src/test/unit/modules/draft-store/draftStore.test.ts
@@ -0,0 +1,40 @@
+import {createClient} from 'redis';
+import {mockCreateClient} from '../../../utils/mockCreateClient';
+import {DraftStoreClient} from '../../../../main/modules/draft-store';
+import {LoggerInstance} from 'winston';
+import express from 'express';
+
+jest.mock('redis');
+
+const mockedBehaviour = {
+  connect: jest.fn(async () => {
+    throw new Error();
+  }),
+};
+mockCreateClient(createClient, mockedBehaviour);
+
+// const mockLogger = {
+//   info: jest.fn(),
+//   error: jest.fn(),
+// };
+
+const mockLogger = {
+  error: jest.fn().mockImplementation((message: string) => message),
+  info: jest.fn().mockImplementation((message: string) => message),
+} as unknown as LoggerInstance;
+
+const app: express.Application = {
+  locals: {
+    draftStoreClient: null,
+  },
+} as unknown as express.Application;
+
+describe('Draft Store Client', () => {
+
+  test('When can\'t connect to Redis, log error message', async () => {
+    const draftStoreClient: DraftStoreClient = new DraftStoreClient(mockLogger);
+    draftStoreClient.enableFor(app);
+    await new Promise(process.nextTick);
+    expect(mockLogger.error).toHaveBeenCalledTimes(1);
+  });
+});
