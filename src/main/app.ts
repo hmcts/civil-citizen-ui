4a364c335f5cc85ee2e9e25c041a7e3df837a046
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-03-01 13:33:21 +0000

    add redis mock

--- src/main/app.ts
+++ src/main/app.ts
@@ -37,6 +37,8 @@ const logger = Logger.getLogger('app');
 
 new PropertiesVolume().enableFor(app);
 new DraftStoreClient(Logger.getLogger('draftStoreClient')).enableFor(app);
+new DraftStoreClient(Logger.getLogger('draftStoreClient')).setData(app);
+
 new AppInsights().enable();
 new Nunjucks(developmentMode, i18next).enableFor(app);
 new Helmet(config.get('security')).enableFor(app);
