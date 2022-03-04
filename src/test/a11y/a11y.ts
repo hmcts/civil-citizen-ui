f10fd4ec949a137a37f1b98948449a9cfd33c277
laarmada <sergiosalmar@yahoo.es>
2022-03-04 16:08:59 +0000

    updating a11y error test

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -6,7 +6,7 @@ import * as urls from '../../main/routes/urls';
 const pa11y = require('pa11y');
 
 const agent = supertest.agent(app);
-const IGNORED_URLS = [urls.CITIZEN_DISABILITY_URL, urls.SIGN_IN_URL, urls.SIGN_OUT_URL, urls.CASES_URL, urls.CALLBACK_URL, urls.DASHBOARD_URL, urls.UNAUTHORISED_URL, urls.CITIZEN_PHONE_NUMBER_URL, urls.UNAUTHORISED_URL, urls.CONFIRM_CITIZEN_DETAILS_URL, urls.CITIZEN_DETAILS_URL, urls.CLAIM_DETAILS_URL, urls.DOB_URL, urls.AGE_ELIGIBILITY_URL, urls.CITIZEN_RESPONSE_TYPE];
+const IGNORED_URLS = [urls.CITIZEN_DISABILITY_URL, urls.SIGN_IN_URL, urls.SIGN_OUT_URL, urls.CASES_URL, urls.CALLBACK_URL, urls.DASHBOARD_URL, urls.UNAUTHORISED_URL, urls.CITIZEN_PHONE_NUMBER_URL, urls.UNAUTHORISED_URL, urls.CONFIRM_CITIZEN_DETAILS_URL, urls.CITIZEN_DETAILS_URL, urls.CLAIM_DETAILS_URL, urls.DOB_URL, urls.AGE_ELIGIBILITY_URL, urls.CITIZEN_RESPONSE_TYPE, urls.ROOT_URL, urls.HOME_URL];
 const urlsNoSignOut = Object.values(urls).filter(url => !IGNORED_URLS.includes(url));
 
 class Pa11yResult {
