f8b424a9bb978bfcdb97340b57701bdecd08a338
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-03-04 11:58:56 +0000

    CIV-1263 add multiple rows and update index on inputs

--- src/main/assets/js/append-row.js
+++ src/main/assets/js/append-row.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('loaded');
   const appendRowButton = document.getElementsByClassName('append-row');
   if (appendRowButton && appendRowButton.length > 0) {
     appendRowButton[0].addEventListener('click', (event) => {
@@ -10,28 +9,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function cloneRow() {
     if (document.getElementsByClassName('multiple-row') && document.getElementsByClassName('multiple-row').length > 0) {
-      const lastElementIndex = document.getElementsByClassName('multiple-row').length - 1;
-      const lastRow = document.getElementsByClassName('multiple-row')[lastElementIndex];
-      const newRow = lastRow.cloneNode(true);
-      incrementIndex(newRow);
-      sanitizeContent(newRow);
-      lastRow.parent().append(newRow);
+      const lastRow = getLastRow();
+      let newRow = lastRow.cloneNode(true);
+      lastRow.parentNode.appendChild(newRow);
+      updateNewRow();
     }
   }
 
-  function incrementIndex(newRow) {
-    let newIndex = 0;
-    const indexRegex = /\[[0-9]+\]/;
-    newRow.html((index, oldHtml) => {
-      return oldHtml.replace(indexRegex, (match, capturedRowIndex) => {
-        newIndex = parseInt(capturedRowIndex) + 1;
-        return '[' + newIndex + ']';
-      });
-    });
+  function getLastRow(){
+    const lastElementIndex = document.getElementsByClassName('multiple-row').length - 1;
+    return document.getElementsByClassName('multiple-row')[lastElementIndex];
   }
 
-  function sanitizeContent(newRow) {
-    newRow.find('input, textarea').val('');
-    newRow.removeClass('govuk-input--error');
+  function updateNewRow(){
+    let newRow = getLastRow();
+    let inputs = newRow.querySelectorAll('input, textarea, select');
+    updateInputs(inputs);
+  }
+
+  function updateInputs(inputs) {
+    if(inputs && inputs.length>0){
+      inputs.forEach(input => {
+        input.value = '';
+        incrementIndexOnName(input);
+      });
+    }
+  }
+
+  function incrementIndexOnName(input) {
+    let newIndex = document.getElementsByClassName('multiple-row').length - 1;
+    const indexRegex = /\[[0-9]+\]/;
+    input.name = input.name.replace(indexRegex, '[' + newIndex + ']');
   }
 });
