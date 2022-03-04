5a78166c867c5af21a0afd5c7aa960b9753c37fd
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-03-04 11:42:05 +0000

    added mock of redis and civil-service

--- src/test/unit/routes/features/response/citizenDetails/citizenDetailsController.test.ts
+++ src/test/unit/routes/features/response/citizenDetails/citizenDetailsController.test.ts
@@ -10,23 +10,81 @@ import {
   VALID_CORRESPONDENCE_CITY,
   VALID_CORRESPONDENCE_POSTCODE,
 } from '../../../../../../main/common/form/validationErrors/errorMessageConstants';
+import {Respondent} from '../../../../../../main/common/models/respondent';
+import {Claim} from '../../../../../../main/common/models/claim';
 
 jest.mock('../../../../../../main/modules/oidc');
 jest.mock('../../../../../../main/modules/draft-store');
 const nock = require('nock');
 
+
+const respondent1: Respondent = {
+  primaryAddress: {
+    county: 'Greater London',
+    country: 'UK',
+    postCode: 'SW1H 9AJ',
+    postTown: 'London',
+    addressLine1: 'Flat 3A Middle Road',
+    addressLine2: '',
+    addressLine3: '',
+  },
+  correspondenceAddress: {
+    county: '',
+    country: '',
+    postCode: '',
+    postTown: '',
+    addressLine1: '',
+    addressLine2: '',
+    addressLine3: '',
+  },
+  individualTitle: 'Mrs.',
+  individualLastName: 'Mary',
+  individualFirstName: 'Richards',
+  telephoneNumber: '0208339922',
+  dateOfBirth: new Date('2022-01-24T15:59:59'),
+  responseType: '',
+};
+const mockResponse: Claim = {
+  legacyCaseReference: '497MC585',
+  applicant1:
+    {
+      individualTitle: 'Mrs',
+      individualLastName: 'Clark',
+      individualFirstName: 'Jane',
+    },
+  totalClaimAmount: 110,
+  respondent1ResponseDeadline: new Date('2022-01-24T15:59:59'),
+  detailsOfClaim: 'the reason i have given',
+  respondent1: respondent1,
+  individualTitle: 'string',
+  individualLastName: 'string',
+  individualFirstName: 'string',
+  formattedResponseDeadline: function (): string {
+    throw new Error('Function not implemented.');
+  },
+  formattedTotalClaimAmount: function (): string {
+    throw new Error('Function not implemented.');
+  },
+};
+
 describe('Confirm Details page', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
+
   beforeEach(() => {
     nock(idamUrl)
       .post('/o/token')
       .reply(200, { id_token: citizenRoleToken });
+    nock('http://localhost:8765')
+      .get('/cases/1643033241924739')
+      .reply(200,  mockResponse);
   });
 
-  test.skip('should return your details page', async () => {
+  test('should return your details page', async () => {
     const mockDraftStore = {
-      get: jest.fn(() => Promise.resolve({ data: {} })),
+      set: jest.fn(() => Promise.resolve({ data: {} })),
+      get: jest.fn(() => Promise.resolve({ data: mockResponse })),
+
     };
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
@@ -40,6 +98,8 @@ describe('Confirm Details page', () => {
   test('POST/Citizen details - should redirect on correct primary address', async () => {
     const mockDraftStore = {
       set: jest.fn(() => Promise.resolve({ data: {} })),
+      get: jest.fn(() => Promise.resolve({ data: JSON.stringify(mockResponse) })),
+
     };
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
@@ -65,6 +125,7 @@ describe('Confirm Details page', () => {
   test('POST/Citizen details - should redirect on correct correspondence address', async () => {
     const mockDraftStore = {
       set: jest.fn(() => Promise.resolve({ data: {} })),
+      get: jest.fn(() => Promise.resolve({ data: JSON.stringify(mockResponse) })),
     };
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
@@ -90,6 +151,8 @@ describe('Confirm Details page', () => {
   test('POST/Citizen details - should return error on empty primary address line', async () => {
     const mockDraftStore = {
       set: jest.fn(() => Promise.resolve({ data: {} })),
+      get: jest.fn(() => Promise.resolve({ data: JSON.stringify(mockResponse) })),
+
     };
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
@@ -116,6 +179,8 @@ describe('Confirm Details page', () => {
   test('POST/Citizen details - should return error on empty primary city', async () => {
     const mockDraftStore = {
       set: jest.fn(() => Promise.resolve({ data: {} })),
+      get: jest.fn(() => Promise.resolve({ data: JSON.stringify(mockResponse) })),
+
     };
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
@@ -142,6 +207,8 @@ describe('Confirm Details page', () => {
   test('POST/Citizen details - should return error on empty primary postcode', async () => {
     const mockDraftStore = {
       set: jest.fn(() => Promise.resolve({ data: {} })),
+      get: jest.fn(() => Promise.resolve({ data: JSON.stringify(mockResponse) })),
+
     };
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
@@ -168,6 +235,8 @@ describe('Confirm Details page', () => {
   test('POST/Citizen details - should return error on empty correspondence address line', async () => {
     const mockDraftStore = {
       set: jest.fn(() => Promise.resolve({ data: {} })),
+      get: jest.fn(() => Promise.resolve({ data: JSON.stringify(mockResponse) })),
+
     };
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
