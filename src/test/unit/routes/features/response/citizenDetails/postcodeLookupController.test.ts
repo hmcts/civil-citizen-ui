4df5c3dfd72a427ecfb916a4fae0a3566522aa00
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-03-08 14:01:59 +0000

    added mock of postcode

--- src/test/unit/routes/features/response/citizenDetails/postcodeLookupController.test.ts
+++ src/test/unit/routes/features/response/citizenDetails/postcodeLookupController.test.ts
@@ -7,6 +7,55 @@ jest.mock('../../../../../../main/modules/oidc');
 jest.mock('../../../../../../main/modules/draft-store');
 const nock = require('nock');
 
+const mockPostcodeServer = 'https://api.os.uk';
+const mockPostcodePath = /\/search\/places\/v1\/postcode\?.+/;
+
+const mockPostcodeLookupResponse = {
+  'header': {
+    'uri': 'https://api.os.uk/search/names/v1/find?offset=0&dataset=DPA,LPI&postcode=SW2%201AN',
+    'query': 'postcode=SW2 1AN',
+    'offset': 0,
+    'totalresults': 33,
+    'format': 'JSON',
+    'dataset': 'DPA',
+    'lr': 'EN,CY',
+    'maxresults': 100,
+    'epoch': '64',
+    'output_srs': 'EPSG:27700',
+  },
+  'results': [
+    {
+      'DPA': {
+        'UPRN': '100070660705',
+        'UDPRN': '23779778',
+        'ADDRESS': '2, DALBERG ROAD, LONDON, SW2 1AN',
+        'BUILDING_NUMBER': '2',
+        'THOROUGHFARE_NAME': 'DALBERG ROAD',
+        'POST_TOWN': 'COVENTRY',
+        'POSTCODE': 'CV5 6GQ',
+        'RPC': '2',
+        'X_COORDINATE': 431895,
+        'Y_COORDINATE': 278312,
+        'STATUS': 'APPROVED',
+        'LOGICAL_STATUS_CODE': '1',
+        'CLASSIFICATION_CODE': 'RD04',
+        'CLASSIFICATION_CODE_DESCRIPTION': 'Terraced',
+        'LOCAL_CUSTODIAN_CODE': 5660,
+        'LOCAL_CUSTODIAN_CODE_DESCRIPTION': 'LAMBETH',
+        'POSTAL_ADDRESS_CODE': 'D',
+        'POSTAL_ADDRESS_CODE_DESCRIPTION': 'A record which is linked to PAF',
+        'BLPU_STATE_CODE_DESCRIPTION': 'Unknown/Not applicable',
+        'TOPOGRAPHY_LAYER_TOID': 'osgb1000005715271',
+        'LAST_UPDATE_DATE': '10/02/2016',
+        'ENTRY_DATE': '19/03/2001',
+        'LANGUAGE': 'EN',
+        'MATCH': 1,
+        'MATCH_DESCRIPTION': 'EXACT',
+      },
+    },
+  ],
+};
+
 describe('Postcode Lookup Controller', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
@@ -14,6 +63,10 @@ describe('Postcode Lookup Controller', () => {
     nock(idamUrl)
       .post('/o/token')
       .reply(200, { id_token: citizenRoleToken });
+    nock(mockPostcodeServer)
+      .get(mockPostcodePath)
+      .reply(200, mockPostcodeLookupResponse);
+
   });
 
   test('should return list of addresses', async () => {
@@ -21,7 +74,7 @@ describe('Postcode Lookup Controller', () => {
       .get(POSTCODE_LOOKUP_URL + '?postcode=CV56GQ')
       .expect((req) => {
         expect(req.text).toContain('100070660705'); // uprn
-        expect(req.text).toContain('5, HIGHLAND ROAD'); // buildingNumber and thoroughfareName
+        expect(req.text).toContain('2, DALBERG ROAD, LONDON, SW2 1AN'); // buildingNumber and thoroughfareName
         expect(req.text).toContain('COVENTRY'); // postTown
         expect(req.text).toContain('CV5 6GQ'); // postcode
         expect(req.text).toContain('D'); // postcodeType
