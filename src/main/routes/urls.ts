a715623e81830e506f94e89b0a16569bf853f758
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-03-08 12:09:40 +0000

    added base url

--- src/main/routes/urls.ts
+++ src/main/routes/urls.ts
@@ -1,5 +1,6 @@
 export type Urls = `/${string}`;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const BASE_CASE_RESPONSE_URL: Urls = '/case/:id/response';
 export const CALLBACK_URL: Urls= '/oauth2/callback';
 export const SIGN_IN_URL: Urls = '/login';
@@ -17,3 +18,4 @@ export const CITIZEN_DETAILS_URL: Urls = '/case/:id/response/your-details';
 export const CONFIRM_CITIZEN_DETAILS_URL: Urls = '/confirm-your-details';
 export const CITIZEN_RESPONSE_TYPE: Urls = '/citizen-response-type';
 
+export const DUMMY_URL = `${BASE_CASE_RESPONSE_URL}/dummy/url `;
