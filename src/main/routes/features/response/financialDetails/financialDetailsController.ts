1a469ce1bc4b119d4e9274d8e85106d5e6fa50af
OsheaF <fergal.oshea@hmcts.net>
2022-03-10 14:20:20 +0000

    CIV-1241 Trying to use DraftStoreService

--- src/main/routes/features/response/financialDetails/financialDetailsController.ts
+++ src/main/routes/features/response/financialDetails/financialDetailsController.ts
@@ -3,18 +3,21 @@ import config from 'config';
 import {BASE_CASE_RESPONSE_URL, CITIZEN_BANK_ACCOUNT_URL, CLAIM_TASK_LIST, FINANCIAL_DETAILS} from '../../../urls';
 import {Claim} from '../../../../common/models/claim';
 import {CivilServiceClient} from '../../../../app/client/civilServiceClient';
+import {DraftStoreService} from '../../../../modules/draft-store/draftStoreService';
 import {Respondent} from '../../../../common/models/respondent';
 import {CounterpartyType} from '../../../../common/models/counterpartyType';
 import {AppRequest} from '../../../../common/models/AppRequest';
+import {CivilClaimResponse} from "models/civilClaimResponse";
 
 
 const financialDetailsViewPath = 'features/response/financialDetails/financial-details';
 const router = express.Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
-
+const draftStoreService : DraftStoreService = new DraftStoreService();
 
 let claim : Claim = new Claim();
+let civilClaimResponse : CivilClaimResponse = new CivilClaimResponse();
 let counterpartyType : CounterpartyType;
 
 function renderPage(res: express.Response, claim: Claim): void {
@@ -25,10 +28,13 @@ function renderPage(res: express.Response, claim: Claim): void {
 router.get(FINANCIAL_DETAILS, async (req, res) => {
   try {
     claim = await civilServiceClient.retrieveClaimDetails(<AppRequest>req,'1646818997929180');
-    claim = await civilServiceClient.retrieveClaimDetails(<AppRequest>req,'1646768947464020');
+    civilClaimResponse = await draftStoreService.getDraftClaimFromStore('1646768947464020');
     console.log('Respondent::' + JSON.stringify(claim.respondent1));
     counterpartyType = claim.respondent1.type;
     console.log('CounterpartyType::' + counterpartyType);
+    console.log('CivilClaimResponse Respondent::' + JSON.stringify(civilClaimResponse.case_data.respondent1));
+    counterpartyType = civilClaimResponse.case_data.respondent1.type;
+    console.log('CivilClaimResponse CounterpartyType::' + counterpartyType);
     renderPage(res, claim);
   } catch (e) {
     console.log(e);
