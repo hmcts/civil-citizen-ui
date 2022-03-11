b721e30482229586ef007057f086939f918166cb
OsheaF <fergal.oshea@hmcts.net>
2022-03-11 09:38:54 +0000

    CIV-1241 Adding unit tests

--- src/main/routes/features/response/financialDetails/financialDetailsController.ts
+++ src/main/routes/features/response/financialDetails/financialDetailsController.ts
@@ -1,19 +1,15 @@
 import * as express from 'express';
-import config from 'config';
 import {BASE_CASE_RESPONSE_URL, CITIZEN_BANK_ACCOUNT_URL, CLAIM_TASK_LIST, FINANCIAL_DETAILS} from '../../../urls';
 import {Claim} from '../../../../common/models/claim';
-import {CivilServiceClient} from '../../../../app/client/civilServiceClient';
 import {DraftStoreService} from '../../../../modules/draft-store/draftStoreService';
-// import {Respondent} from '../../../../common/models/respondent';
 import {CounterpartyType} from '../../../../common/models/counterpartyType';
-import {AppRequest} from '../../../../common/models/AppRequest';
+
 
 
 
 const financialDetailsViewPath = 'features/response/financialDetails/financial-details';
 const router = express.Router();
-const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
-const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
+
 const draftStoreService : DraftStoreService = new DraftStoreService();
 
 let claim : Claim = new Claim();
@@ -28,8 +24,6 @@ router.get(BASE_CASE_RESPONSE_URL + FINANCIAL_DETAILS, async (req, res) => {
   try {
     const id = req.params.id;
     console.log('Id:: ' + id);
-    claim = await civilServiceClient.retrieveClaimDetails(<AppRequest>req, id);
-    //console.log(claim);
     claim = await draftStoreService.getCaseDataFormStore(id);
     console.log(claim);
     console.log('Claim Respondent::' + JSON.stringify(claim.respondent1));
---
+++ src/test/unit/routes/features/response/financial-details/financialDetailsController.test.ts
@@ -0,0 +1,83 @@
+import request from 'supertest';
+import {app} from '../../../../../../main/app';
+import nock from 'nock';
+import config from 'config';
+import {BASE_CASE_RESPONSE_URL, FINANCIAL_DETAILS} from '../../../../../../main/routes/urls';
+import {CounterpartyType} from "../../../../../../main/common/models/counterpartyType";
+
+const claimIndividualMock = require('./claimIndividualMock.json');
+const claimOrganisationMock = require('./claimOrganisationMock.json');
+const claimIndividual: string = JSON.stringify(claimIndividualMock);
+const claimOrganisation: string = JSON.stringify(claimOrganisationMock);
+const mockDraftStore = {
+  set: jest.fn(() => Promise.resolve({})),
+  get: jest.fn(() => Promise.resolve(claimIndividual)),
+};
+
+jest.mock('../../../../../../main/modules/oidc');
+jest.mock('../../../../../../main/modules/draft-store');
+
+describe('Citizen financial details', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+  beforeEach(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+    jest.useFakeTimers();
+    //    jest.setTimeout(100000);
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  describe('on GET', () => {
+    test('should return individual financial details page', async () => {
+      app.locals.draftStoreClient = mockDraftStore;
+      await request(app)
+        .get(BASE_CASE_RESPONSE_URL + FINANCIAL_DETAILS)
+        .set('id', '1646818997929180')
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('details of your finances');
+        });
+    });
+    test('should return organisation financial details page', async () => {
+      const mockDraftStore = {
+        set: jest.fn(() => Promise.resolve({data: {}})),
+        get: jest.fn(() => Promise.resolve(claimOrganisation)),
+      };
+      app.locals.draftStoreClient = mockDraftStore;
+      await request(app)
+        .get(BASE_CASE_RESPONSE_URL + FINANCIAL_DETAILS)
+        .set('id', '1646768947464020')
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('your company or organisation&#39;s most recent statement of accounts');
+        });
+    });
+  });
+
+
+  describe('on POST', () => {
+    test('should redirect for individual',   () => {
+      request(app)
+        .post(BASE_CASE_RESPONSE_URL + FINANCIAL_DETAILS)
+        .send({counterpartyType: CounterpartyType.individual})
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toContain('case/:id/response/statement-of-means/bank-accounts');
+        });
+    });
+    test('should redirect for organisation',  () => {
+      request(app)
+        .post(BASE_CASE_RESPONSE_URL + FINANCIAL_DETAILS)
+        .send({counterpartyType: CounterpartyType.organisation})
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toContain('/case/:id/response/claim-task-list');
+        });
+    });
+  });
+});
