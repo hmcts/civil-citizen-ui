5971e67a88281dc2454616d6eb03536fdd76fcf2
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-03-11 17:53:46 +0000

    CIV-1263 fix a11y test and formatting

--- src/main/modules/statementOfMeans/bankAccounts/bankAccountConverter.ts
+++ src/main/modules/statementOfMeans/bankAccounts/bankAccountConverter.ts
@@ -2,25 +2,25 @@ import {BankAccounts} from '../../../common/form/models/bankAndSavings/bankAccou
 import {CitizenBankAccount} from '../../../common/models/citizenBankAccount';
 import {BankAccount} from '../../../common/form/models/bankAndSavings/bankAccount';
 
-export function convertFormToCitizenBankAccount(bankAccountForm: BankAccounts): CitizenBankAccount[]{
-  if(bankAccountsExist) {
+export function convertFormToCitizenBankAccount(bankAccountForm: BankAccounts): CitizenBankAccount[] {
+  if (bankAccountsExist) {
     const accounts = bankAccountForm.getOnlyCompletedAccounts();
-    if(accounts && accounts.length > 0) {
+    if (accounts && accounts.length > 0) {
       return accounts.map(account => (new CitizenBankAccount(account.typeOfAccount, account.joint, account.balance)));
     }
   }
 }
 
 export function convertCitizenBankAccountsToForm(citizenBankAccounts: CitizenBankAccount[]): BankAccounts {
-  if(citizenBankAccounts && citizenBankAccounts.length >0){
-    const accountsForForm = citizenBankAccounts.map(account =>  new BankAccount(account.typeOfAccount, String(account.joint), account.balance));
+  if (citizenBankAccounts && citizenBankAccounts.length > 0) {
+    const accountsForForm = citizenBankAccounts.map(account => new BankAccount(account.typeOfAccount, String(account.joint), account.balance));
     const bankAccountForm = new BankAccounts(accountsForForm);
     bankAccountForm.addEmptyRowsIfNotEnough();
     return bankAccountForm;
   }
-  return new BankAccounts( [new BankAccount(), new BankAccount()]);
+  return new BankAccounts([new BankAccount(), new BankAccount()]);
 }
 
-function bankAccountsExist(bankAccountForm: BankAccounts): boolean{
-  return bankAccountForm && bankAccountForm.accounts && bankAccountForm.accounts.length>0;
+function bankAccountsExist(bankAccountForm: BankAccounts): boolean {
+  return bankAccountForm && bankAccountForm.accounts && bankAccountForm.accounts.length > 0;
 }
