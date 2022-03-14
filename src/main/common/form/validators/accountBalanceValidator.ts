ae7afff4f65e222a71539ba41ed0f63c0bbdff7d
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-03-14 16:04:35 +0000

    CIV-1263 changed validation so that it does not allow 0 values or amount greater than 99999999999999999999999999

--- src/main/common/form/validators/accountBalanceValidator.ts
+++ src/main/common/form/validators/accountBalanceValidator.ts
@@ -1,5 +1,6 @@
 import {ValidatorConstraint, ValidatorConstraintInterface} from 'class-validator';
 import {VALID_TWO_DECIMAL_NUMBER} from '../validationErrors/errorMessageConstants';
+import {MAX_AMOUNT_VALUE} from './validationConstraints';
 
 /**
  * Validator for account balance that allows two decimal places and negative values
@@ -11,7 +12,8 @@ export class AccountBalanceValidator implements ValidatorConstraintInterface {
       return true;
     }
     const decimalPattern = /^-?\d*\.?\d{0,2}$/;
-    return decimalPattern.test(value);
+    const numericValue = Number(value);
+    return decimalPattern.test(value) && numericValue !==0 && numericValue < MAX_AMOUNT_VALUE ;
   }
   defaultMessage() {
     return VALID_TWO_DECIMAL_NUMBER;
---
+++ src/main/common/form/validators/validationConstraints.ts
@@ -0,0 +1,1 @@
+export const MAX_AMOUNT_VALUE: number = 999999999999999;
