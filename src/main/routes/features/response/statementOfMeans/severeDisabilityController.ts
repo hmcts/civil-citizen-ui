0d32f517b9ae06b34ccecdfff2900fec232dae50
laarmada <sergiosalmar@yahoo.es>
2022-03-14 11:19:59 +0000

    test fixed

--- src/main/routes/features/response/statementOfMeans/severeDisabilityController.ts
+++ src/main/routes/features/response/statementOfMeans/severeDisabilityController.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
 import {CITIZEN_RESIDENCE_URL, CITIZEN_SEVERELY_DISABLED_URL} from '../../../urls';
-import {Disability} from '../../../../common/form/models/statementOfMeans/disability';
+import {SevereDisability} from '../../../../common/form/models/statementOfMeans/severeDisability';
 import {ValidationError, Validator} from 'class-validator';
 import {SevereDisabilityService} from '../../../../modules/statementOfMeans/severeDisabilityService';
 
-const citizenDisabilityViewPath = 'features/response/statement-of-means/disability';
+const citizenSevereDisabilityViewPath = 'features/response/statement-of-means/are-you-severely-disabled';
 const router = express.Router();
-const severeDisability = new Disability();
+const severeDisability = new SevereDisability();
 const severeDisabilityService = new SevereDisabilityService();
 
-function renderView(form: Disability, res: express.Response): void {
-  res.render(citizenDisabilityViewPath, {form});
+function renderView(form: SevereDisability, res: express.Response): void {
+  res.render(citizenSevereDisabilityViewPath, {form});
 }
 
 router.get(CITIZEN_SEVERELY_DISABLED_URL.toString(), async (req, res) => {
@@ -21,7 +21,7 @@ router.get(CITIZEN_SEVERELY_DISABLED_URL.toString(), async (req, res) => {
 
 router.post(CITIZEN_SEVERELY_DISABLED_URL.toString(),
   (req, res) => {
-    const severeDisability: Disability = new Disability(req.body.disability);
+    const severeDisability: SevereDisability = new SevereDisability(req.body.severeDisability);
     const validator = new Validator();
     const errors: ValidationError[] = validator.validateSync(severeDisability);
     if (errors && errors.length > 0) {
--- src/test/unit/routes/features/response/statementOfMeans/severeDisabilityController.test.ts
+++ src/test/unit/routes/features/response/statementOfMeans/severeDisabilityController.test.ts
@@ -54,7 +54,7 @@ describe('SevereDisability', () => {
     app.locals.draftStoreClient = mockDraftStore;
     await request(app)
       .post(CITIZEN_SEVERELY_DISABLED_URL)
-      .send('disability=yes')
+      .send('severeDisability=yes')
       .expect((res) => {
         expect(res.status).toBe(302);
         expect(res.header.location).toEqual(CITIZEN_RESIDENCE_URL);
@@ -66,7 +66,7 @@ describe('SevereDisability', () => {
       app.locals.draftStoreClient = mockDraftStore;
       await request(app)
         .post(CITIZEN_SEVERELY_DISABLED_URL)
-        .send('disability=no')
+        .send('severeDisability=no')
         .expect((res) => {
           expect(res.status).toBe(302);
           expect(res.header.location).toEqual(CITIZEN_RESIDENCE_URL);
@@ -79,7 +79,7 @@ describe('SevereDisability', () => {
       app.locals.draftStoreClient = mockNoDisabilityDraftStore;
       await request(app)
         .post(CITIZEN_SEVERELY_DISABLED_URL)
-        .send('disability=no')
+        .send('severeDisability=no')
         .expect((res) => {
           expect(res.status).toBe(302);
           expect(res.header.location).toEqual(CITIZEN_RESIDENCE_URL);
