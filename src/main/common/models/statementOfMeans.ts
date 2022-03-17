90106c91d41e97bf24f6f55febb3e9286f25629e
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2022-03-17 12:17:28 +0100

    Feature/civ 1267 do you live with a partner (#224)
    * update contact-us-for-help macros
    
    * adjust inline comments
    
    * update contact-us-for-help.njk
    
    * update labels
    
    * remove webchat
    
    * update contact
    
    * update PR comments
    
    * solve PR comment
    
    * solve PR comment
    
    * formatting njk.
    
    * starting
    
    * add test
    
    * updating test
    
    * updating test
    
    * updating a11y error test
    
    * updating a11y error test
    
    * updating a11y error test
    
    * updating a11y error test
    
    * Error control message
    
    * updating test
    
    * creating a new macro
    
    * fix checked radio button
    
    * fix a11y test
    
    * remove eslint ignore
    
    * refactor directory statementOfMeans
    
    * refactor unit test
    
    * a11y test fixed
    
    * add redis get and set
    
    * add unit test
    
    * fix PR comment
    
    * add logger and exception control
    
    * merge from master
    
    * fix PR comments
    
    * fix a11y test
    
    * add url of DO YOU LIVE WITH PARTNER
    
    * add code and unit test
    
    * fix master conflicts
    
    * Update: delete space
    
    * Fix: disability to cohabiting
    
    * update response URL with Id param
    
    * Fix: a11y missing urls
    
    * Fix: partner urls
    
    * Fix: warning lint
    
    * Update: refactor
    
    * Fix: url partner age
    
    * Merge branch 'master' into feature/CIV-1267-do-you-live-with-a-partner
    
    * Update: try catch service
    
    * a11y fix urls
    
    * fix format
    
    Co-authored-by: laarmada <sergiosalmar@yahoo.es>
    Co-authored-by: Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>

--- src/main/common/models/statementOfMeans.ts
+++ src/main/common/models/statementOfMeans.ts
@@ -2,9 +2,12 @@ import {Disability} from '../form/models/statementOfMeans/disability';
 import {SevereDisability} from '../form/models/statementOfMeans/severeDisability';
 import { Partner } from '../form/models/statementOfMeans/partner';
 import {CitizenBankAccount} from './citizenBankAccount';
+import {Cohabiting} from '../form/models/statementOfMeans/partner/cohabiting';
+
 export class StatementOfMeans {
   disability?: Disability;
   severeDisability?: SevereDisability;
   partnerAge?: Partner;
   bankAccounts?: CitizenBankAccount[];
+  cohabiting?: Cohabiting;
 }
