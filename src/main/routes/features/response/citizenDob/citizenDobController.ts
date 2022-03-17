527e00d782d9b36ac1ba7f4441318970a03b1b0d
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-03-17 14:35:53 +0000

    added call of redis in the citizenDobController.ts

--- src/main/routes/features/response/citizenDob/citizenDobController.ts
+++ src/main/routes/features/response/citizenDob/citizenDobController.ts
@@ -5,42 +5,60 @@ import {DOB_URL, CITIZEN_PHONE_NUMBER_URL, AGE_ELIGIBILITY_URL} from '../../../.
 import {Respondent} from '../../../../common/models/respondent';
 import {Claim} from '../../../../common/models/claim';
 import {AgeEligibilityVerification} from '../../../../common/utils/ageEligibilityVerification';
+import {getCaseDataFromStore, saveDraftClaim} from '../../../../modules/draft-store/draftStoreService';
+import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
 
+const {Logger} = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('citizenDobController');
 
 const router = express.Router();
+
 function renderView(res: express.Response, form: CitizenDob): void {
   res.render('features/response/citizenDob/citizen-dob', {form: form});
 }
 
 function redirectToNextPage(req: express.Request, res: express.Response, dob: Date) {
   if (AgeEligibilityVerification.isOverEighteen(dob)) {
-    res.redirect(CITIZEN_PHONE_NUMBER_URL);
+    res.redirect(constructResponseUrlWithIdParams(req.params.id,CITIZEN_PHONE_NUMBER_URL));
   } else {
-    res.redirect(AGE_ELIGIBILITY_URL);
+    res.redirect(constructResponseUrlWithIdParams(req.params.id,AGE_ELIGIBILITY_URL));
   }
 }
 
-router.get(DOB_URL, (req: express.Request, res: express.Response) => {
-  const citizenDob = new CitizenDob();
-  renderView(res, citizenDob);
+router.get(DOB_URL, async (req: express.Request, res: express.Response) => {
+  try {
+    const citizenDob = new CitizenDob();
+    const responseDataRedis: Claim = await getCaseDataFromStore(req.params.id);
+    if (responseDataRedis && responseDataRedis.respondent1.dateOfBirth) {
+      const dateOfBirth =  new Date(responseDataRedis.respondent1.dateOfBirth);
+      citizenDob.day = dateOfBirth.getDay();
+      citizenDob.month = (dateOfBirth.getMonth() + 1);
+      citizenDob.year = dateOfBirth.getFullYear();
+    }
+    renderView(res, citizenDob);
+  } catch (error) {
+    logger.error(error);
+  }
+
 });
 
-router.post(DOB_URL, (req, res) => {
-  const citizenDob = new CitizenDob(req.body.year, req.body.month, req.body.day);
-  const validator = new Validator();
-  citizenDob.errors = validator.validateSync(citizenDob);
-  if (citizenDob.errors && citizenDob.errors.length > 0) {
-    renderView(res, citizenDob);
-  } else {
-    const respondent = new Respondent();
-    respondent.dateOfBirth = citizenDob.dateOfBirth;
-    const claim = new Claim();
-    claim.respondent1 = respondent;
-    claim.legacyCaseReference = 'dob';
-    const draftStoreClient = req.app.locals.draftStoreClient;
-    draftStoreClient.set(claim.legacyCaseReference, JSON.stringify(claim)).then(() => {
+router.post(DOB_URL.toString(), async (req, res) => {
+  try {
+    const citizenDob = new CitizenDob(req.body.year, req.body.month, req.body.day);
+    const validator = new Validator();
+    citizenDob.errors = validator.validateSync(citizenDob);
+    if (citizenDob.errors && citizenDob.errors.length > 0) {
+      renderView(res, citizenDob);
+    } else {
+      const respondent = new Respondent();
+      respondent.dateOfBirth = citizenDob.dateOfBirth;
+      const claim = new Claim();
+      claim.respondent1 = respondent;
+      await saveDraftClaim(req.params.id, claim);
       redirectToNextPage(req, res, respondent.dateOfBirth);
-    });
+    }
+  } catch (error) {
+    logger.error(error);
   }
 });
 
