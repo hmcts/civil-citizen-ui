d625cd5b295b747ba67d46ae287cbf2673e1fc8d
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-03-18 16:03:07 +0000

    CIV-1276 documentation and and formatting

--- src/main/common/models/employment.ts
+++ src/main/common/models/employment.ts
@@ -1,7 +1,6 @@
 import {EmploymentCategory} from '../../common/form/models/statementOfMeans/employment/employmentCategory';
 
-
 export interface Employment {
   declared: boolean;
-  employmentType: EmploymentCategory[]
+  employmentType: EmploymentCategory[];
 }
