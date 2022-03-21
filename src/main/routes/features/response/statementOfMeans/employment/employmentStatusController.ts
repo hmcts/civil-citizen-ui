695e69e46419b77f9b94ec83775377fd85746584
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-03-21 11:47:44 +0000

    CIV-1276 add response 500 and send error temporarily until we have an error page story

--- src/main/routes/features/response/statementOfMeans/employment/employmentStatusController.ts
+++ src/main/routes/features/response/statementOfMeans/employment/employmentStatusController.ts
@@ -45,7 +45,7 @@ router.get(CITIZEN_EMPLOYMENT_URL, async (req, res) => {
     renderView(form, res);
   } catch (error) {
     logger.error(`${(error as Error).stack || error}`);
-    res.status(500);
+    res.status(500).send({error: error.message});
   }
 });
 
@@ -61,7 +61,7 @@ router.post(CITIZEN_EMPLOYMENT_URL, async (req, res) => {
     }
   } catch (error) {
     logger.error(`${(error as Error).stack || error}`);
-    res.status(500);
+    res.status(500).send({error: error.message});
   }
 });
 
