abc1e0ef99ebdf55776c1ece037d92d706823fd0
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-03-23 13:35:13 +0000

    CIV-1274 redirect to other dependants

--- src/main/routes/features/response/statementOfMeans/dependants/dependantTeenagersController.ts
+++ src/main/routes/features/response/statementOfMeans/dependants/dependantTeenagersController.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
-import {DEPENDANT_TEENAGERS_URL} from '../../../../../routes/urls';
+import {DEPENDANT_TEENAGERS_URL, OTHER_DEPENDANTS_URL} from '../../../../../routes/urls';
 import {DependantTeenagers} from '../../../../../common/form/models/statementOfMeans/dependants/dependantTeenagers';
 import {validateForm} from '../../../../../common/form/validators/formValidator';
 import {saveToDraftStore} from '../../../../../modules/statementOfMeans/dependants/dependantTeenagersService';
+import {constructResponseUrlWithIdParams} from '../../../../../common/utils/urlFormatter';
 
 
 const {Logger} = require('@hmcts/nodejs-logging');
@@ -15,8 +16,6 @@ function renderView(form: DependantTeenagers, res: express.Response): void {
 }
 
 function convertToForm(req: express.Request) {
-  console.log(req.body.value);
-  console.log(req.body.maxValue);
   const value = req.body.value ? Number(req.body.value) : undefined;
   const maxValue = req.body.maxValue ? Number(req.body.maxValue) : undefined;
   return new DependantTeenagers(value, maxValue);
@@ -35,6 +34,7 @@ router.post(DEPENDANT_TEENAGERS_URL, async (req, res) => {
       renderView(form, res);
     } else {
       await saveToDraftStore(req.params.id, form);
+      res.redirect(constructResponseUrlWithIdParams(req.params.id, OTHER_DEPENDANTS_URL));
     }
   } catch (error) {
     logger.error(`${(error as Error).stack || error}`);
