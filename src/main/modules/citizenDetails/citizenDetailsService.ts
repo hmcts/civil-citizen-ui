73c17ac31ce5345d5180a1c26e7cf994a0d47599
laarmada <sergiosalmar@yahoo.es>
2022-03-31 21:50:58 +0100

    updating lint warning

--- src/main/modules/citizenDetails/citizenDetailsService.ts
+++ src/main/modules/citizenDetails/citizenDetailsService.ts
@@ -1,8 +1,5 @@
-import {
-  getCaseDataFromStore,
-  saveDraftClaim,
-} from '../../modules/draft-store/draftStoreService';
-import _, {get} from 'lodash';
+import {getCaseDataFromStore, saveDraftClaim} from '../../modules/draft-store/draftStoreService';
+import {get} from 'lodash';
 import {Respondent} from '../../common/models/respondent';
 import {Claim} from '../../common/models/claim';
 import {PrimaryAddress} from '../../common/models/primaryAddress';
@@ -19,16 +16,16 @@ export const getRespondentInformation = async (claimId: string): Promise<Respond
   return new Respondent();
 };
 
-export const saveRespondent = async(claimId: string, citizenAddress: CitizenAddress, citizenCorrespondenceAddress: CitizenCorrespondenceAddress) : Promise<void> => {
+export const saveRespondent = async (claimId: string, citizenAddress: CitizenAddress, citizenCorrespondenceAddress: CitizenCorrespondenceAddress): Promise<void> => {
   const responseData = await getCaseDataFromStore(claimId) || new Claim();
   if (!get(responseData, 'respondent1')) {
     const respondent = new Respondent();
     respondent.primaryAddress = buildPrimaryAddress(citizenAddress);
-    respondent.correspondenceAddress = citizenCorrespondenceAddress.isEmpty() ? undefined :  buildCorrespondenceAddress(citizenCorrespondenceAddress);
+    respondent.correspondenceAddress = citizenCorrespondenceAddress.isEmpty() ? undefined : buildCorrespondenceAddress(citizenCorrespondenceAddress);
     responseData.respondent1 = respondent;
   } else {
     responseData.respondent1.primaryAddress = buildPrimaryAddress(citizenAddress);
-    responseData.respondent1.correspondenceAddress = citizenCorrespondenceAddress.isEmpty() ?  undefined :  buildCorrespondenceAddress(citizenCorrespondenceAddress);
+    responseData.respondent1.correspondenceAddress = citizenCorrespondenceAddress.isEmpty() ? undefined : buildCorrespondenceAddress(citizenCorrespondenceAddress);
   }
   await saveDraftClaim(claimId, responseData);
 };
