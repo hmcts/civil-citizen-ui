8946b5d619582601d0f4a8bb5cc28f7fd69331fc
OsheaF <fergal.oshea@hmcts.net>
2022-04-01 12:34:04 +0100

    CIV-1360 Adding test for fractions - they should be ignored as invalid input

--- src/test/unit/common/form/models/statementOfMeans/dependants/numberOfChildren.test.ts
+++ src/test/unit/common/form/models/statementOfMeans/dependants/numberOfChildren.test.ts
@@ -10,6 +10,12 @@ describe('Number Of Children model tests', () => {
     //Then
     expect(numberOfChildren.totalNumberOfChildren()).toBe(0);
   });
+  it('should have no children when all fractions i.e. not Integers', () => {
+    //Given
+    const numberOfChildren = new NumberOfChildren(2.3, 1.4, 0.4);
+    //Then
+    expect(numberOfChildren.totalNumberOfChildren()).toBe(0);
+  });
   it('should have 2 children when under11 is 2, rest undefined', () => {
     //Given
     const numberOfChildren = new NumberOfChildren(2, undefined, undefined);
