3fa2771ed486354c2409f91878e97cf2806399e8
OsheaF <fergal.oshea@hmcts.net>
2022-04-04 14:57:50 +0100

    CIV-1360 Extra tests for hasChildrenDisabled - cohabiting

--- src/main/routes/urls.ts
+++ src/main/routes/urls.ts
@@ -37,6 +37,7 @@ export const CITIZEN_PAYMENT_OPTION_URL = `${BASE_CASE_RESPONSE_URL}/full-admiss
 export const CITIZEN_PAYMENT_DATE_URL = `${BASE_CASE_RESPONSE_URL}/full-admission/payment-date`;
 export const CITIZEN_DEPENDANTS_EDUCATION_URL = `${STATEMENT_OF_MEANS_URL}/dependants/education`;
 export const CHILDREN_DISABILITY_URL = `${CITIZEN_DEPENDANTS_URL}/children-disability`;
+export const CITIZEN_COURT_ORDER_URL = `${STATEMENT_OF_MEANS_URL}/court-orders`;
 
 
 
