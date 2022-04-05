e79b5379ac7ad7354e130accc9f16f72cd164307
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2022-04-05 11:47:22 +0100

    Make MoJ library work

--- src/main/index.js
+++ src/main/index.js
@@ -2,5 +2,7 @@ import './assets/scss/main.scss';
 import './assets/js/postcode-lookup.js';
 import './assets/js/append-row.js';
 import { initAll } from 'govuk-frontend';
+import './assets/js/jquery';
+import './assets/js/mojAll';
 
 initAll();
