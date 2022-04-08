8817ad1e0ed29d8599c0e4edaa78263f67ed0b70
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2022-04-08 16:24:41 +0200

    Civ 1364 who employs you (#298)
    * New: employers view (WIP)
    
    * New: who employs you (WIP)
    
    * Fix: validations
    
    * Fix: get only completed employers
    
    * Fix: validation errors
    
    * Fix: redirections and refactor
    
    * Fix: lint and prettier
    
    * Update: validation nested components
    
    * New: employer test (WIP)
    
    * Update: who employs you tests
    
    * Fix: use === instead ==
    
    * Fix: bug undefined
    
    * Update: delete comments
    
    * Update: formatting
    
    * Update: changes PR comments
    
    * Fix: test coverage
    
    * Fix: rename router
    
    * Update: delete unused method
    
    * Fix: merge urls
    
    Co-authored-by: Felipe <felipe@LAPTOP3721.V1.com>

--- src/main/common/form/validators/formValidator.ts
+++ src/main/common/form/validators/formValidator.ts
@@ -2,6 +2,7 @@ import {Validator} from 'class-validator';
 import {Form} from '../models/form';
 
 const validator = new Validator();
+const ERROR_FIELD = 'errors';
 
 export async function validateForm(form: Form) {
   const errors = await validator.validate(form);
@@ -9,9 +10,24 @@ export async function validateForm(form: Form) {
 }
 
 export async function validateFormArray(forms: Form[]) {
-  if (forms?.length > 0) {
+  if (forms?.length) {
     for (const form of forms) {
       await validateForm(form);
+    }
+  }
+}
+
+export async function validateFormNested(form: any) {
+  const errors = await validator.validate(form);
+  form.errors = errors;
+  if (errors.length) {
+    const keys = Object.keys(form);
+    for (const key of keys) {
+      if (key !== ERROR_FIELD && form[key] instanceof Array){
+        for (const element of form[key]) {
+          await validateFormNested(element);
+        }
+      }
     }
   }
 }
--- src/test/unit/routes/features/response/statementOfMeans/employment/selfEmployedAsController.test.ts
+++ src/test/unit/routes/features/response/statementOfMeans/employment/selfEmployedAsController.test.ts
@@ -2,7 +2,7 @@ import {app} from '../../../../../../../main/app';
 import request from 'supertest';
 import config from 'config';
 import nock from 'nock';
-import {SELF_EMPLOYED_URL,ON_TAX_PAYMENTS_URL} from '../../../../../../../main/routes/urls';
+import {CITIZEN_SELF_EMPLOYED_URL,ON_TAX_PAYMENTS_URL} from '../../../../../../../main/routes/urls';
 import {TestMessages} from '../../../../../../utils/errorMessageTestConstants';
 import {mockCivilClaim, mockRedisFailure} from '../../../../../../utils/mockDraftStore';
 import {
@@ -28,7 +28,7 @@ describe('Self Employed As', () => {
 describe('on Get', () => {
   test('should return on self employed page successfully', async () => {
     app.locals.draftStoreClient = mockCivilClaim;
-    await request(app).get(SELF_EMPLOYED_URL)
+    await request(app).get(CITIZEN_SELF_EMPLOYED_URL)
       .expect((res) => {
         expect(res.status).toBe(200);
         expect(res.text).toContain('What are you self-employed as?');
@@ -37,7 +37,7 @@ describe('on Get', () => {
   test('should return 500 status code when error occurs', async () => {
     app.locals.draftStoreClient = mockRedisFailure;
     await request(app)
-      .get(SELF_EMPLOYED_URL)
+      .get(CITIZEN_SELF_EMPLOYED_URL)
       .expect((res) => {
         expect(res.status).toBe(500);
         expect(res.body).toEqual({error: TestMessages.REDIS_FAILURE});
@@ -48,7 +48,7 @@ describe('on Get', () => {
 describe('on Post', () => {
   test('should return error when no input text is filled', async () => {
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send('')
       .expect((res) => {
         expect(res.status).toBe(200);
@@ -58,7 +58,7 @@ describe('on Post', () => {
   });
   test('should return errors when job title is defined and amount is not defined', async () => {
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send({jobTitle: 'Developer', annualTurnover: undefined})
       .expect((res) => {
         expect(res.status).toBe(200);
@@ -67,7 +67,7 @@ describe('on Post', () => {
   });
   test('should return errors when job title is defined and amount is -1', async () => {
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send({jobTitle: 'Developer', annualTurnover: -1})
       .expect((res) => {
         expect(res.status).toBe(200);
@@ -76,7 +76,7 @@ describe('on Post', () => {
   });
   test('should return errors when job title is defined and amount is 0', async () => {
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send({jobTitle: 'Developer', annualTurnover: 0})
       .expect((res) => {
         expect(res.status).toBe(200);
@@ -85,7 +85,7 @@ describe('on Post', () => {
   });
   test('should return errors when job title is defined and amount has more than two decimal places', async () => {
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send({jobTitle: 'Developer', annualTurnover: 50.555})
       .expect((res) => {
         expect(res.status).toBe(200);
@@ -94,7 +94,7 @@ describe('on Post', () => {
   });
   test('should return errors when job title is not defined and amount is defined', async () => {
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send({jobTitle: undefined, annualTurnover: 70000})
       .expect((res) => {
         expect(res.status).toBe(200);
@@ -104,7 +104,7 @@ describe('on Post', () => {
   test('should redirect with valid input', async () => {
     app.locals.draftStoreClient = mockCivilClaim;
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send({jobTitle: 'Developer', annualTurnover: 70000})
       .expect((res) => {
         expect(res.status).toBe(302);
@@ -114,7 +114,7 @@ describe('on Post', () => {
   test('should return status 500 when there is error', async () => {
     app.locals.draftStoreClient = mockRedisFailure;
     await request(app)
-      .post(SELF_EMPLOYED_URL)
+      .post(CITIZEN_SELF_EMPLOYED_URL)
       .send({jobTitle: 'Developer', annualTurnover: 70000})
       .expect((res) => {
         expect(res.status).toBe(500);
