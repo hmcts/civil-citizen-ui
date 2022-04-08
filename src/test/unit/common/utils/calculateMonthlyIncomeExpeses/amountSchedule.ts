afdf0b070009ee8db6ec2fd6a18f7a928b187eca
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-04-08 14:37:40 +0100

    CIV-1853 add calculation of totals

--- src/test/unit/common/utils/calculateMonthlyIncomeExpeses/amountSchedule.ts
+++ src/test/unit/common/utils/calculateMonthlyIncomeExpeses/amountSchedule.ts
@@ -4,12 +4,25 @@ export class AmountSchedule {
   static readonly WEEK = new AmountSchedule(ScheduledExpenses.WEEK, 52 / 12);
   static readonly TWO_WEEKS = new AmountSchedule(ScheduledExpenses.TWO_WEEKS, 52 / 12 / 2);
   static readonly FOUR_WEEKS = new AmountSchedule(ScheduledExpenses.FOUR_WEEKS, 52 / 12 / 4);
-  static readonly MONTH = new AmountSchedule('MONTH', 1);
-  name: ScheduledExpenses;
+  static readonly MONTH = new AmountSchedule(ScheduledExpenses.MONTH, 1);
+  name: string;
   valueInMonth: number;
 
   constructor(name: ScheduledExpenses, valueInMonth: number) {
     this.name = name;
-    this.
+    this.valueInMonth = valueInMonth;
+  }
+
+  public static getAll(): AmountSchedule[] {
+    return [
+      AmountSchedule.WEEK,
+      AmountSchedule.TWO_WEEKS,
+      AmountSchedule.FOUR_WEEKS,
+      AmountSchedule.MONTH,
+    ];
+  }
+
+  public static getSchedule(name: string): AmountSchedule | undefined {
+    return AmountSchedule.getAll().find(schedule => schedule.name === name);
   }
 }
--- src/test/unit/common/utils/calculateMonthlyIncomeExpeses/monthlyIncomeExpensesCalculator.ts
+++ src/test/unit/common/utils/calculateMonthlyIncomeExpeses/monthlyIncomeExpensesCalculator.ts
@@ -1,4 +1,23 @@
-export interface AmountPerSchedule {
+import {AmountSchedule} from './amountSchedule';
+
+export interface ScheduledAmount {
   amount: number;
   schedule: string;
 }
+
+const calculateTotalAmount = (amountsPerSchedule: ScheduledAmount[]): string => {
+  let totalAmount = 0;
+  if (amountsPerSchedule.length > 0) {
+    amountsPerSchedule.forEach(scheduledAmount => {
+      const schedule = AmountSchedule.getSchedule(scheduledAmount.schedule);
+      if (schedule) {
+        totalAmount = totalAmount + (scheduledAmount.amount * schedule.valueInMonth);
+      }
+    });
+  }
+  return totalAmount.toPrecision(2);
+};
+
+export {
+  calculateTotalAmount,
+};
