7ff89966b67c418d69c9a3427082762b3f397045
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-04-11 15:31:14 +0100

    CIV-1853 refactor calculate-amount.js

--- src/main/assets/js/calculate-amount.js
+++ src/main/assets/js/calculate-amount.js
@@ -1,31 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
   if (document.getElementsByClassName('amountRow')) {
     document.querySelectorAll('.amountRow .amount')?.forEach(element => element.addEventListener('change', getCalculation));
     document.querySelectorAll('.amountRow .schedule')?.forEach(element => element.addEventListener('change', getCalculation));
-    getCalculation();
+    await getCalculation();
   }
 
   function getSelectedInput(inputs) {
     let selected;
-    if (inputs?.length) {
-      for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked) {
-          selected = inputs[i];
-          break;
-        }
+    for (let i = 0; i < inputs?.length; i++) {
+      if (inputs[i].checked) {
+        selected = inputs[i];
+        break;
       }
     }
     return selected;
   }
 
-  function getCalculation() {
+  function inputsHaveValues(amount, selectedSchedule) {
+    return amount?.length && selectedSchedule && amount[0].value && selectedSchedule.value;
+  }
+
+  async function getCalculation() {
     const data = [];
     const amountRows = Array.from(document.getElementsByClassName('amountRow'));
     amountRows.forEach(element => {
       const amount = element.getElementsByClassName('amount');
       const schedules = element.querySelectorAll('.schedule input');
       const selectedSchedule = getSelectedInput(schedules);
-      if (amount?.length && selectedSchedule && amount[0].value && selectedSchedule.value) {
+      if (inputsHaveValues(amount, selectedSchedule)) {
         data.push({amount: amount[0].value, schedule: selectedSchedule.value});
       }
     });
@@ -37,11 +39,9 @@ document.addEventListener('DOMContentLoaded', function () {
       body: JSON.stringify(data),
     };
     if (data.length > 0) {
-      fetch('/total-income-expense-calculation', options).then((response) => {
-        return response.json();
-      }).then(data => {
-        document.getElementsByClassName('total-monthly-income-expense')[0].innerHTML = data;
-      });
+      const response = await fetch('/total-income-expense-calculation', options);
+      const data = await response.json();
+      document.getElementsByClassName('total-monthly-income-expense')[0].innerHTML = data;
     }
   }
 });
