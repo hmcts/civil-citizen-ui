bc6dd67c0c3ded173aa5ddbc354cf47c215c3c79
OsheaF <fergal.oshea@hmcts.net>
2022-04-12 11:32:05 +0100

    CIV-1135 Adding tests to test that date is prepopulated, if it exists in Redis

--- src/test/unit/routes/features/response/admission/fullAdmission/paymentOption/paymentDateController.test.ts
+++ src/test/unit/routes/features/response/admission/fullAdmission/paymentOption/paymentDateController.test.ts
@@ -78,6 +78,21 @@ describe('Payment date', () => {
         .expect((res) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain('What date will you pay on?');
+          expect(res.text).toContain('name="year" type="text" pattern="[0-9]*"');
+          expect(res.text).toContain('name="month" type="text" pattern="[0-9]*"');
+          expect(res.text).toContain('name="day" type="text" pattern="[0-9]*"');
+        });
+    });
+    test('should return payment date page with payment date loaded from Redis', async () => {
+      app.locals.draftStoreClient = mockCivilClaim;
+      await request(app)
+        .get(CITIZEN_PAYMENT_DATE_URL)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('What date will you pay on?');
+          expect(res.text).toContain('name="year" type="text" value="2025" pattern="[0-9]*');
+          expect(res.text).toContain('name="month" type="text" value="6" pattern="[0-9]*');
+          expect(res.text).toContain('name="day" type="text" value="1" pattern="[0-9]*');
         });
     });
   });
