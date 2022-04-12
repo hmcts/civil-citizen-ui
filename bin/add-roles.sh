58258ac0bc99c2846aa29764f7e64463d947108e
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-04-12 13:57:44 +0100

    add ccd add role script

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 # User used during the CCD import and ccd-role creation
-./bin/utils/ccd-add-role.sh "caseworker-civil"
-./bin/utils/ccd-add-role.sh "caseworker-caa"
-./bin/utils/ccd-add-role.sh "caseworker-approver"
-./bin/utils/ccd-add-role.sh "prd-admin"
-
-roles=("solicitor" "systemupdate" "admin" "staff")
-for role in "${roles[@]}"
-do
-  ./bin/utils/ccd-add-role.sh "caseworker-civil-${role}"
-done
+./bin/ccd-add-role.sh "citizen"
+./bin/ccd-add-role.sh "citizen-ui"
+./bin/ccd-add-role.sh "caseworker-cmc"
+./bin/ccd-add-role.sh "caseworker-cmc-systemupdate"
+./bin/ccd-add-role.sh "caseworker-cmc-pcqextractor"
+./bin/ccd-add-role.sh "payments"
+./bin/ccd-add-role.sh "caseworker-cmc-legaladvisor"
+./bin/ccd-add-role.sh "letter-holder"
+./bin/ccd-add-role.sh "caseworker-cmc-solicitor"
+./bin/ccd-add-role.sh "caseworker-cmc-anonymouscitizen"
+./bin/ccd-add-role.sh "caseworker-cmc-judge"
+./bin/ccd-add-role.sh "payments-refund"
+./bin/ccd-add-role.sh "payments-refund-approver"
