3746f96646fc3af592c7dcf1128ce9b63180f128
LeonardoPalmeiro <93268657+LeonardoPalmeiro@users.noreply.github.com>
2022-04-12 10:19:43 +0100

    Feature/civ 1476 do you have loans or credit card debts (#299)
    * initial commit
    
    * add control and models
    
    * added controller and html
    
    * add validations
    
    * add validations
    
    * added nestedValidator
    
    * added nestedValidator
    
    * removed metadata
    
    * removed metadata
    
    * master version
    
    * add column-full
    
    * ident the code
    
    * add currencyValidator.ts
    
    * add "add a debt button"
    
    * fixed add debt button
    
    * add redis connection
    
    * add unittest
    
    * added unit tests
    
    * fixed unit test and add debt url to ignored-urls.ts
    
    * fixed a11y test
    
    * renamed total owned and add unit test
    
    * fixed unit test
    
    * fixed unit test
    
    * add unit test to form
    
    * add unit test to form
    
    * fixed unit test and renamed controller
    
    * fixed unit test and renamed controller
    
    * fixed unit test and renamed controller
    
    * fixed comment in PR
    
    * fixed QA issue
    
    * removed comment
    
    * fix validation
    
    * fix validation

---
+++ src/main/common/form/models/statementOfMeans/debts/debtItems.ts
@@ -0,0 +1,40 @@
+import {IsNotEmpty, Validate, ValidateIf} from 'class-validator';
+import {
+  ENTER_A_DEBT, VALID_NUMBER_OF_PEOPLE,
+} from '../../../../form/validationErrors/errorMessageConstants';
+import {Form} from '../../../../../common/form/models/form';
+import {CurrencyValidator} from '../../../../../common/form/validators/currencyValidator';
+
+export class DebtItems extends Form{
+
+  @ValidateIf(o => o.isAtLeastOneFieldPopulated())
+  @IsNotEmpty({message: ENTER_A_DEBT})
+    debt: string;
+
+  @ValidateIf(o => o.isAtLeastOneFieldPopulated())
+  @IsNotEmpty({message: VALID_NUMBER_OF_PEOPLE})
+  @Validate(CurrencyValidator)
+    totalOwned: string;
+
+  @ValidateIf(o => o.isAtLeastOneFieldPopulated())
+  @IsNotEmpty({message: VALID_NUMBER_OF_PEOPLE})
+  @Validate(CurrencyValidator)
+    monthlyPayments: string;
+
+
+  constructor(debt: string, totalOwned: string, monthlyPayments: string) {
+    super();
+    this.debt = debt;
+    this.totalOwned = totalOwned;
+    this.monthlyPayments = monthlyPayments;
+  }
+
+  public isEmpty (): boolean {
+    return Object.values(this).every(value => value === undefined || value === '' || value === []);
+  }
+
+  isAtLeastOneFieldPopulated (): boolean {
+    return !this.isEmpty();
+  }
+
+}
---
+++ src/test/unit/routes/features/response/statementOfMeans/debts/debtsController.test.ts
@@ -0,0 +1,260 @@
+import request from 'supertest';
+import {app} from '../../../../../../../main/app';
+import nock from 'nock';
+import config from 'config';
+import {CITIZEN_MONTHLY_EXPENSES_URL, DEBTS_URL} from '../../../../../../../main/routes/urls';
+import * as draftStoreService from '../../../../../../../main/modules/draft-store/draftStoreService';
+import {
+  buildDebtFormNo,
+  buildDebtFormUndefined,
+  buildDebtFormYes,
+  buildDebtFormYesWithDebtEmpty,
+  buildDebtFormYesWithEmptyItems,
+  buildDebtFormYesWithoutItems,
+  buildDebtFormYesWithTotalOwnedEmpty,
+  buildDebtFormYesWithTotalOwnedInvalid,
+  buildDebtFormYesWithTotalOwnedZero,
+} from '../../../../../../utils/mockForm';
+import {Claim} from '../../../../../../../main/common/models/claim';
+import {StatementOfMeans} from '../../../../../../../main/common/models/statementOfMeans';
+import {
+  ENTER_A_DEBT,
+  ENTER_AT_LEAST_ONE_DEBT, REDIS_FAILURE, VALID_NUMBER_OF_PEOPLE, VALID_TWO_DECIMAL_NUMBER,
+  VALID_YES_NO_OPTION,
+} from '../../../../../../../main/common/form/validationErrors/errorMessageConstants';
+
+jest.mock('../../../../../../../main/modules/oidc');
+jest.mock('../../../../../../../main/modules/draft-store/draftStoreService');
+const mockGetCaseData = draftStoreService.getCaseDataFromStore as jest.Mock;
+
+
+describe('Debts', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+  beforeEach(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+  });
+  describe('on Exception', () => {
+    test('should return http 500 when has error in the get method', async () => {
+      mockGetCaseData.mockImplementation(async () => {
+        throw new Error(REDIS_FAILURE);
+      });
+      await request(app)
+        .get(DEBTS_URL)
+        .expect((res) => {
+          expect(res.status).toBe(500);
+          expect(res.body).toEqual({error: REDIS_FAILURE});
+        });
+    });
+  });
+
+  test('should return http 500 when has error in the post method', async () => {
+    mockGetCaseData.mockImplementation(async () => {
+      throw new Error(REDIS_FAILURE);
+    });
+    await request(app)
+      .post(DEBTS_URL)
+      .send(buildDebtFormYes())
+      .expect((res) => {
+        expect(res.status).toBe(500);
+        expect(res.body).toEqual({error: REDIS_FAILURE});
+      });
+  });
+  describe('on GET', () => {
+    test('should open the debts page when in redis has no data ', async () => {
+      mockGetCaseData.mockImplementation(async () => undefined);
+      await request(app)
+        .get(DEBTS_URL)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Do you have loans or credit card debts?');
+        });
+    });
+    test('should open the debts page when in redis has data with option yes', async () => {
+      mockGetCaseData.mockImplementation(async () => {
+        const claim = new Claim();
+        const statementOfMeans = new StatementOfMeans();
+        claim.statementOfMeans = statementOfMeans;
+        claim.statementOfMeans.debts = buildDebtFormYes();
+        return claim;
+      });
+      await request(app)
+        .get(DEBTS_URL)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Do you have loans or credit card debts?');
+        });
+    });
+    test('should open the debts page when in redis has data with option no', async () => {
+      mockGetCaseData.mockImplementation(async () => {
+        const claim = new Claim();
+        const statementOfMeans = new StatementOfMeans();
+        claim.statementOfMeans = statementOfMeans;
+        claim.statementOfMeans.debts = buildDebtFormNo();
+        return claim;
+      });
+      await request(app)
+        .get(DEBTS_URL)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Do you have loans or credit card debts?');
+        });
+    });
+  });
+
+  describe('on POST', () => {
+    test('should validate when has no option selected', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormUndefined())
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toMatch(VALID_YES_NO_OPTION);
+        });
+    });
+    test('should validate when has option is yes but there is no fields selected ', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithoutItems())
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toMatch(ENTER_AT_LEAST_ONE_DEBT);
+        });
+    });
+    test('should validate when has option is yes but debt is empty ', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithDebtEmpty())
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toMatch(ENTER_A_DEBT);
+        });
+    });
+    test('should validate when has option is yes but Total owned is invalid ', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithTotalOwnedInvalid())
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toMatch(VALID_TWO_DECIMAL_NUMBER);
+        });
+    });
+
+    test('should validate when has option is yes but Total owned is zero ', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithTotalOwnedZero())
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toMatch(VALID_NUMBER_OF_PEOPLE);
+        });
+    });
+    test('should validate when has option is yes but Total owned is empty ', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithTotalOwnedEmpty())
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toMatch(VALID_NUMBER_OF_PEOPLE);
+        });
+    });
+
+    test('should should redirect to when option is no when there is no data on redis', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormNo())
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(CITIZEN_MONTHLY_EXPENSES_URL);
+        });
+    });
+
+    test('should should redirect to when option is yes when there is no data on redis', async () => {
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYes())
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(CITIZEN_MONTHLY_EXPENSES_URL);
+        });
+    });
+
+    test('should should redirect to when option is yes when has data on redis', async () => {
+      mockGetCaseData.mockImplementation(async () => {
+        const claim = new Claim();
+        const statementOfMeans = new StatementOfMeans();
+        claim.statementOfMeans = statementOfMeans;
+        claim.statementOfMeans.debts = buildDebtFormYes();
+        return claim;
+      });
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYes())
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(CITIZEN_MONTHLY_EXPENSES_URL);
+        });
+    });
+
+    test('should should redirect to when option is no when has data on redis', async () => {
+      mockGetCaseData.mockImplementation(async () => {
+        const claim = new Claim();
+        const statementOfMeans = new StatementOfMeans();
+        claim.statementOfMeans = statementOfMeans;
+        claim.statementOfMeans.debts = buildDebtFormNo();
+        return claim;
+      });
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormNo())
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(CITIZEN_MONTHLY_EXPENSES_URL);
+        });
+    });
+
+    test('should should redirect to when option is yes but has empty items', async () => {
+      mockGetCaseData.mockImplementation(async () => {
+        const claim = new Claim();
+        const statementOfMeans = new StatementOfMeans();
+        claim.statementOfMeans = statementOfMeans;
+        claim.statementOfMeans.debts = buildDebtFormNo();
+        return claim;
+      });
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithEmptyItems())
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(CITIZEN_MONTHLY_EXPENSES_URL);
+        });
+    });
+
+    test('should should redirect to when option is yes but there is no StatementOfMeans on claim', async () => {
+      mockGetCaseData.mockImplementation(async () => {
+        const claim = new Claim();
+        claim.statementOfMeans = undefined;
+        return claim;
+      });
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithEmptyItems())
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(CITIZEN_MONTHLY_EXPENSES_URL);
+        });
+    });
+    test('should should redirect to monthly expenses when option is yes but claim is undefined', async () => {
+      mockGetCaseData.mockImplementation(async () => new Claim());
+      await request(app)
+        .post(DEBTS_URL)
+        .send(buildDebtFormYesWithEmptyItems())
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(CITIZEN_MONTHLY_EXPENSES_URL);
+        });
+    });
+  });
+});
