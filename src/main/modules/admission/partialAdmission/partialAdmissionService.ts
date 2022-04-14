0e85ba2ec431e89050d2119403a02e059590dc3d
commitBlob <maro.radovic12@gmail.com>
2022-04-14 11:22:23 +0100

    feat(CIV-1122): cleanup
    remove unused code

--- src/main/modules/admission/partialAdmission/partialAdmissionService.ts
+++ src/main/modules/admission/partialAdmission/partialAdmissionService.ts
@@ -1,12 +1,8 @@
 import {getCaseDataFromStore, saveDraftClaim} from 'modules/draft-store/draftStoreService';
 import {Claim} from 'models/claim';
-import {AlreadyPaid} from 'common/form/models/admission/partialAdmission/alreadyPaid';
-import {GenericForm} from 'common/form/models/genericForm';
-import {Validator} from 'class-validator';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('partialAdmissionService');
-const validator = new Validator();
 
 export class PartialAdmissionService {
   public async getClaimAlreadyPaid(claimId: string) {
@@ -17,12 +13,6 @@ export class PartialAdmissionService {
       logger.error(error);
       throw error;
     }
-  }
-
-  public validateAlreadyPaid(alreadyPaid: AlreadyPaid): GenericForm<AlreadyPaid> {
-    const form: GenericForm<AlreadyPaid> = new GenericForm(alreadyPaid);
-    form.errors = validator.validateSync(form.model);
-    return form;
   }
 
   public async saveClaimAlreadyPaid(claimId: string, alreadyPaid: boolean): Promise<void> {
