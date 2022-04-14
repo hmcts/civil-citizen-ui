2e7805bebcf37c2c1c09c21d4919b8bbe6e77286
commitBlob <maro.radovic12@gmail.com>
2022-04-14 11:20:39 +0100

    feat(CIV-1122): validation and redirect
    handle form submission

--- src/main/routes/features/response/admision/partialAdmission/alreadyPaidController.ts
+++ src/main/routes/features/response/admision/partialAdmission/alreadyPaidController.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
-import {CITIZEN_ALREADY_PAID_URL} from '../../../../urls';
+import {CLAIM_TASK_LIST_URL, CITIZEN_ALREADY_PAID_URL} from '../../../../urls';
 import {AlreadyPaid} from 'common/form/models/admission/partialAdmission/alreadyPaid';
 import {PartialAdmissionService} from 'modules/admission/partialAdmission/partialAdmissionService';
 import {GenericForm} from 'common/form/models/genericForm';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 
 const alreadyPaidController = express.Router();
 const citizenAlreadyPaidViewPath = 'features/response/admission/partialAdmission/already-paid';
@@ -17,6 +18,23 @@ alreadyPaidController.get(CITIZEN_ALREADY_PAID_URL, async (req, res) => {
     const alreadyPaid = new AlreadyPaid(await partialAdmissionService.getClaimAlreadyPaid(req.params.id));
     const alreadyPaidForm = new GenericForm(alreadyPaid);
     renderView(alreadyPaidForm , res);
+  } catch (error) {
+    res.status(500).send({error: error.message});
+  }
+});
+
+alreadyPaidController.post(CITIZEN_ALREADY_PAID_URL, async (req, res) => {
+  try {
+    const alreadyPaidData = new AlreadyPaid(req.body.alreadyPaid);
+    const alreadyPaidForm = new GenericForm(alreadyPaidData);
+    await alreadyPaidForm.validate();
+
+    if (alreadyPaidForm.hasErrors()) {
+      renderView(alreadyPaidForm, res);
+    } else {
+      await partialAdmissionService.saveClaimAlreadyPaid(req.params.id, alreadyPaidData.alreadyPaid);
+      res.redirect(constructResponseUrlWithIdParams(req.params.id, CLAIM_TASK_LIST_URL));
+    }
   } catch (error) {
     res.status(500).send({error: error.message});
   }
