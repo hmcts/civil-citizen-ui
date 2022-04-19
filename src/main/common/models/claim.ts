6736f1b6619c0bbfaa75f91dd7c7a7311fe42d69
Kadir <kegilmez@gmail.com>
2022-04-19 11:12:15 +0100

    changed the file name

--- src/main/common/models/claim.ts
+++ src/main/common/models/claim.ts
@@ -4,7 +4,7 @@ import {Respondent} from './respondent';
 import {StatementOfMeans} from './statementOfMeans';
 import {CounterpartyType} from './counterpartyType';
 import {NumberOfDays} from '../form/models/numberOfDays';
-import {PartialAdmission} from './PartialAdmission';
+import {PartialAdmission} from './partialAdmission';
 
 export const MAX_CLAIM_AMOUNT = 10000;
 
