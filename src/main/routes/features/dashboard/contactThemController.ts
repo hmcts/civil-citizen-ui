e815484412d58f725f2b196f68d2a16103e93877
laarmada <sergiosalmar@yahoo.es>
2022-04-20 14:25:46 +0100

    optimize code.

--- src/main/routes/features/dashboard/contactThemController.ts
+++ src/main/routes/features/dashboard/contactThemController.ts
@@ -2,22 +2,21 @@ import * as express from 'express';
 import {CITIZEN_CONTACT_THEM_URL} from '../../urls';
 
 import {getDraftClaimFromStore} from '../../../modules/draft-store/draftStoreService';
-import {GenericForm} from '../../../common/form/models/genericForm';
 import {Claim} from '../../../common/models/claim';
 
-const citizenEmploymentStatusViewPath = 'features/dashboard/contact-them';
+const citizenContactThemViewPath = 'features/dashboard/contact-them';
 const contactThemController = express.Router();
 
 let claim = new Claim();
 
-function renderView(form: GenericForm<Claim>, res: express.Response): void {
-  res.render(citizenEmploymentStatusViewPath, {form: form});
+function renderView(form: Claim, res: express.Response): void {
+  res.render(citizenContactThemViewPath, {form: form});
 }
 
 contactThemController.get(CITIZEN_CONTACT_THEM_URL, async (req, res) => {
   try {
     claim = await getDraftClaimFromStore(req.params.id);
-    renderView(new GenericForm(claim), res);
+    renderView(claim, res);
   } catch (error) {
     res.status(500).send({error: error.message});
   }
