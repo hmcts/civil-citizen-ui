9cc2a023d3752b416438050472af43d33bc21265
OsheaF <fergal.oshea@hmcts.net>
2022-04-21 15:48:03 +0100

    CIV-1124 Adding service and controller

---
+++ src/main/modules/admission/partialAdmission/howMuchHaveYouPaidService.ts
@@ -0,0 +1,48 @@
+import {getCaseDataFromStore, saveDraftClaim} from '../../../modules/draft-store/draftStoreService';
+import {HowMuchHaveYouPaid} from '../../../common/form/models/admission/partialAdmission/howMuchHaveYouPaid';
+import {Claim} from '../../../common/models/claim';
+import {PartialAdmission} from '../../../common/models/partialAdmission';
+
+const {Logger} = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('paymentDateService');
+
+
+class HowMuchHaveYouPaidService {
+
+  public async getHowMuchHaveYouPaid(claimId: string): Promise<HowMuchHaveYouPaid> {
+    try {
+      const claim = await getCaseDataFromStore(claimId);
+      if (claim.partialAdmission?.howMuchHaveYouPaid) {
+        return claim.partialAdmission.howMuchHaveYouPaid;
+      }
+      return new HowMuchHaveYouPaid();
+    } catch (error) {
+      logger.error(error);
+      throw error;
+    }
+  }
+
+  public async saveHowMuchHaveYouPaid(claimId: string, howMuchHaveYouPaid: HowMuchHaveYouPaid) {
+    try {
+      const case_data = await getCaseDataFromStore(claimId) || new Claim();
+      if (case_data.partialAdmission) {
+        case_data.partialAdmission.howMuchHaveYouPaid = howMuchHaveYouPaid;
+      } else {
+        case_data.partialAdmission = new PartialAdmission();
+        case_data.partialAdmission.howMuchHaveYouPaid = howMuchHaveYouPaid;
+      }
+      await saveDraftClaim(claimId, case_data);
+    } catch (error) {
+      logger.error(error);
+      throw error;
+    }
+  }
+
+  public buildHowMuchHaveYouPaid(amount?: number, totalAmount?: number, year?: string, month?: string, day?: string, text?: string): HowMuchHaveYouPaid {
+    return new HowMuchHaveYouPaid(amount, totalAmount, year, month, day, text);
+  }
+
+}
+
+const howMuchHaveYouPaidService = new HowMuchHaveYouPaidService();
+export default howMuchHaveYouPaidService;
---
+++ src/main/routes/features/response/admision/partialAdmission/howMuchHaveYouPaidController.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import {CITIZEN_AMOUNT_YOU_PAID_URL, CLAIM_TASK_LIST_URL} from '../../../../../routes/urls';
+import {GenericForm} from '../../../../../common/form/models/genericForm';
+import {constructResponseUrlWithIdParams} from '../../../../../common/utils/urlFormatter';
+import howMuchHaveYouPaidService from '../../../../../modules/admission/partialAdmission/howMuchHaveYouPaidService';
+import * as winston from 'winston';
+import {HowMuchHaveYouPaid} from '../../../../../common/form/models/admission/partialAdmission/howMuchHaveYouPaid';
+
+
+const {Logger} = require('@hmcts/nodejs-logging');
+let logger = Logger.getLogger('paymentDateController');
+const howMuchHaveYouPaidPath = 'features/response/admission/fullAdmission/partialAdmission/how-much-have-you-paid';
+const howMuchHaveYouPaidController = express.Router();
+const nextMonth = new Date();
+nextMonth.setMonth(nextMonth.getMonth() + 1);
+
+export function setHowMuchHaveYouPaidControllerLogger(winstonLogger: winston.Logger) {
+  logger = winstonLogger;
+}
+
+howMuchHaveYouPaidController
+  .get(
+    CITIZEN_AMOUNT_YOU_PAID_URL, async (req: express.Request, res: express.Response) => {
+      try {
+        const howMuchHaveYouPaid : HowMuchHaveYouPaid = await howMuchHaveYouPaidService.getHowMuchHaveYouPaid(req.params.id);
+        if (howMuchHaveYouPaid) {
+          const dateWhenYouPaid = new Date(howMuchHaveYouPaid.date);
+          howMuchHaveYouPaid.date = dateWhenYouPaid;
+          howMuchHaveYouPaid.year = dateWhenYouPaid.getFullYear();
+          howMuchHaveYouPaid.month = dateWhenYouPaid.getMonth() + 1;
+          howMuchHaveYouPaid.day = dateWhenYouPaid.getDate();
+        }
+        res.render(howMuchHaveYouPaidPath, {
+          form: new GenericForm(howMuchHaveYouPaid), nextMonth : nextMonth,
+        });
+      } catch (error) {
+        logger.error(error);
+        res.status(500).send({error: error.message});
+      }
+    })
+  .post(
+    CITIZEN_AMOUNT_YOU_PAID_URL, async (req, res) => {
+      const howMuchHaveYouPaid = howMuchHaveYouPaidService.buildHowMuchHaveYouPaid(req.body.amount, req.body.totalAmount, req.body.year, req.body.month, req.body.day, req.body.text);
+      const form: GenericForm<HowMuchHaveYouPaid> = new GenericForm<HowMuchHaveYouPaid>(howMuchHaveYouPaid);
+      await form.validate();
+
+      if (form.hasErrors()) {
+        res.render(howMuchHaveYouPaidPath, {
+          form: form, nextMonth : nextMonth,
+        });
+      } else {
+        try {
+          await howMuchHaveYouPaidService.saveHowMuchHaveYouPaid(req.params.id, howMuchHaveYouPaid);
+          res.redirect(constructResponseUrlWithIdParams(req.params.id, CLAIM_TASK_LIST_URL));
+        } catch (error) {
+          logger.error(error);
+          res.status(500).send({error: error.message});
+        }
+      }
+    });
+
+export default howMuchHaveYouPaidController;
