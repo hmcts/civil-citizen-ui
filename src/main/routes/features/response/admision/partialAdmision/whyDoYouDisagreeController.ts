b96629237693859dcb9793dd94496eb4e1522db1
laarmada <sergiosalmar@yahoo.es>
2022-04-25 16:01:55 +0100

    fix PR comments

--- src/main/routes/features/response/admision/partialAdmision/whyDoYouDisagreeController.ts
+++ src/main/routes/features/response/admision/partialAdmision/whyDoYouDisagreeController.ts
@@ -8,11 +8,10 @@ import {constructResponseUrlWithIdParams} from '../../../../../common/utils/urlF
 import {CITIZEN_AMOUNT_YOU_PAID_URL, CITIZEN_WHY_DO_YOU_DISAGREE_URL} from '../../../../urls';
 import {WhyDoYouDisagreeForm} from '../../../../../common/models/whyDoYouDisagreeForm';
 import {GenericForm} from '../../../../../common/form/models/genericForm';
-import {Validator} from 'class-validator';
 
 const whyDoYouDisagreeController = express.Router();
 const whyDoYouDisagreeViewPath = 'features/response/admission/partialAdmission/why-do-you-disagree';
-const validator = new Validator();
+let claimAmount: number;
 
 function renderView(form: GenericForm<WhyDoYouDisagree>, claimAmount: number, res: express.Response) {
   res.render(whyDoYouDisagreeViewPath, {form: form, claimAmount: claimAmount});
@@ -21,8 +20,8 @@ function renderView(form: GenericForm<WhyDoYouDisagree>, claimAmount: number, re
 whyDoYouDisagreeController.get(CITIZEN_WHY_DO_YOU_DISAGREE_URL, async (req, res) => {
   try {
     const form = await getWhyDoYouDisagreeForm(req.params.id);
-
-    renderView(new GenericForm(form.whyDoYouDisagree), form.claimAmount, res);
+    claimAmount = form.claimAmount;
+    renderView(new GenericForm(form.whyDoYouDisagree), claimAmount, res);
   } catch (error) {
     res.status(500).send({error: error.message});
   }
@@ -31,9 +30,10 @@ whyDoYouDisagreeController.get(CITIZEN_WHY_DO_YOU_DISAGREE_URL, async (req, res)
 whyDoYouDisagreeController.post(CITIZEN_WHY_DO_YOU_DISAGREE_URL, async (req, res) => {
   const whyDoYouDisagree = new WhyDoYouDisagree(req.body.text);
   const whyDoYouDisagreeForm = new WhyDoYouDisagreeForm();
+  whyDoYouDisagreeForm.claimAmount = claimAmount;
   whyDoYouDisagreeForm.whyDoYouDisagree = whyDoYouDisagree;
   const form = new GenericForm(whyDoYouDisagree);
-  form.errors = validator.validateSync(form.model);
+  await form.validate();
   try {
     if (form.hasErrors() || form.hasNestedErrors()) {
       renderView(form, whyDoYouDisagreeForm.claimAmount, res);
