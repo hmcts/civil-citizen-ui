75d065868537e30d13ca246d7229dc942e111ad9
Kadir Egilmez <40733099+laperla11@users.noreply.github.com>
2022-04-25 11:00:53 +0100

    TechDebt/CIV-1752/Remove code smells part2 (#348)
    * immediately return the expression
    
    * rename unused function parameter req as _req
    
    * rename the unused function parameter req as _req
    
    * corrected the jwt type
    
    * rename the unused req with _req
    
    * changed the naming
    
    * refactored the mockForm
    
    * removed the duplicate keys
    
    * renamed unused req with _req
    
    * included postCode validator to sonar exclusion files
    
    * remove redundant code

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src/test/a11y'],
   "testRegex": "(/src/test/.test.*|\\.(test|spec))\\.(ts|js)$",
-   "moduleFileExtensions": [
-    "ts",
-    "js"
-  ],
   "testEnvironment": "node",
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  "moduleFileExtensions": ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
