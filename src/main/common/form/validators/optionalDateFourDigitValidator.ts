12d38d9bda88bcb25451a1097f6b4fdc98f2c354
OsheaF <fergal.oshea@hmcts.net>
2022-04-26 10:32:22 +0100

    CIV-1124 Fixes as per code review

--- src/main/common/form/validators/optionalDateFourDigitValidator.ts
+++ src/main/common/form/validators/optionalDateFourDigitValidator.ts
@@ -8,10 +8,7 @@ import {VALID_FOUR_DIGIT_YEAR} from '../validationErrors/errorMessageConstants';
 export class OptionalDateFourDigitValidator implements ValidatorConstraintInterface {
 
   validate(year: number) {
-    if (year !== null && (year < 1000)){
-      return false;
-    }
-    return true;
+    return !(year !== null && (year < 1000));
   }
 
   defaultMessage() {
