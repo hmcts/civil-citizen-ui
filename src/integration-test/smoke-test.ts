d483c161bfa5694b459f4462aa1eca7a2e421ca5
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-04-28 11:48:19 +0100

    removed authenticate() from smoke test.

--- src/integration-test/smoke-test.ts
+++ src/integration-test/smoke-test.ts
@@ -15,19 +15,6 @@ jest.mock('ioredis', () => {
   });
 });
 
-/*async function authenticate() {
-  try {
-    await agent.get('/oauth2/callback')
-      .query('code=ABC')
-      .then((res: Response) => {
-        expect(res.status).toBe(302);
-      });
-  } catch (e) {
-    console.log('error' + e);
-  }
-
-}*/
-
 // TODO: replace this sample test with proper smoke tests later
 describe('Dummy Smoke test - Dashboard page', () => {
   describe('on GET', () => {
@@ -36,7 +23,6 @@ describe('Dummy Smoke test - Dashboard page', () => {
       nock('http://localhost:5000')
         .post('/o/token')
         .reply(200, {id_token: citizenRoleToken});
-      //authenticate();
     });
 
     test('should redirect to dashboard page', async () => {
