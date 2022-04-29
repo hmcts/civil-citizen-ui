eaac4dbc0de72137ad62fe318e7843b30c94cf6c
Kadir <kegilmez@gmail.com>
2022-04-29 01:58:40 +0100

    updated the model to validate empty string

--- src/main/common/form/models/mediation/telephone/companyTelephoneNumber.ts
+++ src/main/common/form/models/mediation/telephone/companyTelephoneNumber.ts
@@ -1,4 +1,4 @@
-import { IsDefined, MaxLength, ValidateIf } from 'class-validator';
+import { IsDefined, MaxLength, ValidateIf, IsNotEmpty } from 'class-validator';
 import { TEXT_TOO_LONG, PHONE_NUMBER_REQUIRED, NAME_REQUIRED, VALID_YES_NO_OPTION } from '../../../validationErrors/errorMessageConstants';
 import { YesNo } from '../../yesNo';
 
@@ -9,39 +9,29 @@ export class CompanyTelephoneNumber  {
 
   @ValidateIf(o => o.option === YesNo.YES)
   @IsDefined({ message: PHONE_NUMBER_REQUIRED })
-  // @IsNotEmpty()({ message: ValidationErrors.NUMBER_REQUIRED })
+  @IsNotEmpty({ message: PHONE_NUMBER_REQUIRED })
   @MaxLength(30, { message: TEXT_TOO_LONG })
     mediationPhoneNumberConfirmation?: string;
-  
+
   @ValidateIf(o => o.option === YesNo.NO)
   @IsDefined({ message: NAME_REQUIRED })
-  // @IsNotBlank({ message: ValidationErrors.NAME_REQUIRED })
+  @IsNotEmpty({ message: NAME_REQUIRED })
   @MaxLength(30, { message: TEXT_TOO_LONG })
     mediationContactPerson?: string;
 
   @ValidateIf(o => o.option === YesNo.NO)
   @IsDefined({ message: PHONE_NUMBER_REQUIRED })
-  // @IsNotBlank({ message: ValidationErrors.NUMBER_REQUIRED })
+  @IsNotEmpty({ message: PHONE_NUMBER_REQUIRED })
   @MaxLength(30, { message: TEXT_TOO_LONG })
     mediationPhoneNumber?: string;
 
-  constructor (option?: YesNo, mediationPhoneNumber?: string, mediationContactName?: string, mediationPhoneNumberConfirmation?: string) {
-    
+  constructor(option?: YesNo, mediationPhoneNumber?: string, mediationContactPerson?: string, mediationPhoneNumberConfirmation?: string) {
+
     this.option = option;
     this.mediationPhoneNumber = mediationPhoneNumber;
-    this.mediationContactPerson = mediationContactName;
+    this.mediationContactPerson = mediationContactPerson;
     this.mediationPhoneNumberConfirmation = mediationPhoneNumberConfirmation;
   }
 
-  static fromObject(value?: any): CompanyTelephoneNumber {
-    if (value == null) {
-      return value;
-    }
-
-    return new CompanyTelephoneNumber(value.option, value.mediationPhoneNumber, value.mediationContactPerson, value.mediationPhoneNumberConfirmation)
-  }
-
-  isCompleted (): boolean {
-    return !!this.option && ((!!this.mediationPhoneNumber && !!this.mediationContactPerson) || !!this.mediationPhoneNumberConfirmation)
-  }
+  
 }
