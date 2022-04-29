aec45fda4b2a6b91173d9b381c89aa482772dd80
Kadir <kegilmez@gmail.com>
2022-04-29 13:35:42 +0100

    only exclude this regex line from conar linting

--- src/main/common/form/validators/postcodeValidator.ts
+++ src/main/common/form/validators/postcodeValidator.ts
@@ -7,7 +7,7 @@ import {DEFENDANT_POSTCODE_NOT_VALID} from '../validationErrors/errorMessageCons
 @ValidatorConstraint({name: 'customInt', async: false})
 export class PostcodeValidator implements ValidatorConstraintInterface {
 
-  readonly UK_POSTCODE_REGEX = /^(([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2}))$/;
+  readonly UK_POSTCODE_REGEX = /^(([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2}))$/; // NO SONAR
 
   validate(value: string) {
     const ukPostCodePattern = this.UK_POSTCODE_REGEX;
