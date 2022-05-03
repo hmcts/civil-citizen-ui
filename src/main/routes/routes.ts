af6d3936ab32d277c53b50c6fcf771c2f0bf6985
RosDiaf <rdiaferia@zaizi.com>
2022-05-03 18:11:20 +0100

    Feature/civ 1982 mediation tel (#390)
    * Mediation - Confirm Individual Telephone Number
    
    * Refactor njk template and update ignore urls
    
    * UI test refactored
    
    * Mediation service added
    
    * Changes added to fix PR build
    
    * Code smell fixed
    
    * Changes added to names to be more descriptive
    
    * Test removed
    
    * Folder structure modified
    
    * variable re-named
    
    * variable re-named
    
    * njk test file re-named
    
    * Mediation base url updated
    
    * Page name changed as per acceptance criteria

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -47,6 +47,7 @@ import courtOrdersController from './features/response/statementOfMeans/courtOrd
 import whyDoYouDisagreeController from './features/response/admission/partialAdmission/whyDoYouDisagreeController';
 import repaymentPlanController from './features/response/repaymentPlan/repaymentPlanController';
 import regularIncomeController from './features/response/statementOfMeans/income/regularIncomeController';
+import mediationIndividualPhoneController from './features/response/mediation/mediationIndividualPhoneController';
 import howMuchHaveYouPaidController from './features/response/admission/partialAdmission/howMuchHaveYouPaidController';
 import mediationDisagreementController from './features/mediation/mediationDisagreementController';
 import freeTelephoneMediationController from './features/response/mediation/freeTelephoneMediationController';
@@ -96,6 +97,7 @@ export default [
   courtOrdersController,
   repaymentPlanController,
   regularIncomeController,
+  mediationIndividualPhoneController,
   howMuchHaveYouPaidController,
   mediationDisagreementController,
   freeTelephoneMediationController,
