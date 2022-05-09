88fd97c55fa92caf3d3510a977633166790afdeb
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-05-09 09:14:05 +0100

    CIV-1978 add functionality to creat an empty form

--- src/main/common/form/models/timeLineOfEvents/defendantTimeline.ts
+++ src/main/common/form/models/timeLineOfEvents/defendantTimeline.ts
@@ -3,10 +3,25 @@ import {MaxLength, ValidateIf} from 'class-validator';
 import {VALID_TEXT_LENGTH} from '../../validationErrors/errorMessageConstants';
 import {FREE_TEXT_MAX_LENGTH} from '../../validators/validationConstraints';
 
+const MIN_ROWS = 4;
+
 export class DefendantTimeline {
   rows: TimelineRow[];
 
   @ValidateIf(o => o.comment !== undefined)
-  @MaxLength(FREE_TEXT_MAX_LENGTH, { message: VALID_TEXT_LENGTH})
+  @MaxLength(FREE_TEXT_MAX_LENGTH, {message: VALID_TEXT_LENGTH})
     comment?: string;
+
+  constructor(rows?: TimelineRow[], comment?: string) {
+    this.rows = rows;
+    this.comment = comment;
+  }
+
+  public static buildEmptyForm(): DefendantTimeline {
+    return new DefendantTimeline([new TimelineRow(), new TimelineRow(), new TimelineRow()]);
+  }
+
+  public static buildPopulatedForm(): DefendantTimeline {
+    
+  }
 }
