469931ed5959356785e688675aa9a4dd7b223383
Sergio Salguero <sergiosalmar@yahoo.es>
2022-05-09 13:33:15 +0100

    Civ 1987 mediation i do not agree to free mediation (#393)
    * view
    
    * business logic and view completed.
    
    * unit test
    
    * refactor folder
    
    * PR comment
    
    * Test review

---
+++ src/main/common/form/models/mediation/noMediationReasonOptions.ts
@@ -0,0 +1,10 @@
+enum NoMediationReasonOptions {
+  ALREADY_TRIED = 'I have already tried to resolve the dispute with the other party, with no success',
+  NOT_SURE = 'I am not sure what would happen in mediation',
+  WOULD_NOT_SOLVE = 'I do not think mediation would solve the dispute',
+  NO_DELAY_IN_HEARING = 'I do not want to delay getting a hearing',
+  JUDGE_TO_DECIDE = 'I want a judge to make a decision on the dispute',
+  OTHER = 'Another reason (please specify)',
+}
+
+export default NoMediationReasonOptions;
--- src/main/common/models/mediation/mediation.ts
+++ src/main/common/models/mediation/mediation.ts
@@ -1,12 +1,15 @@
 import {FreeMediation} from '../../../common/form/models/mediation/freeMediation';
 import {MediationIndividualTelephoneNumber} from './mediationIndividualTelephoneNumber';
+import {NoMediationReason} from '../../../common/form/models/mediation/noMediationReason';
 
 export class Mediation {
   individualTelephone?: MediationIndividualTelephoneNumber;
   mediationDisagreement?: FreeMediation;
+  noMediationReason?: NoMediationReason;
 
-  constructor(individualTelephone?: MediationIndividualTelephoneNumber, mediationDisagreement?: FreeMediation) {
+  constructor(individualTelephone?: MediationIndividualTelephoneNumber, mediationDisagreement?: FreeMediation, noMediationReason?: NoMediationReason) {
     this.individualTelephone = individualTelephone;
     this.mediationDisagreement = mediationDisagreement;
+    this.noMediationReason = noMediationReason;
   }
 }
