2477c9df1916c346c057f4d0375532a4bb6f9268
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-05-09 11:14:10 +0100

    CIV-1978 add functionality to creat populated form and unit test

--- src/main/common/form/models/timeLineOfEvents/defendantTimeline.ts
+++ src/main/common/form/models/timeLineOfEvents/defendantTimeline.ts
@@ -1,12 +1,13 @@
 import TimelineRow from './timelineRow';
-import {MaxLength, ValidateIf} from 'class-validator';
+import {MaxLength, ValidateIf, ValidateNested} from 'class-validator';
 import {VALID_TEXT_LENGTH} from '../../validationErrors/errorMessageConstants';
 import {FREE_TEXT_MAX_LENGTH} from '../../validators/validationConstraints';
 
-const MIN_ROWS = 4;
+const MINIMUM_ROWS = 4;
 
 export class DefendantTimeline {
-  rows: TimelineRow[];
+  @ValidateNested({each: true})
+    rows: TimelineRow[];
 
   @ValidateIf(o => o.comment !== undefined)
   @MaxLength(FREE_TEXT_MAX_LENGTH, {message: VALID_TEXT_LENGTH})
@@ -18,10 +19,28 @@ export class DefendantTimeline {
   }
 
   public static buildEmptyForm(): DefendantTimeline {
-    return new DefendantTimeline([new TimelineRow(), new TimelineRow(), new TimelineRow()]);
+    return new DefendantTimeline(DefendantTimeline.addRemainingRows([]));
   }
 
-  public static buildPopulatedForm(): DefendantTimeline {
-    
+  public static buildPopulatedForm(timelineOfEvents: TimelineRow[], comment?: string): DefendantTimeline {
+    return new DefendantTimeline(DefendantTimeline.addRemainingRows(timelineOfEvents), comment);
   }
+
+  private static addRemainingRows(timelineOfEvents: TimelineRow[]): TimelineRow[] {
+    const additionalRows = [];
+    if (timelineOfEvents?.length < MINIMUM_ROWS) {
+      for (let i = 0; i < MINIMUM_ROWS - timelineOfEvents.length; i++) {
+        additionalRows.push(new TimelineRow());
+      }
+    }
+    return timelineOfEvents.concat(additionalRows);
+  }
+
+  filterOutEmptyRows() {
+    if (this.rows?.length) {
+      this.rows = this.rows.filter(row => row.isAtLeastOneFieldPopulated());
+    }
+  }
+
+
 }
---
+++ src/test/unit/common/form/models/timelineOfEvents/defendantTimeline.test.ts
@@ -0,0 +1,60 @@
+import {DefendantTimeline} from '../../../../../../main/common/form/models/timeLineOfEvents/defendantTimeline';
+import TimelineRow from '../../../../../../main/common/form/models/timeLineOfEvents/timelineRow';
+import {GenericForm} from '../../../../../../main/common/form/models/genericForm';
+import {DESCRIPTION_REQUIRED} from '../../../../../../main/common/form/validationErrors/errorMessageConstants';
+
+describe('defendant timeline', () => {
+  describe('validation', () => {
+    it('should have errors nested when row field is not empty', async () => {
+      //Given
+      const defendant = new DefendantTimeline([new TimelineRow('12/01/2022')]);
+      //When
+      const form = new GenericForm(defendant);
+      await form.validate();
+      //Then
+      expect(form.hasErrors()).toBeTruthy();
+      expect(form.errorFor('rows[0][description]')).toBe(DESCRIPTION_REQUIRED);
+    });
+    it('should not have errors when row fields are empty', async () => {
+      //Given
+      const defendant = new DefendantTimeline([new TimelineRow()]);
+      //When
+      const form = new GenericForm(defendant);
+      await form.validate();
+      //Then
+      expect(form.hasErrors()).toBeTruthy();
+    });
+  });
+  describe('buildEmptyForm', () => {
+    it('should build empty form with for rows', () => {
+      //When
+      const form = DefendantTimeline.buildEmptyForm();
+      //Then
+      expect(form.rows.length).toBe(4);
+      expect(form.comment).toBeUndefined();
+    });
+  });
+  describe('buildPopulatedForm', () => {
+    //Given
+    const comment = 'some comment';
+    const rows = [new TimelineRow('some date', 'description')];
+    //When
+    const form = DefendantTimeline.buildPopulatedForm(rows, comment);
+    //Then
+    expect(form.rows.length).toBe(4);
+    expect(form.comment).toBe(comment);
+    expect(form.rows[0].date).toBe(rows[0].date);
+    expect(form.rows[0].description).toBe(rows[0].description);
+  });
+  describe('filterEmptyRows', () => {
+    //Given
+    const comment = 'some comment';
+    const rows = [new TimelineRow('some date', 'description')];
+    const form = DefendantTimeline.buildPopulatedForm(rows, comment);
+    expect(form.rows.length).toBe(4);
+    //When
+    form.filterOutEmptyRows();
+    //Then
+    expect(form.rows.length).toBe(1);
+  });
+});
