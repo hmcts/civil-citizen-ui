5bcbf0c8691100ab15ce5f0919be1bdd3945f10e
laarmada <sergiosalmar@yahoo.es>
2022-05-11 15:16:25 +0100

    include retrive data from redis

--- src/main/routes/features/response/eligibility/sendYourResponseByEmailController.ts
+++ src/main/routes/features/response/eligibility/sendYourResponseByEmailController.ts
@@ -1,18 +1,29 @@
 import * as express from 'express';
 import {SEND_RESPONSE_BY_EMAIL_URL} from '../../../urls';
+import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
+import {Claim} from '../../../../common/models/claim';
+import {ResponseType} from '../../../../common/form/models/responseType';
+//import {RejectAllOfClaimType} from '../../../../common/form/models/rejectAllOfClaimType';
+import {CounterpartyType} from '../../../../common/models/counterpartyType';
 
 const sendYourResponseByEmailViewPath = 'features/response/eligibility/send-your-response-by-email';
 const sendYourResponseByEmailController = express.Router();
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('sendYourResponseByEmailController');
 
-function renderView(res: express.Response): void {
-  res.render(sendYourResponseByEmailViewPath);
+function renderView(res: express.Response, form: Claim): void {
+  res.render(sendYourResponseByEmailViewPath, {
+    form: form,
+    ResponseType: ResponseType,
+    //  RejectAllOfClaimType: RejectAllOfClaimType,
+    CounterpartyType: CounterpartyType,
+  });
 }
 
 sendYourResponseByEmailController.get(SEND_RESPONSE_BY_EMAIL_URL, async (req, res) => {
   try {
-    renderView(res);
+    const form = await getCaseDataFromStore(req.params.id);
+    renderView(res, form);
   } catch (error) {
     logger.error(error);
     res.status(500).send({error: error.message});
