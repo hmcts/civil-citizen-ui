201754ff7510001ccf8768c7a14633f4269ec521
Kadir <kegilmez@gmail.com>
2022-05-13 09:18:16 +0100

    updated taskList service

--- src/main/modules/taskListService.ts
+++ src/main/modules/taskListService.ts
@@ -1,14 +1,10 @@
-import {TaskStatus} from "../common/models/taskList/TaskStatus";
-import {TaskList} from "../common/models/taskList/taskList";
-import {Claim} from "../common/models/claim";
+import {TaskStatus} from '../common/models/taskList/TaskStatus';
+import {TaskList} from '../common/models/taskList/taskList';
+import {Claim} from '../common/models/claim';
 import {
   buildPrepareYourResponseSection,
   buildRespondToClaimSection, buildSubmitSection,
-  // buildTryToResolveClaimSection,
-  // buildYourHearingRequirementsSection
-} from "../common/utils/taskList/taskListBuilder";
-// import dayjs, {Dayjs} from 'dayjs';
-import { currentDateTime } from '../common/utils/dayJsFactory';
+} from '../common/utils/taskList/taskListBuilder';
 
 /**
  * THIS FILE IS A CONCEPT
@@ -20,23 +16,17 @@ import { currentDateTime } from '../common/utils/dayJsFactory';
 let completed = 0;
 let total = 0;
 
-const getTaskLists = (claim: Claim, caseData: Claim) => {
-  const now = currentDateTime();
+const getTaskLists = (claim: Claim, caseData: Claim, currentClaimId:string) => {
 
   // TASK BUILDER
-  const taskListPrepareYourResponse: TaskList = buildPrepareYourResponseSection(claim, caseData, now);
-  const taskListRespondeToClaim: TaskList = buildRespondToClaimSection(claim);
-  const taskListSubmitYourResponse: TaskList = buildSubmitSection(claim);
-  // const taskListTryToResolveClaim: TaskList = await buildTryToResolveClaimSection(claim);
-  // const taskListYourHearingRequirements: TaskList = await buildYourHearingRequirementsSection(claim);
+  // TODO : depending on the defendant's response type (full admission/partial admission/ rejection) we need to build new taskLists and include them in the taskGroups array
+  const taskListPrepareYourResponse: TaskList = buildPrepareYourResponseSection(claim, caseData, currentClaimId);
+  const taskListRespondeToClaim: TaskList = buildRespondToClaimSection(caseData, currentClaimId);
+  const taskListSubmitYourResponse: TaskList = buildSubmitSection(claim, caseData, currentClaimId);
 
   // GENERATE DATA, TITLE AND DESCRIPTION
-  const xxx = [taskListPrepareYourResponse,
-    taskListRespondeToClaim, taskListSubmitYourResponse,
-    // taskListTryToResolveClaim,
-    // taskListYourHearingRequirements
-  ];
-  return xxx.filter(item => item.tasks.length !== 0);
+  const taskGroups = [taskListPrepareYourResponse, taskListRespondeToClaim, taskListSubmitYourResponse];
+  return taskGroups.filter(item => item.tasks.length !== 0);
 };
 
 const calculateTotalAndCompleted = (taskLists: TaskList[]) => {
@@ -46,20 +36,20 @@ const calculateTotalAndCompleted = (taskLists: TaskList[]) => {
     total += taskList.tasks.length;
     completed += countCompletedTasks(taskList);
   });
-}
+};
 
 const getTitle = (taskLists: TaskList[]) => {
   calculateTotalAndCompleted(taskLists);
   return completed < total ? 'Application incomplete' : 'Application complete';
-}
+};
 
 const getDescription = (taskLists: TaskList[]) => {
   calculateTotalAndCompleted(taskLists);
   return `You have completed ${completed} of ${total} sections`;
-}
+};
 
 const countCompletedTasks = (taskList: TaskList) => {
   return taskList.tasks.filter(task => task.status === TaskStatus.COMPLETE).length;
-}
+};
 
-export { getTaskLists, getTitle, getDescription };
+export { getTaskLists, getTitle, getDescription};
