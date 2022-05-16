d2f6705c325f5d67ad0b53b450074787f3478268
Felipe <felipe@LAPTOP3721.V1.com>
2022-05-16 15:28:01 +0200

    Fix: code smells

--- src/main/assets/js/language-toggle.js
+++ src/main/assets/js/language-toggle.js
@@ -3,12 +3,12 @@ const cymraegLanguage = document.getElementsByClassName('cymraeg-language');
 
 const getCookie = (name) => {
   const cookies = document.cookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    let c = cookies[i].trim().split('=');
-    if (c[0] === name) {
-      return c[1];
-    }
-  }
+
+  cookies.forEach((cookie) => {
+    let c = cookie.trim().split('=');
+    if (c[0] === name) return c[1];
+  });
+
   return '';
 };
 
