e17a271c795080baba6baf8fad1f0b40bb34cefc
Felipe <felipe@LAPTOP3721.V1.com>
2022-05-16 14:46:54 +0200

    Fix: language test

--- src/test/unit/views/features/response/dashboard/dashboard.test.ts
+++ src/test/unit/views/features/response/dashboard/dashboard.test.ts
@@ -2,40 +2,24 @@ import request from 'supertest';
 import {app} from '../../../../../../main/app';
 import config from 'config';
 import {DASHBOARD_URL} from '../../../../../../main/routes/urls';
-import {CIVIL_SERVICE_CASES_URL} from '../../../../../../main/app/client/civilServiceUrls';
 
 jest.mock('../../../../../../main/modules/oidc');
 jest.mock('../../../../../../main/modules/draft-store');
 
 const nock = require('nock');
 
-const agent = request.agent(app);
-
-function authenticate() {
-  agent.get('/oauth2/callback')
-    .query('code=ABC')
-    .then((res) => {
-      expect(res.status).toBe(302);
-    });
-}
-
 describe('Dashboard page', () => {
-  const idamUrl: string = config.get('idamUrl');
   const citizenRoleToken: string = config.get('citizenRoleToken');
-
+  const idamUrl: string = config.get('idamUrl');
   beforeEach(() => {
     nock(idamUrl)
       .post('/o/token')
-      .reply(200, {id_token: citizenRoleToken});
-    authenticate();
-    nock('http://localhost:4000')
-      .post(CIVIL_SERVICE_CASES_URL)
-      .reply(200, {});
+      .reply(200, { id_token: citizenRoleToken });
   });
 
   describe('on GET', () => {
     test('should return dashboard page in english', async () => {
-      await agent
+      await request(app)
         .get(DASHBOARD_URL + '?lang=en')
         .expect((res) => {
           expect(res.status).toBe(200);
@@ -44,7 +28,7 @@ describe('Dashboard page', () => {
     });
 
     test('should return dashboard page in cymraeg', async () => {
-      await agent
+      await request(app)
         .get(DASHBOARD_URL + '?lang=cy')
         .expect((res) => {
           expect(res.status).toBe(200);
