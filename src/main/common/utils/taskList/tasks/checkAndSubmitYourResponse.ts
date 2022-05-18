3c01d74236b67c131827d2e67e73a00c67a32784
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2022-05-18 11:32:49 +0100

    Remove code smells

--- src/main/common/utils/taskList/tasks/checkAndSubmitYourResponse.ts
+++ src/main/common/utils/taskList/tasks/checkAndSubmitYourResponse.ts
@@ -4,9 +4,9 @@ import { TaskStatus } from '../../../models/taskList/TaskStatus';
 import { constructResponseUrlWithIdParams } from '../../urlFormatter';
 
 /**
- * 
+ *
  * this file needs to be revisited when check and submit page is developed.
- * 
+ *
  */
 
 const checkAndSubmitYourResponseTask = {
@@ -15,11 +15,11 @@ const checkAndSubmitYourResponseTask = {
   status: TaskStatus.INCOMPLETE,
 };
 
-export const getCheckAndSubmitYourResponseTask = (claim: Claim, caseData: Claim, claimId: string, isIncompletSubmission: boolean): Task => {
+export const getCheckAndSubmitYourResponseTask = (_claim: Claim, _caseData: Claim, claimId: string, isIncompleteSubmission: boolean): Task => {
   const taskStatus = TaskStatus.INCOMPLETE;
-  // TODO : create the logic for successfull submit and change the taskStatus to TaskStatus.COMPLETE
+  // TODO : create the logic for successful submit and change the taskStatus to TaskStatus.COMPLETE
   // TODO : update the URL constants with the correct ones when these pages developed
-  const redirectionURL = isIncompletSubmission ? 'incomplete-submission' : 'check-and-send';
+  const redirectionURL = isIncompleteSubmission ? 'incomplete-submission' : 'check-and-send';
   const constructedUrl = constructResponseUrlWithIdParams(claimId, redirectionURL);
   return { ...checkAndSubmitYourResponseTask, url: constructedUrl, status: taskStatus };
 };
