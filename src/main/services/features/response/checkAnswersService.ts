96f4b491ba171dec7e2802c31f41147244ca9bb9
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-05-18 17:45:08 +0100

    CIV-2195 refactor make change label translatable

--- src/main/services/features/response/checkAnswersService.ts
+++ src/main/services/features/response/checkAnswersService.ts
@@ -13,13 +13,15 @@ import {t} from 'i18next';
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('checkAnswersService');
 
-const getLng = (lang:string | unknown): string => {
+const changeLabel = (lang: string | unknown): string => t('CHANGE', getLng(lang));
+
+const getLng = (lang: string | unknown): string => {
   return lang ? String(lang) : 'en';
 };
 
-const getDefendantFullName = (claim:Claim): string =>{
-  if(claim.respondent1.individualFirstName && claim.respondent1.individualLastName){
-    return claim.respondent1.individualTitle + ' ' + claim.respondent1.individualFirstName + ' ' + claim.respondent1.individualLastName
+const getDefendantFullName = (claim: Claim): string => {
+  if (claim.respondent1.individualFirstName && claim.respondent1.individualLastName) {
+    return claim.respondent1.individualTitle + ' ' + claim.respondent1.individualFirstName + ' ' + claim.respondent1.individualLastName;
   }
   return claim.respondent1.partyName;
 };
@@ -39,8 +41,8 @@ const buildYourDetailsSection = (claim: Claim, claimId: string, lang: string | u
   return summarySection({
     title: t('CHECK_YOUR_ANSWER.DETAILS_TITLE', {lng: getLng(lang)}),
     summaryRows: [
-      summaryRow(t('CHECK_YOUR_ANSWER.FULL_NAME', {lng: getLng(lang)}), getDefendantFullName(claim), yourDetailsHref, 'Change'),
-      summaryRow(t('CHECK_YOUR_ANSWER.CONTACT_NUMBER', {lng: getLng(lang)}), claim.respondent1.telephoneNumber, phoneNumberHref, 'Change'),
+      summaryRow(t('CHECK_YOUR_ANSWER.FULL_NAME', {lng: getLng(lang)}), getDefendantFullName(claim), yourDetailsHref, changeLabel(lang)),
+      summaryRow(t('CHECK_YOUR_ANSWER.CONTACT_NUMBER', {lng: getLng(lang)}), claim.respondent1.telephoneNumber, phoneNumberHref, changeLabel(lang)),
     ],
   });
 };
@@ -49,10 +51,10 @@ const buildResponseSection = (claim: Claim, claimId: string, lang: string | unkn
   const yourResponseHref = CITIZEN_RESPONSE_TYPE_URL.replace(':id', claimId);
   const paymentOptionHref = CITIZEN_PAYMENT_OPTION_URL.replace(':id', claimId);
   return summarySection({
-    title: t('CHECK_YOUR_ANSWER.RESPONSE_TITLE', {lng:getLng(lang)}),
+    title: t('CHECK_YOUR_ANSWER.RESPONSE_TITLE', {lng: getLng(lang)}),
     summaryRows: [
-      summaryRow(t('CHECK_YOUR_ANSWER.OWE_MONEY', {lng: getLng(lang)}), t(claim.respondent1.responseType, {lng: lang ? String(lang) : 'en'}), yourResponseHref, 'Change'),
-      summaryRow(t('CHECK_YOUR_ANSWER.WHEN_PAY', {lng: getLng(lang)}), t(claim.paymentOption, {lng: lang ? String(lang) : 'en'}), paymentOptionHref, 'Change'),
+      summaryRow(t('CHECK_YOUR_ANSWER.OWE_MONEY', {lng: getLng(lang)}), t(claim.respondent1.responseType, {lng: getLng(lang)}), yourResponseHref, changeLabel(lang)),
+      summaryRow(t('CHECK_YOUR_ANSWER.WHEN_PAY', {lng: getLng(lang)}), t(claim.paymentOption, {lng: getLng(lang)}), paymentOptionHref, changeLabel(lang)),
     ],
   });
 };
