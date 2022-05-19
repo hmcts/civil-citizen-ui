eff35cad3516b055dfcf5935fee0abb857749913
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-05-19 14:22:17 +0100

    CIV-2195 add additional translation test to controller

--- src/test/unit/routes/features/response/checkAnswersController.test.ts
+++ src/test/unit/routes/features/response/checkAnswersController.test.ts
@@ -1,10 +1,7 @@
 import nock from 'nock';
 import config from 'config';
 import * as checkAnswersService from '../../../../../main/services/features/response/checkAnswersService';
-import {
-  CITIZEN_DETAILS_URL,
-  RESPONSE_CHECK_ANSWERS_URL,
-} from '../../../../../main/routes/urls';
+import {CITIZEN_DETAILS_URL, RESPONSE_CHECK_ANSWERS_URL} from '../../../../../main/routes/urls';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
 import {SummarySections} from '../../../../../main/common/models/summaryList/summarySections';
 import {getElementsByXPath} from '../../../../utils/xpathExtractor';
@@ -28,36 +25,51 @@ const respondentCheckAnswersUrl = RESPONSE_CHECK_ANSWERS_URL.replace(':id', CLAI
 describe('Response - Check answers', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamServiceUrl: string = config.get('services.idam.url');
-
+  const checkYourAnswerEng = 'Check your answers';
+  const checkYourAnswerCy = 'Gwiriwch eich ateb';
   beforeEach(() => {
     nock(idamServiceUrl)
       .post('/o/token')
       .reply(200, {id_token: citizenRoleToken});
+    mockGetSummarySections.mockImplementation(() => {
+      return createClaimWithBasicRespondentDetails();
+    });
   });
 
   describe('on GET', () => {
 
     test('should return check answers page', async () => {
-      mockGetSummarySections.mockImplementation(() => {
-        return createClaimWithBasicRespondentDetails();
-      });
-
       const response = await request(app).get(respondentCheckAnswersUrl);
       expect(response.status).toBe(200);
 
       const dom = new JSDOM(response.text);
       const htmlDocument = dom.window.document;
-
       const header = getElementsByXPath("//h1[@class='govuk-heading-l']", htmlDocument);
       const fullName = getElementsByXPath(
         "//dd[@class='govuk-summary-list__value' and preceding-sibling::dt[contains(text(),'Full name')]]",
         htmlDocument);
 
       expect(header.length).toBe(1);
-      expect(header[0].textContent).toBe('Check your answers');
+      expect(header[0].textContent).toBe(checkYourAnswerEng);
       expect(fullName.length).toBe(1);
       expect(fullName[0].textContent?.trim()).toBe(PARTY_NAME);
 
+    });
+    test('should pass english translation via query', async () => {
+      await request(app).get(respondentCheckAnswersUrl)
+        .query({lang: 'en'})
+        .expect((res: Response) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain(checkYourAnswerEng);
+        });
+    });
+    test('should pass cy translation via query', async () => {
+      await request(app).get(respondentCheckAnswersUrl)
+        .query({lang: 'cy'})
+        .expect((res: Response) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain(checkYourAnswerCy);
+        });
     });
     test('should return status 500 when error thrown', async () => {
       mockGetSummarySections.mockImplementation(() => {
