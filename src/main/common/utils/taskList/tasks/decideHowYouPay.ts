54f8e18156055475d60885f682048e45bc197486
commitBlob <maro.radovic12@gmail.com>
2022-05-20 14:46:11 +0100

    feat(CIV-1003): Complete Scenario 2
    Admit & Pay Immediately response

--- src/main/common/utils/taskList/tasks/decideHowYouPay.ts
+++ src/main/common/utils/taskList/tasks/decideHowYouPay.ts
@@ -1,17 +1,21 @@
-import { Task } from '../../../models/taskList/task';
-import { Claim } from '../../../models/claim';
-import { TaskStatus } from '../../../models/taskList/TaskStatus';
-// import { constructResponseUrlWithIdParams } from '../../../../common/utils/urlFormatter';
+import {Task} from '../../../models/taskList/task';
+import {Claim} from '../../../models/claim';
+import {TaskStatus} from '../../../models/taskList/TaskStatus';
+import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
 import {CITIZEN_PAYMENT_OPTION_URL} from '../../../../routes/urls';
+import {isPaymentOptionMissing} from 'common/utils/taskList/tasks/taskListHelpers';
 
-const decideHowYouPayTask =  {
+const decideHowYouPayTask: Task = {
   description: 'Decide how you\'ll pay',
   url: CITIZEN_PAYMENT_OPTION_URL,
   status: TaskStatus.INCOMPLETE,
 };
 
 export const getDecideHowYouPayTask = (caseData: Claim, claimId: string): Task => {
-  console.log('how you pay', caseData);
-  // const constructedUrl = constructResponseUrlWithIdParams(claimId, CITIZEN_PAYMENT_OPTION_URL);
-  return { ...decideHowYouPayTask };
+  let taskStatus = TaskStatus.COMPLETE;
+  if (isPaymentOptionMissing(caseData)) {
+    taskStatus = TaskStatus.INCOMPLETE;
+  }
+  const constructedUrl = constructResponseUrlWithIdParams(claimId, CITIZEN_PAYMENT_OPTION_URL);
+  return { ...decideHowYouPayTask, url: constructedUrl, status: taskStatus };
 };
--- src/main/common/utils/taskList/tasks/taskListHelpers.ts
+++ src/main/common/utils/taskList/tasks/taskListHelpers.ts
@@ -2,23 +2,25 @@ import {Claim} from '../../../models/claim';
 import {Respondent} from '../../../../common/models/respondent';
 import { CounterpartyType } from '../../../../common/models/counterpartyType';
 
-
-export const isCaseDataMissing = (caseData: Claim) => {
+export const isCaseDataMissing = (caseData: Claim): boolean => {
   return !caseData;
 };
 
-export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent) => {
+export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent): boolean => {
   return !respondent1?.correspondenceAddress && !respondent1?.primaryAddress;
 };
 
-export const isDOBMissing = (respondent1: Respondent) => {
+export const isDOBMissing = (respondent1: Respondent): boolean => {
   // check DOB if the defendant type is individual
   return respondent1?.type === CounterpartyType.INDIVIDUAL && !respondent1?.dateOfBirth ;
 };
 
-export const isResponseTypeMissing = (respondent1: Respondent) => {
+export const isResponseTypeMissing = (respondent1: Respondent): boolean => {
   return !respondent1?.responseType;
 };
 
+export const isPaymentOptionMissing = (caseData: Claim): boolean => {
+  return !caseData?.paymentOption;
+};
 
 
