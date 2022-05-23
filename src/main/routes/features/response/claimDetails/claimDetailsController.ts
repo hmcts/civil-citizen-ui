86716dd0977d2fb883799e5eea1fd970a49f3b65
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-05-23 12:32:09 +0100

    CIV-2421 - save claim when not in redis

--- src/main/routes/features/response/claimDetails/claimDetailsController.ts
+++ src/main/routes/features/response/claimDetails/claimDetailsController.ts
@@ -1,16 +1,15 @@
 import * as express from 'express';
 import config from 'config';
-import {CLAIM_DETAILS_URL } from '../../../urls';
-import { CivilServiceClient } from '../../../../app/client/civilServiceClient';
+import {CLAIM_DETAILS_URL} from '../../../urls';
+import {CivilServiceClient} from '../../../../app/client/civilServiceClient';
 import {Claim} from '../../../../common/models/claim';
 import {AppRequest} from 'models/AppRequest';
+import {getCaseDataFromStore, saveDraftClaim} from 'modules/draft-store/draftStoreService';
 
 const claimDetailsController = express.Router();
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
-
-let claim: Claim = new Claim();
 
 function renderPage(res: express.Response, claimDetails: Claim): void {
   res.render('features/response/claimDetails/claim-details', {
@@ -20,8 +19,14 @@ function renderPage(res: express.Response, claimDetails: Claim): void {
 
 // -- GET Claim Details
 claimDetailsController.get(CLAIM_DETAILS_URL, async (req: express.Request, res: express.Response) => {
-  claim = await civilServiceClient.retrieveClaimDetails(req.params.id, <AppRequest>req);
-  if(!claim){
+  let claim = await getCaseDataFromStore((req.params.id));
+  if (!claim) {
+    claim = await civilServiceClient.retrieveClaimDetails(req.params.id, <AppRequest>req);
+    if (claim) {
+      await saveDraftClaim(req.params.id, claim);
+    }
+  }
+  if (!claim) {
     claim = new Claim();
     claim.legacyCaseReference = 'testCaseReference';
     claim.totalClaimAmount = 200;
