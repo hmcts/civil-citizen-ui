f5e074b5415027a68de9fdcd7b62aeaeb3fed8f8
laarmada <sergiosalmar@yahoo.es>
2022-05-23 14:45:05 +0100

    add view test

--- src/main/modules/yourDefenceService.ts
+++ src/main/modules/yourDefenceService.ts
@@ -4,19 +4,6 @@ import {Defence} from '../common/form/models/defence';
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('yourDefenceService');
 
-export const getYourDefence = async (claimId: string): Promise<Defence> => {
-  try {
-    const claim = await getCaseDataFromStore(claimId);
-    if (claim.defence) {
-      return new Defence(claim.defence.text);
-    }
-    return new Defence();
-  } catch (error) {
-    logger.error(error);
-    throw error;
-  }
-};
-
 export const saveYourDefence = async (claimId: string, form: Defence) => {
   try {
     const claim = await getCaseDataFromStore(claimId);
