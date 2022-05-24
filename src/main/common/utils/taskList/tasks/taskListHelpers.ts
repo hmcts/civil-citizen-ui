73173274af5f03297ef0a60f1d70fda3a78c41f9
commitBlob <maro.radovic12@gmail.com>
2022-05-24 12:03:16 +0100

    fix(CIV-2527): redirect to correct pages

--- src/main/common/utils/taskList/tasks/taskListHelpers.ts
+++ src/main/common/utils/taskList/tasks/taskListHelpers.ts
@@ -3,20 +3,20 @@ import {Respondent} from '../../../../common/models/respondent';
 import {CounterpartyType} from '../../../../common/models/counterpartyType';
 import PaymentOptionType from '../../../../common/form/models/admission/fullAdmission/paymentOption/paymentOptionType';
 
-export const isCaseDataMissing = (caseData: Claim) => {
+export const isCaseDataMissing = (caseData: Claim): boolean => {
   return !caseData;
 };
 
-export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent) => {
+export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent): boolean => {
   return !respondent1?.correspondenceAddress && !respondent1?.primaryAddress;
 };
 
-export const isDOBMissing = (respondent1: Respondent) => {
+export const isDOBMissing = (respondent1: Respondent): boolean => {
   // check DOB if the defendant type is individual
   return respondent1?.type === CounterpartyType.INDIVIDUAL && !respondent1?.dateOfBirth ;
 };
 
-export const isResponseTypeMissing = (respondent1: Respondent) => {
+export const isResponseTypeMissing = (respondent1: Respondent): boolean => {
   return !respondent1?.responseType;
 };
 
