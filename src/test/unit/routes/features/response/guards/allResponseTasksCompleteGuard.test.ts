2e1af66102bcc78c6cd71aaf3ae44e1c00b10882
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2022-05-24 23:26:31 +0100

    Add test coverage

---
+++ src/test/unit/routes/features/response/guards/allResponseTasksCompleteGuard.test.ts
@@ -0,0 +1,82 @@
+import {RESPONSE_INCOMPLETE_SUBMISSION_URL} from '../../../../../../main/routes/urls';
+import {
+  AllResponseTasksCompletedGuard,
+} from '../../../../../../main/routes/features/response/guards/allResponseTasksCompletedGuard';
+import express from 'express';
+import {TaskStatus} from '../../../../../../main/common/models/taskList/TaskStatus';
+import {TaskList} from '../../../../../../main/common/models/taskList/taskList';
+
+jest.mock('../../../../../../main/modules/oidc');
+jest.mock('../../../../../../main/services/features/response/checkAnswersService');
+
+const CLAIM_ID = 'aaa';
+const respondentIncompleteSubmissionUrl = RESPONSE_INCOMPLETE_SUBMISSION_URL.replace(':id', CLAIM_ID);
+const TASK_LISTS = (taskStatus: TaskStatus) => [
+  {
+    title: 'Task List',
+    tasks: [
+      {
+        description: 'Task 1',
+        status: taskStatus,
+        url: 'some URL',
+      },
+    ],
+  },
+];
+
+const MOCK_REQUEST = (taskLists: TaskList[]) => {
+  return {
+    session: {
+      claimId: CLAIM_ID,
+      taskLists: taskLists,
+    },
+  } as express.Request;
+};
+
+const MOCK_RESPONSE = {
+  redirect: jest.fn(),
+} as unknown as express.Response;
+
+const MOCK_NEXT = jest.fn() as express.NextFunction;
+
+describe('Response - Incomplete Submission', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('on GET', () => {
+
+    it('should call next middleware function which will render check answers screen', async () => {
+      //Given
+      const mockRequest = MOCK_REQUEST(TASK_LISTS(TaskStatus.COMPLETE));
+      //When
+      await AllResponseTasksCompletedGuard.apply(mockRequest, MOCK_RESPONSE, MOCK_NEXT);
+      //Then
+      expect(MOCK_RESPONSE.redirect).not.toHaveBeenCalled();
+      expect(MOCK_NEXT).toHaveBeenCalledWith();
+    });
+
+    it('should throw error if task list from session is empty', async () => {
+      //Given
+      const mockRequest = MOCK_REQUEST([]);
+      //When
+      await AllResponseTasksCompletedGuard.apply(mockRequest, MOCK_RESPONSE, MOCK_NEXT);
+      //Then
+      expect(MOCK_RESPONSE.redirect).not.toHaveBeenCalled();
+      expect(MOCK_NEXT).toHaveBeenLastCalledWith(expect.objectContaining({
+        message: 'Task list cannot be empty',
+      }));
+    });
+
+    it('should redirect to incomplete submission when tasks incomplete status 500 when error thrown', async () => {
+      //Given
+      const mockRequest = MOCK_REQUEST(TASK_LISTS(TaskStatus.INCOMPLETE));
+      //When
+      await AllResponseTasksCompletedGuard.apply(mockRequest, MOCK_RESPONSE, MOCK_NEXT);
+      //Then
+      expect(MOCK_RESPONSE.redirect).toHaveBeenCalledWith(respondentIncompleteSubmissionUrl);
+      expect(MOCK_NEXT).not.toHaveBeenCalled();
+    });
+  });
+});
