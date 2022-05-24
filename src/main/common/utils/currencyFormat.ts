8788bd7db4b82461411b25d36f559141b13575c4
Kadir <kegilmez@gmail.com>
2022-05-24 17:51:04 +0100

    generated the helper functions

--- src/main/common/utils/currencyFormat.ts
+++ src/main/common/utils/currencyFormat.ts
@@ -6,3 +6,7 @@ const formatter = new Intl.NumberFormat('en-UK',
 export default function currencyFormat(amount: number): string {
   return formatter.format(amount);
 }
+
+export function convertToPoundsFilter(value: number|string): number {
+  return Number(value) / 100;
+}
