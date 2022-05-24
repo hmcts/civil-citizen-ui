54b5390e27a9a7d6ab98069788f461bfa7adaff3
commitBlob <maro.radovic12@gmail.com>
2022-05-24 11:57:12 +0100

    rollback boolean return types

--- src/main/common/utils/taskList/tasks/taskListHelpers.ts
+++ src/main/common/utils/taskList/tasks/taskListHelpers.ts
@@ -3,20 +3,20 @@ import {Respondent} from '../../../../common/models/respondent';
 import {CounterpartyType} from '../../../../common/models/counterpartyType';
 import PaymentOptionType from '../../../../common/form/models/admission/fullAdmission/paymentOption/paymentOptionType';
 
-export const isCaseDataMissing = (caseData: Claim): boolean => {
+export const isCaseDataMissing = (caseData: Claim) => {
   return !caseData;
 };
 
-export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent): boolean => {
+export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent) => {
   return !respondent1?.correspondenceAddress && !respondent1?.primaryAddress;
 };
 
-export const isDOBMissing = (respondent1: Respondent): boolean => {
+export const isDOBMissing = (respondent1: Respondent) => {
   // check DOB if the defendant type is individual
   return respondent1?.type === CounterpartyType.INDIVIDUAL && !respondent1?.dateOfBirth ;
 };
 
-export const isResponseTypeMissing = (respondent1: Respondent): boolean => {
+export const isResponseTypeMissing = (respondent1: Respondent) => {
   return !respondent1?.responseType;
 };
 
