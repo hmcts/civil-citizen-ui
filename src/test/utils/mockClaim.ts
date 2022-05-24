b28d8f144ee79a3caa2b94c861b7383d77d56856
Kadir <kegilmez@gmail.com>
2022-05-24 17:52:15 +0100

    updated the mockClaim for civil service

--- src/test/utils/mockClaim.ts
+++ src/test/utils/mockClaim.ts
@@ -4,6 +4,8 @@ import {CounterpartyType} from '../../main/common/models/counterpartyType';
 import {PrimaryAddress} from '../../main/common/models/primaryAddress';
 import {CorrespondenceAddress} from '../../main/common/models/correspondenceAddress';
 import {NumberOfDays} from '../../main/common/form/models/numberOfDays';
+import {YesNo} from '../../main/common/form/models/yesNo';
+import {InterestClaimFromType, InterestClaimOptions, InterestClaimUntilType, SameRateInterestType} from '../../main/common/form/models/claimDetails';
 
 export const buildPrimaryAddress = (): PrimaryAddress => {
   return {
@@ -40,7 +42,7 @@ export const buildRespondent1 = () : Respondent =>{
 };
 
 export const mockClaim: Claim = {
-  legacyCaseReference: '497MC585',
+ legacyCaseReference: '497MC585',
   applicant1:
     {
       individualTitle: 'Mrs',
@@ -68,6 +70,37 @@ export const mockClaim: Claim = {
   respondent1ResponseDeadline: new Date('2022-01-24T15:59:59'),
   detailsOfClaim: 'the reason i have given',
   respondent1: buildRespondent1(),
+  timelineOfEvents: [
+    {
+      id: 'a08e42f4-6d7a-4f5c-b33d-85e4ef42ad9e',
+      value: {
+        timelineDate: '2022-01-01',
+        timelineDescription: 'I noticed a leak on the landing and told Mr Smith about this.'
+      },
+    },
+  ],
+  claimFee: {
+    calculatedAmountInPence: '11500',
+  },
+  claimInterest: YesNo.YES,
+  interestClaimFrom: InterestClaimFromType.FROM_A_SPECIFIC_DATE,
+  claimAmountBreakup: [
+    {
+      value: {
+        claimAmount: '2000',
+        claimReason: 'House repair',
+      },
+    },
+  ],
+  interestClaimUntil: InterestClaimUntilType.UNTIL_CLAIM_SUBMIT_DATE,
+  interestFromSpecificDate: new Date('2022-05-20'),
+  interestClaimOptions: InterestClaimOptions.SAME_RATE_INTEREST,
+  sameRateInterestSelection: {
+    sameRateInterestType : SameRateInterestType.SAME_RATE_INTEREST_8_PC,
+  },
+  breakDownInterestTotal: 500,
+  submittedDate: new Date('2022-05-23T17:02:02.38407'),
+
   formattedResponseDeadline: function (): string {
     throw new Error('Function not implemented.');
   },
