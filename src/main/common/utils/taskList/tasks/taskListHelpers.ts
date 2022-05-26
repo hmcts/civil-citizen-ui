042382ac463ba4f737d2f6a4b7fffd1a544731ed
Maro <maro.radovic12@gmail.com>
2022-05-26 12:33:54 +0100

    CIV-1003 - Task List Respond To Claim (#437)
    * feat(CIV-1003): Complete Scenario 1
    
    Admit response on How do you respond screen
    
    * feat(CIV-1003): Complete Scenario 2
    
    Admit & Pay Immediately response
    
    * feat(CIV-1003): Complete Scenario 3
    
    Admit & Repayment Plan response
    
    * feat(CIV-1003): Complete Scenario 4
    
    Admit & Repayment Plan response
    
    * refactor(Task List): Use translations
    
    * feat(Task list): Add initial view unit tests
    
    * feat(CIV-1003): Add Complete Status Logic
    
    * feat(CIV-1003): add tests
    
    * feat(CIV-1003): improve test wording
    
    * refactor(CIV-1003): refactor after team feedback
    
    * remove white space
    
    * fix(): revert master change
    
    * rollback boolean return types
    
    * refactor(CIV-1003): remove old translation
    
    * fix(CIV-2527): redirect to correct pages

--- src/main/common/utils/taskList/tasks/taskListHelpers.ts
+++ src/main/common/utils/taskList/tasks/taskListHelpers.ts
@@ -1,24 +1,54 @@
 import {Claim} from '../../../models/claim';
 import {Respondent} from '../../../../common/models/respondent';
-import { CounterpartyType } from '../../../../common/models/counterpartyType';
+import {CounterpartyType} from '../../../../common/models/counterpartyType';
+import PaymentOptionType from '../../../../common/form/models/admission/fullAdmission/paymentOption/paymentOptionType';
 
-
-export const isCaseDataMissing = (caseData: Claim) => {
+export const isCaseDataMissing = (caseData: Claim): boolean => {
   return !caseData;
 };
 
-export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent) => {
+export const isBothCorrespondenceAndPrimaryAddressMissing = (respondent1: Respondent): boolean => {
   return !respondent1?.correspondenceAddress && !respondent1?.primaryAddress;
 };
 
-export const isDOBMissing = (respondent1: Respondent) => {
+export const isDOBMissing = (respondent1: Respondent): boolean => {
   // check DOB if the defendant type is individual
   return respondent1?.type === CounterpartyType.INDIVIDUAL && !respondent1?.dateOfBirth ;
 };
 
-export const isResponseTypeMissing = (respondent1: Respondent) => {
+export const isResponseTypeMissing = (respondent1: Respondent): boolean => {
   return !respondent1?.responseType;
 };
 
+export const isPaymentOptionMissing = (caseData: Claim): boolean => {
+  return !caseData?.paymentOption;
+};
 
+export const isNotPayImmediatelyResponse = (caseData: Claim): boolean => {
+  return (caseData?.paymentOption !== PaymentOptionType.IMMEDIATELY);
+};
 
+export const isRepaymentPlanMissing = (caseData: Claim): boolean => {
+  return !caseData.repaymentPlan;
+};
+
+export const isStatementOfMeansComplete = (caseData: Claim): boolean => {
+  // TODO: this should replicate statement of means guard logic CIV-2630
+  return !!(caseData?.statementOfMeans && Object.keys(caseData.statementOfMeans).length > 1);
+};
+
+export const financialDetailsShared = (caseData: Claim): boolean => {
+  return !!caseData?.taskSharedFinancialDetails;
+};
+
+export const isIndividualWithStatementOfMeansComplete = (caseData: Claim): boolean => {
+  return (isCounterpartyIndividual(caseData.respondent1) && isStatementOfMeansComplete(caseData));
+};
+
+export const isCounterpartyIndividual = (respondent1: Respondent): boolean => {
+  return respondent1.type === CounterpartyType.INDIVIDUAL || respondent1.type === CounterpartyType.SOLE_TRADER;
+};
+
+export const isCounterpartyCompany = (respondent1: Respondent): boolean => {
+  return respondent1.type === CounterpartyType.ORGANISATION || respondent1.type === CounterpartyType.COMPANY;
+};
