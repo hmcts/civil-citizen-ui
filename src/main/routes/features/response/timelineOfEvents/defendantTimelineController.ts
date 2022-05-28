6bca85b327478ee2393235fdbdc940efee70ace2
Kadir <kegilmez@gmail.com>
2022-05-28 03:05:53 +0100

    updated controller and view to display download link

--- src/main/routes/features/response/timelineOfEvents/defendantTimelineController.ts
+++ src/main/routes/features/response/timelineOfEvents/defendantTimelineController.ts
@@ -9,22 +9,25 @@ import {
 import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
 import {getCaseDataFromStore} from '../../../../modules/draft-store/draftStoreService';
 import {TimeLineOfEvents} from '../../../../common/models/timelineOfEvents/timeLineOfEvents';
+import {Document} from '../../../../common/models/document';
 
 const defendantTimelineController = express.Router();
 const defendantTimelineView = 'features/response/timelineOfEvents/defendant-timeline';
 
 
-function renderView(form: GenericForm<DefendantTimeline>, theirTimeline: TimeLineOfEvents[], res: express.Response) {
-  res.render(defendantTimelineView, {form: form, theirTimeline: theirTimeline});
+function renderView(form: GenericForm<DefendantTimeline>, theirTimeline: TimeLineOfEvents[], theirTimeLinePDFDetails: Document, res: express.Response) {
+  res.render(defendantTimelineView, {form: form, theirTimeline: theirTimeline, theirTimeLinePDFDetails,
+  });
 }
 
 defendantTimelineController.get(CITIZEN_TIMELINE_URL,
   async (req, res) => {
     try {
       const claim = await getCaseDataFromStore(req.params.id);
-      const theirTimeline = claim.timelineOfEvents;
+      const theirTimeline = claim?.timelineOfEvents;
+      const theirTimeLinePDFDetails = claim?.specClaimTemplateDocumentFiles;
       const form = new GenericForm(getPartialAdmitTimeline(claim));
-      renderView(form, theirTimeline, res);
+      renderView(form, theirTimeline, theirTimeLinePDFDetails, res);
     } catch (error) {
       res.status(500).send({error: error.message});
     }
@@ -36,7 +39,8 @@ defendantTimelineController.post(CITIZEN_TIMELINE_URL, async (req, res) => {
     await form.validate();
     if (form.hasErrors()) {
       const claim = await getCaseDataFromStore(req.params.id);
-      renderView(form, claim.timelineOfEvents, res);
+      const theirTimeLinePDFDetails = claim?.specClaimTemplateDocumentFiles;
+      renderView(form, claim?.timelineOfEvents, theirTimeLinePDFDetails, res);
     } else {
       await savePartialAdmitTimeline(req.params.id, form.model);
       res.redirect(constructResponseUrlWithIdParams(req.params.id, CITIZEN_EVIDENCE_URL));
