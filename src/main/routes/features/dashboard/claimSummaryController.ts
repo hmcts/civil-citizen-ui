336665e1038dfc85c69521cb640f419f915bd595
Felipe <felipe@LAPTOP3721.V1.com>
2022-05-30 12:17:51 +0200

    New: util getNameByType

--- src/main/routes/features/dashboard/claimSummaryController.ts
+++ src/main/routes/features/dashboard/claimSummaryController.ts
@@ -1,7 +1,8 @@
-import {Claim} from '../../../common/models/claim';
 import * as express from 'express';
+import {Claim} from '../../../common/models/claim';
 import {getCaseDataFromStore} from '../../../modules/draft-store/draftStoreService';
 import {CLAIMANT_SUMMARY_URL} from '../../urls';
+import {getClaimantName, getDefendantName} from '../../../common/utils/getNameByType';
 
 const claimSummaryViewPath = 'features/dashboard/claim-summary';
 const claimSummaryController = express.Router();
@@ -11,7 +12,9 @@ const logger = Logger.getLogger('claimSummaryController');
 claimSummaryController.get(CLAIMANT_SUMMARY_URL, async (req, res) => {
   try {
     const claim: Claim = await getCaseDataFromStore((req.params.id));
-    res.render(claimSummaryViewPath, { claim, claimId: req.params.id });
+    const claimantName = getClaimantName(claim);
+    const defendantName = getDefendantName(claim);
+    res.render(claimSummaryViewPath, { claim, claimId: req.params.id, claimantName, defendantName });
   } catch (error) {
     logger.error(error);
     res.status(500).send({ error: error.message });
