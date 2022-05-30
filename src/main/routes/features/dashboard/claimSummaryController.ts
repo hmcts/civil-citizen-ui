b51548a0e497ea13a8c162ff0f235baf6a8ad5c6
Felipe <felipe@LAPTOP3721.V1.com>
2022-05-30 09:32:26 +0200

    Update: claim summary

--- src/main/routes/features/dashboard/claimSummaryController.ts
+++ src/main/routes/features/dashboard/claimSummaryController.ts
@@ -1,13 +1,9 @@
-import { Claim } from 'common/models/claim';
+import {Claim} from 'common/models/claim';
 import * as express from 'express';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {CLAIMANT_SUMMARY_URL} from '../../urls';
-// import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
-// import {Claim} from '../../../common/models/claim';
-// import {getCaseDataFromStore} from '../../../modules/draft-store/draftStoreService';
-// import {CounterpartyType} from '../../../common/models/counterpartyType';
 
-const claimSummaryViewPath = 'features/dashboard/test';
+const claimSummaryViewPath = 'features/dashboard/claim-summary';
 const claimSummaryController = express.Router();
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('claimSummaryController');
@@ -15,7 +11,7 @@ const logger = Logger.getLogger('claimSummaryController');
 claimSummaryController.get(CLAIMANT_SUMMARY_URL, async (req, res) => {
   try {
     const claim: Claim = await getCaseDataFromStore((req.params.id));
-    res.render(claimSummaryViewPath, {claim, claimId: req.params.id});
+    res.render(claimSummaryViewPath, { claim, claimId: req.params.id });
   } catch (error) {
     logger.error(error);
     res.status(500).send({ error: error.message });
