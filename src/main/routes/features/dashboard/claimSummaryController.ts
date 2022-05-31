9bc2957982a0188c3a08defe3f71710a752ebcae
Felipe <felipe@LAPTOP3721.V1.com>
2022-05-31 16:40:38 +0200

    Update: claim summary component

--- src/main/routes/features/dashboard/claimSummaryController.ts
+++ src/main/routes/features/dashboard/claimSummaryController.ts
@@ -16,24 +16,27 @@ function getComponentDetailItems(lang: string): ComponentDetailItems {
   return {
     title: t('PAGES.CHECK_YOUR_ANSWER.DETAILS_TITLE', {lng: getLng(lang)}),
     subtitle: t('PAGES.CHECK_YOUR_ANSWER.DETAILS_TITLE', {lng: getLng(lang)}),
-    content: ['<a href="https://www.gov.uk/government/publications/debt-respite-scheme-breathing-space-guidance/debt-respite-scheme-breathing-space-guidance-for-creditors#end-of-a-breathing-space">debt respite scheme (opens in new tab)</a>',
-    '<details class="govuk-details" data-module="govuk-details">\n' +
-    '  <summary class="govuk-details__summary">\n' +
-    '    <span class="govuk-details__summary-text">\n' +
-    '      Help with nationality\n' +
-    '    </span>\n' +
-    '  </summary>\n' +
-    '  <div class="govuk-details__text">\n' +
-    '    We need to know your nationality so we can work out which elections you’re entitled to vote in. If you cannot provide your nationality, you’ll have to send copies of identity documents through the post.\n' +
-    '  </div>\n' +
-    '</details>'],
+    content: [
+      '<p class="govuk-body">If you’ve been paid or you’ve made another agreement with the defendant, you need to tell us.</p>',
+      '<p class="govuk-body">More information about the <a class="govuk-link" href="https://www.gov.uk/government/publications/debt-respite-scheme-breathing-space-guidance/debt-respite-scheme-breathing-space-guidance-for-creditors#end-of-a-breathing-space">debt respite scheme (opens in new tab)</a></p>',
+      '<details class="govuk-details" data-module="govuk-details">\n' +
+      '  <summary class="govuk-details__summary">\n' +
+      '    <span class="govuk-details__summary-text">\n' +
+      '      Help with nationality\n' +
+      '    </span>\n' +
+      '  </summary>\n' +
+      '  <div class="govuk-details__text">\n' +
+      '    We need to know your nationality so we can work out which elections you’re entitled to vote in. If you cannot provide your nationality, you’ll have to send copies of identity documents through the post.\n' +
+      '  </div>\n' +
+      '</details>'],
     button: {
-      text: 'ttt',
-      href: 'testttt',
+      text: 'Example',
+      href: '/test',
     },
     hasDivider:  true,
   };
 }
+
 claimSummaryController.get(CLAIMANT_SUMMARY_URL, async (req, res) => {
   try {
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
@@ -42,7 +45,7 @@ claimSummaryController.get(CLAIMANT_SUMMARY_URL, async (req, res) => {
     const claimantName = getClaimantName(claim);
     const defendantName = getDefendantName(claim);
     const content = getComponentDetailItems(lang);
-    res.render(claimSummaryViewPath, { claim, claimId: req.params.id, claimantName, defendantName, content: content });
+    res.render(claimSummaryViewPath, { claim, claimId: req.params.id, claimantName, defendantName, content });
   } catch (error) {
     logger.error(error);
     res.status(500).send({ error: error.message });
