2b959bce29e40c581e99493cc7b46d2e74716cbb
Felipe <felipe@LAPTOP3721.V1.com>
2022-05-31 14:51:16 +0200

    Fix: dashboard

--- src/main/common/utils/getNameByType.ts
+++ src/main/common/utils/getNameByType.ts
@@ -1,5 +1,5 @@
-import {Claim} from "../../common/models/claim";
-import {CounterpartyType} from "../../common/models/counterpartyType";
+import {Claim} from '../../common/models/claim';
+import {CounterpartyType} from '../../common/models/counterpartyType';
 
 const getClaimantName = (claim: Claim): string => {
   if (claim.applicant1.type === CounterpartyType.INDIVIDUAL || claim.applicant1.type === CounterpartyType.SOLE_TRADER) {
@@ -7,7 +7,7 @@ const getClaimantName = (claim: Claim): string => {
   } else if (claim.applicant1.type === CounterpartyType.ORGANISATION || claim.applicant1.type === CounterpartyType.COMPANY) {
     return claim.applicant1.partyName;
   }
-}
+};
 
 const getDefendantName = (claim: Claim): string => {
   if (claim.respondent1.type === CounterpartyType.INDIVIDUAL || claim.respondent1.type === CounterpartyType.SOLE_TRADER) {
@@ -15,6 +15,6 @@ const getDefendantName = (claim: Claim): string => {
   } else if (claim.respondent1.type === CounterpartyType.ORGANISATION || claim.respondent1.type === CounterpartyType.COMPANY) {
     return claim.respondent1.partyName;
   }
-}
+};
 
-export { getClaimantName, getDefendantName }
+export { getClaimantName, getDefendantName };
