9156b19e0700f4de2daf855740414cf2ec9f6c7b
RosDiaf <ros_diaf@hotmail.com>
2022-06-07 10:04:37 +0100

    Check your answers - Financial details 1

--- src/main/common/utils/currencyFormat.ts
+++ src/main/common/utils/currencyFormat.ts
@@ -7,6 +7,10 @@ export default function currencyFormat(amount: number): string {
   return formatter.format(amount);
 }
 
+export function currencyFormatWithNoTrailingZeros(amount: number): string {
+  return currencyFormat(amount).replace(/(\.00+|00+)$/, '');
+}
+
 export function convertToPoundsFilter(value: number | string): number {
   return Number(value) / 100;
 }
