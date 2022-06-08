a4ebe3690e1881249581fde24f6d0215c6b21c70
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-06-08 14:14:40 +0100

    CIV-2588 fee range unit test

---
+++ src/test/unit/common/models/feeRange.test.ts
@@ -0,0 +1,16 @@
+import {CurrentVersion, FeeRange, FlatAmount} from '../../../../main/common/models/feeRange';
+
+describe('Fee range', ()=>{
+  it('should convert fee ranges to table items successfully', ()=>{
+    //Given
+    const feeRange = new FeeRange(500.01, 10000, new CurrentVersion(1, '', '', '', new FlatAmount(20000)));
+
+    //When
+    const tableItems = feeRange.formatFeeRangeToTableItem();
+
+    //Then
+    expect(tableItems.length).toBe(2);
+    expect(tableItems[0].text).toBe('500.01 to 10,000');
+    expect(tableItems[1].text).toBe('£20,000');
+  });
+});
