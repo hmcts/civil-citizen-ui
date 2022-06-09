368d697e5dbfc47852559762b872ed505d5838ab
Felipe <felipe@LAPTOP3721.V1.com>
2022-06-09 12:17:14 +0200

    New: expert guidance test

--- src/main/routes/features/directionsQuestionnaire/expertGuidanceController.ts
+++ src/main/routes/features/directionsQuestionnaire/expertGuidanceController.ts
@@ -1,4 +1,3 @@
-// import { constructResponseUrlWithIdParams } from '../../../common/utils/urlFormatter';
 import * as express from 'express';
 import {EXPERT_GUIDANCE_URL} from '../../urls';
 
@@ -8,14 +7,6 @@ const expertGuidanceViewPath = 'features/directionsQuestionnaire/expert-guidance
 expertGuidanceController.get(EXPERT_GUIDANCE_URL, async (req, res) => {
   try {
     res.render(expertGuidanceViewPath);
-  } catch (error) {
-    res.status(500).send({error: error.message});
-  }
-});
-
-expertGuidanceController.post(EXPERT_GUIDANCE_URL, async (req, res) => {
-  try {
-    // res.redirect(constructResponseUrlWithIdParams(req.params.id, CLAIM_TASK_LIST_URL));
   } catch (error) {
     res.status(500).send({error: error.message});
   }
