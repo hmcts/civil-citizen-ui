d8e7bc75ed43f04a95d603615be95b4b467525b8
Felipe <felipe@LAPTOP3721.V1.com>
2022-06-09 14:34:08 +0200

    Fix: code smell

--- src/main/routes/features/directionsQuestionnaire/expertGuidanceController.ts
+++ src/main/routes/features/directionsQuestionnaire/expertGuidanceController.ts
@@ -4,7 +4,7 @@ import {EXPERT_GUIDANCE_URL} from '../../urls';
 const expertGuidanceController = express.Router();
 const expertGuidanceViewPath = 'features/directionsQuestionnaire/expert-guidance';
 
-expertGuidanceController.get(EXPERT_GUIDANCE_URL, async (req, res) => {
+expertGuidanceController.get(EXPERT_GUIDANCE_URL, async (_req, res) => {
   try {
     res.render(expertGuidanceViewPath);
   } catch (error) {
