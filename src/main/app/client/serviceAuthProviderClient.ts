0600c1bce47cf56e20c20689df7139b79150321d
Kadir <kegilmez@gmail.com>
2022-06-13 17:36:55 +0100

    resolved conflict

--- src/main/app/client/serviceAuthProviderClient.ts
+++ src/main/app/client/serviceAuthProviderClient.ts
@@ -1,7 +1,10 @@
+import config from 'config';
 import Axios, {AxiosInstance, AxiosResponse} from 'axios';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('serviceAuthProviderClient');
+
+const xuiClientId = config.get<string>('services.dmStore.clientID');
 
 export class ServiceAuthProviderClient {
   client: AxiosInstance;
@@ -23,9 +26,10 @@ export class ServiceAuthProviderClient {
   async getServiceAuthorisationToken(): Promise<string> {
     const options = this.getConfig();
     const serviceAuthTokenGeneratorUrl = '/testing-support/lease';
-    // TODO : should be replaced with civil_citizen_ui when civil citizen ui is given permission to access prf documents created by xui
+    // TODO : should be replaced with civil_citizen_ui when civil citizen ui 
+    // is given permission to access prf documents on dm - store created via Xui
     const reqBody = {
-      microservice: 'xui_webapp',
+      microservice: xuiClientId,
     };
     try {
       const response: AxiosResponse<string> = await this.client.post(serviceAuthTokenGeneratorUrl, reqBody, options);
