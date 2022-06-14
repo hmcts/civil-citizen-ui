7a7aa8cbc7be50e4ac477e43cdd99aafbe429a42
Kadir <kegilmez@gmail.com>
2022-06-14 20:12:54 +0100

    minor changes

--- src/main/common/utils/downloadUtils.ts
+++ src/main/common/utils/downloadUtils.ts
@@ -1,13 +1,10 @@
 import * as express from 'express';
 
-export class DownloadUtils {
-
-  static downloadPDF(res: express.Response, content: Buffer, filename: string) {
-    res.writeHead(200, {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': `inline; filename=${filename}.pdf`,
-      'Content-Length': content ? content.length : 0,
-    });
-    res.end(content);
-  }
+export function downloadPDF(res: express.Response, content: Buffer, filename: string) {
+  res.writeHead(200, {
+    'Content-Type': 'application/pdf',
+    'Content-Disposition': `inline; filename=${filename}.pdf`,
+    'Content-Length': content ? content.length : 0,
+  });
+  res.end(content);
 }
