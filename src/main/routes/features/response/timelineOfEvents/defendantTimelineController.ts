2c7bc3f9aced9b75e21b00e4f1e957a28aec3917
Kadir <kegilmez@gmail.com>
2022-06-17 04:00:38 +0100

    updated controllers and views

--- src/main/routes/features/response/timelineOfEvents/defendantTimelineController.ts
+++ src/main/routes/features/response/timelineOfEvents/defendantTimelineController.ts
@@ -1,5 +1,9 @@
 import * as express from 'express';
-import {CITIZEN_EVIDENCE_URL, CITIZEN_TIMELINE_URL} from '../../../urls';
+import {
+  CITIZEN_EVIDENCE_URL,
+  CITIZEN_TIMELINE_URL,
+  CASE_TIMELINE_DOCUMENTS_URL,
+} from '../../../urls';
 import {GenericForm} from '../../../../common/form//models/genericForm';
 import {DefendantTimeline} from '../../../../common/form//models/timeLineOfEvents/defendantTimeline';
 import {
@@ -14,8 +18,10 @@ const defendantTimelineController = express.Router();
 const defendantTimelineView = 'features/response/timelineOfEvents/defendant-timeline';
 
 
-function renderView(form: GenericForm<DefendantTimeline>, theirTimeline: TimeLineOfEvents[], res: express.Response) {
-  res.render(defendantTimelineView, {form: form, theirTimeline: theirTimeline});
+function renderView(form: GenericForm<DefendantTimeline>, theirTimeline: TimeLineOfEvents[], pdfUrl: string, res: express.Response) {
+  res.render(defendantTimelineView, {
+    form: form, theirTimeline: theirTimeline, pdfUrl,
+  });
 }
 
 defendantTimelineController.get(CITIZEN_TIMELINE_URL,
@@ -23,8 +29,9 @@ defendantTimelineController.get(CITIZEN_TIMELINE_URL,
     try {
       const claim = await getCaseDataFromStore(req.params.id);
       const theirTimeline = claim.timelineOfEvents;
+      const pdfUrl = CASE_TIMELINE_DOCUMENTS_URL.replace(':id', req.params.id);
       const form = new GenericForm(getPartialAdmitTimeline(claim));
-      renderView(form, theirTimeline, res);
+      renderView(form, theirTimeline, pdfUrl, res);
     } catch (error) {
       res.status(500).send({error: error.message});
     }
@@ -36,7 +43,8 @@ defendantTimelineController.post(CITIZEN_TIMELINE_URL, async (req, res) => {
     await form.validate();
     if (form.hasErrors()) {
       const claim = await getCaseDataFromStore(req.params.id);
-      renderView(form, claim.timelineOfEvents, res);
+      const pdfUrl = CASE_TIMELINE_DOCUMENTS_URL.replace(':id', req.params.id);
+      renderView(form, claim.timelineOfEvents, pdfUrl, res);
     } else {
       await savePartialAdmitTimeline(req.params.id, form.model);
       res.redirect(constructResponseUrlWithIdParams(req.params.id, CITIZEN_EVIDENCE_URL));
