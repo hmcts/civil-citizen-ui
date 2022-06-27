91afac1ed956234afd45caaae4a681c7faa4c10a
Taoufik Mohdit <taoufik.mohdit@HMCTS.net>
2022-06-27 17:20:37 +0100

    Fix broken tests

--- src/test/unit/views/features/response/dashboard/dashboard.test.ts
+++ src/test/unit/views/features/response/dashboard/dashboard.test.ts
@@ -1,12 +1,22 @@
-import request from 'supertest';
 import {app} from '../../../../../../main/app';
 import config from 'config';
+import Module from 'module';
 import {DASHBOARD_URL} from '../../../../../../main/routes/urls';
 
-jest.mock('../../../../../../main/modules/oidc');
-jest.mock('../../../../../../main/modules/draft-store');
-
 const nock = require('nock');
+const session = require('supertest-session');
+const citizenRoleToken: string = config.get('citizenRoleToken');
+const testSession = session(app);
+
+jest.mock('../../../../../../main/app/auth/user/oidc', () => ({
+  ...jest.requireActual('../../../../../../main/app/auth/user/oidc') as Module,
+  getUserDetails: jest.fn(() => USER_DETAILS),
+}));
+
+export const USER_DETAILS = {
+  accessToken: citizenRoleToken,
+  roles: ['citizen'],
+};
 
 describe('Dashboard page', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
@@ -18,19 +28,33 @@ describe('Dashboard page', () => {
   });
 
   describe('on GET', () => {
+
+    beforeEach((done) => {
+      testSession
+        .get('/oauth2/callback')
+        .query('code=ABC')
+        .expect(302)
+        .end(function (err: Error) {
+          if (err) {
+            return done(err);
+          }
+          return done();
+        });
+    });
+
     test('should return dashboard page in english', async () => {
-      await request(app)
+      await testSession
         .get(DASHBOARD_URL + '?lang=en')
-        .expect((res) => {
+        .expect((res: Response) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain('Claims made against you');
         });
     });
 
     test('should return dashboard page in cymraeg', async () => {
-      await request(app)
+      await testSession
         .get(DASHBOARD_URL + '?lang=cy')
-        .expect((res) => {
+        .expect((res: Response) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain('Smialc edam tsniaga uoy');
         });
