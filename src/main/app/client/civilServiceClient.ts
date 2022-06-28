85cd8a8933e21ad7d4975e6c12e13f862d7f0471
Kadir <kegilmez@gmail.com>
2022-06-28 11:35:58 +0100

    moved the error messages to a seperate fime on client

--- src/main/app/client/civilServiceClient.ts
+++ src/main/app/client/civilServiceClient.ts
@@ -11,6 +11,10 @@ import {
 import {FeeRange, FeeRanges} from '../../common/models/feeRange';
 import {plainToInstance} from 'class-transformer';
 import {CaseDocument} from 'common/models/document/caseDocument';
+import {
+  CLAIM_DETAILS_NOT_AVAILBALE,
+  DOCUMENT_NOT_AVAILABLE,
+} from './errorMessageContants';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('civilServiceClient');
@@ -62,7 +66,7 @@ export class CivilServiceClient {
       const response: AxiosResponse<object> = await this.client.get(`/cases/${claimId}`, config);// nosonar
 
       if (!response.data) {
-        throw new AssertionError({message: 'Claim details not available!'});
+        throw new AssertionError({message: CLAIM_DETAILS_NOT_AVAILBALE});
       }
       return Object.assign(new Claim(), response.data);
     } catch (err: unknown) {
@@ -86,7 +90,7 @@ export class CivilServiceClient {
     try {
       const response: AxiosResponse<object> = await this.client.post(CIVIL_SERVICE_DOWNLOAD_DOCUMENT_URL, documentDetails, config);
       if (!response.data) {
-        throw new AssertionError({message: 'Document is not available.'});
+        throw new AssertionError({message: DOCUMENT_NOT_AVAILABLE});
       }
       return response.data as Buffer;
     } catch (err: unknown) {
