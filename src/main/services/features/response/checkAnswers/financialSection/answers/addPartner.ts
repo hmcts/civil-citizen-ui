ae470a49b6dc8cbec434a775a15b5481ce22589c
RosDiaf <ros_diaf@hotmail.com>
2022-06-28 15:59:44 +0100

    Hide partner disability and pension when no partner declared

--- src/main/services/features/response/checkAnswers/financialSection/answers/addPartner.ts
+++ src/main/services/features/response/checkAnswers/financialSection/answers/addPartner.ts
@@ -36,7 +36,7 @@ export const addPartner = (claim: Claim, financialSection: SummarySection, claim
     financialSection.summaryList.rows.push(summaryRow(t('PAGES.CHECK_YOUR_ANSWER.PARTNER_DOES_RECEIVE_A_PENSION', { lng: getLng(lang) }), partnerPension.charAt(0).toUpperCase() + partnerPension.slice(1), yourPartnerPensionHref, changeLabel(lang)));
   }
 
-  if (claim.statementOfMeans?.disability?.option === YesNo.YES) {
+  if (cohabiting === YesNo.YES) {
     financialSection.summaryList.rows.push(summaryRow(t('PAGES.CHECK_YOUR_ANSWER.PARTNER_IS_DISABLED', { lng: getLng(lang) }), partnerDisability.charAt(0).toUpperCase() + partnerDisability.slice(1), yourPartnerDisabilityHref, changeLabel(lang)));
     if (partnerDisability === YesNo.YES) {
       financialSection.summaryList.rows.push(summaryRow(t('PAGES.CHECK_YOUR_ANSWER.PARTNER_IS_SEVERELY_DISABLED', { lng: getLng(lang) }), partnerSevereDisability.charAt(0).toUpperCase() + partnerSevereDisability.slice(1), yourPartnerSevereDisabilityHref, changeLabel(lang)));
