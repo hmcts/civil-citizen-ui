7432e129ffe23c5c0d4e1d55e717550fff0e8f54
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2022-06-28 12:04:17 +0200

    Fix: error message bug (#546)
    * Fix: error message bug
    
    * Update: add unit test
    
    Co-authored-by: Felipe <felipe@LAPTOP3721.V1.com>

--- src/test/unit/routes/features/response/citizenDob/citizenDobController.test.ts
+++ src/test/unit/routes/features/response/citizenDob/citizenDobController.test.ts
@@ -2,7 +2,7 @@ import {app} from '../../../../../../main/app';
 import config from 'config';
 import nock from 'nock';
 import request from 'supertest';
-import {VALID_DATE, VALID_DAY, VALID_MONTH, VALID_YEAR} from '../../../../../../main/common/form/validationErrors/errorMessageConstants';
+import {VALID_DATE, VALID_DAY, VALID_FOUR_DIGIT_YEAR, VALID_MONTH, VALID_YEAR} from '../../../../../../main/common/form/validationErrors/errorMessageConstants';
 import {AGE_ELIGIBILITY_URL, DOB_URL, CITIZEN_PHONE_NUMBER_URL} from '../../../../../../main/routes/urls';
 import {mockCivilClaim, mockCivilClaimUndefined, mockRedisFailure, mockNoStatementOfMeans} from '../../../../../utils/mockDraftStore';
 import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
@@ -70,7 +70,6 @@ describe('Citizen date of birth', () => {
         .send('day=')
         .expect((res) => {
           expect(res.status).toBe(200);
-          expect(res.text).toContain(VALID_DATE);
           expect(res.text).toContain(VALID_DAY);
           expect(res.text).toContain(VALID_MONTH);
           expect(res.text).toContain(VALID_YEAR);
@@ -107,6 +106,17 @@ describe('Citizen date of birth', () => {
         .expect((res) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain(VALID_DATE);
+        });
+    });
+    test('should return error 4 digit year', async () => {
+      await request(app)
+        .post(DOB_URL)
+        .send('year=22')
+        .send('month=1')
+        .send('day=1')
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain(VALID_FOUR_DIGIT_YEAR);
         });
     });
     test('should accept a valid input', async () => {
