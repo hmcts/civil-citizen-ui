7065f88e81b28a58f101ee8997743fe0c4e081f8
Taoufik Mohdit <91465696+taomoh@users.noreply.github.com>
2022-06-30 15:46:37 +0100

    Feature/civ 2420 get draft records from legacy draft store (#549)
    * Add first working version of Service Auth Provider client
    
    * Move Service Auth Provider client code to its own file
    
    * First iteration of getting draft documents from legacy draft store, for a given user via their access token
    
    * revert server.ts temporary changes
    
    * Configure integration with mocked Legacy Draft Store, including integration with the actual Service Authorisation Provider
    
    * Add URL and secret configuration for test environments
    
    * Fix broken tests
    
    * Bumping chart version
    
    * Fix broken test
    
    * PR feedback
    
    * Formatting
    
    * Increase test coverage and add logging
    
    * Attempt to fix access to CMC Key Vault
    
    * Another attempt to fix access to CMC Key Vault
    Refactor test
    
    * Increase test coverage
    
    * Add debug logging
    
    * update yarn.lock
    
    * Bumping chart version/ fixing aliases
    
    * more logging (troubleshooting)
    
    * more logging (troubleshooting)
    
    * troubleshooting attempt
    
    * check if secret declaration is needed in terraform config
    
    * Move config.get() calls for secrets inside getOcmcDraftClaims function call, to ensure secrets are loaded into nodejs properties by the time the function is called.
    Also clean up log messages introduced earlier for troubleshooting
    
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- src/test/unit/app/client/serviceAuthProviderClient.test.ts
+++ src/test/unit/app/client/serviceAuthProviderClient.test.ts
@@ -1,14 +1,16 @@
 import axios, {AxiosInstance} from 'axios';
 import config from 'config';
-import {ServiceAuthProviderClient} from '../../../../main/app/client/serviceAuthProviderClient';
-
+import {generateServiceToken, ServiceAuthProviderClient} from '../../../../main/app/client/serviceAuthProviderClient';
+import {authenticator} from 'otplib';
 
 jest.mock('axios');
+jest.mock('otplib');
+
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 const baseUrl: string = config.get('baseUrl');
 
 describe('Service Authorisation Provider Client', () => {
-  it('return service aurhorisation token', async () => {
+  it('return service authorisation token', async () => {
 
     const mockResponse = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ4dWlfd2ViYXBwIiwiZXhwIjoxNjU1MTUxNjUzfQ.AAara6z3N4hsvlaZdP_Xi8e9PnQHHBB4HpBBrEkZSk1_9eesnLXn6-r-4VoVBihFoin2DSand4_NqTkahCAF9g';
 
@@ -21,5 +23,16 @@ describe('Service Authorisation Provider Client', () => {
     });
     expect(mockPost.mock.calls[0][0]).toEqual('/testing-support/lease');
     expect(actualToken.length).toEqual(mockResponse.length);
+  });
+
+  it('should throw error when service token generation fails', async () => {
+    //When
+    (authenticator.generate as jest.Mock).mockImplementation(() => {
+      throw new Error('OTP Generation Error');
+    });
+
+    //Then
+    await expect(
+      generateServiceToken('microservice','s2sSecret')).rejects.toThrow('OTP Generation Error');
   });
 });
