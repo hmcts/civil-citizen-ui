7ed5dd6214771ee6ca62ae39c36e030ed1ca0cbe
rajakm <rajakm@gmail.com>
2022-06-30 14:23:22 +0100

    updated the env value

--- src/test/config.ts
+++ src/test/config.ts
@@ -8,7 +8,7 @@ if (!process.env.TEST_PASSWORD) {
 
 export const config = {
   TestUrl: process.env.TEST_URL || 'https://civil-citizen-ui.demo.platform.hmcts.net',
-  env: 'demo',
+  env: 'preview',
   TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
   TestSlowMo: 250,
   WaitForTimeout: 10000,
