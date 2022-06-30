98586fe07bde396d1304be91f5482f988481e31d
OsheaF <fergal.oshea@hmcts.net>
2022-06-30 14:27:45 +0100

    CIV-2747 Delegating getHref()

--- src/main/common/models/dashboard/dashboardItem.ts
+++ src/main/common/models/dashboard/dashboardItem.ts
@@ -5,19 +5,20 @@ export class DashboardItem {
   claimantName: string;
   defendantName: string;
   responseDeadline?: Date;
-
   ocmc?: boolean;
-
-  getHref (){
-    return this.ocmc? '#' : `/dashboard/${this.claimId}/claimant`;
-  }
 }
 
 export class DashboardClaimantItem extends DashboardItem  {
   nextSteps: string;// TODO: this is only a placeholder. To be revisited in a separate story
   actions: string;// TODO: this is only a placeholder. To be revisited in a separate story
+  getHref (){
+    return this.ocmc? '#' : `/dashboard/${this.claimId}/claimant`;
+  }
 }
 
 export class DashboardDefendantItem extends DashboardItem {
   status: string;// TODO: this is only a placeholder. To be revisited in a separate story
+  getHref (){
+    return this.ocmc? '#' : `/dashboard/${this.claimId}/defendant`;
+  }
 }
