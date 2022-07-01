ed26f798702e2ce4afdf7cb39e1f5fa6b6e09581
RosDiaf <ros_diaf@hotmail.com>
2022-07-01 09:06:45 +0100

    Changes reverted

--- src/main/common/utils/taskList/tasks/giveUsDetailsHearing.ts
+++ src/main/common/utils/taskList/tasks/giveUsDetailsHearing.ts
@@ -6,7 +6,7 @@ import {SUPPORT_REQUIRED_URL} from '../../../../routes/urls';
 import {getLng} from '../../../../common/utils/languageToggleUtils';
 import {t} from 'i18next';
 
-export const getGiveUsDetailsHearingTask = (caseData: Claim, claimId: string, lang: string): Task => {
+export const getGiveUsDetailsHearingTask = (_caseData: Claim, claimId: string, lang: string): Task => {
   const giveUsDetailsHearingTask: Task = {
     description: t('TASK_LIST.YOUR_HEARING_REQUIREMENTS.GIVE_US_DETAILS', { lng: getLng(lang) }),
     url: constructResponseUrlWithIdParams(claimId, SUPPORT_REQUIRED_URL),
