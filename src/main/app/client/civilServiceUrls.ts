5d550d8d8a3f4d9c7c64ba03e2bead00a33f2e67
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-07-08 08:32:43 +0100

    CIV-2814 correct url for api call and point temporarily to image civil service pr for token

--- src/main/app/client/civilServiceUrls.ts
+++ src/main/app/client/civilServiceUrls.ts
@@ -3,4 +3,4 @@ export const CIVIL_SERVICE_FEES_URL = '/fees/';
 export const CIVIL_SERVICE_FEES_RANGES = `${CIVIL_SERVICE_FEES_URL}ranges/`;
 export const CIVIL_SERVICE_DOCUMENT_URL = '/case/document/';
 export const CIVIL_SERVICE_DOWNLOAD_DOCUMENT_URL = `${CIVIL_SERVICE_DOCUMENT_URL}downloadSealedDoc/`;
-export const CIVIL_SERVICE_SUBMIT_RESPONSE_EVENT_TOKEN = `${CIVIL_SERVICE_CASES_URL}defendants/:submitterId/responses/:caseId/event-token`;
+export const CIVIL_SERVICE_SUBMIT_RESPONSE_EVENT_TOKEN = `${CIVIL_SERVICE_CASES_URL}defendant/:submitterId/response/:caseId/event-token`;
