9759c3e446e88cee6e1d9fdae328d4abd7d1faea
commitBlob <maro.radovic12@gmail.com>
2022-07-11 11:29:50 +0100

    feat(CIV-2916): refactor task path

--- src/main/common/utils/taskList/tasks/needMoreTime.ts
+++ src/main/common/utils/taskList/tasks/needMoreTime.ts
@@ -2,16 +2,17 @@ import {Task} from '../../../models/taskList/task';
 import {Claim} from '../../../models/claim';
 import {TaskStatus} from '../../../models/taskList/TaskStatus';
 import {t} from 'i18next';
-import {getLng} from 'common/utils/languageToggleUtils';
+import {getLng} from '../../../../common/utils/languageToggleUtils';
+import {UNDERSTANDING_RESPONSE_OPTIONS_URL} from '../../../../routes/urls';
+import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
 
-const needMoreTimeTask = {
-  description: t('TASK_LIST.RESPOND_TO_CLAIM.VIEW_OPTIONS'),
-  url: '/more-time-request',
-  status: TaskStatus.INCOMPLETE,
-};
+export const getNeedMoreTimeTask = (_claim: Claim, claimId: string, language: string): Task => {
+  const needMoreTimeTask = {
+    description: t('TASK_LIST.RESPOND_TO_CLAIM.VIEW_OPTIONS', { lng: getLng(language) }),
+    url: constructResponseUrlWithIdParams(claimId, UNDERSTANDING_RESPONSE_OPTIONS_URL),
+    status: TaskStatus.INCOMPLETE,
+  };
 
-export const getNeedMoreTimeTask = (claim: Claim, language: string): Task => {
-  needMoreTimeTask.description = t('TASK_LIST.RESPOND_TO_CLAIM.VIEW_OPTIONS', { lng: getLng(language) });
   /**
   * TODO: add logic to mark task as complete if
   *  - within response deadline
@@ -19,6 +20,7 @@ export const getNeedMoreTimeTask = (claim: Claim, language: string): Task => {
   *  - or CIV-915 - no selection
   *  - or CIV-915 - my request has been refused selection
   **/
+
   return needMoreTimeTask;
 };
 
