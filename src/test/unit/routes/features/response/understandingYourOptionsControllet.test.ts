9a15abbf891a89dfcbd567195baa34dc5366ed5f
commitBlob <maro.radovic12@gmail.com>
2022-07-11 13:54:35 +0100

    feat(CIV-2916): add controller unit tests WIP

---
+++ src/test/unit/routes/features/response/understandingYourOptionsControllet.test.ts
@@ -0,0 +1,30 @@
+import config from 'config';
+import nock from 'nock';
+import request from 'supertest';
+import {app} from '../../../../../main/app';
+import {mockCivilClaim} from '../../../../utils/mockDraftStore';
+import {UNDERSTANDING_RESPONSE_OPTIONS_URL} from '../../../../../main/routes/urls';
+
+jest.mock('../../../../../main/modules/oidc');
+jest.mock('../../../../../main/modules/draft-store');
+
+describe('Understanding Your Options Controller', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+  beforeEach(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+  });
+
+  describe('on GET', () => {
+    it('should return understanding you options page successfully', async () => {
+      app.locals.draftStoreClient = mockCivilClaim;
+
+      await request(app).get(UNDERSTANDING_RESPONSE_OPTIONS_URL).expect((res) => {
+        expect(res.status).toBe(200);
+        expect(res.text).toContain('Requesting extra time');
+      });
+    });
+  });
+});
