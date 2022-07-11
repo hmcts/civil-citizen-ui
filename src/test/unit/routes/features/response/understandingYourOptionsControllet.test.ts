cb3711da3c2ebba2a0e64913c3733e7e0bb9b8fb
commitBlob <maro.radovic12@gmail.com>
2022-07-11 14:45:16 +0100

    feat(CIV-2916): add controller unit tests

--- src/test/unit/routes/features/response/understandingYourOptionsControllet.test.ts
+++ src/test/unit/routes/features/response/understandingYourOptionsControllet.test.ts
@@ -2,8 +2,13 @@ import config from 'config';
 import nock from 'nock';
 import request from 'supertest';
 import {app} from '../../../../../main/app';
-import {mockCivilClaim} from '../../../../utils/mockDraftStore';
+import {
+  mockCivilClaim,
+  mockRedisFailure,
+  mockRedisFullAdmission,
+} from '../../../../utils/mockDraftStore';
 import {UNDERSTANDING_RESPONSE_OPTIONS_URL} from '../../../../../main/routes/urls';
+import {TestMessages} from '../../../../utils/errorMessageTestConstants';
 
 jest.mock('../../../../../main/modules/oidc');
 jest.mock('../../../../../main/modules/draft-store');
@@ -20,10 +25,25 @@ describe('Understanding Your Options Controller', () => {
   describe('on GET', () => {
     it('should return understanding you options page successfully', async () => {
       app.locals.draftStoreClient = mockCivilClaim;
-
       await request(app).get(UNDERSTANDING_RESPONSE_OPTIONS_URL).expect((res) => {
         expect(res.status).toBe(200);
         expect(res.text).toContain('Requesting extra time');
+      });
+    });
+
+    it('should return understanding you options page when response deadline date is not set', async () => {
+      app.locals.draftStoreClient = mockRedisFullAdmission;
+      await request(app).get(UNDERSTANDING_RESPONSE_OPTIONS_URL).expect((res) => {
+        expect(res.status).toBe(200);
+        expect(res.text).toContain('Requesting extra time');
+      });
+    });
+
+    it('should return an error page if request fails', async () => {
+      app.locals.draftStoreClient = mockRedisFailure;
+      await request(app).get(UNDERSTANDING_RESPONSE_OPTIONS_URL).expect((res) => {
+        expect(res.status).toBe(500);
+        expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
       });
     });
   });
