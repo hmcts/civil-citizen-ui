80ba702aff2cfc8c1f804cb8e547820a03188c3a
commitBlob <maro.radovic12@gmail.com>
2022-07-14 10:19:22 +0100

    feat(CIV-915): add controller tests

--- src/main/common/models/responseDeadline.ts
+++ src/main/common/models/responseDeadline.ts
@@ -2,5 +2,4 @@ import {ResponseOptions} from '../../common/form/models/responseDeadline';
 
 export interface ResponseDeadline {
   option?: ResponseOptions,
-  deadlineDate?: Date; // TODO: refactor respondent1ResponseDeadline into this?
 }
