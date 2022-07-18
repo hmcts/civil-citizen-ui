497103d7df3e584649fdd496a1506b035dbeff9d
commitBlob <maro.radovic12@gmail.com>
2022-07-18 08:44:08 +0100

    feat(CIV-3118): add view unit tests

--- src/test/unit/views/features/response/request-more-time.test.ts
+++ src/test/unit/views/features/response/request-more-time.test.ts
@@ -15,13 +15,16 @@ describe('Request More Time View', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
 
+  beforeEach(async () => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+    app.locals.draftStoreClient = mockCivilClaim;
+  });
+
   describe('on GET', () => {
     let htmlDocument: Document;
     beforeEach(async () => {
-      nock(idamUrl)
-        .post('/o/token')
-        .reply(200, {id_token: citizenRoleToken});
-      app.locals.draftStoreClient = mockCivilClaim;
       const response = await request(app).get(REQUEST_MORE_TIME_URL);
       const dom = new JSDOM(response.text);
       htmlDocument = dom.window.document;
@@ -85,6 +88,29 @@ describe('Request More Time View', () => {
     it('should display contact us for help', () => {
       const contactUs = htmlDocument.getElementsByClassName('govuk-details__summary-text');
       expect(contactUs[0].innerHTML).toContain('Contact us for help');
+    });
+  });
+
+  describe('on POST', () => {
+    let htmlDocument: Document;
+    beforeEach(async () => {
+      const response = await request(app).post(REQUEST_MORE_TIME_URL);
+      const dom = new JSDOM(response.text);
+      htmlDocument = dom.window.document;
+    });
+
+    it('should display error summary component', () => {
+      const errorSummaryTitle = htmlDocument.getElementsByClassName('govuk-error-summary__title')[0];
+      const errorSummaryMessage = htmlDocument.getElementsByClassName('govuk-error-summary__list')[0]
+        .getElementsByTagName('li')[0].getElementsByTagName('a')[0];
+      expect(errorSummaryTitle.innerHTML).toContain('There was a problem');
+      expect(errorSummaryMessage.innerHTML).toContain('Select how much additional time you are asking for');
+      expect(errorSummaryMessage.getAttribute('href')).toBe('#option');
+    });
+
+    it('should display error message', () => {
+      const errorMessage = htmlDocument.getElementsByClassName('govuk-error-message')[0];
+      expect(errorMessage.innerHTML).toContain('Select how much additional time you are asking for');
     });
   });
 });
