a8d50463ec90a154e1088aea9678810db0ebdb86
Maro <maro.radovic12@gmail.com>
2022-07-19 16:16:08 +0100

    tech-debt(CIV-2784): refactor getClaimantName function (#618)

--- src/main/routes/features/response/admission/fullRejection/youHavePaidLessController.ts
+++ src/main/routes/features/response/admission/fullRejection/youHavePaidLessController.ts
@@ -1,15 +1,15 @@
 import * as express from 'express';
 import {CITIZEN_FULL_REJECTION_YOU_PAID_LESS_URL, CLAIM_TASK_LIST_URL} from '../../../../urls';
-import {getClaimantName} from '../../../../../services/features/response/rejectAllOfClaimService';
 import {constructResponseUrlWithIdParams} from '../../../../../common/utils/urlFormatter';
+import {getCaseDataFromStore} from '../../../../../modules/draft-store/draftStoreService';
 
 const youHavePaidLessController = express.Router();
 const youHavePaidLessViewPath = 'features/response/admission/fullRejection/you-have-paid-less';
 
 youHavePaidLessController.get(CITIZEN_FULL_REJECTION_YOU_PAID_LESS_URL, async (req, res, next: express.NextFunction) => {
   try {
-    const claimantName = await getClaimantName(req.params.id);
-    res.render(youHavePaidLessViewPath, {claimantName});
+    const claim = await getCaseDataFromStore(req.params.id);
+    res.render(youHavePaidLessViewPath, {claimantName: claim.getClaimantName()});
   }
   catch (error) {
     next(error);
