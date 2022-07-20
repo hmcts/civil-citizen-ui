51a5c76fda665fbc5878a86ccddb534c60a0f508
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-07-20 16:17:55 +0100

    test view

--- src/test/unit/views/features/response/responseDeadline/new-response-deadline.test.ts
+++ src/test/unit/views/features/response/responseDeadline/new-response-deadline.test.ts
@@ -7,27 +7,28 @@ import * as draftStoreService from '../../../../../../main/modules/draft-store/d
 import {Claim} from '../../../../../../main/common/models/claim';
 import {NEW_RESPONSE_DEADLINE_URL} from '../../../../../../main/routes/urls';
 import {CounterpartyType} from '../../../../../../main/common/models/counterpartyType';
-import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
 
 jest.mock('../../../../../../main/modules/oidc');
 jest.mock('../../../../../../main/modules/draft-store/draftStoreService');
 
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+
 const mockGetCaseDataFromStore = draftStoreService.getCaseDataFromStore as jest.Mock;
 
-describe('Response - New response deadline', () => {
+describe('New response deadline view', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamServiceUrl: string = config.get('services.idam.url');
-  beforeEach(() => {
+  let htmlDocument: Document;
+
+  beforeEach(async () => {
     nock(idamServiceUrl)
       .post('/o/token')
       .reply(200, {id_token: citizenRoleToken});
     nock('http://localhost:4000')
       .post(CIVIL_SERVICE_CALCULATE_DEADLINE)
       .reply(200,  new Date(2022, 9, 31));
-  });
-  test('should return new deadline date successfully', async () => {
     const extendedDate = new Date(2022, 9, 31);
-    const expectedDate = '31 October 2022';
     const claim = new Claim();
     claim.applicant1 = {
       partyName: 'Mr. James Bond',
@@ -37,35 +38,28 @@ describe('Response - New response deadline', () => {
       agreedResponseDeadline : extendedDate,
     };
     mockGetCaseDataFromStore.mockImplementation(async () => claim);
-
-    await request(app).get(NEW_RESPONSE_DEADLINE_URL)
-      .expect((res) => {
-        expect(res.status).toBe(200);
-        expect(res.text).toContain(expectedDate);
-        expect(res.text).toContain(claim.getClaimantName());
-      });
+    const response = await request(app).get(NEW_RESPONSE_DEADLINE_URL);
+    const dom = new JSDOM(response.text);
+    htmlDocument = dom.window.document;
   });
-  test('should show error when proposed extended deadline does not exist', async () =>{
-    const claim = new Claim();
-    claim.applicant1 = {
-      partyName: 'Mr. James Bond',
-      type: CounterpartyType.INDIVIDUAL,
-    };
-    mockGetCaseDataFromStore.mockImplementation(async () => claim);
-    await request(app).get(NEW_RESPONSE_DEADLINE_URL)
-      .expect((res) => {
-        expect(res.status).toBe(500);
-        expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
-      });
+  it('should have correct title',  () => {
+    const header = htmlDocument.getElementsByClassName('govuk-heading-l');
+    expect(header[0].innerHTML).toContain('New response deadline');
   });
-  test('should show error when draft store throws error', async () =>{
-    mockGetCaseDataFromStore.mockImplementation(async () => {
-      throw new Error(TestMessages.REDIS_FAILURE);
-    });
-    await request(app).get(NEW_RESPONSE_DEADLINE_URL)
-      .expect((res) => {
-        expect(res.status).toBe(500);
-        expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
-      });
+  it('should have correct subtitle', () => {
+    const subHeader =  htmlDocument.getElementsByClassName('govuk-heading-m');
+    expect(subHeader[0].innerHTML).toContain('4 pm on 31 October 2022');
+  });
+  it('should have Continue button', () => {
+    const continueButton = htmlDocument.getElementsByClassName('govuk-button');
+    expect(continueButton[0].innerHTML).toContain('Continue');
+  });
+  it('should have Contact for help', ()=>{
+    const details = htmlDocument.getElementsByClassName('govuk-details__summary-text');
+    expect(details[0].innerHTML).toContain('Contact us for help');
+  });
+  it('should have Back button', ()=>{
+    const backButton = htmlDocument.getElementsByClassName('govuk-back-link');
+    expect(backButton[0].innerHTML).toContain('Back');
   });
 });
