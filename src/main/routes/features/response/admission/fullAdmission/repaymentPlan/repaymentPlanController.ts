c73457900cfe6c71b1571ea77ac689396c727fa8
laarmada <sergiosalmar@yahoo.es>
2022-07-21 11:32:21 +0100

    Fix QA comments

--- src/main/routes/features/response/admission/fullAdmission/repaymentPlan/repaymentPlanController.ts
+++ src/main/routes/features/response/admission/fullAdmission/repaymentPlan/repaymentPlanController.ts
@@ -8,6 +8,7 @@ import {
 } from '../../../../../../services/features/response/repaymentPlan/repaymentPlanService';
 import {CITIZEN_REPAYMENT_PLAN_FULL_URL, CLAIM_TASK_LIST_URL} from '../../../../../urls';
 import {validateForm} from '../../../../../../common/form/validators/formValidator';
+import {getCaseDataFromStore} from '../../../../../../modules/draft-store/draftStoreService';
 
 const repaymentPlanViewPath = 'features/response/repaymentPlan/repaymentPlan';
 const repaymentPlanFullAdmissionController = express.Router();
@@ -26,7 +27,8 @@ export const getFirstPaymentExampleDate = () => {
 
 repaymentPlanFullAdmissionController.get(CITIZEN_REPAYMENT_PLAN_FULL_URL, async (req, res, next: express.NextFunction) => {
   try {
-    const form = await getRepaymentPlanForm(req.params.id);
+    const claim = await getCaseDataFromStore(req.params.id);
+    const form = await getRepaymentPlanForm(claim);
     renderView(form, res);
   } catch (error) {
     next(error);
@@ -36,7 +38,8 @@ repaymentPlanFullAdmissionController.get(CITIZEN_REPAYMENT_PLAN_FULL_URL, async 
 repaymentPlanFullAdmissionController.post(CITIZEN_REPAYMENT_PLAN_FULL_URL,
   async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-      const savedValues = await getRepaymentPlanForm(req.params.id);
+      const claim = await getCaseDataFromStore(req.params.id);
+      const savedValues = await getRepaymentPlanForm(claim);
       const form: RepaymentPlanForm = new RepaymentPlanForm(savedValues.totalClaimAmount, req.body.paymentAmount, req.body.repaymentFrequency, req.body.year, req.body.month, req.body.day);
       await validateForm(form);
       if (form.hasErrors()) {
--- src/main/routes/features/response/admission/partialAdmission/repaymentPlan/repaymentPlanController.ts
+++ src/main/routes/features/response/admission/partialAdmission/repaymentPlan/repaymentPlanController.ts
@@ -9,22 +9,27 @@ import {CITIZEN_REPAYMENT_PLAN_PARTIAL_URL, CLAIM_TASK_LIST_URL} from '../../../
 import {validateForm} from '../../../../../../common/form/validators/formValidator';
 import {getFirstPaymentExampleDate} from '../../fullAdmission/repaymentPlan/repaymentPlanController';
 import {ResponseType} from '../../../../../../common/form/models/responseType';
+import {getCaseDataFromStore} from '../../../../../../modules/draft-store/draftStoreService';
 
 const repaymentPlanViewPath = 'features/response/repaymentPlan/repaymentPlan';
 const repaymentPlanPartAdmissionController = express.Router();
+let amount: number;
 
-function renderView(form: RepaymentPlanForm, res: express.Response): void {
+function renderView(form: RepaymentPlanForm, res: express.Response, amount: number): void {
   res.render(repaymentPlanViewPath, {
     form,
     paymentExampleDate: getFirstPaymentExampleDate(),
+    amount,
     admission: ResponseType.PART_ADMISSION,
   });
 }
 
 repaymentPlanPartAdmissionController.get(CITIZEN_REPAYMENT_PLAN_PARTIAL_URL, async (req, res, next: express.NextFunction) => {
   try {
-    const form = await getRepaymentPlanForm(req.params.id);
-    renderView(form, res);
+    const claim = await getCaseDataFromStore(req.params.id);
+    amount = claim.partialAdmissionPaymentAmount();
+    const form = await getRepaymentPlanForm(claim);
+    renderView(form, res, amount);
   } catch (error) {
     next(error);
   }
@@ -33,11 +38,12 @@ repaymentPlanPartAdmissionController.get(CITIZEN_REPAYMENT_PLAN_PARTIAL_URL, asy
 repaymentPlanPartAdmissionController.post(CITIZEN_REPAYMENT_PLAN_PARTIAL_URL,
   async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-      const repaymentPlan = await getRepaymentPlanForm(req.params.id);
+      const claim = await getCaseDataFromStore(req.params.id);
+      const repaymentPlan = await getRepaymentPlanForm(claim);
       const repaymentPlanForm: RepaymentPlanForm = new RepaymentPlanForm(repaymentPlan.totalClaimAmount, req.body.paymentAmount, req.body.repaymentFrequency, req.body.year, req.body.month, req.body.day);
       await validateForm(repaymentPlanForm);
       if (repaymentPlanForm.hasErrors()) {
-        renderView(repaymentPlanForm, res);
+        renderView(repaymentPlanForm, res, amount);
       } else {
         await saveRepaymentPlanData(req.params.id, repaymentPlanForm);
         res.redirect(constructResponseUrlWithIdParams(req.params.id, CLAIM_TASK_LIST_URL));
