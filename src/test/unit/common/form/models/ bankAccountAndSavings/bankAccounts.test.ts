0447db97c2dd5215c9008042dc52149d94cf9c25
commitBlob <maro.radovic12@gmail.com>
2022-07-22 08:49:14 +0100

    fix(CIV-3417): add view tests

--- src/test/unit/common/form/models/ bankAccountAndSavings/bankAccounts.test.ts
+++ src/test/unit/common/form/models/ bankAccountAndSavings/bankAccounts.test.ts
@@ -1,9 +1,11 @@
 import {BankAccounts} from '../../../../../../main/common/form/models/bankAndSavings/bankAccounts';
 import {BankAccount} from '../../../../../../main/common/form/models/bankAndSavings/bankAccount';
 import {Validator} from 'class-validator';
+import {GenericForm} from '../../../../../../main/common/form/models/genericForm';
 
 describe('Bank accounts validation', ()=>{
   const validator = new Validator();
+
   it('should not allow empty value when one field is populated', async () =>{
     //Given
     const account = new BankAccount(undefined, 'true', undefined);
@@ -14,6 +16,7 @@ describe('Bank accounts validation', ()=>{
     expect(result.length).toBe(1);
     expect(result[0].children?.length).toBe(1);
   });
+
   it('should not allow empty value when two fields are populated', async () =>{
     //Given
     const account = new BankAccount(undefined, 'true', '345');
@@ -24,6 +27,7 @@ describe('Bank accounts validation', ()=>{
     expect(result.length).toBe(1);
     expect(result[0].children?.length).toBe(1);
   });
+
   it('should not have errors when all fields are populated', async () =>{
     //Given
     const account = new BankAccount('OTHER', 'true', '345');
@@ -33,16 +37,18 @@ describe('Bank accounts validation', ()=>{
     //Then
     expect(result.length).toBe(0);
   });
+
   it('should return true for has errors when there is a validation error in accounts array element', async ()=>{
     //Given
     const account = new BankAccount(undefined, 'true', undefined);
-    const accounts = new BankAccounts([account, new BankAccount()]);
+    const accounts = new GenericForm(new BankAccounts([account, new BankAccount('', '', '')]));
     //When
     const result = await validator.validate(accounts);
     accounts.errors = result;
     //Then
     expect(accounts.hasErrors()).toBeTruthy();
   });
+
   it('should filter out all empty accounts', ()=>{
     //Given
     const account = new BankAccount('OTHER', 'true', '345');
