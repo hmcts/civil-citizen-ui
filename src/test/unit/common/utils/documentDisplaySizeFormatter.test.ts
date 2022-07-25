39937c82f7733aea8344794f2151f589da9ab542
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-07-25 12:01:26 +0100

    unit test for utility

--- src/test/unit/common/utils/documentDisplaySizeFormatter.test.ts
+++ src/test/unit/common/utils/documentDisplaySizeFormatter.test.ts
@@ -11,4 +11,22 @@ describe('display document in KB', ()=>{
     const expectedResult = '1.205 KB';
     expect(sizeInKB).toEqual(expectedResult);
   });
+  it('should display document in KB successfully when size is greater than 0 and no decimal is specified', ()=>{
+    //Given
+    const documentSizeInBytes = 1024;
+    //When
+    const sizeInKB = displayDocumentSizeInKB(documentSizeInBytes);
+    //Then
+    const expectedResult = '1 KB';
+    expect(sizeInKB).toEqual(expectedResult);
+  });
+  it('should display 0 KB when size is 0', ()=>{
+    //Given
+    const documentSizeInBytes = 0;
+    //When
+    const sizeInKB = displayDocumentSizeInKB(documentSizeInBytes);
+    //Then
+    const expectedResult = '0 KB';
+    expect(sizeInKB).toEqual(expectedResult);
+  });
 });
