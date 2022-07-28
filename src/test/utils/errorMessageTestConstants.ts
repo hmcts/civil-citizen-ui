893fd87f42d5cab1812e269beb3bb8549fd75442
commitBlob <maro.radovic12@gmail.com>
2022-07-28 11:32:00 +0100

    feat(CIV-2900): citizen phone number error messages translations

--- src/test/utils/errorMessageTestConstants.ts
+++ src/test/utils/errorMessageTestConstants.ts
@@ -28,10 +28,8 @@ export const enum TestMessages {
   GAS_AMOUNT_ERROR = 'Enter how much you pay for Gas',
   GAS_SCHEDULE_ERROR = 'Select how often you pay for Gas',
   WATER_AMOUNT_ERROR = 'Enter how much you pay for Water',
-  WATER_SCHEDULE_ERROR = 'Select how often you pay for Water',
   GAS_CORRECT_AMOUNT_ERROR = 'Enter a valid Gas amount, maximum two decimal places',
   CLAIM_CARER = 'Do you claim Carer’s Allowance or Carer’s Credit?',
-  EXPLANATION_TITLE = 'Briefly explain why you can\'t pay immediately',
   EXPLANATION_ERROR = 'Enter an explanation of why you can’t pay now',
   VALID_TWO_DECIMAL_NUMBER = 'Enter a valid amount, maximum two decimal places',
   ENTER_VALID_AMOUNT = 'Enter a valid amount',
@@ -58,4 +56,5 @@ export const enum TestMessages {
   VALID_MONTH = 'Enter a valid month',
   VALID_YEAR = 'Enter a valid year',
   VALID_FOUR_DIGIT_YEAR = 'Enter a 4 digit year',
+  VALID_PHONE_NUMBER = 'Enter a phone number in the correct format'
 }
