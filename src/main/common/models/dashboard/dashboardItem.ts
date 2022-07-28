b34a4013983aab8109b847ff4a3ca1a616469fcd
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-07-28 10:47:43 +0100

    add ocmc link to ocmc claims

--- src/main/common/models/dashboard/dashboardItem.ts
+++ src/main/common/models/dashboard/dashboardItem.ts
@@ -1,3 +1,7 @@
+import config from 'config';
+
+const ocmcBaseUrl = config.get<string>('services.cmc.url');
+
 export class DashboardItem {
   claimId: string;
   claimNumber: string;
@@ -7,17 +11,27 @@ export class DashboardItem {
   responseDeadline?: Date;
   ocmc?: boolean;
   draft?: boolean;
+  url: string;
 
   getHref (){
-    return this.ocmc? '#' : `/dashboard/${this.claimId}/defendant`;
+    return this.ocmc? `${ocmcBaseUrl}${this.url.replace(':claimId', this.claimId)}` : this.url;
   }
 }
 
 export class DashboardClaimantItem extends DashboardItem {
+  constructor() {
+    super();
+    this.url = '/dashboard/:claimId/claimant';
+  }
   nextSteps: string;// TODO: this is only a placeholder. To be revisited in a separate story
   actions: string;// TODO: this is only a placeholder. To be revisited in a separate story
+
 }
 
 export class DashboardDefendantItem extends DashboardItem {
   status: string;// TODO: this is only a placeholder. To be revisited in a separate story
+  constructor() {
+    super();
+    this.url = '/dashboard/:claimId/defendant';
+  }
 }
