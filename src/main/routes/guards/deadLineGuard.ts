2765d13dc7f9ce4e39893c0e9053d65db6ac3d93
Felipe <felipe@LAPTOP3721.V1.com>
2022-07-29 14:28:01 +0200

    Fix: add try catch to guard

--- src/main/routes/guards/deadLineGuard.ts
+++ src/main/routes/guards/deadLineGuard.ts
@@ -4,10 +4,16 @@ import {getCaseDataFromStore} from '../../modules/draft-store/draftStoreService'
 import {ResponseOptions} from '../../common/form/models/responseDeadline';
 
 export const deadLineGuard = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const caseData: Claim = await getCaseDataFromStore(req.session.claimId);
-  if (caseData.responseDeadline?.option === ResponseOptions.ALREADY_AGREED && caseData.responseDeadline?.agreedResponseDeadline) {
-    res.status(401).json("Unauthorized");
-  } else {
-    next();
+  
+  try {
+    const caseData: Claim = await getCaseDataFromStore(req.session.claimId);
+    if (caseData.responseDeadline?.option === ResponseOptions.ALREADY_AGREED && caseData.responseDeadline?.agreedResponseDeadline) {
+      res.status(401).json("Unauthorized");
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
   }
+  
 };
