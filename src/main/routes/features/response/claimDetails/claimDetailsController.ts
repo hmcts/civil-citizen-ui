9761da4e4fcf33b46bac4f70de0a7ff44d3c9b16
sampankumar <sampan.kumar@hmcts.net>
2022-08-02 16:03:19 +0100

    CIV-3720 - moving getting claim data into utility services so that it can be called from different places.

--- src/main/routes/features/response/claimDetails/claimDetailsController.ts
+++ src/main/routes/features/response/claimDetails/claimDetailsController.ts
@@ -1,39 +1,20 @@
 import * as express from 'express';
-import config from 'config';
 import {
   CLAIM_DETAILS_URL,
   CASE_TIMELINE_DOCUMENTS_URL,
   CASE_DOCUMENT_DOWNLOAD_URL,
 } from '../../../urls';
-import {CivilServiceClient} from '../../../../app/client/civilServiceClient';
 import {Claim} from '../../../../common/models/claim';
-import {AppRequest} from 'models/AppRequest';
-import {getCaseDataFromStore, saveDraftClaim} from '../../../../modules/draft-store/draftStoreService';
 import {getInterestDetails} from '../../../../common/utils/interestUtils';
 import {getTotalAmountWithInterestAndFees} from '../../../../modules/claimDetailsService';
 import {DocumentUri} from '../../../../common/models/document/documentType';
+import {getClaimById} from 'modules/utilityService';
 
 const claimDetailsController = express.Router();
-const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
-const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
 claimDetailsController.get(CLAIM_DETAILS_URL, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
-    let claim: Claim = await getCaseDataFromStore((req.params.id));
-    if (claim.isEmpty()) {
-      claim = await civilServiceClient.retrieveClaimDetails(req.params.id, <AppRequest>req);
-      if (claim) {
-        await saveDraftClaim(req.params.id, claim);
-      } else {
-        //Temporarily return a mock claim
-        claim = new Claim();
-        claim.legacyCaseReference = 'testCaseReference';
-        claim.totalClaimAmount = 200;
-        claim.detailsOfClaim = 'detailsOfClaimTest';
-        claim.totalInterest = 15;
-        claim.claimFee = {calculatedAmountInPence: '3500'};
-      }
-    }
+    const claim: Claim = await getClaimById(req.params.id, req);
     const interestData = getInterestDetails(claim);
     const totalAmount = getTotalAmountWithInterestAndFees(claim);
     const timelinePdfUrl = claim.extractDocumentId() && CASE_TIMELINE_DOCUMENTS_URL.replace(':id', req.params.id);
