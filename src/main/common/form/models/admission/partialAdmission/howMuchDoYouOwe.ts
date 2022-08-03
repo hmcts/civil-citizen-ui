d9f648cbd2415b99c1ff371393507257b173fecb
commitBlob <maro.radovic12@gmail.com>
2022-08-03 11:26:51 +0100

    feat(CIV-3467): welsh error messages for how much do you owe

--- src/main/common/form/models/admission/partialAdmission/howMuchDoYouOwe.ts
+++ src/main/common/form/models/admission/partialAdmission/howMuchDoYouOwe.ts
@@ -1,18 +1,12 @@
 import {IsDefined, IsNumber, Min, Validate} from 'class-validator';
-import {
-  AMOUNT_LESS_THAN_CLAIMED,
-  CLAIM_VALID_AMOUNT,
-  VALID_TWO_DECIMAL_NUMBER,
-} from '../../../validationErrors/errorMessageConstants';
 import {MIN_AMOUNT_VALUE} from '../../../validators/validationConstraints';
 import {EqualToOrLessThanPropertyValueValidator} from '../../../validators/equalToOrLessThanPropertyValueValidator';
 
 export class HowMuchDoYouOwe {
-
-  @IsDefined({message: CLAIM_VALID_AMOUNT})
-  @Min(MIN_AMOUNT_VALUE, {message: CLAIM_VALID_AMOUNT})
-  @IsNumber({allowNaN: false, maxDecimalPlaces: 2}, {message: VALID_TWO_DECIMAL_NUMBER})
-  @Validate(EqualToOrLessThanPropertyValueValidator, ['totalAmount', 'strictComparison'], {message: AMOUNT_LESS_THAN_CLAIMED})
+  @IsDefined({message: 'ERRORS.CLAIM_VALID_AMOUNT'})
+  @Min(MIN_AMOUNT_VALUE, {message: 'ERRORS.CLAIM_VALID_AMOUNT'})
+  @IsNumber({allowNaN: false, maxDecimalPlaces: 2}, {message: 'ERRORS.VALID_TWO_DECIMAL_NUMBER'})
+  @Validate(EqualToOrLessThanPropertyValueValidator, ['totalAmount', 'strictComparison'], {message: 'ERRORS.AMOUNT_LESS_THAN_CLAIMED'})
     amount?: number;
 
   totalAmount?: number;
--- src/test/unit/common/form/models/admission/partialAdmission/howMuchDoYouOwe.test.ts
+++ src/test/unit/common/form/models/admission/partialAdmission/howMuchDoYouOwe.test.ts
@@ -2,7 +2,6 @@ import {
   HowMuchDoYouOwe,
 } from '../../../../../../../main/common/form/models/admission/partialAdmission/howMuchDoYouOwe';
 import {Validator} from 'class-validator';
-import {TestMessages} from '../../../../../../utils/errorMessageTestConstants';
 
 const validator = new Validator();
 describe('Partial Admit - How much money do you admit you owe? model', () => {
@@ -15,7 +14,7 @@ describe('Partial Admit - How much money do you admit you owe? model', () => {
       const errors = validator.validateSync(form);
       //Then
       expect(errors?.length).toBe(1);
-      expect(errors[0].constraints?.isDefined).toBe(TestMessages.ENTER_VALID_AMOUNT);
+      expect(errors[0].constraints?.isDefined).toBe('ERRORS.CLAIM_VALID_AMOUNT');
     });
     it('should return errors when input 0 is provided', () => {
       //Given
@@ -24,7 +23,7 @@ describe('Partial Admit - How much money do you admit you owe? model', () => {
       const errors = validator.validateSync(form);
       //Then
       expect(errors?.length).toBe(1);
-      expect(errors[0].constraints?.min).toBe(TestMessages.ENTER_VALID_AMOUNT);
+      expect(errors[0].constraints?.min).toBe('ERRORS.CLAIM_VALID_AMOUNT');
     });
     it('should return errors when more than 2 decimals provided', () => {
       //Given
@@ -33,7 +32,7 @@ describe('Partial Admit - How much money do you admit you owe? model', () => {
       const errors = validator.validateSync(form);
       //Then
       expect(errors?.length).toBe(1);
-      expect(errors[0].constraints?.isNumber ).toBe(TestMessages.VALID_TWO_DECIMAL_NUMBER);
+      expect(errors[0].constraints?.isNumber).toBe('ERRORS.VALID_TWO_DECIMAL_NUMBER');
     });
     it('should return errors when negative amount is provided', () => {
       //Given
@@ -42,7 +41,7 @@ describe('Partial Admit - How much money do you admit you owe? model', () => {
       const errors = validator.validateSync(form);
       //Then
       expect(errors?.length).toBe(1);
-      expect(errors[0].constraints?.min).toBe(TestMessages.ENTER_VALID_AMOUNT);
+      expect(errors[0].constraints?.min).toBe('ERRORS.CLAIM_VALID_AMOUNT');
     });
     it('should return errors when provided amount is bigger than Claim amount', () => {
       //Given
@@ -51,7 +50,7 @@ describe('Partial Admit - How much money do you admit you owe? model', () => {
       const errors = validator.validateSync(form);
       //Then
       expect(errors?.length).toBe(1);
-      expect(errors[0].constraints?.equalOrLessToPropertyValue).toBe(TestMessages.AMOUNT_LESS_THAN_CLAIMED);
+      expect(errors[0].constraints?.equalOrLessToPropertyValue).toBe('ERRORS.AMOUNT_LESS_THAN_CLAIMED');
     });
     it('should return errors when provided amount is equal to Claim amount', () => {
       //Given
@@ -60,7 +59,7 @@ describe('Partial Admit - How much money do you admit you owe? model', () => {
       const errors = validator.validateSync(form);
       //Then
       expect(errors?.length).toBe(1);
-      expect(errors[0].constraints?.equalOrLessToPropertyValue).toBe(TestMessages.AMOUNT_LESS_THAN_CLAIMED);
+      expect(errors[0].constraints?.equalOrLessToPropertyValue).toBe('ERRORS.AMOUNT_LESS_THAN_CLAIMED');
     });
   });
 });
