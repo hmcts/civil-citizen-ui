07bb552f52587070931c436a5faa7718fbc07bef
Kadir <kegilmez@gmail.com>
2022-08-03 12:36:37 +0100

    included error keys

--- src/main/common/form/models/admission/fullAdmission/paymentOption/paymentDate.ts
+++ src/main/common/form/models/admission/fullAdmission/paymentOption/paymentDate.ts
@@ -2,10 +2,7 @@ import {IsDate, Max, Min, Validate, ValidateIf} from 'class-validator';
 import {
   VALID_DATE,
   VALID_DATE_NOT_IN_PAST,
-  VALID_DAY,
   VALID_FOUR_DIGIT_YEAR,
-  VALID_MONTH,
-  VALID_YEAR,
 } from '../../../../validationErrors/errorMessageConstants';
 import {OptionalDateFourDigitValidator} from '../../../../validators/optionalDateFourDigitValidator';
 import {OptionalDateNotInPastValidator} from '../../../../validators/optionalDateNotInPastValidator';
@@ -19,16 +16,16 @@ export class PaymentDate {
   @Validate(OptionalDateNotInPastValidator, {message: VALID_DATE_NOT_IN_PAST})
     date?: Date;
 
-  @Min(1,{message:VALID_DAY })
-  @Max(31,{message:VALID_DAY })
+  @Min(1, {message:'ERRORS.VALID_DAY' })
+  @Max(31, {message: 'ERRORS.VALID_DAY'})
     day: number;
 
-  @Min(1,{message:VALID_MONTH })
-  @Max(12,{message:VALID_MONTH })
+  @Min(1, {message: 'ERRORS.VALID_MONTH' })
+  @Max(12, {message: 'ERRORS.VALID_MONTH' })
     month: number;
 
   @Validate(OptionalDateFourDigitValidator, {message: VALID_FOUR_DIGIT_YEAR})
-  @Max(9999,{message:VALID_YEAR })
+  @Max(9999, {message: 'ERRORS.VALID_YEAR' })
     year: number;
 
   constructor(year?: string, month?: string, day?: string) {
--- src/main/common/form/models/statementOfMeans/disability.ts
+++ src/main/common/form/models/statementOfMeans/disability.ts
@@ -1,9 +1,7 @@
 import {IsDefined, ValidationError} from 'class-validator';
 import {Form} from '../../../form/models/form';
-import {VALID_YES_NO_OPTION} from '../../../form/validationErrors/errorMessageConstants';
-
 export class Disability extends Form {
-  @IsDefined({message: VALID_YES_NO_OPTION})
+  @IsDefined({message: 'ERRORS.VALID_YES_NO_OPTION'})
     option?: string;
 
   constructor(disablity?: string, errors?: ValidationError[]) {
