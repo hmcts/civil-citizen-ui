b4e0faaa3c1329a38583d963f4ea0534032a3a45
Felipe <felipe@LAPTOP3721.V1.com>
2022-08-04 16:46:41 +0200

    Update: test newResponseDeadline

--- src/test/unit/routes/features/response/responseDeadline/newResponseDeadlineController.test.ts
+++ src/test/unit/routes/features/response/responseDeadline/newResponseDeadlineController.test.ts
@@ -1,16 +1,16 @@
-import { app } from '../../../../../../main/app';
+import {app} from '../../../../../../main/app';
 import request from 'supertest';
 import config from 'config';
 import nock from 'nock';
-import { CIVIL_SERVICE_CALCULATE_DEADLINE } from '../../../../../../main/app/client/civilServiceUrls';
+import {CIVIL_SERVICE_CALCULATE_DEADLINE} from '../../../../../../main/app/client/civilServiceUrls';
 import * as draftStoreService from '../../../../../../main/modules/draft-store/draftStoreService';
-import { Claim } from '../../../../../../main/common/models/claim';
+import {Claim} from '../../../../../../main/common/models/claim';
 import {
   CLAIM_TASK_LIST_URL,
   NEW_RESPONSE_DEADLINE_URL,
 } from '../../../../../../main/routes/urls';
-import { CounterpartyType } from '../../../../../../main/common/models/counterpartyType';
-import { TestMessages } from '../../../../../utils/errorMessageTestConstants';
+import {CounterpartyType} from '../../../../../../main/common/models/counterpartyType';
+import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
 
 jest.mock('../../../../../../main/modules/oidc');
 jest.mock('../../../../../../main/modules/draft-store/draftStoreService');
@@ -41,7 +41,6 @@ describe('Response - New response deadline', () => {
         agreedResponseDeadline: extendedDate,
       };
       mockGetCaseDataFromStore.mockImplementation(async () => claim);
-
       await request(app).get(NEW_RESPONSE_DEADLINE_URL)
         .expect((res) => {
           expect(res.status).toBe(200);
@@ -74,25 +73,12 @@ describe('Response - New response deadline', () => {
     });
   });
   describe('on POST', () => {
-    // it('should return 500 code when there is an error', async () => {
-    //   app.locals.draftStoreClient = mockCivilClaim;
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send()
-    //     .expect((res) => {
-    //       expect(res.status).toBe(500);
-    //       expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
-    //     });
-    // });
-    it('should redirect to task list if cookies doesnt exist', async () => {
+    it('should redirect to task list if cookies has newDeadlineDate and NO validation errors', async () => {
       const claim = new Claim();
       claim.responseDeadline = {
         agreedResponseDeadline: extendedDate,
       };
       mockGetCaseDataFromStore.mockImplementation(async () => claim);
-
-      // const newDeadlineDate = {date: '2022-08-21T22:00:00.000Z', year: 2022, month: 8, day: 22, originalResponseDeadline: '2022-08-20T15:59:59'}
-      // app.locals.draftStoreClient = mockCivilClaim;
       await request(app)
         .post(NEW_RESPONSE_DEADLINE_URL)
         .set('Cookie', [`newDeadlineDate=j%3A%7B%22date%22%3A%222022-08-21T22%3A00%3A00.000Z%22%2C%22year%22%3A2022%2C%22month%22%3A8%2C%22day%22%3A22%2C%22originalResponseDeadline%22%3A%222022-08-20T15%3A59%3A59%22%7D`])
@@ -101,86 +87,17 @@ describe('Response - New response deadline', () => {
           expect(res.header.location).toBe(CLAIM_TASK_LIST_URL);
         });
     });
-
-    // it('should return 500 code when there is an error', async () => {
-    //   app.locals.draftStoreClient = mockRedisFailure;
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send()
-    //     .expect((res) => {
-    //       expect(res.status).toBe(500);
-    //       expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
-    //     });
-    // });
-
-    // it('should return error on agreed date in the past', async () => {
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send('year=1999')
-    //     .send('month=1')
-    //     .send('day=1')
-    //     .expect((res) => {
-    //       expect(res.status).toBe(200);
-    //       expect(res.header.location).toBe(CLAIM_TASK_LIST_URL);
-    //     });
-    // });
-
-    // it('should return error on agreed date in the past', async () => {
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send('year=2022')
-    //     .send('month=05')
-    //     .send('day=10')
-    //     .expect((res) => {
-    //       expect(res.status).toBe(200);
-    //       expect(res.header.location).toBe(CLAIM_TASK_LIST_URL);
-    //     });
-    // });
-    // it('should return error on incorrect input', async () => {
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send('year=199')
-    //     .send('month=1')
-    //     .send('day=1')
-    //     .expect((res) => {
-    //       expect(res.status).toBe(200);
-    //       expect(res.header.location).toBe(CLAIM_TASK_LIST_URL);
-    //     });
-    // });
-    // it('should return error on agreed response date is bigger 28 days', async () => {
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send('year=2050')
-    //     .send('month=6')
-    //     .send('day=13')
-    //     .expect((res) => {
-    //       expect(res.status).toBe(200);
-    //       expect(res.header.location).toBe(CLAIM_TASK_LIST_URL);
-    //     });
-    // });
-
-    // it('should accept the 28th day after the original response deadline as input and redirect to next page', async () => {
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send('year=2025')
-    //     .send('month=6')
-    //     .send('day=12')
-    //     .expect((res) => {
-    //       expect(res.status).toBe(302);
-    //       expect(res.header.location).toBe(CLAIM_TASK_LIST_URL);
-    //     });
-    // });
-
-    // it('should accept the input date when it is less then 28 after original response deadline and redirect to next page', async () => {
-    //   await request(app)
-    //     .post(NEW_RESPONSE_DEADLINE_URL)
-    //     .send('year=2025')
-    //     .send('month=6')
-    //     .send('day=11')
-    //     .expect((res) => {
-    //       expect(res.status).toBe(302);
-    //       expect(res.header.location).toBe(CLAIM_TASK_LIST_URL);
-    //     });
-    // });
+    it('should return 500 code when there is an error', async () => {
+      mockGetCaseDataFromStore.mockImplementation(async () => {
+        throw new Error(TestMessages.REDIS_FAILURE);
+      });
+      await request(app)
+        .post(NEW_RESPONSE_DEADLINE_URL)
+        .send()
+        .expect((res) => {
+          expect(res.status).toBe(500);
+          expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+        });
+    });
   });
 });
