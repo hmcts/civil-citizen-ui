3b21c79deb2cfcc9439ef96027e71ca08d52f1be
commitBlob <maro.radovic12@gmail.com>
2022-08-09 10:20:57 +0100

    feat(CIV-3826): roll back middleware changes

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -50,14 +50,13 @@ export class OidcMiddleware {
     });
 
     app.use((req: AppRequest, res: Response, next: NextFunction) => {
-      return next();
-      // if (req.session.user) {
-      //   if (req.session?.user?.roles?.includes(citizenRole)) {
-      //     return next();
-      //   }
-      //   return res.redirect(DASHBOARD_URL);
-      // }
-      // res.redirect(SIGN_IN_URL);
+      if (req.session.user) {
+        if (req.session?.user?.roles?.includes(citizenRole)) {
+          return next();
+        }
+        return res.redirect(DASHBOARD_URL);
+      }
+      res.redirect(SIGN_IN_URL);
     });
   }
 }
