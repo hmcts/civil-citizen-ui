4b86fec885c40071007f895cd0732b993a5a62f9
RosDiaf <ros_diaf@hotmail.com>
2022-08-10 16:51:16 +0100

    length of repayment for Welsh translations

--- src/main/assets/js/calculate-length-repayment.js
+++ src/main/assets/js/calculate-length-repayment.js
@@ -22,8 +22,27 @@
   const repaymentAmount = document.querySelector('.repayment-amount');
   const repaymentInstalments = document.querySelector('.repayment-instalments');
   const repaymentFrequency = document.querySelector('.repayment-frequency');
-  const repaymentLength = document.querySelector('.repayment-length');
   let frequencyVal = null;
+
+  const instalmentsContainer = document.querySelector('#numberOfInstalments');
+  const scheduleContainer = document.querySelector('.schedule');
+  const weekContainer = document.querySelector('#week');
+  const weeksContainer = document.querySelector('#weeks');
+  const twoWeeksContainer = document.querySelector('#two-weeks');
+  const monthContainer = document.querySelector('#month');
+  const monthsContainer = document.querySelector('#months');
+  const twoMonthsContainer = document.querySelector('#two-months');
+  const CLASS_HIDE = 'hide';
+
+  const hideAll = () => {
+    if (scheduleContainer.children.length > 0) {
+      for (let i = 0; i < scheduleContainer.children.length; i++) {
+        scheduleContainer.children[i].classList.add(CLASS_HIDE);
+      }
+    }
+  };
+
+  hideAll();
 
   if (repaymentFrequency) {
     repaymentInstalments.addEventListener('keyup', () => {
@@ -38,29 +57,34 @@
     const getRepaymentSchedule = (val) => {
       let numberOfInstalments = parseFloat(repaymentInstalments.value) > 0 ? Math.ceil(parseFloat(repaymentAmount.value) / parseFloat(repaymentInstalments.value)) : undefined;
 
+      const getNumberOfInstalments = () => instalmentsContainer.innerHTML = numberOfInstalments === 1 || numberOfInstalments > 2 ? numberOfInstalments : '';
+
+      const toggleSchedule = (containerOne, containerTwo, containerThree) => {
+        numberOfInstalments === 1 ? containerOne.classList.remove(CLASS_HIDE) : containerOne.classList.add(CLASS_HIDE);
+        numberOfInstalments === 2 ? containerTwo.classList.remove(CLASS_HIDE) : containerTwo.classList.add(CLASS_HIDE);
+        numberOfInstalments > 2 ? containerThree.classList.remove(CLASS_HIDE) : containerThree.classList.add(CLASS_HIDE);
+      };
+
       switch (val) {
         case 'WEEK':
-          repaymentLength.innerHTML = numberOfInstalments ? numberOfInstalments + pluralize(numberOfInstalments, 'week') : '-';
+          hideAll();
+          getNumberOfInstalments();
+          toggleSchedule(weekContainer, twoWeeksContainer, weeksContainer);
           break;
         case 'TWO_WEEKS':
+          hideAll();
           numberOfInstalments = numberOfInstalments * 2;
-          repaymentLength.innerHTML = numberOfInstalments ? numberOfInstalments + pluralize(numberOfInstalments, 'week') : '-';
+          getNumberOfInstalments();
+          toggleSchedule(weekContainer, twoWeeksContainer, weeksContainer);
           break;
         case 'MONTH':
-          repaymentLength.innerHTML = numberOfInstalments ? numberOfInstalments + pluralize(numberOfInstalments, 'month') : '-';
+          hideAll();
+          getNumberOfInstalments();
+          toggleSchedule(monthContainer, twoMonthsContainer, monthsContainer);
           break;
         default:
           return undefined;
       }
     };
-
-    const pluralize = (num, word) => {
-      const plural = num < 2 ? '' : 's';
-      return ` ${word}${plural}`;
-    };
-
-    if (repaymentFrequency) {
-      getRepaymentSchedule(repaymentFrequency);
-    }
   }
 })();
