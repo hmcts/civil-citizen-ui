2f484383e4bf1fcb4fd2fb5368968068479a8b97
commitBlob <maro.radovic12@gmail.com>
2022-08-10 14:17:04 +0100

    feat(CIV-3698): refactor between 16 and 19 form

--- src/main/common/form/models/statementOfMeans/dependants/betweenSixteenAndNineteenDependants.ts
+++ src/main/common/form/models/statementOfMeans/dependants/betweenSixteenAndNineteenDependants.ts
@@ -1,9 +1,8 @@
-import {Form} from '../../form';
 import {IsDefined, IsNumber, Min, Validate} from 'class-validator';
 import {VALID_INTEGER, VALID_POSITIVE_NUMBER} from '../../../validationErrors/errorMessageConstants';
 import {EqualToOrLessThanPropertyValueValidator} from '../../../validators/equalToOrLessThanPropertyValueValidator';
 
-export class BetweenSixteenAndNineteenDependants extends Form {
+export class BetweenSixteenAndNineteenDependants {
   @IsDefined({message: VALID_INTEGER})
   @IsNumber({allowNaN: false, maxDecimalPlaces: 0}, {message: VALID_INTEGER})
   @Validate(EqualToOrLessThanPropertyValueValidator, ['maxValue'])
@@ -12,7 +11,6 @@ export class BetweenSixteenAndNineteenDependants extends Form {
   maxValue: number;
 
   constructor(value?: number, maxValue?: number) {
-    super();
     this.value = value;
     this.maxValue = maxValue;
   }
