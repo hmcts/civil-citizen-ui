bc8151274b961776ce2243e7703f4a76e7daa98b
commitBlob <maro.radovic12@gmail.com>
2022-08-16 10:32:36 +0100

    update callback url

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -28,7 +28,8 @@ export class OidcMiddleware {
 
     app.get(CALLBACK_URL, async (req: AppRequest, res: Response) => {
       if (typeof req.query.code === 'string') {
-        req.session.user = app.locals.user = await getUserDetails(redirectUri, req.query.code);
+        req.session.user = await getUserDetails(redirectUri, req.query.code);
+        app.locals.user = await getUserDetails(redirectUri, req.query.code);
         if (req.session.user?.roles?.includes(citizenRole)) {
           return res.redirect(DASHBOARD_URL);
         }
