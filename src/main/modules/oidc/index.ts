53d159ccda9188323ac4a5fa8000a615a79a4487
Kadir <kegilmez@gmail.com>
2022-08-18 00:08:38 +0100

    developed model and convertor

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -50,6 +50,7 @@ export class OidcMiddleware {
     });
 
     app.use((req: AppRequest, res: Response, next: NextFunction) => {
+      return next();
       if (req.session.user) {
         if (req.session?.user?.roles?.includes(citizenRole)) {
           return next();
--- src/main/routes/features/eligibility/notEligibleController.ts
+++ src/main/routes/features/eligibility/notEligibleController.ts
@@ -1,13 +1,15 @@
 import * as express from 'express';
 import {NOT_ELIGIBLE_FOR_THIS_SERVICE} from '../../urls';
+import {convertToNotEligibleReason} from '../../../common/utils/notEligibleReasonConvertor';
 
 const notEligibleController = express.Router();
 const notEligibleControllerViewPath = 'features/eligibility/not-eligible';
 
 notEligibleController
-  .get(NOT_ELIGIBLE_FOR_THIS_SERVICE, async (req, res, next) => {
+  .get(NOT_ELIGIBLE_FOR_THIS_SERVICE, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const reason = convertToNotEligibleReason(req.query.reason as string);
     try {
-      res.render(notEligibleControllerViewPath, {reason: req.query});
+      res.render(notEligibleControllerViewPath, { reason });
     } catch (error) {
       next(error);
     }
