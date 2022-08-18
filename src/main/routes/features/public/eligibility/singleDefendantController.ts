3eeabbf766c28df5b001435d88b605a98ffd5e35
commitBlob <maro.radovic12@gmail.com>
2022-08-18 14:59:19 +0100

    feat(CIV-3625): initialise single defendant eligibility page

---
+++ src/main/routes/features/public/eligibility/singleDefendantController.ts
@@ -0,0 +1,40 @@
+import * as express from 'express';
+import {
+  ELIGIBILITY_DEFENDANT_ADDRESS_URL,
+  ELIGIBILITY_SINGLE_DEFENDANT_URL,
+  NOT_ELIGIBLE_URL
+} from '../../../../routes/urls';
+import {GenericForm} from '../../../../common/form/models/genericForm';
+import {GenericYesNo} from '../../../../common/form/models/genericYesNo';
+import {YesNo} from '../../../../common/form/models/yesNo';
+
+const singleDefendantController = express.Router();
+const singleDefendantViewPath = 'features/public/eligibility/single-defendant';
+
+function renderView(genericYesNoForm: GenericForm<GenericYesNo>, res: express.Response): void {
+  const form = Object.assign(genericYesNoForm);
+  form.option = genericYesNoForm.model.option;
+  res.render(singleDefendantViewPath, {form});
+}
+
+singleDefendantController.get(ELIGIBILITY_SINGLE_DEFENDANT_URL, (req, res) => {
+  // get cookie value
+  const genericYesNoForm = new GenericForm(new GenericYesNo());
+  renderView(genericYesNoForm, res);
+});
+
+singleDefendantController.post(ELIGIBILITY_SINGLE_DEFENDANT_URL, (req, res) => {
+  const genericYesNoForm = new GenericForm(new GenericYesNo(req.body.option));
+  genericYesNoForm.validateSync();
+
+  if (genericYesNoForm.hasErrors()) {
+    renderView(genericYesNoForm, res);
+  } else {
+    // save cookieValue
+    genericYesNoForm.model.option === YesNo.YES
+      ? res.redirect(NOT_ELIGIBLE_URL + '?reason=multiple-defendants')
+      : res.redirect(ELIGIBILITY_DEFENDANT_ADDRESS_URL);
+  }
+});
+
+export default singleDefendantController;
