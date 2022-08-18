57a5f3f253b322954fad65f1b1f0599654bcfce2
Kadir <kegilmez@gmail.com>
2022-08-18 12:49:35 +0100

    updated the urls and view

--- src/main/routes/features/eligibility/notEligibleController.ts
+++ src/main/routes/features/eligibility/notEligibleController.ts
@@ -1,12 +1,12 @@
 import * as express from 'express';
-import {NOT_ELIGIBLE_FOR_THIS_SERVICE} from '../../urls';
+import {NOT_ELIGIBLE_FOR_THIS_SERVICE_URL} from '../../urls';
 import {convertToNotEligibleReason} from '../../../common/utils/notEligibleReasonConvertor';
 
 const notEligibleController = express.Router();
 const notEligibleControllerViewPath = 'features/eligibility/not-eligible';
 
 notEligibleController
-  .get(NOT_ELIGIBLE_FOR_THIS_SERVICE, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  .get(NOT_ELIGIBLE_FOR_THIS_SERVICE_URL, (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const reason = convertToNotEligibleReason(req.query.reason as string);
     try {
       res.render(notEligibleControllerViewPath, { reason });
