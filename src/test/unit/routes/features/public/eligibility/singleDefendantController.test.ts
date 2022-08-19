ace4fe8e1efbe453c96c48a27c605dbb49edef21
commitBlob <maro.radovic12@gmail.com>
2022-08-19 09:55:49 +0100

    feat(CIV-3625): WIP

---
+++ src/test/unit/routes/features/public/eligibility/singleDefendantController.test.ts
@@ -0,0 +1,19 @@
+import config from 'config';
+import nock from 'nock';
+import request from 'supertest';
+
+describe('Single Defendant Controller', () => {
+  // TODO: remove this once paths become publicly available as mocking the response token will not be needed
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+
+  beforeEach(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+  });
+
+  describe('on GET', () => {
+
+  });
+});
