d0320903e001767e4a74b5096e27aee07d96e852
OrtizP <pablo.gutierrez@version1.com>
2022-08-22 09:52:18 +0200

    No try catch cause there is not call to service

--- src/main/routes/features/eligibility/tryNewServiceController.ts
+++ src/main/routes/features/eligibility/tryNewServiceController.ts
@@ -4,20 +4,12 @@ import {ELIGIBILITY_URL, ELIGIBILITY_CLAIM_VALUE_URL} from '../../urls';
 const tryNewServiceController = express.Router();
 const tryNewServiceViewPath = 'features/eligibility/try-new-service';
 
-tryNewServiceController.get(ELIGIBILITY_URL, async (_req, res, next: express.NextFunction) => {
-  try {
-    res.render(tryNewServiceViewPath);
-  } catch (error) {
-    next(error);
-  }
+tryNewServiceController.get(ELIGIBILITY_URL, async (_req, res) => {
+  res.render(tryNewServiceViewPath);
 });
 
-tryNewServiceController.post(ELIGIBILITY_URL, async (req, res, next: express.NextFunction) => {
-  try {
-    res.redirect(ELIGIBILITY_CLAIM_VALUE_URL);
-  } catch (error) {
-    next(error);
-  }
+tryNewServiceController.post(ELIGIBILITY_URL, async (req, res) => {
+  res.redirect(ELIGIBILITY_CLAIM_VALUE_URL);
 });
 
 export default tryNewServiceController;
