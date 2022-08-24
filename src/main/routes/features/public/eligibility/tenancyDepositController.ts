69fd4c0cb451b3f71c6ca1ef728441c41c532431
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-08-24 11:00:02 +0100

    fix tests

--- src/main/routes/features/public/eligibility/tenancyDepositController.ts
+++ src/main/routes/features/public/eligibility/tenancyDepositController.ts
@@ -8,7 +8,7 @@ import {GenericForm} from '../../../../common/form/models/genericForm';
 import {GenericYesNo} from '../../../../common/form/models/genericYesNo';
 import {YesNo} from '../../../../common/form/models/yesNo';
 import {NotEligibleReason} from '../../../../common/form/models/eligibility/NotEligibleReason';
-import {constructUrlWithNotEligibleReson} from '../../../../common/utils/urlFormatter';
+import {constructUrlWithNotEligibleReason} from '../../../../common/utils/urlFormatter';
 
 const tenancyDepositController = express.Router();
 const tenancyDepositViewPath = 'features/public/eligibility/tenancy-deposit';
@@ -36,7 +36,7 @@ tenancyDepositController.post(ELIGIBILITY_TENANCY_DEPOSIT_URL, (req, res) => {
     cookie.tenancyDeposit = genericYesNoForm.model.option;
     res.cookie('eligibility', cookie);
     genericYesNoForm.model.option === YesNo.YES
-      ? res.redirect(constructUrlWithNotEligibleReson(NOT_ELIGIBLE_FOR_THIS_SERVICE_URL, NotEligibleReason.CLAIM_IS_FOR_TENANCY_DEPOSIT))
+      ? res.redirect(constructUrlWithNotEligibleReason(NOT_ELIGIBLE_FOR_THIS_SERVICE_URL, NotEligibleReason.CLAIM_IS_FOR_TENANCY_DEPOSIT))
       : res.redirect(ELIGIBILITY_GOVERNMENT_DEPARTMENT_URL);
   }
 });
