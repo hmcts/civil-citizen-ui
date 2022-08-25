3c8b4d73698f7ad2593d248dc75d944c9bc6ad79
Kadir <kegilmez@gmail.com>
2022-08-25 15:51:37 +0100

    updated the language toggle script

--- src/main/assets/js/language-toggle.js
+++ src/main/assets/js/language-toggle.js
@@ -14,12 +14,26 @@ const getCookie = (name) => {
 };
 
 document.addEventListener('DOMContentLoaded', function () {
+  debugger;
+  const currentQuery = window.location.search;
   const lang = getCookie('lang');
   if (lang === 'cy') {
+    const updatedQuery = currentQuery.includes('lang')
+      ? currentQuery.replace('lang=cy', 'lang=en')
+      : currentQuery
+      ? currentQuery + '&&lang=en'
+        : '?lang=en';
+    englishLanguage[0].href = updatedQuery;
     englishLanguage[0].className = 'govuk-!-font-size-19 govuk-link language';
     cymraegLanguage[0].className = 'govuk-!-display-none';
   } else {
+    const updatedQuery = currentQuery.includes('lang')
+      ? currentQuery.replace('lang=en", "lang=cy')
+      : currentQuery
+        ? currentQuery + '&&lang=cy'
+        : '?lang=cy';
     englishLanguage[0].className = 'govuk-!-display-none';
+    cymraegLanguage[0].href = updatedQuery;
     cymraegLanguage[0].className = 'govuk-!-font-size-19 govuk-link language';
   }
 });
