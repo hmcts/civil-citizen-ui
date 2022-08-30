4346a8229b43a6c158e32e41f7db8ed343da4d8f
hmcts-version1-domini <domini.mattos@version1.com>
2022-08-30 15:37:57 +0200

    Final commit before PR creation

--- src/main/routes/features/public/eligibility/applyForHelpWithFeesController.ts
+++ src/main/routes/features/public/eligibility/applyForHelpWithFeesController.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import {
   ELIGIBILITY_HELP_WITH_FEES,
+  ELIGIBILITY_HELP_WITH_FEES_REFERENCE,
 } from '../../../../routes/urls';
 
 const applyForHelpWithFeesController = express.Router();
@@ -8,6 +9,10 @@ const applyForHelpWithFeesControllerViewPath = 'features/public/eligibility/appl
 
 applyForHelpWithFeesController.get(ELIGIBILITY_HELP_WITH_FEES, (req, res) => {
   res.render(applyForHelpWithFeesControllerViewPath);
+});
+
+applyForHelpWithFeesController.post(ELIGIBILITY_HELP_WITH_FEES, (req, res) => {
+  res.redirect(ELIGIBILITY_HELP_WITH_FEES_REFERENCE);
 });
 
 export default applyForHelpWithFeesController;
