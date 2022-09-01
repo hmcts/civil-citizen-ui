4acede2bf63b118b82daac283431df1ec2b35cb3
lfazelyniaHMCTS <lawrence.fazelynia@HMCTS.NET>
2022-09-01 15:06:39 +0100

    CIV-3766 | add the screen and tests for You can use this service

---
+++ src/test/unit/views/features/public/eligibility/you-can-use-service.test.ts
@@ -0,0 +1,57 @@
+import config from 'config';
+import nock from 'nock';
+import {app} from '../../../../../../main/app';
+import request from 'supertest';
+import {ELIGIBLE_FOR_THIS_SERVICE_URL} from '../../../../../../main/routes/urls';
+import {t} from 'i18next';
+
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+const govukBodyClass = 'govuk-body';
+const pageTitle = 'PAGES.YOU_CAN_USE_SERVICE.PAGE_TITLE';
+
+jest.mock('../../../../../../main/modules/oidc');
+jest.mock('../../../../../../main/modules/draft-store');
+
+describe('You can use this service View', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+
+  describe('on GET', () => {
+    let htmlDocument: Document;
+    let paragraphs: HTMLCollection;
+
+    beforeEach(async () => {
+      nock(idamUrl)
+        .post('/o/token')
+        .reply(200, {id_token: citizenRoleToken});
+      const response = await request(app).get(ELIGIBLE_FOR_THIS_SERVICE_URL);
+      const dom = new JSDOM(response.text);
+      htmlDocument = dom.window.document;
+      paragraphs = htmlDocument.getElementsByClassName(govukBodyClass);
+    });
+
+    it('should have page title', () => {
+      expect(htmlDocument.title).toEqual(`Your money claims account - ${t(pageTitle)}`);
+    });
+
+    it('should display header', () => {
+      const header = htmlDocument.getElementsByClassName('govuk-heading-l');
+      expect(header[0].innerHTML).toContain(t('PAGES.YOU_CAN_USE_SERVICE.TITLE'));
+    });
+
+    it('should display paragraph', () => {
+      expect(paragraphs[0].innerHTML).toContain(t('PAGES.YOU_CAN_USE_SERVICE.BASED_ON_ANSWERS'));
+    });
+
+    it('should display Continue button', () => {
+      const button = htmlDocument.getElementsByClassName('govuk-button');
+      expect(button[0].innerHTML).toContain(t('COMMON.BUTTONS.CONTINUE'));
+    });
+
+    it('should display contact us forHelp', () => {
+      const contactUsForHelp = htmlDocument.getElementsByClassName('govuk-details');
+      expect(contactUsForHelp[0].innerHTML).toContain('Contact us for help');
+    });
+  });
+});
