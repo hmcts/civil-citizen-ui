630642709af6ce1007eba10698aeb32d722d6fbb
commitBlob <maro.radovic12@gmail.com>
2022-09-02 09:54:39 +0100

    feat(CIV-262): consume firstContact cookie

--- src/main/routes/features/public/firstContact/claimSummaryController.ts
+++ src/main/routes/features/public/firstContact/claimSummaryController.ts
@@ -4,27 +4,23 @@ import {Claim} from '../../../../common/models/claim';
 import {getClaimById} from '../../../../modules/utilityService';
 import {getInterestDetails} from '../../../../common/utils/interestUtils';
 import {getTotalAmountWithInterestAndFees} from '../../../../modules/claimDetailsService';
+import {YesNo} from '../../../../common/form/models/yesNo';
 
 const firstContactClaimSummaryController = express.Router();
 
 firstContactClaimSummaryController.get(FIRST_CONTACT_CLAIM_SUMMARY_URL,
   async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-      console.log('cookie', req.cookies.firstContact);
-
-      // TODO: get claimId from cookie
-      const claimId = '1645882162449404';
-      const claim: Claim = await getClaimById(claimId, req);
-      const interestData = getInterestDetails(claim);
-      const totalAmount = getTotalAmountWithInterestAndFees(claim);
-      res.render('features/public/firstContact/claim-summary', {
-        claim, totalAmount, interestData,
-      });
-
-      if (req.cookies.firstContact) {
-        // check if claimId and pinVerified is set
+      const cookie = req.cookies['firstContact'];
+      if (cookie?.claimId && cookie?.pinVerified === YesNo.YES) {
+        const claimId = req.cookies.firstContact?.claimId;
+        const claim: Claim = await getClaimById(claimId, req);
+        const interestData = getInterestDetails(claim);
+        const totalAmount = getTotalAmountWithInterestAndFees(claim);
+        res.render('features/public/firstContact/claim-summary', {
+          claim, totalAmount, interestData,
+        });
       } else {
-        // redirect to unauthorised
         res.redirect(FIRST_CONTACT_ACCESS_DENIED_URL);
       }
     } catch (error) {
