411cf1b25616da9e3c9fe7becee133dd9d34eb39
Kadir <kegilmez@gmail.com>
2022-09-05 16:16:42 +0100

    controller and view test

---
+++ src/test/unit/routes/features/response/submitComfirmationController.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import {app} from '../../../../../main/app';
+import nock from 'nock';
+import config from 'config';
+import {RESPONSE_SUBMIT_URL} from '../../../../../main/routes/urls';
+import {mockCivilClaim, mockRedisFailure} from '../../../../utils/mockDraftStore';
+
+jest.mock('../../../../../main/modules/oidc');
+jest.mock('../../../../../main/modules/draft-store');
+
+describe('Submit confirmation controller', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+  beforeEach(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+  });
+  describe('on GET', () => {
+    it('should return submit confirmation from claim', async () => {
+      app.locals.draftStoreClient = mockCivilClaim;
+      await request(app)
+        .get(RESPONSE_SUBMIT_URL)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('You&#39;ve submitted your response');
+        });
+    });
+
+    it('should return http 500 when has error in the get method', async () => {
+      app.locals.draftStoreClient = mockRedisFailure;
+      await request(app)
+        .get(RESPONSE_SUBMIT_URL)
+        .expect((res) => {
+          expect(res.status).toBe(500);
+        });
+    });
+  });
+});
