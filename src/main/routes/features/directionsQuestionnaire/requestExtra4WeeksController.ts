dbb2ee7f199f2710c9a0a5cf8ed46f5f1666068d
Kadir <kegilmez@gmail.com>
2022-09-15 13:10:35 +0100

    implemented generic YesNo model

--- src/main/routes/features/directionsQuestionnaire/requestExtra4WeeksController.ts
+++ src/main/routes/features/directionsQuestionnaire/requestExtra4WeeksController.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import {DQ_REQUEST_EXTRA_4WEEKS_URL, DQ_CONSIDER_CLAIMANT_DOCUMENTS} from '../../urls';
 import {GenericForm} from '../../../common/form/models/genericForm';
-import {TriedToSettle} from '../../../common/models/directionsQuestionnaire/triedToSettle';
+import {RequestExtra4weeks} from 'common/models/directionsQuestionnaire/requestExtra4Weeks';
 import {
   getRequestExtra4weeks,
   getRequestExtra4weeksForm,
@@ -9,12 +9,13 @@ import {
 } from '../../../services/features/directionsQuestionnaire/requestExtra4WeeksService';
 import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
 
+
 const requestExtra4WeeksController = express.Router();
 
-function renderView(form: GenericForm<TriedToSettle>, res: express.Response): void {
-  const triedToSettleClaimForm = Object.assign(form);
-  triedToSettleClaimForm.option = form.model.option;
-  res.render('features/directionsQuestionnaire/request-extra-4weeks', {form: triedToSettleClaimForm});
+function renderView(form: GenericForm<RequestExtra4weeks>, res: express.Response): void {
+  const requestExtra4WeeksClaimForm = Object.assign(form);
+  requestExtra4WeeksClaimForm.option = form.model.option;
+  res.render('features/directionsQuestionnaire/request-extra-4weeks', {form: requestExtra4WeeksClaimForm});
 }
 
 requestExtra4WeeksController.get(DQ_REQUEST_EXTRA_4WEEKS_URL, async (req, res, next) => {
@@ -28,14 +29,14 @@ requestExtra4WeeksController.get(DQ_REQUEST_EXTRA_4WEEKS_URL, async (req, res, n
 requestExtra4WeeksController.post(DQ_REQUEST_EXTRA_4WEEKS_URL, async (req, res, next) => {
   try {
     const claimId = req.params.id;
-    const triedToSettle = getRequestExtra4weeksForm(req.body.option);
-    const form = new GenericForm(triedToSettle);
+    const requestExtra4Weeks = getRequestExtra4weeksForm(req.body.option);
+    const form = new GenericForm(requestExtra4Weeks);
     form.validateSync();
 
     if (form.hasErrors()) {
       renderView(form, res);
     } else {
-      await saveRequestExtra4weeks(claimId, triedToSettle);
+      await saveRequestExtra4weeks(claimId, requestExtra4Weeks);
       res.redirect(constructResponseUrlWithIdParams(claimId, DQ_CONSIDER_CLAIMANT_DOCUMENTS));
     }
   } catch (error) {
--- src/main/services/features/directionsQuestionnaire/requestExtra4WeeksService.ts
+++ src/main/services/features/directionsQuestionnaire/requestExtra4WeeksService.ts
@@ -1,14 +1,16 @@
 import {RequestExtra4weeks} from '../../../common/models/directionsQuestionnaire/requestExtra4Weeks';
 import {getCaseDataFromStore, saveDraftClaim} from '../../../modules/draft-store/draftStoreService';
 import {DirectionQuestionnaire} from '../../../common/models/directionsQuestionnaire/directionQuestionnaire';
+import {GenericYesNo} from '../../../common/form/models/genericYesNo';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('DQ - Request Extra 4 Weeks to settle');
+const requestExtra4weeksErrorMessage = 'ERRORS.VALID_REQUEST_EXTRA_4WEEKS';
 
 const getRequestExtra4weeks = async (claimId: string): Promise<RequestExtra4weeks> => {
   try {
     const caseData = await getCaseDataFromStore(claimId);
-    return caseData?.directionQuestionnaire?.requestExtra4weeks ? caseData.directionQuestionnaire.requestExtra4weeks : new RequestExtra4weeks();
+    return caseData?.directionQuestionnaire?.requestExtra4weeks ? caseData.directionQuestionnaire.requestExtra4weeks : new GenericYesNo(undefined, requestExtra4weeksErrorMessage);
   } catch (error) {
     logger.error(error);
     throw error;
