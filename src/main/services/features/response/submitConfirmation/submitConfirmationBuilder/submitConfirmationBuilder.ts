c957638a499f4eb98fe9de3e37eec2e93bea31ec
Felipe <felipe@LAPTOP3721.V1.com>
2022-09-15 14:34:12 +0200

    Fix: lint errors

--- src/main/services/features/response/submitConfirmation/submitConfirmationBuilder/submitConfirmationBuilder.ts
+++ src/main/services/features/response/submitConfirmation/submitConfirmationBuilder/submitConfirmationBuilder.ts
@@ -23,48 +23,49 @@ import {
 export function buildSubmitStatus(claimId: string, claim: Claim, lang: string): ClaimSummarySection[] {
   const contactYouStatement = getContactYouStatement(lang);
   const financialDetails = getfinancialDetails(claimId, claim, lang);
+  const FAPAyImmediatelyStatus = getFAPAyImmediatelyStatus(claim, lang);
+  const FAPayByDateStatus = getFAPayByDateStatus(claim, lang);
+  const FAPayByInstallmentsStatus = getFAPayByInstallmentsStatus(claim, lang);
+  const PAPayImmediatelyStatus = getPAPayImmediatelyStatus(claim, lang);
+  const PAPayByDateStatus = getPAPayByDateStatus(claim, lang);
+  const PAPayInstallmentsStatus = getPAPayInstallmentsStatus(claim, lang);
 
   switch (claim.responseStatus) {
     case ClaimResponseStatus.FA_PAY_IMMEDIATELY:
-      const FAPAyImmediatelyStatus = getFAPAyImmediatelyStatus(claim, lang);
       return FAPAyImmediatelyStatus;
     case ClaimResponseStatus.FA_PAY_BY_DATE:
-      const FAPayByDateStatus = getFAPayByDateStatus(claim, lang);
       return [...FAPayByDateStatus, ...contactYouStatement, ...financialDetails];
     case ClaimResponseStatus.FA_PAY_INSTALLMENTS:
-      const FAPayByInstallmentsStatus = getFAPayByInstallmentsStatus(claim, lang);
       return [...FAPayByInstallmentsStatus, ...contactYouStatement, ...financialDetails];
     case ClaimResponseStatus.PA_NOT_PAID_PAY_IMMEDIATELY:
-      const PAPayImmediatelyStatus = getPAPayImmediatelyStatus(claim, lang);
       return [...PAPayImmediatelyStatus, ...contactYouStatement];
     case ClaimResponseStatus.PA_NOT_PAID_PAY_BY_DATE:
-      const PAPayByDateStatus = getPAPayByDateStatus(claim, lang);
       return [...PAPayByDateStatus, ...contactYouStatement, ...financialDetails];
     case ClaimResponseStatus.PA_NOT_PAID_PAY_INSTALLMENTS:
-      const PAPayInstallmentsStatus = getPAPayInstallmentsStatus(claim, lang);
       return [...PAPayInstallmentsStatus, ...contactYouStatement, ...financialDetails];
   }
 }
 
 export function buildNextStepsSection(claimId: string, claim: Claim, lang: string): ClaimSummarySection[] {
+  const FAPayImmediatelyNextSteps = getFAPayImmediatelyNextSteps(claimId, claim, lang);
+  const FAPayByDateNextSteps = getFAPayByDateNextSteps(claimId, claim, lang);
+  const FAPayByInstallmentsNextSteps = getFAPayByInstallmentsNextSteps(claimId, claim, lang);
+  const PAPayImmediatelyNextSteps = getPAPayImmediatelyNextSteps(claimId, claim, lang);
+  const PAPayByDateNextSteps = getPAPayByDateNextSteps(claimId, claim, lang);
+  const PAPayInstallmentsNextSteps = getPAPayInstallmentsNextSteps(claimId, claim, lang);
+
   switch (claim.responseStatus) {
     case ClaimResponseStatus.FA_PAY_IMMEDIATELY:
-      const FAPayImmediatelyNextSteps = getFAPayImmediatelyNextSteps(claimId, claim, lang);
       return FAPayImmediatelyNextSteps;
     case ClaimResponseStatus.FA_PAY_BY_DATE:
-      const FAPayByDateNextSteps = getFAPayByDateNextSteps(claimId, claim, lang);
       return FAPayByDateNextSteps;
     case ClaimResponseStatus.FA_PAY_INSTALLMENTS:
-      const FAPayByInstallmentsNextSteps = getFAPayByInstallmentsNextSteps(claimId, claim, lang);
       return FAPayByInstallmentsNextSteps;
     case ClaimResponseStatus.PA_NOT_PAID_PAY_IMMEDIATELY:
-      const PAPayImmediatelyNextSteps = getPAPayImmediatelyNextSteps(claimId, claim, lang);
       return PAPayImmediatelyNextSteps;
     case ClaimResponseStatus.PA_NOT_PAID_PAY_BY_DATE:
-      const PAPayByDateNextSteps = getPAPayByDateNextSteps(claimId, claim, lang);
       return PAPayByDateNextSteps;
     case ClaimResponseStatus.PA_NOT_PAID_PAY_INSTALLMENTS:
-      const PAPayInstallmentsNextSteps = getPAPayInstallmentsNextSteps(claimId, claim, lang);
       return PAPayInstallmentsNextSteps;
   }
 }
