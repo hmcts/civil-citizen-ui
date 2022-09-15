c537baf00d208c1c4307457410ea1166b8ac0b65
Kadir <kegilmez@gmail.com>
2022-09-15 09:14:08 +0100

    developed the service

---
+++ src/main/services/features/directionsQuestionnaire/requestExtra4WeeksService.ts
@@ -0,0 +1,43 @@
+import {RequestExtra4weeks} from '../../../common/models/directionsQuestionnaire/requestExtra4Weeks';
+import {getCaseDataFromStore, saveDraftClaim} from '../../../modules/draft-store/draftStoreService';
+import {DirectionQuestionnaire} from '../../../common/models/directionsQuestionnaire/directionQuestionnaire';
+
+const {Logger} = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('DQ - Request Extra 4 Weeks to settle');
+
+const getRequestExtra4weeks = async (claimId: string): Promise<RequestExtra4weeks> => {
+  try {
+    const caseData = await getCaseDataFromStore(claimId);
+    return caseData?.directionQuestionnaire?.requestExtra4weeks ? caseData.directionQuestionnaire.requestExtra4weeks : new RequestExtra4weeks();
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+};
+
+const getRequestExtra4weeksForm = (requestExtra4weeks: string): RequestExtra4weeks => {
+  return new RequestExtra4weeks(requestExtra4weeks);
+};
+
+const saveRequestExtra4weeks = async (claimId: string, requestExtra4weeks: RequestExtra4weeks) => {
+  try {
+    const caseData = await getCaseDataFromStore(claimId);
+    if (caseData?.directionQuestionnaire) {
+      caseData.directionQuestionnaire = {...caseData.directionQuestionnaire};
+      caseData.directionQuestionnaire.requestExtra4weeks = requestExtra4weeks;
+    } else {
+      caseData.directionQuestionnaire = new DirectionQuestionnaire();
+      caseData.directionQuestionnaire.requestExtra4weeks = requestExtra4weeks;
+    }
+    await saveDraftClaim(claimId, caseData);
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+};
+
+export {
+  getRequestExtra4weeks,
+  getRequestExtra4weeksForm,
+  saveRequestExtra4weeks,
+};
