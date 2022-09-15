18c46c4ec8cc312186a9bcca32b1087d7e2b6c9b
Kadir <kegilmez@gmail.com>
2022-09-15 10:58:23 +0100

    updated the view with icon

--- src/main/common/models/directionsQuestionnaire/requestExtra4Weeks.ts
+++ src/main/common/models/directionsQuestionnaire/requestExtra4Weeks.ts
@@ -1,7 +1,7 @@
 import {IsDefined} from 'class-validator';
 
 export class RequestExtra4weeks {
-  @IsDefined({message: 'ERRORS.VALID_TRIED_TO_SETTLE'})
+  @IsDefined({message: 'ERRORS.VALID_REQUEST_EXTRA_4_WEEKS'})
     option?: string;
 
   constructor(triedToSettle?: string) {
