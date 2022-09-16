6f2525f6c6e0e7492c412ccc21126f2e1dfde0d1
OrtizP <pablo.gutierrez@version1.com>
2022-09-16 15:57:41 +0200

    Fix merge

--- src/main/routes/urls.ts
+++ src/main/routes/urls.ts
@@ -124,7 +124,6 @@ export const CLAIM_COMPLETING_CLAIM_URL = `${BASE_CLAIM_URL}/completing-claim`;
 export const FIRST_CONTACT_ACCESS_DENIED_URL = `${BASE_FIRST_CONTACT_URL}/access-denied`;
 export const FIRST_CONTACT_PIN_URL = `${BASE_FIRST_CONTACT_URL}/pin`;
 export const CLAIM_RESOLVING_DISPUTE_URL = `${BASE_CLAIM_URL}/resolving-this-dispute`;
-export const DQ_CONSIDER_CLAIMANT_DOCUMENTS = `${DIRECTIONS_QUESTIONNAIRE_URL}/consider-claimant-documents`;
 export const DQ_DEFENDANT_EXPERT_EVIDENCE = `${DIRECTIONS_QUESTIONNAIRE_URL}/defendant-expert-evidence`;
 export const DQ_TRIED_TO_SETTLE_CLAIM_URL = `${DIRECTIONS_QUESTIONNAIRE_URL}/tried-to-settle`;
 export const DQ_REQUEST_EXTRA_4WEEKS_URL = `${DIRECTIONS_QUESTIONNAIRE_URL}/request-extra-4-weeks`;
