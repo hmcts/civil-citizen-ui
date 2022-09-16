eef05547460d502263c60e5bee6d64af7c70a09f
OrtizP <pablo.gutierrez@version1.com>
2022-09-16 16:07:44 +0200

    Adjustment of the URL names

--- src/main/routes/features/directionsQuestionnaire/expertSmallClaimsController.ts
+++ src/main/routes/features/directionsQuestionnaire/expertSmallClaimsController.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
 import {
-  DQ_EXPERT_SMALL_CLAIMS,
-  DQ_GIVE_EVIDENCE_YOURSELF,
-  DQ_HAVE_EXPERT_REPORT_URL,
+  DQ_EXPERT_SMALL_CLAIMS_URL,
+  DQ_GIVE_EVIDENCE_YOURSELF_URL,
+  DQ_EXPERT_EXPORTS_URL,
 } from '../../urls';
 
 const expertSmallClaimsController = express.Router();
 const expertSmallClaimsViewPath = 'features/public/eligibility/expert-small-claims';
 
-expertSmallClaimsController.get(DQ_EXPERT_SMALL_CLAIMS, async (req:express.Request, res:express.Response) => {
+expertSmallClaimsController.get(DQ_EXPERT_SMALL_CLAIMS_URL, async (req:express.Request, res:express.Response) => {
   res.render(expertSmallClaimsViewPath,
-    {giveEvidenceYourselfScreenURL: DQ_GIVE_EVIDENCE_YOURSELF , reportWrittenByExpertScreenURL: DQ_HAVE_EXPERT_REPORT_URL});
+    {giveEvidenceYourselfScreenURL: DQ_GIVE_EVIDENCE_YOURSELF_URL , reportWrittenByExpertScreenURL: DQ_EXPERT_EXPORTS_URL});
 });
 
 export default expertSmallClaimsController;
