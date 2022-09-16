6e13b03cac7f4dbdc98793a6ed9d7ba9a87d4ae9
lfazelyniaHMCTS <lawrence.fazelynia@HMCTS.NET>
2022-09-16 16:07:56 +0100

    CIV-4225 | convert error messages to translation keys for model

--- src/main/common/form/models/partDetails.ts
+++ src/main/common/form/models/partDetails.ts
@@ -1,31 +1,25 @@
 import {
-  VALID_TITLE,
-  VALID_FIRST_NAME,
-  VALID_LAST_NAME,
-} from '../validationErrors/errorMessageConstants';
-import {
   ValidateIf,
   MaxLength,
   IsDefined,
   IsNotEmpty,
 } from 'class-validator';
-// tODO : convert error messages to traslation keys
 export class PartyDetails {
 
   @ValidateIf(o => o.title !== undefined)
-  @MaxLength(35, {message: VALID_TITLE})
+  @MaxLength(35, {message: 'ERRORS.ENTER_VALID_TITLE'})
     title?: string;
 
   @ValidateIf(o => o.firstName !== undefined)
-  @IsDefined({message: VALID_FIRST_NAME})
-  @IsNotEmpty({message: VALID_FIRST_NAME})
-  @MaxLength(255, {message: VALID_FIRST_NAME})
+  @IsDefined({message: 'ERRORS.ENTER_FIRST_NAME'})
+  @IsNotEmpty({message: 'ERRORS.ENTER_FIRST_NAME'})
+  @MaxLength(255, {message: 'ERRORS.TEXT_TOO_MANY'})
     firstName?: string;
 
   @ValidateIf(o => o.lastName !== undefined)
-  @IsDefined({message: VALID_LAST_NAME})
-  @IsNotEmpty({message: VALID_LAST_NAME})
-  @MaxLength(255, {message: VALID_LAST_NAME})
+  @IsDefined({message: 'ERRORS.ENTER_LAST_NAME'})
+  @IsNotEmpty({message: 'ERRORS.ENTER_LAST_NAME'})
+  @MaxLength(255, {message: 'ERRORS.TEXT_TOO_MANY'})
     lastName?: string;
 
   constructor(
