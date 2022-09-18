4271b3a41cad8937e243f801aa922645084a983a
laarmada <sergiosalmar@yahoo.es>
2022-09-18 22:52:12 +0100

    adapt to genericService

--- src/main/routes/features/directionsQuestionnaire/defendantExpertEvidenceController.ts
+++ src/main/routes/features/directionsQuestionnaire/defendantExpertEvidenceController.ts
@@ -5,19 +5,19 @@ import {
   DQ_DEFENDANT_YOURSELF_EVIDENCE_URL,
 } from '../../urls';
 import {
+  getGenericOption,
+  getGenericOptionForm,
   saveDirectionQuestionnaire,
 } from '../../../services/features/directionsQuestionnaire/directionQuestionnaireService';
 import {GenericForm} from '../../../common/form/models/genericForm';
 import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
 import {YesNo} from '../../../common/form/models/yesNo';
 import {GenericYesNo} from '../../../common/form/models/genericYesNo';
-import {
-  getExpertEvidence,
-  getExpertEvidenceForm,
-} from '../../../services/features/directionsQuestionnaire/defendantExpertEvidenceService';
 
 const defendantExpertEvidenceController = express.Router();
 const defendantExpertEvidenceViewPath = 'features/directionsQuestionnaire/defendant-expert-evidence';
+const errorMessage = 'ERRORS.DEFENDANT_EXPERT_EVIDENCE_REQUIRED';
+const dqPropertyName = 'defendantExpertEvidence';
 
 function renderView(form: GenericForm<GenericYesNo>, res: express.Response): void {
   res.render(defendantExpertEvidenceViewPath, {form});
@@ -25,7 +25,7 @@ function renderView(form: GenericForm<GenericYesNo>, res: express.Response): voi
 
 defendantExpertEvidenceController.get(DQ_DEFENDANT_EXPERT_EVIDENCE_URL, async (req, res, next: express.NextFunction) => {
   try {
-    const defendantExpertEvidence = await getExpertEvidence(req.params.id);
+    const defendantExpertEvidence = await getGenericOption(req.params.id, dqPropertyName, errorMessage);
     renderView(new GenericForm(defendantExpertEvidence), res);
   } catch (error) {
     next(error);
@@ -35,14 +35,14 @@ defendantExpertEvidenceController.get(DQ_DEFENDANT_EXPERT_EVIDENCE_URL, async (r
 defendantExpertEvidenceController.post(DQ_DEFENDANT_EXPERT_EVIDENCE_URL, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
     const claimId = req.params.id;
-    const defendantExpertEvidence = new GenericForm(getExpertEvidenceForm(req.body.option));
+    const defendantExpertEvidence = new GenericForm(getGenericOptionForm(req.body.option, errorMessage));
     defendantExpertEvidence.validateSync();
 
     if (defendantExpertEvidence.hasErrors()) {
       renderView(defendantExpertEvidence, res);
     } else {
       defendantExpertEvidence.model.option = req.body.option;
-      await saveDirectionQuestionnaire(claimId, defendantExpertEvidence.model, 'defendantExpertEvidence');
+      await saveDirectionQuestionnaire(claimId, defendantExpertEvidence.model, dqPropertyName);
       if (req.body.option === YesNo.YES) {
         res.redirect(constructResponseUrlWithIdParams(claimId, DQ_DEFENDANT_EXPERT_REPORTS_URL));
       } else {
