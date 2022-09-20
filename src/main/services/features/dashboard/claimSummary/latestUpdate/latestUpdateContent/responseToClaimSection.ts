77c3fb844157277bbc88423f1ef707f0ef4f3c15
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-09-20 16:28:05 +0100

    CIV-2432 -formatting

--- src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContent/responseToClaimSection.ts
+++ src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContent/responseToClaimSection.ts
@@ -3,12 +3,12 @@ import {Claim} from '../../../../../../common/models/claim';
 import {CLAIM_TASK_LIST_URL} from '../../../../../../routes/urls';
 
 export const getResponseNotSubmittedTitle = (isResponseDeadlineExtended: boolean): ClaimSummarySection => {
-  return  isResponseDeadlineExtended? ({
+  return isResponseDeadlineExtended ? ({
     type: ClaimSummaryType.TITLE,
     data: {
       text: 'PAGES.LATEST_UPDATE_CONTENT.MORE_TIME_REQUESTED',
     },
-  }): ({
+  }) : ({
     type: ClaimSummaryType.TITLE,
     data: {
       text: 'PAGES.LATEST_UPDATE_CONTENT.YOU_HAVENT_RESPONDED_TO_CLAIM',
