28b661846937699c701335f3d501708836ea7652
commitBlob <maro.radovic12@gmail.com>
2022-09-21 13:08:29 +0100

    refactor(CIV-5005): saveDirectionQuestionnaire on determinationWithoutHearing

--- src/main/services/features/directionsQuestionnaire/determinationWithoutHearingService.ts
+++ src/main/services/features/directionsQuestionnaire/determinationWithoutHearingService.ts
@@ -1,7 +1,6 @@
 import {DeterminationWithoutHearing} from '../../../common/models/directionsQuestionnaire/determinationWithoutHearing';
-import {getCaseDataFromStore, saveDraftClaim} from '../../../modules/draft-store/draftStoreService';
+import {getCaseDataFromStore} from '../../../modules/draft-store/draftStoreService';
 import {YesNo} from '../../../common/form/models/yesNo';
-import {DirectionQuestionnaire} from '../../../common/models/directionsQuestionnaire/directionQuestionnaire';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('determinationWithoutHearing');
@@ -22,19 +21,4 @@ export const getDeterminationWithoutHearingForm = (isDeterminationWithoutHearing
   return new DeterminationWithoutHearing(
     isDeterminationWithoutHearing,
     isDeterminationWithoutHearing === YesNo.YES ? '' : reasonForHearing);
-};
-
-export const saveDeterminationWithoutHearing = async (claimId: string, determinationWithoutHearing: DeterminationWithoutHearing) => {
-  try {
-    const caseData = await getCaseDataFromStore(claimId);
-    if (caseData?.directionQuestionnaire) {
-      caseData.directionQuestionnaire = {...caseData.directionQuestionnaire, determinationWithoutHearing};
-    } else {
-      caseData.directionQuestionnaire = {...new DirectionQuestionnaire(), determinationWithoutHearing};
-    }
-    await saveDraftClaim(claimId, caseData);
-  } catch (error) {
-    logger.error(error);
-    throw error;
-  }
 };
--- src/test/unit/services/features/directionsQuestionnaire/determinationWithoutHearingService.test.ts
+++ src/test/unit/services/features/directionsQuestionnaire/determinationWithoutHearingService.test.ts
@@ -1,21 +1,14 @@
 import * as draftStoreService from '../../../../../main/modules/draft-store/draftStoreService';
 import {Claim} from '../../../../../main/common/models/claim';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
-import {
-  getDeterminationWithoutHearing,
-  saveDeterminationWithoutHearing,
-} from '../../../../../main/services/features/directionsQuestionnaire/determinationWithoutHearingService';
+import {getDeterminationWithoutHearing} from '../../../../../main/services/features/directionsQuestionnaire/determinationWithoutHearingService';
 import {YesNo} from '../../../../../main/common/form/models/yesNo';
-import {
-  DeterminationWithoutHearing,
-} from '../../../../../main/common/models/directionsQuestionnaire/determinationWithoutHearing';
 import {DirectionQuestionnaire} from '../../../../../main/common/models/directionsQuestionnaire/directionQuestionnaire';
 
 jest.mock('../../../../../main/modules/draft-store');
 jest.mock('../../../../../main/modules/draft-store/draftStoreService');
 
 const mockGetCaseDataFromDraftStore = draftStoreService.getCaseDataFromStore as jest.Mock;
-const mockSaveDraftClaim = draftStoreService.saveDraftClaim as jest.Mock;
 
 describe('Determination Without Hearing Service', () => {
   describe('getDeterminationWithoutHearing', () => {
@@ -66,51 +59,6 @@ describe('Determination Without Hearing Service', () => {
       });
 
       await expect(getDeterminationWithoutHearing('claimId')).rejects.toThrow(TestMessages.REDIS_FAILURE);
-    });
-  });
-
-  describe('saveDeterminationWithoutHearing', () => {
-    const determinationWithoutHearing: DeterminationWithoutHearing = {
-      isDeterminationWithoutHearing: YesNo.YES,
-    };
-
-    it('should save determination without hearing successfully', async () => {
-      mockGetCaseDataFromDraftStore.mockImplementation(async () => {
-        return new Claim();
-      });
-      const spySave = jest.spyOn(draftStoreService, 'saveDraftClaim');
-
-      await saveDeterminationWithoutHearing('validClaimId', determinationWithoutHearing);
-      expect(spySave).toHaveBeenCalledWith('validClaimId', {determinationWithoutHearing});
-    });
-
-    it('should update claim determination successfully', async () => {
-      const claim = new Claim();
-      claim.directionQuestionnaire = new DirectionQuestionnaire();
-      claim.directionQuestionnaire.determinationWithoutHearing = determinationWithoutHearing;
-      const updatedDetermination: DeterminationWithoutHearing = {
-        isDeterminationWithoutHearing: YesNo.NO,
-        reasonForHearing: 'my reason',
-      };
-      mockGetCaseDataFromDraftStore.mockImplementation(async () => {
-        return claim;
-      });
-      const spySave = jest.spyOn(draftStoreService, 'saveDraftClaim');
-
-      await saveDeterminationWithoutHearing('validClaimId', updatedDetermination);
-      expect(spySave).toHaveBeenCalledWith('validClaimId', {determinationWithoutHearing: updatedDetermination});
-    });
-
-    it('should return an error on redis failure', async () => {
-      mockGetCaseDataFromDraftStore.mockImplementation(async () => {
-        return new Claim();
-      });
-      mockSaveDraftClaim.mockImplementation(async () => {
-        throw new Error(TestMessages.REDIS_FAILURE);
-      });
-
-      await expect(saveDeterminationWithoutHearing('claimId', {isDeterminationWithoutHearing: YesNo.NO}))
-        .rejects.toThrow(TestMessages.REDIS_FAILURE);
     });
   });
 });
