7a5b93c90fc07d4b4827ac997f89ff689ab86d5b
OrtizP <pablo.gutierrez@version1.com>
2022-09-21 14:03:58 +0200

    Fix URLs names.  - Change DQ_DEFENDANT_YOURSELF_EVIDENCE_URL to DQ_GIVE_EVIDENCE_YOURSELF_URL .4664 have incorrect name because there was another task (4749) already approved from Steve with another name for the same screen.  - Refactor names with final _URL missing.

--- src/main/routes/features/directionsQuestionnaire/considerClaimantDocumentsController.ts
+++ src/main/routes/features/directionsQuestionnaire/considerClaimantDocumentsController.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import {
-  DQ_CONSIDER_CLAIMANT_DOCUMENTS,
-  DQ_DEFENDANT_EXPERT_EVIDENCE,
+  DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL,
+  DQ_DEFENDANT_EXPERT_EVIDENCE_URL,
 } from '../../urls';
 import {GenericForm} from '../../../common/form/models/genericForm';
 import {ConsiderClaimantDocuments} from '../../../common/models/directionsQuestionnaire/considerClaimantDocuments';
@@ -16,7 +16,7 @@ function renderView(form: GenericForm<ConsiderClaimantDocuments>, res: express.R
   res.render(considerClaimantDocumentsViewPath, {form});
 }
 
-considerClaimantDocumentsController.get(DQ_CONSIDER_CLAIMANT_DOCUMENTS, async (req:express.Request, res:express.Response, next: express.NextFunction) => {
+considerClaimantDocumentsController.get(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL, async (req:express.Request, res:express.Response, next: express.NextFunction) => {
   try {
     renderView(new GenericForm(await getConsiderClaimantDocuments(req.params.id)), res);
   } catch (error) {
@@ -24,7 +24,7 @@ considerClaimantDocumentsController.get(DQ_CONSIDER_CLAIMANT_DOCUMENTS, async (r
   }
 });
 
-considerClaimantDocumentsController.post(DQ_CONSIDER_CLAIMANT_DOCUMENTS, async (req:express.Request, res:express.Response, next: express.NextFunction) => {
+considerClaimantDocumentsController.post(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL, async (req:express.Request, res:express.Response, next: express.NextFunction) => {
   try {
     const claimId = req.params.id;
     const considerClaimantDocumentsForm = getConsiderClaimantDocumentsForm(req.body.option, req.body.details);
@@ -35,7 +35,7 @@ considerClaimantDocumentsController.post(DQ_CONSIDER_CLAIMANT_DOCUMENTS, async (
       renderView(form, res);
     } else {
       await saveConsiderClaimantDocuments(claimId, considerClaimantDocumentsForm);
-      res.redirect(DQ_DEFENDANT_EXPERT_EVIDENCE);
+      res.redirect(DQ_DEFENDANT_EXPERT_EVIDENCE_URL);
     }
   } catch (error) {
     next(error);
--- src/test/unit/routes/features/directionsQuestionnaire/considerClaimantDocumentsController.test.ts
+++ src/test/unit/routes/features/directionsQuestionnaire/considerClaimantDocumentsController.test.ts
@@ -4,8 +4,8 @@ import request from 'supertest';
 import {app} from '../../../../../main/app';
 import {mockCivilClaim, mockRedisFailure} from '../../../../utils/mockDraftStore';
 import {
-  DQ_CONSIDER_CLAIMANT_DOCUMENTS,
-  DQ_DEFENDANT_EXPERT_EVIDENCE,
+  DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL,
+  DQ_DEFENDANT_EXPERT_EVIDENCE_URL,
 } from '../../../../../main/routes/urls';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
 import {t} from 'i18next';
@@ -25,7 +25,7 @@ describe('Consider Claimant Documents Controller', () => {
   describe('on GET', () => {
     it('should return consider claimant documents page', async () => {
       app.locals.draftStoreClient = mockCivilClaim;
-      await request(app).get(DQ_CONSIDER_CLAIMANT_DOCUMENTS).expect((res) => {
+      await request(app).get(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL).expect((res) => {
         expect(res.status).toBe(200);
         expect(res.text).toContain('Are there any documents the claimant has that you want the court to consider?');
       });
@@ -34,7 +34,7 @@ describe('Consider Claimant Documents Controller', () => {
     it('should return status 500 when error thrown', async () => {
       app.locals.draftStoreClient = mockRedisFailure;
       await request(app)
-        .get(DQ_CONSIDER_CLAIMANT_DOCUMENTS)
+        .get(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL)
         .expect((res: Response) => {
           expect(res.status).toBe(500);
           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
@@ -48,31 +48,31 @@ describe('Consider Claimant Documents Controller', () => {
     });
 
     it('should return consider claimant documents page', async () => {
-      await request(app).post(DQ_CONSIDER_CLAIMANT_DOCUMENTS).expect((res) => {
+      await request(app).post(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL).expect((res) => {
         expect(res.status).toBe(200);
         expect(res.text).toContain('Are there any documents the claimant has that you want the court to consider?');
       });
     });
 
     it('should redirect to the use expert evidence page if option yes is selected', async () => {
-      await request(app).post(DQ_CONSIDER_CLAIMANT_DOCUMENTS).send({option: 'yes', details : 'details'})
+      await request(app).post(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL).send({option: 'yes', details : 'details'})
         .expect((res) => {
           expect(res.status).toBe(302);
-          expect(res.get('location')).toBe(DQ_DEFENDANT_EXPERT_EVIDENCE);
+          expect(res.get('location')).toBe(DQ_DEFENDANT_EXPERT_EVIDENCE_URL);
         });
     });
 
     it('should redirect to the use expert evidence page if option no is selected', async () => {
-      await request(app).post(DQ_CONSIDER_CLAIMANT_DOCUMENTS).send({option: 'no'})
+      await request(app).post(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL).send({option: 'no'})
         .expect((res) => {
           expect(res.status).toBe(302);
-          expect(res.get('location')).toBe(DQ_DEFENDANT_EXPERT_EVIDENCE);
+          expect(res.get('location')).toBe(DQ_DEFENDANT_EXPERT_EVIDENCE_URL);
         });
     });
 
     it('should show errors when no option is selected', async () => {
       await request(app)
-        .post(DQ_CONSIDER_CLAIMANT_DOCUMENTS)
+        .post(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL)
         .expect((res: Response) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain(t('ERRORS.SELECT_YES_IF_DOCUMENTS'));
@@ -81,7 +81,7 @@ describe('Consider Claimant Documents Controller', () => {
 
     it('should show errors when yes is selected and details not given', async () => {
       await request(app)
-        .post(DQ_CONSIDER_CLAIMANT_DOCUMENTS)
+        .post(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL)
         .send({ option: 'yes', details: '' })
         .expect((res: Response) => {
           expect(res.status).toBe(200);
@@ -92,7 +92,7 @@ describe('Consider Claimant Documents Controller', () => {
     it('should return status 500 when error thrown', async () => {
       app.locals.draftStoreClient = mockRedisFailure;
       await request(app)
-        .post(DQ_CONSIDER_CLAIMANT_DOCUMENTS)
+        .post(DQ_CONSIDER_CLAIMANT_DOCUMENTS_URL)
         .send({option: 'yes', details : 'details'})
         .expect((res) => {
           expect(res.status).toBe(500);
