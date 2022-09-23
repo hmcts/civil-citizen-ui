dc996ed596e0f1c055597e0d6a1a623cecdab020
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-09-23 10:25:43 +0100

    CIV-4665: Update to use generic direction questionnaire

--- src/main/routes/features/directionsQuestionnaire/sentExpertReportsController.ts
+++ src/main/routes/features/directionsQuestionnaire/sentExpertReportsController.ts
@@ -1,12 +1,12 @@
 import * as express from 'express';
 import {DQ_SENT_EXPERT_REPORTS_URL, DQ_SHARE_AN_EXPERT_URL} from '../../urls';
 import {GenericForm} from '../../../common/form/models/genericForm';
-import {SentExpertReports} from 'models/directionsQuestionnaire/sentExpertReports';
+import {SentExpertReports} from '../../../common/models/directionsQuestionnaire/sentExpertReports';
 import {
-  getSentExpertReports,
-  getSentExpertReportsForm,
-  saveSentExpertReports,
-} from '../../../services/features/directionsQuestionnaire/sentExpertReportsService';
+  getDirectionQuestionnaire,
+  saveDirectionQuestionnaire,
+} from '../../../services/features/directionsQuestionnaire/directionQuestionnaireService';
+
 import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
 
 const sentExpertReportsController = express.Router();
@@ -18,7 +18,9 @@ function renderView(form: GenericForm<SentExpertReports>, res: express.Response)
 
 sentExpertReportsController.get(DQ_SENT_EXPERT_REPORTS_URL, async (req, res, next) => {
   try {
-    renderView(new GenericForm(await getSentExpertReports(req.params.id)), res);
+    const directionQuestionnaire = await getDirectionQuestionnaire(req.params.id);
+    const sentExpertReports = directionQuestionnaire.sentExpertReports ? new SentExpertReports(directionQuestionnaire.sentExpertReports.option) : new SentExpertReports();
+    renderView(new GenericForm(sentExpertReports), res);
   } catch (error) {
     next(error);
   }
@@ -27,14 +29,12 @@ sentExpertReportsController.get(DQ_SENT_EXPERT_REPORTS_URL, async (req, res, nex
 sentExpertReportsController.post(DQ_SENT_EXPERT_REPORTS_URL, async (req, res, next) => {
   try {
     const claimId = req.params.id;
-    const expertReports = getSentExpertReportsForm(req.body.sentExpertReportsOptions);
-    const form = new GenericForm(expertReports);
+    const form = new GenericForm(new SentExpertReports(req.body.sentExpertReportsOptions));
     form.validateSync();
-
     if (form.hasErrors()) {
       renderView(form, res);
     } else {
-      await saveSentExpertReports(claimId, expertReports);
+      await saveDirectionQuestionnaire(claimId, form.model, 'sentExpertReports');
       res.redirect(constructResponseUrlWithIdParams(claimId, DQ_SHARE_AN_EXPERT_URL));
     }
   } catch (error) {
