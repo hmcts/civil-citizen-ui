5f3433c867dce802fbf0b50c2afebbe81c9188e2
lfazelyniaHMCTS <lawrence.fazelynia@HMCTS.NET>
2022-09-27 12:26:54 +0100

    CIV-4225 | add more tests

---
+++ src/test/unit/services/features/claim/claimantDetailsService.test.ts
@@ -0,0 +1,116 @@
+import * as draftStoreService from '../../../../../main/modules/draft-store/draftStoreService';
+import {getClaimantInformation, saveClaimant} from '../../../../../main/services/features/claim/claimantDetailsService';
+import {Claim, Party} from '../../../../../main/common/models/claim';
+import {buildPrimaryAddress, mockClaim} from '../../../../utils/mockClaim';
+import {Respondent} from '../../../../../main/common/models/respondent';
+import {YesNo} from '../../../../../main/common/form/models/yesNo';
+import {buildCitizenAddress, buildCitizenCorrespondenceAddress} from '../../../../utils/mockForm';
+import {GenericForm} from '../../../../../main/common/form/models/genericForm';
+import {PartyDetails} from '../../../../../main/common/form/models/partyDetails';
+import {CitizenCorrespondenceAddress} from '../../../../../main/common/form/models/citizenCorrespondenceAddress';
+
+jest.mock('../../../../../main/modules/draft-store');
+jest.mock('../../../../../main/modules/draft-store/draftStoreService');
+
+const mockGetCaseData = draftStoreService.getCaseDataFromStore as jest.Mock;
+const CLAIM_ID = '123';
+const claimData = new Claim();
+const claimantDetails = new GenericForm<PartyDetails>(new PartyDetails(
+  'individualTitle',
+  'individualFirstName',
+  'individualLastName',
+));
+
+describe('Citizen details service', () => {
+  describe('get Claimant Information', () => {
+    it('should return a empty object when no data retrieved', async () => {
+      //Given
+      const spyGetCaseDataFromStore = jest.spyOn(draftStoreService, 'getCaseDataFromStore');
+
+      mockGetCaseData.mockImplementation(async () => {
+        return {};
+      });
+      //when
+      const result: Party = await getClaimantInformation(CLAIM_ID);
+
+      //Then
+      expect(spyGetCaseDataFromStore).toBeCalled();
+      expect(result).not.toBeNull();
+      expect(result).toEqual({});
+    });
+    it('should return a respondent Object with value when data is retrieved', async () => {
+      //Given
+      const spyGetCaseDataFromStore = jest.spyOn(draftStoreService, 'getCaseDataFromStore');
+      mockGetCaseData.mockImplementation(async () => {
+        return mockClaim;
+      });
+      //when
+      const result: Party = await getClaimantInformation(CLAIM_ID);
+
+      //Then
+      expect(spyGetCaseDataFromStore).toBeCalled();
+      expect(result).not.toBeNull();
+      expect(result).toEqual(mockClaim.applicant1);
+    });
+  });
+  describe('save Claimant', () => {
+    it('should save a claimant when has no information on redis ', async () => {
+      //Given
+      const spyGetCaseDataFromStore = jest.spyOn(draftStoreService, 'getCaseDataFromStore');
+      const spySaveDraftClaim = jest.spyOn(draftStoreService, 'saveDraftClaim');
+      claimData.applicant1 = new Respondent();
+      claimData.applicant1.individualTitle = 'individualTitle';
+      claimData.applicant1.individualFirstName = 'individualFirstName';
+      claimData.applicant1.individualLastName = 'individualLastName';
+      claimData.applicant1.primaryAddress = buildPrimaryAddress();
+      claimData.applicant1.provideCorrespondenceAddress = YesNo.NO;
+      mockGetCaseData.mockImplementation(async () => {
+        return new Claim();
+      });
+
+      //when
+      await saveClaimant(CLAIM_ID, buildCitizenAddress(), new GenericForm<CitizenCorrespondenceAddress>(new CitizenCorrespondenceAddress()), YesNo.NO, claimantDetails);
+      //Then
+      expect(spyGetCaseDataFromStore).toBeCalled();
+      expect(spySaveDraftClaim).toBeCalledWith(CLAIM_ID, claimData);
+    });
+
+    it('should save a claim when in redis is undefined', async () => {
+      //Given
+      const spyGetCaseDataFromStore = jest.spyOn(draftStoreService, 'getCaseDataFromStore');
+      const spySaveDraftClaim = jest.spyOn(draftStoreService, 'saveDraftClaim');
+      mockGetCaseData.mockImplementation(async () => {
+        return undefined;
+      });
+      //when
+      await saveClaimant(CLAIM_ID, buildCitizenAddress(), buildCitizenCorrespondenceAddress(), YesNo.NO, claimantDetails);
+      //Then
+      expect(spyGetCaseDataFromStore).toBeCalled();
+      expect(spySaveDraftClaim).toBeCalled();
+    });
+
+    it('should save a claimant when in redis correspondentAddress is undefined or empty and the citizenAddress without information', async () => {
+      //Given
+      const spyGetCaseDataFromStore = jest.spyOn(draftStoreService, 'getCaseDataFromStore');
+      const spySaveDraftClaim = jest.spyOn(draftStoreService, 'saveDraftClaim');
+      claimData.applicant1 = new Respondent();
+      claimData.applicant1.individualTitle = 'individualTitle';
+      claimData.applicant1.individualFirstName = 'individualFirstName';
+      claimData.applicant1.individualLastName = 'individualLastName';
+      claimData.applicant1.primaryAddress = buildPrimaryAddress();
+      claimData.applicant1.provideCorrespondenceAddress = YesNo.NO;
+      mockGetCaseData.mockImplementation(async () => {
+        const claim = mockClaim;
+        const claimant = new Respondent();
+        claimant.primaryAddress = buildPrimaryAddress();
+        claim.respondent1 = claimant;
+        return mockClaim;
+      });
+      //when
+      await saveClaimant(CLAIM_ID, buildCitizenAddress(), new GenericForm<CitizenCorrespondenceAddress>(new CitizenCorrespondenceAddress()), YesNo.NO, claimantDetails);
+      //Then
+      expect(spyGetCaseDataFromStore).toBeCalled();
+      expect(spySaveDraftClaim).toBeCalled();
+    });
+  });
+});
