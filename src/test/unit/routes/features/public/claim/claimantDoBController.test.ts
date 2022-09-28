82448b95cac3ffaa5617d80eebb3b1786b91a636
commitBlob <maro.radovic12@gmail.com>
2022-09-28 14:44:58 +0100

    feat(CIV-4226): WIP

---
+++ src/test/unit/routes/features/public/claim/claimantDoBController.test.ts
@@ -0,0 +1,10 @@
+import config from 'config';
+import nock from 'nock';
+import request from 'supertest';
+import {app} from '../../../../../../main/app';
+
+jest.mock('../../../../../../main/modules/oidc');
+
+describe('Claimant Date of Birth Controller', () => {
+
+});
