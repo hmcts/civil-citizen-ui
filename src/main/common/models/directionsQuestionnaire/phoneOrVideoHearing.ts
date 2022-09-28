6922c1e3f6494f7171a6a81f6f6c8a929cdacead
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-09-28 10:53:22 +0100

    CIV-4690: Changing error message to the related ones

--- src/main/common/models/directionsQuestionnaire/phoneOrVideoHearing.ts
+++ src/main/common/models/directionsQuestionnaire/phoneOrVideoHearing.ts
@@ -3,12 +3,12 @@ import {FREE_TEXT_MAX_LENGTH} from '../../form/validators/validationConstraints'
 import {YesNo} from '../../form/models/yesNo';
 
 export class PhoneOrVideoHearing {
-  @IsDefined({message: 'ERRORS.VALID_YES_NO_SELECTION'})
+  @IsDefined({message: 'ERRORS.VALID_PHONE_OR_VIDEO_HEARING.YES_NO'})
     option?: YesNo;
 
   @ValidateIf(o => o.option === YesNo.YES)
-  @IsDefined({message: 'ERRORS.DEFENDANT_EXPERT_EXPLAIN_EXAMINE'})
-  @IsNotEmpty({message: 'ERRORS.DEFENDANT_EXPERT_EXPLAIN_EXAMINE'})
+  @IsDefined({message: 'ERRORS.VALID_PHONE_OR_VIDEO_HEARING.TELL_US_WHY'})
+  @IsNotEmpty({message: 'ERRORS.VALID_PHONE_OR_VIDEO_HEARING.TELL_US_WHY'})
   @MaxLength(FREE_TEXT_MAX_LENGTH, {message: 'ERRORS.TEXT_TOO_LONG'})
     details?: string;
 
