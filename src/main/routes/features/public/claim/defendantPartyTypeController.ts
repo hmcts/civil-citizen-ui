0a6025ce4ee942a3dee083399bdb6dc8a4b91fc8
commitBlob <maro.radovic12@gmail.com>
2022-09-30 13:27:50 +0100

    feat(CIV-4285): save selection into the respondent object

--- src/main/routes/features/public/claim/defendantPartyTypeController.ts
+++ src/main/routes/features/public/claim/defendantPartyTypeController.ts
@@ -4,27 +4,35 @@ import {GenericForm} from '../../../../common/form/models/genericForm';
 import {PartyTypeSelection} from '../../../../common/form/models/claim/partyTypeSelection';
 import {redirectToPage} from '../../../../services/features/claim/partyTypeService';
 import {ClaimantOrDefendant} from '../../../../common/models/partyType';
+import {getDefendantInformation, saveDefendant} from '../../../../services/features/claim/defendantDetailsService';
+import {Party} from '../../../../common/models/claim';
+import {AppRequest} from '../../../../common/models/AppRequest';
 
 const defendantPartyTypeViewPath = 'features/public/claim/defendant-party-type';
 const defendantPartyTypeController = express.Router();
 
-defendantPartyTypeController.get(CLAIM_DEFENDANT_PARTY_TYPE_URL, (req: express.Request, res: express.Response) => {
-  const defendantPartyType = req.cookies.claim_issue_journey ? req.cookies.claim_issue_journey.defendantPartyType : null;
+defendantPartyTypeController.get(CLAIM_DEFENDANT_PARTY_TYPE_URL, async(req: AppRequest, res: express.Response) => {
+  const caseId = req.session?.user?.id;
+  const defendant: Party = await getDefendantInformation(caseId);
+  const defendantPartyType = defendant?.type;
   const form = new GenericForm(new PartyTypeSelection(defendantPartyType));
   res.render(defendantPartyTypeViewPath, {form});
 });
 
-defendantPartyTypeController.post(CLAIM_DEFENDANT_PARTY_TYPE_URL, (req: express.Request, res: express.Response) => {
-  const form = new GenericForm(new PartyTypeSelection(req.body.option));
-  form.validateSync();
+defendantPartyTypeController.post(CLAIM_DEFENDANT_PARTY_TYPE_URL, async (req: AppRequest, res: express.Response, next: express.NextFunction) => {
+  try {
+    const caseId = req.session?.user?.id;
+    const form = new GenericForm(new PartyTypeSelection(Object.assign(req.body).option));
+    form.validateSync();
 
-  if (form.hasErrors()) {
-    res.render(defendantPartyTypeViewPath, {form});
-  } else {
-    const cookie = req.cookies['claim_issue_journey'] ? req.cookies['claim_issue_journey'] : {};
-    cookie.defendantPartyType = req.body.option;
-    res.cookie('claim_issue_journey', cookie);
-    redirectToPage(form.model.option, res, ClaimantOrDefendant.DEFENDANT);
+    if (form.hasErrors()) {
+      res.render(defendantPartyTypeViewPath, {form});
+    } else {
+      await saveDefendant(caseId, 'type', form.model.option);
+      redirectToPage(form.model.option, res, ClaimantOrDefendant.DEFENDANT);
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
