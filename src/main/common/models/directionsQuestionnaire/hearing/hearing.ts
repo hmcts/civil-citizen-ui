ef1712959fb4caf414f455aba8d686cd020b56c8
OrtizP <pablo.gutierrez@version1.com>
2022-10-03 16:15:02 +0200

    Model

--- src/main/common/models/directionsQuestionnaire/hearing/hearing.ts
+++ src/main/common/models/directionsQuestionnaire/hearing/hearing.ts
@@ -1,10 +1,12 @@
 import {GenericYesNo} from '../../../form/models/genericYesNo';
 import {DeterminationWithoutHearing} from './determinationWithoutHearing';
 import {ConsiderClaimantDocuments} from './considerClaimantDocuments';
+import {WhyUnavailableForHearing} from './whyUnavailableForHearing';
 
 export class Hearing {
   triedToSettle?: GenericYesNo;
   determinationWithoutHearing?: DeterminationWithoutHearing;
   requestExtra4weeks?: GenericYesNo;
   considerClaimantDocuments?: ConsiderClaimantDocuments;
+  whyUnavailableForHearing?: WhyUnavailableForHearing;
 }
