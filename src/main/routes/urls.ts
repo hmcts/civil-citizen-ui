ed79908a89c29490fb677a42cebecd1a49f20f21
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-10-03 14:48:37 +0100

    CIV-4288: Adding URL

--- src/main/routes/urls.ts
+++ src/main/routes/urls.ts
@@ -142,3 +142,4 @@ export const DQ_OTHER_WITNESSES_AVAILABILITY_DATES_FOR_HEARING_URL = `${DIRECTIO
 export const DQ_EXPERT_GUIDANCE_URL = `${DIRECTIONS_QUESTIONNAIRE_URL}/expert-guidance`;
 export const CLAIM_CLAIMANT_INDIVIDUAL_DETAILS_URL = `${BASE_CLAIM_URL}/claimant-individual-details`;
 export const CLAIMANT_DOB_URL = `${BASE_CLAIM_URL}/claimant-dob`;
+export const CLAIMANT_DEFENDANT_EMAIL_URL = `${BASE_CLAIM_URL}/defendant-email`;
