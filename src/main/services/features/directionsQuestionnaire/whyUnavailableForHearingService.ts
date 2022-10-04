7799e3d3cac213a9117132697eacfccfe34b7dc1
OrtizP <pablo.gutierrez@version1.com>
2022-10-04 11:40:11 +0200

    Sonar fix

--- src/main/services/features/directionsQuestionnaire/whyUnavailableForHearingService.ts
+++ src/main/services/features/directionsQuestionnaire/whyUnavailableForHearingService.ts
@@ -1,14 +1,7 @@
-const {Logger} = require('@hmcts/nodejs-logging');
-const logger = Logger.getLogger('whyUnavailableForHearingService');
 
 export const getCalculatedDays = async (): Promise<number> => {
-  try{
-    //TODO: Change the hardcoded 22 to the correct number of days (To be calculated on the page "Are there any dates in
-    // the next 12 months when you, your experts or your witnesses cannot attend a hearing?")
-    return 22;
-  } catch (error) {
-    logger.error(error);
-    throw error;
-  }
+  //TODO: Change the hardcoded 22 to the correct number of days (To be calculated on the page "Are there any dates in
+  // the next 12 months when you, your experts or your witnesses cannot attend a hearing?")
+  return 22;
 };
 
