b5d1c62449893c8db7ecd1514d91df7585e0cab6
OrtizP <pablo.gutierrez@version1.com>
2022-10-04 11:14:32 +0200

    Moved calculation of days to a Service

--- src/main/routes/features/directionsQuestionnaire/whyUnavailableForHearingController.ts
+++ src/main/routes/features/directionsQuestionnaire/whyUnavailableForHearingController.ts
@@ -10,16 +10,15 @@ import {
   saveDirectionQuestionnaire,
 } from '../../../services/features/directionsQuestionnaire/directionQuestionnaireService';
 import {WhyUnavailableForHearing} from '../../../common/models/directionsQuestionnaire/hearing/whyUnavailableForHearing';
+import {getCalculatedDays} from '../../../services/features/directionsQuestionnaire/whyUnavailableForHearingService';
 
 const whyUnavailableForHearingController = express.Router();
 const whyUnavailableForHearingViewPath = 'features/directionsQuestionnaire/why-unavailable-for-hearing';
 const dqPropertyName = 'whyUnavailableForHearing';
 const dqParentName = 'hearing';
 
-function renderView(form: GenericForm<WhyUnavailableForHearing>, res: express.Response): void {
-  //TO DO: Change the hardcoded 22 to the correct number of days (To be calculated on the page "Are there any dates in
-  // the next 12 months when you, your experts or your witnesses cannot attend a hearing?")
-  res.render(whyUnavailableForHearingViewPath, {form, days:22});
+function renderView(form: GenericForm<WhyUnavailableForHearing>, res: express.Response, days:number): void {
+  res.render(whyUnavailableForHearingViewPath, {form, days});
 }
 
 whyUnavailableForHearingController.get(DQ_UNAVAILABLE_FOR_HEARING, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -28,7 +27,8 @@ whyUnavailableForHearingController.get(DQ_UNAVAILABLE_FOR_HEARING, async (req: e
     const directionQuestionnaire = await getDirectionQuestionnaire(req.params.id);
     const whyUnavailableForHearing = directionQuestionnaire.hearing?.whyUnavailableForHearing ?
       directionQuestionnaire.hearing.whyUnavailableForHearing : new WhyUnavailableForHearing();
-    renderView(new GenericForm(whyUnavailableForHearing), res);
+    const days = await getCalculatedDays();
+    renderView(new GenericForm(whyUnavailableForHearing), res, days);
   } catch (error) {
     next(error);
   }
@@ -38,10 +38,11 @@ whyUnavailableForHearingController.post(DQ_UNAVAILABLE_FOR_HEARING, async (req: 
   try {
     const claimId = req.params.id;
     const whyUnavailableForHearing = new GenericForm(new WhyUnavailableForHearing(req.body.reason));
+    const days = await getCalculatedDays();
     whyUnavailableForHearing.validateSync();
 
     if (whyUnavailableForHearing.hasErrors()) {
-      renderView(whyUnavailableForHearing, res);
+      renderView(whyUnavailableForHearing, res, days);
     } else {
       await saveDirectionQuestionnaire(claimId, whyUnavailableForHearing.model, dqPropertyName, dqParentName);
       res.redirect(constructResponseUrlWithIdParams(claimId, DQ_PHONE_OR_VIDEO_HEARING_URL));
