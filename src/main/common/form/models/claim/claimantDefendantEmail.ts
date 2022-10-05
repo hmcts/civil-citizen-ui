4373b0bfb7230a46f6864250b0bc350377d7c1ad
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-10-05 09:14:14 +0100

    CIV-4288: Changing the error message

--- src/main/common/form/models/claim/claimantDefendantEmail.ts
+++ src/main/common/form/models/claim/claimantDefendantEmail.ts
@@ -3,7 +3,7 @@ import {IsEmail, ValidateIf} from "class-validator";
 export class ClaimantDefendantEmail {
 
   @ValidateIf(o => o.emailAddress)
-  @IsEmail({IsEmailOption: 'allow_display_name'}, {message: 'ERRORS.ENTER_VALID_EMAIL'})
+  @IsEmail({IsEmailOption: 'allow_display_name'}, {message: 'ERRORS.VALID_ENTER_VALID_EMAIL_ADDRESS'})
   emailAddress?: string;
 
   constructor(emailAddress?: string) {
