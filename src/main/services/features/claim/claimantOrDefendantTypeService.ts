9bb3b85c77d52ad2d4aacbf537cd82961fe03ae6
commitBlob <maro.radovic12@gmail.com>
2022-10-05 13:19:02 +0100

    feat(CIV-4320): add logic in getPartyTypeDependingOnRoute to cater for claimant routes

--- src/main/services/features/claim/claimantOrDefendantTypeService.ts
+++ src/main/services/features/claim/claimantOrDefendantTypeService.ts
@@ -1,13 +1,19 @@
 import {PartyType} from '../../../common/models/partyType';
 
 const getPartyTypeDependingOnRoute = (urlRoute: string): PartyType => {
-  //'/claim/defendant-company-details',
-  //       '/claim/defendant-individual-details',
-  //       '/claim/defendant-organisation-details',
-  //       '/claim/defendant-sole-trader-details'
   switch (urlRoute) {
+    case '/claim/claimant-company-details':
     case '/claim/defendant-company-details':
       return PartyType.COMPANY;
+    case '/claim/claimant-individual-details':
+    case '/claim/defendant-individual-details':
+      return PartyType.INDIVIDUAL;
+    case '/claim/claimant-organisation-details':
+    case '/claim/defendant-organisation-details':
+      return PartyType.ORGANISATION;
+    case '/claim/claimant-sole-trader-details':
+    case '/claim/defendant-sole-trader-details':
+      return PartyType.SOLE_TRADER;
     default:
       return undefined;
   }
