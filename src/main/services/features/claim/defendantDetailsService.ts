5c162b34505f4fdab541ef68005cefc859437ec2
commitBlob <maro.radovic12@gmail.com>
2022-10-06 13:36:23 +0100

    feat(CIV-4320): update comment after feedback

--- src/main/services/features/claim/defendantDetailsService.ts
+++ src/main/services/features/claim/defendantDetailsService.ts
@@ -13,7 +13,8 @@ const getDefendantInformation = async (claimId: string): Promise<Party> => {
  * @param claimId - required - can be either claimId or userId (for screens before claim is created)
  * @param propertyName - optional - used when we want to update one defendant property
  * @param propertyValue - optional - the actual value we want to save in the defendant object,
- * can be either single value or object (such as address object). For more info see defendantDetailsController post
+ * can be either single value or object (such as address object). If propertyValue is an object
+ * we need to pass saveObject as true. For more info see defendantDetailsController post
  * @param saveObject - optional - if provided will save propertyValues as object, if updating one property
  * this is not needed
  *
