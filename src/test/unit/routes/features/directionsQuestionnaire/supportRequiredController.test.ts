5422aafd6908f2de4409570973f9fb63456927cd
Kadir <kegilmez@gmail.com>
2022-10-07 22:02:33 +0100

    completeted controller test

--- src/test/unit/routes/features/directionsQuestionnaire/supportRequiredController.test.ts
+++ src/test/unit/routes/features/directionsQuestionnaire/supportRequiredController.test.ts
@@ -4,14 +4,23 @@ import config from 'config';
 import request from 'supertest';
 import {app} from '../../../../../main/app';
 import {
+  NO_NAME_SELECTED,
+  NO_SUPPORT_SELECTED,
   NO_LANGUAGE_ENTERED,
   NO_OTHER_SUPPORT,
   NO_SIGN_LANGUAGE_ENTERED,
 } from '../../../../../main/common/form/validationErrors/errorMessageConstants';
 
-import {mockCivilClaim, mockRedisFailure} from '../../../../utils/mockDraftStore';
-import {CLAIM_TASK_LIST_URL, SUPPORT_REQUIRED_URL} from '../../../../../main/routes/urls';
+import {
+  mockCivilClaim,
+  mockRedisFailure,
+} from '../../../../utils/mockDraftStore';
+import {
+  CLAIM_TASK_LIST_URL,
+  SUPPORT_REQUIRED_URL,
+} from '../../../../../main/routes/urls';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
+import {YesNo} from '../../../../../main/common/form/models/yesNo';
 
 jest.mock('../../../../../main/modules/oidc');
 jest.mock('../../../../../main/modules/draft-store');
@@ -38,7 +47,7 @@ describe('Support required', () => {
         .get(supportRequiredUrl)
         .expect((res: Response) => {
           expect(res.status).toBe(200);
-          expect(res.text).toContain('Select any support you&#39;d require for a court hearing (optional)');
+          expect(res.text).toContain('Do you, your experts or witnesses need support to attend a hearing');
         });
     });
     it('should return status 500 when error thrown', async () => {
@@ -56,12 +65,11 @@ describe('Support required', () => {
       app.locals.draftStoreClient = mockCivilClaim;
     });
 
-    it('when languageSelected checked, and languageInterpreted provided, should redirect to claim task list screen', async () => {
+    it('when there is no option selection, should redirect to claim task list screen', async () => {
       await request(app)
         .post(supportRequiredUrl)
         .send({
-          declared: 'languageSelected',
-          languageInterpreted: 'Croatian',
+          model: {items:[]},
         })
         .expect((res: express.Response) => {
           expect(res.status).toBe(302);
@@ -69,12 +77,17 @@ describe('Support required', () => {
         });
     });
 
-    it('when signLanguageSelected checked, and signLanguageInterpreted provided, should redirect to claim task list screen', async () => {
+    it('when yes selected, name provided and any checkbox selected, should redirect to claim task list screen', async () => {
       await request(app)
         .post(supportRequiredUrl)
         .send({
-          declared: 'signLanguageSelected',
-          signLanguageInterpreted: 'Sign',
+          option: YesNo.YES,
+          declared: ['disabledAccess'],
+          model: {
+            items: [
+              {fullName: 'johndoe'},
+            ],
+          },
         })
         .expect((res: express.Response) => {
           expect(res.status).toBe(302);
@@ -82,12 +95,12 @@ describe('Support required', () => {
         });
     });
 
-    it('when otherSupportSelected checked, and otherSupport provided, should redirect to claim task list screen', async () => {
+    it('when no selected, should redirect to claim task list screen', async () => {
       await request(app)
         .post(supportRequiredUrl)
         .send({
-          declared: 'otherSupportSelected',
-          otherSupport: 'Other',
+          option: YesNo.NO,
+          model: {},
         })
         .expect((res: express.Response) => {
           expect(res.status).toBe(302);
@@ -95,25 +108,68 @@ describe('Support required', () => {
         });
     });
 
-    it('should show error when languageSelected is checked and no languageInterpreted is provided', async () => {
+    it('changing from yes to no should redirect to claim task list screen', async () => {
       await request(app)
         .post(supportRequiredUrl)
         .send({
-          declared: 'languageSelected',
-          languageInterpreted: '',
+          option: YesNo.NO,
+          declared: ['disabledAccess'],
+          model: {
+            items: [
+              {fullName: 'johndoe'},
+            ],
+          },
+        })
+        .expect((res: express.Response) => {
+          expect(res.status).toBe(302);
+          expect(res.get('location')).toBe(CLAIM_TASK_LIST_URL.replace(':id', 'aaa'));
+        });
+    });
+
+    it('should show error when yes selected but no name provided', async () => {
+      await request(app)
+        .post(supportRequiredUrl)
+        .send({
+          option: YesNo.YES,
+          model: {
+            items: [{
+              fullName: '',
+            }]},
+        })
+        .expect((res:Response) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain(NO_NAME_SELECTED);
+        });
+    });
+
+    it('should show error when yes selected but no support selected', async () => {
+      await request(app)
+        .post(supportRequiredUrl)
+        .send({
+          option: YesNo.YES,
+          model: {
+            items: [{
+              fullName: 'johndoe',
+            }],
+          },
         })
         .expect((res: Response) => {
           expect(res.status).toBe(200);
-          expect(res.text).toContain(NO_LANGUAGE_ENTERED);
+          expect(res.text).toContain(NO_SUPPORT_SELECTED);
         });
     });
 
-    it('should show error when signLanguageSelected is checked and no signLanguageInterpreted is provided', async () => {
+    it('should show error when yes and sign language interpreter selected, but no free text provided', async () => {
       await request(app)
         .post(supportRequiredUrl)
         .send({
-          declared: 'signLanguageSelected',
-          signLanguageInterpreted: '',
+          option: YesNo.YES,
+          declared: ['signLanguageInterpreter'],
+          model: {
+            items: [{
+              fullName: 'johndoe',
+            }],
+          },
         })
         .expect((res: Response) => {
           expect(res.status).toBe(200);
@@ -121,33 +177,39 @@ describe('Support required', () => {
         });
     });
 
-    it('should show errors when languageSelected, signLanguageSelected and otherSupportSelected are checked but no information is provided', async () => {
+    it('should show error when yes and language interpreter selected, but no free text provided', async () => {
       await request(app)
         .post(supportRequiredUrl)
         .send({
-          declared: ['languageSelected', 'signLanguageSelected', 'otherSupportSelected'],
-          languageInterpreted: '',
-          signLanguageInterpreted: '',
-          otherSupport: '',
+          option: YesNo.YES,
+          declared: ['languageInterpreter'],
+          model: {
+            items: [{
+              fullName: 'johndoe',
+            }],
+          },
         })
         .expect((res: Response) => {
           expect(res.status).toBe(200);
-          expect(res.text).toContain(NO_SIGN_LANGUAGE_ENTERED);
           expect(res.text).toContain(NO_LANGUAGE_ENTERED);
+        });
+    });
+
+    it('should show error when yes and other support selected, but no free text provided', async () => {
+      await request(app)
+        .post(supportRequiredUrl)
+        .send({
+          option: YesNo.YES,
+          declared: ['otherSupport'],
+          model: {
+            items: [{
+              fullName: 'johndoe',
+            }],
+          },
+        })
+        .expect((res: Response) => {
+          expect(res.status).toBe(200);
           expect(res.text).toContain(NO_OTHER_SUPPORT);
-        });
-    });
-
-    it('should show errors when otherSupportSelected is checked and no otherSupport is provided', async () => {
-      await request(app)
-        .post(supportRequiredUrl)
-        .send({
-          declared: 'otherSupportSelected',
-          otherSupport: '',
-        })
-        .expect((res: Response) => {
-          expect(res.status).toBe(200);
-          expect(res.text).toContain(NO_SIGN_LANGUAGE_ENTERED);
         });
     });
 
@@ -156,8 +218,13 @@ describe('Support required', () => {
       await request(app)
         .post(supportRequiredUrl)
         .send({
-          declared: 'languageSelected',
-          languageInterpreted: 'Croatian',
+          option: YesNo.NO,
+          declared: ['disabledAccess'],
+          model: {
+            items: [
+              {fullName: 'johndoe'},
+            ],
+          },
         })
         .expect((res: Response) => {
           expect(res.status).toBe(500);
