c0e021b2a6f8f589a62389c61081a4e4fa48fc23
Felipe <felipe@LAPTOP3721.V1.com>
2022-10-07 15:38:54 +0200

    Update: add coverage

--- src/test/unit/services/features/response/submission/compareAddress.test.ts
+++ src/test/unit/services/features/response/submission/compareAddress.test.ts
@@ -1,25 +1,30 @@
+import { PrimaryAddress } from "../../../../../../main/common/models/primaryAddress";
 import {addressHasChange} from "../../../../../../main/services/features/response/submission/compareAddress";
 
-const addressData = {
+const addressData: PrimaryAddress = {
   PostTown: 'XXX123',
   AddressLine1: 'Street',
   AddressLine2: '1',
   AddressLine3: '5A',
 };
 
-const completeAddress = {
+const completeAddress: PrimaryAddress = {
   County: 'Test',
   Country: 'Test',
   ...addressData
 };
 
-const addressRedis = {
-  ...addressData
-};
-
-const addressUpdated = {
+const addressUpdated: PrimaryAddress = {
   ...addressData,
   PostCode: '0000',
+};
+const addressWithEmptyString: PrimaryAddress = {
+  ...addressData,
+  PostCode: '',
+};
+const addressWithUndefined: PrimaryAddress = {
+  ...addressData,
+  PostCode: undefined,
 };
 
 describe('Compare addresses util', () => {
@@ -31,7 +36,13 @@ describe('Compare addresses util', () => {
   });
   it('should return false if same addresses excluding Country and County', () => {
     //When
-    const isAddressUpdated = addressHasChange(completeAddress, addressRedis);
+    const isAddressUpdated = addressHasChange(completeAddress, addressData);
+    //Then
+    expect(isAddressUpdated).toEqual(false);
+  });
+  it('should return false comparing undefined with empty string', () => {
+    //When
+    const isAddressUpdated = addressHasChange(addressWithEmptyString, addressWithUndefined);
     //Then
     expect(isAddressUpdated).toEqual(false);
   });
