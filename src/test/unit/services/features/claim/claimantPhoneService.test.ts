8b886768c30b643f8c24aabca5ef6119964ce3df
OrtizP <pablo.gutierrez@version1.com>
2022-10-10 14:14:20 +0200

    Move to correct folder claimantPhoneService.test.ts

--- src/test/unit/services/features/claim/claimantPhoneService.test.ts
+++ src/test/unit/services/features/claim/claimantPhoneService.test.ts
@@ -1,10 +1,10 @@
-import * as draftStoreService from '../../../../../../main/modules/draft-store/draftStoreService';
-import {getClaimantPhone,saveClaimantPhone} from '../../../../../../main/services/features/claim/claimantPhoneService';
-import {Claim, Party} from '../../../../../../main/common/models/claim';
-import {CorrespondenceAddress} from '../../../../../../main/common/models/correspondenceAddress';
-import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
-import {CitizenTelephoneNumber} from '../../../../../../main/common/form/models/citizenTelephoneNumber';
-import {PartyType} from '../../../../../../main/common/models/partyType';
+import * as draftStoreService from '../../../../../main/modules/draft-store/draftStoreService';
+import {getClaimantPhone,saveClaimantPhone} from '../../../../../main/services/features/claim/claimantPhoneService';
+import {Claim, Party} from '../../../../../main/common/models/claim';
+import {CorrespondenceAddress} from '../../../../../main/common/models/correspondenceAddress';
+import {TestMessages} from '../../../../utils/errorMessageTestConstants';
+import {CitizenTelephoneNumber} from '../../../../../main/common/form/models/citizenTelephoneNumber';
+import {PartyType} from '../../../../../main/common/models/partyType';
 
 jest.mock('../../../../../../main/modules/draft-store');
 jest.mock('../../../../../../main/modules/draft-store/draftStoreService');
