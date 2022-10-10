91aa3ee2e41c776cb15007a8fdbc790d2865b858
OrtizP <pablo.gutierrez@version1.com>
2022-10-10 14:14:56 +0200

    Fix view and service

--- src/main/services/features/claim/interest/interestTypeService.ts
+++ src/main/services/features/claim/interest/interestTypeService.ts
@@ -1,13 +1,14 @@
 import {getCaseDataFromStore, saveDraftClaim} from '../../../../modules/draft-store/draftStoreService';
 import InterestClaimOption from '../../../../common/form/models/claim/interest/interestClaimOption';
 import {InterestClaimOptionsType} from '../../../../common/form/models/claim/interest/interestClaimOptionsType';
+import {Claim} from "models/claim";
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('interestTypeService');
 
 export const getInterestTypeForm = async (claimId: string): Promise<InterestClaimOption> => {
   try {
-    const claim = await getCaseDataFromStore(claimId);
+    const claim: Claim = await getCaseDataFromStore(claimId);
     if (claim.isInterestClaimOptionExists()) {
       return new InterestClaimOption(InterestClaimOptionsType[claim.interestClaimOptions as keyof typeof InterestClaimOptionsType]);
     }
