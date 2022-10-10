7a6f0e93bdc5e5a3127d5237b986ed100ea18e1e
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-10-10 17:19:14 +0100

    CIV-4288: change from before each to before all

--- src/test/unit/views/features/public/claim/defendant-email.test.ts
+++ src/test/unit/views/features/public/claim/defendant-email.test.ts
@@ -18,7 +18,7 @@ describe('Claimant Defendant Email View', () => {
   describe('on GET', () => {
     let htmlDocument: Document;
     let mainWrapper: Element;
-    beforeEach(async () => {
+    beforeAll(async () => {
       nock(idamUrl)
         .post('/o/token')
         .reply(200, { id_token: citizenRoleToken });
