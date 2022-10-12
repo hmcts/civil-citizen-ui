36b0f733656c7aaa9be07a557f60bc13fb680606
OrtizP <pablo.gutierrez@version1.com>
2022-10-12 10:55:50 +0200

    Devs comments

--- src/test/unit/views/features/claim/yourDetails/claimant-organisation-details.test.ts
+++ src/test/unit/views/features/claim/yourDetails/claimant-organisation-details.test.ts
@@ -17,7 +17,7 @@ describe('Claimant Organisation Details View', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
 
-  beforeEach(() => {
+  beforeAll(() => {
     nock(idamUrl)
       .post('/o/token')
       .reply(200, {id_token: citizenRoleToken});
@@ -28,7 +28,7 @@ describe('Claimant Organisation Details View', () => {
     let htmlDocument: Document;
     let mainWrapper: Element;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
       const response = await request(app).get(CLAIMANT_ORGANISATION_DETAILS_URL);
       const dom = new JSDOM(response.text);
       htmlDocument = dom.window.document;
