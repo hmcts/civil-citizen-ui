45b1be253da335d38dcf46b315ba11ebf8a99f56
Kadir <kegilmez@gmail.com>
2022-10-13 12:07:53 +0100

    fixed the otherWitness issue

--- src/main/routes/features/directionsQuestionnaire/otherWitnessesController.ts
+++ src/main/routes/features/directionsQuestionnaire/otherWitnessesController.ts
@@ -11,7 +11,7 @@ import {
 
 const otherWitnessesController = Router();
 const otherWitnessesViewPath = 'features/directionsQuestionnaire/otherWitnesses/other-witnesses';
-const dqPropertyName = 'otherwitnesses';
+const dqPropertyName = 'otherWitnesses';
 const dqParentName = 'witnesses';
 
 function renderView(form: GenericForm<OtherWitnesses>, res: Response): void {
@@ -35,8 +35,7 @@ otherWitnessesController.post(DQ_DEFENDANT_WITNESSES_URL,
       if (form.hasErrors()) {
         renderView(form, res);
       } else {
-        const witnesses = { otherWitnesses: form.model };
-        await saveDirectionQuestionnaire(req.params.id,witnesses,dqPropertyName,dqParentName);
+        await saveDirectionQuestionnaire(req.params.id, form.model, dqPropertyName, dqParentName);
         res.redirect(constructResponseUrlWithIdParams(req.params.id, DQ_OTHER_WITNESSES_AVAILABILITY_DATES_FOR_HEARING_URL));
       }
     } catch (error) {
