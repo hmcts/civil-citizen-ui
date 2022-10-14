39977df64b6c244efe2b1aed1acbcfe13ce56ccf
commitBlob <maro.radovic12@gmail.com>
2022-10-14 12:59:58 +0100

    feat(CIV-4339): create total interest model

--- src/main/common/form/models/interest/interest.ts
+++ src/main/common/form/models/interest/interest.ts
@@ -1,7 +1,9 @@
 import {InterestStartDate} from './interestStartDate';
+import {TotalInterest} from './totalInterest';
 
 export class Interest {
   interestStartDate?: InterestStartDate;
+  totalInterest?: TotalInterest;
 
   constructor(interestStartDate?: InterestStartDate) {
     this.interestStartDate = interestStartDate;
