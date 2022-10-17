8f9d8aeaf05d2b674fa8275266a6e22faedb0f4d
OrtizP <pablo.gutierrez@version1.com>
2022-10-17 14:11:48 +0200

    Controller and URLs

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -144,6 +144,7 @@ import claimInterestController from './features/claim/interest/claimInterestCont
 import claimantInterestFromController from './features/claim/interest/claimantInterestFromController';
 import claimantOrganisationDetailsController from './features/claim/yourDetails/claimantOrganisationDetailsController';
 import defendantEmailController from './features/claim/yourDetails/defendantEmailController';
+import reasonController from './features/claim/details/reasonController';
 
 export default [
   homeController,
@@ -280,4 +281,5 @@ export default [
   interestStartDateController,
   claimantInterestFromController,
   defendantEmailController,
+  reasonController,
 ];
