4398a671d54abd6cda65890a7e28353eb56e06b7
OrtizP <pablo.gutierrez@version1.com>
2022-10-17 14:10:37 +0200

    Model

---
+++ src/main/common/form/models/claim/details/reason.ts
@@ -0,0 +1,10 @@
+import {IsNotEmpty} from 'class-validator';
+
+export class Reason {
+  @IsNotEmpty({message: 'ERRORS.REASON_REQUIRED'})
+  text?: string;
+
+  constructor(text?: string) {
+    this.text = text;
+  }
+}
