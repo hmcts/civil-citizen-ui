352f9764f5e281459227e8f48ada16cf5f91a44e
Kadir <kegilmez@gmail.com>
2022-10-18 14:35:44 +0100

    resolve the conflict

--- src/main/routes/features/directionsQuestionnaire/witnesses/otherWitnessesController.ts
+++ src/main/routes/features/directionsQuestionnaire/witnesses/otherWitnessesController.ts
@@ -13,7 +13,7 @@ import {
 
 const otherWitnessesController = Router();
 const otherWitnessesViewPath = 'features/directionsQuestionnaire/witnesses/otherWitnesses/other-witnesses';
-const dqPropertyName = 'otherwitnesses';
+const dqPropertyName = 'otherWitnesses';
 const dqParentName = 'witnesses';
 
 function renderView(form: GenericForm<OtherWitnesses>, res: Response): void {
@@ -37,8 +37,7 @@ otherWitnessesController.post(DQ_DEFENDANT_WITNESSES_URL,
       if (form.hasErrors()) {
         renderView(form, res);
       } else {
-        const witnesses = {otherWitnesses: form.model};
-        await saveDirectionQuestionnaire(req.params.id, witnesses, dqPropertyName, dqParentName);
+        await saveDirectionQuestionnaire(req.params.id, form.model, dqPropertyName, dqParentName);
         res.redirect(constructResponseUrlWithIdParams(req.params.id, DQ_OTHER_WITNESSES_AVAILABILITY_DATES_FOR_HEARING_URL));
       }
     } catch (error) {
