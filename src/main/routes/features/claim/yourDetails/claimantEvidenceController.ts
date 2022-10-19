de0269bb1299e86eabac7438e6bdc6389fe44adb
RosDiaf <ros_diaf@hotmail.com>
2022-10-19 16:29:25 +0100

    build form method added to model

--- src/main/routes/features/claim/yourDetails/claimantEvidenceController.ts
+++ src/main/routes/features/claim/yourDetails/claimantEvidenceController.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response, Router} from 'express';
-import {Evidence,INIT_ROW_COUNT} from '../../../../common/form/models/evidence/evidence';
+import {Evidence} from '../../../../common/form/models/evidence/evidence'; //buildEmptyForm
 import {
   getClaimDetails,
   saveClaimDetails,
@@ -23,7 +23,7 @@ function renderView(form: GenericForm<Evidence>, res: Response): void {
 evidenceController.get(CLAIM_EVIDENCE_URL, async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const claimDetails: ClaimDetails = await getClaimDetails(req.session.user?.id);
-    getEvidence(claimDetails);
+    Evidence.buildForm(claimDetails);
     renderView(new GenericForm<Evidence>(claimDetails.evidence), res);
   } catch (error) {
     next(error);
@@ -32,30 +32,19 @@ evidenceController.get(CLAIM_EVIDENCE_URL, async (req: AppRequest, res: Response
 
 evidenceController.post(CLAIM_EVIDENCE_URL, async (req: AppRequest | Request, res: Response, next: NextFunction) => {
   try {
-    let form = new GenericForm(new Evidence('', utilEvidence.transformToEvidences(req.body)));
+    const form = new GenericForm(new Evidence('', utilEvidence.transformToEvidences(req.body)));
     form.validateSync();
     if (form.hasErrors()) {
       renderView(form, res);
     } else {
       const appRequest = <AppRequest>req;
-      form = new GenericForm(new Evidence('', utilEvidence.removeEmptyValueToEvidences(req.body)));
-      await saveClaimDetails(appRequest.session.user?.id, form.model,'evidence');
+      form.model.evidenceItem = utilEvidence.removeEmptyValueToEvidences(req.body);
+      await saveClaimDetails(appRequest.session.user?.id, form.model, 'evidence');
       res.redirect(CLAIMANT_TASK_LIST_URL);
     }
   } catch (error) {
     next(error);
   }
 });
-
-const getEvidence = (claimDetails: ClaimDetails) => {
-  if (claimDetails.evidence) {
-    claimDetails.evidence = new Evidence(claimDetails.evidence.comment,claimDetails.evidence.evidenceItem);
-    if (claimDetails.evidence.evidenceItem.length < INIT_ROW_COUNT) {
-      claimDetails.evidence.setRows(INIT_ROW_COUNT - claimDetails.evidence.evidenceItem.length);
-    }
-  } else {
-    claimDetails.evidence = new Evidence();
-  }
-};
 
 export default evidenceController;
