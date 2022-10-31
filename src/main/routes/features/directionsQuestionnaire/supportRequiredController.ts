c2070311bb8c0e93fe757fce5efb0a50a7ab1e28
Kadir <kegilmez@gmail.com>
2022-10-31 23:11:22 +0000

    refactored the controller and service

--- src/main/routes/features/directionsQuestionnaire/supportRequiredController.ts
+++ src/main/routes/features/directionsQuestionnaire/supportRequiredController.ts
@@ -1,4 +1,4 @@
-import {NextFunction, Router} from 'express';
+import {Router, Response} from 'express';
 import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
 import {CLAIM_TASK_LIST_URL, SUPPORT_REQUIRED_URL} from '../../urls';
 import {GenericForm} from '../../../common/form/models/genericForm';
@@ -8,24 +8,31 @@ import {
   getSupportRequiredForm,
   generatePeopleListWithSelectedValues,
 } from '../../../services/features/directionsQuestionnaire/supportRequiredService';
+import {SupportRequiredList} from 'common/models/directionsQuestionnaire/supportRequired';
 
 const supportRequiredController = Router();
 const supportRequiredViewPath = 'features/directionsQuestionnaire/support-required-list';
 const dqPropertyName = 'supportRequiredList';
 const dqParentName = 'hearing';
 
-supportRequiredController.get(SUPPORT_REQUIRED_URL, async (req, res, next: NextFunction) => {
+async function renderView(form: GenericForm<SupportRequiredList>, claimId: string, lang: string, res: Response) {
+  const selectedNames = form.model?.items?.map(item => item.fullName);
+  const peopleLists = await generatePeopleListWithSelectedValues(claimId, selectedNames, lang);
+  res.render(supportRequiredViewPath, {form, peopleLists})
+}
+
+supportRequiredController.get(SUPPORT_REQUIRED_URL, async (req, res, next) => {
   try {
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-    const [supportRequiredList, peopleLists] = await getSupportRequired(req.params.id, lang);
+    const supportRequiredList = await getSupportRequired(req.params.id);
     const form = new GenericForm(supportRequiredList);
-    res.render(supportRequiredViewPath, {form, peopleLists});
+    renderView(form, req.params.id, lang, res)
   } catch (error) {
     next(error);
   }
 });
 
-supportRequiredController.post(SUPPORT_REQUIRED_URL, async (req, res, next: NextFunction) => {
+supportRequiredController.post(SUPPORT_REQUIRED_URL, async (req, res, next) => {
   try {
     const claimId = req.params.id;
     const supportRequiredList = getSupportRequiredForm(req);
@@ -33,9 +40,7 @@ supportRequiredController.post(SUPPORT_REQUIRED_URL, async (req, res, next: Next
     form.validateSync();
     if (form.hasErrors()) {
       const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-      const selectedNames = form.model?.items?.map(item => item.fullName);
-      const peopleLists = await generatePeopleListWithSelectedValues(claimId, selectedNames, lang);
-      res.render(supportRequiredViewPath, {form, peopleLists});
+      renderView(form, claimId, lang, res)
     } else {
       await saveDirectionQuestionnaire(claimId, form.model, dqPropertyName, dqParentName);
       res.redirect(constructResponseUrlWithIdParams(claimId, CLAIM_TASK_LIST_URL));
