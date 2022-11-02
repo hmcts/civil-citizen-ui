2e14cca014db1de07b1f325dedf64ddd05945a19
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-11-02 16:16:47 +0000

    CIV-5558: Set Up claimResponse Field

--- src/main/common/models/claim.ts
+++ src/main/common/models/claim.ts
@@ -40,6 +40,7 @@ import {InterestClaimOptionsType} from '../../common/form/models/claim/interest/
 import {Interest} from '../form/models/interest/interest';
 import {RejectAllOfClaimType} from '../../common/form/models/rejectAllOfClaimType';
 import {ClaimDetails} from '../../common/form/models/claim/details/claimDetails';
+import {ClaimantResponse} from './claimantResponse';
 
 export class Claim {
   legacyCaseReference: string;
@@ -81,6 +82,7 @@ export class Claim {
   respondentSolicitor1AgreedDeadlineExtension?: Date;
   directionQuestionnaire?: DirectionQuestionnaire;
   respondent1ResponseDate?: Date;
+  claimantResponse?: ClaimantResponse;
 
   get responseStatus(): ClaimResponseStatus {
     if (this.isFullAdmission() && this.isPaymentOptionPayImmediately()) {
