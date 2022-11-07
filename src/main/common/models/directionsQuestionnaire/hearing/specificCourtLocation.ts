430f5d1178d8a2b175549b3eadfd02ed48a79526
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-11-07 14:02:30 +0000

    CIV-4715 populate drop down

--- src/main/common/models/directionsQuestionnaire/hearing/specificCourtLocation.ts
+++ src/main/common/models/directionsQuestionnaire/hearing/specificCourtLocation.ts
@@ -1,14 +1,22 @@
 import {IsDefined, ValidateIf} from 'class-validator';
 import {YesNoUpperCase} from 'common/form/models/yesNo';
-import {CourtLocation} from 'models/courts/courtLocations';
 
 export class SpecificCourtLocation {
 
   @IsDefined({message: 'ERRORS.SPECIFIC_COURT.SELECT_YES_NO'})
     option: YesNoUpperCase;
-  @ValidateIf(o => {return o.isSpecificCourtRequired();})
+  @ValidateIf(o =>  o.isSpecificCourtRequired())
   @IsDefined({message: 'ERRORS.SPECIFIC_COURT.SELECT_COURT_LOCATION'})
-    courtLocation: CourtLocation;
+    courtLocation: string;
+  @ValidateIf(o => o.isSpecificCourtRequired())
+  @IsDefined({message: 'PAGES.SPECIFIC_COURT.REASON'})
+    reason: string;
+
+  constructor(option?: YesNoUpperCase, courtLocation?: string, reason?: string) {
+    this.option = option;
+    this.courtLocation = courtLocation;
+    this.reason = reason;
+  }
 
   isSpecificCourtRequired(){
     return this.option === YesNoUpperCase.YES;
