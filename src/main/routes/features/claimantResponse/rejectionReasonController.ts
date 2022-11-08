b7c49d88f726f8ec95feb05133dc65d5bd59ce60
neeta-hmcts <neeta.shinde@hmcts.net>
2022-11-08 12:08:40 +0000

    addressed review comments and modify view file.

--- src/main/routes/features/claimantResponse/rejectionReasonController.ts
+++ src/main/routes/features/claimantResponse/rejectionReasonController.ts
@@ -2,18 +2,21 @@ import {NextFunction, Router} from 'express';
 import {CLAIMANT_RESPONSE_REJECTION_REASON_URL, CLAIMANT_RESPONSE_TASK_LIST_URL} from '../../urls';
 import {GenericForm} from '../../../common/form/models/genericForm';
 import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
-import {
-  getRejectionReason,
-  saveRejectionReason,
-} from '../../../../main/services/features/claimantResponse/rejectionReasonService';
 import {RejectionReason} from '../../../common/form/models/claimantResponse/rejectionReason';
+import {
+  getClaimantResponse,
+  saveClaimantResponse,
+} from '../../../../main/services/features/claimantResponse/claimantResponseService';
 
 const claimantRejectionReasonPath = 'features/claimantResponse/rejection-reason';
 const rejectionReasonController = Router();
 
 rejectionReasonController.get(CLAIMANT_RESPONSE_REJECTION_REASON_URL, async (req, res, next: NextFunction) => {
   try {
-    res.render(claimantRejectionReasonPath, {form: new GenericForm(await getRejectionReason(req.params.id))});
+    const claimantResponse = await getClaimantResponse(req.params.id);
+    const rejectionReason = claimantResponse ?
+      claimantResponse.rejectionReason : new RejectionReason();
+    res.render(claimantRejectionReasonPath, {form: new GenericForm(rejectionReason)});
   } catch (error) {
     next(error);
   }
@@ -27,7 +30,7 @@ rejectionReasonController.post(CLAIMANT_RESPONSE_REJECTION_REASON_URL, async (re
     res.render(claimantRejectionReasonPath, {form});
   } else {
     try {
-      await saveRejectionReason(req.params.id, reason);
+      await saveClaimantResponse(req.params.id, reason, 'rejectionReason');
       res.redirect(constructResponseUrlWithIdParams(req.params.id, CLAIMANT_RESPONSE_TASK_LIST_URL));
     } catch (error) {
       next(error);
