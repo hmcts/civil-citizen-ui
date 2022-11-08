b630e8aa93cbeeb663572a1438e3c971ec859bed
commitBlob <maro.radovic12@gmail.com>
2022-11-08 15:54:16 +0000

    fix(A11y): use claim function

--- src/main/routes/features/mediation/freeTelephoneMediationController.ts
+++ src/main/routes/features/mediation/freeTelephoneMediationController.ts
@@ -1,15 +1,15 @@
 import {NextFunction, Router} from 'express';
 import {CITIZEN_FREE_TELEPHONE_MEDIATION_URL} from '../../urls';
-import {getDraftClaimFromStore} from '../../../modules/draft-store/draftStoreService';
+import {getCaseDataFromStore} from '../../../modules/draft-store/draftStoreService';
 
 const freeTelephoneMediationController = Router();
 const citizenFreeTelephoneMediationViewPath = 'features/mediation/free-telephone-mediation';
 
 freeTelephoneMediationController.get(CITIZEN_FREE_TELEPHONE_MEDIATION_URL, async (req, res, next: NextFunction) => {
   try {
-    const civilClaim = await getDraftClaimFromStore(req.params.id);
+    const civilClaim = await getCaseDataFromStore(req.params.id);
     res.render(citizenFreeTelephoneMediationViewPath,
-      {isBusiness: (civilClaim.case_data.respondent1.type === 'ORGANISATION' || civilClaim.case_data.respondent1.type === 'COMPANY')},
+      {isBusiness: civilClaim.isBusiness()},
     );
   } catch (error) {
     next(error);
