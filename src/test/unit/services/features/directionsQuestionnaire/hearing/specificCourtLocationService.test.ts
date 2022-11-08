31420b16923dc9348ed48bbfcf1ed4ae160c60ad
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-11-08 08:48:58 +0000

    CIV-4715 add test place holder for service

---
+++ src/test/unit/services/features/directionsQuestionnaire/hearing/specificCourtLocationService.test.ts
@@ -0,0 +1,7 @@
+describe('specific court location service test', ()=> {
+  describe('getListOfCourtLocations', ()=>{
+    it('should return cached court locations when data exists in cache', async ()=> {
+
+    });
+  });
+});
