f94431325fb26ae02aa8232f9e15f7e392a5d99f
Kadir <kegilmez@gmail.com>
2022-11-14 13:13:46 +0000

    change the line to prevent conflict

--- src/main/services/features/claim/amount/claimAmountBreakdownService.ts
+++ src/main/services/features/claim/amount/claimAmountBreakdownService.ts
@@ -23,8 +23,8 @@ export const getClaimAmountBreakdownForm = async (claimantId: string) : Promise<
 export const saveClaimAmountBreakdownForm = async (claimantId: string, amountBreakdown: AmountBreakdown) => {
   try{
     const claim = await getCaseDataFromStore(claimantId);
-    claim.claimAmountBreakup = amountBreakdown.getPopulatedRows().map((row)=> convertFormToJson(row));
     claim.totalClaimAmount = amountBreakdown.totalAmount;
+    claim.claimAmountBreakup = amountBreakdown.getPopulatedRows().map((row)=> convertFormToJson(row));
     await saveDraftClaim(claimantId, claim);
   }catch(error){
     logger.error(error);
