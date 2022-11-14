bda5b559fe8a03b3c0cb780768e67218c190f9fa
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2022-11-14 10:59:03 +0000

    forEach instead of for

--- src/main/services/features/response/checkAnswers/hearingRequirementSection/buildHearingRequirementSection.ts
+++ src/main/services/features/response/checkAnswers/hearingRequirementSection/buildHearingRequirementSection.ts
@@ -8,7 +8,7 @@ import {DQ_DEFENDANT_WITNESSES_URL} from '../../../../../routes/urls';
 import {OtherWitnessItems} from '../../../../../common/models/directionsQuestionnaire/witnesses/otherWitnessItems';
 import {YesNo} from '../../../../../common/form/models/yesNo';
 
-const changeLabel = (lang: string | unknown): string => t('COMMON.BUTTONS.CHANGE', {lng: getLng(lang)});
+const changeLabel = (lang: string): string => t('COMMON.BUTTONS.CHANGE', {lng: getLng(lang)});
 
 const getEmptyStringIfUndefined = (value: string): string => value || '';
 
@@ -16,16 +16,15 @@ const getWitnesses = (claim: Claim, claimId: string, lang: string | unknown): Su
   const witnessesHref = constructResponseUrlWithIdParams(claimId, DQ_DEFENDANT_WITNESSES_URL);
   const witnesses: OtherWitnessItems[] = claim.directionQuestionnaire.witnesses.otherWitnesses.witnessItems;
   const summaryRows: SummaryRow [] = [];
-  let index = 0;
 
-  for(const witness of witnesses) {
-    summaryRows.push(summaryRow(`${t('PAGES.CHECK_YOUR_ANSWER.WITNESS', {lng: getLng(lang)})} ${index += 1}`, '', witnessesHref, changeLabel(lang)));
+  witnesses.forEach((witness, index) => {
+    summaryRows.push(summaryRow(`${t('PAGES.CHECK_YOUR_ANSWER.WITNESS', {lng: getLng(lang)})} ${index + 1}`, '', witnessesHref, changeLabel(lang)));
     summaryRows.push(summaryRow(t('COMMON.INPUT_LABELS.FIRST_NAME', {lng: getLng(lang)}), getEmptyStringIfUndefined(witness.firstName)));
     summaryRows.push(summaryRow(t('COMMON.INPUT_LABELS.LAST_NAME', {lng: getLng(lang)}), getEmptyStringIfUndefined(witness.lastName)));
     summaryRows.push(summaryRow(t('PAGES.CHECK_YOUR_ANSWER.EMAIL_ADDRESS', {lng: getLng(lang)}), getEmptyStringIfUndefined(witness.email)));
     summaryRows.push(summaryRow(t('PAGES.CHECK_YOUR_ANSWER.PHONE_NUMBER', {lng: getLng(lang)}), getEmptyStringIfUndefined(witness.telephone)));
     summaryRows.push(summaryRow(t('PAGES.CHECK_YOUR_ANSWER.TELL_US_WHY', {lng: getLng(lang)}), getEmptyStringIfUndefined(witness.details)));
-  }
+  });
 
   return summaryRows;
 };
