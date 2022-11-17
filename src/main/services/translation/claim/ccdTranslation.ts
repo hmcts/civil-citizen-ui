f5c281666c060903c77ec3d1e4d0831b1e451eb1
Kadir <kegilmez@gmail.com>
2022-11-17 23:20:57 +0000

    fixed the return type

--- src/main/services/translation/claim/ccdTranslation.ts
+++ src/main/services/translation/claim/ccdTranslation.ts
@@ -1,7 +1,8 @@
+import {CCDClaim} from '../../../common/models/civilClaimResponse';
 import {Claim} from '../../../common/models/claim';
 import {toCCDParty} from '../response/convertToCCDParty';
 
-export const translateDraftClaimToCCD = (claim: Claim) => {
+export const translateDraftClaimToCCD = (claim: Claim): CCDClaim => {
   return {
     applicant1: toCCDParty(claim.applicant1),
     respondent1: toCCDParty(claim.respondent1),
