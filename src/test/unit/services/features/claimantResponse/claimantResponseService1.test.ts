97e247f7f2046502c4b3be4f15918531ea4e41e2
Sampan Kumar <sampan.kumar@hmcts.net>
2022-11-21 14:41:49 +0000

    trying few timeout issues

--- src/test/unit/services/features/claimantResponse/claimantResponseService1.test.ts
+++ src/test/unit/services/features/claimantResponse/claimantResponseService1.test.ts
@@ -5,10 +5,10 @@ import {
   getClaimantResponse,
 } from 'services/features/claimantResponse/claimantResponseService';
 
-jest.mock('main/modules/draft-store');
-jest.mock('main/modules/draft-store/draftStoreService');
-jest.mock('main/common/utils/languageToggleUtils');
-jest.mock('main/modules/i18n');
+jest.mock('../../../../../main/modules/draft-store');
+jest.mock('../../../../../main/modules/draft-store/draftStoreService');
+jest.mock('../../../../../main/common/utils/languageToggleUtils');
+jest.mock('../../../../../main/modules/i18n');
 jest.mock('i18next', () => ({
   t: (i: string | unknown) => i,
   use: jest.fn(),
