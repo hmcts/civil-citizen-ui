5d42a51aa209d417f57735589a1bee00d821e5e2
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-11-25 15:17:25 +0000

    CIV-5611 point to ccd definition and camunda on related branches

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+params="$1"
+branchName="$2"
+
+git clone https://github.com/hmcts/civil-ccd-definition.git
+cd civil-ccd-definition
+
+echo "Switch to ${branchName} branch on civil-ccd-definition"
+git checkout ${branchName}
 
 definition_input_dir=$(realpath 'ccd-definition')
 definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-civil-dev.xlsx"
