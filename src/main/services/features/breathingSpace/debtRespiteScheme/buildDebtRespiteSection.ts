18ca753e84c1350dc084948fc0d9d23eb39868a4
pascalkknde <Pascal.Kankonde@HMCTS.NET>
2022-12-01 16:43:15 +0000

    fixing 500 error

--- src/main/services/features/breathingSpace/debtRespiteScheme/buildDebtRespiteSection.ts
+++ src/main/services/features/breathingSpace/debtRespiteScheme/buildDebtRespiteSection.ts
@@ -21,15 +21,18 @@ export const buildDebtRespiteSection = (breathingSpace: BreathingSpace, claimId:
 
   const lng = getLng(lang);
   const referenceNumber = breathingSpace?.debtRespiteReferenceNumber?.referenceNumber ?? '';
-  const breathingType = 'PAGES.BREATHING_SPACE_DEBT_RESPITE_TYPE' + '.' + breathingSpace?.debtRespiteOption?.type;
+  const debtRespiteStartDate = (breathingSpace.debtRespiteStartDate)? formatDateToFullDate(breathingSpace?.debtRespiteStartDate?.date):''
+  const debtRespiteEndDate = (breathingSpace.debtRespiteEndDate)? formatDateToFullDate(breathingSpace?.debtRespiteEndDate?.date):''
+
+  const breathingType = (breathingSpace?.debtRespiteOption)?'PAGES.BREATHING_SPACE_DEBT_RESPITE_TYPE' + '.' + breathingSpace?.debtRespiteOption?.type:'';
 
   return summarySection({
     title: '',
     summaryRows: [
       summaryRow(t('PAGES.CLAIMANT_DEBT_RESPITE_CHECK_ANSWERS.REFERENCE_NUMBER', {lng}), referenceNumber, referenceNumberHref, changeLabel(lang)),
-      summaryRow(t('PAGES.CLAIMANT_DEBT_RESPITE_CHECK_ANSWERS.WHEN_DID_IT_START', {lng}), formatDateToFullDate(breathingSpace?.debtRespiteStartDate?.date), debtRespiteStartDateHref, changeLabel(lang)),
+      summaryRow(t('PAGES.CLAIMANT_DEBT_RESPITE_CHECK_ANSWERS.WHEN_DID_IT_START', {lng}), debtRespiteStartDate, debtRespiteStartDateHref, changeLabel(lang)),
       summaryRow(t('PAGES.CLAIMANT_DEBT_RESPITE_CHECK_ANSWERS.TYPE.WHAT_TYPE_IS_IT', {lng}), t(breathingType), debtRespiteOptionDateHref, changeLabel(lang)),
-      summaryRow(t('PAGES.CLAIMANT_DEBT_RESPITE_CHECK_ANSWERS.EXPECTED_END_DATE', {lng}), formatDateToFullDate(breathingSpace?.debtRespiteEndDate?.date), debtRespiteEndDateHref, changeLabel(lang)),
+      summaryRow(t('PAGES.CLAIMANT_DEBT_RESPITE_CHECK_ANSWERS.EXPECTED_END_DATE', {lng}), debtRespiteEndDate, debtRespiteEndDateHref, changeLabel(lang)),
     ],
   });
 };
