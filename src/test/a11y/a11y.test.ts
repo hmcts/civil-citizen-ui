8f8b0fe1968f2bedf90f97ad61e70a4a74e41798
Christopher Pateman <christopher.pateman@version1.com>
2022-12-01 10:57:35 +0000

    TEST - Reduce tests

--- src/test/a11y/a11y.test.ts
+++ src/test/a11y/a11y.test.ts
@@ -88,7 +88,7 @@ describe('Accessibility', () => {
   //const citizenRoleToken: string = config.get('citizenRoleToken');
   //const idamUrl: string = config.get('idamUrl');
 
-/*beforeEach(() => {
+  /*beforeEach(() => {
     nock(idamUrl)
       .post('/o/token')
       .reply(200, {id_token: citizenRoleToken});
