fcf1aa6afa2466b48a3eabe41e858d642e768436
pascalkknde <Pascal.Kankonde@HMCTS.NET>
2022-12-01 10:36:05 +0000

    Fixing indentation.

--- src/main/routes/features/claim/breathingSpace/checkAnswersController.ts
+++ src/main/routes/features/claim/breathingSpace/checkAnswersController.ts
@@ -10,30 +10,30 @@ const checkAnswersViewPath = 'features/breathingSpace/check-answers';
 const breathingSpaceCheckAnswersController = Router();
 
 function renderView(req: AppRequest, res: Response, breathingSpace: BreathingSpace, userId: string) {
-    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-    const summarySections = getSummarySections(userId, breathingSpace, lang);
-    res.render(checkAnswersViewPath, {summarySections});
+  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+  const summarySections = getSummarySections(userId, breathingSpace, lang);
+  res.render(checkAnswersViewPath, {summarySections});
 }
 
 breathingSpaceCheckAnswersController.get(BREATHING_SPACE_CHECK_ANSWERS_URL,
     async (req: AppRequest, res: Response, next: NextFunction) => {
-        try {
-            const claimId = req.params.id;
-            const breathingSpace = await getBreathingSpace(claimId);
-            renderView(req, res, breathingSpace, claimId);
-        } catch (error) {
-            next(error);
-        }
+      try {
+        const claimId = req.params.id;
+        const breathingSpace = await getBreathingSpace(claimId);
+        renderView(req, res, breathingSpace, claimId);
+      } catch (error) {
+        next(error);
+      }
     });
 
 breathingSpaceCheckAnswersController.post(BREATHING_SPACE_CHECK_ANSWERS_URL, async (req: AppRequest, res: Response, next: NextFunction) => {
-    try {
-        // TODO: submitResponse to be implemented
-        const userId = req.session?.user?.id;
-        res.redirect(constructResponseUrlWithIdParams(userId, DASHBOARD_CLAIMANT_URL));
-    } catch (error) {
-        next(error);
-    }
+  try {
+    // TODO: submitResponse to be implemented
+    const userId = req.session?.user?.id;
+    res.redirect(constructResponseUrlWithIdParams(userId, DASHBOARD_CLAIMANT_URL));
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default breathingSpaceCheckAnswersController;
