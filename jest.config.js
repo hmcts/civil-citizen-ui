957fa14dd4a0e960676c2587978e481438407ba9
Sampan Kumar <sampan.kumar@hmcts.net>
2022-12-04 23:18:43 +0000

    MAking it an array Ignoring some tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src/test/unit'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   testEnvironment: 'node',
-  testPathIgnorePatterns:'src/test/unit/app/client',
+  testPathIgnorePatterns:['src/test/unit/app/client'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
