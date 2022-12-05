9d2aede587def13f1f800e3e70d2592e29053746
Kadir <kegilmez@gmail.com>
2022-12-05 17:20:41 +0000

    updated the view

--- src/main/routes/features/claimantResponse/settleAdmittedController.ts
+++ src/main/routes/features/claimantResponse/settleAdmittedController.ts
@@ -1,50 +1,48 @@
 import {NextFunction, Request, Response, Router} from 'express';
 import {
-  CLAIMANT_RESPONSE_REJECTION_REASON_URL ,
-  CLAIMANT_RESPONSE_SETTLE_CLAIM_URL,
+  CLAIMANT_RESPONSE_SETTLE_ADMITTED_CLAIM_URL,
   CLAIMANT_RESPONSE_TASK_LIST_URL,
-} from '../../urls';
-import {GenericForm} from '../../../common/form/models/genericForm';
-import {GenericYesNo} from '../../../common/form/models/genericYesNo';
-import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
-import {Claim} from '../../../common/models/claim';
-import {getCaseDataFromStore} from '../../../modules/draft-store/draftStoreService';
-import {saveClaimantResponse} from '../../../services/features/claimantResponse/claimantResponseService';
-import {YesNo} from '../../../common/form/models/yesNo';
+} from 'routes/urls';
+import {Claim} from 'common/models/claim';
+
+import {GenericForm} from 'common/form/models/genericForm';
+import {GenericYesNo} from 'common/form/models/genericYesNo';
+import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
+import {saveClaimantResponse} from 'services/features/claimantResponse/claimantResponseService';
+import {currencyFormatWithNoTrailingZeros} from 'common/utils/currencyFormat';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 
 const settleAdmittedController = Router();
-const settleClaimViewPath = 'features/claimantResponse/settle-claim';
+const settleClaimViewPath = 'features/claimantResponse/settle-admitted';
 
-function renderView(form: GenericForm<GenericYesNo>, res: Response, admittedAmount: number): void {
-  res.render(settleClaimViewPath, {form, admittedAmount});
+async function renderView(form: GenericForm<GenericYesNo>, claimId: string, res: Response): Promise<void> {
+  const claim = await getCaseDataFromStore(claimId);
+  res.render(settleClaimViewPath, {
+    form,
+    totalAmount: currencyFormatWithNoTrailingZeros(claim.totalClaimAmount),
+    admittedAmount: claim.partialAdmissionPaymentAmount(),
+  });
 }
 
-let admittedAmount: number;
-
-settleAdmittedController.get(CLAIMANT_RESPONSE_SETTLE_CLAIM_URL, async (req: Request, res, next: NextFunction) => {
-  const claimId = req.params.id;
+settleAdmittedController.get(CLAIMANT_RESPONSE_SETTLE_ADMITTED_CLAIM_URL, async (req: Request, res, next: NextFunction) => {  const claimId = req.params.id;
   try {
     const claim: Claim = await getCaseDataFromStore(claimId);
-    renderView(new GenericForm(claim.claimantResponse?.hasPartAdmittedBeenAccepted), res, claim.partialAdmissionPaymentAmount());
+    await renderView(new GenericForm(claim.claimantResponse?.hasPartAdmittedBeenAccepted), claimId, res);
   } catch (error) {
     next(error);
   }
 });
 
-settleAdmittedController.post(CLAIMANT_RESPONSE_SETTLE_CLAIM_URL, async (req: Request, res, next: NextFunction) => {
+settleAdmittedController.post(CLAIMANT_RESPONSE_SETTLE_ADMITTED_CLAIM_URL, async (req: Request, res, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const form = new GenericForm(new GenericYesNo(req.body.option, 'ERRORS.VALID_YES_NO_SELECTION'));
     form.validateSync();
     if (form.hasErrors()) {
-      renderView(form, res, admittedAmount);
+      await renderView(form, claimId, res);
     } else {
       await saveClaimantResponse(claimId, form.model, 'hasPartAdmittedBeenAccepted');
-      if (form.model.option === YesNo.YES) {
-        res.redirect(constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_TASK_LIST_URL));
-      } else {
-        res.redirect(constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_REJECTION_REASON_URL ));
-      }
+      res.redirect(constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_TASK_LIST_URL));
     }
   } catch (error) {
     next(error);
