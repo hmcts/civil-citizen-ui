2736e3a1724e1cf5fba7f5423b19696e964d2b64
neeta-hmcts <neeta.shinde@hmcts.net>
2022-12-13 09:46:17 +0000

    recreated PR

--- src/main/routes/features/claimantResponse/ccj/judgmentAmountSummaryController.ts
+++ src/main/routes/features/claimantResponse/ccj/judgmentAmountSummaryController.ts
@@ -15,6 +15,7 @@ const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServi
 async function renderView(req: AppRequest, res: Response, claim: Claim, lang: string) {
   const claimFee = await civilServiceClient.getClaimAmountFee(claim?.totalClaimAmount, req);
   const judgmentSummaryDetails = getJudgmentAmountSummary(claim, claimFee, lang);
+
   res.render(judgementAmountSummaryViewPath, {
     claimAmount: claim.totalClaimAmount,
     claimFee,
