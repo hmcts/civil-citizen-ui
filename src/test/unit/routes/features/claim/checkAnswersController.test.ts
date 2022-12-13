cbc397c3b948e009aab332c8597ce9403b35896c
Felipe <felipe@LAPTOP3721.V1.com>
2022-12-13 11:29:19 +0100

    Fix: test merge master

--- src/test/unit/routes/features/claim/checkAnswersController.test.ts
+++ src/test/unit/routes/features/claim/checkAnswersController.test.ts
@@ -11,7 +11,11 @@ import {Claim} from '../../../../../main/common/models/claim';
 import {ClaimDetails} from '../../../../../main/common/form/models/claim/details/claimDetails';
 import {HelpWithFees} from '../../../../../main/common/form/models/claim/details/helpWithFees';
 
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+const request = require('supertest');
 const {app} = require('../../../../../main/app');
+const session = require('supertest-session');
 const civilServiceUrl = config.get<string>('services.civilService.url');
 const data = require('../../../../utils/mocks/defendantClaimsMock.json');
 
@@ -114,7 +118,7 @@ describe('Response - Check answers', () => {
     it('should pass english translation via query', async () => {
       await session(app).get(CLAIM_CHECK_ANSWERS_URL)
         .query({lang: 'en'})
-        .expect((res) => {
+        .expect((res: Response) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain(checkYourAnswerEng);
         });
@@ -126,7 +130,7 @@ describe('Response - Check answers', () => {
       });
       await request(app)
         .get(CLAIM_CHECK_ANSWERS_URL)
-        .expect((res) => {
+        .expect((res: Response) => {
           expect(res.status).toBe(500);
           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
         });
@@ -242,18 +246,192 @@ describe('Response - Check answers', () => {
         });
     });
 
+    // it('should return 500 when error in service', async () => {
+    //   mockGetSummarySections.mockImplementation(() => {
+    //     throw new Error(TestMessages.REDIS_FAILURE);
+    //   });
+    //   await request(app)
+    //     .post(CLAIM_CHECK_ANSWERS_URL)
+    //     .send(data)
+    //     .expect((res: Response) => {
+    //       expect(res.status).toBe(302);
+    //       expect(res.).toBe(CLAIM_CONFIRMATION_URL);
+    //     });
+    // });
     it('should return 500 when error in service', async () => {
       mockGetSummarySections.mockImplementation(() => {
         throw new Error(TestMessages.REDIS_FAILURE);
       });
       await request(app)
         .post(CLAIM_CHECK_ANSWERS_URL)
-        .send(data)
-        .expect((res ) => {
-          expect(res.status).toBe(302);
-          expect(res.header.location).toBe(CLAIM_CONFIRMATION_URL);
+        .send()
+        .expect((res: Response) => {
+          expect(res.status).toBe(500);
+          expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
         });
     });
   });
 });
 
+
+
+// import nock from 'nock';
+// import config from 'config';
+// import {getSummarySections} from '../../../../../main/services/features/claim/checkAnswers/checkAnswersService';
+// import {CLAIM_CHECK_ANSWERS_URL} from '../../../../../main/routes/urls';
+// import {TestMessages} from '../../../../utils/errorMessageTestConstants';
+// import {getElementsByXPath} from '../../../../utils/xpathExtractor';
+// import {createClaimWithBasicDetails} from '../../../../utils/mocks/claimDetailsMock';
+
+// const jsdom = require('jsdom');
+// const {JSDOM} = jsdom;
+
+// const request = require('supertest');
+// const {app} = require('../../../../../main/app');
+// const session = require('supertest-session');
+// const civilServiceUrl = config.get<string>('services.civilService.url');
+// const data = require('../../../../utils/mocks/defendantClaimsMock.json');
+
+// jest.mock('../../../../../main/modules/oidc');
+// jest.mock('../../../../../main/modules/claimDetailsService');
+// jest.mock('../../../../../main/services/features/claim/checkAnswers/checkAnswersService');
+
+// const mockGetSummarySections = getSummarySections as jest.Mock;
+// const PARTY_NAME = 'Mrs. Mary Richards';
+
+// describe('Response - Check answers', () => {
+//   const citizenRoleToken: string = config.get('citizenRoleToken');
+//   const idamServiceUrl: string = config.get('services.idam.url');
+//   const checkYourAnswerEng = 'Check your answers';
+//   const checkYourAnswerCy = 'Gwiriwch eich ateb';
+
+//   beforeAll(() => {
+//     nock(idamServiceUrl)
+//       .post('/o/token')
+//       .reply(200, {id_token: citizenRoleToken});
+//     nock(civilServiceUrl)
+//       .get('/cases/defendant/123')
+//       .reply(200, {data: data});
+//     nock(civilServiceUrl)
+//       .get('/cases/claimant/123')
+//       .reply(200, {data: data});
+//   });
+
+//   describe('on GET', () => {
+
+//     it('should return check answers page', async () => {
+//       mockGetSummarySections.mockImplementation(() => {
+//         return createClaimWithBasicDetails();
+//       });
+
+//       const response = await session(app).get(CLAIM_CHECK_ANSWERS_URL);
+//       expect(response.status).toBe(200);
+
+//       const dom = new JSDOM(response.text);
+//       const htmlDocument = dom.window.document;
+//       const header = getElementsByXPath("//h1[@class='govuk-heading-l']", htmlDocument);
+
+//       expect(header.length).toBe(1);
+//       expect(header[0].textContent).toBe(checkYourAnswerEng);
+
+//     });
+//     it('should return check answers page with Your details and their details sections', async () => {
+//       mockGetSummarySections.mockImplementation(() => {
+//         return createClaimWithBasicDetails();
+//       });
+
+//       const response = await session(app).get(CLAIM_CHECK_ANSWERS_URL);
+//       expect(response.status).toBe(200);
+
+//       const dom = new JSDOM(response.text);
+//       const htmlDocument = dom.window.document;
+//       const header = getElementsByXPath("//h1[@class='govuk-heading-l']", htmlDocument);
+//       const fullName = getElementsByXPath(
+//         "//dd[@class='govuk-summary-list__value' and preceding-sibling::dt[contains(text(),'Full name')]]",
+//         htmlDocument);
+//       const address = getElementsByXPath(
+//         "//dd[@class='govuk-summary-list__value' and preceding-sibling::dt[contains(text(),'Address')]]",
+//         htmlDocument);
+//       const correspondence = getElementsByXPath(
+//         "//dd[@class='govuk-summary-list__value' and preceding-sibling::dt[contains(text(),'Correspondence address')]]",
+//         htmlDocument);
+//       const contact = getElementsByXPath(
+//         "//dd[@class='govuk-summary-list__value' and preceding-sibling::dt[contains(text(),'Contact number (optional)')]]",
+//         htmlDocument);
+//       const email = getElementsByXPath(
+//         "//dd[@class='govuk-summary-list__value' and preceding-sibling::dt[contains(text(),'Email')]]",
+//         htmlDocument);
+
+//       expect(header.length).toBe(1);
+//       expect(header[0].textContent).toBe(checkYourAnswerEng);
+//       expect(fullName.length).toBe(2);
+//       expect(fullName[0].textContent?.trim()).toBe(PARTY_NAME);
+//       expect(fullName[1].textContent?.trim()).toBe(PARTY_NAME);
+//       expect(address.length).toBe(2);
+//       expect(address[0].textContent?.trim()).toBe('54 avenue');
+//       expect(address[1].textContent?.trim()).toBe('Simon street');
+//       expect(correspondence.length).toBe(1);
+//       expect(correspondence[0].textContent?.trim()).toBe('Same as address');
+//       expect(contact.length).toBe(2);
+//       expect(contact[0].textContent?.trim()).toBe('12345');
+//       expect(contact[1].textContent?.trim()).toBe('98765');
+//       expect(email.length).toBe(1);
+//       expect(email[0].textContent?.trim()).toBe('contact@gmail.com');
+//     });
+
+//     it('should pass english translation via query', async () => {
+//       await session(app).get(CLAIM_CHECK_ANSWERS_URL)
+//         .query({lang: 'en'})
+//         .expect((res: Response) => {
+//           expect(res.status).toBe(200);
+//           expect(res.text).toContain(checkYourAnswerEng);
+//         });
+//     });
+//     it('should pass cy translation via query', async () => {
+//       await session(app).get(CLAIM_CHECK_ANSWERS_URL)
+//         .query({lang: 'cy'})
+//         .expect((res: Response) => {
+//           expect(res.status).toBe(200);
+//           expect(res.text).toContain(checkYourAnswerCy);
+//         });
+//     });
+
+//     it('should return status 500 when error thrown', async () => {
+//       mockGetSummarySections.mockImplementation(() => {
+//         throw new Error(TestMessages.REDIS_FAILURE);
+//       });
+//       await session(app)
+//         .get(CLAIM_CHECK_ANSWERS_URL)
+//         .expect((res: Response) => {
+//           expect(res.status).toBe(500);
+//           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+//         });
+//     });
+//   });
+//   describe('on Post', () => {
+//     it('should return 500 when error in service', async () => {
+//       mockGetSummarySections.mockImplementation(() => {
+//         throw new Error(TestMessages.REDIS_FAILURE);
+//       });
+//       await request(app)
+//         .post(CLAIM_CHECK_ANSWERS_URL)
+//         .send()
+//         .expect((res: Response) => {
+//           expect(res.status).toBe(500);
+//           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+//         });
+//     });
+
+//     // it('should return http 500 when has error in the post method', async () => {
+//     //   app.locals.draftStoreClient = mockRedisFailure;
+//     //   await request(app)
+//     //     .post(CLAIMANT_DOB_URL)
+//     //     .send({day: 4, month: 5, year: 1952})
+//     //     .expect((res) => {
+//     //       expect(res.status).toBe(500);
+//     //       expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+//     //     });
+//     // });
+    
+//   });
+// });
