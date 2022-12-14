2e8eecaf3619e1ddb2e54bcb701180ed3bfb36e8
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2022-12-14 12:03:11 +0000

    fix sonar coverage

--- src/test/unit/routes/features/claim/claimSubmittedController.test.ts
+++ src/test/unit/routes/features/claim/claimSubmittedController.test.ts
@@ -14,6 +14,13 @@ describe('Claim - Claim Submitted', () => {
   const idamServiceUrl: string = config.get('services.idam.url');
   const citizenRoleToken: string = config.get('citizenRoleToken');
 
+  // const claimId = '1645882162449409';
+  const claimId = '1111111111';
+
+  nock('http://localhost:3000')
+    .get(CLAIM_CONFIRMATION_URL.replace(':id', claimId))
+    .reply(200, {id: claimId});
+
   beforeAll(() => {
     nock(idamServiceUrl)
       .post('/o/token')
@@ -27,9 +34,34 @@ describe('Claim - Claim Submitted', () => {
       await request(app)
         .get(CLAIM_CONFIRMATION_URL)
         .expect((res) => {
-          console.log(res.text);
           expect(res.status).toBe(200);
           expect(res.text).toContain('Claim submitted');
+        });
+    });
+
+    it('should contain proper claim ID', async () => {
+      app.locals.draftStoreClient = mockCivilClaim;
+
+      await request(app)
+        .get(CLAIM_CONFIRMATION_URL.replace(':id', claimId))
+        .expect(res => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Claim submitted');
+          expect(res.text).toContain(claimId);
+        });
+    });
+
+    it('should contain help with fees info', async () => {
+      app.locals.draftStoreClient = mockCivilClaim;
+
+      const text = 'Your claim will be issued once your Help With Fees application has been confirmed.';
+      await request(app)
+        .get(CLAIM_CONFIRMATION_URL.replace(':id', claimId))
+        .expect(res => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Claim submitted');
+          expect(res.text).toContain(text);
+
         });
     });
   });
