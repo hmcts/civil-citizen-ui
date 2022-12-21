9402af8b43b7edc5cc3e59308b02f2d4f3031ccb
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-12-21 10:29:55 +0000

    Update Check Style

--- src/test/unit/services/translation/claim/ccdTransaltion.test.ts
+++ src/test/unit/services/translation/claim/ccdTransaltion.test.ts
@@ -2,7 +2,7 @@ import {Claim} from 'common/models/claim';
 import {translateDraftClaimToCCD} from 'services/translation/claim/ccdTranslation';
 import {Party} from 'common/models/party';
 import {YesNoUpperCamelCase} from 'common/form/models/yesNo';
-import {PartyType} from "models/partyType";
+import {PartyType} from 'models/partyType';
 
 describe('translate draft claim to ccd version', () => {
   it('should translate applicant1 to ccd', () => {
@@ -19,13 +19,13 @@ describe('translate draft claim to ccd version', () => {
     claim.applicant1 = new Party();
     claim.applicant1.type = PartyType.COMPANY;
     claim.applicant1.partyDetails = {
-      partyName: "test"
+      partyName: 'test',
     };
     //When
     const ccdClaim = translateDraftClaimToCCD(claim);
     //Then
     expect(ccdClaim.applicant1).not.toBeUndefined();
-    expect(ccdClaim.applicant1?.companyName).toBe("test");
+    expect(ccdClaim.applicant1?.companyName).toBe('test');
   });
   it('should translate respondent 1 to ccd', () => {
     //Given
@@ -33,13 +33,13 @@ describe('translate draft claim to ccd version', () => {
     claim.respondent1 = new Party();
     claim.respondent1.type = PartyType.COMPANY;
     claim.respondent1.partyDetails = {
-      partyName: "test"
+      partyName: 'test',
     };
     //When
     const ccdClaim = translateDraftClaimToCCD(claim);
     //Then
     expect(ccdClaim.respondent1).not.toBeUndefined();
-    expect(ccdClaim.respondent1?.companyName).toBe("test");
+    expect(ccdClaim.respondent1?.companyName).toBe('test');
   });
 
 });
