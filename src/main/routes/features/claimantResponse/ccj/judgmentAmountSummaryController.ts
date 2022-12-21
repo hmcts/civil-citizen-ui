56cbb4c490f24503f5c7fef4c87ecefa54b29adb
OrtizP <pablo.gutierrez@version1.com>
2022-12-21 09:19:52 +0100

    Controller and service changes for judgment amount summary

--- src/main/routes/features/claimantResponse/ccj/judgmentAmountSummaryController.ts
+++ src/main/routes/features/claimantResponse/ccj/judgmentAmountSummaryController.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Response, Router} from 'express';
-import {CCJ_PAID_AMOUNT_SUMMARY_URL} from '../../../urls';
+import {CCJ_EXTENDED_PAID_AMOUNT_SUMMARY_URL, CCJ_PAID_AMOUNT_SUMMARY_URL} from '../../../urls';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {AppRequest} from 'models/AppRequest';
 import {CivilServiceClient} from 'client/civilServiceClient';
@@ -14,7 +14,7 @@ const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServi
 
 async function renderView(req: AppRequest, res: Response, claim: Claim, lang: string) {
   const claimFee = await civilServiceClient.getClaimAmountFee(claim?.totalClaimAmount, req);
-  const judgmentSummaryDetails = getJudgmentAmountSummary(claim, claimFee, lang);
+  const judgmentSummaryDetails = getJudgmentAmountSummary(claim, claimFee, lang, req.url);
   res.render(judgementAmountSummaryViewPath, {
     claimAmount: claim.totalClaimAmount,
     claimFee,
@@ -22,7 +22,7 @@ async function renderView(req: AppRequest, res: Response, claim: Claim, lang: st
   });
 }
 
-judgmentAmountSummaryController.get(CCJ_PAID_AMOUNT_SUMMARY_URL, async (req: AppRequest, res: Response, next: NextFunction) => {
+judgmentAmountSummaryController.get([CCJ_PAID_AMOUNT_SUMMARY_URL,CCJ_EXTENDED_PAID_AMOUNT_SUMMARY_URL], async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const claim = await getCaseDataFromStore(req.params.id);
