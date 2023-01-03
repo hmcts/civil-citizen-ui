093f81fc8b841285882d008cb14c22e51574d526
Kadir <kegilmez@gmail.com>
2023-01-03 14:30:25 +0000

    updated the directionQuestionnaire service

--- src/main/services/features/directionsQuestionnaire/directionQuestionnaireService.ts
+++ src/main/services/features/directionsQuestionnaire/directionQuestionnaireService.ts
@@ -2,6 +2,7 @@ import {getCaseDataFromStore, saveDraftClaim} from 'modules/draft-store/draftSto
 import {DirectionQuestionnaire} from 'models/directionsQuestionnaire/directionQuestionnaire';
 import {GenericYesNo} from 'form/models/genericYesNo';
 import {DirectionQuestionnaireErrorMessages} from 'form/models/directionQuestionnaireErrorMessages';
+import {ClaimantResponse} from 'common/models/claimantResponse';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('directionQuestionnaireService');
@@ -9,6 +10,9 @@ const logger = Logger.getLogger('directionQuestionnaireService');
 const getDirectionQuestionnaire = async (claimId: string): Promise<DirectionQuestionnaire> => {
   try {
     const claim = await getCaseDataFromStore(claimId);
+    if (claim.isClaimantIntentionPending()) {
+      return claim.claimantResponse?.directionQuestionnaire ? claim.claimantResponse.directionQuestionnaire : new DirectionQuestionnaire();
+    }
     return (claim?.directionQuestionnaire) ? claim.directionQuestionnaire : new DirectionQuestionnaire();
   } catch (error) {
     logger.error(error);
@@ -40,7 +44,29 @@ const getGenericOptionForm = (option: string, propertyName: string): GenericYesN
 const saveDirectionQuestionnaire = async (claimId: string, value: any, directionQuestionnairePropertyName: string, parentPropertyName?: string): Promise<void> => {
   try {
     const claim: any = await getCaseDataFromStore(claimId);
-    if (claim.directionQuestionnaire) {
+
+    if (claim.isClaimantIntentionPending()) {
+      if (claim.claimantResponse?.directionQuestionnaire) {
+        if (parentPropertyName && claim.claimantResponse.directionQuestionnaire[parentPropertyName]) {
+          claim.claimantResponse.directionQuestionnaire[parentPropertyName][directionQuestionnairePropertyName] = value;
+        } else if (parentPropertyName && !claim.directionQuestionnaire[parentPropertyName]) {
+          claim.claimantResponse.directionQuestionnaire[parentPropertyName] = {[directionQuestionnairePropertyName]: value};
+        } else {
+          claim.claimantResponse.directionQuestionnaire[directionQuestionnairePropertyName] = value;
+        }
+      } else {
+        const directionQuestionnaire: any = new DirectionQuestionnaire();
+        if (parentPropertyName) {
+          directionQuestionnaire[parentPropertyName] = {[directionQuestionnairePropertyName]: value};
+        } else {
+          directionQuestionnaire[directionQuestionnairePropertyName] = value;
+        }
+        claim.claimantResponse = claim?.claimantResponse ? claim.claimantResponse : new ClaimantResponse();
+        claim.claimantResponse.directionQuestionnaire = directionQuestionnaire;
+      }
+    }
+
+    if (claim.directionQuestionnaire && claim.isDefendantNotResponded()) {
       if (parentPropertyName && claim.directionQuestionnaire[parentPropertyName]) {
         claim.directionQuestionnaire[parentPropertyName][directionQuestionnairePropertyName] = value;
       } else if (parentPropertyName && !claim.directionQuestionnaire[parentPropertyName]) {
@@ -48,7 +74,7 @@ const saveDirectionQuestionnaire = async (claimId: string, value: any, direction
       } else {
         claim.directionQuestionnaire[directionQuestionnairePropertyName] = value;
       }
-    } else {
+    } else if (claim.isDefendantNotResponded()) {
       const directionQuestionnaire: any = new DirectionQuestionnaire();
       if (parentPropertyName) {
         directionQuestionnaire[parentPropertyName] = {[directionQuestionnairePropertyName]: value};
