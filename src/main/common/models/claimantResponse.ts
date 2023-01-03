0f35b980e3ef5a342d123e1a0351fcaaa1fcf35f
Kadir <kegilmez@gmail.com>
2023-01-03 14:23:39 +0000

    updated the model to allow saving for claimant

--- src/main/common/models/claimantResponse.ts
+++ src/main/common/models/claimantResponse.ts
@@ -2,6 +2,7 @@ import {CitizenDate} from '../form/models/claim/claimant/citizenDate';
 import {GenericYesNo} from '../form/models/genericYesNo';
 import {CCJRequest} from './claimantResponse/ccj/ccjRequest';
 import {RejectionReason} from '../form/models/claimantResponse/rejectionReason';
+import {DirectionQuestionnaire} from './directionsQuestionnaire/directionQuestionnaire';
 
 export class ClaimantResponse {
   hasDefendantPaidYou?: GenericYesNo;
@@ -12,4 +13,5 @@ export class ClaimantResponse {
   intentionToProceed?: GenericYesNo;
   rejectionReason?: RejectionReason;
   chooseHowToProceed?: GenericYesNo;
+  directionQuestionnaire?: DirectionQuestionnaire;
 }
