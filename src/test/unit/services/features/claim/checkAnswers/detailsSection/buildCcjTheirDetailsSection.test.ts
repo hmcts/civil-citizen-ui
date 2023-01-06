9886a6800099c274ebf8dbdc7313c2720a52794b
KyleMulvihillV1 <kyle.mulvihill@hmcts.net>
2023-01-06 13:45:45 +0000

    CIV-5681 remove constVal from unit tests

--- src/test/unit/services/features/claim/checkAnswers/detailsSection/buildCcjTheirDetailsSection.test.ts
+++ src/test/unit/services/features/claim/checkAnswers/detailsSection/buildCcjTheirDetailsSection.test.ts
@@ -2,7 +2,6 @@ import {
   createClaimWithBasicRespondentDetails,
   createClaimWithIndividualDetails,
 } from '../../../../../../utils/mockClaimForCheckAnswers';
-import * as constVal from '../../../../../../utils/checkAnswersConstants';
 import {Email} from 'models/Email';
 import {getSummarySections} from 'services/features/claimantResponse/ccj/ccjCheckAnswersService';
 
@@ -18,20 +17,21 @@ const LAST_NAME = 'Richards';
 const FULL_NAME = `${TITLE} ${FIRST_NAME} ${LAST_NAME}`;
 const EMAIL_ADDRESS = 'contact@gmail.com';
 const ADDRESS = '23 Brook lane<br>Bristol<br>BS13SS';
+const CLAIM_ID = 'claimId';
 
 describe('Citizen Details Section', () => {
   const claim = createClaimWithBasicRespondentDetails();
   it('should return your Individual details summary sections', async () => {
     //When
-    const summarySections = await getSummarySections(constVal.CLAIM_ID, claim, 'cimode');
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'cimode');
     //Then
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].title).toBe('PAGES.CHECK_YOUR_ANSWER.THEIR_DETAILS_TITLE_DEFENDANT');
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows.length).toBe(3);
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[0].value.html).toBe(FULL_NAME);
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[0].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.FULL_NAME');
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[1].value.html).toBe(ADDRESS);
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[2].value.html).toBe(EMAIL_ADDRESS);
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[2].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.EMAIL');
+    expect(summarySections.sections[0].title).toBe('PAGES.CHECK_YOUR_ANSWER.THEIR_DETAILS_TITLE_DEFENDANT');
+    expect(summarySections.sections[0].summaryList.rows.length).toBe(3);
+    expect(summarySections.sections[0].summaryList.rows[0].value.html).toBe(FULL_NAME);
+    expect(summarySections.sections[0].summaryList.rows[0].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.FULL_NAME');
+    expect(summarySections.sections[0].summaryList.rows[1].value.html).toBe(ADDRESS);
+    expect(summarySections.sections[0].summaryList.rows[2].value.html).toBe(EMAIL_ADDRESS);
+    expect(summarySections.sections[0].summaryList.rows[2].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.EMAIL');
   });
 
   it('should return address when it exists', async () => {
@@ -39,18 +39,18 @@ describe('Citizen Details Section', () => {
     const claim = createClaimWithIndividualDetails();
     const address = '24 Brook lane<br>Bristol<br>BS13SS';
     //When
-    const summarySections = await getSummarySections(constVal.CLAIM_ID, claim, 'en');
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'en');
     //Then
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[1].value.html).toBe(address);
+    expect(summarySections.sections[0].summaryList.rows[1].value.html).toBe(address);
   });
 
   it('should return full name of a person when full name is present', async () => {
     //Given
     const claim = createClaimWithIndividualDetails();
     //When
-    const summarySections = await getSummarySections(constVal.CLAIM_ID, claim, 'en');
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'en');
     //Then
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[0].value.html).toBe(FULL_NAME);
+    expect(summarySections.sections[0].summaryList.rows[0].value.html).toBe(FULL_NAME);
   });
 
   it('should return email when it exists', async () => {
@@ -60,8 +60,8 @@ describe('Citizen Details Section', () => {
       claim.respondent1.emailAddress = new Email(EMAIL_ADDRESS);
     }
     //When
-    const summarySections = await getSummarySections(constVal.CLAIM_ID, claim, 'en');
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'en');
     //Then
-    expect(summarySections.sections[constVal.INDEX_CCJ_THEIR_DETAILS_SECTION].summaryList.rows[2].value.html).toBe(EMAIL_ADDRESS);
+    expect(summarySections.sections[0].summaryList.rows[2].value.html).toBe(EMAIL_ADDRESS);
   });
 });
--- src/test/unit/services/features/claim/checkAnswers/financialSection/buildPaymentSection.test.ts
+++ src/test/unit/services/features/claim/checkAnswers/financialSection/buildPaymentSection.test.ts
@@ -3,7 +3,6 @@ import {
   createCCJClaimWithClaimResponseDetailsForPayBySetDate,
 } from '../../../../../../utils/mockClaimForCheckAnswers';
 import {getSummarySections} from 'services/features/claimantResponse/ccj/ccjCheckAnswersService';
-import * as constVal from '../../../../../../utils/checkAnswersConstants';
 import {
   CCJ_PAID_AMOUNT_URL,
 } from 'routes/urls';
@@ -21,43 +20,44 @@ const AMOUNT_ALREADY_PAID = '£200';
 const AMOUNT_TO_BE_PAID = '£800';
 const FIRST_PAYMENT_DATE = '6 June 2023';
 const INSTALMENTS_OF = '£200';
+const CLAIM_ID = 'claimId';
 
 describe('Payment Section', () => {
 
   it('should return your Payment summary sections for pay by set date', async () => {
     //When
     const claim = createCCJClaimWithClaimResponseDetailsForPayBySetDate();
-    const summarySections = await getSummarySections(constVal.CLAIM_ID, claim, 'cimode');
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'cimode');
     //Then
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].title).toBe('PAGES.CHECK_YOUR_ANSWER.PAYMENT_TITLE');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows.length).toBe(5);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[0].value.html).toBe(YES_OPTION);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[0].actions?.items.length).toBe(1);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[0].actions?.items[0].href).toBe(constructResponseUrlWithIdParams(constVal.CLAIM_ID, CCJ_PAID_AMOUNT_URL));
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[0].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_HAS_DEFENDANT_PAID_SOME');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[1].value.html).toBe(AMOUNT_ALREADY_PAID);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[1].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_AMOUNT_ALREADY_PAID');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[2].value.html).toBe(AMOUNT_TO_BE_PAID);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[2].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_TOTAL_TO_BE_PAID');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[3].value.html).toBe('COMMON.PAYMENT_OPTION.BY_SET_DATE');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[3].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_HOW_TO_PAY');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[4].value.html).toBe(DATE);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[4].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_WHEN_DO_YOU_WANT_TO_BE_PAID_BY');
+    expect(summarySections.sections[1].title).toBe('PAGES.CHECK_YOUR_ANSWER.PAYMENT_TITLE');
+    expect(summarySections.sections[1].summaryList.rows.length).toBe(5);
+    expect(summarySections.sections[1].summaryList.rows[0].value.html).toBe(YES_OPTION);
+    expect(summarySections.sections[1].summaryList.rows[0].actions?.items.length).toBe(1);
+    expect(summarySections.sections[1].summaryList.rows[0].actions?.items[0].href).toBe(constructResponseUrlWithIdParams(CLAIM_ID, CCJ_PAID_AMOUNT_URL));
+    expect(summarySections.sections[1].summaryList.rows[0].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_HAS_DEFENDANT_PAID_SOME');
+    expect(summarySections.sections[1].summaryList.rows[1].value.html).toBe(AMOUNT_ALREADY_PAID);
+    expect(summarySections.sections[1].summaryList.rows[1].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_AMOUNT_ALREADY_PAID');
+    expect(summarySections.sections[1].summaryList.rows[2].value.html).toBe(AMOUNT_TO_BE_PAID);
+    expect(summarySections.sections[1].summaryList.rows[2].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_TOTAL_TO_BE_PAID');
+    expect(summarySections.sections[1].summaryList.rows[3].value.html).toBe('COMMON.PAYMENT_OPTION.BY_SET_DATE');
+    expect(summarySections.sections[1].summaryList.rows[3].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_HOW_TO_PAY');
+    expect(summarySections.sections[1].summaryList.rows[4].value.html).toBe(DATE);
+    expect(summarySections.sections[1].summaryList.rows[4].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_WHEN_DO_YOU_WANT_TO_BE_PAID_BY');
   });
 
   it('should return your Payment summary sections for pay by instalments', async () => {
     //When
     const claim = createCCJClaimWithClaimResponseDetailsForPayByInstalments();
-    const summarySections = await getSummarySections(constVal.CLAIM_ID, claim, 'cimode');
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'cimode');
     //Then
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[3].value.html).toBe('COMMON.PAYMENT_OPTION.INSTALMENTS');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[3].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_HOW_TO_PAY');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[4].value.html).toBe(INSTALMENTS_OF);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[4].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_INSTALMENTS_OF');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[5].value.html).toBe(FIRST_PAYMENT_DATE);
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[5].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_FIRST_PAYMENT_DATE');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[6].value.html).toBe('COMMON.PAYMENT_FREQUENCY.WEEK');
-    expect(summarySections.sections[constVal.INDEX_CCJ_PAYMENT_SECTION].summaryList.rows[6].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_INSTALMENTS_PAYMENT_FREQUENCY');
+    expect(summarySections.sections[1].summaryList.rows[3].value.html).toBe('COMMON.PAYMENT_OPTION.INSTALMENTS');
+    expect(summarySections.sections[1].summaryList.rows[3].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_HOW_TO_PAY');
+    expect(summarySections.sections[1].summaryList.rows[4].value.html).toBe(INSTALMENTS_OF);
+    expect(summarySections.sections[1].summaryList.rows[4].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_INSTALMENTS_OF');
+    expect(summarySections.sections[1].summaryList.rows[5].value.html).toBe(FIRST_PAYMENT_DATE);
+    expect(summarySections.sections[1].summaryList.rows[5].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_FIRST_PAYMENT_DATE');
+    expect(summarySections.sections[1].summaryList.rows[6].value.html).toBe('COMMON.PAYMENT_FREQUENCY.WEEK');
+    expect(summarySections.sections[1].summaryList.rows[6].key.text).toBe('PAGES.CHECK_YOUR_ANSWER.CCJ_INSTALMENTS_PAYMENT_FREQUENCY');
   });
 
 });
