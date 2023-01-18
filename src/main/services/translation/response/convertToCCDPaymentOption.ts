82fb7fec7d86c7b66b748bd892718a4cd9a6b33a
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-01-18 17:29:21 +0000

    CIV-6536: Adding Translation for Full Admit

--- src/main/services/translation/response/convertToCCDPaymentOption.ts
+++ src/main/services/translation/response/convertToCCDPaymentOption.ts
@@ -1,7 +1,24 @@
 import {PaymentOptionType} from '../../../common/form/models/admission/paymentOption/paymentOptionType';
 import {CCDPaymentOption} from '../../../common/models/ccdResponse/ccdPaymentOption';
+import {Claim} from "models/claim";
+import {ResponseType} from "form/models/responseType";
 
-export const toCCDPaymentOption = (paymentOptionType: PaymentOptionType) : CCDPaymentOption => {
+export const toCCDPaymentOption = (claim: Claim) : CCDPaymentOption => {
+  let paymentOptionType
+
+  switch(claim.respondent1?.responseType) {
+    case ResponseType.PART_ADMISSION:
+      paymentOptionType = claim.partialAdmission.paymentIntention.paymentOption;
+      break;
+    case ResponseType.FULL_ADMISSION:
+      paymentOptionType = claim.fullAdmission.paymentIntention.paymentOption;
+      break;
+    default:
+      paymentOptionType = undefined;
+  }
+
+  logger.info('payment type 2' + paymentOptionType)
+
   switch(paymentOptionType) {
     case PaymentOptionType.INSTALMENTS:
       return CCDPaymentOption.REPAYMENT_PLAN;
