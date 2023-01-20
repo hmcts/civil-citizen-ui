ff2fd25955ef7c97d385cd792b8351611fd92ac2
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-01-20 22:51:28 +0000

    update dashboard controller

--- src/test/unit/routes/features/dashboard/dashboardController.test.ts
+++ src/test/unit/routes/features/dashboard/dashboardController.test.ts
@@ -10,45 +10,33 @@ import {
   mockCivilClaimFullAdmissionPaymentOptionInstalments,
 } from '../../../../utils/mockDraftStore';
 
+const data = require('../../../../utils/mocks/defendantClaimsWithDifferentStatuesMock.json');
+
 jest.mock('../../../../../main/modules/draft-store');
 jest.mock('../../../../../main/modules/oidc');
-
-const mockClaimWithStatus = (stat: string) => {
-  nock('http://localhost:4000')
-    .get(CIVIL_SERVICE_CASES_URL + 'defendant/undefined')
-    .reply(200, [
-      {
-        claimId: '1645882162449409',
-        claimNumber: '000MC009',
-        claimantName: 'John Test',
-        claimAmount: '1000',
-        responseDeadline: '2023-09-25T13:46:07.287',
-        defendantResponseStatus: stat,
-        respondent1ResponseDeadline: '2023-09-25T13:46:07.287',
-      },
-    ]);
-};
 
 describe('Dashboard page', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
 
-  beforeAll(() => {
+  beforeEach(() => {
     nock(idamUrl)
       .post('/o/token')
       .reply(200, {id_token: citizenRoleToken});
+    nock('http://localhost:4000')
+      .get(CIVIL_SERVICE_CASES_URL + 'defendant/undefined')
+      .reply(200, data);
   });
 
   describe('on GET', () => {
     it('should return dashboard page', async () => {
-
+      app.locals.draftStoreClient = mockCivilClaim;
       const res = await request(app).get(DASHBOARD_URL);
       expect(res.status).toBe(200);
       expect(res.text).toContain('Claims made by or against you');
     });
 
     it('should have proper text for NO_RESPONSE status', async () => {
-      mockClaimWithStatus('NO_RESPONSE');
       app.locals.draftStoreClient = mockCivilClaim;
       const res = await request(app).get(DASHBOARD_URL);
       expect(res.status).toBe(200);
@@ -56,7 +44,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for ELIGIBLE_FOR_CCJ status', async () => {
-      mockClaimWithStatus('ELIGIBLE_FOR_CCJ');
       app.locals.draftStoreClient = mockCivilClaim;
       const res = await request(app).get(DASHBOARD_URL);
       expect(res.status).toBe(200);
@@ -64,7 +51,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE and pay IMMEDIATELY status', async () => {
-      mockClaimWithStatus('ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE');
       app.locals.draftStoreClient = mockCivilClaim;
       const res = await request(app).get(DASHBOARD_URL);
       expect(res.status).toBe(200);
@@ -72,7 +58,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE and pay BY_SPECIFIED_DATE status', async () => {
-      mockClaimWithStatus('ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionBySpecifiedDate;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -81,7 +66,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE and pay INSTALMENTS status', async () => {
-      mockClaimWithStatus('ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -90,7 +74,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for MORE_TIME_REQUESTED', async () => {
-      mockClaimWithStatus('MORE_TIME_REQUESTED');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -99,7 +82,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for PAID_IN_FULL', async () => {
-      mockClaimWithStatus('PAID_IN_FULL');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -108,7 +90,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for CLAIMANT_ACCEPTED_STATES_PAID', async () => {
-      mockClaimWithStatus('CLAIMANT_ACCEPTED_STATES_PAID');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -117,7 +98,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for PAID_IN_FULL_CCJ_CANCELLED', async () => {
-      mockClaimWithStatus('PAID_IN_FULL_CCJ_CANCELLED');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -126,7 +106,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for PAID_IN_FULL_CCJ_SATISFIED', async () => {
-      mockClaimWithStatus('PAID_IN_FULL_CCJ_SATISFIED');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -135,7 +114,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for TRANSFERRED', async () => {
-      mockClaimWithStatus('TRANSFERRED');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
@@ -144,7 +122,6 @@ describe('Dashboard page', () => {
     });
 
     it('should have proper text for REDETERMINATION_BY_JUDGE', async () => {
-      mockClaimWithStatus('REDETERMINATION_BY_JUDGE');
       app.locals.draftStoreClient = mockCivilClaimFullAdmissionPaymentOptionInstalments;
 
       const res = await request(app).get(DASHBOARD_URL);
