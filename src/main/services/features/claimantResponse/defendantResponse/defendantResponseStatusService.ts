16eb29de1c0453bda5be597948a05a1cd866f7f1
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-01-24 10:48:35 +0000

    add lang and test for defendantResponseService

--- src/main/services/features/claimantResponse/defendantResponse/defendantResponseStatusService.ts
+++ src/main/services/features/claimantResponse/defendantResponse/defendantResponseStatusService.ts
@@ -1,7 +1,7 @@
 import {DashboardDefendantItem} from 'common/models/dashboard/dashboardItem';
 import {DefendantResponseStatus} from 'models/defendantResponseStatus';
 import {t} from 'i18next';
-import { Claim } from 'common/models/claim';
+import {Claim} from 'common/models/claim';
 
 const daysRemainingString = (days: number): string => {
   if(days > 0) {
@@ -13,42 +13,42 @@ const daysRemainingString = (days: number): string => {
   }
 };
 
-export const getStringStatus = (item: DashboardDefendantItem, claim: Claim): string => {
+export const getStringStatus = (item: DashboardDefendantItem, claim: Claim, lang: string): string => {
   switch (item.defendantResponseStatus){
     case DefendantResponseStatus.NO_RESPONSE:
-      return t('PAGES.DASHBOARD.RESPONSE_TO_CLAIM', { noRemainingDays: daysRemainingString(claim?.getRemainingDays()) });
+      return t('PAGES.DASHBOARD.RESPONSE_TO_CLAIM', lang, {noRemainingDays: daysRemainingString(claim?.getRemainingDays())});
 
     case DefendantResponseStatus.ELIGIBLE_FOR_CCJ:
-      return t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_YOU_HAVE_NOT_RESPONDED') + '\n' +
-        t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_ASK_FOR_CCJ', { claimantName: item.claimantName }) + '\n' +
-        t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_STILL_CAN_RESPOND');
+      return t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_YOU_HAVE_NOT_RESPONDED', lang) + '\n' +
+        t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_ASK_FOR_CCJ', lang, { claimantName: item.claimantName }) + '\n' +
+        t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_STILL_CAN_RESPOND', lang);
 
     case DefendantResponseStatus.ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE:
       if(claim.isFAPaymentOptionPayImmediately() ||
         claim.isPAPaymentOptionPayImmediately())
-        return t('PAGES.DASHBOARD.PAST_DEADLINE_IMMEDIATELY');
+        return t('PAGES.DASHBOARD.PAST_DEADLINE_IMMEDIATELY', lang);
       if(claim.isFAPaymentOptionBySetDate() ||
         claim.isPAPaymentOptionByDate())
-        return t('PAGES.DASHBOARD.PAST_DEADLINE_BY_SPECIFIED_DATE', { paymentDate: claim.fullAdmission.paymentIntention.paymentDate });
+        return t('PAGES.DASHBOARD.PAST_DEADLINE_BY_SPECIFIED_DATE', lang, { paymentDate: claim.fullAdmission.paymentIntention.paymentDate });
       if(claim.isFAPaymentOptionInstallments() ||
         claim.isPAPaymentOptionInstallments())
-        return t('PAGES.DASHBOARD.PAST_DEADLINE_INSTALMENTS');
+        return t('PAGES.DASHBOARD.PAST_DEADLINE_INSTALMENTS', lang);
       break;
 
     case DefendantResponseStatus.MORE_TIME_REQUESTED:
-      return t('PAGES.DASHBOARD.MORE_TIME_REQUESTED', { responseDeadline: item.responseDeadline });
+      return t('PAGES.DASHBOARD.MORE_TIME_REQUESTED', lang, { responseDeadline: item.responseDeadline });
 
     case DefendantResponseStatus.PAID_IN_FULL:
     case DefendantResponseStatus.CLAIMANT_ACCEPTED_STATES_PAID:
-      return t('PAGES.DASHBOARD.CLAIM_SETTLED');
+      return t('PAGES.DASHBOARD.CLAIM_SETTLED', lang);
     case DefendantResponseStatus.PAID_IN_FULL_CCJ_CANCELLED:
     case DefendantResponseStatus.PAID_IN_FULL_CCJ_SATISFIED:
-      return t('PAGES.DASHBOARD.CONFIRMED_PAID', { claimantName: item.claimantName });
+      return t('PAGES.DASHBOARD.CONFIRMED_PAID', lang, { claimantName: item.claimantName });
 
     case DefendantResponseStatus.TRANSFERRED:
-      return t('PAGES.DASHBOARD.CASE_SENT_TO_COURT');
+      return t('PAGES.DASHBOARD.CASE_SENT_TO_COURT', lang);
 
     case DefendantResponseStatus.REDETERMINATION_BY_JUDGE:
-      return t('PAGES.DASHBOARD.REDETERMINATION_BY_JUDGE', { claimantName: item.claimantName });
+      return t('PAGES.DASHBOARD.REDETERMINATION_BY_JUDGE', lang, { claimantName: item.claimantName });
   }
 };
--- src/test/unit/services/features/claimantResponse/defendantResponse/defendantResponseStatusService.test.ts
+++ src/test/unit/services/features/claimantResponse/defendantResponse/defendantResponseStatusService.test.ts
@@ -2,51 +2,145 @@ import {getStringStatus} from 'services/features/claimantResponse/defendantRespo
 import {Claim} from 'models/claim';
 import {DashboardDefendantItem} from 'models/dashboard/dashboardItem';
 import {DefendantResponseStatus} from 'models/defendantResponseStatus';
+import {t} from 'i18next';
+import {PaymentOptionType} from "form/models/admission/paymentOption/paymentOptionType";
+import {FullAdmission} from "models/fullAdmission";
+import {PaymentIntention} from "form/models/admission/paymentIntention";
+
+jest.mock('i18next', () => ({
+  t: (i: string | unknown) => i,
+  use: jest.fn(),
+}));
 
 describe('Defendant status service', () =>{
-  it('should return proper status for NO_RESPONSE due today', () => {
-    class MockClaim extends Claim {
-      getRemainingDays(): number {
-        return 0;
-      }
-    }
-
-    const claim = new MockClaim();
+  it('should return proper status for NO_RESPONSE', () => {
+    const claim = new Claim();
     const item = new DashboardDefendantItem();
     item.defendantResponseStatus = DefendantResponseStatus.NO_RESPONSE;
 
-    const result = getStringStatus(item, claim);
+    const result = getStringStatus(item, claim, 'eng');
 
-    expect(result).toContain('Response to claim. Due today.');
+    expect(result).toContain(t('PAGES.DASHBOARD.RESPONSE_TO_CLAIM'));
   });
 
-  it('should return proper status for NO_RESPONSE 1 day remaining', () => {
-    class MockClaim extends Claim {
-      getRemainingDays(): number {
-        return 1;
-      }
-    }
-    const claim = new MockClaim();
+  it('should return proper status for ELIGIBLE_FOR_CCJ', () => {
+    const claim = new Claim();
     const item = new DashboardDefendantItem();
-    item.defendantResponseStatus = DefendantResponseStatus.NO_RESPONSE;
+    item.defendantResponseStatus = DefendantResponseStatus.ELIGIBLE_FOR_CCJ;
 
-    const result = getStringStatus(item, claim);
+    const result = getStringStatus(item, claim, 'eng');
 
-    expect(result).toContain('Response to claim. 1 days remaining.');
+    expect(result).toContain(t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_YOU_HAVE_NOT_RESPONDED'));
+    expect(result).toContain(t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_ASK_FOR_CCJ'));
+    expect(result).toContain(t('PAGES.DASHBOARD.ELIGIBLE_FOR_CCJ_STILL_CAN_RESPOND'));
   });
 
-  it('should return proper status for NO_RESPONSE 1 day overdue', () => {
-    class MockClaim extends Claim {
-      getRemainingDays(): number {
-        return -1;
-      }
-    }
-    const claim = new MockClaim();
+  it('should return proper status for ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE, IMMEDIATELY', () => {
+    const claim = new Claim();
+    claim.fullAdmission = new FullAdmission();
+    claim.fullAdmission.paymentIntention = new PaymentIntention();
+    claim.fullAdmission.paymentIntention.paymentOption = PaymentOptionType.IMMEDIATELY;
     const item = new DashboardDefendantItem();
-    item.defendantResponseStatus = DefendantResponseStatus.NO_RESPONSE;
+    item.defendantResponseStatus = DefendantResponseStatus.ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE;
 
-    const result = getStringStatus(item, claim);
+    const result = getStringStatus(item, claim, 'eng');
 
-    expect(result).toContain('Response to claim. 1 days overdue.');
+    expect(result).toContain(t('PAGES.DASHBOARD.PAST_DEADLINE_IMMEDIATELY'));
+  });
+
+  it('should return proper status for ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE, BY_SET_DATE', () => {
+    const claim = new Claim();
+    claim.fullAdmission = new FullAdmission();
+    claim.fullAdmission.paymentIntention = new PaymentIntention();
+    claim.fullAdmission.paymentIntention.paymentOption = PaymentOptionType.BY_SET_DATE;
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.PAST_DEADLINE_BY_SPECIFIED_DATE'));
+  });
+
+  it('should return proper status for ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE, INSTALMENTS', () => {
+    const claim = new Claim();
+    claim.fullAdmission = new FullAdmission();
+    claim.fullAdmission.paymentIntention = new PaymentIntention();
+    claim.fullAdmission.paymentIntention.paymentOption = PaymentOptionType.INSTALMENTS;
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.ELIGIBLE_FOR_CCJ_AFTER_FULL_ADMIT_PAY_IMMEDIATELY_PAST_DEADLINE;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.PAST_DEADLINE_INSTALMENTS'));
+  });
+
+  it('should return proper status for MORE_TIME_REQUESTED', () => {
+    const claim = new Claim();
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.MORE_TIME_REQUESTED;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.MORE_TIME_REQUESTED'));
+  });
+
+  it('should return proper status for PAID_IN_FULL', () => {
+    const claim = new Claim();
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.PAID_IN_FULL;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.CLAIM_SETTLED'));
+  });
+
+  it('should return proper status for CLAIMANT_ACCEPTED_STATES_PAID', () => {
+    const claim = new Claim();
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.PAID_IN_FULL;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.CLAIM_SETTLED'));
+  });
+
+  it('should return proper status for PAID_IN_FULL_CCJ_CANCELLED', () => {
+    const claim = new Claim();
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.PAID_IN_FULL_CCJ_CANCELLED;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.CONFIRMED_PAID'));
+  });
+
+  it('should return proper status for PAID_IN_FULL_CCJ_SATISFIED', () => {
+    const claim = new Claim();
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.PAID_IN_FULL_CCJ_SATISFIED;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.CONFIRMED_PAID'));
+  });
+
+  it('should return proper status for TRANSFERRED', () => {
+    const claim = new Claim();
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.TRANSFERRED;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.CASE_SENT_TO_COURT'));
+  });
+
+  it('should return proper status for REDETERMINATION_BY_JUDGE', () => {
+    const claim = new Claim();
+    const item = new DashboardDefendantItem();
+    item.defendantResponseStatus = DefendantResponseStatus.REDETERMINATION_BY_JUDGE;
+
+    const result = getStringStatus(item, claim, 'eng');
+
+    expect(result).toContain(t('PAGES.DASHBOARD.REDETERMINATION_BY_JUDGE'));
   });
 });
