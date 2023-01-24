5449288ca062d276d4fe8d4996352e9447ddcf7b
laarmada <sergiosalmar@yahoo.es>
2023-01-24 12:14:07 +0000

    refactor RESPONSE_TASK_LIST_URL

--- src/main/routes/features/response/citizenDob/citizenDobController.ts
+++ src/main/routes/features/response/citizenDob/citizenDobController.ts
@@ -1,6 +1,6 @@
 import {NextFunction, Request, Response, Router} from 'express';
 import {CitizenDob} from '../../../../common/form/models/citizenDob';
-import {AGE_ELIGIBILITY_URL, CITIZEN_PHONE_NUMBER_URL, CLAIM_TASK_LIST_URL, DOB_URL} from '../../../../routes/urls';
+import {AGE_ELIGIBILITY_URL, CITIZEN_PHONE_NUMBER_URL, DOB_URL, RESPONSE_TASK_LIST_URL} from '../../../../routes/urls';
 import {Party} from '../../../../common/models/party';
 import {Claim} from '../../../../common/models/claim';
 import {AgeEligibilityVerification} from '../../../../common/utils/ageEligibilityVerification';
@@ -18,7 +18,7 @@ function renderView(form: GenericForm<CitizenDob>, res: Response): void {
 function redirectToNextPage(req: Request, res: Response, dob: Date, respondent: Party) {
   if (AgeEligibilityVerification.isOverEighteen(dob)) {
     if (respondent?.partyPhone) {
-      res.redirect(constructResponseUrlWithIdParams(req.params.id, CLAIM_TASK_LIST_URL));
+      res.redirect(constructResponseUrlWithIdParams(req.params.id, RESPONSE_TASK_LIST_URL));
     } else {
       res.redirect(constructResponseUrlWithIdParams(req.params.id, CITIZEN_PHONE_NUMBER_URL));
     }
