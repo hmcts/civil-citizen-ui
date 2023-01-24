c10bc145a8fa2aafca5b70364472d92f34971bc8
Kadir <kegilmez@gmail.com>
2023-01-24 22:24:21 +0000

    developed the unit test for task list controller

---
+++ src/test/unit/routes/features/claimantResponse/claimantResponseTasklistController.test.ts
@@ -0,0 +1,45 @@
+import request from 'supertest';
+import nock from 'nock';
+import config from 'config';
+import {app} from '../../../../../main/app';
+import {CLAIMANT_RESPONSE_TASK_LIST_URL} from '../../../../../main/routes/urls';
+import {mockCivilClaim, mockRedisFailure} from '../../../../utils/mockDraftStore';
+
+jest.mock('../../../../../main/modules/oidc');
+jest.mock('../../../../../main/modules/draft-store');
+
+describe('Claimant response task list', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+
+  beforeAll(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+  });
+
+  describe('on GET', () => {
+    it('should display task list', async () => {
+      app.locals.draftStoreClient = mockCivilClaim;
+      await request(app)
+        .get(CLAIMANT_RESPONSE_TASK_LIST_URL)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Your response');
+          expect(res.text).toContain('How they responded');
+          expect(res.text).toContain('Choose what to do next');
+          expect(res.text).toContain('Submit');
+
+        });
+    });
+
+    it('should return http 500 when has error in the get method', async () => {
+      app.locals.draftStoreClient = mockRedisFailure;
+      await request(app)
+        .get(CLAIMANT_RESPONSE_TASK_LIST_URL)
+        .expect((res) => {
+          expect(res.status).toBe(500);
+        });
+    });
+  });
+});
