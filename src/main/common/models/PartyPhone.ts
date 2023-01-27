f5546f6f1da922aac04ec0f1ee4b9f83d7177ecc
Felipe <felipe@LAPTOP3721.V1.com>
2023-01-27 13:44:04 +0100

    Update: partyPhone validation

--- src/main/common/models/PartyPhone.ts
+++ src/main/common/models/PartyPhone.ts
@@ -1,11 +1,10 @@
-import {IsNotEmpty, MaxLength, Validate, ValidateIf} from 'class-validator';
+import {IsNotEmpty, Validate, ValidateIf} from 'class-validator';
 import {PhoneUKValidator} from 'common/form/validators/phoneUKValidator';
 
 export class PartyPhone {
   @ValidateIf(o => o.phone !== undefined)
-  @MaxLength(20, {message: 'ERRORS.PHONE_TOO_MANY'})
   @IsNotEmpty({message: 'ERRORS.NOT_TO_REMOVE_PHONE_NUMBER'})
-  @Validate(PhoneUKValidator, ['originalResponseDeadline', 28], {message: 'ERRORS.DATE_NOT_MORE_THAN_28_DAYS'})
+  @Validate(PhoneUKValidator, {message: 'ERRORS.VALID_PHONE_NUMBER'})
     phone?: string;
 
   constructor(phone?: string) {
