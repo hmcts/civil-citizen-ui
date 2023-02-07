0ed533bb6281f330780c47410f2131e9ea496ab1
Felipe <felipe@LAPTOP3721.V1.com>
2023-02-07 12:35:50 +0100

    New: calculate unavailable dates

---
+++ src/main/services/features/directionsQuestionnaire/hearing/unavailableDatesCalculation.ts
@@ -0,0 +1,28 @@
+import {
+  UnavailableDates,
+  UnavailableDateType,
+} from "common/models/directionsQuestionnaire/hearing/unavailableDates";
+
+export const getUnavailableDatesLength = (unavailableDates: UnavailableDates) => {
+  let result: Date[] = [];
+  unavailableDates.items.forEach(item => {
+    if (item.type === UnavailableDateType.SINGLE_DATE) {
+      result.push(item.from);
+    } else {
+      const test = getDatesBetween(item.from, item.until);
+      result = [...result, ...test];
+    }
+  });
+  const uniqueDates = [...new Set(result.map(date => date.toString()))];
+  return uniqueDates.length;
+}
+
+const getDatesBetween = (startDate: Date, endDate: Date) => {
+  const currentDate = new Date(startDate.getTime());
+  const dates = [];
+  while (currentDate <= endDate) {
+    dates.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  return dates;
+}
