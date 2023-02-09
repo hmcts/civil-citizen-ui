b154c531fa7e7c8465f1f6e250453b2745f96b37
Christopher Pateman <christopher.pateman@version1.com>
2023-02-09 13:04:26 +0000

    CIV-5698 - Add new vault to chart and move existing secrets

--- infrastructure/_main.tf
+++ infrastructure/_main.tf
@@ -1,14 +1,3 @@
-data "azurerm_key_vault" "civil_vault" {
-  name                = "civil-${var.env}"
-  resource_group_name = "civil-service-${var.env}"
-}
-
-resource "azurerm_key_vault_secret" "draft_store_access_key" {
-  name         = "draft-store-access-key"
-  value        = module.citizen-ui-draft-store.access_key
-  key_vault_id = data.azurerm_key_vault.civil_vault.id
-}
-
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
--- infrastructure/key-vault-secrets.tf
+++ infrastructure/key-vault-secrets.tf
@@ -12,3 +12,14 @@ resource "azurerm_key_vault_secret" "app_insights_instrumentation_key" {
     module.key-vault
   ]
 }
+
+resource "azurerm_key_vault_secret" "redis_draft_store_access_key" {
+  name         = "draft-store-access-key"
+  value        = module.citizen-ui-draft-store.access_key
+  key_vault_id = module.key-vault.key_vault_id
+
+  content_type = "secret"
+  tags = merge(var.common_tags, {
+    "source" : "redis ${module.citizen-ui-draft-store.host_name}"
+  })
+}
