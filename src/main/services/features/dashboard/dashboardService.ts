2798367180d8ae5ea9ea20dd983d5234ffcac7d7
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2023-02-09 14:55:45 +0000

    Dashboard display correct translations with dynamic fields and english and welsh selection

--- src/main/services/features/dashboard/dashboardService.ts
+++ src/main/services/features/dashboard/dashboardService.ts
@@ -8,12 +8,14 @@ import {CivilServiceClient} from 'client/civilServiceClient';
 import config from 'config';
 import {AppRequest} from 'models/AppRequest';
 import {t} from 'i18next';
+import {getLng} from 'common/utils/languageToggleUtils';
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 export const getClaimsForDefendant = async (req : Request): Promise<DashboardDefendantItem[]> => {
+  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
   const claimsForDefendant = await civilServiceClient.getClaimsForDefendant(<AppRequest>req);
-  claimsForDefendant?.forEach(item => item.translatedStatus = translate(item.getStatus().translationKey,item.getStatus().parameter));
+  claimsForDefendant?.forEach(item => item.translatedStatus = translate(item.getStatus().translationKey,item.getStatus().parameter, lang));
   console.log(claimsForDefendant);
   return claimsForDefendant;
 };
@@ -22,9 +24,9 @@ export const getClaimsForClaimant = async (req: Request): Promise<DashboardClaim
   return await civilServiceClient.getClaimsForClaimant(<AppRequest>req);
 };
 
-const translate = (translationKey: string, param?: DashboardStatusTranslationParam) =>{
+const translate = (translationKey: string, param?: DashboardStatusTranslationParam, lang?: string | unknown) =>{
   if(param) {
-    const keyValue = {[param.key]: param.value};
+    const keyValue = {[param.key]: param.value, lng: getLng(lang)};
     return t(translationKey, keyValue);
   }
   return t(translationKey);
