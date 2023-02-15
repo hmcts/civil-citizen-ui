7037ff1e0c9855444f93e50d16f0ffc13afd6e6e
Christopher Pateman <christopher.pateman@version1.com>
2023-02-15 15:55:54 +0000

    CIV-5698 - Add civil MI to vault

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -3,6 +3,11 @@ data "azuread_group" "dts_civil" {
   security_enabled = true
 }
 
+data "azurerm_user_assigned_identity" "civil" {
+  name                = "civil-${var.env}"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "${var.product}-${var.component}-${var.env}"
@@ -14,4 +19,27 @@ module "key-vault" {
   product_group_object_id = data.azuread_group.dts_civil.object_id
   common_tags             = var.common_tags
   create_managed_identity = false
+}
+
+resource "azurerm_key_vault_access_policy" "civil_mi" {
+  key_vault_id = module.key-vault.key_vault_id
+
+  object_id = data.azurerm_user_assigned_identity.civil.principal_id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  key_permissions = [
+    "Get",
+    "List",
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+
 }
