4509890c9e22dd0ac992e62d883a34b131663306
OrtizP <pablo.gutierrez@version1.com>
2023-02-20 09:02:00 +0100

    CIV-5797: Remove Kenneth task-work

--- src/main/common/models/ccdResponse/ccdResponseCuiFields.ts
+++ src/main/common/models/ccdResponse/ccdResponseCuiFields.ts
@@ -1,8 +1,6 @@
-import {CCDFinancialDetailsCuiFields} from 'models/ccdResponse/ccdFinancialDetailsCuiFields';
 import {CCDMediation} from 'models/ccdResponse/ccdMediation';
 
 export interface CCDResponseCuiFields {
-  respondent1FinancialDetailsFromCui?: CCDFinancialDetailsCuiFields,
   respondent1MediationFromCui?: CCDMediation,
 
 }
--- src/main/services/translation/response/convertToCCDFromCuiOnlyFields.ts
+++ src/main/services/translation/response/convertToCCDFromCuiOnlyFields.ts
@@ -1,11 +1,9 @@
 import {CCDResponseCuiFields} from 'models/ccdResponse/ccdResponseCuiFields';
 import {Claim} from 'models/claim';
-import {toCCDFieldsOnlyInCuiFinancialDetails} from 'models/ccdResponse/convertToCCDFromCuiOnlyFinancialDetails';
 import {toCCDMediation} from 'services/translation/response/convertToCCDMediation';
 
 export const toCCDFieldsOnlyInCui = (claim: Claim): CCDResponseCuiFields => {
   return {
-    respondent1FinancialDetailsFromCui: toCCDFieldsOnlyInCuiFinancialDetails(claim.statementOfMeans),
     respondent1MediationFromCui: toCCDMediation(claim.mediation),
   };
 };
