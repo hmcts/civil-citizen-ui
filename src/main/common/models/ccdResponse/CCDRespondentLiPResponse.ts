1222d2206d00bf8a5b95b536ea8d4df19b390b01
OrtizP <pablo.gutierrez@version1.com>
2023-02-20 12:14:52 +0100

    CIV-5797: Rename fields

---
+++ src/main/common/models/ccdResponse/CCDRespondentLiPResponse.ts
@@ -0,0 +1,6 @@
+import {CCDMediation} from 'models/ccdResponse/ccdMediation';
+
+export interface CCDRespondentLiPResponse {
+  respondent1MediationLipResponse?: CCDMediation,
+
+}
--- src/main/common/models/ccdResponse/ccdResponseCuiFields.ts
+++
@@ -1,6 +0,0 @@
-import {CCDMediation} from 'models/ccdResponse/ccdMediation';
-
-export interface CCDResponseCuiFields {
-  respondent1MediationFromCui?: CCDMediation,
-
-}
