5416cef1dc50342d6dd1fd61284faab59fb72b1b
OrtizP <pablo.gutierrez@version1.com>
2023-02-21 11:44:30 +0100

    CIV-5797: Use same approach from Kenneth on convertToCCDYesNo.ts

--- src/main/services/translation/response/convertToCCDYesNo.ts
+++ src/main/services/translation/response/convertToCCDYesNo.ts
@@ -2,16 +2,16 @@ import {YesNo, YesNoUpperCamelCase} from 'common/form/models/yesNo';
 import {GenericYesNo} from 'form/models/genericYesNo';
 
 export const toCCDYesNo = (value: YesNo) => {
-  return value === undefined ? undefined :
-    value === YesNo.YES ? YesNoUpperCamelCase.YES : YesNoUpperCamelCase.NO;
+  if (value)
+    return value === YesNo.YES ? YesNoUpperCamelCase.YES : YesNoUpperCamelCase.NO;
 };
 
 export const toCCDYesNoFromGenericYesNo = (value: GenericYesNo) => {
-  return value?.option === undefined ? undefined :
-    value?.option === YesNo.YES ? YesNoUpperCamelCase.YES : YesNoUpperCamelCase.NO;
+  if(value?.option)
+    return value?.option === YesNo.YES ? YesNoUpperCamelCase.YES : YesNoUpperCamelCase.NO;
 };
 
 export const toCCDYesNoFromBoolean = (value: boolean) => {
-  return value === undefined ? undefined :
-    value ? YesNoUpperCamelCase.YES : YesNoUpperCamelCase.NO;
+  if(value !== undefined)
+    return value ? YesNoUpperCamelCase.YES : YesNoUpperCamelCase.NO;
 };
