d2031b74dd867decb3439dcab766da95c0d3701e
Felipe <felipe@LAPTOP3721.V1.com>
2023-02-21 14:41:17 +0100

    Update: revert get number from inputs

--- src/main/assets/js/append-row.js
+++ src/main/assets/js/append-row.js
@@ -125,19 +125,24 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  // function getNumberFromElementName() {
-  //   const elements = document.getElementsByClassName('multiple-row');
-  //   const lastItem = elements[elements.length-1];
-  //   const inputs = lastItem.getElementsByTagName('input');
-  //   if (inputs.length) {
-  //     const number = inputs[0].id.split('-')[1];
-  //     return Number(number) + 1;
-  //   } 
-  //   return document.getElementsByClassName('multiple-row').length;
-  // }
+  function getNumberFromElementName() {
+    const elements = document.getElementsByClassName('multiple-row');
+    const lastItem = elements[elements.length-1];
+    const lastRadioInput = lastItem.getElementsByClassName('govuk-radios__input');
+    const lastCheckboxInput = lastItem.getElementsByClassName('govuk-checkboxes__input');
+
+    if (lastRadioInput.length) {
+      const number = lastRadioInput[0].id.split('-')[1];
+      return Number(number) + 1;
+    } else if (lastCheckboxInput.length) {
+      const number = lastCheckboxInput[0].id.split('-')[1];
+      return Number(number) + 1;
+    } 
+    return document.getElementsByClassName('multiple-row').length;
+  }
 
   function incrementIndexOnNameAndId(element) {
-    const newIndex = document.getElementsByClassName('multiple-row').length;
+    const newIndex = getNumberFromElementName();
     if (element.name) {
       element.name = element.name.replace(indexRegex, '[' + newIndex + ']');
       element.id = element.id.replace(checkboxIndexRegex, '-' + newIndex + '-');
@@ -147,7 +152,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function updateAttributes(element) {
-    const newIndex = document.getElementsByClassName('multiple-row').length;
+    const newIndex = getNumberFromElementName();
     if (element.getAttribute('for')) {
       element.setAttribute(
         'for',
