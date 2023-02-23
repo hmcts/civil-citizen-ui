db8d4d79a3f4bfaf46419112a25df1af409118f6
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-02-23 11:45:24 +0000

    add all logics and fix bug when the link need variables

--- src/main/common/form/models/claimSummarySection.ts
+++ src/main/common/form/models/claimSummarySection.ts
@@ -9,46 +9,6 @@ export interface ClaimSummarySection {
   type?: ClaimSummaryType;
   data?: ClaimSummaryItem;
 }
-export interface Variables{
-  name: string;
-  value: string;
-}
-export class LastUpdateSectionBuilder {
-  private _title: ClaimSummarySection;
-  private _sections: ClaimSummarySection[];
-
-  constructor() {
-    this._title = undefined;
-    this._sections = undefined;
-  }
-
-  setTitle(value: ClaimSummarySection) {
-    this._title = value;
-    return this;
-  }
-
-  get title(): ClaimSummarySection {
-    return this._title;
-  }
-
-  get sections(): ClaimSummarySection[] {
-    return this._sections;
-  }
-
-  setSections(value: ClaimSummarySection[]) {
-    this._sections = value;
-    return this;
-  }
-  build() {
-    const claimSummarySections: ClaimSummarySection[] = [];
-    claimSummarySections.push(this._title);
-    this.sections.forEach((item) => {
-      claimSummarySections.push(item);
-    });
-    return claimSummarySections;
-  }
-}
-
 export interface ClaimSummaryItem {
   text?: string;
   href?: string;
