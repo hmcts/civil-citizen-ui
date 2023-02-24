29e4b5e80836705b1567a37e26f5bc39e30e267b
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-02-24 17:57:08 +0000

    fixed comments

--- src/main/modules/i18n/languageService.ts
+++ src/main/modules/i18n/languageService.ts
@@ -1,12 +1,11 @@
 import {app} from '../../app';
+import {Request, Response, NextFunction} from 'express';
 
-export const setLanguage = function (req: any, res: any, next: any) {
-  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-  app.locals.lang = lang;
+export const setLanguage = (req: Request, res: Response, next: NextFunction) => {
+  app.locals.lang = req.query.lang ? req.query.lang : req.cookies.lang;
   next();
 };
 
 export const getLanguage = () => {
-  const lang = app.locals.lang;
-  return lang;
+  return app.locals.lang;
 };
