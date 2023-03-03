64f82b07c6b617a982222b08f869d705e5db9b07
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-03-03 08:20:39 +0000

    create mapper

---
+++ src/main/common/models/Mapper/MapperCCDCUI.ts
@@ -0,0 +1,23 @@
+import elements = CodeceptJS.browserCodecept.elements;
+
+const configObjectCCDToCUI ={
+  'defenceAdmitPartPaymentTimeRouteRequired': 'Claim.Respondent1.ResponseType',
+};
+
+class MapperToCui {
+
+/*  mapToCUI(){
+
+  }
+  recursivelyIterateProperties() {
+    for (const prop in Object.keys(configObjectCCDToCUI)) {
+      console.log(prop);
+      if (!(typeof(jsonObject[prop]) === 'string')
+
+        && !(jsonObject[prop] instanceof Array)) {
+        this.recursivelyIterateProperties(jsonObject[prop]);
+      }
+
+    }
+  }*/
+}
