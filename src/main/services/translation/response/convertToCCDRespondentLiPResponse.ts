d630f3d9a615ba63916ae5424e689dc68b3415a8
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-03-03 13:00:04 +0000

    update path for imports

--- src/main/services/translation/response/convertToCCDRespondentLiPResponse.ts
+++ src/main/services/translation/response/convertToCCDRespondentLiPResponse.ts
@@ -1,6 +1,6 @@
-import {CCDRespondentLiPResponse} from 'models/ccdResponse/CCDRespondentLiPResponse';
-import {Claim} from 'models/claim';
-import {toUpperCaseGenericYesNo} from 'form/models/yesNo';
+import {CCDRespondentLiPResponse} from '../../../common/models/ccdResponse/CCDRespondentLiPResponse';
+import {Claim} from '../../../common/models/claim';
+import {toUpperCaseGenericYesNo} from '../../../common/form/models/yesNo';
 
 export const toCCDRespondentLiPResponse = (claim: Claim): CCDRespondentLiPResponse => {
   return {
