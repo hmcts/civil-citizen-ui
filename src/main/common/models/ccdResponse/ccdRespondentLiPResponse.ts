1446d3fb41d9cfe98bdc955368724e29f0d89e74
OrtizP <pablo.gutierrez@version1.com>
2023-03-07 08:56:14 +0100

    CIV-5797: Resolve merge conflicts

--- src/main/common/models/ccdResponse/ccdRespondentLiPResponse.ts
+++ src/main/common/models/ccdResponse/ccdRespondentLiPResponse.ts
@@ -1,7 +1,9 @@
 import {
   CCDFinancialDetailsLiP,
 } from 'models/ccdResponse/ccdFinancialDetailsLiP';
+import {CCDMediation} from 'models/ccdResponse/ccdMediation';
 
 export interface CCDRespondentLiPResponse {
   respondent1LiPFinancialDetails?: CCDFinancialDetailsLiP,
+  respondent1MediationLiPResponse?: CCDMediation,
 }
