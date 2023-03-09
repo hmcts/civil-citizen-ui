43e556cfefa0ee6cdb2c9f5ae564a7a7343f9611
OrtizP <pablo.gutierrez@version1.com>
2023-03-09 08:49:25 +0100

    CIV-5944: Creating the model

--- src/main/common/models/ccdResponse/ccdRespondentLiPResponse.ts
+++ src/main/common/models/ccdResponse/ccdRespondentLiPResponse.ts
@@ -2,8 +2,11 @@ import {
   CCDFinancialDetailsLiP,
 } from 'models/ccdResponse/ccdFinancialDetailsLiP';
 import {CCDMediation} from 'models/ccdResponse/ccdMediation';
+import {CCDAddress} from 'models/ccdResponse/ccdAddress';
 
 export interface CCDRespondentLiPResponse {
   respondent1LiPFinancialDetails?: CCDFinancialDetailsLiP,
   respondent1MediationLiPResponse?: CCDMediation,
+  respondent1ContactPerson?: string,
+  respondent1CorrespondenceAddress?: CCDAddress,
 }
--- src/main/services/translation/response/convertToCCDRespondentLiPResponse.ts
+++ src/main/services/translation/response/convertToCCDRespondentLiPResponse.ts
@@ -4,10 +4,13 @@ import {
 } from 'services/translation/response/convertToCCDResponseLiPFinancialDetails';
 import {toCCDMediation} from 'services/translation/response/convertToCCDMediation';
 import {CCDRespondentLiPResponse} from 'models/ccdResponse/ccdRespondentLiPResponse';
+import {toCCDAddress} from 'services/translation/response/convertToCCDAddress';
 
 export const toCCDRespondentLiPResponse = (claim: Claim): CCDRespondentLiPResponse => {
   return {
     respondent1LiPFinancialDetails : toCCDResponseLiPFinancialDetails(claim.statementOfMeans),
     respondent1MediationLiPResponse: toCCDMediation(claim.mediation),
+    respondent1ContactPerson: claim.respondent1?.partyDetails?.contactPerson,
+    respondent1CorrespondenceAddress: claim.respondent1?.partyDetails?.provideCorrespondenceAddress === 'yes' ? toCCDAddress(claim.respondent1?.partyDetails?.correspondenceAddress) : undefined,
   };
 };
