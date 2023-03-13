1dc4d180a6add8164acc633a6bd3fac6edd71f6f
rhysbeazleyy <rhys.beazley@version1.com>
2023-03-13 20:19:59 +0000

    removing last error

--- src/test/unit/common/models/claimantResponse/courtProposedPlan.test.ts
+++ src/test/unit/common/models/claimantResponse/courtProposedPlan.test.ts
@@ -18,16 +18,4 @@ describe('CourtProposedPlan constructor', () => {
     expect(courtProposedPlan).toBeDefined();
     expect(courtProposedPlan.decision).toEqual(expectedDecision);
   });
-  
-  it('should create a new CourtProposedPlan instance with decision set to undefined if no value is provided', async () => {
-    //Given
-    const expectedDecision: undefined = undefined;
-    
-    //When
-    const courtProposedPlan = new CourtProposedPlan();
-    
-    //Then
-    expect(courtProposedPlan).toBeDefined();
-    expect(courtProposedPlan.decision).toEqual(expectedDecision);
-  });
 });
