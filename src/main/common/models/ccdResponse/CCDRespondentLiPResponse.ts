a0da128e4c2e8e966043baae51c1dc97d4d5d174
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-03-13 14:21:56 +0000

    Delete CCDRespondentLiPResponse.ts
    duplicate file

--- src/main/common/models/ccdResponse/CCDRespondentLiPResponse.ts
+++
@@ -1,8 +0,0 @@
-import {CCDDQExtraDetails} from 'models/ccdResponse/ccdDQExtraDetails';
-import {CCDHearingSupport} from 'models/ccdResponse/ccdHearingSupport';
-
-export interface CCDRespondentLiPResponse {
-  respondent1DQExtraDetails?: CCDDQExtraDetails,
-  respondent1DQHearingSupportLip?: CCDHearingSupport,
-
-}
