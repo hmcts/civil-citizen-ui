6edf01841ad61788084f02c4d6e6129cab879c37
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-03-14 11:25:56 +0100

    CIV-5944: Refactor to use constant instead literal string

--- src/main/services/translation/response/convertToCCDRespondentLiPResponse.ts
+++ src/main/services/translation/response/convertToCCDRespondentLiPResponse.ts
@@ -5,12 +5,13 @@ import {
 import {toCCDMediation} from 'services/translation/response/convertToCCDMediation';
 import {CCDRespondentLiPResponse} from 'models/ccdResponse/ccdRespondentLiPResponse';
 import {toCCDAddress} from 'services/translation/response/convertToCCDAddress';
+import {YesNo} from 'form/models/yesNo';
 
 export const toCCDRespondentLiPResponse = (claim: Claim): CCDRespondentLiPResponse => {
   return {
     respondent1LiPFinancialDetails : toCCDResponseLiPFinancialDetails(claim.statementOfMeans),
     respondent1MediationLiPResponse: toCCDMediation(claim.mediation),
     respondent1LiPContactPerson: claim.respondent1?.partyDetails?.contactPerson,
-    respondent1LiPCorrespondenceAddress: claim.respondent1?.partyDetails?.provideCorrespondenceAddress === 'yes' ? toCCDAddress(claim.respondent1?.partyDetails?.correspondenceAddress) : undefined,
+    respondent1LiPCorrespondenceAddress: claim.respondent1?.partyDetails?.provideCorrespondenceAddress === YesNo.YES ? toCCDAddress(claim.respondent1?.partyDetails?.correspondenceAddress) : undefined,
   };
 };
