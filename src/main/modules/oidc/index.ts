63d03038786958c078eeb8f7609c969407446c04
Paul Hancock <Paul.Hancock1@HMCTS.NET>
2023-03-14 11:25:13 +0000

    Use app.local.user to get the user details

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -11,9 +11,6 @@ import {
   UNAUTHORISED_URL,
 } from '../../routes/urls';
 
-const {Logger} = require('@hmcts/nodejs-logging');
-const logger = Logger.getLogger('taskListController');
-
 const requestIsForAssigningClaimForDefendant = (req: Request): boolean => {
   return req.originalUrl.startsWith(ASSIGN_CLAIM_URL) && req.query?.id !== undefined;
 };
@@ -44,23 +41,17 @@ export class OidcMiddleware {
     });
 
     app.get(CALLBACK_URL, async (req: AppRequest, res: Response) => {
-      logger.error('index.ts callback - req.query.code=' + req?.query?.code);
       if (typeof req.query.code === 'string') {
-        logger.error('index.ts callback - getting user details');
         req.session.user = app.locals.user = await getUserDetails(redirectUri, req.query.code);
         if (app.locals.assignClaimId) {
-          logger.error('index.ts callback - assigning claim id');
           const assignClaimUrlWithClaimId = buildAssignClaimUrlWithId(req, app);
           return res.redirect(assignClaimUrlWithClaimId);
         }
         if (app.locals.user?.roles?.includes(citizenRole)) {
-          logger.error('index.ts callback - redirect to dashboard');
           return res.redirect(DASHBOARD_URL);
         }
-        logger.error('index.ts callback - unauthorised url');
         return res.redirect(UNAUTHORISED_URL);
       } else {
-        logger.error('index.ts callback - else (req.query.code is not a string) - redirect to dashboard');
         res.redirect(DASHBOARD_URL);
       }
     });
@@ -80,27 +71,18 @@ export class OidcMiddleware {
     });
 
     app.use((req: Request, res: Response, next: NextFunction) => {
-      const appReq: AppRequest = <AppRequest>req;
-      logger.error('index.ts app.use start: appReq.session.user=' + appReq.session?.user
-        + ' app.locals.user=' + app.locals.user + ' appReq.session=' + appReq.session
-        + ' appReq.session.id=' + appReq.session?.id);
       if (app.locals.user) {
         if (app.locals.user.roles?.includes(citizenRole)) {
-          logger.error('index.ts app.use return 1 - next');
           return next();
         }
-        logger.error('index.ts app.use return 2 - dashboard');
         return res.redirect(DASHBOARD_URL);
       }
       if (requestIsForPinAndPost(req)) {
-        logger.error('index.ts app.use return 3 - next (pin and post)');
         return next();
       }
       if (requestIsForAssigningClaimForDefendant(req)) {
-        logger.error('index.ts app.use debug 4 - setting claim id');
         app.locals.assignClaimId = <string>req.query.id;
       }
-      logger.error('index.ts app.use end - redirecting to sign in url');
       return res.redirect(SIGN_IN_URL);
     });
   }
