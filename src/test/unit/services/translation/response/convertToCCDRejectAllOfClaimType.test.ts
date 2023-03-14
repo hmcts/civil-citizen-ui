ff1ae29ed61b65ccea85ce34fc27ccbe8f07325b
hmcts-version1-domini <domini.mattos@version1.com>
2023-03-14 12:35:24 +0100

    make line shorter and given when then in tests

--- src/test/unit/services/translation/response/convertToCCDRejectAllOfClaimType.test.ts
+++ src/test/unit/services/translation/response/convertToCCDRejectAllOfClaimType.test.ts
@@ -7,23 +7,32 @@ import {toCCDRejectAllOfClaimType} from 'services/translation/response/convertTo
 describe('translate RejectAllOfClaimType to CCD model', () => {
 
   it('should return HAS_PAID_THE_AMOUNT_CLAIMED', () => {
+    // Given
     const claim = new Claim();
+    // When
     claim.rejectAllOfClaim = new RejectAllOfClaim(RejectAllOfClaimType.ALREADY_PAID);
     const rejectAlOfClaimTypeResponseCCD = toCCDRejectAllOfClaimType(claim.rejectAllOfClaim.option);
+    // Then
     expect(rejectAlOfClaimTypeResponseCCD).toBe(CCDRejectAllOfClaimType.HAS_PAID_THE_AMOUNT_CLAIMED);
   });
 
   it('should return DISPUTES_THE_CLAIM', () => {
+    // Given
     const claim = new Claim();
+    // When
     claim.rejectAllOfClaim = new RejectAllOfClaim(RejectAllOfClaimType.DISPUTE);
     const rejectAlOfClaimTypeResponseCCD = toCCDRejectAllOfClaimType(claim.rejectAllOfClaim.option);
+    // Then
     expect(rejectAlOfClaimTypeResponseCCD).toBe(CCDRejectAllOfClaimType.DISPUTES_THE_CLAIM);
   });
 
   it('should return undefined', () => {
+    // Given
     const claim = new Claim();
+    // When
     claim.rejectAllOfClaim = new RejectAllOfClaim('test');
     const rejectAlOfClaimTypeResponseCCD = toCCDRejectAllOfClaimType(claim.rejectAllOfClaim.option);
+    // Then
     expect(rejectAlOfClaimTypeResponseCCD).toBe(undefined);
   });
 });
