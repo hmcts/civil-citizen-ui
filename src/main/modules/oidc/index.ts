12b4341505285cf734d4c4d2c61bad95f4e55cf3
KumarSam <sampank@gmail.com>
2023-03-16 13:11:18 +0000

    Changing the url to preview.platform.hmcts.net

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -47,7 +47,7 @@ export class OidcMiddleware {
           const assignClaimUrlWithClaimId = buildAssignClaimUrlWithId(req, app);
           return res.redirect(assignClaimUrlWithClaimId);
         }
-        if (app.locals.user?.roles?.includes(citizenRole)) {
+        if (req.session.user?.roles?.includes(citizenRole)) {
           return res.redirect(DASHBOARD_URL);
         }
         return res.redirect(UNAUTHORISED_URL);
@@ -58,7 +58,7 @@ export class OidcMiddleware {
 
     app.get(SIGN_OUT_URL, (req: AppRequest, res: Response) => {
       const params = new URLSearchParams({
-        'id_token_hint': app.locals.user?.accessToken,
+        'id_token_hint': req.session.user?.accessToken,
         'post_logout_redirect_uri': applicationUrl,
       });
 
@@ -71,8 +71,9 @@ export class OidcMiddleware {
     });
 
     app.use((req: Request, res: Response, next: NextFunction) => {
-      if (app.locals.user) {
-        if (app.locals.user.roles?.includes(citizenRole)) {
+      const appReq: AppRequest = <AppRequest>req;
+      if (appReq.session?.user) {
+        if (appReq.session.user.roles?.includes(citizenRole)) {
           return next();
         }
         return res.redirect(DASHBOARD_URL);
