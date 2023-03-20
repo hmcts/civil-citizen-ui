d9249a08b56f6c881976b46bac41aea0c5c212a6
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-03-20 13:31:30 +0000

    CIV-0000 change aat urls

--- bin/variables/load-aat-environment-variables.sh
+++ bin/variables/load-aat-environment-variables.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-aat.service.aat.platform.hmcts.net"
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-aat.aat.platform.hmcts.net"
