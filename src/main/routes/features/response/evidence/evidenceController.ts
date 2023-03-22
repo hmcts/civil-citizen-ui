cd536b139b1d04f7c00736e91dda77caf62bacc7
laperla11 <kegilmez@gmail.com>
2023-03-22 09:56:11 +0000

    fixed the bug on evidence controller

--- src/main/routes/features/response/evidence/evidenceController.ts
+++ src/main/routes/features/response/evidence/evidenceController.ts
@@ -22,9 +22,9 @@ function renderView(form: GenericForm<Evidence>, res: Response): void {
 evidenceController.get(CITIZEN_EVIDENCE_URL, async (req, res, next: NextFunction) => {
   try {
     const evidence = await getEvidence(req.params.id);
-    const form: Evidence = new Evidence();
-    if (evidence.evidenceItem?.length < INIT_ROW_COUNT) {
-      form.setRows(INIT_ROW_COUNT - evidence.evidenceItem?.length);
+    const form: Evidence = Object.assign(new Evidence(), evidence)
+    if (form.evidenceItem.length < INIT_ROW_COUNT) {
+      form.setRows(INIT_ROW_COUNT - form.evidenceItem.length);
     }
     renderView(new GenericForm<Evidence>(form), res);
   } catch (error) {
