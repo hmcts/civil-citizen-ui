df0554ba75512c5b6c182795f3bee37dfdc8b013
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-03-28 11:21:07 +0100

    CIV-7617: Update from review

--- src/main/services/translation/convertToCUI/convertToCUICareerAllowance.ts
+++
@@ -1,8 +0,0 @@
-import {YesNoUpperCamelCase} from 'form/models/yesNo';
-import {GenericYesNo} from 'form/models/genericYesNo';
-import {toCUIGenericYesNo} from 'services/translation/convertToCUI/convertToCUIYesNo';
-
-export const toCUICarerAllowanceCredit = (careerAllowance: YesNoUpperCamelCase): GenericYesNo => {
-  if (careerAllowance) return toCUIGenericYesNo(careerAllowance);
-};
-
--- src/main/services/translation/convertToCUI/convertToCUISelfEmploymentTaxPayment.ts
+++ src/main/services/translation/convertToCUI/convertToCUISelfEmploymentTaxPayment.ts
@@ -3,7 +3,7 @@ import {TaxPayments} from 'models/taxPayments';
 import {toCUIBoolean} from 'services/translation/convertToCUI/convertToCUIYesNo';
 
 export const toCUISelfEmploymentTaxPayment = (selfEmploymentDetails: CCDSelfEmploymentDetails): TaxPayments => {
-  if (!selfEmploymentDetails)
+  if (selfEmploymentDetails)
   {
     return {
       owed: toCUIBoolean(selfEmploymentDetails.isBehindOnTaxPayment),
