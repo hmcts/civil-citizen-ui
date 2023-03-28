cfd3d4556466543808b1f06133185ef49f4d2651
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-03-28 11:27:49 +0200

    CIV-7613 : Translate correspondence address

--- src/main/services/translation/convertToCUI/cuiTranslation.ts
+++ src/main/services/translation/convertToCUI/cuiTranslation.ts
@@ -4,13 +4,18 @@ import {toCUIClaimDetails} from 'services/translation/convertToCUI/convertToCUIC
 import {toCUIEvidence} from 'services/translation/convertToCUI/convertToCUIEvidence';
 import {toCUIParty} from 'services/translation/convertToCUI/convertToCUIParty';
 import {toCUIMediation} from 'services/translation/convertToCUI/convertToCUIMediation';
+import {YesNo} from 'form/models/yesNo';
+import {toCUIAddress} from 'services/translation/convertToCUI/convertToCUIAddress';
 
-export const translateCCDCaseDatatoCUIModel = (ccdClaim: CCDClaim): Claim => {
+export const translateCCDCaseDataToCUIModel = (ccdClaim: CCDClaim): Claim => {
   const claim: Claim = Object.assign(new Claim(), ccdClaim);
   claim.claimDetails = toCUIClaimDetails(ccdClaim);
   claim.evidence = toCUIEvidence(ccdClaim?.speclistYourEvidenceList);
   claim.applicant1 = toCUIParty(ccdClaim?.applicant1);
   claim.respondent1 = toCUIParty(ccdClaim?.respondent1);
+  claim.respondent1.partyDetails.correspondenceAddress = toCUIAddress(ccdClaim.respondent1LiPResponse?.respondent1LiPCorrespondenceAddress);
+  claim.respondent1.partyDetails.provideCorrespondenceAddress = claim.respondent1.partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
+  claim.respondent1.partyDetails.postToThisAddress = claim.respondent1.partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
   claim.mediation = toCUIMediation(ccdClaim?.respondent1LiPResponse?.respondent1MediationLiPResponse);
   return claim;
 };
