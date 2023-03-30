7e3a4425be2453a1edc44052ec1a5cccdf5207d4
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-03-30 10:33:48 +0200

    CIV-7613 : Remove unused method

--- src/main/services/translation/convertToCUI/cuiTranslation.ts
+++ src/main/services/translation/convertToCUI/cuiTranslation.ts
@@ -4,10 +4,6 @@ import {toCUIClaimDetails} from 'services/translation/convertToCUI/convertToCUIC
 import {toCUIEvidence} from 'services/translation/convertToCUI/convertToCUIEvidence';
 import {toCUIParty, toCUIPartyRespondent} from 'services/translation/convertToCUI/convertToCUIParty';
 import {toCUIMediation} from 'services/translation/convertToCUI/convertToCUIMediation';
-import {YesNo} from 'form/models/yesNo';
-import {toCUIAddress} from 'services/translation/convertToCUI/convertToCUIAddress';
-import {PartyDetails} from 'form/models/partyDetails';
-import {CCDRespondentLiPResponse} from 'models/ccdResponse/ccdRespondentLiPResponse';
 import {toCUIStatementOfMeans} from 'services/translation/convertToCUI/convertToCUIStatementOfMeans';
 
 export const translateCCDCaseDataToCUIModel = (ccdClaim: CCDClaim): Claim => {
@@ -21,9 +17,3 @@ export const translateCCDCaseDataToCUIModel = (ccdClaim: CCDClaim): Claim => {
   return claim;
 };
 
-export const convertExtraPartyDetailsFieldsForRespondent1 = (partyDetails: PartyDetails, ccdRespondent1: CCDRespondentLiPResponse) => {
-  partyDetails.correspondenceAddress = toCUIAddress(ccdRespondent1?.respondent1LiPCorrespondenceAddress);
-  partyDetails.provideCorrespondenceAddress = partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
-  partyDetails.postToThisAddress = partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
-  partyDetails.contactPerson = ccdRespondent1?.respondent1LiPContactPerson;
-};
