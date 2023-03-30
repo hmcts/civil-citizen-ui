76c3bb86feb20ef0d94ea3038fb11e5b10932382
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-03-30 09:29:50 +0200

    CIV-7613 : Refactor cuiTranslation methods

--- src/main/services/translation/convertToCUI/cuiTranslation.ts
+++ src/main/services/translation/convertToCUI/cuiTranslation.ts
@@ -2,20 +2,28 @@ import {Claim} from 'models/claim';
 import {CCDClaim} from 'models/civilClaimResponse';
 import {toCUIClaimDetails} from 'services/translation/convertToCUI/convertToCUIClaimDetails';
 import {toCUIEvidence} from 'services/translation/convertToCUI/convertToCUIEvidence';
-import {toCUIParty} from 'services/translation/convertToCUI/convertToCUIParty';
+import {toCUIParty, toCUIPartyRespondent} from 'services/translation/convertToCUI/convertToCUIParty';
 import {toCUIMediation} from 'services/translation/convertToCUI/convertToCUIMediation';
 import {YesNo} from 'form/models/yesNo';
 import {toCUIAddress} from 'services/translation/convertToCUI/convertToCUIAddress';
+import {PartyDetails} from 'form/models/partyDetails';
+import {CCDRespondentLiPResponse} from 'models/ccdResponse/ccdRespondentLiPResponse';
+import {toCUIStatementOfMeans} from 'services/translation/convertToCUI/convertToCUIStatementOfMeans';
 
 export const translateCCDCaseDataToCUIModel = (ccdClaim: CCDClaim): Claim => {
   const claim: Claim = Object.assign(new Claim(), ccdClaim);
   claim.claimDetails = toCUIClaimDetails(ccdClaim);
   claim.evidence = toCUIEvidence(ccdClaim?.speclistYourEvidenceList);
   claim.applicant1 = toCUIParty(ccdClaim?.applicant1);
-  claim.respondent1 = toCUIParty(ccdClaim?.respondent1);
-  claim.respondent1.partyDetails.correspondenceAddress = toCUIAddress(ccdClaim.respondent1LiPResponse?.respondent1LiPCorrespondenceAddress);
-  claim.respondent1.partyDetails.provideCorrespondenceAddress = claim.respondent1.partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
-  claim.respondent1.partyDetails.postToThisAddress = claim.respondent1.partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
+  claim.respondent1 = toCUIPartyRespondent(ccdClaim?.respondent1,ccdClaim?.respondent1LiPResponse);
   claim.mediation = toCUIMediation(ccdClaim?.respondent1LiPResponse?.respondent1MediationLiPResponse);
+  claim.statementOfMeans = toCUIStatementOfMeans(ccdClaim);
   return claim;
+};
+
+export const convertExtraPartyDetailsFieldsForRespondent1 = (partyDetails: PartyDetails, ccdRespondent1: CCDRespondentLiPResponse) => {
+  partyDetails.correspondenceAddress = toCUIAddress(ccdRespondent1?.respondent1LiPCorrespondenceAddress);
+  partyDetails.provideCorrespondenceAddress = partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
+  partyDetails.postToThisAddress = partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
+  partyDetails.contactPerson = ccdRespondent1?.respondent1LiPContactPerson;
 };
