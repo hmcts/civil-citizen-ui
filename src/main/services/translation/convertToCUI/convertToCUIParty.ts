28ad812d9f473ce7caf081daa821862e98c29efd
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-03-30 09:32:48 +0200

    CIV-7613 : Refactor toCUIParty with new dateOfBirth converter

--- src/main/services/translation/convertToCUI/convertToCUIParty.ts
+++ src/main/services/translation/convertToCUI/convertToCUIParty.ts
@@ -1,11 +1,13 @@
-import {CCDParty} from '../../../common/models/ccdResponse/ccdParty';
-import {Party} from '../../../common/models/party';
-import {PartyType} from '../../../common/models/partyType';
-import {PartyDetails} from '../../../common/form/models/partyDetails';
-import {PartyPhone} from '../../../common/models/PartyPhone';
-import {Email} from '../../../common/models/Email';
-import {CitizenDate} from '../../../common/form/models/claim/claimant/citizenDate';
+import {CCDParty} from 'models/ccdResponse/ccdParty';
+import {Party} from 'models/party';
+import {PartyType} from 'models/partyType';
+import {PartyDetails} from 'form/models/partyDetails';
+import {PartyPhone} from 'models/PartyPhone';
+import {Email} from 'models/Email';
+import {CitizenDate} from 'form/models/claim/claimant/citizenDate';
 import {toCUIAddress} from 'services/translation/convertToCUI/convertToCUIAddress';
+import {CCDRespondentLiPResponse} from 'models/ccdResponse/ccdRespondentLiPResponse';
+import {YesNo} from 'form/models/yesNo';
 
 export const toCUIParty = (ccdParty: CCDParty): Party => {
   const cuiParty = new Party();
@@ -14,14 +16,14 @@ export const toCUIParty = (ccdParty: CCDParty): Party => {
     cuiParty.partyDetails.individualFirstName = ccdParty?.individualFirstName ? ccdParty.individualFirstName : undefined;
     cuiParty.partyDetails.individualLastName = ccdParty.individualLastName ? ccdParty?.individualLastName : undefined;
     cuiParty.partyDetails.individualTitle = ccdParty.individualTitle ? ccdParty?.individualTitle : undefined;
-    cuiParty.dateOfBirth = new CitizenDate();
-    cuiParty.dateOfBirth.date = ccdParty?.individualDateOfBirth ? new Date(ccdParty.individualDateOfBirth) : undefined;
+    const auxDate = new Date(ccdParty.individualDateOfBirth);
+    cuiParty.dateOfBirth = ccdParty?.individualDateOfBirth ? new CitizenDate(auxDate.getDate().toString(), (auxDate.getMonth()+1).toString(), auxDate.getFullYear().toString()) : undefined;
   } else if (ccdParty?.type === PartyType.SOLE_TRADER) {
     cuiParty.partyDetails.individualFirstName = ccdParty.soleTraderFirstName ? ccdParty?.soleTraderFirstName : undefined;
     cuiParty.partyDetails.individualLastName = ccdParty.soleTraderLastName ? ccdParty?.soleTraderLastName : undefined;
     cuiParty.partyDetails.individualTitle = ccdParty.soleTraderTitle ? ccdParty?.soleTraderTitle : undefined;
-    cuiParty.dateOfBirth = new CitizenDate();
-    cuiParty.dateOfBirth.date = ccdParty.soleTraderDateOfBirth ? new Date(ccdParty?.soleTraderDateOfBirth) : undefined;
+    const auxDate = new Date(ccdParty.soleTraderDateOfBirth);
+    cuiParty.dateOfBirth = ccdParty?.soleTraderDateOfBirth ? new CitizenDate(auxDate.getDate().toString(), (auxDate.getMonth() + 1).toString(), auxDate.getFullYear().toString()) : undefined;
     cuiParty.partyDetails.soleTraderTradingAs = ccdParty.soleTraderTradingAs ? ccdParty?.soleTraderTradingAs : undefined;
   } else if (ccdParty?.type === PartyType.COMPANY) {
     cuiParty.partyDetails.partyName = ccdParty?.partyName ? ccdParty.partyName : undefined;
@@ -32,5 +34,13 @@ export const toCUIParty = (ccdParty: CCDParty): Party => {
   cuiParty.emailAddress = ccdParty?.partyEmail ? new Email(ccdParty?.partyEmail) : undefined;
   cuiParty.type = ccdParty?.type ? ccdParty?.type : undefined;
   cuiParty.partyDetails.primaryAddress = toCUIAddress(ccdParty?.primaryAddress);
+  return cuiParty;
+};
+
+export const toCUIPartyRespondent = (ccdParty: CCDParty, ccdRespondent1: CCDRespondentLiPResponse): Party => {
+  const cuiParty: Party =toCUIParty(ccdParty);
+  cuiParty.partyDetails.correspondenceAddress = toCUIAddress(ccdRespondent1?.respondent1LiPCorrespondenceAddress);
+  cuiParty.partyDetails.postToThisAddress = cuiParty.partyDetails.correspondenceAddress ? YesNo.YES : YesNo.NO;
+  cuiParty.partyDetails.contactPerson = ccdRespondent1?.respondent1LiPContactPerson;
   return cuiParty;
 };
