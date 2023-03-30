a35f6b2be65394002450aaa992d00b969cdd48a8
Kurt Plaschke <k.plaschke@hull.ac.uk>
2023-03-30 15:04:12 +0100

    added service request calls to create claim test

--- src/test/functionalTests/specClaimHelpers/fixtures/events/createClaimSpec.js
+++ src/test/functionalTests/specClaimHelpers/fixtures/events/createClaimSpec.js
@@ -206,4 +206,19 @@ module.exports = {
 
     return userData;
   },
+
+  serviceUpdateDto: (caseId, paymentStatus) => {
+    return {
+      service_request_reference: '1324646546456',
+      ccd_case_number: caseId,
+      service_request_amount: '167.00',
+      service_request_status: paymentStatus,
+      payment: {
+        payment_amount: 167.00,
+        payment_reference: '13213223',
+        payment_method: 'by account',
+        case_reference: 'example of case ref'
+      }
+    };
+  },
 };
