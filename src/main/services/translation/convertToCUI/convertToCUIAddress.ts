03dc5181e58e630cea4514617f769d1dc31bc3ba
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-03-30 13:32:12 +0200

    CIV-7613 : Refactor convert address

--- src/main/services/translation/convertToCUI/convertToCUIAddress.ts
+++ src/main/services/translation/convertToCUI/convertToCUIAddress.ts
@@ -3,6 +3,6 @@ import {Address} from 'form/models/address';
 
 export const toCUIAddress = (address: CCDAddress) : Address => {
   if(address){
-    return new Address(address?.AddressLine1, address?.AddressLine2, address?.AddressLine3, address?.PostTown, address?.PostCode);
+    return new Address(address.AddressLine1, address.AddressLine2, address.AddressLine3, address.PostTown, address.PostCode);
   }
 };
