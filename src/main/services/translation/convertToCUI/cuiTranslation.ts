88d6a6d564019c6a5f104e4ac96296ba7379f254
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-04-03 12:30:42 +0100

    add sdoOrderDocument on cuiTranslation.ts

--- src/main/services/translation/convertToCUI/cuiTranslation.ts
+++ src/main/services/translation/convertToCUI/cuiTranslation.ts
@@ -16,6 +16,7 @@ export const translateCCDCaseDataToCUIModel = (ccdClaim: CCDClaim): Claim => {
   claim.mediation = toCUIMediation(ccdClaim?.respondent1LiPResponse?.respondent1MediationLiPResponse);
   claim.statementOfMeans = toCUIStatementOfMeans(ccdClaim);
   claim.claimBilingualLanguagePreference = toCUIClaimBilingualLangPreference(ccdClaim?.respondent1LiPResponse?.respondent1ResponseLanguage);
+  claim.sdoOrderDocument = ccdClaim?.sdoOrderDocument;
   return claim;
 };
 
