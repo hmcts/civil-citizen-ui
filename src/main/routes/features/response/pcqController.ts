a8db6b00f7b01554758a0393f87a4383d78e808c
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-04-04 07:20:52 +0100

    add POST handler to pcqController

--- src/main/routes/features/response/pcqController.ts
+++ src/main/routes/features/response/pcqController.ts
@@ -1,18 +1,29 @@
 import {NextFunction, Request, Response, Router} from 'express';
 import {PCQ_URL} from 'routes/urls';
-import {getCaseDataFromStore, saveDraftClaim} from 'modules/draft-store/draftStoreService';
+import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {Claim} from 'models/claim';
+import {YesNoUpperCamelCase} from 'form/models/yesNo';
 
 const pcqViewPath = 'features/response/pcq';
 const pcqController = Router();
 
 pcqController.get(PCQ_URL, async (req: Request, res: Response, next: NextFunction) => {
+  res.render(pcqViewPath, {caseId: req.params.id});
+});
+
+pcqController.post(PCQ_URL, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const claim: Claim = await getCaseDataFromStore(req.params.id);
 
-    claim.pcqPageSeen = true;
-    await saveDraftClaim(claim.id, claim);
-    res.render(pcqViewPath);
+    claim.pcqPageSeen = req.body.test;
+    console.log(req.body.text);
+
+    if(req.body.test === YesNoUpperCamelCase.YES) {
+      console.log('TUEEEEE');
+    } else {
+      console.log('false');
+    }
+
   } catch (error) {
     next(error);
   }
