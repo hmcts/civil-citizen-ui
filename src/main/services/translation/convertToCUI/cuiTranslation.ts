88766b80cb26ce4fc3bb9762543813af20e57770
Kadir <kegilmez@gmail.com>
2023-04-05 09:44:09 +0100

    translate first 3 fields

--- src/main/services/translation/convertToCUI/cuiTranslation.ts
+++ src/main/services/translation/convertToCUI/cuiTranslation.ts
@@ -6,6 +6,10 @@ import {toCUIParty, toCUIPartyRespondent} from 'services/translation/convertToCU
 import {toCUIMediation} from 'services/translation/convertToCUI/convertToCUIMediation';
 import {toCUIStatementOfMeans} from 'services/translation/convertToCUI/convertToCUIStatementOfMeans';
 import {toCUIClaimBilingualLangPreference} from 'services/translation/convertToCUI/convertToCUIRespondentLiPResponse';
+import {DirectionQuestionnaire} from 'common/models/directionsQuestionnaire/directionQuestionnaire';
+import {Hearing} from 'common/models/directionsQuestionnaire/hearing/hearing';
+import {ConsiderClaimantDocuments} from 'common/models/directionsQuestionnaire/hearing/considerClaimantDocuments';
+import {toCUIGenericYesNo, toCUIYesNo} from 'services/translation/convertToCUI/convertToCUIYesNo';
 
 export const translateCCDCaseDataToCUIModel = (ccdClaim: CCDClaim): Claim => {
   const claim: Claim = Object.assign(new Claim(), ccdClaim);
@@ -16,6 +20,22 @@ export const translateCCDCaseDataToCUIModel = (ccdClaim: CCDClaim): Claim => {
   claim.mediation = toCUIMediation(ccdClaim?.respondent1LiPResponse?.respondent1MediationLiPResponse);
   claim.statementOfMeans = toCUIStatementOfMeans(ccdClaim);
   claim.claimBilingualLanguagePreference = toCUIClaimBilingualLangPreference(ccdClaim?.respondent1LiPResponse?.respondent1ResponseLanguage);
+  claim.directionQuestionnaire = toCUIDirectionQuestionnaire(ccdClaim);
   return claim;
 };
+
+export function toCUIDirectionQuestionnaire(ccdClaim: CCDClaim): DirectionQuestionnaire {
+  if (ccdClaim) {
+    const directionQuestionnaire = new DirectionQuestionnaire();
+    directionQuestionnaire.hearing = new Hearing();
+    directionQuestionnaire.hearing.triedToSettle = toCUIGenericYesNo(ccdClaim.respondent1LiPResponse?.respondent1DQExtraDetails?.triedToSettle);
+    directionQuestionnaire.hearing.requestExtra4weeks = toCUIGenericYesNo(ccdClaim.respondent1LiPResponse?.respondent1DQExtraDetails?.requestExtra4weeks);
+    directionQuestionnaire.hearing.considerClaimantDocuments =
+    {
+      option: toCUIYesNo(ccdClaim.respondent1LiPResponse?.respondent1DQExtraDetails?.considerClaimantDocuments),
+      details: ccdClaim.respondent1LiPResponse?.respondent1DQExtraDetails?.considerClaimantDocumentsDetails,
+    } as ConsiderClaimantDocuments;
+    return directionQuestionnaire;
+  }
+}
 
