d9ca7ec02f9e72037e924600fa9617614ac747e0
Pettedson John <pettedson.john@hmcts.net>
2023-04-20 07:42:20 +0100

    Testing the pa11y files with the pipepline

---
+++ src/test/a11y/constants.ts
@@ -0,0 +1,4 @@
+export const PageUrls = {
+  HOME: '/',
+  DASHBOARD: '/dashboard',
+} as const;
---
+++ src/test/a11y/dummy-test.ts
@@ -0,0 +1,5 @@
+describe('Fake test to work around an issue', () => {
+  it('does nothing', async () => {
+    //see https://github.com/facebook/jest/issues/5783#issuecomment-450626450
+  });
+});
