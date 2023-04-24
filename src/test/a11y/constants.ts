817b139a6a7ef2853fb7854ade7935a63c86a419
Pettedson John <pettedson.john@hmcts.net>
2023-04-24 22:18:47 +0100

    Implementing the Framework for the Accessibility Testing....

--- src/test/a11y/constants.ts
+++ src/test/a11y/constants.ts
@@ -1,4 +1,10 @@
 export const PageUrls = {
+
+  //Page URL's for the CUI Common Pages.
   HOME: '/',
   DASHBOARD: '/dashboard',
+
+  //Page URL's for the CUI Functional Pages.
+
+  //Page URL's for the Case Progression Functional Pages.
 } as const;
