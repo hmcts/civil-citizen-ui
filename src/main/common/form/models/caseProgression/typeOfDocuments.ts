b6ace597e2d2f14d255ffd60573b90feed5a77bc
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-04-24 10:46:12 +0100

    create type of documents

---
+++ src/main/common/form/models/caseProgression/typeOfDocuments.ts
@@ -0,0 +1,10 @@
+class TypeOfDocuments {
+  value ?: string;
+  text ?: string;
+
+  constructor(value: string, text: string) {
+    this.value = value;
+    this.text = text;
+  }
+}
+
---
+++ src/main/routes/features/caseProgression/typeOfDocumentsController.ts
@@ -0,0 +1,76 @@
+import {NextFunction, Request, Response, Router} from 'express';
+import { CLAIM_CONFIRMATION_URL, TYPES_OF_DOCUMENTS_URL} from '../../urls';
+import {
+  getStatementOfTruth,
+  getSummarySections,
+  saveStatementOfTruth,
+} from 'services/features/claim/checkAnswers/checkAnswersService';
+import {deleteDraftClaimFromStore, getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
+import {Claim} from 'common/models/claim';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+import {AppRequest} from 'common/models/AppRequest';
+import {submitClaim} from 'services/features/claim/submission/submitClaim';
+import {GenericForm} from 'common/form/models/genericForm';
+import {StatementOfTruthForm} from 'common/form/models/statementOfTruth/statementOfTruthForm';
+import {QualifiedStatementOfTruth} from 'common/form/models/statementOfTruth/qualifiedStatementOfTruth';
+import {YesNo} from 'common/form/models/yesNo';
+
+const checkAnswersViewPath = 'features/claim/check-answers';
+const typeOfDocumentsController = Router();
+const paymentUrl = 'https://www.payments.service.gov.uk/card_details/:id';
+
+function renderView(res: Response, form: GenericForm<StatementOfTruthForm> | GenericForm<QualifiedStatementOfTruth>, claim: Claim, userId: string, lang: string) {
+  const summarySections = getSummarySections(userId, claim, lang);
+  const signatureType = form.model?.type;
+  let payment;
+  if (claim.claimDetails?.helpWithFees?.option === YesNo.NO) {
+    payment = 100;
+  }
+  res.render(checkAnswersViewPath, {
+    form, summarySections, signatureType,
+    payment,
+  });
+}
+
+typeOfDocumentsController.get(TYPES_OF_DOCUMENTS_URL,
+  // AllResponseTasksCompletedGuard.apply(CLAIM_INCOMPLETE_SUBMISSION_URL), TODO implement guard
+  async (req: AppRequest, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.session?.user?.id;
+      const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+      const claim = await getCaseDataFromStore(userId);
+      const form = new GenericForm(getStatementOfTruth(claim));
+      renderView(res, form, claim, userId, lang);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+typeOfDocumentsController.post(TYPES_OF_DOCUMENTS_URL, async (req: Request | AppRequest, res: Response, next: NextFunction) => {
+  try {
+    const userId = (<AppRequest>req).session?.user?.id;
+    const isFullAmountRejected = (req.body?.isFullAmountRejected === 'true');
+    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+    const form = new GenericForm((req.body.type === 'qualified')
+      ? new QualifiedStatementOfTruth(isFullAmountRejected, req.body.signed, req.body.directionsQuestionnaireSigned, req.body.signerName, req.body.signerRole)
+      : new StatementOfTruthForm(isFullAmountRejected, req.body.type, req.body.signed, req.body.directionsQuestionnaireSigned));
+    const claim = await getCaseDataFromStore(userId);
+    await form.validate();
+    if (form.hasErrors()) {
+      renderView(res, form, claim, userId, lang);
+    } else {
+      await saveStatementOfTruth(userId, form.model);
+      const submittedClaim = await submitClaim(<AppRequest>req);
+      await deleteDraftClaimFromStore(userId);
+      if (claim.claimDetails.helpWithFees.option === YesNo.NO) {
+        res.redirect(constructResponseUrlWithIdParams(userId, paymentUrl));
+      } else {
+        res.redirect(constructResponseUrlWithIdParams(submittedClaim.id, CLAIM_CONFIRMATION_URL));
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default typeOfDocumentsController;
