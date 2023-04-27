f2edfce715a32636db9fb0bf364918f9a650408e
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-04-27 13:21:04 +0200

    CIV-8203 : Fix debtRespiteEndDate.ts

--- src/main/common/models/breathingSpace/debtRespiteEndDate.ts
+++ src/main/common/models/breathingSpace/debtRespiteEndDate.ts
@@ -1,35 +1,39 @@
-import {IsDate, Max, Min, Validate, ValidateIf} from 'class-validator';
-import {OptionalDateFourDigitValidator} from '../../../common/form/validators/optionalDateFourDigitValidator';
-import {OptionalDateNotInPastValidator} from '../../../common/form/validators/optionalDateNotInPastValidator';
-import {DateConverter} from '../../../common/utils/dateConverter';
-import {toNumberOrUndefined} from '../../../common/utils/numberConverter';
+import {IsDate, IsInt, Max, Min, Validate, ValidateIf} from 'class-validator';
+import {OptionalDateFourDigitValidator} from 'form/validators/optionalDateFourDigitValidator';
+import {OptionalDateNotInPastValidator} from 'form/validators/optionalDateNotInPastValidator';
+import {DateConverter} from 'common/utils/dateConverter';
+import {toNumberOrString} from 'common/utils/numberConverter';
 
 export class DebtRespiteEndDate {
 
-  @ValidateIf(o => (o.day > 0 && o.day <32 && o.month > 0 && o.month < 13 && o.year > 999))
+  @ValidateIf(o => (o.day > 0 && o.day <32 && o.month > 0 && o.month < 13 && o.year > 1871))
   @IsDate({message: 'ERRORS.VALID_DATE'})
   @Validate(OptionalDateNotInPastValidator, {message: 'ERRORS.VALID_DEBT_RESPITE_END_DATE'})
     date?: Date;
 
   @ValidateIf(o => (o.day || o.month || o.year))
+  @IsInt({message: 'ERRORS.VALID_DAY'})
   @Min(1, {message:'ERRORS.VALID_DAY' })
   @Max(31, {message: 'ERRORS.VALID_DAY'})
-    day: number;
+    day: number|string;
 
   @ValidateIf(o => (o.day || o.month || o.year))
+  @IsInt({message: 'ERRORS.VALID_MONTH'})
   @Min(1, {message: 'ERRORS.VALID_MONTH' })
   @Max(12, {message: 'ERRORS.VALID_MONTH' })
-    month: number;
+    month: number|string;
 
   @ValidateIf(o => (o.day || o.month || o.year))
+  @IsInt({message: 'ERRORS.VALID_YEAR'})
   @Validate(OptionalDateFourDigitValidator, {message: 'ERRORS.VALID_FOUR_DIGIT_YEAR'})
+  @Min(1872, {message: 'ERRORS.VALID_YEAR'})
   @Max(9999, {message: 'ERRORS.VALID_YEAR' })
-    year: number;
+    year: number|string;
 
   constructor(day?: string, month?: string, year?: string) {
     this.date = DateConverter.convertToDate(year, month, day);
-    this.year = toNumberOrUndefined(year);
-    this.month = toNumberOrUndefined(month);
-    this.day = toNumberOrUndefined(day);
+    this.year = toNumberOrString(year);
+    this.month = toNumberOrString(month);
+    this.day = toNumberOrString(day);
   }
 }
