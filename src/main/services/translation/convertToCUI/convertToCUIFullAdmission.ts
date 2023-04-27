5c8ab2bb8d3801c23f8d7b7110e7f2975ba2f1ac
hmcts-version1-domini <domini.mattos@version1.com>
2023-04-27 23:29:08 +0200

    Fix lint

--- src/main/services/translation/convertToCUI/convertToCUIFullAdmission.ts
+++ src/main/services/translation/convertToCUI/convertToCUIFullAdmission.ts
@@ -1,9 +1,9 @@
-import {CCDClaim} from "models/civilClaimResponse";
-import {FullAdmission} from "models/fullAdmission";
-import {toCUIPaymentIntention} from "services/translation/convertToCUI/convertToCUIPartialAdmission";
+import {CCDClaim} from 'models/civilClaimResponse';
+import {FullAdmission} from 'models/fullAdmission';
+import {toCUIPaymentIntention} from 'services/translation/convertToCUI/convertToCUIPartialAdmission';
 
 export const toCUIFullAdmission = (ccdClaim: CCDClaim): FullAdmission => {
   const fa: FullAdmission = new FullAdmission();
   fa.paymentIntention = toCUIPaymentIntention(ccdClaim);
   return fa;
-}
+};
--- src/test/unit/services/translation/convertToCUI/convertToCUIFullAdmission.test.ts
+++ src/test/unit/services/translation/convertToCUI/convertToCUIFullAdmission.test.ts
@@ -1,8 +1,8 @@
-import {CCDClaim} from "models/civilClaimResponse";
-import {toCUIFullAdmission} from "services/translation/convertToCUI/convertToCUIFullAdmission";
-import {CCDPaymentOption} from "models/ccdResponse/ccdPaymentOption";
-import {PaymentOptionType} from "form/models/admission/paymentOption/paymentOptionType";
-import {FullAdmission} from "models/fullAdmission";
+import {CCDClaim} from 'models/civilClaimResponse';
+import {toCUIFullAdmission} from 'services/translation/convertToCUI/convertToCUIFullAdmission';
+import {CCDPaymentOption} from 'models/ccdResponse/ccdPaymentOption';
+import {PaymentOptionType} from 'form/models/admission/paymentOption/paymentOptionType';
+import {FullAdmission} from 'models/fullAdmission';
 
 describe('toCUIFullAdmission', () => {
   it('should translate CCDRepaymentPlanFrequency to CUI repaymentPlanFrequency with undefined', () => {
