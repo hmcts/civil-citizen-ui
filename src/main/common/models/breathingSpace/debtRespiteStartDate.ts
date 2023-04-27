9a1ebd3af3299c87d91a6d43de6e68fd4926af68
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-04-27 13:02:45 +0200

    CIV-8203 : Kadir's approach

--- src/main/common/models/breathingSpace/debtRespiteStartDate.ts
+++ src/main/common/models/breathingSpace/debtRespiteStartDate.ts
@@ -1,15 +1,16 @@
-import {IsDate, isNumber, Max, Min, Validate, ValidateIf} from 'class-validator';
+import {IsDate, IsInt, Max, Min, Validate, ValidateIf} from 'class-validator';
 import {OptionalDateFourDigitValidator} from 'form/validators/optionalDateFourDigitValidator';
 import {DateConverter} from 'common/utils/dateConverter';
 import {OptionalDateInPastValidator} from 'form/validators/optionalDateInPastValidator';
-import {toNumberOrUndefined} from 'common/utils/numberConverter';
+import {toNumberOrString} from 'common/utils/numberConverter';
+import {ValidationArgs} from 'common/form/models/genericForm';
 
 const generateErrorMessage = (messageName: string): string => {
   return messageName ? messageName : 'ERRORS.VALID_DATE_START_NOT_AFTER_TODAY';
 };
 
 const withMessage = (buildErrorFn: (messageName: string) => string) => {
-  return (args: any): string => {
+  return (args: ValidationArgs<DebtRespiteStartDate>): string => {
     return buildErrorFn(args.object.messageName);
   };
 };
@@ -17,39 +18,35 @@ const withMessage = (buildErrorFn: (messageName: string) => string) => {
 export class DebtRespiteStartDate {
   messageName?: string;
 
-  @ValidateIf(o => (o.day > 0 && o.day <32 && o.month > 0 && o.month < 13 && o.year > 1872))
+  @ValidateIf(o => (o.day < 32 && o.month < 13 && o.year > 999))
   @IsDate({message: 'ERRORS.VALID_DATE'})
   @Validate(OptionalDateInPastValidator, {message: withMessage(generateErrorMessage)})
     date?: Date;
 
   @ValidateIf(o => (o.day || o.month || o.year))
+  @IsInt({message: 'ERRORS.VALID_DAY'})
   @Min(1, {message: 'ERRORS.VALID_DAY'})
   @Max(31, {message: 'ERRORS.VALID_DAY'})
-    day?: number;
+    day?: number|string;
 
   @ValidateIf(o => (o.day || o.month || o.year))
+  @IsInt({message: 'ERRORS.VALID_YEAR'})
   @Min(1, {message: 'ERRORS.VALID_MONTH'})
   @Max(12, {message: 'ERRORS.VALID_MONTH'})
-    month?: number;
+    month?: number|string;
 
   @ValidateIf(o => (o.day || o.month || o.year))
+  @IsInt({message: 'ERRORS.VALID_YEAR'})
   @Min(1872, {message: 'ERRORS.VALID_YEAR'})
   @Validate(OptionalDateFourDigitValidator, {message: 'ERRORS.VALID_FOUR_DIGIT_YEAR'})
-    year?: number;
+    year?: number|string;
 
   constructor(day?: string, month?: string, year?: string, messageName?: string) {
-    console.log(day);
-    console.log(month);
-    console.log(year);
+
     this.date = DateConverter.convertToDate(year, month, day);
-    this.year = toNumberOrUndefined(year);
-    this.month = toNumberOrUndefined(month);
-    this.day = toNumberOrUndefined(day);
+    this.year = toNumberOrString(year);
+    this.month = toNumberOrString(month);
+    this.day = toNumberOrString(day);
     this.messageName = messageName;
-    if(!isNumber(day) || !isNumber(month) || !isNumber(year)){
-      this.day = -1;
-      this.month = -1;
-      this.year = -1;
-    }
   }
 }
