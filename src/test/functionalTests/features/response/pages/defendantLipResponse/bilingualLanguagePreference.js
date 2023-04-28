e43c94c98cb88b03e15f58e068bcd17bc8a87c02
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-04-28 09:56:33 +0100

    Update bilingualLanguagePreference.js
    undo changes

--- src/test/functionalTests/features/response/pages/defendantLipResponse/bilingualLanguagePreference.js
+++ src/test/functionalTests/features/response/pages/defendantLipResponse/bilingualLanguagePreference.js
@@ -10,7 +10,7 @@ class BilingualLanguagePreference {
   verifyContent() {
     I.see('Do you want to respond to this claim in Welsh?');
     I.see('You can respond to this claim in Welsh. After you\'ve submitted your response, anything else you may have to do relating to the claim will be in English. You\'ll receive emails and letters in English.');
-    I.click(fields.welshAndEnglish);
+    I.click(fields.english);
     I.click('Save and continue');
   }
 }
