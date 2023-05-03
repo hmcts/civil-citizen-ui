914ab1d9d4659d1007f684d19721f3409c10907e
laarmada <sergiosalmar@yahoo.es>
2023-05-03 16:33:14 +0100

    adding model first option

---
+++ src/main/common/form/models/caseProgression/uploadDocumentstype.ts
@@ -0,0 +1,66 @@
+import {IsDefined, Validate, ValidateIf, ValidateNested} from 'class-validator';
+import {CaseDocument} from 'models/document/caseDocument';
+import {
+  EvidenceUploadDisclosure,
+  EvidenceUploadFiles,
+  EvidenceUploadTrial,
+  EvidenceUploadWitness,
+} from 'models/document/documentType';
+import {AtLeastOneCheckboxSelectedValidator} from 'form/validators/atLeastOneCheckboxSelectedValidator';
+import {YesNo} from 'form/models/yesNo';
+
+export class uploadDocumentTypeList {
+  @IsDefined({message: 'ERRORS.SELECT_YES_IF_SUPPORT'})
+    option: YesNo;
+  @ValidateIf(o => o.option === YesNo.YES)
+  @ValidateNested()
+    items?: uploadDocument;
+
+  [key: string]: YesNo | uploadDocument;
+  constructor(option?: YesNo, items?: uploadDocument) {
+    this.option = option;
+    this.items = items;
+  }
+}
+
+export interface SupportRequiredParams{
+  disclosure?: UploadDocumentTypes[],
+  witness?: UploadDocumentTypes[],
+  expert?: UploadDocumentTypes[],
+  trial?: UploadDocumentTypes[],
+}
+
+export class uploadDocument {
+
+  @ValidateNested()
+    disclosure?: UploadDocumentTypes[];
+  @ValidateNested()
+    witness?: UploadDocumentTypes[];
+  @ValidateNested()
+    expert?: UploadDocumentTypes[];
+  @ValidateNested()
+    trial?: UploadDocumentTypes[];
+  @Validate(AtLeastOneCheckboxSelectedValidator, {message: 'ERRORS.SELECT_SUPPORT' })
+    checkboxGrp?: boolean [];
+
+  [key: string]: string | UploadDocumentTypes[] | boolean[];
+  constructor(params?: SupportRequiredParams) {
+    this.disclosure = params?.disclosure;
+    this.witness = params?.witness;
+    this.expert = params?.expert;
+    this.trial = params?.trial;
+    //Todo foreach one inside of each section
+    this.checkboxGrp = [
+      params?.disclosure?.[0].selected,
+      params?.witness?.[0].selected,
+      params?.expert?.[0].selected,
+      params?.trial?.[0].selected,
+    ];
+  }
+}
+export class UploadDocumentTypes{
+  selected?: boolean;
+  caseDocument?: CaseDocument;
+  documentType?:EvidenceUploadWitness|EvidenceUploadDisclosure|EvidenceUploadFiles|EvidenceUploadTrial;
+}
+
--- src/main/common/models/document/caseDocument.ts
+++ src/main/common/models/document/caseDocument.ts
@@ -1,11 +1,17 @@
 import {Document} from './document';
-import {DocumentType} from './documentType';
+import {
+  DocumentType,
+  EvidenceUploadDisclosure,
+  EvidenceUploadFiles,
+  EvidenceUploadTrial,
+  EvidenceUploadWitness
+} from './documentType';
 
 export interface CaseDocument {
   createdBy: string;
   documentLink: Document;
   documentName: string;
-  documentType: DocumentType;
+  documentType: DocumentType |EvidenceUploadWitness|EvidenceUploadDisclosure|EvidenceUploadFiles|EvidenceUploadTrial;
   documentSize: number;
   createdDatetime: Date;
 }
