1733bcd11ed3beba1a9519f263dcfca4c73ffe29
laarmada <sergiosalmar@yahoo.es>
2023-05-04 15:28:31 +0100

    fix sonar bug

--- src/main/routes/features/caseProgression/uploadDocumentsController.ts
+++ src/main/routes/features/caseProgression/uploadDocumentsController.ts
@@ -1,17 +1,16 @@
-import {NextFunction, Router} from 'express';
+import {NextFunction, Request, Response, Router} from 'express';
 import {CP_UPLOAD_DOCUMENTS_URL} from '../../urls';
-import {Claim} from 'models/claim';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {getWitnessContent} from 'services/features/caseProgression/witnessService';
 
 const uploadDocumentsViewPath = 'features/caseProgression/upload-documents';
 const uploadDocumentsController = Router();
-uploadDocumentsController.get(CP_UPLOAD_DOCUMENTS_URL, async (req, res, next: NextFunction) => {
+uploadDocumentsController.get(CP_UPLOAD_DOCUMENTS_URL, async (req:Request, res:Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
-    const claim: Claim = await getCaseDataFromStore(claimId);
+    const claim = await getCaseDataFromStore(claimId);
 
-    if (claim && !claim.isEmpty()) {
+    if (claim) {
       const disclosureContent:string = undefined ; //TODO getDisclosureContent(claim, claimId);
       const witnessContent = getWitnessContent(claimId, claim);
       const expertContent:string = undefined ; //TODO = getExpertContent(claim, claimId);
