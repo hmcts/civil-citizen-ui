e6d1b1643b1eb29de4b6f25abfa4b27f2979cd21
laarmada <sergiosalmar@yahoo.es>
2023-05-04 15:45:56 +0100

    fix sonar bug

--- src/main/routes/features/caseProgression/uploadDocumentsController.ts
+++ src/main/routes/features/caseProgression/uploadDocumentsController.ts
@@ -1,22 +1,26 @@
-import {NextFunction, Request, Response, Router} from 'express';
+import {NextFunction, Response, Router} from 'express';
 import {CP_UPLOAD_DOCUMENTS_URL} from '../../urls';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {getWitnessContent} from 'services/features/caseProgression/witnessService';
+import {Claim} from 'models/claim';
+import {ClaimSummaryContent} from 'form/models/claimSummarySection';
 
 const uploadDocumentsViewPath = 'features/caseProgression/upload-documents';
 const uploadDocumentsController = Router();
-uploadDocumentsController.get(CP_UPLOAD_DOCUMENTS_URL, async (req:Request, res:Response, next: NextFunction) => {
+
+function renderView(claim: Claim, claimId:string, disclosureContent: string, witnessContent: ClaimSummaryContent[],expertContent: string,trialContent: string, res: Response): void {
+  res.render(uploadDocumentsViewPath, {claim, claimId, disclosureContent, witnessContent,expertContent,trialContent});
+}
+uploadDocumentsController.get(CP_UPLOAD_DOCUMENTS_URL, async (req, res, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const claim = await getCaseDataFromStore(claimId);
+    const disclosureContent:string = undefined ; //TODO getDisclosureContent(claim, claimId);
+    const witnessContent = getWitnessContent(claimId, claim);
+    const expertContent:string = undefined ; //TODO = getExpertContent(claim, claimId);
+    const trialContent:string = undefined ; //TODO = getTrialContent(claim, claimId);
+    renderView (claim, claimId, disclosureContent, witnessContent,expertContent,trialContent,res);
 
-    if (claim) {
-      const disclosureContent:string = undefined ; //TODO getDisclosureContent(claim, claimId);
-      const witnessContent = getWitnessContent(claimId, claim);
-      const expertContent:string = undefined ; //TODO = getExpertContent(claim, claimId);
-      const trialContent:string = undefined ; //TODO = getTrialContent(claim, claimId);
-      res.render(uploadDocumentsViewPath, {claim, claimId, disclosureContent, witnessContent,expertContent,trialContent});
-    }
   } catch (error) {
     next(error);
   }
