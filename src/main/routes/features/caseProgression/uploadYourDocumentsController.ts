8b28e1708415cead95136dd933880eea758c807f
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-05-08 15:38:19 +0100

    Move function to service

--- src/main/routes/features/caseProgression/uploadYourDocumentsController.ts
+++ src/main/routes/features/caseProgression/uploadYourDocumentsController.ts
@@ -1,48 +1,21 @@
 import {NextFunction, RequestHandler, Router} from 'express';
-import {UPLOAD_YOUR_DOCUMENTS_URL,TYPES_OF_DOCUMENTS_URL} from '../../urls';
+import {UPLOAD_YOUR_DOCUMENTS_URL} from '../../urls';
 import {AppRequest} from 'models/AppRequest';
 import config from 'config';
 import {CivilServiceClient} from 'client/civilServiceClient';
-import {LatestUpdateSectionBuilder} from 'models/LatestUpdateSectionBuilder/latestUpdateSectionBuilder';
-import {Claim} from 'models/claim';
+import {getUploadYourDocumentsContents} from 'services/features/caseProgression/uploadYourDocumentsContents';
 
 const uploadYourDocumentsViewPath = 'features/caseProgression/upload-your-documents';
 const uploadYourDocumentsController = Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
-function getUploadYourDocumentsContents(claimId: string, claim: Claim) {
-  return new LatestUpdateSectionBuilder()
-    .addTitle('PAGES.UPLOAD_YOUR_DOCUMENTS.TITLE')
-    .addLeadParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.CASE_REFERENCE', {claimId: claimId})
-    .addLeadParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.PARTIES', {
-      claimantName: claim.getClaimantFullName(),
-      defendantName: claim.getDefendantFullName(),
-    })
-    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.CHECK_THE_ORDER')
-    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.YOU_CANNOT_WITHDRAW')
-    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.THE_OTHER_PARTIES')
-    .addSubTitle('PAGES.UPLOAD_YOUR_DOCUMENTS.DEADLINES_FOR_UPLOADING')
-    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.CHECK_THE_ORDER_DEADLINES')
-    .addLink('PAGES.UPLOAD_YOUR_DOCUMENTS.APPLY_TO_THE_COURT', 'TODO: apply to the court URL',
-      'PAGES.UPLOAD_YOUR_DOCUMENTS.AFTER_THE_DEADLINE',
-      'PAGES.UPLOAD_YOUR_DOCUMENTS.IF_YOU_WANT_ANY')
-    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.YOU_DO_NOT_HAVE')
-    .addSubTitle('PAGES.UPLOAD_YOUR_DOCUMENTS.BEFORE_YOU_UPLOAD_YOUR')
-    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.BEFORE_YOU_UPLOAD_THE')
-    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.EACH_DOCUMENT_MUST')
-    .addStartButton('PAGES.UPLOAD_YOUR_DOCUMENTS.START_NOW', TYPES_OF_DOCUMENTS_URL
-      .replace(':id', claim.id))
-    .build();
-}
-
 uploadYourDocumentsController.get([UPLOAD_YOUR_DOCUMENTS_URL], (async (req, res, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
     if (claim && !claim.isEmpty()) {
-      const uploadYourDocumentsContents = getUploadYourDocumentsContents(claimId, claim);
-      res.render(uploadYourDocumentsViewPath, {uploadYourDocumentsContents});
+      res.render(uploadYourDocumentsViewPath, {uploadYourDocumentsContents:getUploadYourDocumentsContents(claimId, claim)});
     }
   } catch (error) {
     next(error);
---
+++ src/main/services/features/caseProgression/uploadYourDocumentsContents.ts
@@ -0,0 +1,28 @@
+import {LatestUpdateSectionBuilder} from 'models/LatestUpdateSectionBuilder/latestUpdateSectionBuilder';
+import {Claim} from 'models/claim';
+import {TYPES_OF_DOCUMENTS_URL} from 'routes/urls';
+
+export const getUploadYourDocumentsContents = (claimId: string, claim: Claim) => {
+  return new LatestUpdateSectionBuilder()
+    .addTitle('PAGES.UPLOAD_YOUR_DOCUMENTS.TITLE')
+    .addLeadParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.CASE_REFERENCE', {claimId: claimId})
+    .addLeadParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.PARTIES', {
+      claimantName: claim.getClaimantFullName(),
+      defendantName: claim.getDefendantFullName(),
+    })
+    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.CHECK_THE_ORDER')
+    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.YOU_CANNOT_WITHDRAW')
+    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.THE_OTHER_PARTIES')
+    .addSubTitle('PAGES.UPLOAD_YOUR_DOCUMENTS.DEADLINES_FOR_UPLOADING')
+    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.CHECK_THE_ORDER_DEADLINES')
+    .addLink('PAGES.UPLOAD_YOUR_DOCUMENTS.APPLY_TO_THE_COURT', 'TODO: apply to the court URL',
+      'PAGES.UPLOAD_YOUR_DOCUMENTS.AFTER_THE_DEADLINE',
+      'PAGES.UPLOAD_YOUR_DOCUMENTS.IF_YOU_WANT_ANY')
+    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.YOU_DO_NOT_HAVE')
+    .addSubTitle('PAGES.UPLOAD_YOUR_DOCUMENTS.BEFORE_YOU_UPLOAD_YOUR')
+    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.BEFORE_YOU_UPLOAD_THE')
+    .addParagraph('PAGES.UPLOAD_YOUR_DOCUMENTS.EACH_DOCUMENT_MUST')
+    .addStartButton('PAGES.UPLOAD_YOUR_DOCUMENTS.START_NOW', TYPES_OF_DOCUMENTS_URL
+      .replace(':id', claim.id))
+    .build();
+};
