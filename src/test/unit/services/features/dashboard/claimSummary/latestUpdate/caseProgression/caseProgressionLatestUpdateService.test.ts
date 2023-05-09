ac4c5af3f0415cc7d8236e72e49a09626f221690
jakiurmalik <jakiurmalik@users.noreply.github.com>
2023-05-09 10:51:27 +0100

    CIV-8115 present evidence upload update under hearing notice update

--- src/test/unit/services/features/dashboard/claimSummary/latestUpdate/caseProgression/caseProgressionLatestUpdateService.test.ts
+++ src/test/unit/services/features/dashboard/claimSummary/latestUpdate/caseProgression/caseProgressionLatestUpdateService.test.ts
@@ -1,45 +1,94 @@
-import {Claim} from 'models/claim';
 import {
-  getClaimSummaryContent,
+  getCaseProgressionLatestUpdates,
   getEvidenceUploadLatestUpdateContent, getHearingTrialUploadLatestUpdateContent,
 } from 'services/features/dashboard/claimSummary/latestUpdate/caseProgression/caseProgressionLatestUpdateService';
 import {
   buildEvidenceUploadSection, buildHearingTrialLatestUploadSection,
 } from 'services/features/dashboard/claimSummary/latestUpdate/caseProgression/latestUpdateContentBuilderCaseProgression';
 import {getCaseProgressionHearingMock} from '../../../../../../../utils/caseProgression/mockCaseProgressionHearing';
+import {CaseState} from 'form/models/claimDetails';
 
 describe('Case Progression Latest Update Content service', () => {
-  const mockClaim = require('../../../../../../../utils/mocks/civilClaimResponseMock.json');
-  const claim = new Claim();
-  const mockClaimId = '5129';
-  const caseData = Object.assign(claim, mockClaim.case_data);
+  const claim = require('../../../../../../../utils/mocks/civilClaimResponseMock.json');
+  const claimId = claim.id;
+  const claimWithSdo = {
+    ...claim,
+    state: CaseState.AWAITING_APPLICANT_INTENTION,
+    case_data: {
+      ...claim.case_data,
+      sdoOrderDocument: {
+        value: {
+          createdBy: 'test',
+          documentLink: 'test',
+          documentName: 'test',
+          documentSize: 'test',
+          documentType: 'test',
+          createdDatetime: 'test',
+        },
+        classification: 'test',
+      },
+    },
+  };
 
-  const evidenceUploadContent = getEvidenceUploadLatestUpdateContent(mockClaimId, caseData);
-  it('should return evidence upload section for latest update content', () => {
-    //when
-    const evidenceUploadSection = buildEvidenceUploadSection(claim);
-    const expectedContent = evidenceUploadSection.map((sectionContent, index) => ({
-      contentSections: sectionContent,
-      hasDivider: index < evidenceUploadSection.length - 1,
-    }));
+  it('getEvidenceUploadLatestUpdateContent: should return evidence upload section for latest update content', () => {
+    //Given
+    const evidenceUploadContentExpected = getEvidenceUploadLatestUpdateContent(claimId, claimWithSdo);
+
+    //When
+    const evidenceUploadSectionResult = buildEvidenceUploadSection(claimWithSdo);
 
     //Then
-    expect(evidenceUploadContent).toMatchObject(expectedContent);
-    expect(evidenceUploadContent[0].contentSections).toEqual(expectedContent[0].contentSections);
-    expect(evidenceUploadContent[0].contentSections.length).toEqual(expectedContent[0].contentSections.length);
+    expect(evidenceUploadContentExpected).toMatchObject(evidenceUploadSectionResult);
+    expect(evidenceUploadSectionResult[0][0].data.text).toEqual('PAGES.LATEST_UPDATE_CONTENT.EVIDENCE_UPLOAD.TITLE');
+    expect(evidenceUploadSectionResult[0].length).toEqual(4);
   });
 
-  it('should return CaseProgression hearing upload section for latest update content', () => {
+  it('getEvidenceUploadLatestUpdateContent: evidence upload should have additional content with hearing', () => {
+    //Given:
+    claimWithSdo.caseProgressionHearing = getCaseProgressionHearingMock();
+    const claimWithSdoAndHearing = {
+      ...claimWithSdo,
+      hasCaseProgressionHearingDocuments: () => true,
+      hasSdoOrderDocument: () => true,
+    };
+
+    //When
+    const evidenceUploadSectionResult = buildEvidenceUploadSection(claimWithSdoAndHearing);
+
+    //Then
+    expect(evidenceUploadSectionResult[0].length).toEqual(6);
+    expect(evidenceUploadSectionResult[0][4].data.text).toEqual('PAGES.LATEST_UPDATE_CONTENT.EVIDENCE_UPLOAD.DOCUMENTS_SUBMITTED_NOT_CONSIDERED');
+  });
+
+  it('getHearingTrialUploadLatestUpdateContent: should return CaseProgression hearing upload section for latest update content', () => {
     //Given
     claim.caseProgressionHearing = getCaseProgressionHearingMock();
     const hearingUploadSectionExpected = buildHearingTrialLatestUploadSection(claim, 'en');
-    const hearingUploadSectionExpect = getClaimSummaryContent(hearingUploadSectionExpected);
 
     //when
     const hearingUploadSectionResult = getHearingTrialUploadLatestUpdateContent(claim, 'en');
 
     //Then
-    expect(hearingUploadSectionExpect).toMatchObject(hearingUploadSectionResult);
+    expect(hearingUploadSectionExpected).toMatchObject(hearingUploadSectionResult);
+    expect(hearingUploadSectionResult[0][0].data.text).toEqual('PAGES.LATEST_UPDATE_CONTENT.CASE_PROGRESSION.TRIAL_HEARING_CONTENT.YOUR_HEARING_TITLE');
+  });
 
+  it('getCaseProgressionLatestUpdates: should return hearing notice and evidence upload contents', () => {
+    //Given:
+    claimWithSdo.caseProgressionHearing = getCaseProgressionHearingMock();
+    const claimWithSdoAndHearing = {
+      ...claimWithSdo,
+      hasCaseProgressionHearingDocuments: () => true,
+      hasSdoOrderDocument: () => true,
+    };
+
+    //When
+    const result = getCaseProgressionLatestUpdates(claimWithSdoAndHearing, 'en');
+
+    //Then
+    expect(result.length).toEqual(2);
+    expect(result[0].contentSections[0].data.text).toEqual('PAGES.LATEST_UPDATE_CONTENT.CASE_PROGRESSION.TRIAL_HEARING_CONTENT.YOUR_HEARING_TITLE');
+    expect(result[1].contentSections[0].data.text).toEqual('PAGES.LATEST_UPDATE_CONTENT.EVIDENCE_UPLOAD.TITLE');
+    expect(result[1].contentSections.length).toEqual(6);
   });
 });
