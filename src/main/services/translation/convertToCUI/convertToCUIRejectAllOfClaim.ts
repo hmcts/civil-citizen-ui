84bbe5e5d8557055b293e8b9d6f015e1b4209f41
Kadir <kegilmez@gmail.com>
2023-05-10 16:40:36 +0100

    Merge branch 'master' of https://github.com/hmcts/civil-citizen-ui into dev

---
+++ src/main/services/translation/convertToCUI/convertToCUIRejectAllOfClaim.ts
@@ -0,0 +1,14 @@
+import {RejectAllOfClaim} from 'form/models/rejectAllOfClaim';
+import {CCDClaim} from 'models/civilClaimResponse';
+import {toCUIRejectAllOfClaimType} from 'services/translation/convertToCUI/convertToCUIRejectAllOfClaimType';
+import {WhyDoYouDisagree} from 'form/models/admission/partialAdmission/whyDoYouDisagree';
+import {toCUIRespondToClaim} from 'services/translation/convertToCUI/convertToCUIRespondToClaim';
+import {Defence} from 'form/models/defence';
+
+export const toCUIRejectAllOfClaim = (ccdClaim: CCDClaim): RejectAllOfClaim => {
+  const whyDoYouDisagree = new WhyDoYouDisagree(ccdClaim?.detailsOfWhyDoesYouDisputeTheClaim);
+  const defence = new Defence(ccdClaim?.detailsOfWhyDoesYouDisputeTheClaim);
+
+  return new RejectAllOfClaim(toCUIRejectAllOfClaimType(ccdClaim?.defenceRouteRequired),
+    toCUIRespondToClaim(ccdClaim?.respondToClaim), whyDoYouDisagree, defence);
+};
