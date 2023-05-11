32eda75b3a1d4e924735bf328d6fe787cab9cc39
Pettedson John <pettedson.john@hmcts.net>
2023-05-11 09:43:55 +0100

    Adding the Files for Lint Fixing...

--- src/test/a11y/constants.ts
+++ src/test/a11y/constants.ts
@@ -3,11 +3,8 @@ export const PageUrls = {
   //Page URL's for the CUI Common Pages.
   HOME: '/',
   DASHBOARD: '/dashboard',
+  //Page URL's for the CUI Functional Pages.
   DEFENDANT : '/dashboard/:cuiCaseId/defendant',
   TASK_LIST : '/case/:cuiCaseId/response/task-list',
-
-
-  //Page URL's for the CUI Functional Pages.
-
   //Page URL's for the Case Progression Functional Pages.
 } as const;
