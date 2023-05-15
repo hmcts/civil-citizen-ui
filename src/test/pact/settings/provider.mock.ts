ea4b6f1b7009f190b5849178cf839ab7041cd763
Pettedson John <pettedson.john@hmcts.net>
2023-05-15 12:25:40 +0100

    Adding the PACT tests to work...

---
+++ src/test/pact/settings/provider.mock.ts
@@ -0,0 +1,25 @@
+import { Pact } from '@pact-foundation/pact';
+import * as path from 'path';
+
+export interface PactTestSetupConfig {
+  provider: string;
+  port: number;
+}
+
+export class PactTestSetup {
+  provider: Pact;
+  port: number;
+
+  constructor(config: PactTestSetupConfig) {
+    this.provider = new Pact({
+      consumer: 'xui_webApp',
+      dir: path.resolve(process.cwd(), 'api/test/pact/pacts'),
+      log: path.resolve(process.cwd(), 'api/test/pact/logs', `${config.provider}.log`),
+      logLevel: 'info',
+      pactfileWriteMode: 'merge',
+      port: this.port,
+      provider: config.provider,
+      spec: 2
+    });
+  }
+}
