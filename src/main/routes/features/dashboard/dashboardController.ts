1cb4efccb5d5c169252e49c80c12a844af74f5e0
laperla11 <kegilmez@gmail.com>
2023-05-15 10:31:27 +0100

    CIV-8023 implemented pagination for claimsAsDefendant

--- src/main/routes/features/dashboard/dashboardController.ts
+++ src/main/routes/features/dashboard/dashboardController.ts
@@ -6,10 +6,12 @@ import {AppRequest, UserDetails} from 'models/AppRequest';
 import {getOcmcDraftClaims} from '../../../app/client/legacyDraftStoreClient';
 import {DashboardClaimantItem, DashboardDefendantItem} from '../../../common/models/dashboard/dashboardItem';
 import {CivilServiceClient} from 'client/civilServiceClient';
+import {buildPaginationData} from 'services/features/dashboard/claimPaginationService';
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 const ocmcBaseUrl = config.get<string>('services.cmc.url');
+const CASE_PER_PAGE = 10;
 
 function renderPage(res: Response, claimsAsClaimant: DashboardClaimantItem[], claimDraftSaved: DashboardClaimantItem,
   claimsAsDefendant: DashboardDefendantItem[], responseDraftSaved: boolean,
@@ -32,14 +34,15 @@ dashboardController.get(DASHBOARD_URL, async function (req, res, next) {
   const lang = req.query.lang ? req.query.lang : req.cookies.lang;
   const appRequest = <AppRequest> req;
   const user: UserDetails = appRequest.session.user;
-  try{
+  try {
     const claimsAsClaimant : DashboardClaimantItem[] = await civilServiceClient.getClaimsForClaimant(appRequest);
-    const claimsAsDefendant : DashboardDefendantItem[] = await civilServiceClient.getClaimsForDefendant(appRequest);
+    const claimsAsDefendant: DashboardDefendantItem[] = await civilServiceClient.getClaimsForDefendant(appRequest);
+    const claimsAsDefendantPaginationData = buildPaginationData(claimsAsDefendant, req.query?.page as string, CASE_PER_PAGE, lang);
     const claimDraftSaved = await getOcmcDraftClaims(user?.accessToken);
     const responseDraftSaved = false;
     const paginationArgumentClaimant: object = {};
-    const paginationArgumentDefendant: object = {};
-    renderPage(res, claimsAsClaimant, claimDraftSaved, claimsAsDefendant, responseDraftSaved, paginationArgumentClaimant, paginationArgumentDefendant, lang);
+    const paginationArgumentDefendant: object = claimsAsDefendantPaginationData.paginationArguments;
+    renderPage(res, claimsAsClaimant, claimDraftSaved, claimsAsDefendantPaginationData.paginatedClaims, responseDraftSaved, paginationArgumentClaimant, paginationArgumentDefendant, lang);
   }catch(error){
     next(error);
   }
