c7770564cb6ca9659be2e7a21c0c92cb31d028e3
meulendijksm <max.meulendijks@hmcts.net>
2023-05-16 16:31:30 +0100

    CIV-7748 move toggle to buildDownloadHearingNoticeSection

--- src/main/services/features/dashboard/claimSummaryService.ts
+++ src/main/services/features/dashboard/claimSummaryService.ts
@@ -1,13 +1,10 @@
 import {ClaimSummaryContent} from 'form/models/claimSummarySection';
 import {Claim} from 'models/claim';
 import {buildDownloadSealedClaimSection, buildDownloadHearingNoticeSection} from './claimDocuments/claimDocumentContentBuilder';
-import {isCaseProgressionV1Enable} from '../../../app/auth/launchdarkly/launchDarklyClient';
 
-function getDocumentsContent(claim: Claim, claimId: string, lang?: string): ClaimSummaryContent[] {
+function getDocumentsContent (claim: Claim, claimId: string, lang?: string): ClaimSummaryContent[] {
   const downloadClaimSection = buildDownloadSealedClaimSection(claim, claimId, lang);
-  const downloadHearingNoticeSection = isCaseProgressionV1Enable()
-    ? buildDownloadHearingNoticeSection(claim, claimId, lang)
-    : undefined;
+  const downloadHearingNoticeSection = buildDownloadHearingNoticeSection(claim, claimId, lang);
   return [{
     contentSections: [
       downloadClaimSection,
