905d46179db985eb325a206cc46fab826a4d2c94
meulendijksm <max.meulendijks@hmcts.net>
2023-05-16 16:02:02 +0100

    CIV-7748 moved feature toggle

--- src/main/services/features/dashboard/claimSummaryService.ts
+++ src/main/services/features/dashboard/claimSummaryService.ts
@@ -1,10 +1,13 @@
 import {ClaimSummaryContent} from 'form/models/claimSummarySection';
 import {Claim} from 'models/claim';
 import {buildDownloadSealedClaimSection, buildDownloadHearingNoticeSection} from './claimDocuments/claimDocumentContentBuilder';
+import {isCaseProgressionV1Enable} from '../../../app/auth/launchdarkly/launchDarklyClient';
 
 function getDocumentsContent(claim: Claim, claimId: string, lang?: string): ClaimSummaryContent[] {
   const downloadClaimSection = buildDownloadSealedClaimSection(claim, claimId, lang);
-  const downloadHearingNoticeSection = buildDownloadHearingNoticeSection(claim, claimId, lang);
+  const downloadHearingNoticeSection = isCaseProgressionV1Enable()
+    ? buildDownloadHearingNoticeSection(claim, claimId, lang)
+    : undefined;
   return [{
     contentSections: [
       downloadClaimSection,
--- src/test/unit/services/features/dashboard/claimDocuments/claimDocumentContentBuilder.test.ts
+++ src/test/unit/services/features/dashboard/claimDocuments/claimDocumentContentBuilder.test.ts
@@ -1,5 +1,7 @@
 import {mockClaim} from '../../../../../utils/mockClaim';
+import {getCaseProgressionHearingMock} from '../../../../../utils/caseProgression/mockCaseProgressionHearing';
 import {
+  buildDownloadHearingNoticeSection,
   buildDownloadSealedClaimSection,
 } from '../../../../../../main/services/features/dashboard/claimDocuments/claimDocumentContentBuilder';
 import {CASE_DOCUMENT_DOWNLOAD_URL} from '../../../../../../main/routes/urls';
@@ -14,5 +16,26 @@ describe('Claim document content builder', ()=>{
     expect(claimDocumentSection.data?.href).toBe(CASE_DOCUMENT_DOWNLOAD_URL.replace(':id', '1').replace(':documentType', DocumentUri.SEALED_CLAIM));
     expect(claimDocumentSection.data?.text).toContain('45 KB');
     expect(claimDocumentSection.data?.subtitle).toContain('21 June 2022');
+  });
+});
+
+describe('Hearing Notice document content builder', ()=>{
+  it('should return nothing as correct information missing', ()=>{
+    //When
+    const hearingNocticeSection = buildDownloadHearingNoticeSection(mockClaim, '1', 'eng');
+    //Then
+    expect(hearingNocticeSection).toBeUndefined();
+  });
+
+  it('should return json with document size in KB and link to download the pdf', ()=>{
+    //given
+    mockClaim.caseProgressionHearing = getCaseProgressionHearingMock();
+    //When
+    const hearingNocticeSection = buildDownloadHearingNoticeSection(mockClaim, '1', 'eng');
+    //Then
+    expect(hearingNocticeSection).not.toBeUndefined();
+    expect(hearingNocticeSection.data?.href).toBe(CASE_DOCUMENT_DOWNLOAD_URL.replace(':id', '1').replace(':documentType', DocumentUri.HEARING_FORM));
+    expect(hearingNocticeSection.data?.text).toContain('55 KB');
+    expect(hearingNocticeSection.data?.subtitle).toContain('21 June 2022');
   });
 });
