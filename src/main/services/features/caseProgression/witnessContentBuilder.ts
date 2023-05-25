db76ebd1138c953f3b459fe9fc0a584f2a87907b
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-05-25 11:47:29 +0100

    CIV-7937 check selections to show sections

--- src/main/services/features/caseProgression/witnessContentBuilder.ts
+++ src/main/services/features/caseProgression/witnessContentBuilder.ts
@@ -5,15 +5,18 @@ import {
   getInput, getUpload,
   getWitnessSubtitle,
 } from 'services/features/caseProgression/uploadDocumentsSection';
+import {PageSectionBuilder} from 'common/utils/pageSectionBuilder';
 
 export const buildWitnessSection = (claim: Claim, claimId: string): ClaimSummarySection[] => {
   const sectionContent = [];
-
+  const pageSectionBuilder = new PageSectionBuilder();
+  const witnessYourStatementTitle = pageSectionBuilder.addTitle('PAGES.UPLOAD_DOCUMENTS.WITNESS')._claimSummarySections;
   const witnessYourStatementContent = getWitnessSubtitle('PAGES.UPLOAD_DOCUMENTS.STATEMENT');
   const inputContent = getInput('PAGES.UPLOAD_DOCUMENTS.NAME','govuk-!-width-three-half');
   const dateContent = getDate('PAGES.UPLOAD_DOCUMENTS.DATE', 'PAGES.UPLOAD_DOCUMENTS.DATE_EXAMPLE');
   const uploadContent = getUpload('PAGES.UPLOAD_DOCUMENTS.UPLOAD','PAGES.UPLOAD_DOCUMENTS.NO_UPLOAD');
 
+  sectionContent.push(witnessYourStatementTitle);
   sectionContent.push(witnessYourStatementContent);
   sectionContent.push(inputContent);
   sectionContent.push(dateContent);
--- src/main/services/features/caseProgression/witnessService.ts
+++ src/main/services/features/caseProgression/witnessService.ts
@@ -2,16 +2,23 @@ import {ClaimSummaryContent} from 'form/models/claimSummarySection';
 import {Claim} from 'models/claim';
 
 import {buildWitnessSection} from 'services/features/caseProgression/witnessContentBuilder';
+import {EvidenceUploadWitness} from 'models/document/documentType';
 
 export const getWitnessContent = (claimId: string, claim: Claim, documentTypes: any): ClaimSummaryContent[] => {
-  const witnessSection = buildWitnessSection(claim, claimId);
-  const witnessContent = [witnessSection];
+  const sectionContent = [];
 
-  const filteredWitnessContent = witnessContent.filter(sectionContent => sectionContent.length);
-  return filteredWitnessContent.map((sectionContent, index) => {
+  if (documentTypes.includes(EvidenceUploadWitness.WITNESS_STATEMENT)) {
+    const witnessSection = buildWitnessSection(claim, claimId);
+    const witnessContent = [witnessSection];
+
+    const filteredWitnessContent = witnessContent.filter(sectionContent => sectionContent.length);
+    sectionContent.push(filteredWitnessContent);
+  }
+
+  return sectionContent.flat().map((sectionContent, index) => {
     return ({
       contentSections: sectionContent,
-      hasDivider: index < filteredWitnessContent.length - 1,
+      hasDivider: index < sectionContent.length - 1,
     });
   });
 };
