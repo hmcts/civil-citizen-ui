ea253104bd7743cd51fad66d6a3e03089fa3c8ac
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-05-26 15:36:50 +0200

    CIV-7531 : Converter and service for responseDeadline object

---
+++ src/main/services/features/common/responseDeadlineService.ts
@@ -0,0 +1,16 @@
+import {Claim} from '../../../common/models/claim';
+import {AppRequest} from 'models/AppRequest';
+import config from 'config';
+import {CivilServiceClient} from 'client/civilServiceClient';
+import {toCUIResponseDeadline} from 'services/translation/convertToCUI/convertToCUIResponseDeadline';
+const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
+const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
+const setResponseDeadline = async (claim: Claim, req: AppRequest) => {
+  const agreedDeadlineDate: Date = await civilServiceClient.getAgreedDeadlineResponseDate(claim.id, req);
+  claim.responseDeadline = toCUIResponseDeadline(agreedDeadlineDate);
+
+};
+
+export {
+  setResponseDeadline,
+};
