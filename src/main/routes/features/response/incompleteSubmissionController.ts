fe31bc03b19f65f443cb5ed49b5d4ed230899648
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-05-26 15:39:11 +0200

    CIV-7531 : Get responseDeadlineAgreement on taskList controllers

--- src/main/routes/features/response/incompleteSubmissionController.ts
+++ src/main/routes/features/response/incompleteSubmissionController.ts
@@ -1,14 +1,15 @@
 import {NextFunction, Router} from 'express';
 import {RESPONSE_TASK_LIST_URL, RESPONSE_INCOMPLETE_SUBMISSION_URL} from '../../urls';
-import {outstandingTasksFromCase} from '../../../services/features/common/taskListService';
-import {Task} from '../../../common/models/taskList/task';
-import {getCaseDataFromStore} from '../../../modules/draft-store/draftStoreService';
-import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
+import {outstandingTasksFromCase} from 'services/features/common/taskListService';
+import {Task} from 'models/taskList/task';
+import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+import {AppRequest} from 'models/AppRequest';
 
 const incompleteSubmissionViewPath = 'features/response/incomplete-submission';
 const incompleteSubmissionController = Router();
 
-incompleteSubmissionController.get(RESPONSE_INCOMPLETE_SUBMISSION_URL, async (req, res, next: NextFunction) => {
+incompleteSubmissionController.get(RESPONSE_INCOMPLETE_SUBMISSION_URL, async (req: AppRequest, res, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
