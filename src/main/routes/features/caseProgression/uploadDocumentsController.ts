c4802119b42b243f870b9f47d89fffbe08831d71
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-05-26 09:13:46 +0100

    CIV-7937 check selections to show sections

--- src/main/routes/features/caseProgression/uploadDocumentsController.ts
+++ src/main/routes/features/caseProgression/uploadDocumentsController.ts
@@ -12,11 +12,11 @@ uploadDocumentsController.get(CP_UPLOAD_DOCUMENTS_URL, (async (req:Request, res:
     const claim: Claim = await getCaseDataFromStore(claimId);
 
     if (claim && !claim.isEmpty()) {
-      const disclosureContent: any = undefined; //TODO getDisclosureContent(claim, claimId);
+      const disclosureContent:string = undefined ; //TODO getDisclosureContent(claim, claimId);
       const witnessContent = getWitnessContent(claimId, claim);
-      const expertContent: any = undefined; //TODO = getExpertContent(claim, claimId);
-      const trialContent: any = undefined; //TODO = getTrialContent(claim, claimId);
-      res.render(uploadDocumentsViewPath, {claim, claimId, disclosureContent, witnessContent, expertContent, trialContent});
+      const expertContent:string = undefined ; //TODO = getExpertContent(claim, claimId);
+      const trialContent:string = undefined ; //TODO = getTrialContent(claim, claimId);
+      res.render(uploadDocumentsViewPath, {claim, claimId, disclosureContent, witnessContent,expertContent,trialContent});
     }
   } catch (error) {
     next(error);
