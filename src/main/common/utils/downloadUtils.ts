39f8902b3cd937b718a8cb0d3f580b513d6a72ad
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-05-31 15:26:18 +0100

    fix the unit test

--- src/main/common/utils/downloadUtils.ts
+++ src/main/common/utils/downloadUtils.ts
@@ -14,7 +14,7 @@ export function downloadFile(res: Response, fileResponse: FileResponse) {
   res.writeHead(200, {
     'Content-Type': fileResponse.contentType,
     'Content-Disposition': `attachment; filename=${fileResponse.fileName}`,
-    'Content-Length': fileResponse.data ? fileResponse.data.length : 0,
+    'Content-Length': fileResponse.data.length,
   });
   res.end(fileResponse.data);
 }
