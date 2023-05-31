acd0ce6dbf3f8e3b6e8c2c8ed4cd5e179dd03a87
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-05-31 08:49:27 +0100

    CIV-8565 fix SonarLint issues

--- src/main/services/features/caseProgression/trialContentBuilder.ts
+++ src/main/services/features/caseProgression/trialContentBuilder.ts
@@ -1,11 +1,10 @@
-import {Claim} from 'models/claim';
 import {ClaimSummarySection} from 'form/models/claimSummarySection';
 import {UploadDocumentsSectionBuilder} from 'models/caseProgression/uploadDocumentsSectionBuilder';
 
 const trialDocumentsType = 'trial_documents';
 const fileUpload = 'file_upload';
 
-export const buildTrialCaseSummarySection = (claim: Claim, claimId: string): ClaimSummarySection[] => {
+export const buildTrialCaseSummarySection = (): ClaimSummarySection[] => {
   return new UploadDocumentsSectionBuilder()
     .addTitle('PAGES.UPLOAD_DOCUMENTS.TRIAL.CASE_SUMMARY')
     .addUploadArray('PAGES.UPLOAD_DOCUMENTS.UPLOAD','', trialDocumentsType,
@@ -13,7 +12,7 @@ export const buildTrialCaseSummarySection = (claim: Claim, claimId: string): Cla
     .build();
 };
 
-export const buildTrialSkeletonSection = (claim: Claim, claimId: string): ClaimSummarySection[] => {
+export const buildTrialSkeletonSection = (): ClaimSummarySection[] => {
   return new UploadDocumentsSectionBuilder()
     .addTitle('PAGES.UPLOAD_DOCUMENTS.TRIAL.SKELETON')
     .addUploadArray('PAGES.UPLOAD_DOCUMENTS.UPLOAD','', trialDocumentsType,
@@ -21,7 +20,7 @@ export const buildTrialSkeletonSection = (claim: Claim, claimId: string): ClaimS
     .build();
 };
 
-export const buildTrialLegalSection = (claim: Claim, claimId: string): ClaimSummarySection[] => {
+export const buildTrialLegalSection = (): ClaimSummarySection[] => {
   return new UploadDocumentsSectionBuilder()
     .addTitle('PAGES.UPLOAD_DOCUMENTS.TRIAL.LEGAL')
     .addUploadArray('PAGES.UPLOAD_DOCUMENTS.UPLOAD','', trialDocumentsType,
@@ -29,7 +28,7 @@ export const buildTrialLegalSection = (claim: Claim, claimId: string): ClaimSumm
     .build();
 };
 
-export const buildTrialCostSection = (claim: Claim, claimId: string): ClaimSummarySection[] => {
+export const buildTrialCostSection = (): ClaimSummarySection[] => {
   return new UploadDocumentsSectionBuilder()
     .addTitle('PAGES.UPLOAD_DOCUMENTS.TRIAL.COSTS')
     .addUploadArray('PAGES.UPLOAD_DOCUMENTS.UPLOAD','', trialDocumentsType,
@@ -37,7 +36,7 @@ export const buildTrialCostSection = (claim: Claim, claimId: string): ClaimSumma
     .build();
 };
 
-export const buildTrialDocumentarySection = (claim: Claim, claimId: string): ClaimSummarySection[] => {
+export const buildTrialDocumentarySection = (): ClaimSummarySection[] => {
   return new UploadDocumentsSectionBuilder()
     .addTitle('PAGES.UPLOAD_DOCUMENTS.TRIAL.DOCUMENTARY')
     .addInputArray('PAGES.UPLOAD_DOCUMENTS.TYPE_OF_DOCUMENT','govuk-!-width-three-half','PAGES.UPLOAD_DOCUMENTS.TYPE_OF_DOCUMENT_EXAMPLE',trialDocumentsType,'type')
--- src/main/services/features/caseProgression/trialService.ts
+++ src/main/services/features/caseProgression/trialService.ts
@@ -2,38 +2,38 @@ import {ClaimSummaryContent} from 'form/models/claimSummarySection';
 import {Claim} from 'models/claim';
 import {
   buildTrialCaseSummarySection, buildTrialCostSection,
-  buildTrialDocumentarySection, buildTrialLegalSection, buildTrialSkeletonSection
+  buildTrialDocumentarySection, buildTrialLegalSection, buildTrialSkeletonSection,
 } from 'services/features/caseProgression/trialContentBuilder';
 
 export const getTrialContent = (claimId: string, claim: Claim): ClaimSummaryContent[] => {
   const sectionContent: any[] = [];
 
   if(claim?.caseProgression?.defendantUploadDocuments?.trial[0]?.selected) {
-    const trialCaseSummarySection = buildTrialCaseSummarySection(claim, claimId);
+    const trialCaseSummarySection = buildTrialCaseSummarySection();
     const trialCaseSummaryContent = [trialCaseSummarySection];
     sectionContent.push(trialCaseSummaryContent);
   }
 
   if(claim?.caseProgression?.defendantUploadDocuments?.trial[1]?.selected) {
-    const trialSkeletonSection = buildTrialSkeletonSection(claim, claimId);
+    const trialSkeletonSection = buildTrialSkeletonSection();
     const trialSkeletonContent = [trialSkeletonSection];
     sectionContent.push(trialSkeletonContent);
   }
 
   if(claim?.caseProgression?.defendantUploadDocuments?.trial[2]?.selected) {
-    const trialLegalSection = buildTrialLegalSection(claim, claimId);
+    const trialLegalSection = buildTrialLegalSection();
     const trialLegalContent = [trialLegalSection];
     sectionContent.push(trialLegalContent);
   }
 
   if(claim?.caseProgression?.defendantUploadDocuments?.trial[3]?.selected) {
-    const trialCostSection = buildTrialCostSection(claim, claimId);
+    const trialCostSection = buildTrialCostSection();
     const trialCostContent = [trialCostSection];
     sectionContent.push(trialCostContent);
   }
 
   if(claim?.caseProgression?.defendantUploadDocuments?.trial[4]?.selected) {
-    const trialDocumentarySection = buildTrialDocumentarySection(claim, claimId);
+    const trialDocumentarySection = buildTrialDocumentarySection();
     const trialDocumentaryContent = [trialDocumentarySection];
     sectionContent.push(trialDocumentaryContent);
   }
