7e1395e113aa12e17b623703cacce40eade0aa15
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-06-05 14:29:46 +0200

    CIV-7531 : Refactor and fixs

--- src/main/services/features/common/responseDeadlineAgreedService.ts
+++ src/main/services/features/common/responseDeadlineAgreedService.ts
@@ -3,13 +3,15 @@ import {AppRequest} from 'models/AppRequest';
 import config from 'config';
 import {CivilServiceClient} from 'client/civilServiceClient';
 import {toCUIResponseDeadline} from 'services/translation/convertToCUI/convertToCUIResponseDeadline';
+import {saveDraftClaim} from 'modules/draft-store/draftStoreService';
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 const setResponseDeadline = async (claim: Claim, req: AppRequest) => {
   const claimId : string = claim.id ? claim.id : req.params.id;
   const agreedDeadlineDate: Date = await civilServiceClient.getAgreedDeadlineResponseDate(claimId, req);
   claim.responseDeadline = toCUIResponseDeadline(agreedDeadlineDate);
-
+  claim.respondentSolicitor1AgreedDeadlineExtension = agreedDeadlineDate;
+  await saveDraftClaim(claim.id,  claim);
 };
 
 export {
