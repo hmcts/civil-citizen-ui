c597e6cb8641686549991ae94f2b5d1a01959e1c
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-06-06 15:38:37 +0200

    Update: pcq client (WIP)

--- src/main/app/client/pcq/pcqClient.ts
+++ src/main/app/client/pcq/pcqClient.ts
@@ -1,16 +1,17 @@
-import * as express from "express";
+// import * as express from "express";
 // import { RoutablePath } from 'shared/router/routablePath'
 // import { buildURL } from 'utils/callbackBuilder'
-import * as config from "config";
+// import * as config from "config";
+import config = require("config");
 import axios from "axios";
 import { PartyType } from "common/models/partyType";
 // import { BaseParameters, InvokingParameters } from './models/pcqParameters'
 // import { TokenGenerator } from './tokenGenerator'
 
-const pcqBaseUrl: string = `${config.get<string>("pcq.url")}`;
-const serviceId = "civil-citizen-ui";
+const pcqBaseUrl: string = config.get("services.pcq.url");
+const SERVICE_ID = "civil-citizen-ui";
 
-export const isPcqHealthy = async () => {
+export const isPcqHealthy = async (): Promise<boolean> => {
   try {
     const response = await axios.get(pcqBaseUrl + "/health");
     if (response.data.status === "UP") {
@@ -22,100 +23,71 @@ export const isPcqHealthy = async () => {
   }
 };
 
-export const isPcqElegible = async (pcqID: string, type: PartyType) => {
-  try {
-    const response = await axios.get(pcqBaseUrl + "/health");
-    if (
-      response.data.status === "UP" &&
-      !pcqID &&
-      (type === PartyType.INDIVIDUAL || type === PartyType.SOLE_TRADER)
-    ) {
-      return true;
-    }
-    return false;
-  } catch (error) {
-    return false;
+export const isPcqElegible = async (
+  pcqID: string,
+  type: PartyType
+): Promise<boolean> => {
+  const isHealthy = await isPcqHealthy();
+  const isIndividualOrSoleTrader =
+    type === PartyType.INDIVIDUAL || type === PartyType.SOLE_TRADER;
+  if (isHealthy && !pcqID && isIndividualOrSoleTrader) {
+    return true;
   }
+  return false;
 };
 
-// export class PcqClient {
-//   static async isEligibleRedirect(
-//     pcqID: string,
-//     partyType: string
-//   ): Promise<boolean> {
-//     try {
-//       const response = request.get(pcqBaseUrl + "/health", { json: true });
-//       if (
-//         response["status"] === "UP" &&
-//         pcqID === undefined &&
-//         partyType === "individual"
-//       ) {
-//         return true;
-//       }
-//       return false;
-//     } catch (err) {
-//       return false;
-//     }
+export const generatePcqtUrl = (
+  pcqId: string,
+  actor: string, //Respondents
+  ccdCaseId: string, //ccdCaseId
+  partyId: string, //Respondent's Email Address
+  returnUri: string,
+  lang: string
+): string => {
+  const pcqParameters: PcqParameters = {
+    pcqId: pcqId,
+    serviceId: SERVICE_ID,
+    actor: actor,
+    ccdCaseId: ccdCaseId,
+    partyId: partyId,
+    returnUrl: returnUri,
+    language: lang,
+    token: "test", // token: TokenGenerator.gen(baseParameters)
+  };
+
+  const qs = Object.entries(pcqParameters)
+    .map(([key, value]) => key + "=" + value)
+    .join("&");
+    
+  return `${pcqBaseUrl}/service-endpoint?${qs}`;
+};
+// (
+//   req: express.Request,
+//   claimtype: string,
+//   pcqID: string,
+//   partyEmailId: string,
+//   ccdCaseId: number,
+//   receiver: RoutablePath,
+//   externalId: string
+// ): string {
+//   if (receiver === undefined) {
+//     throw new Error("Request is undefined");
 //   }
-
-//   static generateRedirectUrl(
-//     req: express.Request,
-//     claimtype: string,
-//     pcqID: string,
-//     partyEmailId: string,
-//     ccdCaseId: number,
-//     receiver: RoutablePath,
-//     externalId: string
-//   ): string {
-//     if (receiver === undefined) {
-//       throw new Error("Request is undefined");
-//     }
-//     let ccdId = "";
-//     if (ccdCaseId) {
-//       ccdId = ccdCaseId.toString();
-//     }
-//     const returnUri = buildURL(req, receiver.uri)
-//       .split("https://")[1]
-//       .replace(":externalId", externalId);
-//     return this.getServiceEndpoint(
-//       ccdId,
-//       partyEmailId,
-//       pcqID,
-//       returnUri,
-//       serviceId,
-//       claimtype
-//     );
+//   let ccdId = "";
+//   if (ccdCaseId) {
+//     ccdId = ccdCaseId.toString();
 //   }
-
-//   static getServiceEndpoint(
-//     ccdCaseId: string,
-//     partyId: string,
-//     pcqId: string,
-//     returnUri: string,
-//     actorCmc: string,
-//     claimtype: string
-//   ): string {
-//     const baseParameters: BaseParameters = {
-//       pcqId: pcqId,
-//       serviceId: actorCmc,
-//       actor: claimtype,
-//       ccdCaseId: ccdCaseId,
-//       partyId: partyId,
-//       returnUrl: returnUri,
-//       language: "en",
-//     };
-
-//     const invokingParameters: InvokingParameters = {
-//       ...baseParameters,
-//       token: TokenGenerator.gen(baseParameters),
-//     };
-
-//     const qs = Object.keys(invokingParameters)
-//       .map((key) => key + "=" + invokingParameters[key])
-//       .join("&");
-
-//     return `${pcqBaseUrl}/service-endpoint?${qs}`;
-//   }
+//   const returnUri = buildURL(req, receiver.uri)
+//     .split("https://")[1]
+//     .replace(":externalId", externalId);
+//   return this.getServiceEndpoint(
+//     ccdId,
+//     partyEmailId,
+//     pcqID,
+//     returnUri,
+//     serviceId,
+//     claimtype
+//   );
 // }
 
 export interface GetServiceEndpointParameters {
@@ -126,7 +98,7 @@ export interface GetServiceEndpointParameters {
   language?: string;
 }
 
-export interface BaseParameters {
+export interface PcqParameters {
   serviceId: string;
   actor: string;
   pcqId: string;
@@ -134,8 +106,5 @@ export interface BaseParameters {
   partyId: string;
   returnUrl: string;
   language?: string;
-}
-
-export interface InvokingParameters extends BaseParameters {
   token: string;
 }
--- src/main/routes/guards/pcqGuard.ts
+++ src/main/routes/guards/pcqGuard.ts
@@ -1,27 +1,53 @@
-import { isPcqEnable } from 'app/auth/launchdarkly/launchDarklyClient';
-import {  isPcqElegible, isPcqHealthy } from 'client/pcq/pcqClient';
-import {NextFunction, Request, Response} from 'express';
+import {
+  generatePcqtUrl,
+  isPcqElegible,
+  isPcqHealthy,
+} from "client/pcq/pcqClient";
+import { Claim } from "common/models/claim";
+import { PartyType } from "common/models/partyType";
+import { constructResponseUrlWithIdParams } from "common/utils/urlFormatter";
+import { NextFunction, Request, Response } from "express";
+import { getCaseDataFromStore } from "modules/draft-store/draftStoreService";
+import { RESPONSE_CHECK_ANSWERS_URL } from "routes/urls";
 // import {constructResponseUrlWithIdParams} from "common/utils/urlFormatter";
 // import {RESPONSE_CHECK_ANSWERS_URL} from 'routes/urls';
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-export const isFirstTimeInPCQ = async (req: Request, res: Response, next: NextFunction) => {
-  let redirectUri = null;
-  
-  const pcqId = draft.document.defendantDetails.partyDetails.pcqId;
-  const type = draft.document.defendantDetails.partyDetails.type;
+export const isFirstTimeInPCQ = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  // let redirectUri = null;
+  const caseData: Claim = await getCaseDataFromStore(req.params.id);
+  // const pcqId = draft.document.defendantDetails.partyDetails.pcqId;
+  // const type = draft.document.defendantDetails.partyDetails.type;
 
-  if (await isPcqEnable() && await isPcqHealthy()) {
-    if (await isPcqElegible(pcqId, type)) {
-      // redirectUri = PcqClient.generateRedirectUrl(req, 'DEFENDANT', pcqID, user.email, claim.ccdCaseId, Paths.checkAndSendPage, draft.document.externalId);
-      // if (pcqId === undefined) {
-      //   let pcqID = uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-      //   pcqId = pcqID;
-      //   new DraftService().save(draft, user.bearerToken);
-      // }
-    } else {
-      next();
-    }
+  const pcqId: string = null; //TODO: generate PCQ ID
+  const type: PartyType = caseData.respondent1.type;
+  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+  const claimId = req.params.id;
+
+  const isHealthy = await isPcqHealthy();
+  const isElegible = await isPcqElegible(pcqId, type);
+
+  console.log("isHealthy", isHealthy);
+  console.log("isElegible", isElegible);
+
+  if (isHealthy && isElegible) {
+    // TODO:
+    // generate token
+    // save it on CCD?
+
+    const pcqUrl = generatePcqtUrl(
+      "pcqId",
+      "actor", //RESPONDENT, DEFENDANT??????
+      "ccdCaseId",
+      caseData.respondent1.emailAddress.emailAddress,
+      constructResponseUrlWithIdParams(claimId, RESPONSE_CHECK_ANSWERS_URL),
+      lang
+    );
+    console.log("pcqUrl", pcqUrl);
   } else {
     next();
   }
