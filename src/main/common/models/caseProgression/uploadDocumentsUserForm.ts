1fe55f26bffef798af22588d86554584cc6fa3e0
meulendijksm <max.meulendijks@hmcts.net>
2023-06-08 17:05:39 +0100

    CIV-8641 - Removed lint error

--- src/main/common/models/caseProgression/uploadDocumentsUserForm.ts
+++ src/main/common/models/caseProgression/uploadDocumentsUserForm.ts
@@ -1,12 +1,8 @@
-import {IsAllowedMimeType} from 'form/validators/isAllowedMimeType';
-import {IsFileSize} from 'form/validators/isFileSize';
 
 export class FileUpload {
   fieldname: string;
   originalname: string;
-  @IsAllowedMimeType({ message: 'ERRORS.VALID_MIME_TYPE_FILE' })
-    mimetype: string;
+  mimetype: string;
   buffer: ArrayBuffer;
-  @IsFileSize({ message: 'ERRORS.VALID_SIZE_FILE' })
-    size: number;
+  size: number;
 }
