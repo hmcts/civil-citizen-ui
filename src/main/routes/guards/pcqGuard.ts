a4bc9c4806d7ef44085f435b453e17e01ff817e5
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-06-12 15:46:01 +0200

    Fix: fix pcq guard

--- src/main/routes/guards/pcqGuard.ts
+++ src/main/routes/guards/pcqGuard.ts
@@ -2,60 +2,63 @@ import {
   generatePcqtUrl,
   isPcqElegible,
   isPcqHealthy,
-} from 'client/pcq/pcqClient';
-import { isPcqShutterOn } from 'app/auth/launchdarkly/launchDarklyClient';
-import { generatePcqId } from 'client/pcq/generatePcqId';
-import { Claim } from 'common/models/claim';
-import { PartyType } from 'common/models/partyType';
-import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
-import { NextFunction, Request, Response } from 'express';
-import { getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
-import { RESPONSE_CHECK_ANSWERS_URL } from 'routes/urls';
-import { savePcqIdClaim } from 'client/pcq/savePcqIdClaim';
+} from "client/pcq/pcqClient";
+import { isPcqShutterOn } from "app/auth/launchdarkly/launchDarklyClient";
+import { generatePcqId } from "client/pcq/generatePcqId";
+import { Claim } from "common/models/claim";
+import { PartyType } from "common/models/partyType";
+import { constructResponseUrlWithIdParams } from "common/utils/urlFormatter";
+import { NextFunction, Request, Response } from "express";
+import { getCaseDataFromStore } from "modules/draft-store/draftStoreService";
+import { RESPONSE_CHECK_ANSWERS_URL } from "routes/urls";
+import { savePcqIdClaim } from "client/pcq/savePcqIdClaim";
 
-const ACTOR = 'DEFENDANT';
+const ACTOR = "DEFENDANT";
 
 export const isFirstTimeInPCQ = async (req: Request, res: Response, next: NextFunction) => {
-  const caseData: Claim = await getCaseDataFromStore(req.params.id);
-  const pcqShutterOn = await isPcqShutterOn();
-  console.log('isPcqShutterOn', pcqShutterOn);
-  console.log('caseData.pcqId', caseData.pcqId);
+  try {
+    const caseData: Claim = await getCaseDataFromStore(req.params.id);
+    const pcqShutterOn = await isPcqShutterOn();
 
-  if (pcqShutterOn || caseData.pcqId) {
-    next();
-  }
+    if (pcqShutterOn || caseData.pcqId) {
+      return next();
+    }
 
-  const type: PartyType = caseData.respondent1.type;
-  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-  const claimId = req.params.id;
-  const defendantEmail = caseData.respondent1.emailAddress.emailAddress;
+    const type: PartyType = caseData.respondent1.type;
+    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+    const claimId = req.params.id;
+    const defendantEmail = caseData.respondent1.emailAddress.emailAddress;
 
-  const isHealthy = await isPcqHealthy();
-  const isElegible = isPcqElegible(type);
-  
-  console.log('isHealthy', isHealthy);
-  console.log('isElegible', isElegible);
-  
-  if (isHealthy && isElegible) {
-    const pcqId = generatePcqId();
-    await savePcqIdClaim(pcqId, claimId);
-    console.log('pcqId', pcqId);
+    const isHealthy = await isPcqHealthy();
+    const isElegible = isPcqElegible(type);
 
-    const pcqUrl = generatePcqtUrl(
-      pcqId,
-      ACTOR,
-      claimId,
-      defendantEmail,
-      getRedirectionUrl(req.headers.host, claimId),
-      lang,
-    );
+    if (isHealthy && isElegible) {
+      const pcqId = generatePcqId();
+      await savePcqIdClaim(pcqId, claimId);
+      
+      const pcqUrl = generatePcqtUrl(
+        pcqId,
+        ACTOR,
+        claimId,
+        defendantEmail,
+        getRedirectionUrl(req.headers.host, claimId),
+        lang
+        );
+        
+      console.log("pcqId", pcqId);
+      console.log("isPcqShutterOn", pcqShutterOn);
+      console.log("caseData.pcqId", caseData.pcqId);
+      console.log("isHealthy", isHealthy);
+      console.log("isElegible", isElegible);
+      console.log("getRedirectionUrl",getRedirectionUrl(req.headers.host, claimId));
+      console.log("pcqUrl", pcqUrl);
 
-    console.log('getRedirectionUrl', getRedirectionUrl(req.headers.host, claimId));
-    console.log('pcqUrl', pcqUrl);
-
-    res.redirect(pcqUrl);
-  } else {
-    next();
+      res.redirect(pcqUrl);
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
