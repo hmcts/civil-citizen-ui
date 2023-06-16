f4991826325b9fcf39200ce33dd56a5de5d4216a
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-06-16 16:14:25 +0200

    Fix: create custom service for expert required

--- src/main/routes/features/directionsQuestionnaire/experts/expertSmallClaimsController.ts
+++ src/main/routes/features/directionsQuestionnaire/experts/expertSmallClaimsController.ts
@@ -5,12 +5,10 @@ import {
   DQ_GIVE_EVIDENCE_YOURSELF_URL,
 } from '../../../urls';
 import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
-import {saveDirectionQuestionnaire} from 'services/features/directionsQuestionnaire/directionQuestionnaireService';
+import {saveExpertRequiredValue} from 'services/features/directionsQuestionnaire/expertRequiredValueService';
 
 const expertSmallClaimsController = Router();
 const expertSmallClaimsViewPath = 'features/directionsQuestionnaire/experts/expert-small-claims';
-const dqPropertyName = 'expertRequired';
-const dqParentName = 'experts';
 
 expertSmallClaimsController.get(DQ_EXPERT_SMALL_CLAIMS_URL, async (req: Request, res: Response) => {
   res.render(expertSmallClaimsViewPath);
@@ -21,13 +19,7 @@ expertSmallClaimsController.post(DQ_EXPERT_SMALL_CLAIMS_URL, async (req: Request
     const claimId = req.params.id;
     const expertRequiredValue = req.body.expertYes ? true : false;
     const redirectUrl = req.body.expertYes ? DQ_EXPERT_REPORT_DETAILS_URL : DQ_GIVE_EVIDENCE_YOURSELF_URL;
-    if(!expertRequiredValue) {
-      await saveDirectionQuestionnaire(claimId, null, 'expertReportDetails', dqParentName);
-      await saveDirectionQuestionnaire(claimId, null, 'permissionForExpert', dqParentName);
-      await saveDirectionQuestionnaire(claimId, null, 'expertCanStillExamine', dqParentName);
-      await saveDirectionQuestionnaire(claimId, null, 'expertDetailsList', dqParentName);
-    }
-    await saveDirectionQuestionnaire(claimId, expertRequiredValue, dqPropertyName, dqParentName);
+    await saveExpertRequiredValue(claimId, expertRequiredValue);
     res.redirect(constructResponseUrlWithIdParams(claimId, redirectUrl));
   } catch (error) {
     next(error);
