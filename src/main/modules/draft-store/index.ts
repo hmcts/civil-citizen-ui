c0e865196aa2e12bac223c25383d8b9cf6d578a1
KumarSam <sampank@gmail.com>
2023-06-20 15:40:29 +0100

    CIV-0000 upgrade redis version

--- src/main/modules/draft-store/index.ts
+++ src/main/modules/draft-store/index.ts
@@ -4,7 +4,7 @@ import {LoggerInstance} from 'winston';
 
 const Redis = require('ioredis');
 
-const REDIS_DATA = require('./redisData.json');
+//const REDIS_DATA = require('./redisData.json');
 
 export class DraftStoreClient {
   public static REDIS_CONNECTION_SUCCESS = 'Connected to Redis instance successfully';
@@ -21,12 +21,12 @@ export class DraftStoreClient {
     this.logger.info(client.ping());
     app.locals.draftStoreClient = client;
     this.logger.info(DraftStoreClient.REDIS_CONNECTION_SUCCESS);
-    app.locals.draftStoreClient.on('connect', () => {
+    /*app.locals.draftStoreClient.on('connect', () => {
       REDIS_DATA.forEach(async (element: any) => {
         await client.set(element.id, JSON.stringify(element, null, 4)).then(() =>
           this.logger.info(`Mock data ${element.id} saved to Redis`),
         );
       });
-    });
+    });*/
   }
 }
