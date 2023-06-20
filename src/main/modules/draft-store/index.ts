df5ffe6a35f62e52c4616b8d59eb5c7437d2d79d
KumarSam <sampank@gmail.com>
2023-06-20 15:25:41 +0100

    CIV-0000 upgrade redis version

--- src/main/modules/draft-store/index.ts
+++ src/main/modules/draft-store/index.ts
@@ -16,13 +16,12 @@ export class DraftStoreClient {
     const protocol = config.get('services.draftStore.redis.tls') ? 'rediss://' : 'redis://';
     const connectionString = `${protocol}:${config.get('services.draftStore.redis.key')}@${config.get('services.draftStore.redis.host')}:${config.get('services.draftStore.redis.port')}`;
     this.logger.info(`connectionString: ${connectionString}`);
-    const client = new Redis(connectionString);
+    const client = new Redis(connectionString, {connectTimeout: 10000});
 
     this.logger.info(client.ping());
     app.locals.draftStoreClient = client;
     this.logger.info(DraftStoreClient.REDIS_CONNECTION_SUCCESS);
-    app.locals.draftStoreClient.connect('connect', () => {
-      this.logger.info('inside on connect---');
+    app.locals.draftStoreClient.on('connect', () => {
       REDIS_DATA.forEach(async (element: any) => {
         await client.set(element.id, JSON.stringify(element, null, 4)).then(() =>
           this.logger.info(`Mock data ${element.id} saved to Redis`),
