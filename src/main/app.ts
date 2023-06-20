fa7866ca3150f6ce72b3fdbe9cc2983a4b10f176
KumarSam <sampank@gmail.com>
2023-06-20 09:09:12 +0100

    CIV-0000 upgrade redis version

--- src/main/app.ts
+++ src/main/app.ts
@@ -17,7 +17,7 @@ import {CSRFToken} from './modules/csrf';
 import routes from './routes/routes';
 import {setLanguage} from 'modules/i18n/languageService';
 import {isServiceShuttered} from './app/auth/launchdarkly/launchDarklyClient';
-import {draftStoreClient} from "modules/draft-store/index_test";
+import {draftStoreClient} from 'modules/draft-store/index_test';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const {setupDev} = require('./development');
