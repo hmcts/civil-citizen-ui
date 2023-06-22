a9b4d91a55f7885ed8a6540acb8a51c18ae1d638
Pettedson John <pettedson.john@hmcts.net>
2023-06-22 11:22:04 +0100

    Testing all of the Case Progression Tests.......

--- src/test/functionalTests/features/response/steps/caseProgressionSteps.js
+++ src/test/functionalTests/features/response/steps/caseProgressionSteps.js
@@ -1,13 +1,22 @@
 const CaseProgressionLatestUpdate = require("../pages/caseProgression/caseProgressionLatestUpdate");
+const ContactUs = require("../pages/common/contactUs");
 
 
 const I = actor(); // eslint-disable-line no-unused-vars
 const caseProgressionLatestUpdatePage = new CaseProgressionLatestUpdate();
 
 
+
 class UploadEvidenceSteps {
+
   initiateUploadEvidenceJourney(claimRef) {
     caseProgressionLatestUpdatePage.open(claimRef);
+    caseProgressionLatestUpdatePage.nextAction('Upload documents')
+  }
+
+  initiateHearingNoticeJourney(claimRef) {
+    caseProgressionLatestUpdatePage.open(claimRef);
+    caseProgressionLatestUpdatePage.nextAction('View hearing notice')
   }
 
 }
