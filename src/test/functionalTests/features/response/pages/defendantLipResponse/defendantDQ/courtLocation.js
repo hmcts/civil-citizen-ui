1852e8c0b9e9754426260331dd94359c54fb04d4
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-06-23 12:45:33 +0100

    Update courtLocation.js

--- src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/courtLocation.js
+++ src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/courtLocation.js
@@ -14,7 +14,7 @@ class CourtLocation {
     I.see('Do you want to ask for the hearing to be held at a specific court?', 'h1');
     I.see('You can ask for the hearing to be held at a specific court, for example, if you spend weekdays a long distance from your home. The court will consider both parties\' circumstances when deciding where to hold the hearing.');
     I.click(fields.yesButton);
-    I.selectOption(fields.courtLocation, 'Leeds Combined Court Centre - THE Court House, 1 Oxford Row - LS1 3BG');
+    I.selectOption(fields.courtLocation, 'Leeds Combined Court Centre - The Court House, 1 Oxford Row - LS1 3BG');
     I.fillField(fields.courtLocationReason, 'Nearest court');
     I.click('Save and continue');
   }
