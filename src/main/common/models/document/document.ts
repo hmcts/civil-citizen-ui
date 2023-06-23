1e6aacbd285c30702d3b28160e13f9b69d6c82bd
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-06-23 09:19:56 +0100

    CIV-8977 Making additional fields optional for compatibility with the existing code

--- src/main/common/models/document/document.ts
+++ src/main/common/models/document/document.ts
@@ -2,7 +2,7 @@ export interface Document {
   document_url: string,
   document_filename: string,
   document_binary_url: string,
-  document_hash: string,
-  category_id: string
+  document_hash?: string,
+  category_id?: string
 }
 
