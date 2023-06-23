5d212a10066dc244c8c46af690a1436afd9d742d
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-06-23 13:12:24 +0100

    Update courtLocation.js

--- src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/courtLocation.js
+++ src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/courtLocation.js
@@ -1,3 +1,4 @@
+const config =  require('../../../../../../config');
 
 const I = actor();
 
@@ -11,11 +12,10 @@ const fields ={
 class CourtLocation {
 
   selectPreferredCourtLocation() {
-    I.waitForElement(fields.courtLocation, 10);
     I.see('Do you want to ask for the hearing to be held at a specific court?', 'h1');
     I.see('You can ask for the hearing to be held at a specific court, for example, if you spend weekdays a long distance from your home. The court will consider both parties\' circumstances when deciding where to hold the hearing.');
     I.click(fields.yesButton);
-    I.selectOption(fields.courtLocation, 'Leeds Combined Court Centre - The Court House, 1 Oxford Row - LS1 3BG');
+    I.selectOption(fields.courtLocation, config.defendantSelectedCourt);
     I.fillField(fields.courtLocationReason, 'Nearest court');
     I.click('Save and continue');
   }
