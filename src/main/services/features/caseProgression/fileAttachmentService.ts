1b8a52c43c1b88ba56270868ae85d2484d9ec086
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-06-23 12:26:34 +0100

    CIV-8977 File attachment service done

--- src/main/services/features/caseProgression/fileAttachmentService.ts
+++ src/main/services/features/caseProgression/fileAttachmentService.ts
@@ -10,31 +10,77 @@ import {Request} from 'express';
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
+class CaseDocumentsAttachmentResult {
+  caseProgression: CaseProgression;
+  errors: string[];
+
+  constructor(caseProgression: CaseProgression, errors: string[]) {
+    this.caseProgression = caseProgression;
+    this.errors = errors;
+  }
+}
+
 function getDocument(uploadDocumentTypes: UploadDocumentTypes): Document {
   return Object.values(uploadDocumentTypes).find(value => value instanceof Document);
 }
 
-function getDocuments(uploadDocuments: UploadDocuments): Document[] {
-  const documents: Document[] = [];
-  uploadDocuments.expert.forEach(elem => documents.push(getDocument(elem)));
-  uploadDocuments.disclosure.forEach(elem => documents.push(getDocument(elem)));
-  uploadDocuments.trial.forEach(elem => documents.push(getDocument(elem)));
-  uploadDocuments.witness.forEach(elem => documents.push(getDocument(elem)));
+function getDocuments(uploadDocuments: UploadDocumentTypes[][]): Map<Document, UploadDocumentTypes> {
+  const documents: Map<Document, UploadDocumentTypes> = new Map();
+  for (const uploadDocumentTypes of uploadDocuments) {
+    uploadDocumentTypes.forEach(elem => documents.set(getDocument(elem), elem));
+  }
   return documents;
 }
 
-export const attachCaseDocuments = async (claimId: string, claimantOrDefendant: ClaimantOrDefendant, caseProgression: CaseProgression, req: Request): Promise<CaseProgression> => {
-  let documentsToAttach: Document[];
-  if (claimantOrDefendant===ClaimantOrDefendant.DEFENDANT) {
-    documentsToAttach = getDocuments(caseProgression.defendantUploadDocuments);
-  } else {
-    documentsToAttach = getDocuments(caseProgression.claimantUploadDocuments);
-  }
-  const attachedDocuments = await civilServiceClient.attachCaseDocuments(claimId, documentsToAttach, <AppRequest>req);
+function areEqual(document: Document, attachedDocument: Document): boolean {
+  return document.document_filename === attachedDocument.document_filename
+    && document.document_binary_url === attachedDocument.document_binary_url
+    && document.document_url === attachedDocument.document_url
+    && document.document_hash === attachedDocument.document_hash
+    && document.category_id === attachedDocument.category_id;
+}
 
-  if (documentsToAttach.length === attachedDocuments.length) {
-    return caseProgression;
+function isAttachedTo(document: Document, attachedDocuments: Document[]): boolean {
+  for (const attachedDocument of attachedDocuments) {
+    if (areEqual(document, attachedDocument)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function remove(element: UploadDocumentTypes, collection: UploadDocumentTypes[]): boolean {
+  const index = collection.indexOf(element);
+  if (index > -1) {
+    collection.splice(index, 1);
+  }
+  return index > -1;
+}
+
+function removeFrom(elementToRemove: UploadDocumentTypes, uploadDocumentsToProcess: UploadDocumentTypes[][]) {
+  for (const collection of uploadDocumentsToProcess) {
+    const removed = remove(elementToRemove, collection);
+    if (removed) {
+      return;
+    }
+  }
+}
+
+export const attachCaseDocuments = async (claimId: string, claimantOrDefendant: ClaimantOrDefendant, caseProgression: CaseProgression, req: Request): Promise<CaseDocumentsAttachmentResult> => {
+  const uploadDocuments: UploadDocuments = claimantOrDefendant === ClaimantOrDefendant.DEFENDANT ? caseProgression.defendantUploadDocuments : caseProgression.claimantUploadDocuments;
+  const uploadDocumentsToProcess: UploadDocumentTypes[][] = [uploadDocuments.expert, uploadDocuments.disclosure, uploadDocuments.trial, uploadDocuments.witness];
+  const documentsToAttach: Map<Document, UploadDocumentTypes> = getDocuments(uploadDocumentsToProcess);
+  const attachedDocuments = await civilServiceClient.attachCaseDocuments(claimId, Array.from(documentsToAttach.keys()), <AppRequest>req);
+  const errors: string[] = [];
+
+  if (documentsToAttach.size !== attachedDocuments.length) {
+    for (const document of documentsToAttach.keys()) {
+      if (!isAttachedTo(document, attachedDocuments)) {
+        errors.push(`${document.document_filename} has not been attached to the case ${claimId}.`);
+        removeFrom(documentsToAttach.get(document), uploadDocumentsToProcess);
+      }
+    }
   }
 
-  return null;
+  return new CaseDocumentsAttachmentResult(caseProgression, errors);
 };
