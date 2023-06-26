d66e695c590c44577b42309df449a4bd115bca2f
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-06-26 15:02:56 +0100

    uploadYourDocumentsController fix

--- src/main/routes/features/caseProgression/uploadYourDocumentsController.ts
+++ src/main/routes/features/caseProgression/uploadYourDocumentsController.ts
@@ -1,14 +1,11 @@
 import {NextFunction, RequestHandler, Router} from 'express';
 import {UPLOAD_YOUR_DOCUMENTS_URL} from '../../urls';
-import config from 'config';
-import {CivilServiceClient} from 'client/civilServiceClient';
 import {getUploadYourDocumentsContents} from 'services/features/caseProgression/uploadYourDocumentsContents';
 import {getClaimById} from 'modules/utilityService';
 
 const uploadYourDocumentsViewPath = 'features/caseProgression/upload-your-documents';
 const uploadYourDocumentsController = Router();
-const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
-new CivilServiceClient(civilServiceApiBaseUrl);
+
 uploadYourDocumentsController.get([UPLOAD_YOUR_DOCUMENTS_URL], (async (req, res, next: NextFunction) => {
   try {
     const claimId = req.params.id;
