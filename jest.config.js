09dc19e159687c03125178d1819728eae794f30b
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-06-28 00:29:38 +0100

    add exclusion

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src/test/unit'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   testEnvironment: 'node',
-  testPathIgnorePatterns:['src/test/unit/routes/features/claim', 'src/test/unit/routes/features/claimantResponse', 'checkAnswersController.test.ts', 'submitReponse.test.ts', 'pinController.test.ts', 'submitComfirmationController.test.ts','newResponseDeadlineController.test.ts', 'documentDownloadController.test.ts', 'dashboardController.test.ts'],
+  testPathIgnorePatterns:['src/test/unit/routes/features/claim', 'src/test/unit/routes/features/claimantResponse', 'checkAnswersController.test.ts', 'submitReponse.test.ts', 'pinController.test.ts', 'submitComfirmationController.test.ts','newResponseDeadlineController.test.ts', 'documentDownloadController.test.ts', 'dashboardController.test.ts', 'claimDetailsController.test.ts'],
   transform: {
     '^.+\\.ts?$': ['ts-jest', {  isolatedModules: true, }],
   },
