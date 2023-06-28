223a5fe1d54e6df8458be222aa968934352318bc
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-06-28 11:00:09 +0100

    CIV-8842 addressing review comment

--- src/main/services/features/caseProgression/disclosureContentBuilder.ts
+++ src/main/services/features/caseProgression/disclosureContentBuilder.ts
@@ -13,10 +13,16 @@ const fileUpload = 'file_upload';
 
 export const buildDisclosureDocumentSection = (section: TypeOfDocumentSection = null, index = 0, form: GenericForm<UploadDocumentsUserForm> = null): ClaimSummarySection[] => {
   const errorFieldNamePrefix = `${documentsForDisclosure}[${documentsForDisclosure}][${index}]`;
+  const errors = {
+    invalidDayError : form?.errorFor(`${errorFieldNamePrefix}[dateDay]`, documentsForDisclosure),
+    invalidMonthError : form?.errorFor(`${errorFieldNamePrefix}[dateMonth]`, documentsForDisclosure),
+    invalidYearError : form?.errorFor(`${errorFieldNamePrefix}[dateYear]`, documentsForDisclosure),
+    invalidDateError : form?.errorFor(`${errorFieldNamePrefix}[date]`, documentsForDisclosure),
+  };
   return new UploadDocumentsSectionBuilder()
     .addTitle('PAGES.UPLOAD_DOCUMENTS.DISCLOSURE.DISCLOSURE_DOCUMENTS')
     .addInputArray('PAGES.UPLOAD_DOCUMENTS.TYPE_OF_DOCUMENT', '', 'PAGES.UPLOAD_DOCUMENTS.TYPE_OF_DOCUMENT_EXAMPLE', documentsForDisclosure, 'typeOfDocument', section?.typeOfDocument, index, form?.errorFor(`${errorFieldNamePrefix}[typeOfDocument]`, documentsForDisclosure))
-    .addDateArray('PAGES.UPLOAD_DOCUMENTS.DOCUMENT_ISSUE_DATE', form?.errorFor(`${errorFieldNamePrefix}[dateDay]`, documentsForDisclosure), form?.errorFor(`${errorFieldNamePrefix}[dateMonth]`, documentsForDisclosure), form?.errorFor(`${errorFieldNamePrefix}[dateYear]`, documentsForDisclosure), form?.errorFor(`${errorFieldNamePrefix}[date]`, documentsForDisclosure), 'PAGES.UPLOAD_DOCUMENTS.DATE_EXAMPLE', documentsForDisclosure, 'date', section?.dateDay.toString(), section?.dateMonth.toString(), section?.dateYear.toString(), index)
+    .addDateArray('PAGES.UPLOAD_DOCUMENTS.DOCUMENT_ISSUE_DATE', errors, 'PAGES.UPLOAD_DOCUMENTS.DATE_EXAMPLE', documentsForDisclosure, 'date', section?.dateDay.toString(), section?.dateMonth.toString(), section?.dateYear.toString(), index)
     .addUploadArray('PAGES.UPLOAD_DOCUMENTS.UPLOAD', '', documentsForDisclosure, fileUpload, index)
     .addRemoveSectionButton(form?.model.documentsForDisclosure?.length > 1 || false)
     .build();
--- src/main/services/features/caseProgression/trialContentBuilder.ts
+++ src/main/services/features/caseProgression/trialContentBuilder.ts
@@ -49,10 +49,16 @@ export const buildTrialCostSection = (section: FileOnlySection = null, index = 0
 export const buildTrialDocumentarySection = (section: TypeOfDocumentSection = null, index = 0, form: GenericForm<UploadDocumentsUserForm> = null): ClaimSummarySection[] => {
   const errorFieldNamePrefix = `${trialDocumentary}[${trialDocumentary}][${index}]`;
   const missingInputError = form?.errorFor(`${errorFieldNamePrefix}[typeOfDocument]`, trialDocumentary) !== undefined ? 'ERRORS.VALID_ENTER_TYPE_OF_DOCUMENT' : null;
+  const errors = {
+    invalidDayError : form?.errorFor(`${errorFieldNamePrefix}[dateDay]`, trialDocumentary),
+    invalidMonthError : form?.errorFor(`${errorFieldNamePrefix}[dateMonth]`, trialDocumentary),
+    invalidYearError : form?.errorFor(`${errorFieldNamePrefix}[dateYear]`, trialDocumentary),
+    invalidDateError : form?.errorFor(`${errorFieldNamePrefix}[date]`, trialDocumentary),
+  };
   return new UploadDocumentsSectionBuilder()
     .addTitle('PAGES.UPLOAD_DOCUMENTS.TRIAL.DOCUMENTARY')
     .addInputArray('PAGES.UPLOAD_DOCUMENTS.TYPE_OF_DOCUMENT', '', 'PAGES.UPLOAD_DOCUMENTS.TYPE_OF_DOCUMENT_EXAMPLE', trialDocumentary, 'typeOfDocument', section?.typeOfDocument, index, missingInputError)
-    .addDateArray('PAGES.UPLOAD_DOCUMENTS.DOCUMENT_ISSUE_DATE', form?.errorFor(`${errorFieldNamePrefix}[dateDay]`, trialDocumentary), form?.errorFor(`${errorFieldNamePrefix}[dateMonth]`, trialDocumentary), form?.errorFor(`${errorFieldNamePrefix}[dateYear]`, trialDocumentary), form?.errorFor(`${errorFieldNamePrefix}[date]`, trialDocumentary),'PAGES.UPLOAD_DOCUMENTS.DATE_EXAMPLE', trialDocumentary, 'date', section?.dateDay.toString(), section?.dateMonth.toString(), section?.dateYear.toString(), index)
+    .addDateArray('PAGES.UPLOAD_DOCUMENTS.DOCUMENT_ISSUE_DATE', errors,'PAGES.UPLOAD_DOCUMENTS.DATE_EXAMPLE', trialDocumentary, 'date', section?.dateDay.toString(), section?.dateMonth.toString(), section?.dateYear.toString(), index)
     .addUploadArray('PAGES.UPLOAD_DOCUMENTS.UPLOAD','', trialDocumentary, fileUpload, index)
     .addRemoveSectionButton(form?.model.trialDocumentary?.length > 1 || false)
     .build();
