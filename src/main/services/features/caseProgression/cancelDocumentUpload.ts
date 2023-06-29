f9858ad8faf341654307cdcb0bf462fc88d94468
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-06-29 15:30:23 +0100

    CIV-8019 Cancel document upload function simplified

--- src/main/services/features/caseProgression/cancelDocumentUpload.ts
+++ src/main/services/features/caseProgression/cancelDocumentUpload.ts
@@ -2,9 +2,8 @@ import {Claim} from 'models/claim';
 import {UploadYourDocumentsSectionBuilder} from 'models/caseProgression/uploadYourDocumentsSectionBuilder';
 import {caseNumberPrettify} from 'common/utils/stringUtils';
 import {ClaimantOrDefendant} from 'models/partyType';
-import {UploadDocuments, UploadDocumentTypes} from 'models/caseProgression/uploadDocumentsType';
+import {UploadDocuments} from 'models/caseProgression/uploadDocumentsType';
 import {saveDraftClaim} from 'modules/draft-store/draftStoreService';
-import {boolean} from 'boolean';
 
 export const getCancelYourUpload = (claimId: string, claim: Claim) => {
   return new UploadYourDocumentsSectionBuilder()
@@ -23,22 +22,8 @@ export async function cancelDocumentUpload(claimId: string, claim: Claim, claima
   const uploadDocuments: UploadDocuments = claimantOrDefendant === ClaimantOrDefendant.DEFENDANT
     ? claim.caseProgression.defendantUploadDocuments : claim.caseProgression.claimantUploadDocuments;
   for (const uploadDocumentValue of Object.values(uploadDocuments)) {
-    if (uploadDocumentValue instanceof Array && uploadDocumentValue.length > 0 && !(uploadDocumentValue[0] instanceof boolean)) {
-      for (const uploadDocumentValueElement of uploadDocumentValue) {
-        if (!(uploadDocumentValueElement instanceof boolean)) {
-          if ((<UploadDocumentTypes>uploadDocumentValueElement).selected) {
-            const index = (<UploadDocumentTypes[]>uploadDocumentValue).indexOf(<UploadDocumentTypes>uploadDocumentValueElement);
-            if (index > -1) {
-              uploadDocumentValue.splice(index, 1);
-            }
-          }
-        }
-      }
-    }
-  }
-  if (uploadDocuments.checkboxGrp.length > 0 && uploadDocuments.checkboxGrp.every(v => v !== null && v !== undefined)) {
-    for (let index = 0; index < uploadDocuments.checkboxGrp.length; index++) {
-      uploadDocuments.checkboxGrp[index] = false;
+    if (uploadDocumentValue instanceof Array && uploadDocumentValue.length > 0) {
+      uploadDocumentValue.splice(0, uploadDocumentValue.length);
     }
   }
 
