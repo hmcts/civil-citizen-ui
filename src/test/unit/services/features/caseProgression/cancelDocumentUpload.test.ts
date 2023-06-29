c4b9eaa208b9839a6eb04c822cd11456fc0ee421
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-06-29 15:53:20 +0100

    CIV-8019 Unit test data updated

--- src/test/unit/services/features/caseProgression/cancelDocumentUpload.test.ts
+++ src/test/unit/services/features/caseProgression/cancelDocumentUpload.test.ts
@@ -18,8 +18,6 @@ const claimantWitnessStatementDocument: Document = {
   document_url: 'http://dm-store:8080/documents/e9fd1e10-baf2-4d95-bc79-bdeb9f3a2ab6',
   document_filename: 'witness_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/e9fd1e10-baf2-4d95-bc79-bdeb9f3a2ab6/binary',
-  document_hash: 'bfb0337b6b922c16bda85adf37807cbc70ab2cc15399c5532a92d3ac0ce22561',
-  category_id: '',
 };
 
 const claimantWitnessStatement: UploadEvidenceWitness = {
@@ -33,8 +31,6 @@ const claimantWitnessSummaryDocument: Document = {
   document_url: 'http://dm-store:8080/documents/e9fd1e11-bbf2-4d95-bc79-beeb9f3a2ab6',
   document_filename: 'witness_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/e9fd1e11-bbf2-4d95-bc79-beeb9f3a2ab6/binary',
-  document_hash: '7770bacecebc447caa0e7c7eda65b603f822ef41695f89e495da9d826454346b',
-  category_id: '',
 };
 
 const claimantWitnessSummary: UploadEvidenceWitness = {
@@ -48,8 +44,6 @@ const defendantWitnessStatementDocument: Document = {
   document_url: 'http://dm-store:8080/documents/a9fd1e10-baf2-4d95-bc79-bdeb9f3a2ab6',
   document_filename: 'witness_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/a9fd1e10-baf2-4d95-bc79-bdeb9f3a2ab6/binary',
-  document_hash: '2e84809a242c21c42efdafb5742c85ed6fdd7660012c0b55d13476a6d1d8cc1d',
-  category_id: '',
 };
 
 const defendantWitnessStatement: UploadEvidenceWitness = {
@@ -63,8 +57,6 @@ const defendantWitnessSummaryDocument: Document = {
   document_url: 'http://dm-store:8080/documents/e9fd1e11-bbf2-4d95-bc79-beeb9f3a2ab6',
   document_filename: 'witness_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/e9fd1e11-bbf2-4d95-bc79-beeb9f3a2ab6/binary',
-  document_hash: 'd64a8f6cbc6fcf56dbf5ea29fc620c650f2570e88f32a9b97d80989e4cd70402',
-  category_id: '',
 };
 
 const defendantWitnessSummary: UploadEvidenceWitness = {
@@ -78,8 +70,6 @@ const claimantAnswersForExpertDocument: Document = {
   document_url: 'http://dm-store:8080/documents/d9fd1e10-daf2-4d95-dc79-ddeb9f3a2ab6',
   document_filename: 'expert_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/d9fd1e10-daf2-4d95-dc79-ddeb9f3a2ab6/binary',
-  document_hash: 'ecada475a75cb334c769e40bf694b7cfccd82804e0e1edb9f95d08c6357e56c2',
-  category_id: '',
 };
 
 const claimantAnswersForExpert: UploadEvidenceExpert = {
@@ -98,8 +88,6 @@ const claimantExpertReportDocument: Document = {
   document_url: 'http://dm-store:8080/documents/d3fd1e10-daf3-4d93-dc73-ddeb9f3a2ab3',
   document_filename: 'expert_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/d3fd1e10-daf3-4d93-dc73-ddeb9f3a2ab3/binary',
-  document_hash: 'b93759b36e9aaf6ad8aa5b00e1cef537e2fd248d998224e218b678183b417370',
-  category_id: '',
 };
 
 const claimantExpertReport: UploadEvidenceExpert = {
@@ -118,8 +106,6 @@ const defendantAnswersForExpertDocument: Document = {
   document_url: 'http://dm-store:8080/documents/d91d1e10-d1f2-4195-dc79-d1eb9f3a2ab6',
   document_filename: 'expert_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/d91d1e10-d1f2-4195-dc79-d1eb9f3a2ab6/binary',
-  document_hash: 'bfcbc3cf3ca59a0b4b8223cb7c30588bdfa0402bc99bf546f2d8b6a0ca59b2ce',
-  category_id: '',
 };
 
 const defendantAnswersForExpert: UploadEvidenceExpert = {
@@ -138,8 +124,6 @@ const defendantExpertReportDocument: Document = {
   document_url: 'http://dm-store:8080/documents/d3fd1410-da43-4d43-dc73-ddeb9f3a2ab3',
   document_filename: 'expert_document.pdf',
   document_binary_url: 'http://dm-store:8080/documents/d3fd1410-da43-4d43-dc73-ddeb9f3a2ab3/binary',
-  document_hash: '0333a79ddc0e56576612a4fc6ede898bd300b786e28db10bd50063a16329c8cc',
-  category_id: '',
 };
 
 const defendantExpertReport: UploadEvidenceExpert = {
