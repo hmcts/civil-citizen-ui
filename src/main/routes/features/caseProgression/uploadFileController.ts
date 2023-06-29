e3de913e5a1f0b63237fa7ba24cde8aff0f360af
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-06-29 19:54:58 +0100

    fix sonar problems

--- src/main/routes/features/caseProgression/uploadFileController.ts
+++ src/main/routes/features/caseProgression/uploadFileController.ts
@@ -7,7 +7,12 @@ import {t} from 'i18next';
 
 const multer = require('multer');
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 100 * 1024 * 1024, // 100 MB
+  },
+});
 
 const uploadFileController = Router();
 
