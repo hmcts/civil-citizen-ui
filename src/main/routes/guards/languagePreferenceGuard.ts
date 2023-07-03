2f775769c2016fecad40e60f57fae65d4929885c
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-07-03 12:11:27 +0200

    Fix: sonar bug

--- src/main/routes/guards/languagePreferenceGuard.ts
+++ src/main/routes/guards/languagePreferenceGuard.ts
@@ -3,13 +3,15 @@ import {getCaseDataFromStore} from '../../modules/draft-store/draftStoreService'
 import {constructResponseUrlWithIdParams} from '../../common/utils/urlFormatter';
 import {DASHBOARD_URL} from '../urls';
 
-export const languagePreferenceGuard = async (req: Request, res: Response, next: NextFunction) => {
+export const languagePreferenceGuard = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const caseData = await getCaseDataFromStore(req.params.id);
-    if (caseData.claimBilingualLanguagePreference) {
-      return res.redirect(constructResponseUrlWithIdParams(req.params.id, DASHBOARD_URL));
-    } else {
-      next();
+    async () => {
+      const caseData = await getCaseDataFromStore(req.params.id);
+      if (caseData.claimBilingualLanguagePreference) {
+        res.redirect(constructResponseUrlWithIdParams(req.params.id, DASHBOARD_URL));
+      } else {
+        next();
+      }
     }
   } catch (error) {
     next(error);
