6f2ba4ac1753e70c89bb5ed82453e0cffee42290
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-07-04 09:36:25 +0100

    CIV-8019 Failing test fixed

--- src/test/unit/services/features/caseProgression/cancelDocumentUpload.test.ts
+++ src/test/unit/services/features/caseProgression/cancelDocumentUpload.test.ts
@@ -1,10 +1,9 @@
 import {cancelDocumentUpload} from 'services/features/caseProgression/cancelDocumentUpload';
 import {app} from '../../../../../main/app';
 import * as requestModels from 'models/AppRequest';
-import * as utilityService from 'modules/utilityService';
 import {Claim} from 'models/claim';
+import * as utilityService from 'modules/utilityService';
 
-jest.mock('../../../../../main/modules/utilityService');
 const REDIS_DATA = require('../../../../../main/modules/draft-store/redisData.json');
 declare const appRequest: requestModels.AppRequest;
 const mockedAppRequest = requestModels as jest.Mocked<typeof appRequest>;
@@ -25,18 +24,15 @@ describe('cancelDocumentUpload', () => {
   it('should call draft store client to remove a claim with provided id and reimport it afresh from ccd', async () => {
     //Given
     const claimId = '1645882162449409';
-    const mockGetClaimById = utilityService.getClaimById as jest.Mock;
-    mockGetClaimById.mockImplementation(async () => {
-      const claim = new Claim();
-      claim.id = claimId;
-      return claim;
-    });
     app.locals.draftStoreClient = createMockDraftStore(REDIS_DATA[0]);
     const spyDel = jest.spyOn(app.locals.draftStoreClient, 'del');
+    const claimToReturn = new Claim();
+    claimToReturn.id = claimId;
+    const spyGetClaimById = jest.spyOn(utilityService, 'getClaimById').mockReturnValue(Promise.resolve(claimToReturn));
     //When
     await cancelDocumentUpload(claimId, mockedAppRequest);
     //Then
     expect(spyDel).toBeCalled();
-    expect(mockGetClaimById).toBeCalled();
+    expect(spyGetClaimById).toBeCalled();
   });
 });
