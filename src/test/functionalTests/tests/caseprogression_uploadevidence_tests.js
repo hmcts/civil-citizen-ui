1c17f9f5b06360ce7ca61f06bba47d3e0a53e4cb
Raja <raja.mani@hmcts.net>
2023-07-07 12:11:41 +0100

    added the change for case progression as well

--- src/test/functionalTests/tests/caseprogression_uploadevidence_tests.js
+++ src/test/functionalTests/tests/caseprogression_uploadevidence_tests.js
@@ -1,14 +1,23 @@
 const config = require('../../config');
 const UploadEvidenceSteps = require('../features/caseProgression/steps/caseProgressionSteps');
 const LoginSteps = require('../features/home/steps/login');
+const ResponseSteps = require('../features/response/steps/lipDefendantResponseSteps');
 
 let claimRef;
+let caseData;
+let claimNumber;
+let securityCode;
 
 Feature('Case progression journey - Defendant & Claimant Response with RejectAll');
 
 Before(async ({api}) => {
   if (['preview', 'demo'  ].includes(config.runningEnv)) {
     claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser);
+    console.log('claim has been created Successfully    <===>  '  , claimRef);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber = caseData.legacyCaseReference;
+    securityCode = caseData.respondent1PinToPostLRspec.accessCode;
+    await ResponseSteps.AssignCaseToLip(claimNumber, securityCode);
     await api.performCitizenResponse(config.defendantCitizenUser, claimRef);
     await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.rejectAll,'JUDICIAL_REFERRAL');
     await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef);
