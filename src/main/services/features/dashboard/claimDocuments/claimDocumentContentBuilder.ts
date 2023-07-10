34d260faefb9684238209f6b5e0e9b2a92ce5a29
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-07-10 16:26:58 +0100

    defendant defence PDF on document tab

--- src/main/services/features/dashboard/claimDocuments/claimDocumentContentBuilder.ts
+++ src/main/services/features/dashboard/claimDocuments/claimDocumentContentBuilder.ts
@@ -22,24 +22,24 @@ const buildDownloadSealedClaimSection = (claim: Claim, claimId: string, lang: st
   }
 };
 
-const buildGeneratedPDFSection = (claim: Claim, claimId: string, lang: string): ClaimSummarySection[] => {
+const buildDownloadSealedResponseSection = (claim: Claim, claimId: string, lang: string): ClaimSummarySection => {
   const document = claim.getDocumentDetails(DocumentType.DEFENDANT_DEFENCE);
   const downloadClaimLabel = 'PAGES.CLAIM_SUMMARY.DOWNLOAD_RESPONSE';
   const createdLabel = 'PAGES.CLAIM_SUMMARY.DOCUMENT_CREATED';
 
   if(document) {
-    return [{
+    return {
       type: ClaimSummaryType.LINK,
       data: {
         href: CASE_DOCUMENT_DOWNLOAD_URL.replace(':id', claimId).replace(':documentType', DocumentUri.DEFENDANT_DEFENCE),
         text: `${t(downloadClaimLabel, lang)} (PDF, ${displayDocumentSizeInKB(document.documentSize)})`,
         subtitle: `${t(createdLabel, lang)} ${formatDateToFullDate(document.createdDatetime)}`,
       },
-    }];
+    };
   }
 };
 
 export {
   buildDownloadSealedClaimSection,
-  buildGeneratedPDFSection,
+  buildDownloadSealedResponseSection,
 };
--- src/test/unit/services/features/dashboard/claimDocuments/claimDocumentContentBuilder.test.ts
+++ src/test/unit/services/features/dashboard/claimDocuments/claimDocumentContentBuilder.test.ts
@@ -1,6 +1,7 @@
 import {mockClaim} from '../../../../../utils/mockClaim';
 import {
   buildDownloadSealedClaimSection,
+  buildDownloadSealedResponseSection,
 } from '../../../../../../main/services/features/dashboard/claimDocuments/claimDocumentContentBuilder';
 import {CASE_DOCUMENT_DOWNLOAD_URL} from '../../../../../../main/routes/urls';
 import {DocumentUri} from '../../../../../../main/common/models/document/documentType';
@@ -14,5 +15,15 @@ describe('Claim document content builder', ()=>{
     expect(claimDocumentSection.data?.href).toBe(CASE_DOCUMENT_DOWNLOAD_URL.replace(':id', '1').replace(':documentType', DocumentUri.SEALED_CLAIM));
     expect(claimDocumentSection.data?.text).toContain('45 KB');
     expect(claimDocumentSection.data?.subtitle).toContain('21 June 2022');
+  });
+
+  it('should return json with document size in KB and link to defendant response pdf', ()=>{
+    //When
+    const claimDocumentSection = buildDownloadSealedResponseSection(mockClaim, '2', 'eng');
+    //Then
+    expect(claimDocumentSection).not.toBeUndefined();
+    expect(claimDocumentSection.data?.href).toBe(CASE_DOCUMENT_DOWNLOAD_URL.replace(':id', '2').replace(':documentType', DocumentUri.DEFENDANT_DEFENCE));
+    expect(claimDocumentSection.data?.text).toContain('33 KB');
+    expect(claimDocumentSection.data?.subtitle).toContain('22 June 2022');
   });
 });
