5e17b52d615746fdaea7d83c6d5f5a47ed36e875
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-07-11 09:13:12 +0100

    CIV-9239 Method signatures changed

--- src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationBuilder.ts
+++ src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationBuilder.ts
@@ -1,6 +1,5 @@
 import {ClaimSummarySection, ClaimSummaryType} from 'form/models/claimSummarySection';
 import {t} from 'i18next';
-import {Claim} from 'models/claim';
 
 export function getNextStepsTitle(lang: string): ClaimSummarySection[] {
   return [
@@ -13,17 +12,15 @@ export function getNextStepsTitle(lang: string): ClaimSummarySection[] {
   ];
 }
 
-export function buildNextStepsSection(claimId: string, claim: Claim, lng: string, readyForTrialOrHearing: boolean): ClaimSummarySection[] {
+export function buildNextStepsSection(readyForTrialOrHearing: boolean, lng: string): ClaimSummarySection[] {
   if (readyForTrialOrHearing) {
     // TODO: build next steps section for CIV-9204
   } else {
-    const claimantName = claim.getClaimantFullName();
-
     return [
       {
         type: ClaimSummaryType.PARAGRAPH,
         data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_CALL', {claimantName, lng}),
+          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_CALL', {lng}),
         },
       },
     ];
