e029da6d191cc2c07bc99ff4e22f8ef7ae5def13
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-07-11 14:49:58 +0100

    CIV-9239 Another paragraph content created

--- src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationBuilder.ts
+++ src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationBuilder.ts
@@ -1,6 +1,7 @@
 import {ClaimSummarySection, ClaimSummaryType} from 'form/models/claimSummarySection';
 import {t} from 'i18next';
 import {Claim} from 'models/claim';
+import {DEFENDANT_DOCUMENTS_URL} from 'routes/urls';
 
 export function getNextStepsTitle(lang: string): ClaimSummarySection[] {
   return [
@@ -17,13 +18,20 @@ export function buildNextStepsSection(claimId: string, claim: Claim, lng: string
   if (readyForTrialOrHearing) {
     // TODO: build next steps section for CIV-9204
   } else {
-    const claimantName = claim.getClaimantFullName();
-
     return [
       {
         type: ClaimSummaryType.PARAGRAPH,
         data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_CALL', {claimantName, lng}),
+          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_CALL', {lng}),
+        },
+      },
+      {
+        type: ClaimSummaryType.LINK,
+        data: {
+          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.NOTICES_AND_ORDERS', {lng}),
+          href: DEFENDANT_DOCUMENTS_URL.replace(':id', claimId),
+          textBefore: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_CAN_VIEW_TRIAL_ARRANGEMENTS', {lng}),
+          textAfter: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IN_THE_CASE_DETAILS', {lng}),
         },
       },
     ];
