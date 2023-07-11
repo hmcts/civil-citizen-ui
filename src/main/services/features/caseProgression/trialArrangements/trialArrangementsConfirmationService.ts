b95b8b9a26b919be13dc1182953be55973c4866b
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-07-11 17:11:43 +0100

    CIV-9239 Remaining section added

--- src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationService.ts
+++ src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationService.ts
@@ -1,10 +1,12 @@
 import {Claim} from 'models/claim';
 import {ClaimSummarySection} from 'form/models/claimSummarySection';
 import {
+  buildNextStepsSection,
   getNextStepsTitle,
 } from 'services/features/caseProgression/trialArrangements/trialArrangementsConfirmationBuilder';
 
 export const getTrialArrangementsConfirmationContent = (claimId: string, claim: Claim, lang: string, readyForTrialOrHearing: boolean): ClaimSummarySection[] => {
   const nextStepsTitle = getNextStepsTitle(lang);
-  return [nextStepsTitle].flat();
+  const nextStepsSection = buildNextStepsSection(claimId, claim, lang, readyForTrialOrHearing);
+  return [nextStepsTitle, nextStepsSection].flat();
 };
