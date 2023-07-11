29ae1b5538bb8a5f6a5791d9da54d396ab46311f
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-07-11 17:11:18 +0100

    CIV-9239 Remaining paragraphs added

--- src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationBuilder.ts
+++ src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationBuilder.ts
@@ -20,6 +20,26 @@ export function buildNextStepsSection(claimId: string, claim: Claim, lng: string
   } else {
     return [
       {
+        type: ClaimSummaryType.HTML,
+        data: {
+          html: `<div class="warning-text-container">
+                    <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
+                    <strong class="govuk-warning-text__text">
+                    ${t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.TRIAL_OR_HEARING_WILL_GO_AHEAD_AS_PLANNED', {lng})}
+                    </strong>
+                 </div>`,
+        },
+      },
+      {
+        type: ClaimSummaryType.LINK,
+        data: {
+          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.MAKE_AN_APPLICATION', {lng}),
+          href: 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1087082/N244.pdf',
+          textBefore: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IF_YOU_WANT_THE_DATE_OF_THE_HEARING', {lng}),
+          textAfter: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.TO_THE_COURT_AND_PAY', {lng}),
+        },
+      },
+      {
         type: ClaimSummaryType.PARAGRAPH,
         data: {
           text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_CALL', {lng}),
