67a0fa009644ef1754e1352ea28785f19afec89c
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-07-11 10:40:44 +0100

    revert changes

--- src/main/routes/features/document/documentDownloadController.ts
+++ src/main/routes/features/document/documentDownloadController.ts
@@ -7,15 +7,16 @@ import {convertToDocumentType} from 'common/utils/documentTypeConverter';
 import {AppRequest} from 'models/AppRequest';
 import {DocumentType} from 'models/document/documentType';
 import {Claim} from 'models/claim';
+import {getClaimById} from 'modules/utilityService';
 
 const documentDownloadController = Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 
-const civilServiceClientForDocRetrieve: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
+const civilServiceClientForDocRetrieve: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl, true);
 
 documentDownloadController.get(CASE_DOCUMENT_DOWNLOAD_URL, async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const claim: Claim = await civilServiceClientForDocRetrieve.retrieveClaimDetails(req.params.id, <AppRequest>req);
+    const claim: Claim = await getClaimById(req.params.id, <AppRequest>req);
     const documentType = convertToDocumentType(req.params.documentType);
     const documentDetails = claim.getDocumentDetails(DocumentType[documentType]);
     const pdfDocument: Buffer = await civilServiceClientForDocRetrieve.retrieveDocument(documentDetails, <AppRequest>req);
