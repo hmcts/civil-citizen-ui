252f30b7f42e1914b842f8321bec4226f6d3c903
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-07-11 11:53:31 +0100

    Add is-case-ready screen

--- src/main/services/features/caseProgression/trialArrangements/isCaseReady.ts
+++ src/main/services/features/caseProgression/trialArrangements/isCaseReady.ts
@@ -5,12 +5,12 @@ import {caseNumberPrettify} from 'common/utils/stringUtils';
 export const getIsCaseReady = (claimId: string, claim: Claim) => {
   return new UploadYourDocumentsSectionBuilder()
     .addMainTitle('PAGES.IS_CASE_READY.PAGE_TITLE')
-    .addLeadParagraph('PAGES.IS_CASE_READY.CLAIM_NUMBER', {claimId:caseNumberPrettify(claimId)})
+    .addLeadParagraph('PAGES.IS_CASE_READY.CLAIM_NUMBER', {claimId:caseNumberPrettify(claimId)}, 'govuk-!-margin-bottom-0')
     .addLeadParagraph('PAGES.IS_CASE_READY.PARTIES', {
       claimantName: claim.getClaimantFullName(),
       defendantName: claim.getDefendantFullName(),
     })
-    .addTitle('PAGES.IS_CASE_READY.IS_CASE_READY')
+    .addTitle('PAGES.IS_CASE_READY.IS_CASE_READY',null,'govuk-!-margin-bottom-0')
     .addParagraph('PAGES.IS_CASE_READY.YOU_ARE_REMINDED')
     .build();
 };
