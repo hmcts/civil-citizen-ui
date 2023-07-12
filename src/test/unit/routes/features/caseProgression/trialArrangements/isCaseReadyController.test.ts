6e43b5dc22f3d3351eb7936898f7ec98dafc6471
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-07-12 09:15:39 +0100

    Add tests

---
+++ src/test/unit/routes/features/caseProgression/trialArrangements/isCaseReadyController.test.ts
@@ -0,0 +1,103 @@
+import {
+  mockCivilClaim,
+} from '../../../../../utils/mockDraftStore';
+import {DEFENDANT_SUMMARY_URL, HAS_ANYTHING_CHANGED_URL, IS_CASE_READY_URL} from 'routes/urls';
+import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
+import {app} from '../../../../../../main/app';
+import config from 'config';
+import nock from 'nock';
+const session = require('supertest-session');
+import {YesNo} from 'form/models/yesNo';
+import {CIVIL_SERVICE_CASES_URL} from 'client/civilServiceUrls';
+import {t} from 'i18next';
+
+jest.mock('../../../../../../main/modules/oidc');
+jest.mock('../../../../../../main/modules/draft-store');
+
+const claim = require('../../../../../utils/mocks/civilClaimResponseMock.json');
+const claimId = claim.id;
+const civilServiceUrl = config.get<string>('services.civilService.url');
+const testSession = session(app);
+
+describe('Cancel document upload - On GET', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+
+  beforeAll(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+  });
+
+  it('should render page successfully if cookie has correct values', async () => {
+    //Given
+    nock(civilServiceUrl)
+      .get(CIVIL_SERVICE_CASES_URL + claimId)
+      .reply(200, claim);
+    //When
+    await testSession
+      .get(IS_CASE_READY_URL.replace(':id', claimId))
+    //Then
+      .expect((res: { status: unknown; text: unknown; }) => {
+        expect(res.status).toBe(200);
+        expect(res.text).toContain(t('PAGES.EVIDENCE_UPLOAD_CANCEL.TITLE'));
+      });
+  });
+
+  it('should return "Something went wrong" page when claim does not exist', async () => {
+    //Given
+    nock(civilServiceUrl)
+      .get(CIVIL_SERVICE_CASES_URL + '1111')
+      .reply(404, null);
+    //When
+    await testSession
+      .get(IS_CASE_READY_URL.replace(':id', '1111'))
+    //Then
+      .expect((res: { status: unknown; text: unknown; }) => {
+        expect(res.status).toBe(500);
+        expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+      });
+  });
+});
+
+describe('Cancel document upload - on POST', () => {
+  beforeEach(() => {
+    app.locals.draftStoreClient = mockCivilClaim;
+  });
+  it('should display error when neither Yes nor No were selected', async () => {
+
+    //Given
+    nock(civilServiceUrl)
+      .post(CIVIL_SERVICE_CASES_URL + '1111')
+      .reply(200, claimId);
+
+    //When
+    await testSession
+      .post(IS_CASE_READY_URL.replace(':id', '1111'))
+      .send({})
+    //Then
+      .expect((res: {status: unknown, text: unknown}) => {
+        expect(res.status).toBe(200);
+        expect(res.text).toContain(t('ERRORS.VALID_YES_NO_OPTION_NAC_YDW'));
+      });
+  });
+
+  it('should redirect to upload-documents page', async () => {
+
+    //Given
+    nock(civilServiceUrl)
+      .post(CIVIL_SERVICE_CASES_URL + '1111')
+      .reply(200, claimId);
+
+    //When
+    await testSession
+      .post(IS_CASE_READY_URL.replace(':id', '1111'))
+      .send({option: YesNo.NO})
+      //Then
+      .expect((res: {status: unknown, header: {location: unknown}}) => {
+        expect(res.status).toBe(302);
+        expect(res.header.location).toEqual(HAS_ANYTHING_CHANGED_URL.replace(':id', '1111'));
+      });
+  });
+});
+
