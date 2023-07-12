4072f0e87e016f8c301c1f482d147be7f860c026
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2023-07-12 19:44:38 +0100

    test

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,25 +1,11 @@
 #!/usr/bin/env node
 const { Logger } = require('@hmcts/nodejs-logging');
-import {readFileSync} from 'fs';
-import {createServer} from 'https';
-import * as path from 'path';
 import { app } from './app';
 
 const logger = Logger.getLogger('server');
 const port: number = parseInt(process.env.PORT) || 3001;
 
-if (app.locals.ENV === 'development') {
-  const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl');
-  const sslOptions = {
-    cert: readFileSync(path.join(sslDirectory, 'localhost.crt')),
-    key: readFileSync(path.join(sslDirectory, 'localhost.key')),
-  };
-  const server = createServer(sslOptions, app);
-  server.listen(port, () => {
-    logger.info(`Application started: https://localhost:${port}`);
-  });
-} else {
-  app.listen(port, () => {
-    logger.info(`Application started: http://localhost:${port}`);
-  });
-}
+app.listen(port, () => {
+  logger.info(`Application started: http://localhost:${port}`);
+});
+
