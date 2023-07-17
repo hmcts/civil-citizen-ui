07fdf72b860f4e6428b54c975496cb1fa36bfcd5
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-07-17 16:46:38 +0100

    CIV-9716 Set Up page design

--- src/main/routes/features/claimantResponse/partPaymentReceivedController.ts
+++ src/main/routes/features/claimantResponse/partPaymentReceivedController.ts
@@ -22,11 +22,12 @@ const partPaymentReceivedController = Router();
 const partPaymentReceivedViewPath = 'features/claimantResponse/part-payment-received';
 const claimantResponsePropertyName = 'hasDefendantPaidYou';
 
-function renderView(form: GenericForm<GenericYesNo>, res: Response, paidAmount: number): void {
-  res.render(partPaymentReceivedViewPath, {form, paidAmount});
+function renderView(form: GenericForm<GenericYesNo>, res: Response, paidAmount: number, isPaidInFull: boolean): void {
+  res.render(partPaymentReceivedViewPath, {form, paidAmount, isPaidInFull});
 }
 
 let paidAmount: number;
+let isPaidInFull: boolean;
 
 partPaymentReceivedController.get(CLAIMANT_RESPONSE_PART_PAYMENT_RECEIVED_URL, async (req, res, next: NextFunction) => {
   const claimId = req.params.id;
@@ -34,7 +35,9 @@ partPaymentReceivedController.get(CLAIMANT_RESPONSE_PART_PAYMENT_RECEIVED_URL, a
     const claim: Claim = await getCaseDataFromStore(claimId);
     const claimantResponse = claim?.claimantResponse ? claim.claimantResponse : new ClaimantResponse();
     paidAmount = claim.isRejectAllOfClaimAlreadyPaid();
-    renderView(new GenericForm(claimantResponse.hasDefendantPaidYou), res, paidAmount);
+    isPaidInFull = claim.hasPaidInFull();
+    console.log(claim.rejectAllOfClaim.howMuchHaveYouPaid.totalClaimAmount)
+    renderView(new GenericForm(claimantResponse.hasDefendantPaidYou), res, paidAmount, isPaidInFull);
   } catch (error) {
     next(error);
   }
@@ -47,7 +50,7 @@ partPaymentReceivedController.post(CLAIMANT_RESPONSE_PART_PAYMENT_RECEIVED_URL, 
     genericYesNoForm.validateSync();
 
     if (genericYesNoForm.hasErrors()) {
-      renderView(genericYesNoForm, res, paidAmount);
+      renderView(genericYesNoForm, res, paidAmount, isPaidInFull);
     } else {
       await saveClaimantResponse(claimId, genericYesNoForm.model, claimantResponsePropertyName);
       res.redirect(constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_TASK_LIST_URL));
