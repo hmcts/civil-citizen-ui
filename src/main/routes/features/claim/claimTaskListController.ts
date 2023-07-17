ffe56c82ca31c911b6d10f84a143c45e504fc8ca
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-07-17 10:24:45 +0200

    Fix: sonar bug then

--- src/main/routes/features/claim/claimTaskListController.ts
+++ src/main/routes/features/claim/claimTaskListController.ts
@@ -1,6 +1,5 @@
 import {Router, Response, NextFunction} from 'express';
 import {CLAIMANT_TASK_LIST_URL} from '../../urls';
-import {Claim} from 'models/claim';
 import {AppRequest} from 'models/AppRequest';
 import {getTaskLists} from 'services/features/claim/taskListService';
 import {calculateTotalAndCompleted} from 'services/features/common/taskListService';
@@ -11,18 +10,19 @@ const taskListViewPath = 'features/claim/task-list';
 const claimTaskListController = Router();
 
 claimTaskListController.get(CLAIMANT_TASK_LIST_URL, async (req: AppRequest, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const userId = req.session?.user?.id;
-    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-    const caseData: Claim = await getCaseDataFromStore(userId);
+  const userId = req.session?.user?.id;
+  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+  getCaseDataFromStore(userId)
+  .then((caseData) => {
     const taskLists = getTaskLists(caseData, userId, lang);
     const {completed, total} = calculateTotalAndCompleted(taskLists);
     const description = t('PAGES.CLAIM_TASK_LIST.COMPLETED_SECTIONS', {completed, total});
     const title = completed < total ? t('PAGES.CLAIM_TASK_LIST.APPLICATION_COMPLETE') : t('PAGES.CLAIM_TASK_LIST.APPLICATION_INCOMPLETE');
     res.render(taskListViewPath, {taskLists, title, description});
-  } catch (error) {
+  })
+  .catch((error) => {
     next(error);
-  }
+  });
 });
 
 export default claimTaskListController;
