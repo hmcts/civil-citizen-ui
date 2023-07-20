3af75464a3ae70f49511cf774935cfba4bda9ced
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-07-20 12:28:33 +0100

    CIV-9409 New fields added

--- src/main/common/models/caseProgression/caseProgression.ts
+++ src/main/common/models/caseProgression/caseProgression.ts
@@ -1,8 +1,11 @@
 import {UploadDocuments} from 'models/caseProgression/uploadDocumentsType';
+import {YesNoUpperCamelCase} from 'form/models/yesNo';
 
 export class CaseProgression{
   claimantUploadDocuments?: UploadDocuments;
   defendantUploadDocuments?: UploadDocuments;
   claimantLastUploadDate?: Date;
   defendantLastUploadDate?: Date;
+  trialReadyApplicant?: YesNoUpperCamelCase;
+  trialReadyRespondent1?: YesNoUpperCamelCase;
 }
