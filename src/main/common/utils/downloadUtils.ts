ef0df6156ad93c4001ffd2948456b0db95e9d8bf
laperla11 <kegilmez@gmail.com>
2023-07-20 14:52:32 +0100

    CIV-3046 improved test coverage

--- src/main/common/utils/downloadUtils.ts
+++ src/main/common/utils/downloadUtils.ts
@@ -4,7 +4,7 @@ import {FileResponse} from 'models/FileResponse';
 export function displayPDF(res: Response, fileResponse: FileResponse) {
   res.writeHead(200, {
     'Content-Type': 'application/pdf',
-    'Content-Disposition': `inline; filename=${fileResponse?.fileName}`,
+    'Content-Disposition': `inline; filename=${fileResponse.fileName}`,
     'Content-Length': fileResponse?.data?.length,
   });
   res.end(fileResponse.data);
