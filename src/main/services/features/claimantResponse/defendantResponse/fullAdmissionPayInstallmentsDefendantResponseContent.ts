e99a7a92a59b7025ab9e664427c53e838b6e2c79
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-07-20 11:52:47 +0200

    Fix: lint

--- src/main/services/features/claimantResponse/defendantResponse/fullAdmissionPayInstallmentsDefendantResponseContent.ts
+++ src/main/services/features/claimantResponse/defendantResponse/fullAdmissionPayInstallmentsDefendantResponseContent.ts
@@ -1,9 +1,9 @@
 import {
   ClaimSummarySection,
   ClaimSummaryType,
-} from "common/form/models/claimSummarySection";
-import {constructRepaymentPlanSection} from "../claimantResponseService";
-import {Claim} from "common/models/claim";
+} from 'common/form/models/claimSummarySection';
+import {constructRepaymentPlanSection} from '../claimantResponseService';
+import {Claim} from 'common/models/claim';
 
 const getResponseSummaryText = (claim: Claim) => {
   const defendantName = claim.getDefendantFullName();
@@ -11,14 +11,14 @@ const getResponseSummaryText = (claim: Claim) => {
     {
       type: ClaimSummaryType.PARAGRAPH,
       data: {
-        text: "PAGES.REVIEW_DEFENDANTS_RESPONSE.FULL_ADMISSION_PAY_BY_INSTALLMENTS.DEFENDANT_ADMITS",
+        text: 'PAGES.REVIEW_DEFENDANTS_RESPONSE.FULL_ADMISSION_PAY_BY_INSTALLMENTS.DEFENDANT_ADMITS',
         variables: { defendant: defendantName },
       },
     },
     {
       type: ClaimSummaryType.PARAGRAPH,
       data: {
-        text: "PAGES.REVIEW_DEFENDANTS_RESPONSE.FULL_ADMISSION_PAY_BY_INSTALLMENTS.THEY_OFFERED_PAY",
+        text: 'PAGES.REVIEW_DEFENDANTS_RESPONSE.FULL_ADMISSION_PAY_BY_INSTALLMENTS.THEY_OFFERED_PAY',
       },
     },
   ];
