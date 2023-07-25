ee7bec69d75a8c6e8f83a9b1cffb33dbb4bb9790
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-07-25 09:36:10 +0200

    Fix: delete download

--- src/main/routes/features/claim/claimSubmittedController.ts
+++ src/main/routes/features/claim/claimSubmittedController.ts
@@ -1,9 +1,7 @@
 import {NextFunction, Router} from 'express';
-import {CASE_DOCUMENT_DOWNLOAD_URL, CLAIM_CONFIRMATION_URL, CLAIM_FEE_URL} from '../../urls';
+import {CLAIM_CONFIRMATION_URL, CLAIM_FEE_URL} from '../../urls';
 import {getClaimById} from 'modules/utilityService';
-import {DocumentType} from 'models/document/documentType';
 import {formatDateToFullDate} from 'common/utils/dateUtils';
-import {getSystemGeneratedCaseDocumentIdByType} from 'models/document/systemGeneratedCaseDocuments';
 
 const claimSubmittedView = 'features/claim/claim-submitted';
 const claimSubmittedController = Router();
@@ -16,14 +14,16 @@ claimSubmittedController.get(CLAIM_CONFIRMATION_URL, async (req, res, next: Next
 
     if(!claim.isEmpty()) {
       const claimNumber = claim.legacyCaseReference;
-      const downloadHref = CASE_DOCUMENT_DOWNLOAD_URL.replace(':id', claimId).replace(':documentId', getSystemGeneratedCaseDocumentIdByType(claim.systemGeneratedCaseDocuments, DocumentType.SEALED_CLAIM));
       const defendantFullName = claim.getDefendantFullName();
       const defendantResponseLimit = formatDateToFullDate(claim.respondent1ResponseDeadline, lang);
       const helpWithFee = claim.hasHelpWithFees();
       const claimFeeUrl = CLAIM_FEE_URL;
       res.render(claimSubmittedView, {
-        claimNumber, defendantFullName, defendantResponseLimit,
-        helpWithFee, downloadHref, claimFeeUrl,
+        claimNumber, 
+        defendantFullName, 
+        defendantResponseLimit,
+        helpWithFee, 
+        claimFeeUrl,
       });
     }
   } catch (error) {
