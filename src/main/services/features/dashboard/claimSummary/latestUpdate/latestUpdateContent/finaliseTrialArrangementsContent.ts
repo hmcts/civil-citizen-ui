0d539b677dabc9d522fd293cbd7dc9a1b5d39683
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-07-26 17:16:36 +0100

    CIV-9613 Href to the correct tab added

--- src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContent/finaliseTrialArrangementsContent.ts
+++ src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContent/finaliseTrialArrangementsContent.ts
@@ -1,7 +1,8 @@
 import {LatestUpdateSectionBuilder} from 'models/LatestUpdateSectionBuilder/latestUpdateSectionBuilder';
 import {Claim} from 'models/claim';
 import {t} from 'i18next';
-import {CP_FINALISE_TRIAL_ARRANGEMENTS_URL, DEFENDANT_DOCUMENTS_URL} from 'routes/urls';
+import {CP_FINALISE_TRIAL_ARRANGEMENTS_URL, DEFENDANT_SUMMARY_TAB_URL} from 'routes/urls';
+import {TabId} from 'routes/tabs';
 
 const FINALISE_TRIAL_ARRANGEMENTS = 'PAGES.LATEST_UPDATE_CONTENT.CASE_PROGRESSION.FINALISE_TRIAL_ARRANGEMENTS';
 
@@ -11,7 +12,7 @@ export const getFinaliseTrialArrangements = (claim: Claim) => {
                                 <span class="govuk-body govuk-!-font-weight-bold">${t(`${FINALISE_TRIAL_ARRANGEMENTS}.IF_THERE_ARE_CHANGES_END`, {finalisingTrialArrangementsDeadline: deadline})}</span>.
                               </p>`;
   const linkText = `${FINALISE_TRIAL_ARRANGEMENTS}.DIRECTIONS_QUESTIONNAIRE`;
-  const linkHref =  DEFENDANT_DOCUMENTS_URL.replace(':id', claim.id);
+  const linkHref =  DEFENDANT_SUMMARY_TAB_URL.replace(':id', claim.id).replace(':tab', TabId.NOTICES);
   const linkTextBefore = `${FINALISE_TRIAL_ARRANGEMENTS}.YOU_MAY_WISH_TO_REVIEW`;
   const linkTextAfter = `${FINALISE_TRIAL_ARRANGEMENTS}.UNDER_NOTICES_AND_ORDERS`;
 
