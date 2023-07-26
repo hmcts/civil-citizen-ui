949f25e9c43fec717856d92e8ff80aee1a08780c
KumarSam <sampank@gmail.com>
2023-07-26 13:11:52 +0100

    CIV-0000 adding s2s key in key vault

--- infrastructure/key-vault-secrets-serviceauth.tf
+++ infrastructure/key-vault-secrets-serviceauth.tf
@@ -18,7 +18,7 @@ data "azurerm_key_vault_secret" "s2s_secrets" {
 resource "azurerm_key_vault_secret" "s2s" {
   name         = "s2s-secret"
   value        = data.azurerm_key_vault_secret.key_from_vault.value
-  key_vault_id = data.azurerm_key_vault.key_vault.id
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
 }
 
 
