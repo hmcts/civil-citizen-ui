d9912675bf0fa80d3128c4a17bbe87d1eb81eedc
KumarSam <sampank@gmail.com>
2023-07-26 11:19:54 +0100

    CIV-0000 adding s2s key in key vault

---
+++ infrastructure/key-vault-secrets-serviceauth.tf
@@ -0,0 +1,20 @@
+data "azurerm_key_vault" "key_vault" {
+  name                = "${var.product}-${var.env}" # update these values if required
+  resource_group_name = "${var.product}-${var.env}" # update these values if required
+}
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "key_from_vault" {
+  name         = "microservicekey-cui" # update key name e.g. microservicekey-your-name
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "s2s" {
+  name         = "s2s-secret"
+  value        = data.azurerm_key_vault_secret.key_from_vault.value
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
