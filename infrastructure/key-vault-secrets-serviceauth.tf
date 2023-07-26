64614996bb7a9c14640b903f717ce705dd9455a3
KumarSam <sampank@gmail.com>
2023-07-26 12:16:55 +0100

    CIV-0000 adding s2s key in key vault

--- infrastructure/key-vault-secrets-serviceauth.tf
+++ infrastructure/key-vault-secrets-serviceauth.tf
@@ -3,13 +3,38 @@ data "azurerm_key_vault" "s2s_vault" {
   resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
 
-data "azurerm_key_vault_secret" "key_from_vault" {
-  name         = "microservicekey-cui" # update key name e.g. microservicekey-your-name
+locals {
+  s2s_secrets = ["microservicekey-cui"]
+}
+
+data "azurerm_key_vault_secret" "s2s_secrets" {
+  for_each = toset(local.s2s_secrets)
+
+  name         = each.value
   key_vault_id = data.azurerm_key_vault.s2s_vault.id
 }
+
 
 resource "azurerm_key_vault_secret" "s2s" {
   name         = "s2s-secret"
   value        = data.azurerm_key_vault_secret.key_from_vault.value
   key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+
+resource "azurerm_key_vault_secret" "s2s_secrets" {
+  for_each = data.azurerm_key_vault_secret.s2s_secrets
+
+  name         = each.value.name
+  value        = each.value.value
+  key_vault_id = module.key-vault.key_vault_id
+
+  content_type = "secret"
+  tags = merge(var.common_tags, {
+    "source" : "Key Vault ${data.azurerm_key_vault.s2s_vault.name}"
+  })
+
+  depends_on = [
+    module.key-vault
+  ]
 }
