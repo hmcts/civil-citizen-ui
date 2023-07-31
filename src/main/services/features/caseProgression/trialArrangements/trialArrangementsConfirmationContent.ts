a0b22ce0d3f87745107f556e5f1d3c9d976a6e5f
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-07-31 16:17:53 +0100

    refactoring to builder

--- src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationContent.ts
+++ src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationContent.ts
@@ -1,86 +1,42 @@
-import {ClaimSummarySection, ClaimSummaryType} from 'form/models/claimSummarySection';
-import {t} from 'i18next';
+import {ClaimSummarySection} from 'form/models/claimSummarySection';
 import {Claim} from 'models/claim';
 import {DEFENDANT_DOCUMENTS_URL} from 'routes/urls';
-
-export function getNextStepsTitle(lang: string): ClaimSummarySection[] {
-  return [
-    {
-      type: ClaimSummaryType.TITLE,
-      data: {
-        text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.WHAT_HAPPENS_NEXT', {lng: lang}),
-      },
-    },
-  ];
-}
+import {
+  FinaliseYourTrialSectionBuilder,
+} from 'models/caseProgression/trialArrangements/finaliseYourTrialSectionBuilder';
 
 export function getNextStepsSection(claimId: string, claim: Claim, lng: string, readyForTrialOrHearing: boolean): ClaimSummarySection[] {
+  const title = 'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.WHAT_HAPPENS_NEXT';
+  const document = 'https://www.gov.uk/government/publications/form-n244-application-notice';
   if (readyForTrialOrHearing) {
-    return [
-      {
-        type: ClaimSummaryType.LINK,
-        data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.NOTICES_AND_ORDERS', {lng}),
-          href: DEFENDANT_DOCUMENTS_URL.replace(':id', claimId),
-          textBefore: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_CAN_VIEW_TRIAL_ARRANGEMENTS', {lng}),
-          textAfter: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IN_THE_CASE_DETAILS', {lng}),
-        },
-      },
-      {
-        type: ClaimSummaryType.LINK,
-        data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.MAKE_AN_APPLICATION', {lng}),
-          href: 'https://www.gov.uk/government/publications/form-n244-application-notice',
-          textBefore: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IF_THERE_ARE_ANY_CHANGES_TO_THE_ARRANGEMENTS', {lng}),
-          textAfter: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.AS_SOON_AS_POSSIBLE_AND_PAY', {lng}),
-          externalLink: true,
-        },
-      },
-      {
-        type: ClaimSummaryType.PARAGRAPH,
-        data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_PHONE', {lng}),
-        },
-      },
-    ];
-  } else {
-    return [
-      {
-        type: ClaimSummaryType.HTML,
-        data: {
-          html: `<div class="warning-text-container">
-                    <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
-                    <strong class="govuk-warning-text__text">
-                    ${t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.TRIAL_OR_HEARING_WILL_GO_AHEAD_AS_PLANNED', {lng})}
-                    </strong>
-                 </div>`,
-        },
-      },
-      {
-        type: ClaimSummaryType.LINK,
-        data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.MAKE_AN_APPLICATION', {lng}),
-          href: 'https://www.gov.uk/government/publications/form-n244-application-notice',
-          textBefore: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IF_YOU_WANT_THE_DATE_OF_THE_HEARING', {lng}),
-          textAfter: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.TO_THE_COURT_AND_PAY', {lng}),
-          externalLink: true,
-        },
-      },
-      {
-        type: ClaimSummaryType.PARAGRAPH,
-        data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_CALL', {lng}),
-        },
-      },
-      {
-        type: ClaimSummaryType.LINK,
-        data: {
-          text: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.NOTICES_AND_ORDERS', {lng}),
-          href: DEFENDANT_DOCUMENTS_URL.replace(':id', claimId),
-          textBefore: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_CAN_VIEW_TRIAL_ARRANGEMENTS', {lng}),
-          textAfter: t('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IN_THE_CASE_DETAILS', {lng}),
-        },
-      },
-    ];
+    return new FinaliseYourTrialSectionBuilder()
+      .addMainTitle(title)
+      .addLink('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.NOTICES_AND_ORDERS',
+        DEFENDANT_DOCUMENTS_URL.replace(':id', claimId),
+        'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_CAN_VIEW_TRIAL_ARRANGEMENTS',
+        'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IN_THE_CASE_DETAILS')
+      .addLink('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.MAKE_AN_APPLICATION',
+        document,
+        'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IF_THERE_ARE_ANY_CHANGES_TO_THE_ARRANGEMENTS',
+        'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.AS_SOON_AS_POSSIBLE_AND_PAY',
+        '',
+        true)
+      .addParagraph('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_PHONE')
+      .build();
   }
+  return new FinaliseYourTrialSectionBuilder()
+    .addMainTitle(title)
+    .addWarning('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.TRIAL_OR_HEARING_WILL_GO_AHEAD_AS_PLANNED')
+    .addLink('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.MAKE_AN_APPLICATION',
+      document,
+      'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IF_YOU_WANT_THE_DATE_OF_THE_HEARING',
+      'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.TO_THE_COURT_AND_PAY',
+      '',
+      true)
+    .addParagraph('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_WILL_NEED_TO_CALL')
+    .addLink('PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.NOTICES_AND_ORDERS',
+      DEFENDANT_DOCUMENTS_URL.replace(':id', claimId),
+      'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.YOU_CAN_VIEW_TRIAL_ARRANGEMENTS',
+      'PAGES.FINALISE_TRIAL_ARRANGEMENTS.CONFIRMATION.IN_THE_CASE_DETAILS')
+    .build();
 }
--- src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationService.ts
+++ src/main/services/features/caseProgression/trialArrangements/trialArrangementsConfirmationService.ts
@@ -2,11 +2,8 @@ import {Claim} from 'models/claim';
 import {ClaimSummarySection} from 'form/models/claimSummarySection';
 import {
   getNextStepsSection,
-  getNextStepsTitle,
 } from 'services/features/caseProgression/trialArrangements/trialArrangementsConfirmationContent';
 
 export const getTrialArrangementsConfirmationContent = (claimId: string, claim: Claim, lang: string, readyForTrialOrHearing: boolean): ClaimSummarySection[] => {
-  const nextStepsTitle = getNextStepsTitle(lang);
-  const nextStepsSection = getNextStepsSection(claimId, claim, lang, readyForTrialOrHearing);
-  return [nextStepsTitle, nextStepsSection].flat();
+  return getNextStepsSection(claimId, claim, lang, readyForTrialOrHearing);
 };
