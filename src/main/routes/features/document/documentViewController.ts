4f11495d1162e0f0ba436dd318de760b57b51709
meulendijksm <max.meulendijks@hmcts.net>
2023-08-01 17:48:47 +0100

    CIV-7748 - ignore sonarcloud error

--- src/main/routes/features/document/documentViewController.ts
+++ src/main/routes/features/document/documentViewController.ts
@@ -7,6 +7,8 @@ import {viewFile} from 'common/utils/downloadUtils';
 const documentViewController = Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClientForDocRetrieve: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl, true);
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 documentViewController.get(CASE_DOCUMENT_VIEW_URL, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const document = await civilServiceClientForDocRetrieve.retrieveDocument(req.params.documentId);
