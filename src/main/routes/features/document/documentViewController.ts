f67590b073767c15cd35d4042e844e3d64b40386
meulendijksm <max.meulendijks@hmcts.net>
2023-08-02 15:14:48 +0100

    CIV-7748 - make documentViewController act as RequestHandler

--- src/main/routes/features/document/documentViewController.ts
+++ src/main/routes/features/document/documentViewController.ts
@@ -8,12 +8,12 @@ const documentViewController = Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClientForDocRetrieve: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl, true);
 
-documentViewController.get(CASE_DOCUMENT_VIEW_URL, async (req: Request, res: Response, next: NextFunction) => {
+documentViewController.get(CASE_DOCUMENT_VIEW_URL, (async (req: Request, res: Response, next: NextFunction) => {
   try {
     const document = await civilServiceClientForDocRetrieve.retrieveDocument(req.params.documentId);
     viewFile(res, document);
   } catch (error) {
     next(error);
   }
-}) as RequestHandler;
+}) as RequestHandler);
 export default documentViewController;
