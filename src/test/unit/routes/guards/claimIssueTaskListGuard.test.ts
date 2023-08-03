e851978fc43afc516b834b0da68f2309a955cd74
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-08-03 10:48:59 +0200

    CIV-9831 : Refactor tests

--- src/test/unit/routes/guards/claimIssueTaskListGuard.test.ts
+++ src/test/unit/routes/guards/claimIssueTaskListGuard.test.ts
@@ -1,68 +1,77 @@
-import {Request, Response, NextFunction} from 'express';
 import {Claim} from 'models/claim';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {app} from '../../../../main/app';
-import {claimIssueTaskListGuard} from 'routes/guards/claimIssueTaskListGuard';
+import request from 'supertest';
+import {
+  BASE_ELIGIBILITY_URL,
+  CLAIMANT_TASK_LIST_URL,
+} from 'routes/urls';
+import config from 'config';
+import nock from 'nock';
+import {t} from 'i18next';
 
-jest.mock('../../../../main/modules/draft-store/draftStoreService');
+jest.mock('../../../../main/modules/oidc');
 jest.mock('../../../../main/modules/draft-store');
+jest.mock('../../../../main/modules/draft-store/draftStoreService');
 
 const mockGetCaseData = getCaseDataFromStore as jest.Mock;
 
-const MOCK_REQUEST = { session: {user : { id: '123' } }} as unknown as Request;
-const MOCK_RESPONSE = { redirect: jest.fn() } as unknown as Response;
-const MOCK_NEXT = jest.fn() as NextFunction;
-
 describe('Claim Issue TaskList Guard', () => {
-  beforeEach(() => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+
+  beforeAll(() => {
     jest.resetAllMocks();
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
   });
   it('should redirect if claim not exists and eligibility not completed', async () => {
     //Given
-    const mockClaim = new Claim();
-    mockClaim.id = null;
 
+    const mockClaim = new Claim();
     mockGetCaseData.mockImplementation(async () => mockClaim);
     app.request.cookies = {};
     //When
-    await claimIssueTaskListGuard(MOCK_REQUEST, MOCK_RESPONSE, MOCK_NEXT);
+    const res = await request(app).get(CLAIMANT_TASK_LIST_URL).send();
     //Then
-    expect(MOCK_RESPONSE.redirect).toHaveBeenCalled();
+    expect(res.status).toBe(302);
+    expect(res.header.location).toBe(BASE_ELIGIBILITY_URL);
   });
 
   it('should access to claim/task-list page when claim exist', async () => {
     //Given
     const mockClaim = new Claim();
     mockClaim.id = '1';
-
     mockGetCaseData.mockImplementation(async () => mockClaim);
     //When
-    await claimIssueTaskListGuard(MOCK_REQUEST, MOCK_RESPONSE, MOCK_NEXT);
+    const res = await request(app).get(CLAIMANT_TASK_LIST_URL).send();
     //Then
-    expect(MOCK_NEXT).toHaveBeenCalled();
+    expect(res.status).toBe(200);
+    expect(res.text).toContain(t('PAGES.CLAIM_TASK_LIST.PAGE_TITLE'));
   });
   it('should access to claim/task-list  page when eligibility questions completed', async () => {
     //Given
     const mockClaim = new Claim();
-    app.request.cookies = {eligibilityCompleted: true};
-
     mockGetCaseData.mockImplementation(async () => mockClaim);
+    app.request.cookies = {eligibilityCompleted: true};
     //When
-    await claimIssueTaskListGuard(MOCK_REQUEST, MOCK_RESPONSE, MOCK_NEXT);
+    const res = await request(app).get(CLAIMANT_TASK_LIST_URL).send();
     //Then
-    expect(MOCK_NEXT).toHaveBeenCalled();
+    expect(res.status).toBe(200);
+    expect(res.text).toContain(t('PAGES.CLAIM_TASK_LIST.PAGE_TITLE'));
   });
   it('should access to claim/task-list page when eligibility question completed and claim exist', async () => {
     //Given
     const mockClaim = new Claim();
     mockClaim.id = '1';
-    app.request.cookies = {eligibilityCompleted: true};
-
     mockGetCaseData.mockImplementation(async () => mockClaim);
+    app.request.cookies = {eligibilityCompleted: true};
     //When
-    await claimIssueTaskListGuard(MOCK_REQUEST, MOCK_RESPONSE, MOCK_NEXT);
+    const res = await request(app).get(CLAIMANT_TASK_LIST_URL).send();
     //Then
-    expect(MOCK_NEXT).toHaveBeenCalled();
+    expect(res.status).toBe(200);
+    expect(res.text).toContain(t('PAGES.CLAIM_TASK_LIST.PAGE_TITLE'));
   });
 
 });
