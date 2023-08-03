b847bbe77eac1ded2b8fb5a7356b2db3c2a47a6c
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-08-03 10:57:04 +0200

    CIV-9831 : 1 hour expiration date

--- src/main/routes/features/public/eligibility/eligibleController.ts
+++ src/main/routes/features/public/eligibility/eligibleController.ts
@@ -13,8 +13,9 @@ const youCanUseViewPath = 'features/public/eligibility/eligible';
 
 eligibleController.get([ELIGIBILITY_HWF_ELIGIBLE_URL, ELIGIBILITY_HWF_ELIGIBLE_REFERENCE_URL, ELIGIBLE_FOR_THIS_SERVICE_URL], (req: Request, res: Response) => {
   const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+  const MILLISECONDS_IN_1_HOUR = 3600000;
   if(!req.cookies['eligibilityCompleted']){
-    res.cookie('eligibilityCompleted', true, {maxAge: 240000, httpOnly: true });
+    res.cookie('eligibilityCompleted', true, {maxAge: MILLISECONDS_IN_1_HOUR, httpOnly: true });
   }
   const youCanUseContent = getYouCanUseContent(req.url, getLng(lang));
   const claimTaskListUrl = CLAIMANT_TASK_LIST_URL;
