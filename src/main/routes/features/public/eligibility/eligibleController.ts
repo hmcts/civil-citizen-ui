2473cc63a0734bcf8305ed64a063fa460871d215
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-08-03 10:11:34 +0200

    CIV-9831 : Cookie settle

--- src/main/routes/features/public/eligibility/eligibleController.ts
+++ src/main/routes/features/public/eligibility/eligibleController.ts
@@ -4,18 +4,17 @@ import {
   ELIGIBILITY_HWF_ELIGIBLE_REFERENCE_URL,
   ELIGIBLE_FOR_THIS_SERVICE_URL,
   CLAIMANT_TASK_LIST_URL,
-} from '../../../../routes/urls';
-import {getYouCanUseContent} from '../../../../services/features/eligibility/eligibleService';
-import {getLng} from '../../../../common/utils/languageToggleUtils';
+} from 'routes/urls';
+import {getYouCanUseContent} from 'services/features/eligibility/eligibleService';
+import {getLng} from 'common/utils/languageToggleUtils';
 
 const eligibleController = Router();
 const youCanUseViewPath = 'features/public/eligibility/eligible';
 
 eligibleController.get([ELIGIBILITY_HWF_ELIGIBLE_URL, ELIGIBILITY_HWF_ELIGIBLE_REFERENCE_URL, ELIGIBLE_FOR_THIS_SERVICE_URL], (req: Request, res: Response) => {
   const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-  if(!req.cookies['eligibilityCompletedV2']){
-    const cookie = true;
-    res.cookie('eligibilityCompletedV2', cookie);
+  if(!req.cookies['eligibilityCompleted']){
+    res.cookie('eligibilityCompleted', true, {maxAge: 240000, httpOnly: true });
   }
   const youCanUseContent = getYouCanUseContent(req.url, getLng(lang));
   const claimTaskListUrl = CLAIMANT_TASK_LIST_URL;
