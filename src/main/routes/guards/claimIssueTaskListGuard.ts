264e35899cc6a8a85909849a13417b7ead1533d0
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-08-03 10:09:56 +0200

    CIV-9831 : Guard refactor

--- src/main/routes/guards/claimIssueTaskListGuard.ts
+++ src/main/routes/guards/claimIssueTaskListGuard.ts
@@ -1,21 +1,23 @@
-import {NextFunction, Response} from 'express';
+import {NextFunction, Request, Response} from 'express';
 import {Claim} from 'models/claim';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {AppRequest} from 'models/AppRequest';
+import {BASE_ELIGIBILITY_URL} from 'routes/urls';
 
-export class ClaimIssueTaskListGuard {
-  static apply(redirectUrl: string) {
-    return async (req: AppRequest, res: Response, next: NextFunction): Promise<void> => {
-      try {
-        const userId = req.session?.user?.id;
-        const caseData: Claim = await getCaseDataFromStore(userId);
-        if (!caseData.id && !req.cookies['eligibilityCompletedV2']){
-          res.redirect(redirectUrl);
-        }
-        return next();
-      } catch (error) {
-        next(error);
+export const claimIssueTaskListGuard = (req: Request, res: Response, next: NextFunction) => {
+  (async () => {
+    try {
+      const appReq: AppRequest = <AppRequest>req;
+      const userId = appReq.session?.user?.id;
+      const caseData: Claim = await getCaseDataFromStore(userId);
+      if (!caseData.id && !req.cookies['eligibilityCompleted']){
+        res.redirect(BASE_ELIGIBILITY_URL);
+      }else{
+        next();
       }
-    };
-  }
-}
+    } catch (error) {
+      next(error);
+    }
+  })();
+};
+
