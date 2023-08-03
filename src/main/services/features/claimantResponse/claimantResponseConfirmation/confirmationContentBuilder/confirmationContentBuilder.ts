039d5abdfbcb4848dcb0c6e7567b2c43214fd3f4
laperla11 <kegilmez@gmail.com>
2023-08-03 17:42:57 +0100

    CIV-8784 developed the content for partAdmit payImmediately accepted scenario

--- src/main/services/features/claimantResponse/claimantResponseConfirmation/confirmationContentBuilder/confirmationContentBuilder.ts
+++ src/main/services/features/claimantResponse/claimantResponseConfirmation/confirmationContentBuilder/confirmationContentBuilder.ts
@@ -3,19 +3,26 @@ import {YesNo} from 'common/form/models/yesNo';
 import {Claim} from 'common/models/claim';
 import {ClaimResponseStatus} from 'common/models/claimResponseStatus';
 import {getClaimantResponseStatus, getRCDisputeNotContinueNextSteps} from './disputeConfirmationContentBuilder';
+import {getPAPayImmediatelyAcceptedNextSteps} from './partAdmitConfirmationContentBuilder';
 
 export function buildClaimantResponseSection(claim: Claim, lang: string): ClaimSummarySection[] {
   const RCDisputeNotContinueStatus = getClaimantResponseStatus(claim, 'PAGES.CLAIMANT_RESPONSE_CONFIRMATION.RC_DISPUTE.NOT_PROCEED_WITH_CLAIM', lang);
+  const PAPayImmediatelyAcceptedStatus = getClaimantResponseStatus(claim, 'PAGES.CLAIMANT_RESPONSE_CONFIRMATION.PA_PAY_IMMEDIATELY.ACCEPTED_DEFENDANT_RESPONSE', lang);
 
   if (claim.claimantResponse?.intentionToProceed?.option === YesNo.NO) {
     return RCDisputeNotContinueStatus;
+  } else if (claim.responseStatus === ClaimResponseStatus.PA_NOT_PAID_PAY_IMMEDIATELY && claim.claimantResponse?.hasPartAdmittedBeenAccepted?.option === YesNo.YES) {
+    return PAPayImmediatelyAcceptedStatus;
   }
 }
 
 export function buildNextStepsSection(claim: Claim, lang: string): ClaimSummarySection[] {
   const RCDisputeNotContinueNextSteps = getRCDisputeNotContinueNextSteps(claim, lang);
+  const PAPayImmediatelyAcceptedNextSteps = getPAPayImmediatelyAcceptedNextSteps(claim, lang);
 
   if (claim.responseStatus === ClaimResponseStatus.RC_DISPUTE && claim.claimantResponse?.intentionToProceed?.option === YesNo.NO) {
     return RCDisputeNotContinueNextSteps;
+  } else if (claim.responseStatus === ClaimResponseStatus.PA_NOT_PAID_PAY_IMMEDIATELY && claim.claimantResponse?.hasPartAdmittedBeenAccepted?.option === YesNo.YES) {
+    return PAPayImmediatelyAcceptedNextSteps;
   }
 }
