b554a05be76cabf4241eab2863f1a7e6f2e994e1
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-08-08 12:37:54 +0100

    CIV-9201 fix tests

--- src/test/unit/routes/features/caseProgression/trialArrangements/finaliseTrialArrangementsController.test.ts
+++ src/test/unit/routes/features/caseProgression/trialArrangements/finaliseTrialArrangementsController.test.ts
@@ -7,6 +7,7 @@ import {CIVIL_SERVICE_CASES_URL} from 'client/civilServiceUrls';
 import Module from 'module';
 import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
 import {mockCivilClaim, mockRedisFailure} from '../../../../../utils/mockDraftStore';
+import {DocumentType} from 'models/document/documentType';
 const session = require('supertest-session');
 const testSession = session(app);
 const citizenRoleToken: string = config.get('citizenRoleToken');
@@ -24,6 +25,37 @@ jest.mock('../../../../../../main/app/auth/user/oidc', () => ({
 describe('"finalise trial arrangements" page test', () => {
   const claim = require('../../../../../utils/mocks/civilClaimResponseMock.json');
   const claimId = claim.id;
+  claim.case_data.systemGeneratedCaseDocuments= [
+    {
+      id: '1',
+      value: {
+        createdBy: 'cui',
+        documentType: DocumentType.DEFENDANT_DEFENCE,
+        documentLink:  {
+          document_url: 'url1',
+          document_filename: 'filename1',
+          document_binary_url: 'documents/123/binary',
+        },
+        documentName: 'documentName',
+        createdDatetime: new Date(Date.now()),
+        documentSize: 1,
+      },
+    },
+    {
+      id: '1',
+      value: {
+        createdBy: 'cui',
+        documentType: DocumentType.SDO_ORDER,
+        documentLink: {
+          document_url: 'url1',
+          document_filename: 'filename1',
+          document_binary_url: 'documents/123/binary',
+        },
+        documentName: 'documentName',
+        createdDatetime: new Date(Date.now()),
+        documentSize: 1,
+      },
+    }];
   const civilServiceUrl = config.get<string>('services.civilService.url');
   const idamUrl: string = config.get('idamUrl');
 
