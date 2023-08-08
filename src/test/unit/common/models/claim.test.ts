db1c5f1d76eb07fcb969553e7345f2181ace8171
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-08-08 12:27:11 +0200

    New: unit tests

--- src/test/unit/common/models/claim.test.ts
+++ src/test/unit/common/models/claim.test.ts
@@ -39,6 +39,17 @@ import {Experts} from 'common/models/directionsQuestionnaire/experts/experts';
 import {ExpertDetails} from 'models/directionsQuestionnaire/experts/expertDetails';
 import {ExpertDetailsList} from 'common/models/directionsQuestionnaire/experts/expertDetailsList';
 import {CaseProgressionHearing, CaseProgressionHearingDocuments} from 'models/caseProgression/caseProgressionHearing';
+import { ClaimantResponse } from 'common/models/claimantResponse';
+import { CCJRequest } from 'common/models/claimantResponse/ccj/ccjRequest';
+import { DefendantDOB } from 'common/models/claimantResponse/ccj/defendantDOB';
+import { PaidAmount } from 'common/models/claimantResponse/ccj/paidAmount';
+import { CcjPaymentOption } from 'common/form/models/claimantResponse/ccj/ccjPaymentOption';
+import { QualifiedStatementOfTruth } from 'common/form/models/statementOfTruth/qualifiedStatementOfTruth';
+import { DateOfBirth } from 'common/models/claimantResponse/ccj/dateOfBirth';
+import { PaymentDate } from 'common/form/models/admission/fullAdmission/paymentOption/paymentDate';
+import { RepaymentPlanInstalments } from 'common/models/claimantResponse/ccj/repaymentPlanInstalments';
+import { InstalmentFirstPaymentDate } from 'common/models/claimantResponse/ccj/instalmentFirstPaymentDate';
+import { TransactionSchedule } from 'common/form/models/statementOfMeans/expensesAndIncome/transactionSchedule';
 
 jest.mock('../../../../main/modules/i18n/languageService', ()=> ({
   getLanguage: jest.fn(),
@@ -1247,6 +1258,60 @@ describe('Documents', () => {
       claim = new Claim();
       //Then
       expect(claim.hasExpertDetails()).toBeFalsy();
+    });
+  });
+  describe('isCCJCompleted', () => {
+    let claim: Claim;
+    beforeEach(() => {
+      claim = new Claim;
+    });
+
+    it('should return false', () => {
+      //Then
+      expect(claim.isCCJCompleted()).toBeFalsy();
+    });
+    
+    it('should return true when paid by immediately', () => {
+      //Given
+      claim.claimantResponse = new ClaimantResponse();
+      claim.claimantResponse.ccjRequest = new CCJRequest();
+      claim.claimantResponse.ccjRequest.defendantDOB = new DefendantDOB(YesNo.NO);
+      claim.claimantResponse.ccjRequest.paidAmount = new PaidAmount(YesNo.NO);
+      claim.claimantResponse.ccjRequest.ccjPaymentOption = new CcjPaymentOption(PaymentOptionType.IMMEDIATELY);
+      claim.claimantResponse.ccjRequest.statementOfTruth = new QualifiedStatementOfTruth(true);
+      //Then
+      expect(claim.isCCJCompleted()).toBeTruthy();
+    });
+
+    it('should return true when paid by set day', () => {
+      //Given
+      claim.claimantResponse = new ClaimantResponse();
+      claim.claimantResponse.ccjRequest = new CCJRequest();
+      claim.claimantResponse.ccjRequest.defendantDOB = new DefendantDOB(YesNo.YES, new DateOfBirth({year:'1991', month:'1', day:'1'}));
+      claim.claimantResponse.ccjRequest.paidAmount = new PaidAmount(YesNo.YES, 10, 100);
+      claim.claimantResponse.ccjRequest.ccjPaymentOption = new CcjPaymentOption(PaymentOptionType.BY_SET_DATE);
+      claim.claimantResponse.ccjRequest.defendantPaymentDate = new PaymentDate('2000', '1', '1');
+      claim.claimantResponse.ccjRequest.statementOfTruth = new QualifiedStatementOfTruth(true);
+      //Then
+      expect(claim.isCCJCompleted()).toBeTruthy();
+    });
+
+    it('should return true when paid by instalments', () => {
+      //Given
+      claim.claimantResponse = new ClaimantResponse();
+      claim.claimantResponse.ccjRequest = new CCJRequest();
+      claim.claimantResponse.ccjRequest.defendantDOB = new DefendantDOB(YesNo.YES, new DateOfBirth({year:'1991', month:'1', day:'1'}));
+      claim.claimantResponse.ccjRequest.paidAmount = new PaidAmount(YesNo.YES, 10, 100);
+      claim.claimantResponse.ccjRequest.ccjPaymentOption = new CcjPaymentOption(PaymentOptionType.INSTALMENTS);
+      claim.claimantResponse.ccjRequest.repaymentPlanInstalments =  new RepaymentPlanInstalments(
+        '500', 
+        new InstalmentFirstPaymentDate({year:'1991', month:'1', day:'1'}),
+        TransactionSchedule.WEEK,
+        10,
+        );
+      claim.claimantResponse.ccjRequest.statementOfTruth = new QualifiedStatementOfTruth(true);
+      //Then
+      expect(claim.isCCJCompleted()).toBeTruthy();
     });
   });
 
