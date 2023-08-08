2b8987c0411fc597ee9659493c1844095d0381a1
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-08-08 13:47:35 +0200

    Fix: return promise

--- src/main/routes/features/claimantResponse/ccj/ccjConfirmationController.ts
+++ src/main/routes/features/claimantResponse/ccj/ccjConfirmationController.ts
@@ -1,18 +1,18 @@
-import {NextFunction, Router} from 'express';
+import {NextFunction, RequestHandler, Router} from 'express';
 import {CCJ_CONFIRMATION_URL} from '../../../urls';
 import {getCaseDataFromStore} from '../../../../../main/modules/draft-store/draftStoreService';
 import {ccjConfirmationGuard} from 'routes/guards/ccjConfirmationGuard';
 
 const ccjConfirmationController = Router();
 
-ccjConfirmationController.get(CCJ_CONFIRMATION_URL, ccjConfirmationGuard, async (req, res, next: NextFunction) => {
+ccjConfirmationController.get(CCJ_CONFIRMATION_URL, ccjConfirmationGuard, (async (req, res, next: NextFunction) => {
   try {
     const claim = await getCaseDataFromStore(req.params.id);
     const defendantName = claim.getDefendantFullName();
-    return res.render('features/claimantResponse/ccj/ccj-confirmation', {defendantName});
+    res.render('features/claimantResponse/ccj/ccj-confirmation', {defendantName});
   } catch (error) {
-    return next(error);
+    next(error);
   }
-});
+}) as RequestHandler);
 
 export default ccjConfirmationController;
