3cfc1d13466be6e022b08f1ac2c2211ea5116f44
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-08-10 11:20:03 +0100

    CIV-8105 Function updated with the tile content.

--- src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContent/viewFinalGeneralOrderContent.ts
+++ src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContent/viewFinalGeneralOrderContent.ts
@@ -1,7 +1,19 @@
 import {LatestUpdateSectionBuilder} from 'models/LatestUpdateSectionBuilder/latestUpdateSectionBuilder';
 import {ClaimSummarySection} from 'form/models/claimSummarySection';
 
+const VIEW_ORDER = 'PAGES.LATEST_UPDATE_CONTENT.CASE_PROGRESSION.VIEW_ORDER';
+
 export const getViewFinalGeneralOrder = ():  ClaimSummarySection[] => {
-  const latestUpdateSectionBuilder = new LatestUpdateSectionBuilder();
+  const title = `${VIEW_ORDER}.TITLE`;
+  const judgeHasMadeOrder = `${VIEW_ORDER}.JUDGE_HAS_MADE_ORDER`;
+  const orderIsAvailable = `${VIEW_ORDER}.ORDER_IS_AVAILABLE`;
+  const viewOrderText = `${VIEW_ORDER}.VIEW_ORDER`;
+  const viewOrderHref = 'href'; // TODO - add a correct URL once CIV-9958 is finished
+
+  const latestUpdateSectionBuilder = new LatestUpdateSectionBuilder()
+    .addTitle(title)
+    .addParagraph(judgeHasMadeOrder)
+    .addParagraph(orderIsAvailable)
+    .addButton(viewOrderText, viewOrderHref);
   return latestUpdateSectionBuilder.build();
 };
