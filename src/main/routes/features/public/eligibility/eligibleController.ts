d908817928cb4682cd65f7509dee3fb3f21a0669
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-08-11 08:32:01 +0200

    CIV-9831 : Change expiration to 60min back

--- src/main/routes/features/public/eligibility/eligibleController.ts
+++ src/main/routes/features/public/eligibility/eligibleController.ts
@@ -13,10 +13,9 @@ const youCanUseViewPath = 'features/public/eligibility/eligible';
 
 eligibleController.get([ELIGIBILITY_HWF_ELIGIBLE_URL, ELIGIBILITY_HWF_ELIGIBLE_REFERENCE_URL, ELIGIBLE_FOR_THIS_SERVICE_URL], (req: Request, res: Response) => {
   const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-  //const MILLISECONDS_IN_1_HOUR = 3600000;
-  const MILLISECONDS_IN_5_MIN = 300000;
+  const MILLISECONDS_IN_1_HOUR = 3600000;
   if(!req.cookies['eligibilityCompleted']){
-    res.cookie('eligibilityCompleted', true, {maxAge: MILLISECONDS_IN_5_MIN, httpOnly: true });
+    res.cookie('eligibilityCompleted', true, {maxAge: MILLISECONDS_IN_1_HOUR, httpOnly: true });
   }
   const youCanUseContent = getYouCanUseContent(req.url, getLng(lang));
   const claimTaskListUrl = CLAIMANT_TASK_LIST_URL;
