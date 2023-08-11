cae3f1314cc296b2e865936e69bb461b38cac671
rhysbeazleyy <Rhys.Beazley@HMCTS.NET>
2023-08-11 16:29:04 +0100

    Update claimantIntentGuard.test.ts

--- src/test/unit/routes/guards/claimantIntentGuard.test.ts
+++ src/test/unit/routes/guards/claimantIntentGuard.test.ts
@@ -1,22 +1,22 @@
-import { claimantIntentGuard } from "routes/guards/claimantIntentGuard";
-import { Request, Response } from "express";
-import { Claim } from "common/models/claim";
-import { getClaimById } from "modules/utilityService";
-import { constructResponseUrlWithIdParams } from "common/utils/urlFormatter";
-import { DASHBOARD_CLAIMANT_URL } from "routes/urls";
-jest.mock("modules/utilityService", () => ({
+import { claimantIntentGuard } from 'routes/guards/claimantIntentGuard';
+import { Request, Response } from 'express';
+import { Claim } from 'common/models/claim';
+import { getClaimById } from 'modules/utilityService';
+import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
+import { DASHBOARD_CLAIMANT_URL } from 'routes/urls';
+jest.mock('modules/utilityService', () => ({
   getClaimById: jest.fn(),
 }));
-describe("claimantIntentGuard", () => {
+describe('claimantIntentGuard', () => {
   let req: Partial<Request>;
   let res: Partial<Response> & { redirect: jest.Mock };
   let next: jest.Mock;
   beforeEach(() => {
-    req = { params: { id: "123" } };
+    req = { params: { id: '123' } };
     res = { redirect: jest.fn() };
     next = jest.fn();
   });
-  it("should call next if isClaimantIntentionPending returns true", async () => {
+  it('should call next if isClaimantIntentionPending returns true', async () => {
     const claim: Partial<Claim> = {
       isClaimantIntentionPending: jest.fn().mockReturnValue(true),
     };
@@ -25,20 +25,20 @@ describe("claimantIntentGuard", () => {
     expect(next).toHaveBeenCalled();
     expect(res.redirect).not.toHaveBeenCalled();
   });
-  it("should redirect if isClaimantIntentionPending returns false", async () => {
+  it('should redirect if isClaimantIntentionPending returns false', async () => {
     const claim = {
       isClaimantIntentionPending: jest.fn().mockReturnValue(false),
     };
     (getClaimById as jest.Mock).mockResolvedValueOnce(claim as any);
     const redirectUrl = constructResponseUrlWithIdParams(
       req.params.id,
-      DASHBOARD_CLAIMANT_URL
+      DASHBOARD_CLAIMANT_URL,
     );
     await claimantIntentGuard(req as Request, res as Response, next);
     expect(res.redirect).toHaveBeenCalledWith(redirectUrl);
   });
-  it("should pass the error to next if there is an exception", async () => {
-    const error = new Error("Test error");
+  it('should pass the error to next if there is an exception', async () => {
+    const error = new Error('Test error');
     (getClaimById as jest.Mock).mockRejectedValueOnce(error);
     await claimantIntentGuard(req as Request, res as Response, next);
     expect(next).toHaveBeenCalledWith(error);
