15c0d15a2a08e4a81bdd9e7d232492a8bf4a7f60
laperla11 <kegilmez@gmail.com>
2023-08-14 09:20:35 +0100

    CIV-4694 fixed code styles

--- src/main/assets/js/cookies-controller.js
+++ src/main/assets/js/cookies-controller.js
@@ -2,10 +2,10 @@
  * This code controls displaying success notification banner on cookies page after successfulyy saving cookies preferences
  */
 
-if (document.getElementById("cui-cookies-submit")) {
-  document.getElementById("cui-cookies-submit").addEventListener("click", () => {
-    [...document.getElementsByClassName("govuk-notification-banner--success")].forEach((element) => {
-      element.classList.remove("govuk-visually-hidden");
+if (document.getElementById('cui-cookies-submit')) {
+  document.getElementById('cui-cookies-submit').addEventListener('click', () => {
+    [...document.getElementsByClassName('govuk-notification-banner--success')].forEach((element) => {
+      element.classList.remove('govuk-visually-hidden');
     });
   });
-};
+}
