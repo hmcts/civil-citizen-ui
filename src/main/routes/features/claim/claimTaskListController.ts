ea92affbda1a814fc470b3ce3ef4a4636e80e378
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-08-16 14:20:03 +0100

    change return type for GET claimTaskListController

--- src/main/routes/features/claim/claimTaskListController.ts
+++ src/main/routes/features/claim/claimTaskListController.ts
@@ -12,7 +12,7 @@ import {claimIssueTaskListGuard} from 'routes/guards/claimIssueTaskListGuard';
 const taskListViewPath = 'features/claim/task-list';
 const claimTaskListController = Router();
 
-claimTaskListController.get(CLAIMANT_TASK_LIST_URL, claimIssueTaskListGuard, async (req: AppRequest, res: Response, next: NextFunction) : void  => {
+claimTaskListController.get(CLAIMANT_TASK_LIST_URL, claimIssueTaskListGuard, async (req: AppRequest, res: Response, next: NextFunction): Promise<void> => {
   const userId = req.session?.user?.id;
   const lang = req.query.lang ? req.query.lang : req.cookies.lang;
   const isReleasedTwoEnabled = await isCUIReleaseTwoEnabled();
