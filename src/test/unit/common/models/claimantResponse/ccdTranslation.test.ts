139d1ade8010b6505c3bca8de71fb4e6dd6ecab2
rhysbeazleyy <Rhys.Beazley@HMCTS.NET>
2023-08-16 09:28:00 +0100

    Update ccdTranslation.test.ts

--- src/test/unit/common/models/claimantResponse/ccdTranslation.test.ts
+++ src/test/unit/common/models/claimantResponse/ccdTranslation.test.ts
@@ -1,59 +1,45 @@
-import { CCDClaim } from 'models/civilClaimResponse';
 import { Claim } from 'models/claim';
-import { PaymentOptionType } from 'form/models/admission/paymentOption/paymentOptionType';
-import { toCCDDJPaymentFrequency } from 'services/translation/response/convertToCCDDJPaymentFrequency';
-import { toCCDDJPaymentOption } from 'services/translation/claimantResponse/convertToCCDDJPaymentOption';
-import { toCCDParty } from '../../../../../main/services/translation/response/convertToCCDParty';
-import { toCCDYesNo } from '../../../../../main/services/translation/response/convertToCCDYesNo';
 import { translateClaimantResponseDJToCCD } from '../../../../../main/services/translation/claimantResponse/ccdTranslation';
-import { YesNo, YesNoUpperCamelCase } from 'common/form/models/yesNo';
+import { YesNoUpperCamelCase } from 'common/form/models/yesNo';
+import { Party } from 'common/models/party';
+import { PartyType } from 'common/models/partyType';
 
-describe('translateClaimantResponseDJToCCD', () => {
-  const claim: Claim = {
-    applicant1: {
-      name: 'John Doe',
-    },
-    respondent1: {
-      name: 'Jane Smith',
-    },
-    totalClaimAmount: 100,
-    claimantResponse: {
-      ccjRequest: {
-        paidAmount: {
-          option: 'YES',
-          amount: 50,
-          totalAmount: 100,
-        },
-        ccjPaymentOption: {
-          type: PaymentOptionType.BY_SET_DATE,
-        },
-        defendantPaymentDate: {
-          date: '2022-01-01',
-        },
-      },
-    },
-  };
-
-  test('should translate the claimant response to CCD format', () => {
-    // given
-    const expectedCCDClaim: CCDClaim = {
-      applicant1: toCCDParty(claim.applicant1),
-      respondent1: toCCDParty(claim.respondent1),
-      applicant1Represented: YesNoUpperCamelCase.NO,
-      totalClaimAmount: claim.totalClaimAmount,
-      partialPayment: toCCDYesNo(claim.claimantResponse?.ccjRequest?.paidAmount?.option),
-      partialPaymentAmount: claim.claimantResponse?.ccjRequest?.paidAmount?.option === YesNo.YES ? claim.claimantResponse?.ccjRequest?.paidAmount?.amount.toString() : undefined,
-      paymentTypeSelection: toCCDDJPaymentOption(claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type),
-      paymentSetDate: claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type === PaymentOptionType.BY_SET_DATE ? claim.claimantResponse?.ccjRequest?.defendantPaymentDate?.date : undefined,
-      repaymentFrequency: claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type === PaymentOptionType.INSTALMENTS ? toCCDDJPaymentFrequency(claim.claimantResponse?.ccjRequest?.repaymentPlanInstalments?.paymentFrequency) : undefined,
-      repaymentDue: claim.claimantResponse?.ccjRequest?.paidAmount?.option === YesNo.YES ? (claim.claimantResponse?.ccjRequest?.paidAmount?.totalAmount - claim.claimantResponse?.ccjRequest?.paidAmount?.amount).toString() : undefined,
-      repaymentSuggestion: claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type === PaymentOptionType.INSTALMENTS ? claim.claimantResponse?.ccjRequest?.repaymentPlanInstalments?.amount.toString() : undefined,
-    };
-
-    // when
-    const result = translateClaimantResponseDJToCCD(claim);
-
-    // then
-    expect(result).toEqual(expectedCCDClaim);
+describe('translate draft claim to ccd version', () => {
+    it('should translate applicant1 to ccd', () => {
+      //Given
+      const claim = new Claim();
+      //When
+      const ccdClaim = translateClaimantResponseDJToCCD(claim);
+      //Then
+      expect(ccdClaim.applicant1Represented).toBe(YesNoUpperCamelCase.NO);
+    });
+    it('should translate applicant 1 to ccd', () => {
+      //Given
+      const claim = new Claim();
+      claim.applicant1 = new Party();
+      claim.applicant1.type = PartyType.COMPANY;
+      claim.applicant1.partyDetails = {
+        partyName: 'test',
+      };
+      //When
+      const ccdClaim = translateClaimantResponseDJToCCD(claim);
+      //Then
+      expect(ccdClaim.applicant1).not.toBeUndefined();
+      expect(ccdClaim.applicant1?.companyName).toBe('test');
+    });
+    it('should translate respondent 1 to ccd', () => {
+      //Given
+      const claim = new Claim();
+      claim.respondent1 = new Party();
+      claim.respondent1.type = PartyType.COMPANY;
+      claim.respondent1.partyDetails = {
+        partyName: 'test',
+      };
+      //When
+      const ccdClaim = translateClaimantResponseDJToCCD(claim);
+      //Then
+      expect(ccdClaim.respondent1).not.toBeUndefined();
+      expect(ccdClaim.respondent1?.companyName).toBe('test');
+    });
+  
   });
-});
