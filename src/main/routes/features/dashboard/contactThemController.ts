7385c5a95626805540d5c25934629ea92b04f7be
laperla11 <kegilmez@gmail.com>
2023-08-17 12:59:30 +0100

    CIV-10010 removed the back link from the page

--- src/main/routes/features/dashboard/contactThemController.ts
+++ src/main/routes/features/dashboard/contactThemController.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response, Router} from 'express';
-import {CITIZEN_CONTACT_THEM_URL, CLAIM_DETAILS_URL, FINANCIAL_DETAILS_URL} from '../../urls';
+import {CITIZEN_CONTACT_THEM_URL, CLAIM_DETAILS_URL} from '../../urls';
 import {Claim} from '../../../common/models/claim';
 import {getCaseDataFromStore} from '../../../modules/draft-store/draftStoreService';
 import {constructResponseUrlWithIdParams} from '../../../common/utils/urlFormatter';
@@ -8,12 +8,11 @@ import {getAddress, getSolicitorName} from '../../../../main/services/features/r
 const citizenContactThemViewPath = 'features/dashboard/contact-them';
 const contactThemController = Router();
 
-function renderView(res: Response, claim: Claim, claimantDetailsUrl: string, claimDetailsUrl: string, financialDetailsUrl: string): void {
+function renderView(res: Response, claim: Claim, claimantDetailsUrl: string, claimDetailsUrl: string): void {
   res.render(citizenContactThemViewPath, {
     claim,
     claimantDetailsUrl,
     claimDetailsUrl,
-    backUrl: financialDetailsUrl,
     address: getAddress(claim),
     solicitorName: getSolicitorName(claim),
   });
@@ -25,8 +24,7 @@ contactThemController.get(
       const claim: Claim = await getCaseDataFromStore(req.params.id);
       const claimantDetailsUrl = constructResponseUrlWithIdParams(req.params.id, CITIZEN_CONTACT_THEM_URL);
       const claimDetailsUrl = constructResponseUrlWithIdParams(req.params.id, CLAIM_DETAILS_URL);
-      const financialDetailsUrl = constructResponseUrlWithIdParams(req.params.id, FINANCIAL_DETAILS_URL);
-      renderView(res, claim, claimantDetailsUrl, claimDetailsUrl, financialDetailsUrl);
+      renderView(res, claim, claimantDetailsUrl, claimDetailsUrl);
     } catch (error) {
       next(error);
     }
