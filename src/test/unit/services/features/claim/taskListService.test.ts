9964f2ed5602b83060fdda68b0af8302d235ec4f
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2023-08-21 16:38:59 +0100

    update task list service tests

--- src/test/unit/services/features/claim/taskListService.test.ts
+++ src/test/unit/services/features/claim/taskListService.test.ts
@@ -327,5 +327,25 @@ describe('Claim Task List service', () => {
       ];
       expect(taskList).toMatchObject(expectedTaskList);
     });
+
+    it('should return incomplete task list', () => {
+      //Given
+
+      //When
+      const taskList = getTaskLists(undefined, userId, lng);
+      //Then
+      expectedTaskConsiderOptions.tasks[0].status = TaskStatus.INCOMPLETE;
+      expectedTaskPrepareClaim.tasks[0].status = TaskStatus.INCOMPLETE;
+      expectedTaskPrepareClaim.tasks[1].status = TaskStatus.INCOMPLETE;
+      expectedTaskPrepareClaim.tasks[2].status = TaskStatus.INCOMPLETE;
+      expectedTaskPrepareClaim.tasks[3].status = TaskStatus.INCOMPLETE;
+      expectedTaskPrepareClaim.tasks[4].status = TaskStatus.INCOMPLETE;
+      const expectedTaskList: TaskList[] = [
+        expectedTaskConsiderOptions,
+        expectedTaskPrepareClaim,
+        expectedTaskSubmit,
+      ];
+      expect(taskList).toMatchObject(expectedTaskList);
+    });
   });
 });
