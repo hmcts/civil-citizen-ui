8b8bc74191501f06132b8e8c9afbcd561ee8d052
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-08-24 09:06:18 +0100

    CIV-9201 Add case details on top

--- src/main/services/features/caseProgression/trialArrangements/checkAnswersService.ts
+++ src/main/services/features/caseProgression/trialArrangements/checkAnswersService.ts
@@ -1,11 +1,12 @@
 import {SummarySections} from '../../../../common/models/summaryList/summarySections';
 import {Claim} from '../../../../common/models/claim';
 import {
+  buildCaseInfoContents,
   buildIsCaseReadyForTrialOrHearing,
 } from 'services/features/response/checkAnswers/caseProgression/trialArrangements/buildTrialReadyConfirmationSection';
+import {PageSectionBuilder} from "common/utils/pageSectionBuilder";
 
 const buildSummarySections = (claim: Claim, claimId: string, lang: string | unknown): SummarySections => {
-
   return {
     sections: [
       buildIsCaseReadyForTrialOrHearing(claim, claimId, lang),
@@ -17,3 +18,6 @@ export const getSummarySections = (claimId: string, claim: Claim, lang?: string 
   return buildSummarySections(claim, claimId, lang);
 };
 
+export const getCaseInfoContents = (claimId: string, claim: Claim, lang?: string | unknown): PageSectionBuilder => {
+  return buildCaseInfoContents(claim, claimId, lang);
+};
