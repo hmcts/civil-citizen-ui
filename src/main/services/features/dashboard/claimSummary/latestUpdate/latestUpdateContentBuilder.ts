d3bc1d9be786a7e5e4f81229d9a8c6e4d32c160e
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-08-28 09:51:44 +0200

    CIV-10145 : New latest update content

--- src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContentBuilder.ts
+++ src/main/services/features/dashboard/claimSummary/latestUpdate/latestUpdateContentBuilder.ts
@@ -239,11 +239,11 @@ function getPartAdmitAlreadyPaidNotAccepted(claim: Claim) {
 
 function getPartAdmitNotPaidNotAccepted(claim: Claim) {
   const fullAmount = claim.totalClaimAmount;
-  const partAmount = claim.partAdmitPaidValuePounds;
-  const claimantFullName = claim.getClaimantFullName();
+  const partAmount = claim.partialAdmission?.howMuchDoYouOwe?.amount;
+  const claimantName = claim.getClaimantFullName();
   const claimId = claim.id;
   return new LatestUpdateSectionBuilder()
-    .addTitle(`${PAGES_LATEST_UPDATE_CONTENT}.REJECTED_YOUR_ADMISSION`, { claimantFullName, partAmount })
+    .addTitle(`${PAGES_LATEST_UPDATE_CONTENT}.REJECTED_YOUR_ADMISSION`, { claimantName, partAmount })
     .addParagraph(`${PAGES_LATEST_UPDATE_CONTENT}.THEY_BELIEVE_FULL_AMOUNT_CLAIMED`, { fullAmount })
     .addParagraph(`${PAGES_LATEST_UPDATE_CONTENT}.YOU_MIGHT_HAVE_TO_GO_TO_A_COURT_HEARING`)
     .addResponseDocumentLink(`${PAGES_LATEST_UPDATE_CONTENT}.DOWNLOAD_YOUR_RESPONSE`, claimId, getSystemGeneratedCaseDocumentIdByType(claim.systemGeneratedCaseDocuments, DocumentType.DEFENDANT_DEFENCE))
@@ -289,7 +289,7 @@ function generateLastUpdateResponseSections(claimResponseStatus: ClaimResponseSt
     [ClaimResponseStatus.PA_ALREADY_PAID_ACCEPTED_SETTLED]: getPartAdmitAlreadyPaidSettled(claim, lng),
     [ClaimResponseStatus.PA_ALREADY_PAID_ACCEPTED_NOT_SETTLED]: getPartAdmitAlreadyPaidNotSettled(claim),
     [ClaimResponseStatus.PA_ALREADY_PAID_NOT_ACCEPTED]: getPartAdmitAlreadyPaidNotAccepted(claim),
-    [ClaimResponseStatus.PA_NOT_PAID_PAY_IMMEDIATELY_NOT_ACCEPTED]: getPartAdmitNotPaidNotAccepted(claim),
+    [ClaimResponseStatus.PA_NOT_PAID_NOT_ACCEPTED]: getPartAdmitNotPaidNotAccepted(claim),
   };
   return claimResponsesStatus[claimResponseStatus as keyof typeof claimResponsesStatus];
 }
@@ -430,7 +430,6 @@ export const buildResponseToClaimSection = (claim: Claim, claimId: string, lang:
   const responseDeadlinePassedContent = getPastResponseDeadlineContent(claim, lng);
   const respondToClaimLink = getRespondToClaimLink(claimId, lng);
   const responseStatus = claim.responseStatus;
-  console.log(responseStatus);
   if (claim.isDefendantNotResponded()) {
     sectionContent.push(responseNotSubmittedTitle);
     if (claim.isDeadLinePassed()) {
@@ -452,7 +451,6 @@ export const buildResponseToClaimSection = (claim: Claim, claimId: string, lang:
   } else if(claim.hasClaimantRequestedCCJ() && !claim.isClaimSettled()) {
     sectionContent.push(generateCCJRequestedLatestUpdate(claim, lng));
   } else if (claim.isClaimSettled()) {
-    console.log('settled');
     sectionContent.push(generateClaimSettledLatestUpdate(claim, lng));
   } else if (claim.hasApplicant1DeadlinePassed()) {
     sectionContent.push(getLastUpdateForClaimDismissed(claim));
