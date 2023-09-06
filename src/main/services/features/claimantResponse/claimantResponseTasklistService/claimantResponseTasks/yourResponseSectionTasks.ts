6fdb3cc969e02480e7540b46bf40fdc4db023cdf
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-09-06 11:24:12 +0200

    CIV-9018 : Remove getFreeTelephoneMediation to reuse the existing one.

--- src/main/services/features/claimantResponse/claimantResponseTasklistService/claimantResponseTasks/yourResponseSectionTasks.ts
+++ src/main/services/features/claimantResponse/claimantResponseTasklistService/claimantResponseTasks/yourResponseSectionTasks.ts
@@ -3,13 +3,10 @@ import {Claim} from 'common/models/claim';
 import {TaskStatus} from 'common/models/taskList/TaskStatus';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {
-  CITIZEN_FREE_TELEPHONE_MEDIATION_URL,
   CLAIMANT_RESPONSE_PART_PAYMENT_RECEIVED_URL,
   CLAIMANT_RESPONSE_SETTLE_CLAIM_URL,
 } from 'routes/urls';
 import {Task} from 'models/taskList/task';
-import {YesNo} from 'common/form/models/yesNo';
-import {hasClaimantResponseContactPersonAndCompanyPhone} from 'common/utils/taskList/tasks/taskListHelpers';
 
 export function getHaveYouBeenPaidTask(claim: Claim, claimId: string, lang: string): Task {
   const haveYouBeenPaidTask = {
@@ -39,29 +36,5 @@ export function getSettleTheClaimForTask(claim: Claim, claimId: string, lang: st
     settleTheClaimForTask.status = TaskStatus.COMPLETE;
   }
   return settleTheClaimForTask;
-}
-
-export function getFreeTelephoneMediationTask(claim: Claim, claimId: string, lang: string): Task {
-  const freeTelephoneMediationTask = {
-    description: t('CLAIMANT_RESPONSE_TASK_LIST.CHOOSE_WHAT_TODO_NEXT.FREE_TELEPHONE_MEDIATION', {lng: lang}),
-    url: constructResponseUrlWithIdParams(claimId, CITIZEN_FREE_TELEPHONE_MEDIATION_URL),
-    status: TaskStatus.INCOMPLETE,
-  };
-
-  const mediation = claim.claimantResponse?.mediation;
-
-  if (mediation?.mediationDisagreement?.option === YesNo.NO) {
-    freeTelephoneMediationTask.status = TaskStatus.COMPLETE;
-  } else {
-    if (mediation?.canWeUse?.option === YesNo.YES || mediation?.canWeUse?.mediationPhoneNumber) {
-      freeTelephoneMediationTask.status = TaskStatus.COMPLETE;
-    }
-    if (mediation?.companyTelephoneNumber?.option === YesNo.NO && hasClaimantResponseContactPersonAndCompanyPhone(claim)) {
-      freeTelephoneMediationTask.status = TaskStatus.COMPLETE;
-    } else if (mediation?.companyTelephoneNumber?.mediationPhoneNumberConfirmation) {
-      freeTelephoneMediationTask.status = TaskStatus.COMPLETE;
-    }
-  }
-  return freeTelephoneMediationTask;
 }
 
