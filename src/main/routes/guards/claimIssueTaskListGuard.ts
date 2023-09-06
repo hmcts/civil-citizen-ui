3fc877dcb012844940c5ac3ee25417d8b6c71fe5
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-09-06 11:43:24 +0100

    CIV-8912 Claim issue Incomplete Task Page

--- src/main/routes/guards/claimIssueTaskListGuard.ts
+++ src/main/routes/guards/claimIssueTaskListGuard.ts
@@ -2,7 +2,11 @@ import {NextFunction, Request, Response} from 'express';
 import {Claim} from 'models/claim';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {AppRequest} from 'models/AppRequest';
-import {BASE_ELIGIBILITY_URL} from 'routes/urls';
+import {BASE_ELIGIBILITY_URL, CLAIM_INCOMPLETE_SUBMISSION_URL} from 'routes/urls';
+import {getTaskLists, outstandingTasksFromTaskLists} from 'services/features/common/taskListService';
+import assert from 'assert';
+import {Task} from 'models/taskList/task';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 
 export const claimIssueTaskListGuard = (req: Request, res: Response, next: NextFunction) => {
   (async () => {
@@ -13,7 +17,17 @@ export const claimIssueTaskListGuard = (req: Request, res: Response, next: NextF
       if (!caseData.id && !req.cookies['eligibilityCompleted']){
         res.redirect(BASE_ELIGIBILITY_URL);
       }else{
-        next();
+        const lang = req?.query?.lang ? req.query.lang : req?.cookies?.lang;
+        const taskLists = getTaskLists(caseData,  appReq.session.claimId, lang);
+        assert(taskLists && taskLists.length > 0, 'Task list cannot be empty');
+        const outstandingTasks: Task[] = outstandingTasksFromTaskLists(taskLists);
+
+        const allTasksCompleted = outstandingTasks?.length === 0;
+        if (allTasksCompleted) {
+          return next();
+        }
+
+        res.redirect(constructResponseUrlWithIdParams(appReq.session.claimId, CLAIM_INCOMPLETE_SUBMISSION_URL));
       }
     } catch (error) {
       next(error);
