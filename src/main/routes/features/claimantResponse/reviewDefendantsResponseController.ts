d31b54798e82a5a1af53d31a4e3fefc4782af18c
laperla11 <kegilmez@gmail.com>
2023-09-08 12:46:56 +0100

    CIV-9615 added link for response download

--- src/main/routes/features/claimantResponse/reviewDefendantsResponseController.ts
+++ src/main/routes/features/claimantResponse/reviewDefendantsResponseController.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response, Router} from 'express';
-import {CLAIMANT_RESPONSE_REVIEW_DEFENDANTS_RESPONSE_URL, CLAIMANT_RESPONSE_TASK_LIST_URL} from 'routes/urls';
+import {CASE_DOCUMENT_DOWNLOAD_URL, CLAIMANT_RESPONSE_REVIEW_DEFENDANTS_RESPONSE_URL, CLAIMANT_RESPONSE_TASK_LIST_URL} from 'routes/urls';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {Claim} from 'models/claim';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
@@ -14,6 +14,8 @@ import {
   getDefendantsResponseContent,
 } from 'services/features/claimantResponse/defendantResponse/defendantResponseSummaryService';
 import {formatDateToFullDate} from 'common/utils/dateUtils';
+import {getSystemGeneratedCaseDocumentIdByType} from 'common/models/document/systemGeneratedCaseDocuments';
+import {DocumentType} from 'common/models/document/documentType';
 
 const reviewDefendantsResponseController = Router();
 const revieDefendantResponseViewPath = 'features/claimantResponse/review-defendants-response';
@@ -24,8 +26,7 @@ reviewDefendantsResponseController.get(CLAIMANT_RESPONSE_REVIEW_DEFENDANTS_RESPO
     const claimId = req.params.id;
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const claim: Claim = await getCaseDataFromStore(claimId);
-    // TODO: to be done after CIV-5793 is completed
-    const downloadResponseLink = '#';
+    const downloadResponseLink = CASE_DOCUMENT_DOWNLOAD_URL.replace(':id', req.params.id).replace(':documentId', getSystemGeneratedCaseDocumentIdByType(claim.systemGeneratedCaseDocuments, DocumentType.DEFENDANT_DEFENCE));;
     const financialDetails = getFinancialDetails(claim, lang);
     const defendantsResponseContent = getDefendantsResponseContent(claim, getLng(lang));
     const repaymentPlan = constructRepaymentPlanSection(claim, getLng(lang));
