ee90438c6f017074c2be8562e3f4ca39a2e567f5
Pettedson John <pettedson.john@hmcts.net>
2023-09-20 22:17:23 +0100

    Adding the automated test for the purposes of Testing the features....

--- src/test/functionalTests/tests/caseprogression_uploadevidence_tests.js
+++ src/test/functionalTests/tests/caseprogression_uploadevidence_tests.js
@@ -1,6 +1,6 @@
 const config = require('../../config');
 //const performSDOSteps = require('../features/caseProgression/steps/performSDO');
-//const UploadEvidenceSteps = require('../features/caseProgression/steps/caseProgressionSteps');
+const UploadEvidenceSteps = require('../features/caseProgression/steps/caseProgressionSteps');
 const LoginSteps = require('../features/home/steps/login');
 
 let claimRef;
@@ -17,15 +17,13 @@ Before(async ({api}) => {
   }
   await api.performCitizenResponse(config.defendantCitizenUser, claimRef, 'FastTrack');
   await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL');
-  //performSDOSteps.loadSDOPage(claimRef);
-  await api.getSDODocumentDetails(config.judgeUserWithRegionId1);
-  //await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef);
-  //await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef);
+  await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef);
+  await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef);
 });
 
 //Disabled the test due to the defect https://tools.hmcts.net/jira/browse/CIV-9730
 Scenario('Response with RejectAll and DisputeAll For the Case Progression and Hearing Scheduled Process To Complete', () => {
-  /*if (['preview', 'demo'].includes(config.runningEnv)) {
+  if (['preview', 'demo'].includes(config.runningEnv)) {
     UploadEvidenceSteps.initiateUploadEvidenceJourney(claimRef);
-  }*/
+  }
 }).tag('@regression');
