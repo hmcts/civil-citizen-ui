562c45fde6a58865d1559ef413b09c49f828b2ba
Raja <raja.mani@hmcts.net>
2023-09-21 11:01:12 +0100

    updated the feature name

--- src/test/functionalTests/tests/createClaim_LipvLip_eligibilityCheck_tests.js
+++ src/test/functionalTests/tests/createClaim_LipvLip_eligibilityCheck_tests.js
@@ -1,6 +1,6 @@
 const CreateLipvLipClaimSteps  =  require('../features/createClaim/steps/createLipvLipClaimSteps');
 
-Feature('Response with AdmitAll');
+Feature('Create Lip v Lip claim');
 
 Scenario('Verify the Eligibility Check journey @citizenUIR2', async () => {
   await CreateLipvLipClaimSteps.EligibilityCheckSteps();
