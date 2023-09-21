d25b46c304d9d28880bbfd2b2fd63fe938a76742
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-09-21 16:43:44 +0100

    CIV-9406 Function extended to cover full class creation for defendant and partial for claimant

--- src/main/services/translation/convertToCUI/convertToCUITrialArrangements.ts
+++ src/main/services/translation/convertToCUI/convertToCUITrialArrangements.ts
@@ -1,14 +1,24 @@
 import {CCDClaim} from 'models/civilClaimResponse';
-import {TrialArrangements} from 'models/caseProgression/trialArrangements/trialArrangements';
-
-const claimantPrefix = 'claimant';
-const defendantPrefix = 'defendant';
+import {TrialArrangements, TrialArrangementsDocument} from 'models/caseProgression/trialArrangements/trialArrangements';
+import {CaseRole} from 'form/models/caseRoles';
+import {toCUIYesNo} from 'services/translation/convertToCUI/convertToCUIYesNo';
+import {HasAnythingChangedForm} from 'models/caseProgression/trialArrangements/hasAnythingChangedForm';
 
 export const toCUITrialArrangements = (ccdClaim: CCDClaim, isClaimant: boolean): TrialArrangements => {
-  if (ccdClaim && ccdClaim.trialReadyDocuments) {
+  if (ccdClaim ) {
     const trialArrangements: TrialArrangements = new TrialArrangements();
-    trialArrangements.trialArrangementsDocument = isClaimant ? ccdClaim.trialReadyDocuments.find(doc => doc.value.documentName.toLowerCase().includes(claimantPrefix))
-      : ccdClaim.trialReadyDocuments.find(doc => doc.value.documentName.toLowerCase().includes(defendantPrefix));
-    return trialArrangements.trialArrangementsDocument ? trialArrangements : undefined;
+    if (isClaimant) {
+      const claimantTrialArrangementsDocuments : TrialArrangementsDocument[] = ccdClaim.trialReadyDocuments.filter(doc =>`[${doc.value.ownedBy}]` === CaseRole.CLAIMANT || `[${doc.value.ownedBy}]` === CaseRole.APPLICANTSOLICITORONE);
+      claimantTrialArrangementsDocuments.sort((a, b) => new Date(b.value.createdDatetime).getTime() - new Date(a.value.createdDatetime).getTime());
+      trialArrangements.trialArrangementsDocument = claimantTrialArrangementsDocuments[0];
+    } else {
+      const defendantTrialArrangementsDocuments : TrialArrangementsDocument[] = ccdClaim.trialReadyDocuments.filter(doc => `[${doc.value.ownedBy}]` === CaseRole.DEFENDANT);
+      defendantTrialArrangementsDocuments.sort((a, b) => new Date(b.value.createdDatetime).getTime() - new Date(a.value.createdDatetime).getTime());
+      trialArrangements.trialArrangementsDocument = defendantTrialArrangementsDocuments[0];
+      trialArrangements.isCaseReady = toCUIYesNo(ccdClaim?.trialReadyRespondent1);
+      trialArrangements.hasAnythingChanged = new HasAnythingChangedForm(toCUIYesNo(ccdClaim?.respondent1RevisedHearingRequirements?.revisedHearingRequirements), ccdClaim?.respondent1RevisedHearingRequirements?.revisedHearingComments);
+      trialArrangements.otherTrialInformation = ccdClaim?.respondent1HearingOtherComments?.hearingOtherComments;
+    }
+    return trialArrangements;
   }
 };
