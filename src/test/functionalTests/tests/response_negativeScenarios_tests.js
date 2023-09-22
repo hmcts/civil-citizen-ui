88ba595741bb966737f9d06b17e40dd6548e77c7
Annika <annika.moorthy@solirius.com>
2023-09-22 13:28:33 +0100

    error messages covered

---
+++ src/test/functionalTests/tests/response_negativeScenarios_tests.js
@@ -0,0 +1,41 @@
+const config = require('../../config');
+
+const ResponseSteps  =  require('../features/response/steps/lipDefendantResponseSteps');
+const LoginSteps =  require('../features/home/steps/login');
+const DashboardSteps = require('../features/dashboard/steps/dashboard');
+
+const admitAll = 'full-admission';
+const bySetDate = 'bySetDate';
+const repaymentPlan = 'repaymentPlan';
+const iHaveAlreadyAgreedMoretime = 'iHaveAlreadyAgreedMoretime';
+
+let claimRef;
+let caseData;
+let claimNumber;
+let securityCode;
+
+Feature('Negative Scenarios for Defendant Response');
+
+Before(async ({api}) => {
+  if (['preview', 'demo'  ].includes(config.runningEnv)) {
+    claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser);
+    console.log('claimRef has been created Successfully    <===>  '  , claimRef);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber = await caseData.legacyCaseReference;
+    securityCode = await caseData.respondent1PinToPostLRspec.accessCode;
+    console.log('claim number', claimNumber);
+    console.log('Security code', securityCode);
+    await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    // await DashboardSteps.VerifyClaimOnDashboard(claimNumber);
+  }else{
+    claimRef = await api.createSpecifiedClaimLRvLR(config.applicantSolicitorUser);
+    console.log('claimRef has been created Successfully    <===>  '  , claimRef);
+    await LoginSteps.EnterUserCredentials(config.defendantLRCitizenUser.email, config.defendantLRCitizenUser.password);
+  }
+});
+
+Scenario('View your options before response deadline error screen', async () => {
+  await ResponseSteps.RespondToClaim(claimRef);
+  await ResponseSteps.EnterPersonalDetails(claimRef);
+  await ResponseSteps.EnterYourOptionsForDeadlineError(claimRef, iHaveAlreadyAgreedMoretime);
+});
