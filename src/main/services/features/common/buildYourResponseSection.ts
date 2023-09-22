d37208e4c7415ba9e189af0025a0f502bfa9124a
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-09-22 15:15:59 +0200

    New: build your response claimantresponse

---
+++ src/main/services/features/common/buildYourResponseSection.ts
@@ -0,0 +1,76 @@
+import {YesNo, YesNoUpperCase} from "common/form/models/yesNo";
+import {Claim} from "common/models/claim";
+import {SummaryRow, summaryRow} from "common/models/summaryList/summaryList";
+import {SummarySection, summarySection} from "common/models/summaryList/summarySections";
+import {changeLabel} from "common/utils/checkYourAnswer/changeButton";
+import {getLng} from "common/utils/languageToggleUtils";
+import {constructResponseUrlWithIdParams} from "common/utils/urlFormatter";
+import {t} from "i18next";
+import {CLAIMANT_RESPONSE_SETTLE_CLAIM_URL} from "routes/urls";
+
+export const buildYourResponseSection = (claim: Claim, claimId: string, lang: string| unknown): SummarySection => {
+
+  const lng = getLng(lang);
+  const paidSection = summarySection({
+    title: t('PAGES.CHECK_YOUR_ANSWER.YOUR_RESPONSE', {lng}),
+    summaryRows: [],
+  });
+
+  // if (claim.claimantResponse?.hasPartPaymentBeenAccepted?.option) {
+  //   paidSection.summaryList.rows.push(getDoYouWantToSettlePaid(claim, claimId, lng));
+  // }
+
+  paidSection.summaryList.rows.push(getDoYouAgreeDefendantPaid(claim, claimId, lng));
+  paidSection.summaryList.rows.push(getDoYouWantToSettlePaid(claim, claimId, lng));
+  paidSection.summaryList.rows.push(getReasonForRejecting(claim, claimId, lng));
+
+  return paidSection;
+};
+
+export const getDoYouAgreeDefendantPaid = ( claim : Claim, claimId: string, lng: string): SummaryRow => {
+
+  const option = claim.claimantResponse.hasPartPaymentBeenAccepted.option === YesNo.YES
+  ? YesNoUpperCase.YES
+  : YesNoUpperCase.NO;
+
+  let paidAmount: number;
+  if (claim.isFullDefence()) {
+    paidAmount = claim.isRejectAllOfClaimAlreadyPaid();
+  } else if(claim.isPartialAdmissionPaid()){
+    paidAmount = claim.partialAdmissionPaidAmount();
+  }
+
+  return summaryRow(
+    t('PAGES.CHECK_YOUR_ANSWER.DO_YOU_AGREE_PAID', {lng, paidAmount}),
+    t(`COMMON.${option}`, {lng}),
+    constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_SETTLE_CLAIM_URL),
+    changeLabel(lng),
+  );
+};
+
+export const getDoYouWantToSettlePaid = ( claim : Claim, claimId: string, lng: string): SummaryRow => {
+
+  const option = claim.claimantResponse.hasPartPaymentBeenAccepted.option === YesNo.YES
+  ? YesNoUpperCase.YES
+  : YesNoUpperCase.NO;
+
+  const paidAmount = claim.isFullDefence() 
+    ? claim.isRejectAllOfClaimAlreadyPaid()
+    : claim.partialAdmissionPaidAmount();
+
+  return summaryRow(
+    t('PAGES.CHECK_YOUR_ANSWER.DO_YOU_WANT_TO_SETTLE_PAID', {lng, paidAmount}),
+    t(`COMMON.${option}`, {lng}),
+    constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_SETTLE_CLAIM_URL),
+    changeLabel(lng),
+  );
+};
+
+export const getReasonForRejecting = (claim : Claim, claimId: string, lng: string): SummaryRow => {
+  return summaryRow(
+    t('PAGES.CHECK_YOUR_ANSWER.REASON_FOR_REJECTING', {lng}),
+    claim.claimantResponse.rejectionReason.text,
+    constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_SETTLE_CLAIM_URL),
+    changeLabel(lng),
+  );
+};
