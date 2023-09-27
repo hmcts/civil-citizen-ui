9f3cc6129bd000062750d6ce2e97f2f1fcc76d0e
jeswanth <jeswanth.reddy@hmcts.net>
2023-09-27 13:57:01 +0100

    lint issues fix

--- src/main/services/features/claimantResponse/checkAnswers/checkAnswersPayBySetDateOrInstallments.ts
+++ src/main/services/features/claimantResponse/checkAnswers/checkAnswersPayBySetDateOrInstallments.ts
@@ -1,31 +1,30 @@
-import { Claim } from "common/models/claim";
-import { SummaryRow, summaryRow } from "common/models/summaryList/summaryList";
-import { changeLabel } from "common/utils/checkYourAnswer/changeButton";
-import { formatDateToFullDate } from "common/utils/dateUtils";
-import { getAmount, getFinalPaymentDate, getFirstRepaymentDate, getPaymentAmount, getPaymentDate, getRepaymentFrequency } from "common/utils/repaymentUtils";
-import { constructResponseUrlWithIdParams } from "common/utils/urlFormatter";
-import { t } from "i18next";
-import { CLAIMANT_RESPONSE_CHOOSE_HOW_TO_PROCEED_URL } from "routes/urls";
+import { Claim } from 'common/models/claim';
+import { SummaryRow, summaryRow } from 'common/models/summaryList/summaryList';
+import { changeLabel } from 'common/utils/checkYourAnswer/changeButton';
+import { formatDateToFullDate } from 'common/utils/dateUtils';
+import { getAmount, getFinalPaymentDate, getFirstRepaymentDate, getPaymentAmount, getPaymentDate, getRepaymentFrequency } from 'common/utils/repaymentUtils';
+import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
+import { t } from 'i18next';
+import { CLAIMANT_RESPONSE_CHOOSE_HOW_TO_PROCEED_URL } from 'routes/urls';
 
 export const buildSummaryForPayBySetDate = (claim: Claim, claimId: string, lang: string | unknown): SummaryRow[] => {
-    const paymentDate = formatDateToFullDate(getPaymentDate(claim));
-    const fullName = claim.getDefendantFullName();
-    const amount = getAmount(claim);
-    return [
-        summaryRow(t('PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA', { lang }), t('PAGES.CHECK_YOUR_ANSWER.WILL_PAY_BY_PAYMENT_DATE', { fullName, amount, paymentDate, lang }), constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_CHOOSE_HOW_TO_PROCEED_URL), changeLabel(lang as string)),
-        summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA', { lang }), `${paymentDate}`),
-    ];
+  const paymentDate = formatDateToFullDate(getPaymentDate(claim));
+  const fullName = claim.getDefendantFullName();
+  const amount = getAmount(claim);
+  return [
+    summaryRow(t('PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA', { lang }), t('PAGES.CHECK_YOUR_ANSWER.WILL_PAY_BY_PAYMENT_DATE', { fullName, amount, paymentDate, lang }), constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_CHOOSE_HOW_TO_PROCEED_URL), changeLabel(lang as string)),
+    summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA', { lang }), `${paymentDate}`),
+  ];
 };
 
 export const buildSummaryForPayByInstallments = (claim: Claim, claimId: string, lang: string | unknown): SummaryRow[] => {
-    const fullName = claim.getDefendantFullName();
-    const amount = getAmount(claim);
-    const instalmentAmount = getPaymentAmount(claim);
-    const frequency = t(`COMMON.PAYMENT_FREQUENCY.${getRepaymentFrequency(claim)}`, { lang })?.toLowerCase();
-    const instalmentDate = formatDateToFullDate(getFirstRepaymentDate(claim));
-    return [
-        summaryRow(t('PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA', { lang }), t('PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS', { lang, fullName, amount, instalmentAmount, instalmentDate, frequency }), constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_CHOOSE_HOW_TO_PROCEED_URL), changeLabel(lang as string)),
-        summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA', { lang }), `${formatDateToFullDate(getFinalPaymentDate(claim))}`),
-    ];
-
+  const fullName = claim.getDefendantFullName();
+  const amount = getAmount(claim);
+  const instalmentAmount = getPaymentAmount(claim);
+  const frequency = t(`COMMON.PAYMENT_FREQUENCY.${getRepaymentFrequency(claim)}`, { lang })?.toLowerCase();
+  const instalmentDate = formatDateToFullDate(getFirstRepaymentDate(claim));
+  return [
+    summaryRow(t('PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA', { lang }), t('PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS', { lang, fullName, amount, instalmentAmount, instalmentDate, frequency }), constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_CHOOSE_HOW_TO_PROCEED_URL), changeLabel(lang as string)),
+    summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA', { lang }), `${formatDateToFullDate(getFinalPaymentDate(claim))}`),
+  ];
 };
--- src/test/unit/services/features/claimantResponse/checkAnswersPayBySetDateOrInstallments.test.ts
+++ src/test/unit/services/features/claimantResponse/checkAnswersPayBySetDateOrInstallments.test.ts
@@ -1,65 +1,64 @@
-import { PaymentOptionType } from "common/form/models/admission/paymentOption/paymentOptionType";
-import { ResponseType } from "common/form/models/responseType";
-import { ChooseHowProceed } from "common/models/chooseHowProceed";
-import { Claim } from "common/models/claim";
-import { ClaimantResponse } from "common/models/claimantResponse";
-import { formatDateToFullDate } from "common/utils/dateUtils";
-import { getSummarySections } from "services/features/claimantResponse/checkAnswers/checkAnswersService";
-
+import { PaymentOptionType } from 'common/form/models/admission/paymentOption/paymentOptionType';
+import { ResponseType } from 'common/form/models/responseType';
+import { ChooseHowProceed } from 'common/models/chooseHowProceed';
+import { Claim } from 'common/models/claim';
+import { ClaimantResponse } from 'common/models/claimantResponse';
+import { formatDateToFullDate } from 'common/utils/dateUtils';
+import { getSummarySections } from 'services/features/claimantResponse/checkAnswers/checkAnswersService';
 
 describe('Check Answers Page', () => {
 
-    describe('Build check answers for pay by set date either for part admit or full admit ', () => {
-        let claim: Claim;
-        beforeEach(() => {
-            claim = new Claim();
+  describe('Build check answers for pay by set date either for part admit or full admit ', () => {
+    let claim: Claim;
+    beforeEach(() => {
+      claim = new Claim();
 
-        });
-
-        it('should show the check your answers for pay by set date for part admit', () => {
-            claim.partialAdmission = { paymentIntention: { paymentOption: PaymentOptionType.BY_SET_DATE, paymentDate: new Date() } };
-            claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
-            const expectedPaymentDate = formatDateToFullDate(new Date());
-            const result = getSummarySections('12345', claim, 'en');
-
-            expect(result.sections[0].summaryList.rows.length).toEqual(2);
-            expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_PAY_BY_PAYMENT_DATE' } });
-            expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
-        });
-
-        it('should show the check your answers for pay by set date for full admit', () => {
-            claim.fullAdmission = { paymentIntention: { paymentOption: PaymentOptionType.BY_SET_DATE, paymentDate: new Date() } };
-            claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
-            claim.respondent1 = { responseType: ResponseType.FULL_ADMISSION };
-            const expectedPaymentDate = formatDateToFullDate(new Date());
-            const result = getSummarySections('12345', claim, 'en');
-
-            expect(result.sections[0].summaryList.rows.length).toEqual(2);
-            expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_PAY_BY_PAYMENT_DATE' } });
-            expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
-        });
-
-        it('should show the check your answers for pay by installments for part admit', () => {
-            claim.partialAdmission = { paymentIntention: { paymentOption: PaymentOptionType.INSTALMENTS, paymentDate: new Date() } };
-            claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
-            const expectedPaymentDate = formatDateToFullDate(new Date());
-            const result = getSummarySections('12345', claim, 'en');
-
-            expect(result.sections[0].summaryList.rows.length).toEqual(2);
-            expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS' } });
-            expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
-        });
-
-        it('should show the check your answers for pay by installments for full admit', () => {
-            claim.fullAdmission = { paymentIntention: { paymentOption: PaymentOptionType.INSTALMENTS, paymentDate: new Date() } };
-            claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
-            claim.respondent1 = { responseType: ResponseType.FULL_ADMISSION };
-            const expectedPaymentDate = formatDateToFullDate(new Date());
-            const result = getSummarySections('12345', claim, 'en');
-
-            expect(result.sections[0].summaryList.rows.length).toEqual(2);
-            expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS' } });
-            expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
-        });
     });
-})
+
+    it('should show the check your answers for pay by set date for part admit', () => {
+      claim.partialAdmission = { paymentIntention: { paymentOption: PaymentOptionType.BY_SET_DATE, paymentDate: new Date() } };
+      claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
+      const expectedPaymentDate = formatDateToFullDate(new Date());
+      const result = getSummarySections('12345', claim, 'en');
+
+      expect(result.sections[0].summaryList.rows.length).toEqual(2);
+      expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_PAY_BY_PAYMENT_DATE' } });
+      expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
+    });
+
+    it('should show the check your answers for pay by set date for full admit', () => {
+      claim.fullAdmission = { paymentIntention: { paymentOption: PaymentOptionType.BY_SET_DATE, paymentDate: new Date() } };
+      claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
+      claim.respondent1 = { responseType: ResponseType.FULL_ADMISSION };
+      const expectedPaymentDate = formatDateToFullDate(new Date());
+      const result = getSummarySections('12345', claim, 'en');
+
+      expect(result.sections[0].summaryList.rows.length).toEqual(2);
+      expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_PAY_BY_PAYMENT_DATE' } });
+      expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
+    });
+
+    it('should show the check your answers for pay by installments for part admit', () => {
+      claim.partialAdmission = { paymentIntention: { paymentOption: PaymentOptionType.INSTALMENTS, paymentDate: new Date() } };
+      claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
+      const expectedPaymentDate = formatDateToFullDate(new Date());
+      const result = getSummarySections('12345', claim, 'en');
+
+      expect(result.sections[0].summaryList.rows.length).toEqual(2);
+      expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS' } });
+      expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
+    });
+
+    it('should show the check your answers for pay by installments for full admit', () => {
+      claim.fullAdmission = { paymentIntention: { paymentOption: PaymentOptionType.INSTALMENTS, paymentDate: new Date() } };
+      claim.claimantResponse = { chooseHowToProceed: { option: ChooseHowProceed.SIGN_A_SETTLEMENT_AGREEMENT } } as ClaimantResponse;
+      claim.respondent1 = { responseType: ResponseType.FULL_ADMISSION };
+      const expectedPaymentDate = formatDateToFullDate(new Date());
+      const result = getSummarySections('12345', claim, 'en');
+
+      expect(result.sections[0].summaryList.rows.length).toEqual(2);
+      expect(result.sections[0].summaryList.rows[0]).toEqual({ 'actions': { 'items': [{ 'href': '/case/12345/claimant-response/choose-how-to-proceed', 'text': 'COMMON.BUTTONS.CHANGE', 'visuallyHiddenText': ' PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }] }, 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.THE_AGREEMENT_CYA' }, 'value': { 'html': 'PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS' } });
+      expect(result.sections[0].summaryList.rows[1]).toEqual({ 'key': { 'text': 'PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA' }, 'value': { 'html': expectedPaymentDate } });
+    });
+  });
+});
