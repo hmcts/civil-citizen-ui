af83940d67128a64a80060ee3e393e2266aae28f
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-09-28 11:40:46 +0100

    remove redis call on the documentsUploadedController

--- src/test/unit/routes/features/caseProgression/documentsUploadedController.test.ts
+++ src/test/unit/routes/features/caseProgression/documentsUploadedController.test.ts
@@ -1,16 +1,10 @@
 import request from 'supertest';
-import {
-  mockCivilClaim,
-  mockRedisFailure,
-} from '../../../../utils/mockDraftStore';
 import {CP_EVIDENCE_UPLOAD_SUBMISSION_URL} from 'routes/urls';
-import {TestMessages} from '../../../../utils/errorMessageTestConstants';
 import {app} from '../../../../../main/app';
 import config from 'config';
 import nock from 'nock';
 
 jest.mock('../../../../../main/modules/oidc');
-jest.mock('../../../../../main/modules/draft-store');
 
 describe('Documents uploaded controller', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
@@ -23,20 +17,10 @@ describe('Documents uploaded controller', () => {
   });
 
   it('should render the page successfully', async () => {
-    app.locals.draftStoreClient = mockCivilClaim;
-    await request(app).get(CP_EVIDENCE_UPLOAD_SUBMISSION_URL).expect((res) => {
-      expect(res.status).toBe(200);
-      expect(res.text).toContain('Documents uploaded');
-    });
-  });
-
-  it('should return 500 error page for redis failure', async () => {
-    app.locals.draftStoreClient = mockRedisFailure;
-    await request(app)
-      .get(CP_EVIDENCE_UPLOAD_SUBMISSION_URL)
+    await request(app).get(CP_EVIDENCE_UPLOAD_SUBMISSION_URL.replace(':id', '1234123412341234'))
       .expect((res) => {
-        expect(res.status).toBe(500);
-        expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+        expect(res.status).toBe(200);
+        expect(res.text).toContain('Documents uploaded');
       });
   });
 
