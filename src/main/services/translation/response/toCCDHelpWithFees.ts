bad7aa946bd68f277098681e754245e98e19577f
neeta-hmcts <neeta.shinde@hmcts.net>
2023-09-28 08:08:14 +0100

    fixed test failuer.

--- src/main/services/translation/response/toCCDHelpWithFees.ts
+++ src/main/services/translation/response/toCCDHelpWithFees.ts
@@ -2,6 +2,7 @@ import {HelpWithFees} from 'form/models/claim/details/helpWithFees';
 import {CCDHelpWithFees} from 'form/models/claimDetails';
 
 export const toCCDHelpWithFees = (helpWithFees: HelpWithFees | undefined): CCDHelpWithFees => {
+  if (!helpWithFees) return undefined;
   return {
     helpWithFee: helpWithFees.option,
     helpWithFeesReferenceNumber: helpWithFees.referenceNumber,
