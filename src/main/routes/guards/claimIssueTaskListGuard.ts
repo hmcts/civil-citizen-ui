fe3847ecbea08aed51a25854c22217b99b35d50c
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-10-02 10:41:23 +0200

    CIV-10829 : Remove unused code

--- src/main/routes/guards/claimIssueTaskListGuard.ts
+++ src/main/routes/guards/claimIssueTaskListGuard.ts
@@ -2,14 +2,14 @@ import {NextFunction, Request, Response, RequestHandler} from 'express';
 import {Claim} from 'common/models/claim';
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {AppRequest} from 'models/AppRequest';
-import {BASE_ELIGIBILITY_URL, CLAIM_CONFIRMATION_URL} from 'routes/urls';
+import {BASE_ELIGIBILITY_URL} from 'routes/urls';
 
 export const claimIssueTaskListGuard = (async (req: Request, res: Response, next: NextFunction) => {
   try {
     const appReq: AppRequest = <AppRequest>req;
     const userId = appReq.session?.user?.id;
     const caseData: Claim = await getCaseDataFromStore(userId);
-    if (!caseData?.isDraftClaim() && !req.cookies['eligibilityCompleted'] && !req.originalUrl.includes(CLAIM_CONFIRMATION_URL)) {
+    if (!caseData?.isDraftClaim() && !req.cookies['eligibilityCompleted']) {
       res.redirect(BASE_ELIGIBILITY_URL);
     } else {
       next();
