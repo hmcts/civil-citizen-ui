61387c66b850aaea21d723c0cbb439e066973a49
Raja <raja.mani@hmcts.net>
2023-10-03 10:52:15 +0100

    updated the CP Functional test

--- src/test/functionalTests/tests/caseprogression_uploadevidence_tests.js
+++ src/test/functionalTests/tests/caseprogression_uploadevidence_tests.js
@@ -7,17 +7,14 @@ let claimRef;
 Feature('Case progression journey - Defendant & Claimant Response with RejectAll');
 
 Before(async ({api}) => {
+  //Once the CUI Release is done, we can remove this IF statement, so that tests will run on AAT as well.
   if (['preview', 'demo'].includes(config.runningEnv)) {
     claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser, '', 'FastTrack');
     await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
-  } else {
-    claimRef = await api.createSpecifiedClaimLRvLR(config.applicantSolicitorUser, '', 'FastTrack');
-    await LoginSteps.EnterUserCredentials(config.defendantLRCitizenUser.email, config.defendantLRCitizenUser.password);
+    await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL');
+    await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef);
+    await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef);
   }
-  await api.performCitizenResponse(config.defendantCitizenUser, claimRef, 'FastTrack');
-  await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL');
-  await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef);
-  await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef);
 });
 
 Scenario('Response with RejectAll and DisputeAll For the Case Progression and Hearing Scheduled Process To Complete', () => {
