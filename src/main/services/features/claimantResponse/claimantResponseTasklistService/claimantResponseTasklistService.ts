dcd147853ff6b9522b3c9f563ecad45446066e78
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-10-04 12:54:49 +0200

    CIV-10497 : Navigation fix for FA

--- src/main/services/features/claimantResponse/claimantResponseTasklistService/claimantResponseTasklistService.ts
+++ src/main/services/features/claimantResponse/claimantResponseTasklistService/claimantResponseTasklistService.ts
@@ -13,7 +13,7 @@ export function getClaimantResponseTaskLists (claim: Claim, claimId: string, lng
   const lang = getLng(lng);
   const taskGroups : TaskList[] = [];
   taskGroups.push(buildHowDefendantRespondSection(claim, claimId, lang));
-  if(claim.isPartialAdmissionNotPaid()) {
+  if(claim.isPartialAdmissionNotPaid() || claim.isFullAdmission()) {
     taskGroups.push(buildWhatToDoNextSection(claim, claimId, lang));
   }
   if(claim.isPartialAdmissionPaid() || claim.isFullDefence()){
