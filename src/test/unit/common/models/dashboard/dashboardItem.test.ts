98bf367ef7dab18d07dc9ad750c9b7b1a78e999b
Manish Garg <manish.garg@hmcts.net>
2023-10-06 12:16:01 +0100

    CIV-8904 Claimant Dashboard Status

--- src/test/unit/common/models/dashboard/dashboardItem.test.ts
+++ src/test/unit/common/models/dashboard/dashboardItem.test.ts
@@ -37,12 +37,14 @@ describe('Dashboard Items', ()=> {
       //Then
       expect(href).toEqual(ocmcBaseUrl + '/dashboard/1/claimant');
     });
+
     it('should return correct url for ccd claims', ()=> {
       //When
       const href = ccdClaimantClaim.getHref();
       //Then
       expect(href).toEqual( '/dashboard/1/claimant');
     });
+
     it('should translate claim to dashboard item when claim is not empty', () => {
       //Given
       const claim = new Claim();
@@ -52,6 +54,7 @@ describe('Dashboard Items', ()=> {
       //Then
       expect(item).not.toBeUndefined();
     });
+
     it('should return undefined when claim is empty', () => {
       //Given
       const claim = new Claim();
@@ -60,13 +63,33 @@ describe('Dashboard Items', ()=> {
       //Then
       expect(item).toBeUndefined();
     });
+
     it('should return undefined when claim is undefined', () => {
       //When
       const item = toDraftClaimDashboardItem(undefined);
       //Then
       expect(item).toBeUndefined();
     });
+
+    it('should return empty status when there is no status', () => {
+      //Given
+      ccdClaimantClaim.status = 'CHANGE_BY_CLAIMANT';
+      //When
+      const status = ccdClaimantClaim.getStatus('en');
+      //Then
+      expect(status).toBe('');
+    });
+
+    it('should return matched status value when status exists', () => {
+      //Given
+      ccdClaimantClaim.status = 'NO_RESPONSE';
+      //When
+      const status = ccdClaimantClaim.getStatus('en');
+      //Then
+      expect(status).toContain('PAGES.DASHBOARD.STATUS_CLAIMANT.NO_RESPONSE_ON_TIME');
+    });
   });
+
   describe('Dashboard defendant item', ()=>{
     const ocmcDefendantClaim = new DashboardDefendantItem();
     ocmcDefendantClaim.claimId = '1';
@@ -81,12 +104,14 @@ describe('Dashboard Items', ()=> {
       //Then
       expect(href).toEqual(ocmcBaseUrl + '/dashboard/1/defendant');
     });
+
     it('should return correct url for ccd claims', ()=> {
       //When
       const href = ccdDefendantClaim.getHref();
       //Then
       expect(href).toEqual( '/dashboard/1/defendant');
     });
+
     it('should return translated status for claim', () => {
       //Given
       const dashboardClaim = new DashboardDefendantItem();
@@ -95,12 +120,13 @@ describe('Dashboard Items', ()=> {
       //When
       const status = dashboardClaim.getStatus('en');
       //Then
-      expect(status).toBe('PAGES.DASHBOARD.STATUS.NO_RESPONSE_ON_TIME');
+      expect(status).toBe('PAGES.DASHBOARD.STATUS_DEFENDANT.NO_RESPONSE_ON_TIME');
     });
+
     it('should return the translated string without parameters when params is provided but empty', () => {
       // Given
-      const translationKey = 'PAGES.DASHBOARD.STATUS.CLAIMANT_CONFIRMED_PAYMENT';
-      const expectedTranslation = 'PAGES.DASHBOARD.STATUS.CLAIMANT_CONFIRMED_PAYMENT';
+      const translationKey = 'PAGES.DASHBOARD.STATUS_DEFENDANT.CLAIMANT_CONFIRMED_PAYMENT';
+      const expectedTranslation = 'PAGES.DASHBOARD.STATUS_DEFENDANT.CLAIMANT_CONFIRMED_PAYMENT';
       const params: DashboardStatusTranslationParam[] = [];
       const lang = 'cy';
 
@@ -111,6 +137,7 @@ describe('Dashboard Items', ()=> {
       expect(result).toBe(expectedTranslation);
     });
   });
+
   describe('Dashboard claimant item', ()=>{
     const ocmcDefendantClaim = new DashboardClaimantItem();
     ocmcDefendantClaim.claimId = '1';
@@ -119,18 +146,21 @@ describe('Dashboard Items', ()=> {
     const ccdDefendantClaim = new DashboardClaimantItem();
     ccdDefendantClaim.claimId = '1';
     ccdDefendantClaim.ocmc = false;
+
     it('should return correct url for ocmc claim', ()=> {
       //When
       const href = ocmcDefendantClaim.getHref();
       //Then
       expect(href).toEqual(ocmcBaseUrl + '/dashboard/1/claimant');
     });
+
     it('should return correct url for ccd claims', ()=> {
       //When
       const href = ccdDefendantClaim.getHref();
       //Then
       expect(href).toEqual( '/dashboard/1/claimant');
     });
+
     it('should return translated status for claim', () => {
       //Given
       const dashboardClaim = new DashboardClaimantItem();
@@ -138,8 +168,9 @@ describe('Dashboard Items', ()=> {
       //When
       const status = dashboardClaim.getStatus('en');
       //Then
-      expect(status).toBe('PAGES.DASHBOARD.STATUS.NO_RESPONSE_ON_TIME');
+      expect(status).toBe('PAGES.DASHBOARD.STATUS_CLAIMANT.NO_RESPONSE_ON_TIME');
     });
+
     it('should return the translated string without parameters when params is provided but empty', () => {
       // Given
       const translationKey = 'PAGES.DASHBOARD.STATUS.CLAIMANT_CONFIRMED_PAYMENT';
