ad4a9c797b487dccfed19115689e73baba69a117
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2023-10-09 13:11:28 +0100

    CIV-10370 add coverage

--- src/test/unit/routes/features/caseProgression/documentsUploadedController.test.ts
+++ src/test/unit/routes/features/caseProgression/documentsUploadedController.test.ts
@@ -3,6 +3,9 @@ import {CP_EVIDENCE_UPLOAD_SUBMISSION_URL} from 'routes/urls';
 import {app} from '../../../../../main/app';
 import config from 'config';
 import nock from 'nock';
+import {Claim} from 'models/claim';
+import {CaseProgression} from 'models/caseProgression/caseProgression';
+import {mockCivilClaim, mockRedisFailure} from '../../../../utils/mockDraftStore';
 
 jest.mock('../../../../../main/modules/oidc');
 
@@ -21,6 +24,33 @@ describe('Documents uploaded controller', () => {
       .expect((res) => {
         expect(res.status).toBe(200);
         expect(res.text).toContain('Documents uploaded');
+      });
+  });
+
+  it('should render the page successfully on claimant request', async () => {
+
+    app.locals.draftStoreClient = mockCivilClaim;
+    const mockClaimId = '1645882162449409';
+    const caseData = new Claim();
+    caseData.caseProgression = new CaseProgression();
+
+    await request(app).get(CP_EVIDENCE_UPLOAD_SUBMISSION_URL.replace(':id', mockClaimId))
+      .expect((res) => {
+        expect(res.status).toBe(200);
+        expect(res.text).toContain('Documents uploaded');
+      });
+  });
+
+  it('should return 500 error when there is a Redis failure', async () => {
+
+    app.locals.draftStoreClient = mockRedisFailure;
+    const mockClaimId = '1645882162449409';
+    const caseData = new Claim();
+    caseData.caseProgression = new CaseProgression();
+
+    await request(app).get(CP_EVIDENCE_UPLOAD_SUBMISSION_URL.replace(':id', mockClaimId))
+      .expect((res) => {
+        expect(res.status).toBe(500);
       });
   });
 
