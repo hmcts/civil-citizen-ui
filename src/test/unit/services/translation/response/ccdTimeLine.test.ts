5cdc023411845a5d35060ce225713e5dbb4d0f2e
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-10-10 13:53:24 +0100

    CIV-10903 claim fee and timeline added

--- src/test/unit/services/translation/response/ccdTimeLine.test.ts
+++ src/test/unit/services/translation/response/ccdTimeLine.test.ts
@@ -1,16 +1,16 @@
 import {Claim} from 'models/claim';
 import {toCCDTimeline} from 'services/translation/response/convertToCCDTimeLine';
-import {CCDTimeLineOfEvents} from 'models/ccdResponse/ccdTimeLineOfEvents';
 import {TimelineRow} from 'form/models/timeLineOfEvents/timelineRow';
 import {ClaimDetails} from 'form/models/claim/details/claimDetails';
 import {ClaimantTimeline} from 'form/models/timeLineOfEvents/claimantTimeline';
+import {toCCDTimelineEvent} from 'models/ccdResponse/ccdTimeLine';
 
 describe('translate TimeLine to CCD model', () => {
 
   it('should return undefined if timeline doesnt exist', () => {
     const claimEmpty = new Claim();
     claimEmpty.claimDetails = new ClaimDetails();
-    const timelineResponseCCD = toCCDTimeline(claimEmpty.claimDetails.timeline);
+    const timelineResponseCCD = toCCDTimelineEvent(claimEmpty.claimDetails.timeline);
     expect(timelineResponseCCD).toBe(undefined);
   });
   it('should translate timeline to CCD', () => {
@@ -22,17 +22,7 @@ describe('translate TimeLine to CCD model', () => {
         new TimelineRow(2, 2, 2002, 'test 2'),
       ],
     );
-    const claimAmountCCD: CCDTimeLineOfEvents[] = [
-      {
-        id: '0',
-        value: {timelineDate: new Date('01-01-2001'), timelineDescription: 'test 1'},
-      },
-      {
-        id: '1',
-        value: {timelineDate: new Date('02-02-2002'), timelineDescription: 'test 2'},
-      },
-    ];
-    const timelineResponseCCD = toCCDTimeline(claim.claimDetails.timeline);
-    expect(timelineResponseCCD).toMatchObject(claimAmountCCD);
+    const timelineResponseCCD = toCCDTimeline(claim.claimDetails.timeline)
+    expect(timelineResponseCCD).toHaveLength(2);
   });
 });
