c7548022d1eea887772fc7e00c294767531c16d0
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-10-10 10:15:29 +0100

    fix pipeline

--- src/test/functionalTests/features/caseProgression/pages/uploadEvidence/uploadYourDocument.js
+++ src/test/functionalTests/features/caseProgression/pages/uploadEvidence/uploadYourDocument.js
@@ -296,7 +296,7 @@ class UploadYourDocument {
     I.fillField('witnessStatement[0][dateInputFields][dateDay]', '01');
     I.fillField('witnessStatement[0][dateInputFields][dateMonth]', '02');
     I.fillField('witnessStatement[0][dateInputFields][dateYear]', '2023');
-    I.attachFile('witnessStatement[0][fileUpload]', 'features/caseProgression/data/TestBMP.bmp');
+    I.attachFile('witnessStatement[0][fileUpload]', 'src/test/functionalTests/features/caseProgression/data/TestBMP.bmp');
     I.click('[method=\'post\'] > .govuk-grid-column-two-thirds > div:nth-of-type(1) #add-another-witness-list');
     I.fillField('witnessStatement[1][witnessName]', 'Witness Statement - Witness Nae 2');
     I.fillField('witnessStatement[1][dateInputFields][dateDay]', '02');
