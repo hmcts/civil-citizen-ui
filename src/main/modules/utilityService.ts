ecbae5356efb71ed3c827c3e00498b76835b1e2d
laperla11 <kegilmez@gmail.com>
2023-10-11 14:38:03 +0100

    CIV-10977 generate redis key with caseid+userId

--- src/main/modules/utilityService.ts
+++ src/main/modules/utilityService.ts
@@ -1,6 +1,6 @@
 import {AppRequest} from 'models/AppRequest';
 import config from 'config';
-import {getCaseDataFromStore, saveDraftClaim} from '../modules/draft-store/draftStoreService';
+import {generateRedisKey, getCaseDataFromStore, saveDraftClaim} from '../modules/draft-store/draftStoreService';
 import {CivilServiceClient} from '../app/client/civilServiceClient';
 import {Claim} from '../../main/common/models/claim';
 import {Request} from 'express';
@@ -17,22 +17,23 @@ const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServi
  * @param claimId, req
  * @returns claim
  */
-export const getClaimById = async (claimId: string, req: Request): Promise<Claim> => {
-  let claim: Claim = await getCaseDataFromStore(claimId);
-  if(claim.isEmpty()) {
+export const getClaimById = async (claimId: string, req: Request, useRedisKey: boolean = false): Promise<Claim> => {
+  const redisKey = useRedisKey ? generateRedisKey(<AppRequest>req) : claimId;
+  let claim: Claim = await getCaseDataFromStore(redisKey, true);
+  if (claim.isEmpty()) {
     claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
     if(claim) {
-      await saveDraftClaim(claimId, claim);
-    } else {
-      claim = new Claim();
-      claim.legacyCaseReference = 'testCaseReference';
-      claim.totalClaimAmount = 200;
-      claim.claimDetails = new ClaimDetails(new Reason('reasontext'));
-      claim.totalInterest = 15;
-      claim.claimFee = {calculatedAmountInPence: '3500'};
+      await saveDraftClaim(redisKey, claim, true);
+      } else {
+        claim = new Claim();
+        claim.legacyCaseReference = 'testCaseReference';
+        claim.totalClaimAmount = 200;
+        claim.claimDetails = new ClaimDetails(new Reason('reasontext'));
+        claim.totalInterest = 15;
+        claim.claimFee = {calculatedAmountInPence: '3500'};
+      }
     }
-  }
-  return claim;
+    return claim;
 };
 
 export const getRedisStoreForSession = () => {
--- src/main/routes/features/directionsQuestionnaire/experts/expertSmallClaimsController.ts
+++ src/main/routes/features/directionsQuestionnaire/experts/expertSmallClaimsController.ts
@@ -6,6 +6,8 @@ import {
 } from '../../../urls';
 import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
 import {saveExpertRequiredValue} from 'services/features/directionsQuestionnaire/expertRequiredValueService';
+import {generateRedisKey} from 'modules/draft-store/draftStoreService';
+import {AppRequest} from 'common/models/AppRequest';
 
 const expertSmallClaimsController = Router();
 const expertSmallClaimsViewPath = 'features/directionsQuestionnaire/experts/expert-small-claims';
@@ -19,7 +21,7 @@ expertSmallClaimsController.post(DQ_EXPERT_SMALL_CLAIMS_URL, async (req: Request
     const claimId = req.params.id;
     const expertRequiredValue = req.body.expertYes ? true : false;
     const redirectUrl = req.body.expertYes ? DQ_EXPERT_REPORT_DETAILS_URL : DQ_GIVE_EVIDENCE_YOURSELF_URL;
-    await saveExpertRequiredValue(claimId, expertRequiredValue);
+    await saveExpertRequiredValue(generateRedisKey(<AppRequest>req), expertRequiredValue);
     res.redirect(constructResponseUrlWithIdParams(claimId, redirectUrl));
   } catch (error) {
     next(error);
