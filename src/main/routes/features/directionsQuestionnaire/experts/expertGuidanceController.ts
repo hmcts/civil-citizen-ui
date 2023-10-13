8d2b8204e9e7da65c451e1103089b346b15ff684
laperla11 <kegilmez@gmail.com>
2023-10-13 17:05:46 +0100

    CIV-10977 fixed RequestHandler type

--- src/main/routes/features/directionsQuestionnaire/experts/expertGuidanceController.ts
+++ src/main/routes/features/directionsQuestionnaire/experts/expertGuidanceController.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import {RequestHandler, Router} from 'express';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {EXPERT_GUIDANCE_URL, PERMISSION_FOR_EXPERT_URL} from 'routes/urls';
 import {getDirectionQuestionnaire} from 'services/features/directionsQuestionnaire/directionQuestionnaireService';
@@ -7,17 +7,17 @@ import {AppRequest} from 'common/models/AppRequest';
 
 const expertGuidanceController = Router();
 
-expertGuidanceController.get(EXPERT_GUIDANCE_URL, async (req, res, next) => {
+expertGuidanceController.get(EXPERT_GUIDANCE_URL, (async (req, res, next) => {
   try {
     await getDirectionQuestionnaire(generateRedisKey(<AppRequest>req));
     res.render('features/directionsQuestionnaire/experts/expert-guidance');
   } catch (error) {
     next(error);
   }
-});
+}) as RequestHandler);
 
-expertGuidanceController.post(EXPERT_GUIDANCE_URL, (req, res) => {
+expertGuidanceController.post(EXPERT_GUIDANCE_URL, ((req, res) => {
   res.redirect(constructResponseUrlWithIdParams(req.params.id, PERMISSION_FOR_EXPERT_URL));
-});
+}) as RequestHandler);
 
 export default expertGuidanceController;
