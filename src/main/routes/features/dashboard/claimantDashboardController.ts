d492a29e97e30b1aaa9f04aeeafa07c6f28b2d5d
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-10-13 14:05:43 +0200

    Fix: sonar

--- src/main/routes/features/dashboard/claimantDashboardController.ts
+++ src/main/routes/features/dashboard/claimantDashboardController.ts
@@ -1,4 +1,4 @@
-import {NextFunction, Request, Response, Router} from 'express';
+import {NextFunction, Request, RequestHandler, Response, Router} from 'express';
 import config from 'config';
 import {DASHBOARD_CLAIMANT_URL} from '../../urls';
 import {CivilServiceClient} from 'client/civilServiceClient';
@@ -11,7 +11,7 @@ const claimantDashboardController = Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
-claimantDashboardController.get(DASHBOARD_CLAIMANT_URL, async (req: Request, res: Response, next: NextFunction) => {
+claimantDashboardController.get(DASHBOARD_CLAIMANT_URL, (async (req: Request, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
@@ -22,6 +22,6 @@ claimantDashboardController.get(DASHBOARD_CLAIMANT_URL, async (req: Request, res
   } catch (error) {
     next(error);
   }
-});
+}) as RequestHandler);
 
 export default claimantDashboardController;
