3de4ac48d6edc5c3d55890ed4dd02bc2f62b7421
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-10-13 13:28:53 +0200

    Fix: ignore sonar

--- src/main/routes/features/dashboard/claimantDashboardController.ts
+++ src/main/routes/features/dashboard/claimantDashboardController.ts
@@ -12,18 +12,16 @@ const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
 claimantDashboardController.get(DASHBOARD_CLAIMANT_URL, async (req: Request, res: Response, next: NextFunction) => {
-  (async () => {
-    try {
-      const claimId = req.params.id;
-      const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-      const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
-      const dashboardNotifications = getClaimantNotifications(claim, lang);
-      const dashboardTaskList = getDashboardTaskList(claim, lang);
-      res.render(claimantDashboardViewPath, {claim, claimId, dashboardTaskList, dashboardNotifications});
-    } catch (error) {
-      next(error);
-    }
-  })();
+  try {
+    const claimId = req.params.id;
+    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+    const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
+    const dashboardNotifications = getClaimantNotifications(claim, lang);
+    const dashboardTaskList = getDashboardTaskList(claim, lang);
+    res.render(claimantDashboardViewPath, {claim, claimId, dashboardTaskList, dashboardNotifications});
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default claimantDashboardController;
