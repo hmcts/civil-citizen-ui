1876e25ac7cfa1399a54454a4e3c6357f81ea0b3
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-10-13 12:21:12 +0200

    Fix: sonar bug

--- src/main/routes/features/dashboard/claimantDashboardController.ts
+++ src/main/routes/features/dashboard/claimantDashboardController.ts
@@ -12,16 +12,18 @@ const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
 claimantDashboardController.get(DASHBOARD_CLAIMANT_URL, async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const claimId = req.params.id;
-    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-    const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
-    const dashboardNotifications = getClaimantNotifications(claim, lang);
-    const dashboardTaskList = getDashboardTaskList(claim, lang);
-    return res.render(claimantDashboardViewPath, {claim, claimId, dashboardTaskList, dashboardNotifications});
-  } catch (error) {
-    next(error);
-  }
+  (async () => {
+    try {
+      const claimId = req.params.id;
+      const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+      const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
+      const dashboardNotifications = getClaimantNotifications(claim, lang);
+      const dashboardTaskList = getDashboardTaskList(claim, lang);
+      res.render(claimantDashboardViewPath, {claim, claimId, dashboardTaskList, dashboardNotifications});
+    } catch (error) {
+      next(error);
+    }
+  })();
 });
 
 export default claimantDashboardController;
