6813a53812601d6a90553ecd8a24d1c0275c7b1a
GorilaEspacial <woodworkerdeveloper@gmail.com>
2023-10-19 15:57:57 +0200

    Fix: sign settlment agreement content

--- src/main/routes/features/claimantResponse/signSettlmentAgreementController.ts
+++ src/main/routes/features/claimantResponse/signSettlmentAgreementController.ts
@@ -9,6 +9,7 @@ import {getClaimantResponse, saveClaimantResponse} from 'services/features/claim
 import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {SignSettlmentAgreement} from 'common/form/models/claimantResponse/signSettlementAgreement';
 import {
+  convertFrequencyToText,
   getAmount,
   getFinalPaymentDate,
   getFirstRepaymentDate,
@@ -18,6 +19,7 @@ import {
 import {SignSettlmentAgreementGuard} from 'routes/guards/signSettlmentAgreementGuard';
 import {formatDateToFullDate} from 'common/utils/dateUtils';
 import {Claim} from 'common/models/claim';
+import {t} from 'i18next';
 
 const signSettlementAgreementViewPath = 'features/claimantResponse/sign-settlement-agreement';
 const signSettlementAgreementController = Router();
@@ -28,25 +30,39 @@ function renderView(form: GenericForm<SignSettlmentAgreement>, res: Response, da
   res.render(signSettlementAgreementViewPath, {form, data});
 }
 
-const getRepaymentPlan = (claim: Claim, req: Request) => {
+const getPaymentText = (claim: Claim, req: Request): object => {
   const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-  const data = {
-    amount: getAmount(claim),
-    defendant: claim.getDefendantFullName(),
-    firstRepaymentDate: formatDateToFullDate(getFirstRepaymentDate(claim), lang),
-    finalRepaymentDate: formatDateToFullDate(getFinalPaymentDate(claim), lang),
-    paymentAmount: getPaymentAmount(claim),
-    repaymentFrequency: getRepaymentFrequency(claim),
-  };
 
-  return data;
+  if (claim.isPAPaymentOptionByDate() || claim.isFAPaymentOptionBySetDate()) {
+    return {
+      paymentText: t('PAGES.CHECK_YOUR_ANSWER.WILL_PAY_BY_PAYMENT_DATE', {
+        lang: lang,
+        fullName: claim.getDefendantFullName(),
+        amount: getAmount(claim),
+        paymentDate: formatDateToFullDate(claim.getPaymentDate()),
+      }),
+      completionDate: t('PAGES.CLAIMANT_TERMS_OF_AGREEMENT.DETAILS.COMPLETION_DATE.DATE', { finalRepaymentDate: formatDateToFullDate(claim.getPaymentDate()) }),
+    };
+  } else if (claim.isFAPaymentOptionInstallments() || claim.isPAPaymentOptionInstallments()){
+    return {
+      paymentText: t('PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS', {
+        lang: lang,
+        fullName: claim.getDefendantFullName(),
+        amount: getAmount(claim),
+        instalmentAmount: getPaymentAmount(claim),
+        instalmentDate: formatDateToFullDate(getFirstRepaymentDate(claim), lang),
+        frequency: convertFrequencyToText(getRepaymentFrequency(claim), lang).toLowerCase(),
+      }),
+      completionDate: t('PAGES.CLAIMANT_TERMS_OF_AGREEMENT.DETAILS.COMPLETION_DATE.DATE', { finalRepaymentDate: formatDateToFullDate(getFinalPaymentDate(claim), lang) }),
+    };
+  }
 };
 
 signSettlementAgreementController.get(CLAIMANT_SIGN_SETTLEMENT_AGREEMENT, SignSettlmentAgreementGuard.apply(CLAIMANT_RESPONSE_TASK_LIST_URL), async (req: Request, res: Response, next: NextFunction) => {
   try {
     const claim = await getCaseDataFromStore(req.params.id);
     const claimantResponse = await getClaimantResponse(req.params.id);
-    renderView(new GenericForm(claimantResponse.signSettlementAgreement), res, getRepaymentPlan(claim, req));
+    renderView(new GenericForm(claimantResponse.signSettlementAgreement), res, getPaymentText(claim, req));
   } catch (error) {
     next(error);
   }
@@ -60,7 +76,7 @@ signSettlementAgreementController.post(CLAIMANT_SIGN_SETTLEMENT_AGREEMENT, async
 
     if (signSettlementAgreement.hasErrors()) {
       const claim = await getCaseDataFromStore(req.params.id);
-      renderView(signSettlementAgreement, res, getRepaymentPlan(claim, req));
+      renderView(signSettlementAgreement, res, getPaymentText(claim, req));
     } else {
       await saveClaimantResponse(claimId, signSettlementAgreement.model.signed, crPropertyName, crParentName);
       res.redirect(constructResponseUrlWithIdParams(claimId, CLAIMANT_RESPONSE_TASK_LIST_URL));
