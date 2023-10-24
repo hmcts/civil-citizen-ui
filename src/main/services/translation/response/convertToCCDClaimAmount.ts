5036f1f29ccc8b45ca66e4fce9e878df21a27721
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2023-10-24 16:12:28 +0100

    CIV-10458 Fix lint

--- src/main/services/translation/response/convertToCCDClaimAmount.ts
+++ src/main/services/translation/response/convertToCCDClaimAmount.ts
@@ -1,6 +1,6 @@
 import {ClaimAmountBreakup} from '../../../common/form/models/claimDetails';
 import {CCDClaimAmountBreakup} from '../../../common/models/ccdResponse/ccdClaimAmountBreakup';
-import {convertToPenceFromString} from "services/translation/claim/moneyConversation";
+import {convertToPenceFromString} from 'services/translation/claim/moneyConversation';
 
 export const toCCDClaimAmount = (claimAmountBreakup: ClaimAmountBreakup[]): CCDClaimAmountBreakup[] => {
   if (!claimAmountBreakup) return undefined;
