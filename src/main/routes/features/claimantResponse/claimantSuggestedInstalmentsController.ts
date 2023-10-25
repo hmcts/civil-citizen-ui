b7fea4fbe64fe6879dd631a112d0b734bf027416
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2023-10-25 16:28:33 +0100

    CIV-11238 changes

--- src/main/routes/features/claimantResponse/claimantSuggestedInstalmentsController.ts
+++ src/main/routes/features/claimantResponse/claimantSuggestedInstalmentsController.ts
@@ -6,27 +6,33 @@ import {
   CLAIMANT_RESPONSE_PAYMENT_PLAN_URL,
   CLAIMANT_RESPONSE_TASK_LIST_URL,
 } from 'routes/urls';
-import {saveClaimantResponse} from 'services/features/claimantResponse/claimantResponseService';
+import {getFinancialDetails, saveClaimantResponse} from 'services/features/claimantResponse/claimantResponseService';
 import {
   getClaimantSuggestedInstalmentsForm,
   getClaimantSuggestedInstalmentsPlan,
 } from 'services/features/claimantResponse/claimantSuggestedInstalmentsService';
 import { generateRedisKey } from 'modules/draft-store/draftStoreService';
 import { AppRequest } from 'common/models/AppRequest';
+import {Claim} from 'models/claim';
+import {getClaimById} from 'modules/utilityService';
 
 const claimantSuggestedInstalmentsViewPath = 'features/claimantResponse/instalments-plan';
 const claimantSuggestedInstalmentsController = Router();
 const crParentName = 'suggestedPaymentIntention';
 const crPropertyName = 'repaymentPlan';
 
-function renderView(form: GenericForm<RepaymentPlanForm>, res: Response): void {
-  res.render(claimantSuggestedInstalmentsViewPath, {form});
+function renderView(form: GenericForm<RepaymentPlanForm>, res: Response, req: Request, claim:Claim): void {
+  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+  const financialDetails = getFinancialDetails(claim, lang);
+  res.render(claimantSuggestedInstalmentsViewPath, {form, claim, financialDetails});
 }
 
 claimantSuggestedInstalmentsController.get(CLAIMANT_RESPONSE_PAYMENT_PLAN_URL,  async (req, res, next: NextFunction) => {
   try {
     const claimantSuggestedInstalmentsPlan = await getClaimantSuggestedInstalmentsPlan(generateRedisKey(req as unknown as AppRequest));
-    renderView(new GenericForm(claimantSuggestedInstalmentsPlan), res);
+    const claimId = req.params.id;
+    const claim: Claim = await getClaimById(claimId, req, true);
+    renderView(new GenericForm(claimantSuggestedInstalmentsPlan), res, req, claim);
   } catch (error) {
     next(error);
   }
@@ -39,7 +45,9 @@ claimantSuggestedInstalmentsController.post(CLAIMANT_RESPONSE_PAYMENT_PLAN_URL,
       const form = new GenericForm(claimantSuggestedInstalments);
       form.validateSync();
       if (form.hasErrors()) {
-        renderView(form, res);
+        const claimId = req.params.id;
+        const claim: Claim = await getClaimById(claimId, req, true);
+        renderView(form, res, req, claim);
       } else {
         // TODO : trigger court calculator when it's developed and update redirection url with the result of it
         await saveClaimantResponse(generateRedisKey(req as unknown as AppRequest), form.model, crPropertyName, crParentName);
