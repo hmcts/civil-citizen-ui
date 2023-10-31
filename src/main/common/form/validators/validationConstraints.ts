5a13827970804955c6445e81c227955f5ada9180
jeswanth <jeswanth.reddy@hmcts.net>
2023-10-31 15:31:43 +0000

    code for Limit number of characters entered on the text boxes are not working as expected

--- src/main/common/form/validators/validationConstraints.ts
+++ src/main/common/form/validators/validationConstraints.ts
@@ -3,6 +3,7 @@ export const FREE_TEXT_MAX_LENGTH = 99000;
 export const FREE_TEXT_1000_MAX_LENGTH = 1000;
 export const MIN_AMOUNT_VALUE = 0.01;
 export const SIGNER_NAME_MAX_LENGTH = 70;
-export const SIGNER_ROLE_MAX_LENGTH = 255;
+export const SIGNER_ROLE_MAX_LENGTH = 70;
 export const MAX_CLAIM_AMOUNT_TOTAL = 25000;
+export const ADDRESS_LINE_MAX_LENGTH = 70;
 
