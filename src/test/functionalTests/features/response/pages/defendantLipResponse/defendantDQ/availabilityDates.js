1ccee446edf6419eb62d8664d811b2112e16f279
neeta-hmcts <neeta.shinde@hmcts.net>
2023-11-02 07:52:00 +0000

    fixed build issue

--- src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/availabilityDates.js
+++ src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/availabilityDates.js
@@ -1,6 +1,6 @@
 const I = actor();
 const config = require('../../../../../../config');
-const {addMonths} = require('common/utils/dateUtils');
+import {addMonths} from 'common/utils/dateUtils';
 
 const fields ={
   singleDate: 'input[id="items-0-single-date"]',
