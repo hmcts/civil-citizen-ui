24644050a0b4342101255cded55b818fa0fd70ef
paul-pearson <paul.pearson@version1.com>
2023-11-03 11:00:17 +0000

    CIV-8027 Add test for pay by set date

--- src/test/unit/routes/features/settlementAgreement/respondSettlementAgreementConfirmationController.test.ts
+++ src/test/unit/routes/features/settlementAgreement/respondSettlementAgreementConfirmationController.test.ts
@@ -6,6 +6,9 @@ import {Claim} from 'common/models/claim';
 import {DEFENDANT_SIGN_SETTLEMENT_AGREEMENT_CONFIRMATION} from 'routes/urls';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
 import {getClaimById} from 'modules/utilityService';
+import {PaymentIntention} from 'form/models/admission/paymentIntention';
+import {PaymentOptionType} from 'form/models/admission/paymentOption/paymentOptionType';
+import {FullAdmission} from 'models/fullAdmission';
 
 jest.mock('../../../../../main/modules/oidc');
 jest.mock('../../../../../main/modules/draft-store/draftStoreService');
@@ -17,12 +20,17 @@ jest.mock('routes/guards/respondSettlementAgreementConfirmationGuard', () => ({
 }));
 const mockGetCaseData = getClaimById as jest.Mock;
 
-const mockClaim = new Claim();
-mockClaim.defendantSignedSettlementAgreement = true;
-
 describe('Claimant response confirmation controller', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
+
+  function getMockClaim() {
+    const mockClaim = new Claim();
+    mockClaim.fullAdmission = new FullAdmission();
+    mockClaim.fullAdmission.paymentIntention = new PaymentIntention();
+    mockClaim.fullAdmission.paymentIntention.paymentOption = PaymentOptionType.BY_SET_DATE;
+    return mockClaim;
+  }
 
   beforeEach(() => {
     nock(idamUrl)
@@ -30,14 +38,26 @@ describe('Claimant response confirmation controller', () => {
       .reply(200, {id_token: citizenRoleToken});
     nock('http://localhost:4000')
       .get('/cases/:id')
-      .reply(200, mockClaim);
+      .reply(200, getMockClaim());
   });
+
   describe('on GET', () => {
-    it('should return settlement agreement confirmation', async () => {
+    it('should return accept settlement agreement confirmation', async () => {
+      const mockClaim = getMockClaim();
+      mockClaim.defendantSignedSettlementAgreement = true;
       mockGetCaseData.mockImplementation(() => mockClaim);
       const res = await request(app).get(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT_CONFIRMATION);
       expect(res.status).toBe(200);
       expect(res.text).toContain("You've both signed a settlement agreement");
+    });
+
+    it('should return reject settlement agreement confirmation', async () => {
+      const mockClaim = getMockClaim();
+      mockClaim.defendantRejectedSettlementAgreement = true;
+      mockGetCaseData.mockImplementation(() => mockClaim);
+      const res = await request(app).get(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT_CONFIRMATION);
+      expect(res.status).toBe(200);
+      expect(res.text).toContain("You've rejected the settlement agreement");
     });
 
     it('should return http 500 when has error in the get method', async () => {
