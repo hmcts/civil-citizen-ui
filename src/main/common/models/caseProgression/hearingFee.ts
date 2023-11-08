2780ad71ba2fb85c59fdb738a3eb264e45a63db7
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-11-08 07:54:06 +0000

    add the fields

--- src/main/common/models/caseProgression/hearingFee.ts
+++ src/main/common/models/caseProgression/hearingFee.ts
@@ -1,5 +1,6 @@
 import {convertToPoundsFilter} from 'common/utils/currencyFormat';
 import {CCDClaimFee} from 'models/ccdResponse/ccdClaimFee';
+import {HearingDateTimeFormatter} from 'services/features/caseProgression/hearingDateTimeFormatter';
 
 export class HearingFee {
   claimFee?: CCDClaimFee;
@@ -13,9 +14,12 @@ export class HearingFee {
   getClaimFeeFormatted(): number {
     return convertToPoundsFilter(this.claimFee.calculatedAmountInPence);
   }
+
+  getHearingDueDateFormatted(lang:string): string {
+    return HearingDateTimeFormatter.getHearingDateFormatted(this.hearingDueDate, lang);
+  }
 }
 
 export enum CaseProgressionHearingFeeType {
   HEARING = 'HEARING',
-  CLAIMISSUED = 'CLAIMISSUED',
 }
