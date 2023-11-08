f4691f079a303624ea29699f23986ff3e6aadc0e
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2023-11-08 12:05:14 +0000

    CIV-11185 update controller

--- src/main/routes/features/caseProgression/hearingFee/cancelHearingFeeJourneyController.ts
+++ src/main/routes/features/caseProgression/hearingFee/cancelHearingFeeJourneyController.ts
@@ -9,13 +9,9 @@ import {deleteDraftClaimFromStore} from 'modules/draft-store/draftStoreService';
 const cancelHearingFeeJourneyController: Router = Router();
 
 cancelHearingFeeJourneyController.get([HEARING_FEE_CANCEL_JOURNEY], (async (req, res, next: NextFunction) => {
-  try {
-    const claimId = req.params.id;
-    await deleteDraftClaimFromStore(claimId);
-    res.redirect(constructResponseUrlWithIdParams(claimId, DASHBOARD_CLAIMANT_URL));
-  } catch (error) {
-    next(error);
-  }
+  const claimId = req.params.id;
+  await deleteDraftClaimFromStore(claimId);
+  res.redirect(constructResponseUrlWithIdParams(claimId, DASHBOARD_CLAIMANT_URL));
 }) as RequestHandler);
 
 export default cancelHearingFeeJourneyController;
