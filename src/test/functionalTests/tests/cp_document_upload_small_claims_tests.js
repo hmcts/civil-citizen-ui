6081e11e8707caff27412c3bd13f4218c28dcf92
marianadpereira <mariana.pereira@hmcts.net>
2023-11-09 17:49:59 +0000

    adding small claims test

--- src/test/functionalTests/tests/cp_document_upload_small_claims_tests.js
+++ src/test/functionalTests/tests/cp_document_upload_small_claims_tests.js
@@ -3,7 +3,7 @@ const UploadEvidenceSteps = require('../features/caseProgression/steps/caseProgr
 const DateUtilsComponent = require('../features/caseProgression/util/DateUtilsComponent');
 const LoginSteps = require('../features/home/steps/login');
 
-const claimType = 'FastTrack';
+const claimType = 'SmallClaims';
 let claimRef;
 
 Feature('Case progression journey - Verify Documents tab - Uploaded Evidence by LR');
@@ -11,18 +11,18 @@ Feature('Case progression journey - Verify Documents tab - Uploaded Evidence by 
 Before(async ({api}) => {
   //Once the CUI Release is done, we can remove this IF statement, so that tests will run on AAT as well.
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    const fourWeeksFromToday = DateUtilsComponent.DateUtilsComponent.rollDateToCertainWeeks(4);
     claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser, '', claimType);
+    await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
     await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType);
     await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL');
     await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef);
-    await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef, DateUtilsComponent.DateUtilsComponent.formatDateToYYYYMMDD(fourWeeksFromToday));
-    await api.performEvidenceUpload(config.applicantSolicitorUser, claimRef);
+    await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef);
+    await api.performEvidenceUpload(config.applicantSolicitorUser, claimRef);xx
     await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
   }
 });
 
-Scenario('Case progression journey - Fast Track - Verify uploaded documents by LR in the Documents tab', () => {
+Scenario('Case progression journey - Small Claims - Verify uploaded documents by LR in the Documents tab', () => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     UploadEvidenceSteps.verifyDocumentsUploadedBySolicitor(claimRef, claimType);
   }
