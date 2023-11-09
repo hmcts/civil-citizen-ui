397c0b85abaa42bc0982be00c4d1feceb89ef566
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-11-09 14:53:05 +0000

    add unit test

---
+++ src/test/unit/services/features/caseProgression/hearingFee/heringFeeStartPageContent.test.ts
@@ -0,0 +1,22 @@
+import {getHearingFeeStartPageContent} from 'services/features/caseProgression/hearingFee/hearingFeeStartPageContent';
+import {Claim} from 'models/claim';
+import {HearingFeeInformation} from 'models/caseProgression/hearingFee';
+import {CaseProgressionHearing} from 'models/caseProgression/caseProgressionHearing';
+const fixedDate = new Date(2023, 3, 26);
+describe('getPayHearingFeeStartScreenContent', () => {
+  it('should return all the content', () => {
+    //Given
+    const claim = new Claim();
+    claim.id = '1234567890';
+    claim.caseProgressionHearing = new CaseProgressionHearing(null, null,null,null, null, new HearingFeeInformation({calculatedAmountInPence: '1000', code: 'test', version: '1'}, fixedDate));
+    //When
+    const payHearingFeeStartScreenContentActual = getHearingFeeStartPageContent(claim.id, 'en', claim.caseProgressionHearing.hearingFeeInformation);
+
+    //Then
+    expect(payHearingFeeStartScreenContentActual[0].data.text).toEqual('COMMON.MICRO_TEXT.HEARING_FEE');
+    expect(payHearingFeeStartScreenContentActual[1].data.text).toEqual('PAGES.PAY_HEARING_FEE.START_PAGE.TITLE');
+    expect(payHearingFeeStartScreenContentActual[2].data.text).toEqual('PAGES.PAY_HEARING_FEE.START_PAGE.YOU_MUST_PAY');
+    expect(payHearingFeeStartScreenContentActual[3].data.text).toEqual('PAGES.PAY_HEARING_FEE.START_PAGE.IF_YOU_DO_NOT_PAY');
+    expect(payHearingFeeStartScreenContentActual[4].data.text).toEqual('COMMON.BUTTONS.START_NOW');
+  });
+});
