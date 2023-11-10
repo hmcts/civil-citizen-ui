0d07e8b878eb3e393ab6f03bf1120229480c0649
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2023-11-10 07:54:30 +0000

    hearing_fee_dev update controller

--- src/main/routes/features/caseProgression/hearingFee/cancelHearingFeeJourneyController.ts
+++ src/main/routes/features/caseProgression/hearingFee/cancelHearingFeeJourneyController.ts
@@ -8,7 +8,7 @@ import {deleteDraftClaimFromStore} from 'modules/draft-store/draftStoreService';
 
 const cancelHearingFeeJourneyController: Router = Router();
 
-cancelHearingFeeJourneyController.get([HEARING_FEE_CANCEL_JOURNEY], (async (req, res, next: NextFunction) => {
+cancelHearingFeeJourneyController.get(HEARING_FEE_CANCEL_JOURNEY, (async (req, res, next: NextFunction) => {
   const claimId = req.params.id;
   await deleteDraftClaimFromStore(claimId);
   res.redirect(constructResponseUrlWithIdParams(claimId, DASHBOARD_CLAIMANT_URL));
