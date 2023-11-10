b0da8f666f6815b9c5e6775723aee92360f96254
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2023-11-10 15:07:51 +0000

    CIV-9024 changes

--- src/main/common/models/claimantResponse/ccdClaimantResponse.ts
+++ src/main/common/models/claimantResponse/ccdClaimantResponse.ts
@@ -9,12 +9,15 @@ import {CCDExpert} from 'models/ccdResponse/ccdExpert';
 import {CCDClaimantLiPResponse} from 'services/translation/claimantResponse/convertToCCDClaimantLiPResponse';
 import {CCDMediation} from '../ccdResponse/ccdMediation';
 import {CCDPaymentOption} from 'models/ccdResponse/ccdPaymentOption';
+import {
+  ClaimantResponseRequestDefaultJudgementToCCD
+} from 'services/translation/claimantResponse/ccdRequestJudgementTranslation';
 
 export interface CCDClaimantMediationLip extends CCDMediation {
   hasAgreedFreeMediation?: YesNoUpperCamelCase;
 }
 
-export interface CCDClaimantResponse extends ClaimUpdate{
+export interface CCDClaimantResponse extends ClaimUpdate, ClaimantResponseRequestDefaultJudgementToCCD {
   applicant1DQLanguage?: CCDWelshLanguageRequirements;
   applicant1DQVulnerabilityQuestions?: CCDVulnerability;
   applicant1DQRequestedCourt?: CCDSpecificCourtLocations;
--- src/main/services/features/claimantResponse/submitClaimantResponse.ts
+++ src/main/services/features/claimantResponse/submitClaimantResponse.ts
@@ -4,6 +4,10 @@ import {AppRequest} from 'common/models/AppRequest';
 import {Claim} from 'common/models/claim';
 import { generateRedisKey, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
 import {translateClaimantResponseToCCD} from 'services/translation/claimantResponse/claimantResponseCCDTranslation';
+import {ClaimantResponse} from 'models/claimantResponse';
+import {
+  translateClaimantResponseRequestDefaultJudgementToCCD
+} from 'services/translation/claimantResponse/ccdRequestJudgementTranslation';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('submitClaimantResponse');
@@ -14,7 +18,13 @@ const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServi
 export const submitClaimantResponse = async (req: AppRequest): Promise<Claim> => {
   try {
     const claim = await getCaseDataFromStore(generateRedisKey(req as unknown as AppRequest));
-    const ccdResponse = translateClaimantResponseToCCD(claim);
+    let ccdResponse = translateClaimantResponseToCCD(claim);
+    const claimantResponse = Object.assign(new ClaimantResponse(), claim.claimantResponse);
+    if(claimantResponse.isCCJRequested && isResponseTypeRequestedCCJRequest(claim)) {
+      const claimFeeAmount = await civilServiceClient.getClaimAmountFee(claim?.totalClaimAmount, req);
+      const ccdResponseForRequestDefaultJudgement = translateClaimantResponseRequestDefaultJudgementToCCD(claim, claimFeeAmount);
+      ccdResponse = {...ccdResponseForRequestDefaultJudgement,...ccdResponse};
+    }
     logger.info('Sumbmitting claimant intention...',ccdResponse);
     return await civilServiceClient.submitClaimantResponseEvent(req.params.id, ccdResponse, req);
   } catch (err) {
@@ -22,3 +32,8 @@ export const submitClaimantResponse = async (req: AppRequest): Promise<Claim> =>
     throw err;
   }
 };
+
+function isResponseTypeRequestedCCJRequest(claim: Claim): boolean {
+  return ((claim.isFullAdmission() && (claim.isFAPaymentOptionInstallments() || claim.isFAPaymentOptionBySetDate())) ||
+    (claim.isPartialAdmission() && (claim.isPAPaymentOptionByDate() || claim.isPAPaymentOptionInstallments())));
+}
