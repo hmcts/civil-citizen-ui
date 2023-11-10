2af16ce9448553456e16ed17d9cff7994328a0f2
laarmada <sergiosalmar@yahoo.es>
2023-11-10 09:46:15 +0000

    add unit test

---
+++ src/test/unit/routes/features/caseProgression/hearingFee/applyHelpWithFeeController.test.ts
@@ -0,0 +1,52 @@
+import request from 'supertest';
+import {app} from '../../../../../../main/app';
+import nock from 'nock';
+import config from 'config';
+import {
+  APPLY_HELP_WITH_FEES,
+  HEARING_FEE_APPLY_HELP_FEE_SELECTION,
+} from 'routes/urls';
+
+jest.mock('../../../../../../main/modules/oidc');
+jest.mock('../../../../../../main/modules/draft-store');
+
+describe('Apply for help with fees', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+
+  beforeAll(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+  });
+
+  describe('on GET', () => {
+    it('should return resolving apply help fees page', async () => {
+      await request(app)
+        .get(APPLY_HELP_WITH_FEES)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Apply for Help with Fees (open in a new window)');
+          expect(res.text).toContain('Hearing fee');
+          expect(res.text).toContain('Apply for help with fees');
+          expect(res.text).toContain('If you already have a help with fees reference number in relation to the claim issue fee or any application fees, you should not use this reference number for this application.');
+          expect(res.text).toContain('Instead, you should make a new help');
+          expect(res.text).toContain('During your application, you');
+          expect(res.text).toContain('Once you have made your application, return');
+        });
+    });
+  });
+
+  describe('on POST', () => {
+
+    it('should redirect to help with fee selection if continue', async () => {
+      await request(app)
+        .post(APPLY_HELP_WITH_FEES)
+        .send()
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toEqual(HEARING_FEE_APPLY_HELP_FEE_SELECTION);
+        });
+    });
+  });
+});
