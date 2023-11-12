89b9ea725b9a44433ece7e27fa1ea5adce2b16cc
jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
2023-11-12 14:56:28 +0000

    Update addYourTimeLine.js

--- src/test/functionalTests/features/response/pages/defendantLipResponse/whyDoYouDisagree/addYourTimeLine.js
+++ src/test/functionalTests/features/response/pages/defendantLipResponse/whyDoYouDisagree/addYourTimeLine.js
@@ -14,7 +14,7 @@ const buttons = {
 };
 
 const newDate = new Date();
-const extendedDay = newDate.getDate();
+const extendedDay = newDate.getDay();
 const extendedMonth = newDate.getMonth();
 const extendedYear = newDate.getFullYear();
 
