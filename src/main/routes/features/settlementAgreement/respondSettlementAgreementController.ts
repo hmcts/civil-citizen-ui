e0cca3ae02c4c232e13b8cf1be200e1fe510cf07
paul-pearson <paul.pearson@version1.com>
2023-11-13 12:31:54 +0000

    CIV-8147 Reformat files with prettier

--- src/main/routes/features/settlementAgreement/respondSettlementAgreementController.ts
+++ src/main/routes/features/settlementAgreement/respondSettlementAgreementController.ts
@@ -1,83 +1,138 @@
-import {NextFunction, Request, RequestHandler, Response, Router} from 'express';
-import {DEFENDANT_SIGN_SETTLEMENT_AGREEMENT} from '../../urls';
-import {GenericForm} from 'common/form/models/genericForm';
-import {generateRedisKey, getCaseDataFromStore, saveDraftClaim} from 'modules/draft-store/draftStoreService';
 import {
-    getAmount,
-    getFinalPaymentDate,
-    getFirstRepaymentDate,
-    getPaymentAmount, getPaymentDate, getPaymentOptionType,
-    getRepaymentFrequency,
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+  Router,
+} from 'express';
+import { DEFENDANT_SIGN_SETTLEMENT_AGREEMENT } from '../../urls';
+import { GenericForm } from 'common/form/models/genericForm';
+import {
+  generateRedisKey,
+  getCaseDataFromStore,
+  saveDraftClaim,
+} from 'modules/draft-store/draftStoreService';
+import {
+  getAmount,
+  getFinalPaymentDate,
+  getFirstRepaymentDate,
+  getPaymentAmount,
+  getPaymentDate,
+  getPaymentOptionType,
+  getRepaymentFrequency,
 } from 'common/utils/repaymentUtils';
-import {formatDateToFullDate} from 'common/utils/dateUtils';
-import {Claim} from 'common/models/claim';
-import {GenericYesNo} from 'form/models/genericYesNo';
-import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
-import {AppRequest} from 'common/models/AppRequest';
-import {CivilServiceClient} from 'client/civilServiceClient';
+import { formatDateToFullDate } from 'common/utils/dateUtils';
+import { Claim } from 'common/models/claim';
+import { GenericYesNo } from 'form/models/genericYesNo';
+import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
+import { AppRequest } from 'common/models/AppRequest';
+import { CivilServiceClient } from 'client/civilServiceClient';
 import config from 'config';
 import { toCCDYesNo } from 'services/translation/response/convertToCCDYesNo';
-import {getClaimById} from 'modules/utilityService';
+import { getClaimById } from 'modules/utilityService';
 
-const respondSettlementAgreementViewPath = 'features/settlementAgreement/respond-settlement-agreement';
+const respondSettlementAgreementViewPath =
+  'features/settlementAgreement/respond-settlement-agreement';
 const respondSettlementAgreementController = Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
-const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
+const civilServiceClient: CivilServiceClient = new CivilServiceClient(
+  civilServiceApiBaseUrl
+);
 
-function renderView(form: GenericForm<GenericYesNo>, res: Response, data?: object): void {
-    res.render(respondSettlementAgreementViewPath, {form, data});
+function renderView(
+  form: GenericForm<GenericYesNo>,
+  res: Response,
+  data?: object
+): void {
+  res.render(respondSettlementAgreementViewPath, { form, data });
 }
 
 const getSettlementAgreementData = (claim: Claim, req: Request) => {
-    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-    const data = {
-        amount: getAmount(claim),
-        claimant: claim.getClaimantFullName(),
-        defendant: claim.getDefendantFullName(),
-        paymentOption: getPaymentOptionType(claim),
-        firstRepaymentDate: formatDateToFullDate(getFirstRepaymentDate(claim), lang),
-        finalRepaymentDate: formatDateToFullDate(getFinalPaymentDate(claim), lang),
-        paymentDate: formatDateToFullDate(getPaymentDate(claim), lang),
-        paymentAmount: getPaymentAmount(claim),
-        repaymentFrequency: getRepaymentFrequency(claim),
-    };
+  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+  const data = {
+    amount: getAmount(claim),
+    claimant: claim.getClaimantFullName(),
+    defendant: claim.getDefendantFullName(),
+    paymentOption: getPaymentOptionType(claim),
+    firstRepaymentDate: formatDateToFullDate(
+      getFirstRepaymentDate(claim),
+      lang
+    ),
+    finalRepaymentDate: formatDateToFullDate(getFinalPaymentDate(claim), lang),
+    paymentDate: formatDateToFullDate(getPaymentDate(claim), lang),
+    paymentAmount: getPaymentAmount(claim),
+    repaymentFrequency: getRepaymentFrequency(claim),
+  };
 
-    return data;
+  return data;
 };
 
-respondSettlementAgreementController.get(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT, (async (req: Request, res: Response, next: NextFunction) => {
+respondSettlementAgreementController.get(
+  DEFENDANT_SIGN_SETTLEMENT_AGREEMENT,
+  (async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const claimId = req.params.id;
-        const claim = await getClaimById(claimId, req, true);
-        // TODO: Populate form from saved response once this is implemented in the model
-        renderView(new GenericForm(new GenericYesNo(req.body.option || claim.respondentSignSettlementAgreement?.option, 'PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION')), res, getSettlementAgreementData(claim, req));
+      const claimId = req.params.id;
+      const claim = await getClaimById(claimId, req, true);
+      // TODO: Populate form from saved response once this is implemented in the model
+      renderView(
+        new GenericForm(
+          new GenericYesNo(
+            req.body.option || claim.respondentSignSettlementAgreement?.option,
+            'PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION'
+          )
+        ),
+        res,
+        getSettlementAgreementData(claim, req)
+      );
     } catch (error) {
-        next(error);
+      next(error);
     }
-}) as RequestHandler);
+  }) as RequestHandler
+);
 
-respondSettlementAgreementController.post(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT, (async (req: Request, res: Response, next) => {
+respondSettlementAgreementController.post(
+  DEFENDANT_SIGN_SETTLEMENT_AGREEMENT,
+  (async (req: Request, res: Response, next) => {
     try {
-        const claimId = req.params.id;
-        const redisKey = generateRedisKey(req as unknown as AppRequest);
-        const respondSettlementAgreementOption = new GenericYesNo(req.body.option, 'PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION');
-        const respondSettlementAgreement = new GenericForm(respondSettlementAgreementOption);
+      const claimId = req.params.id;
+      const redisKey = generateRedisKey(req as unknown as AppRequest);
+      const respondSettlementAgreementOption = new GenericYesNo(
+        req.body.option,
+        'PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION'
+      );
+      const respondSettlementAgreement = new GenericForm(
+        respondSettlementAgreementOption
+      );
 
-        respondSettlementAgreement.validateSync();
-        const claim = await getCaseDataFromStore(redisKey);
-        if (respondSettlementAgreement.hasErrors()) {
-            renderView(respondSettlementAgreement, res, getSettlementAgreementData(claim, req));
-        } else {
-            // TODO : Save respondSettlementAgreement.model.option value and redirect to next page
-            claim.respondentSignSettlementAgreement = respondSettlementAgreementOption;
-            await saveDraftClaim(redisKey, claim, true);
+      respondSettlementAgreement.validateSync();
+      const claim = await getCaseDataFromStore(redisKey);
+      if (respondSettlementAgreement.hasErrors()) {
+        renderView(
+          respondSettlementAgreement,
+          res,
+          getSettlementAgreementData(claim, req)
+        );
+      } else {
+        // TODO : Save respondSettlementAgreement.model.option value and redirect to next page
+        claim.respondentSignSettlementAgreement =
+          respondSettlementAgreementOption;
+        await saveDraftClaim(redisKey, claim, true);
 
-            await civilServiceClient.submitDefendantSignSettlementAgreementEvent(claimId,{'respondentSignSettlementAgreement' : toCCDYesNo(claim.respondentSignSettlementAgreement.option)}, <AppRequest>req);
-            res.redirect(constructResponseUrlWithIdParams(claimId, '<Next page>>'));
-        }
+        await civilServiceClient.submitDefendantSignSettlementAgreementEvent(
+          claimId,
+          {
+            respondentSignSettlementAgreement: toCCDYesNo(
+              claim.respondentSignSettlementAgreement.option
+            ),
+          },
+          <AppRequest>req
+        );
+        res.redirect(constructResponseUrlWithIdParams(claimId, '<Next page>>'));
+      }
     } catch (error) {
-        next(error);
+      next(error);
     }
-}) as RequestHandler);
+  }) as RequestHandler
+);
 
 export default respondSettlementAgreementController;
--- src/test/unit/routes/features/settlementAgreement/respondSettlementAgreementController.test.ts
+++ src/test/unit/routes/features/settlementAgreement/respondSettlementAgreementController.test.ts
@@ -1,44 +1,44 @@
-import config from "config";
-import { t } from "i18next";
-import nock from "nock";
-import request from "supertest";
-import { app } from "../../../../../main/app";
-import { DEFENDANT_SIGN_SETTLEMENT_AGREEMENT } from "../../../../../main/routes/urls";
+import config from 'config';
+import { t } from 'i18next';
+import nock from 'nock';
+import request from 'supertest';
+import { app } from '../../../../../main/app';
+import { DEFENDANT_SIGN_SETTLEMENT_AGREEMENT } from '../../../../../main/routes/urls';
 import {
   mockCivilClaim,
   mockRedisFailure,
-} from "../../../../utils/mockDraftStore";
-import { TestMessages } from "../../../../utils/errorMessageTestConstants";
-import { ResponseType } from "common/form/models/responseType";
-import { TransactionSchedule } from "common/form/models/statementOfMeans/expensesAndIncome/transactionSchedule";
-import { formatDateToFullDate } from "common/utils/dateUtils";
-import * as draftStoreService from "modules/draft-store/draftStoreService";
-import { CIVIL_SERVICE_SUBMIT_EVENT } from "client/civilServiceUrls";
-import { Claim } from "common/models/claim";
-import { GenericYesNo } from "common/form/models/genericYesNo";
+} from '../../../../utils/mockDraftStore';
+import { TestMessages } from '../../../../utils/errorMessageTestConstants';
+import { ResponseType } from 'common/form/models/responseType';
+import { TransactionSchedule } from 'common/form/models/statementOfMeans/expensesAndIncome/transactionSchedule';
+import { formatDateToFullDate } from 'common/utils/dateUtils';
+import * as draftStoreService from 'modules/draft-store/draftStoreService';
+import { CIVIL_SERVICE_SUBMIT_EVENT } from 'client/civilServiceUrls';
+import { Claim } from 'common/models/claim';
+import { GenericYesNo } from 'common/form/models/genericYesNo';
 
-jest.mock("../../../../../main/modules/oidc");
-jest.mock("../../../../../main/modules/draft-store");
+jest.mock('../../../../../main/modules/oidc');
+jest.mock('../../../../../main/modules/draft-store');
 
-describe("Respond To Settlement Agreement", () => {
-  const citizenRoleToken: string = config.get("citizenRoleToken");
-  const idamUrl: string = config.get("idamUrl");
+describe('Respond To Settlement Agreement', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
 
   beforeAll(() => {
-    nock(idamUrl).post("/o/token").reply(200, { id_token: citizenRoleToken });
-    nock("http://localhost:4000")
+    nock(idamUrl).post('/o/token').reply(200, { id_token: citizenRoleToken });
+    nock('http://localhost:4000')
       .post(
-        CIVIL_SERVICE_SUBMIT_EVENT.replace(":submitterId", "undefined").replace(
-          ":caseId",
-          ":id",
-        ),
+        CIVIL_SERVICE_SUBMIT_EVENT.replace(':submitterId', 'undefined').replace(
+          ':caseId',
+          ':id'
+        )
       )
       .reply(200, {});
   });
 
-  describe("on GET", () => {
+  describe('on GET', () => {
     const date = new Date(Date.now());
-    it("should return respond to settlement agreement page", async () => {
+    it('should return respond to settlement agreement page', async () => {
       const civilClaimResponseMock = {
         case_data: {
           respondent1: {
@@ -46,7 +46,7 @@ describe("Respond To Settlement Agreement", () => {
           },
           partialAdmission: {
             alreadyPaid: {
-              option: "yes",
+              option: 'yes',
             },
             howMuchDoYouOwe: {
               amount: 200,
@@ -65,7 +65,7 @@ describe("Respond To Settlement Agreement", () => {
       app.locals.draftStoreClient = {
         set: jest.fn(() => Promise.resolve({})),
         get: jest.fn(() =>
-          Promise.resolve(JSON.stringify(civilClaimResponseMock)),
+          Promise.resolve(JSON.stringify(civilClaimResponseMock))
         ),
       };
 
@@ -74,24 +74,24 @@ describe("Respond To Settlement Agreement", () => {
         .expect((res) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain(
-            t("PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.TITLE"),
+            t('PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.TITLE')
           );
           expect(res.text).toContain(
             t(
-              "PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.THE_AGREEMENT.REPAYMENT_PLAN",
+              'PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.THE_AGREEMENT.REPAYMENT_PLAN',
               {
-                defendant: "",
-                amount: "200",
-                paymentAmount: "50",
-                theAgreementRepaymentFrequency: "week",
+                defendant: '',
+                amount: '200',
+                paymentAmount: '50',
+                theAgreementRepaymentFrequency: 'week',
                 firstRepaymentDate: formatDateToFullDate(date),
-              },
-            ),
+              }
+            )
           );
         });
     });
 
-    it("should return status 500 when error thrown", async () => {
+    it('should return status 500 when error thrown', async () => {
       app.locals.draftStoreClient = mockRedisFailure;
       await request(app)
         .get(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT)
@@ -102,49 +102,49 @@ describe("Respond To Settlement Agreement", () => {
     });
   });
 
-  describe("on POST", () => {
+  describe('on POST', () => {
     beforeEach(() => {
       app.locals.draftStoreClient = mockCivilClaim;
     });
 
-    it("should return error on empty post", async () => {
+    it('should return error on empty post', async () => {
       await request(app)
         .post(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT)
         .expect((res) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain(
             t(
-              "PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION",
-            ),
+              'PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION'
+            )
           );
         });
     });
 
-    it("should redirect to the confirmation if sign agreement checkbox is selected", async () => {
+    it('should redirect to the confirmation if sign agreement checkbox is selected', async () => {
       const spySaveDraftClaimMock = jest.spyOn(
         draftStoreService,
-        "saveDraftClaim",
+        'saveDraftClaim'
       );
       const respondentSignSettlementAgreement = new GenericYesNo(
-        "yes",
-        "PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION",
+        'yes',
+        'PAGES.DEFENDANT_RESPOND_TO_SETTLEMENT_AGREEMENT.DETAILS.VALID_YES_NO_OPTION'
       );
       jest
-        .spyOn(draftStoreService, "generateRedisKey")
-        .mockReturnValueOnce("1");
+        .spyOn(draftStoreService, 'generateRedisKey')
+        .mockReturnValueOnce('1');
       jest
-        .spyOn(draftStoreService, "getCaseDataFromStore")
+        .spyOn(draftStoreService, 'getCaseDataFromStore')
         .mockResolvedValueOnce({} as Claim);
 
       await request(app)
         .post(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT)
-        .send({ option: "yes" })
+        .send({ option: 'yes' })
         .expect((res) => {
           expect(res.status).toBe(302);
           expect(spySaveDraftClaimMock).toHaveBeenCalledWith(
-            "1",
+            '1',
             { respondentSignSettlementAgreement },
-            true,
+            true
           );
 
           //TODO: Check header location with confirmation page url
@@ -152,12 +152,12 @@ describe("Respond To Settlement Agreement", () => {
         });
     });
 
-    it("should return status 500 when error thrown", async () => {
+    it('should return status 500 when error thrown', async () => {
       app.locals.draftStoreClient = mockRedisFailure;
 
       await request(app)
         .post(DEFENDANT_SIGN_SETTLEMENT_AGREEMENT)
-        .send({ option: "yes" })
+        .send({ option: 'yes' })
         .expect((res) => {
           expect(res.status).toBe(500);
           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
