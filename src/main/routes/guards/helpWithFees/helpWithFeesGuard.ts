beda2331fa4516adcf0c18109ee8f8248853dc2f
meulendijksm <max.meulendijks@hmcts.net>
2023-11-15 15:09:16 +0000

    CIV-7828 - removed guard from page

--- src/main/routes/guards/helpWithFees/helpWithFeesGuard.ts
+++ src/main/routes/guards/helpWithFees/helpWithFeesGuard.ts
@@ -4,6 +4,7 @@ import {getClaimById} from 'modules/utilityService';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {DASHBOARD_CLAIMANT_URL} from 'routes/urls';
 
+//TODO: This guard was created as the help with fees journey is generic, and it is not certain that the new page can always rely on the new dashboard solution.
 export const helpWithFeesGuard = (async (req: Request, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
