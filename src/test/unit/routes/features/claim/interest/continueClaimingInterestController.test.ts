e015ebcf160c3c3b4d53932249d1a5664a5281c2
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-11-17 14:24:54 +0100

    CIV-0000 : Fix claim journey tests

--- src/test/unit/routes/features/claim/interest/continueClaimingInterestController.test.ts
+++ src/test/unit/routes/features/claim/interest/continueClaimingInterestController.test.ts
@@ -17,6 +17,7 @@ jest.mock('../../../../../../main/modules/draft-store');
 describe('Continue Claiming Interest page', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
+  app.request.cookies = {eligibilityCompleted: true};
 
   beforeAll(() => {
     nock(idamUrl)
