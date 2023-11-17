dccc93caaee0b1bc210b0ec6e0517c6ef29a5b75
sankaviv1 <sankavi.mohanraj@version1.com>
2023-11-17 09:24:21 +0000

    CIV-11059 Defendant response mandatory phone number

--- src/main/common/form/models/citizenTelephoneNumber.ts
+++ src/main/common/form/models/citizenTelephoneNumber.ts
@@ -1,9 +1,22 @@
-import {Validate} from 'class-validator';
+import {IsNotEmpty, Validate, ValidateIf} from 'class-validator';
 import {PhoneUKValidator} from '../validators/phoneUKValidator';
 
-export class CitizenTelephoneNumber {
+export class CitizenTelephoneNumberOptional {
   @Validate(PhoneUKValidator, {message: 'ERRORS.VALID_PHONE_NUMBER'})
     telephoneNumber?: string;
+  ccdPhoneExist?: boolean;
+
+  constructor(telephoneNumber?: string,  ccdPhoneExist?: boolean) {
+    this.telephoneNumber = telephoneNumber?.trim();
+    this.ccdPhoneExist = ccdPhoneExist;
+  }
+}
+
+export class CitizenTelephoneNumber {
+  @ValidateIf(o => o.telephoneNumber !== undefined)
+  @Validate(PhoneUKValidator, {message: 'ERRORS.VALID_PHONE_NUMBER'})
+  @IsNotEmpty({message: 'ERRORS.ENTER_TELEPHONE_NUMBER'})
+  telephoneNumber?: string;
   ccdPhoneExist?: boolean;
 
   constructor(telephoneNumber?: string,  ccdPhoneExist?: boolean) {
--- src/main/routes/features/response/citizenPhoneNumber/citizenPhoneController.ts
+++ src/main/routes/features/response/citizenPhoneNumber/citizenPhoneController.ts
@@ -1,5 +1,8 @@
 import {NextFunction, Response, Router} from 'express';
-import {CitizenTelephoneNumber} from '../../../../common/form/models/citizenTelephoneNumber';
+import {
+  CitizenTelephoneNumber,
+  CitizenTelephoneNumberOptional
+} from '../../../../common/form/models/citizenTelephoneNumber';
 import {CITIZEN_PHONE_NUMBER_URL, RESPONSE_TASK_LIST_URL} from '../../../urls';
 import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
 import {GenericForm} from '../../../../common/form/models/genericForm';
@@ -7,18 +10,24 @@ import {ClaimantOrDefendant} from '../../../../common/models/partyType';
 import {getTelephone, saveTelephone} from '../../../../services/features/claim/yourDetails/phoneService';
 import {AppRequest} from 'common/models/AppRequest';
 import {generateRedisKey} from 'modules/draft-store/draftStoreService';
+import {Claim} from "models/claim";
+import {getClaimById} from "modules/utilityService";
+import {isCarmEnabledForCase} from "common/utils/carmToggleUtils";
 
 const citizenPhoneViewPath = 'features/response/citizenPhoneNumber/citizen-phone';
 const citizenPhoneController = Router();
 
-function renderView(form: GenericForm<CitizenTelephoneNumber>, res: Response): void {
-  res.render(citizenPhoneViewPath, {form});
+function renderView(form: GenericForm<CitizenTelephoneNumber>, res: Response, carmEnabled: boolean): void {
+  res.render(citizenPhoneViewPath, {form, carmEnabled: carmEnabled});
 }
 
 citizenPhoneController.get(CITIZEN_PHONE_NUMBER_URL, async (req, res, next: NextFunction) => {
   try {
     const citizenTelephoneNumber: CitizenTelephoneNumber = await getTelephone(generateRedisKey(<AppRequest>req), ClaimantOrDefendant.DEFENDANT);
-    renderView(new GenericForm<CitizenTelephoneNumber>(citizenTelephoneNumber), res);
+    const currentClaimId = req.params.id;
+    const claim: Claim = await getClaimById(currentClaimId, req, true);
+    let carmEnabled = await isCarmEnabledForCase(claim.submittedDate);
+    renderView(new GenericForm<CitizenTelephoneNumber>(citizenTelephoneNumber), res, carmEnabled);
   } catch (error) {
     next(error);
   }
@@ -26,11 +35,15 @@ citizenPhoneController.get(CITIZEN_PHONE_NUMBER_URL, async (req, res, next: Next
 citizenPhoneController.post(CITIZEN_PHONE_NUMBER_URL,
   async (req, res, next: NextFunction) => {
     try {
-      const model: CitizenTelephoneNumber = new CitizenTelephoneNumber(req.body.telephoneNumber === '' ? undefined : req.body.telephoneNumber);
+      const currentClaimId = req.params.id;
+      const claim: Claim = await getClaimById(currentClaimId, req, true);
+      let carmEnabled = await isCarmEnabledForCase(new Date(claim.submittedDate));
+      const model = carmEnabled ?  new CitizenTelephoneNumber(req.body.telephoneNumber)
+        : new CitizenTelephoneNumberOptional(req.body.telephoneNumber === '' ? undefined : req.body.telephoneNumber);
       const citizenTelephoneNumberForm = new GenericForm(model);
       citizenTelephoneNumberForm.validateSync();
       if (citizenTelephoneNumberForm.hasErrors()) {
-        renderView(citizenTelephoneNumberForm, res);
+        renderView(citizenTelephoneNumberForm, res, carmEnabled);
       } else {
         await saveTelephone(generateRedisKey(<AppRequest>req), model, ClaimantOrDefendant.DEFENDANT);
         const redirectURL = constructResponseUrlWithIdParams(req.params.id, RESPONSE_TASK_LIST_URL);
