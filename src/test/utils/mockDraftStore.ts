066964a0b0ff184f1af1e9a04179d63ba5640df5
sankaviv1 <sankavi.mohanraj@version1.com>
2023-11-20 15:33:40 +0000

    add and update tests

--- src/test/utils/mockDraftStore.ts
+++ src/test/utils/mockDraftStore.ts
@@ -25,6 +25,15 @@ import civilClaimResponseDocumentUploadedMock from './mocks/civilClaimResponseDo
 import civilClaimResponseFastTrackMock from './mocks/civilClaimResponseFastTrackMock.json';
 
 import {LoggerInstance} from 'winston';
+import {Claim} from "models/claim";
+
+const mockDraftClaim = (claim: Claim) => ({
+  set: jest.fn(() => Promise.resolve({})),
+  get: jest.fn(() => Promise.resolve(JSON.stringify(claim))),
+  del: jest.fn(() => Promise.resolve({})),
+  ttl: jest.fn(() => Promise.resolve({})),
+  expireat: jest.fn(() => Promise.resolve({})),
+});
 
 const mockCivilClaim = {
   set: jest.fn(() => Promise.resolve({})),
@@ -226,4 +235,5 @@ export {
   mockCivilClaimDocumentUploaded,
   mockCivilClaimFastTrack,
   civilClaimResponseMock,
+  mockDraftClaim,
 };
