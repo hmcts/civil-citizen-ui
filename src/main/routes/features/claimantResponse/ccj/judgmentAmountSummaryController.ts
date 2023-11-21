9bdc948e7227d0c019c90a6f2fd78299361f5f69
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-11-21 09:27:43 +0000

    Update judgmentAmountSummaryController.ts

--- src/main/routes/features/claimantResponse/ccj/judgmentAmountSummaryController.ts
+++ src/main/routes/features/claimantResponse/ccj/judgmentAmountSummaryController.ts
@@ -33,7 +33,6 @@ judgmentAmountSummaryController.get(CCJ_PAID_AMOUNT_SUMMARY_URL, async (req: App
   }
 });
 
-
 judgmentAmountSummaryController.post(CCJ_PAID_AMOUNT_SUMMARY_URL, async (req: AppRequest, res: Response) => {
   res.redirect(constructResponseUrlWithIdParams(req.params.id, CCJ_PAYMENT_OPTIONS_URL));
 });
