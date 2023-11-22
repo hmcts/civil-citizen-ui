08fdb9a35bb884ee458338022d828a935fb4c69d
rhysbeazleyy <Rhys.Beazley@HMCTS.NET>
2023-11-22 12:14:13 +0000

    date translation for how they want to pay

--- src/main/services/features/claimantResponse/getSetDatePaymentDetails.ts
+++ src/main/services/features/claimantResponse/getSetDatePaymentDetails.ts
@@ -2,17 +2,18 @@ import {ClaimantResponse} from 'common/models/claimantResponse';
 import {GenericYesNo} from 'common/form/models/genericYesNo';
 import {formatDateToFullDate} from 'common/utils/dateUtils';
 import {Claim} from 'models/claim';
+import { t } from 'i18next';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('fullAdmitSetDatePaymentService');
 
-export const getSetDatePaymentDetails = (claim: Claim): {fullAdmitAcceptPayment: GenericYesNo; defendantName: string; proposedSetDate: string} => {
+export const getSetDatePaymentDetails = (claim: Claim, lang: string): {fullAdmitAcceptPayment: GenericYesNo; defendantName: string; proposedSetDate: string} => {
   try {
     const claimantResponse = (claim?.claimantResponse) ? claim.claimantResponse : new ClaimantResponse();
     const fullAdmitAcceptPayment = claimantResponse?.fullAdmitSetDateAcceptPayment;
     const defendantName = claim.getDefendantFullName();
     const paymentIntention = claim.getPaymentIntention();
-    const proposedSetDate = formatDateToFullDate(paymentIntention?.paymentDate);
+    const proposedSetDate = t(formatDateToFullDate(paymentIntention?.paymentDate, lang));
     return {
       fullAdmitAcceptPayment,
       defendantName,
