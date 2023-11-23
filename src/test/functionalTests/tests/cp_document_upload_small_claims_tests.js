97e73ec6736a1bdff60a7af473a1cae634c26fe4
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-11-23 15:17:09 +0000

    test

--- src/test/functionalTests/tests/cp_document_upload_small_claims_tests.js
+++ src/test/functionalTests/tests/cp_document_upload_small_claims_tests.js
@@ -6,14 +6,22 @@ const LoginSteps = require('../features/home/steps/login');
 
 const claimType = 'SmallClaims';
 let claimRef;
+let caseData;
+let claimNumber;
+let securityCode;
 
 Feature('Case progression journey - Verify Documents tab - Uploaded Evidence by LR');
 
 Before(async ({api}) => {
   //Once the CUI Release is done, we can remove this IF statement, so that tests will run on AAT as well.
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    const fourWeeksFromToday = DateUtilsComponent.DateUtilsComponent.rollDateToCertainWeeks(4);
+    // const fourWeeksFromToday = DateUtilsComponent.DateUtilsComponent.rollDateToCertainWeeks(4);
     claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser, '', claimType);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber = await caseData.legacyCaseReference;
+    securityCode = await caseData.respondent1PinToPostLRspec.accessCode;
+    console.log('claim number', claimNumber);
+    console.log('Security code', securityCode);
     /*   await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
     await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType);
     await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL');
