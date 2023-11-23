126e6b02cb5d7765fc5d3ff1f565d1b550fb31d5
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-11-23 11:41:57 +0000

    fixed lint issue

--- src/test/functionalTests/tests/cp_document_upload_small_claims_tests.js
+++ src/test/functionalTests/tests/cp_document_upload_small_claims_tests.js
@@ -14,7 +14,7 @@ Before(async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     const fourWeeksFromToday = DateUtilsComponent.DateUtilsComponent.rollDateToCertainWeeks(4);
     claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser, '', claimType);
- /*   await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    /*   await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
     await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType);
     await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL');
     await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef);
