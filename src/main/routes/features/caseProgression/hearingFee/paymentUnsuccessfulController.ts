25c22fced7f0d3ab4f20e04734114634931d7e0d
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2023-11-23 13:10:38 +0000

    CIV-7824 fixing sonar build issue

--- src/main/routes/features/caseProgression/hearingFee/paymentUnsuccessfulController.ts
+++ src/main/routes/features/caseProgression/hearingFee/paymentUnsuccessfulController.ts
@@ -8,23 +8,25 @@ import {getClaimById} from 'modules/utilityService';
 const paymentUnsuccessfulViewPath = 'features/caseProgression/hearingFee/payment-unsuccessful';
 const paymentUnsuccessfulController: Router = Router();
 
-paymentUnsuccessfulController.get(PAY_HEARING_FEE_UNSUCCESSFUL_URL, async (req, res, next: NextFunction) => {
-  try {
-    const claimId = req.params.id;
-    const claim = await getClaimById(claimId, req, true);
+paymentUnsuccessfulController.get(PAY_HEARING_FEE_UNSUCCESSFUL_URL, (req, res, next: NextFunction) => {
+  (async () => {
+    try {
+      const claimId = req.params.id;
+      const claim = await getClaimById(claimId, req, true);
 
-    if (!claim.isEmpty()) {
-      const claimNumber = claim.getFormattedCaseReferenceNumber(claimId);
-      const claimDashboardUrl = DASHBOARD_CLAIMANT_URL.replace(':id', claimId);
-      res.render(paymentUnsuccessfulViewPath, {
-        claimNumber,
-        claimId,
-        claimDashboardUrl,
-      });
+      if (!claim.isEmpty()) {
+        const claimNumber = claim.getFormattedCaseReferenceNumber(claimId);
+        const claimDashboardUrl = DASHBOARD_CLAIMANT_URL.replace(':id', claimId);
+        res.render(paymentUnsuccessfulViewPath, {
+          claimNumber,
+          claimId,
+          claimDashboardUrl,
+        });
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
+  })();
 });
 
 export default paymentUnsuccessfulController;
