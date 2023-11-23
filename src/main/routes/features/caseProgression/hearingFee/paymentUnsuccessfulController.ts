c1dd450a6435a9b85eaedb066dcf6222cf17300f
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2023-11-23 15:08:27 +0000

    CIV-7824: CIV-7824:Changes related to code review comments

--- src/main/routes/features/caseProgression/hearingFee/paymentUnsuccessfulController.ts
+++ src/main/routes/features/caseProgression/hearingFee/paymentUnsuccessfulController.ts
@@ -1,9 +1,12 @@
-import {NextFunction, Router} from 'express';
+import {NextFunction, RequestHandler, Response, Router} from 'express';
 import {
   DASHBOARD_CLAIMANT_URL,
   PAY_HEARING_FEE_UNSUCCESSFUL_URL,
 } from 'routes/urls';
 import {getClaimById} from 'modules/utilityService';
+import {AppRequest} from 'common/models/AppRequest';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+
 
 const paymentUnsuccessfulViewPath = 'features/caseProgression/hearingFee/payment-unsuccessful';
 const paymentUnsuccessfulController: Router = Router();
@@ -13,20 +16,23 @@ paymentUnsuccessfulController.get(PAY_HEARING_FEE_UNSUCCESSFUL_URL, (req, res, n
     try {
       const claimId = req.params.id;
       const claim = await getClaimById(claimId, req, true);
-
-      if (!claim.isEmpty()) {
-        const claimNumber = claim.getFormattedCaseReferenceNumber(claimId);
-        const claimDashboardUrl = DASHBOARD_CLAIMANT_URL.replace(':id', claimId);
-        res.render(paymentUnsuccessfulViewPath, {
-          claimNumber,
-          claimId,
-          claimDashboardUrl,
-        });
-      }
+      const claimNumber = claim.getFormattedCaseReferenceNumber(claimId);
+      const claimDashboardUrl = DASHBOARD_CLAIMANT_URL.replace(':id', claimId);
+      res.render(paymentUnsuccessfulViewPath, {
+        claimNumber,
+        claimDashboardUrl,
+      });
     } catch (error) {
       next(error);
     }
   })();
 });
 
+paymentUnsuccessfulController.post(PAY_HEARING_FEE_UNSUCCESSFUL_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
+  try {
+    res.redirect(constructResponseUrlWithIdParams(req.params.id, DASHBOARD_CLAIMANT_URL));
+  } catch (error) {
+    next(error);
+  }
+}) as RequestHandler);
 export default paymentUnsuccessfulController;
