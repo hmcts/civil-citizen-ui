2e1b4692aeaa7ffc6f533f4bf0b7b6c9eaf08a5e
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-11-24 13:33:45 +0000

    CIV-11077 and CIV-11020 testing

--- src/test/functionalTests/tests/response_partAdmit_tests.js
+++ src/test/functionalTests/tests/response_partAdmit_tests.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const config =  require('../../config');
 const ResponseSteps  =  require('../features/response/steps/lipDefendantResponseSteps');
 const LoginSteps =  require('../features/home/steps/login');
@@ -27,105 +28,36 @@ Before(async ({api}) => {
     securityCode = await caseData.respondent1PinToPostLRspec.accessCode;
     console.log('claim number', claimNumber);
     console.log('Security code', securityCode);
-    await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
-    await DashboardSteps.VerifyClaimOnDashboard(claimNumber);
+    // await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    // await DashboardSteps.VerifyClaimOnDashboard(claimNumber);
   }else{
-    claimRef = await api.createSpecifiedClaimLRvLR(config.applicantSolicitorUser);
-    console.log('claimRef has been created Successfully    <===>  '  , claimRef);
-    await LoginSteps.EnterUserCredentials(config.defendantLRCitizenUser.email, config.defendantLRCitizenUser.password);
+    // claimRef = await api.createSpecifiedClaimLRvLR(config.applicantSolicitorUser);
+    // console.log('claimRef has been created Successfully    <===>  '  , claimRef);
+    // await LoginSteps.EnterUserCredentials(config.defendantLRCitizenUser.email, config.defendantLRCitizenUser.password);
   }
 });
 
-Scenario('Response with PartAdmit-AlreadyPaid @citizenUI @partAdmit @regression @nightly', async ({api}) => {
-  await ResponseSteps.RespondToClaim(claimRef);
-  await ResponseSteps.EnterPersonalDetails(claimRef);
-  await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
-  await ResponseSteps.EnterResponseToClaim(claimRef, partAdmit);
-  await ResponseSteps.SelectPartAdmitAlreadyPaid('yes');
-  await ResponseSteps.EnterHowMuchYouHavePaid(claimRef, 500, partAdmit);
-  await ResponseSteps.EnterWhyYouDisagreeTheClaimAmount(claimRef, partAdmit);
-  await ResponseSteps.AddYourTimeLineEvents();
-  await ResponseSteps.EnterYourEvidenceDetails();
-  await ResponseSteps.EnterFreeTelephoneMediationDetails(claimRef);
-  await ResponseSteps.EnterDQForSmallClaims(claimRef);
-  await ResponseSteps.CheckAndSubmit(claimRef, partAdmit);
-  if (['preview', 'demo'  ].includes(config.runningEnv)) {
-    // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-9655
-    // await api.enterBreathingSpace(config.applicantSolicitorUser);
-    // await api.liftBreathingSpace(config.applicantSolicitorUser);
-    await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.partAdmitAmountPaid, config.claimState.JUDICIAL_REFERRAL);
-    await api.createSDO(config.judgeUserWithRegionId3, config.sdoSelectionType.judgementSumSelectedYesAssignToSmallClaimsNoDisposalHearing);
-  }
+Scenario('Response with PartAdmit-AlreadyPaid @citizenUI @partAdmit @regression @123', async () => {
+  // await ResponseSteps.RespondToClaim(claimRef);
+  // await ResponseSteps.EnterPersonalDetails(claimRef);
+  // await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
+  // await ResponseSteps.EnterResponseToClaim(claimRef, partAdmit);
+  // await ResponseSteps.SelectPartAdmitAlreadyPaid('yes');
+  // await ResponseSteps.EnterHowMuchYouHavePaid(claimRef, 500, partAdmit);
+  // await ResponseSteps.EnterWhyYouDisagreeTheClaimAmount(claimRef, partAdmit);
+  // await ResponseSteps.AddYourTimeLineEvents();
+  // await ResponseSteps.EnterYourEvidenceDetails();
+  // await ResponseSteps.EnterFreeTelephoneMediationDetails(claimRef);
+  // await ResponseSteps.EnterDQForSmallClaims(claimRef);
+  // await ResponseSteps.CheckAndSubmit(claimRef, partAdmit);
+  // if (['preview', 'demo'  ].includes(config.runningEnv)) {
+  //   // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-9655
+  //   // await api.enterBreathingSpace(config.applicantSolicitorUser);
+  //   // await api.liftBreathingSpace(config.applicantSolicitorUser);
+  //   await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.partAdmitAmountPaid, config.claimState.JUDICIAL_REFERRAL);
+  //   await api.createSDO(config.judgeUserWithRegionId3, config.sdoSelectionType.judgementSumSelectedYesAssignToSmallClaimsNoDisposalHearing);
+  // }
 });
 
-Scenario('Response with PartAdmit-havent paid and Immediate payment @citizenUI @partAdmit @regression @nightly', async ({api}) => {
-  await ResponseSteps.RespondToClaim(claimRef);
-  await ResponseSteps.EnterPersonalDetails(claimRef);
-  await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
-  await ResponseSteps.EnterResponseToClaim(claimRef, partAdmit);
-  await ResponseSteps.SelectPartAdmitAlreadyPaid('no');
-  await ResponseSteps.EnterHowMuchMoneyYouOwe(claimRef, 500, partAdmit);
-  await ResponseSteps.EnterWhyYouDisagreeTheClaimAmount(claimRef, partAdmit);
-  await ResponseSteps.AddYourTimeLineEvents();
-  await ResponseSteps.EnterYourEvidenceDetails();
-  await ResponseSteps.EnterPaymentOption(claimRef, partAdmit, immediatePayment);
-  await ResponseSteps.EnterFreeTelephoneMediationDetails(claimRef);
-  await ResponseSteps.EnterDQForSmallClaims(claimRef);
-  await ResponseSteps.CheckAndSubmit(claimRef, partAdmit);
-  if (['preview', 'demo'  ].includes(config.runningEnv)) {
-    // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-9655
-    // await api.enterBreathingSpace(config.applicantSolicitorUser);
-    // await api.liftBreathingSpace(config.applicantSolicitorUser);
-    await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.partAdmitHaventPaidPartiallyWantsToPayImmediately, config.claimState.IN_MEDIATION);
-    await api.mediationSuccessful(config.caseWorker);
-  }
-});
 
-Scenario('Response with PartAdmit and Date to PayOn @citizenUI @partAdmit @regression @nightly', async ({api}) => {
-  await ResponseSteps.RespondToClaim(claimRef);
-  await ResponseSteps.EnterPersonalDetails(claimRef);
-  await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
-  await ResponseSteps.EnterResponseToClaim(claimRef, partAdmit);
-  await ResponseSteps.SelectPartAdmitAlreadyPaid('no');
-  await ResponseSteps.EnterHowMuchMoneyYouOwe(claimRef, 500, partAdmit);
-  await ResponseSteps.EnterWhyYouDisagreeTheClaimAmount(claimRef, partAdmit);
-  await ResponseSteps.AddYourTimeLineEvents();
-  await ResponseSteps.EnterYourEvidenceDetails();
-  await ResponseSteps.EnterPaymentOption(claimRef, partAdmit, bySetDate);
-  await ResponseSteps.EnterDateToPayOn();
-  await ResponseSteps.EnterFinancialDetails(claimRef);
-  await ResponseSteps.EnterFreeTelephoneMediationDetails(claimRef);
-  await ResponseSteps.EnterDQForSmallClaims(claimRef);
-  await ResponseSteps.CheckAndSubmit(claimRef, partAdmit);
-  if (['preview', 'demo'  ].includes(config.runningEnv)) {
-    // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-9655
-    // await api.enterBreathingSpace(config.applicantSolicitorUser);
-    // await api.liftBreathingSpace(config.applicantSolicitorUser);
-    await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.partAdmitWithPartPaymentOnSpecificDate, config.claimState.PROCEEDS_IN_HERITAGE_SYSTEM);
-  }
-});
 
-Scenario('Response with PartAdmit and Repayment plan @citizenUI @partAdmit @nightly @regression', async () => {
-  await ResponseSteps.RespondToClaim(claimRef);
-  await ResponseSteps.EnterPersonalDetails(claimRef);
-  await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
-  await ResponseSteps.EnterResponseToClaim(claimRef, partAdmit);
-  await ResponseSteps.SelectPartAdmitAlreadyPaid('no');
-  await ResponseSteps.EnterHowMuchMoneyYouOwe(claimRef, 500, partAdmit);
-  await ResponseSteps.EnterWhyYouDisagreeTheClaimAmount(claimRef, partAdmit);
-  await ResponseSteps.AddYourTimeLineEvents();
-  await ResponseSteps.EnterYourEvidenceDetails();
-  await ResponseSteps.EnterPaymentOption(claimRef, partAdmit, repaymentPlan);
-  await ResponseSteps.EnterRepaymentPlan(claimRef);
-  await ResponseSteps.EnterFinancialDetails(claimRef);
-  await ResponseSteps.EnterFreeTelephoneMediationDetails(claimRef);
-  await ResponseSteps.EnterDQForSmallClaims(claimRef);
-  await ResponseSteps.CheckAndSubmit(claimRef, partAdmit);
-  if (['preview', 'demo'  ].includes(config.runningEnv)) {
-    // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-9655
-    // await api.enterBreathingSpace(config.applicantSolicitorUser);
-    // await api.liftBreathingSpace(config.applicantSolicitorUser);
-    // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-10659
-    // await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.partAdmitWithPartPaymentAsPerInstallmentPlan, config.claimState.PROCEEDS_IN_HERITAGE_SYSTEM);
-  }
-});
