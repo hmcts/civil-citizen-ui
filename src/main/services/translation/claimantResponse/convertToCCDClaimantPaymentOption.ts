ce460a73ad0605e05c99e412bdac3d4cf845ea8e
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2023-11-30 10:17:09 +0000

    build issue fixed

--- src/main/services/translation/claimantResponse/convertToCCDClaimantPaymentOption.ts
+++ src/main/services/translation/claimantResponse/convertToCCDClaimantPaymentOption.ts
@@ -2,10 +2,10 @@ import {PaymentOptionType} from 'common/form/models/admission/paymentOption/paym
 import {ClaimantResponse} from 'models/claimantResponse';
 
 export const toCCDClaimantSuggestedPayByInstalments = (claimantResponse?: ClaimantResponse): boolean => {
-    return claimantResponse?.suggestedPaymentIntention?.paymentOption === PaymentOptionType.INSTALMENTS;
+  return claimantResponse?.suggestedPaymentIntention?.paymentOption === PaymentOptionType.INSTALMENTS;
 };
 
 export const toCCDClaimantSuggestedPayByDate = (claimantResponse?: ClaimantResponse): boolean => {
-    return claimantResponse?.suggestedPaymentIntention?.paymentOption === PaymentOptionType.BY_SET_DATE;
+  return claimantResponse?.suggestedPaymentIntention?.paymentOption === PaymentOptionType.BY_SET_DATE;
 };
 
