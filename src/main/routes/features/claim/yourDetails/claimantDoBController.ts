c62855dece8160cd22c34e4745a4e77d3f7f1d1d
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-11-30 19:31:17 +0000

    CIV-11895 citizen date for claimant

--- src/main/routes/features/claim/yourDetails/claimantDoBController.ts
+++ src/main/routes/features/claim/yourDetails/claimantDoBController.ts
@@ -3,9 +3,11 @@ import {CLAIMANT_DOB_URL, CLAIMANT_PHONE_NUMBER_URL} from '../../../urls';
 import {GenericForm} from '../../../../common/form/models/genericForm';
 import {CitizenDate} from '../../../../common/form/models/claim/claimant/citizenDate';
 import {Claim} from '../../../../common/models/claim';
-import {getCaseDataFromStore} from '../../../../modules/draft-store/draftStoreService';
+import {
+  getCaseDataFromStore,
+  saveDraftClaim
+} from '../../../../modules/draft-store/draftStoreService';
 import {AppRequest} from '../../../../common/models/AppRequest';
-import {saveClaimantProperty} from '../../../../../main/services/features/claim/yourDetails/claimantDetailsService';
 
 const claimantDoBController = Router();
 const claimantDoBViewPath = 'features/response/citizenDob/citizen-dob';
@@ -35,7 +37,9 @@ claimantDoBController.post(CLAIMANT_DOB_URL, async (req: AppRequest | Request, r
     if (form.hasErrors()) {
       res.render(claimantDoBViewPath, {form, today: new Date(), claimantView: true});
     } else {
-      await saveClaimantProperty(claimId, 'dateOfBirth', form.model.date);
+      const claim = await getCaseDataFromStore(claimId);
+      claim.applicant1.dateOfBirth =new CitizenDate(day, month, year);;
+      await saveDraftClaim(claimId, claim);
       res.redirect(CLAIMANT_PHONE_NUMBER_URL);
     }
   } catch (error) {
