ef5481601bf9508aa0548b18960e170caf0a9ad0
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-12-05 09:20:24 +0000

    added route when defendant is company or not

--- src/main/routes/urls.ts
+++ src/main/routes/urls.ts
@@ -78,6 +78,7 @@ export const CITIZEN_ALREADY_PAID_URL = `${PARTIAL_ADMISSION_URL}/already-paid`;
 export const CITIZEN_PARTIAL_ADMISSION_PAYMENT_OPTION_URL = `${PARTIAL_ADMISSION_URL}/payment-option`;
 export const CITIZEN_AMOUNT_YOU_PAID_URL = `${PARTIAL_ADMISSION_URL}/how-much-have-you-paid`;
 export const MEDIATION_DISAGREEMENT_URL = `${MEDIATION_URL}/mediation-disagreement`;
+export const MEDIATION_CONTACT_PERSON_CONFIRMATION_URL = `${MEDIATION_URL}/mediation-contact-person`;
 export const MEDIATION_ALTERNATIVE_EMAIL_URL = `${MEDIATION_URL}/alternative-email`;
 export const MEDIATION_ALTERNATIVE_PHONE_URL = `${MEDIATION_URL}/alternative-phone`;
 export const MEDIATION_NEXT_3_MONTHS_URL = `${MEDIATION_URL}/next-three-months`;
