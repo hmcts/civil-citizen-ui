6f74332f8c6fc58d76e0ed7eb9230da91a9bdff4
rhysbeazleyy <Rhys.Beazley@HMCTS.NET>
2023-12-05 16:34:36 +0000

    Update uploadYourDocumentsController.ts

--- src/main/routes/features/caseProgression/uploadYourDocumentsController.ts
+++ src/main/routes/features/caseProgression/uploadYourDocumentsController.ts
@@ -2,14 +2,16 @@ import {NextFunction, RequestHandler, Router} from 'express';
 import {UPLOAD_YOUR_DOCUMENTS_URL} from '../../urls';
 import {getUploadYourDocumentsContents} from 'services/features/caseProgression/uploadYourDocumentsContents';
 import {getClaimById} from 'modules/utilityService';
+import {AppRequest} from 'common/models/AppRequest';
 
 const uploadYourDocumentsViewPath = 'features/caseProgression/upload-your-documents';
 const uploadYourDocumentsController = Router();
 
-uploadYourDocumentsController.get([UPLOAD_YOUR_DOCUMENTS_URL], (async (req, res, next: NextFunction) => {
+uploadYourDocumentsController.get([UPLOAD_YOUR_DOCUMENTS_URL], (async (req: AppRequest, res, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const claim = await getClaimById(claimId, req);
+    req.session.previousUrl = req.originalUrl;
     res.render(uploadYourDocumentsViewPath, {uploadYourDocumentsContents:getUploadYourDocumentsContents(claimId, claim)});
   } catch (error) {
     next(error);
