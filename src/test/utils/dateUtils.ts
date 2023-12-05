11ff9c9a7f6102caaa1e1c86f0a8bad506342269
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-12-05 08:01:03 +0000

    fixed uni task

--- src/test/utils/dateUtils.ts
+++ src/test/utils/dateUtils.ts
@@ -5,7 +5,7 @@ export const FIXED_DATE = new Date(2023, 3, 26);
 export const FIXED_TIME_HOUR_MINUTE = '1000';
 
 export const CURRENT_DATE = new Date();
-export const CURRENT_DAY = CURRENT_DATE.getDay();
-export const CURRENT_DAY_PLUS_1 = CURRENT_DATE.getDay() + 1;
+export const CURRENT_DAY = CURRENT_DATE.getDate();
+export const CURRENT_DAY_PLUS_1 = CURRENT_DAY + 1;
 export const CURRENT_MONTH = CURRENT_DATE.getMonth() + 1;
 export const CURRENT_YEAR = CURRENT_DATE.getFullYear();
