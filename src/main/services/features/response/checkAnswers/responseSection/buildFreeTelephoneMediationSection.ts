da4f58678c8e9c546757590df320d668a2731a90
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2023-12-14 14:59:02 +0000

    added the labels

--- src/main/services/features/response/checkAnswers/responseSection/buildFreeTelephoneMediationSection.ts
+++ src/main/services/features/response/checkAnswers/responseSection/buildFreeTelephoneMediationSection.ts
@@ -7,7 +7,15 @@ import {getLng} from '../../../../../common/utils/languageToggleUtils';
 import {
   CAN_WE_USE_COMPANY_URL,
   CAN_WE_USE_URL,
-  CITIZEN_FREE_TELEPHONE_MEDIATION_URL, DQ_AVAILABILITY_DATES_FOR_HEARING_URL,
+  CITIZEN_FREE_TELEPHONE_MEDIATION_URL,
+  DQ_AVAILABILITY_DATES_FOR_HEARING_URL,
+  MEDIATION_ALTERNATIVE_CONTACT_PERSON_URL,
+  MEDIATION_ALTERNATIVE_EMAIL_URL,
+  MEDIATION_ALTERNATIVE_PHONE_URL,
+  MEDIATION_CONTACT_PERSON_CONFIRMATION_URL,
+  MEDIATION_EMAIL_CONFIRMATION_URL,
+  MEDIATION_NEXT_3_MONTHS_URL,
+  MEDIATION_PHONE_CONFIRMATION_URL, MEDIATION_UNAVAILABLE_SELECT_DATES_URL,
 } from '../../../../../routes/urls';
 import {YesNo, YesNoUpperCase} from '../../../../../common/form/models/yesNo';
 import {CompanyTelephoneNumber} from 'form/models/mediation/companyTelephoneNumber';
@@ -78,37 +86,47 @@ export const buildFreeTelephoneMediationSection = (claim: Claim, claimId: string
 };
 
 export const buildMediationSection = (claim: Claim, claimId: string, lang: string | unknown): SummarySection => {
+  //TODO create sting labels for all the options
   const mediationSection = summarySection({
     title: t('PAGES.FREE_TELEPHONE_MEDIATION.PAGE_TITLE', {lng: getLng(lang)}),
     summaryRows: [],
   });
-  //yes part
-  //Todo added the hrefs
   mediationSection.summaryList.rows.push(summaryRow(t('PAGES.MEDIATION_CONTACT_PERSON_CONFIRMATION.PAGE_TEXT_DEFENDANT', {lng: getLng(lang), defendantContactPerson: claim.respondent1.partyDetails.contactPerson}),
     t(`COMMON.VARIATION_2.${claim.mediation.isMediationContactNameCorrect.option.toUpperCase()}`, {lng: getLng(lang)}),
-    'freeMediationHref', changeLabel(lang)));
+    constructResponseUrlWithIdParams(claimId,MEDIATION_CONTACT_PERSON_CONFIRMATION_URL), changeLabel(lang)));
+  if(claim.mediation.isMediationContactNameCorrect.option === YesNo.NO){
+    mediationSection.summaryList.rows.push(summaryRow(t('COMMON.CONTACT_NAME', {lng: getLng(lang)}),
+      claim.mediation.alternativeMediationContactPerson.alternativeContactPerson,
+      constructResponseUrlWithIdParams(claimId,MEDIATION_ALTERNATIVE_CONTACT_PERSON_URL), changeLabel(lang)));
+  }
+
   mediationSection.summaryList.rows.push(summaryRow(t('PAGES.MEDIATION_PHONE_CONFIRMATION.PAGE_TEXT', {lng: getLng(lang), defendantPhone: claim.respondent1.partyPhone.phone}),
     t(`COMMON.VARIATION_2.${claim.mediation.isMediationPhoneCorrect.option.toUpperCase()}`, {lng: getLng(lang)}),
-    'freeMediationHref', changeLabel(lang)));
+    constructResponseUrlWithIdParams(claimId,MEDIATION_PHONE_CONFIRMATION_URL), changeLabel(lang)));
+  if(claim.mediation.isMediationPhoneCorrect.option === YesNo.NO){
+    mediationSection.summaryList.rows.push(summaryRow(t('COMMON.CONTACT_NUMBER', {lng: getLng(lang)}),
+      claim.mediation.alternativeMediationTelephone.alternativeTelephone,
+      constructResponseUrlWithIdParams(claimId,MEDIATION_ALTERNATIVE_PHONE_URL), changeLabel(lang)));
+  }
 
   mediationSection.summaryList.rows.push(summaryRow(t('PAGES.MEDIATION_EMAIL_CONFIRMATION.PAGE_TEXT', {lng: getLng(lang), defendantEmail: claim.respondent1.emailAddress.emailAddress}),
     t(`COMMON.VARIATION_2.${claim.mediation.isMediationEmailCorrect.option.toUpperCase()}`, {lng: getLng(lang)}),
-    'freeMediationHref', changeLabel(lang)));
+    constructResponseUrlWithIdParams(claimId,MEDIATION_EMAIL_CONFIRMATION_URL), changeLabel(lang)));
   if(claim.mediation.isMediationEmailCorrect.option === YesNo.NO){
-    mediationSection.summaryList.rows.push(summaryRow(t('PAGES.MEDIATION_EMAIL_CONFIRMATION.PAGE_TEXT', {lng: getLng(lang), defendantEmail: claim.respondent1.emailAddress.emailAddress}),
-      t(`COMMON.VARIATION_2.${claim.mediation.alternativeMediationEmail}`, {lng: getLng(lang)}),
-      'freeMediationHref', changeLabel(lang)));
+    mediationSection.summaryList.rows.push(summaryRow(t('COMMON.CONTACT_EMAIL', {lng: getLng(lang)}),
+      claim.mediation.alternativeMediationEmail.alternativeEmailAddress,
+      constructResponseUrlWithIdParams(claimId,MEDIATION_ALTERNATIVE_EMAIL_URL), changeLabel(lang)));
   }
 
   mediationSection.summaryList.rows.push(summaryRow(t('PAGES.UNAVAILABILITY_NEXT_THREE_MONTHS_MEDIATION_CONFIRMATION.PAGE_TEXT', {lng: getLng(lang)}),
     t(`COMMON.VARIATION_2.${claim.mediation.hasUnavailabilityNextThreeMonths.option.toUpperCase()}`, {lng: getLng(lang)}),
-    'freeMediationHref', changeLabel(lang)));
+    constructResponseUrlWithIdParams(claimId,MEDIATION_NEXT_3_MONTHS_URL), changeLabel(lang)));
   if(claim.mediation.hasUnavailabilityNextThreeMonths.option === YesNo.YES){
     const hasUnavailableDatesMediation = getListOfUnavailableDate(claim.mediation.unavailableDatesForMediation, getLng(lang));
     mediationSection.summaryList.rows.push(summaryRow(
       t('PAGES.CANT_ATTEND_HEARING_IN_NEXT_12MONTHS.UNAVAILABLE_DATES', {lang}),
       ` ${[...hasUnavailableDatesMediation].join('<br>')}`,
-      constructResponseUrlWithIdParams(claimId, DQ_AVAILABILITY_DATES_FOR_HEARING_URL),
+      constructResponseUrlWithIdParams(claimId,MEDIATION_UNAVAILABLE_SELECT_DATES_URL),
       changeLabel(lang),
     ));
   }
