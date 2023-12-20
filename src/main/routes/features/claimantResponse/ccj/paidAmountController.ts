b1e90bf9cdfa2b73408f3f6f7c295e8f5ae051ac
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2023-12-20 16:17:10 +0100

    CIV-11812 : Typo fix and fix code analysis warning
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/routes/features/claimantResponse/ccj/paidAmountController.ts
+++ src/main/routes/features/claimantResponse/ccj/paidAmountController.ts
@@ -4,12 +4,12 @@ import {
   CCJ_EXTENDED_PAID_AMOUNT_URL,
   CCJ_PAID_AMOUNT_SUMMARY_URL,
   CCJ_PAID_AMOUNT_URL,
-} from '../../../urls';
-import {GenericForm} from '../../../../common/form/models/genericForm';
-import {constructResponseUrlWithIdParams} from '../../../../common/utils/urlFormatter';
-import {getClaimantResponse, saveClaimantResponse} from '../../../../services/features/claimantResponse/claimantResponseService';
-import {PaidAmount} from '../../../../common/models/claimantResponse/ccj/paidAmount';
-import { generateRedisKey, getCaseDataFromStore } from '../../../../modules/draft-store/draftStoreService';
+} from 'routes/urls';
+import {GenericForm} from 'form/models/genericForm';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+import {getClaimantResponse, saveClaimantResponse} from 'services/features/claimantResponse/claimantResponseService';
+import {PaidAmount} from 'models/claimantResponse/ccj/paidAmount';
+import { generateRedisKey, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
 import { AppRequest } from 'common/models/AppRequest';
 import {getClaimById} from 'modules/utilityService';
 
