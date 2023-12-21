4604b70c370c2238a9f9eead5a5b840aa30989ba
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2023-12-21 11:52:51 +0000

    CIV-7829 adding test cases

--- src/test/unit/services/features/caseProgression/dashboardService.test.ts
+++ src/test/unit/services/features/caseProgression/dashboardService.test.ts
@@ -34,6 +34,35 @@ describe('Dashboard Service', () => {
     expect(taskList[0].tasks[3].status).toEqual(TaskStatus.DONE);
   });
 
+  it('should show task done due to successful payment status data available in redis ', () => {
+
+    mockClaim = require('../../../../utils/mocks/civilClaimResponseMock.json');
+    claimWithPaymentStatus = {
+      ...mockClaim,
+      state: CaseState.AWAITING_APPLICANT_INTENTION,
+      case_data: {
+        ...mockClaim.case_data,
+        isClaimant: jest.fn().mockReturnValue(true),
+        isLRClaimant: jest.fn(),
+        isLRDefendant: jest.fn(),
+        caseProgression: {
+          hearing: {
+            paymentInformation: {
+              status: 'Success',
+            },
+          },
+        },
+      },
+    };
+    //when
+    const taskList = generateNewDashboard(claimWithPaymentStatus.case_data);
+
+    //Then
+    expect(taskList.length).toEqual(2);
+    expect(taskList[0].tasks[3].description).toEqual('PAGES.DASHBOARD.HEARINGS.PAY_FEE');
+    expect(taskList[0].tasks[3].status).toEqual(TaskStatus.DONE);
+  });
+
   it('should show task not available due to failed payment status', () => {
     mockClaim = require('../../../../utils/mocks/civilClaimResponseMock.json');
     claimWithPaymentStatus = {
