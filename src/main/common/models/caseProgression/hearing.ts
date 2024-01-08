0abfef4431e48540a9f8e7e518d7902c613dc992
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-01-08 12:48:21 +0000

    CIV-7829 review comments

--- src/main/common/models/caseProgression/hearing.ts
+++ src/main/common/models/caseProgression/hearing.ts
@@ -2,5 +2,5 @@ import {PaymentInformation} from 'models/feePayment/paymentInformation';
 
 export class Hearing {
   paymentInformation? : PaymentInformation;
-  helpWithFeesRequested: string;
+  helpWithFeesRequested?: string;
 }
