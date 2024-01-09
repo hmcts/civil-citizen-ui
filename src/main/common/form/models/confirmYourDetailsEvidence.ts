8dd292e3ea8a51d2ea1b4967d41cfd092be94b4e
hmcts-version1-domini <domini.mattos@version1.com>
2024-01-09 16:42:27 +0100

    fix lint

--- src/main/common/form/models/confirmYourDetailsEvidence.ts
+++ src/main/common/form/models/confirmYourDetailsEvidence.ts
@@ -2,22 +2,22 @@ import {IsEmail, IsNotEmpty, Validate, ValidateIf} from 'class-validator';
 import {OptionalIntegerValidator} from 'form/validators/optionalIntegerValidator';
 
 export class ConfirmYourDetailsEvidence {
-    @IsNotEmpty({message: 'ERRORS.ENTER_YOUR_FIRST_NAME'})
-    firstName?: string;
+  @IsNotEmpty({message: 'ERRORS.ENTER_YOUR_FIRST_NAME'})
+  firstName?: string;
 
-    @IsNotEmpty({message: 'ERRORS.ENTER_YOUR_LAST_NAME'})
-    lastName?: string;
+  @IsNotEmpty({message: 'ERRORS.ENTER_YOUR_LAST_NAME'})
+  lastName?: string;
 
-    @ValidateIf(o => o.emailAddress)
-    @IsEmail({allow_display_name: true}, {message: 'ERRORS.ENTER_VALID_EMAIL_EVIDENCE'})
-    emailAddress?: string;
+  @ValidateIf(o => o.emailAddress)
+  @IsEmail({allow_display_name: true}, {message: 'ERRORS.ENTER_VALID_EMAIL_EVIDENCE'})
+  emailAddress?: string;
 
-    @ValidateIf(o => o.phoneNumber)
-    @Validate(OptionalIntegerValidator, {message: 'ERRORS.VALID_PHONE_NUMBER_EVIDENCE'})
-    phoneNumber?: number;
+  @ValidateIf(o => o.phoneNumber)
+  @Validate(OptionalIntegerValidator, {message: 'ERRORS.VALID_PHONE_NUMBER_EVIDENCE'})
+  phoneNumber?: number;
 
-    @IsNotEmpty({message: 'ERRORS.ENTER_YOUR_JOB_TITLE'})
-    jobTitle: string;
+  @IsNotEmpty({message: 'ERRORS.ENTER_YOUR_JOB_TITLE'})
+  jobTitle: string;
 
   constructor(firstName?: string, lastName?: string, emailAddress?: string, phoneNumber?: number, jobTitle?: string) {
     this.firstName = firstName;
