1e9c0a23dabd8d4507bbcdf68d54f9cfc3901455
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-01-11 12:44:00 +0100

    Fix: welsh enum

--- src/main/services/features/caseProgression/trialArrangements/hearingDurationAndOtherInformation.ts
+++ src/main/services/features/caseProgression/trialArrangements/hearingDurationAndOtherInformation.ts
@@ -5,7 +5,7 @@ import {
 } from 'common/models/caseProgression/trialArrangements/finaliseYourTrialSectionBuilder';
 import {HearingDurationFormatter} from 'services/features/caseProgression/hearingDurationFormatter';
 
-export const getHearingDurationAndOtherInformation = (claim: Claim, caseIdPrettified?: string) => {
+export const getHearingDurationAndOtherInformation = (claim: Claim, lng: string, caseIdPrettified?: string) => {
 
   return new FinaliseYourTrialSectionBuilder()
     .addMainTitle(t('PAGES.TRIAL_DURATION_TRIAL_ARRANGEMENTS.TITLE'))
@@ -15,7 +15,7 @@ export const getHearingDurationAndOtherInformation = (claim: Claim, caseIdPretti
       defendantName: claim.getDefendantFullName(),
     })
     .addTitle('PAGES.TRIAL_DURATION_TRIAL_ARRANGEMENTS.TRIAL_DURATION_TITLE')
-    .addParagraphWithHTML(t('PAGES.TRIAL_DURATION_TRIAL_ARRANGEMENTS.TRIAL_DURATION_PARAGRAPH', {hearingDuration: HearingDurationFormatter.formatHearingDuration(claim.caseProgressionHearing.hearingDuration)}))
+    .addParagraphWithHTML(t('PAGES.TRIAL_DURATION_TRIAL_ARRANGEMENTS.TRIAL_DURATION_PARAGRAPH', {lng, hearingDuration: HearingDurationFormatter.formatHearingDuration(claim.caseProgressionHearing.hearingDuration, lng),}))
     .addParagraph('PAGES.TRIAL_DURATION_TRIAL_ARRANGEMENTS.REQUIRE_LESS_TIME')
     .addInsetText('PAGES.TRIAL_DURATION_TRIAL_ARRANGEMENTS.REQUIRE_MORE_TIME')
     .addTitle('PAGES.TRIAL_DURATION_TRIAL_ARRANGEMENTS.OTHER_INFORMATION_TITLE')
