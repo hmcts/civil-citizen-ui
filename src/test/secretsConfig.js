2e94f3938b00508e13dcf2a7bb681b01c0211ec1
skhajuria <sankhajuria@gmail.com>
2024-01-11 12:15:06 +0000

    Add new keys in the TF

--- src/test/secretsConfig.js
+++ src/test/secretsConfig.js
@@ -1,9 +1,6 @@
 const config = require('config');
 const {get, set} = require('lodash');
 const propertiesVolume = require('@hmcts/properties-volume');
-const {Logger} = require('@hmcts/nodejs-logging');
-const logger = Logger.getLogger('secretsConfig');
-
 
 const PropertiesVolume = {
   enableFor: (server) => {
@@ -17,7 +14,6 @@ const PropertiesVolume = {
       PropertiesVolume.setSecret('secrets.civil-citizen-ui.citizen-draft-store-secondary', 'services.draftStore.legacy.s2s.secondarySecret');
       PropertiesVolume.setSecret('secrets.civil-citizen-ui.cmc-s2s-secret', 'services.serviceAuthProvider.cmcS2sSecret');
       PropertiesVolume.setSecret('secrets.civil-citizen-ui.civil-citizen-ui-token-key', 'services.pcq.tokenKey');
-      logger.info('Evn is >>>>>>>>>'+server.locals.ENV);
       if (server.locals.ENV === 'production') {
         PropertiesVolume.setSecret('secrets.civil-cui.launch-darkly-sdk-key', 'launchDarkly.sdk');
       } else {
