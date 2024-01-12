68f44489b7252cb8ad33acb8e7b425d887bd834b
jeswanth <jeswanth.reddy@hmcts.net>
2024-01-12 17:56:07 +0000

    mocked the launchdarkly-node-server-sdk module

--- jest.setup.redis-mock.js
+++ jest.setup.redis-mock.js
@@ -1,2 +1,20 @@
+const ldClientMock = {
+  track: jest.fn(),
+  identify: jest.fn(),
+  allFlags: jest.fn(),
+  close: jest.fn(),
+  flush: jest.fn(),
+  getContext: jest.fn(),
+  off: jest.fn(),
+  on: jest.fn(),
+  setStreaming: jest.fn(),
+  variation: jest.fn(),
+  variationDetail: jest.fn(),
+  waitForInitialization: jest.fn(),
+  waitUntilGoalsReady: jest.fn(),
+  waitUntilReady: jest.fn(),
+};
 jest.mock('ioredis', () => jest.requireActual('ioredis-mock'));
-jest.mock('./src/main/app/auth/launchdarkly/launchDarklyClient');
+jest.mock('launchdarkly-node-server-sdk', () => ({
+  init: jest.fn().mockReturnValue(ldClientMock),
+}));
