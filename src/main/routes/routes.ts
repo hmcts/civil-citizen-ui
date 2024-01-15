2f96608244658a498e8cb405097665d370b10de4
rhysbeazleyy <Rhys.Beazley@HMCTS.NET>
2024-01-15 16:59:26 +0000

    first commit

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -264,6 +264,7 @@ import claimBilingualLangPreferenceController from 'routes/features/claim/biling
 import paymentSuccessfulController from 'routes/features/claim/paymentSuccessfulController';
 import alternativeContactPersonMediationController
   from 'routes/features/mediation/alternativeContactPersonMediationController';
+import liPvLiPClaimCreationController from 'routes/features/claim/liPvLiPClaimCreationController';
 export default [
   homeController,
   dashboardController,
@@ -498,4 +499,5 @@ export default [
   alternativeTelephoneMediationController,
   paymentSuccessfulController,
   alternativeContactPersonMediationController,
+  liPvLiPClaimCreationController,
 ];
