aa3ab4f457bc7bec4f6548e63bde0b562cba55da
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2024-01-15 12:41:06 +0000

    Update civilServiceClient.ts

--- src/main/app/client/civilServiceClient.ts
+++ src/main/app/client/civilServiceClient.ts
@@ -315,7 +315,6 @@ export class CivilServiceClient {
       const response: AxiosResponse<object> = await this.client.post(CIVIL_SERVICE_SUBMIT_EVENT // nosonar
         .replace(':submitterId', userId)
         .replace(':caseId', claimId), data, config);// nosonar
-   
       const claimResponse = response.data as CivilClaimResponse;
       return convertCaseToClaim(claimResponse);
     } catch (err: unknown) {
