76d73fb1eb4e41f984e2b4b9d62251939fe696c5
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-01-16 16:33:05 +0000

    my configurations

--- src/test/config.js
+++ src/test/config.js
@@ -1,10 +1,11 @@
 
 const PropertiesVolume = require('./secretsConfig');
 
-const defaultPassword = process.env.CITIZEN_PASSWORD;
+const defaultPassword = process.env.CITIZEN_PASSWORD || 'Password12!';
 const defaultPasswordSystemUser = process.env.SYSTEM_USER_PASSWORD;
 const judgeDefaultPassword = process.env.JUDGE_PASSWORD;
-const testUrl = process.env.TEST_URL || 'https://moneyclaims.demo.platform.hmcts.net';
+// const testUrl = process.env.TEST_URL || 'https://moneyclaims.demo.platform.hmcts.net';
+const testUrl = process.env.TEST_URL || 'https://civil-citizen-ui-pr-2923.preview.platform.hmcts.net';
 const testHeadlessBrowser = process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true;
 
 if (!process.env.TEST_PASSWORD) {
@@ -13,7 +14,7 @@ if (!process.env.TEST_PASSWORD) {
 module.exports = {
   TestUrl: testUrl,
   env: process.env.ENVIRONMENT_NAME || 'local',
-  TestHeadlessBrowser: testHeadlessBrowser,
+  TestHeadlessBrowser: false,
   TestSlowMo: 250,
   WaitForTimeout: 20000,
   WaitForText: 60,
@@ -22,23 +23,23 @@ module.exports = {
     url: 'http://localhost:5555',
   },
   url: {
-    /*manageCase: process.env.URL || 'https://xui-civil-citizen-ui-pr-3591.preview.platform.hmcts.net',
+    manageCase: process.env.URL || 'https://xui-civil-citizen-ui-pr-2923.preview.platform.hmcts.net',
     authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
     ccdDataStore: process.env.CCD_DATA_STORE_URL || 'https://ccd-data-store-api-civil-citizen-ui-pr-3591.preview.platform.hmcts.net',
     dmStore:process.env.DM_STORE_URL || 'http://dm-store-aat.service.core-compute-aat.internal',
     idamApi:  'https://idam-api.aat.platform.hmcts.net',
-    civilService: process.env.CIVIL_SERVICE_URL || 'https://civil-citizen-ui-pr-3591-civil-service.preview.platform.hmcts.net',*/
-    manageCase: process.env.URL || 'https://manage-case.demo.platform.hmcts.net/',
-    authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
-    ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
-    dmStore: process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
-    idamApi: process.env.IDAM_API_URL || 'https://idam-api.demo.platform.hmcts.net',
-    idamWeb: process.env.IDAM_WEB_URL || 'https://idam-web-public.demo.platform.hmcts.net',
-    civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal',
-    waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-demo.service.core-compute-demo.internal',
-    generalApplication: process.env.CIVIL_GENERAL_APPLICATIONS_URL || 'http://civil-general-applications-demo.service.core-compute-demo.internal',
-    caseAssignmentService: process.env.AAC_API_URL || 'http://aac-manage-case-assignment-demo.service.core-compute-demo.internal',
-    wiremockServiceE2e: 'http://localhost:1111',
+    idamWeb: 'https://idam-web-public.aat.platform.hmcts.net',
+    civilService: process.env.CIVIL_SERVICE_URL || 'https://civil-citizen-ui-pr-3591-civil-service.preview.platform.hmcts.net',
+    
+    // manageCase: process.env.URL || 'https://manage-case.demo.platform.hmcts.net/',
+    // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
+    // ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
+    // dmStore: process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
+    // idamApi: process.env.IDAM_API_URL || 'https://idam-api.demo.platform.hmcts.net',
+    // idamWeb: process.env.IDAM_WEB_URL || 'https://idam-web-public.demo.platform.hmcts.net',
+    // civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal',
+    // waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-demo.service.core-compute-demo.internal',
+    // generalApplication: process.env.CIVIL_GENERAL_APPLICATIONS_URL || 'http://civil-general-applications-demo.service.core-compute-demo.internal',
   },
   getDomain: (url) => new URL(url).host,
   s2s: {
