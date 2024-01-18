06f2000dd3642eb1031918afb86af4fb646ba2ba
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2024-01-18 17:39:41 +0000

    CIV-11541 Bilingual check on state control

--- src/main/routes/guards/claimantResponseConfirmationGuard.ts
+++ src/main/routes/guards/claimantResponseConfirmationGuard.ts
@@ -4,7 +4,6 @@ import {CLAIMANT_RESPONSE_TASK_LIST_URL} from '../../routes/urls';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {CivilServiceClient} from 'client/civilServiceClient';
 import {AppRequest} from 'common/models/AppRequest';
-import {ClaimBilingualLanguagePreference} from 'models/claimBilingualLanguagePreference';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('claimantResponseConfirmationGuard');
@@ -14,7 +13,7 @@ export const claimantResponseConfirmationGuard = (async (req: Request, res: Resp
     const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
     const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
     const claim = await civilServiceClient.retrieveClaimDetails(req.params?.id, <AppRequest>req);
-    if (claim.isClaimantIntentionPending() && ((!claim.claimantBilingualLanguagePreference) || claim.claimantBilingualLanguagePreference === ClaimBilingualLanguagePreference.ENGLISH)) {
+    if (claim.isClaimantIntentionPending() && !req.originalUrl.includes('claimant-response/confirmation')) {
       logger.info('Redirecting to claimant response task list from ', req.url);
       res.redirect(constructResponseUrlWithIdParams(req.params.id, CLAIMANT_RESPONSE_TASK_LIST_URL));
     } else {
