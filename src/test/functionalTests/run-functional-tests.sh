159ea8de15c33e30bb195018503e473dcb65a30f
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2024-01-18 14:44:53 +0000

    Revert "just carm test"
    This reverts commit e2a9fa9afd288686708cda0845c0283a1bd3beb1.

--- src/test/functionalTests/run-functional-tests.sh
+++ src/test/functionalTests/run-functional-tests.sh
@@ -4,6 +4,7 @@ set -ex
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ] || [ ${ENVIRONMENT} == demo ]; then
+  yarn test:cui-r1
   yarn test:carm
 else
   yarn test:cui-r1
