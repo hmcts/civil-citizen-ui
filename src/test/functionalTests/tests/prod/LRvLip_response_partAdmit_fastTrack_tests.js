4cfe55d4174b95d7acfd7df33858a58cf5cc95e1
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-01-19 14:04:16 +0000

    update

--- src/test/functionalTests/tests/prod/LRvLip_response_partAdmit_fastTrack_tests.js
+++ src/test/functionalTests/tests/prod/LRvLip_response_partAdmit_fastTrack_tests.js
@@ -12,7 +12,7 @@ let caseData;
 let claimNumber;
 let securityCode;
 
-Feature('Response with PartAdmit - FastTrack');
+Feature('Response with PartAdmit - FastTrack @kiyron');
 
 Before(async ({api}) => {
   await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
