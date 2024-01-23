0e05f85ce60b3883c01187b5a4aa826ce5dfe859
rhysbeazleyy <Rhys.Beazley@HMCTS.NET>
2024-01-23 20:19:09 +0000

    Update createClaimDraft.test.ts

--- src/test/unit/routes/features/claim/createClaimDraft.test.ts
+++ src/test/unit/routes/features/claim/createClaimDraft.test.ts
@@ -24,123 +24,123 @@ describe('createDraftClaim Router', () => {
       console.log(response)
     });
 
-  describe('processDraftClaim function', () =>     
-    it('should process the draftClaim correctly', () => {
-      const draftClaim = {
-        case_data: {
-          draftClaimCreatedAt: '2023-12-05T11:33:43.386Z',
-          id: '',
-          resolvingDispute: true,
-          completingClaimConfirmed: true,
-          applicant1: {
-            type: 'INDIVIDUAL',
-            partyDetails: {
-              individualTitle: '',
-              individualLastName: 'ss',
-              individualFirstName: 'ss',
-              provideCorrespondenceAddress: 'no',
-              primaryAddress: {
-                addressLine1: '1234',
-                addressLine2: '',
-                addressLine3: '',
-                city: 'sheffield',
-                postCode: 'S12eu',
-              },
-              correspondenceAddress: {
-                addressLine1: '',
-                addressLine2: '',
-                addressLine3: '',
-                city: '',
-                postCode: '',
-              },
-              carmEnabled: false,
-            },
-            dateOfBirth: {
-              date: '1995-01-01T00:00:00.000Z',
-              year: 1995,
-              month: 1,
-              day: 1,
-            },
-            partyPhone: {},
-            emailAddress: {
-              emailAddress: 'citizen.user1@gmail.com',
-            },
-          },
-          respondent1: {
-            type: 'INDIVIDUAL',
-            partyDetails: {
-              individualTitle: '',
-              individualLastName: 'aa',
-              individualFirstName: 'aa',
-              primaryAddress: {
-                addressLine1: '12345',
-                addressLine2: '',
-                addressLine3: '',
-                city: 'sheffield',
-                postCode: 's71ne',
-              },
-            },
-            emailAddress: {
-              emailAddress: 'civilmoneyclaimsdemo@gmail.com',
-            },
-            partyPhone: {
-              phone: '',
-            },
-          },
-          totalClaimAmount: 9000,
-          claimAmountBreakup: [
-            {
-              value: {
-                claimAmount: '9000',
-                claimReason: 'test',
-              },
-            },
-          ],
-          claimInterest: 'no',
-          interest: {},
-          claimDetails: {
-            helpWithFees: {
-              option: 'no',
-              referenceNumber: '',
-            },
-            reason: {
-              text: 'test',
-            },
-            timeline: {
-              rows: [
-                {
-                  date: '2000-01-01T00:00:00.000Z',
-                  description: 'test',
-                  year: '2000',
-                  month: '01',
-                  day: '01',
+    describe('processDraftClaim function', () =>     
+      it('should process the draftClaim correctly', () => {
+        const draftClaim = {
+          case_data: {
+            draftClaimCreatedAt: '2023-12-05T11:33:43.386Z',
+            id: '',
+            resolvingDispute: true,
+            completingClaimConfirmed: true,
+            applicant1: {
+              type: 'INDIVIDUAL',
+              partyDetails: {
+                individualTitle: '',
+                individualLastName: 'ss',
+                individualFirstName: 'ss',
+                provideCorrespondenceAddress: 'no',
+                primaryAddress: {
+                  addressLine1: '1234',
+                  addressLine2: '',
+                  addressLine3: '',
+                  city: 'sheffield',
+                  postCode: 'S12eu',
                 },
-              ],
+                correspondenceAddress: {
+                  addressLine1: '',
+                  addressLine2: '',
+                  addressLine3: '',
+                  city: '',
+                  postCode: '',
+                },
+                carmEnabled: false,
+              },
+              dateOfBirth: {
+                date: '1995-01-01T00:00:00.000Z',
+                year: 1995,
+                month: 1,
+                day: 1,
+              },
+              partyPhone: {},
+              emailAddress: {
+                emailAddress: 'citizen.user1@gmail.com',
+              },
             },
-            evidence: {
-              comment: '',
-              evidenceItem: '',
+            respondent1: {
+              type: 'INDIVIDUAL',
+              partyDetails: {
+                individualTitle: '',
+                individualLastName: 'aa',
+                individualFirstName: 'aa',
+                primaryAddress: {
+                  addressLine1: '12345',
+                  addressLine2: '',
+                  addressLine3: '',
+                  city: 'sheffield',
+                  postCode: 's71ne',
+                },
+              },
+              emailAddress: {
+                emailAddress: 'civilmoneyclaimsdemo@gmail.com',
+              },
+              partyPhone: {
+                phone: '',
+              },
             },
-            statementOfTruth: {
-              isFullAmountRejected: false,
-              type: 'basic',
-              signed: 'true',
-              acceptNoChangesAllowed: 'true',
+            totalClaimAmount: 9000,
+            claimAmountBreakup: [
+              {
+                value: {
+                  claimAmount: '9000',
+                  claimReason: 'test',
+                },
+              },
+            ],
+            claimInterest: 'no',
+            interest: {},
+            claimDetails: {
+              helpWithFees: {
+                option: 'no',
+                referenceNumber: '',
+              },
+              reason: {
+                text: 'test',
+              },
+              timeline: {
+                rows: [
+                  {
+                    date: '2000-01-01T00:00:00.000Z',
+                    description: 'test',
+                    year: '2000',
+                    month: '01',
+                    day: '01',
+                  },
+                ],
+              },
+              evidence: {
+                comment: '',
+                evidenceItem: '',
+              },
+              statementOfTruth: {
+                isFullAmountRejected: false,
+                type: 'basic',
+                signed: 'true',
+                acceptNoChangesAllowed: 'true',
+              },
             },
+            claimFee: {
+              calculatedAmountInPence: '45500',
+              code: 'FEE0208',
+              version: '3',
+            },
+            pcqId: '4c10fec5-1278-45f3-89f0-d3d016d47f95',
           },
-          claimFee: {
-            calculatedAmountInPence: '45500',
-            code: 'FEE0208',
-            version: '3',
-          },
-          pcqId: '4c10fec5-1278-45f3-89f0-d3d016d47f95',
-        },
-      };
+        };
 
-      const expectedOutput = draftClaim; 
+        const expectedOutput = draftClaim; 
 
-    const result = draftClaim; 
-    expect(result).toEqual(expectedOutput);
-  }));
- });
+        const result = draftClaim; 
+        expect(result).toEqual(expectedOutput);
+      }));
+  });
 });
