a6cb574ab7cbbdee133b8f9ee1bad5b335fc834f
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-01-24 17:34:28 +0000

    CIV-12095 update from QA

--- src/main/services/features/claim/payment/claimFeePaymentConfirmationContent.ts
+++ src/main/services/features/claim/payment/claimFeePaymentConfirmationContent.ts
@@ -2,7 +2,6 @@ import {Claim} from 'models/claim';
 import {PaymentSuccessfulSectionBuilder} from 'services/features/claim/payment/claimFeePaymentSuccessfulSectionBuilder';
 import {convertToPoundsFilter, currencyFormatWithNoTrailingZeros} from 'common/utils/currencyFormat';
 import {PaymentUnsuccessfulSectionBuilder} from 'services/features/claim/payment/claimFeePaymentUnsuccessfulSectionBuilder';
-import {t} from 'i18next';
 
 export const getPaymentSuccessfulPanelContent = (claim : Claim) => {
   return new PaymentSuccessfulSectionBuilder()
@@ -30,22 +29,23 @@ export const getPaymentUnsuccessfulBodyContent = (claim : Claim, lng : string, c
   const callCharges = 'www.gov.uk/call-charges';
   const phoneNumber = '0300 123 7050';
   return new PaymentUnsuccessfulSectionBuilder()
-
-    .addMainTitle(t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.PAGE_TITLE', { lng }))
-    .addParagraph(t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.CLAIM_NUMBER', {claimId: claimId, lng }))
-    .addParagraph(t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.CLAIMANT_V_DEFENDANT', {claimantName: claim.getClaimantFullName(), defendantName: claim.getDefendantFullName(), lng }))
-    .addParagraph(t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.NO_MONEY_TAKEN', { lng }))
+    .addMainTitle('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.PAGE_TITLE', { lng })
+    .addParagraph('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.CLAIM_NUMBER', { claimId: claimId, lng })
+    .addParagraph('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.CLAIMANT_V_DEFENDANT', {claimantName: claim.getClaimantFullName(), defendantName: claim.getDefendantFullName(), lng })
+    .addParagraph('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.NO_MONEY_TAKEN', { lng })
     .addLink(
-      t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.TRY_PAYMENT_AGAIN', { lng }),
+      'PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.TRY_PAYMENT_AGAIN',
       claim?.claimDetails?.claimFeePayment?.nextUrl,
-      t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.GO_BACK', { lng }),
-      '.')
+      'PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.GO_BACK',
+      '.',
+      { lng })
     .addPhoneNumber(lng, phoneNumber)
-    .addTitle(t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.OPEN_TIMES', { lng }))
-    .addParagraph(t('COMMON.CONTACT_US_FOR_HELP.OPENING_HOURS', { lng }))
+    .addTitle('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.OPEN_TIMES', { lng })
+    .addParagraph('COMMON.CONTACT_US_FOR_HELP.OPENING_HOURS', { lng })
     .addLink(
       callCharges, callChargesLink,
-      t('PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.FIND_OUT_CHARGES', { lng }),
-      '.')
+      'PAGES.PAYMENT_CONFIRMATION.UNSUCCESSFUL.FIND_OUT_CHARGES',
+      '.',
+      { lng })
     .build();
 };
