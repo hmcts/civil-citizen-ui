c360c52fe72d254feaf53f0bf2b688b76efa108f
rhysbeazleyy <Rhys.Beazley@HMCTS.NET>
2024-01-24 11:41:56 +0000

    Update createDraftClaim.ts

--- src/main/routes/features/claim/createDraftClaim.ts
+++ src/main/routes/features/claim/createDraftClaim.ts
@@ -1,8 +1,6 @@
 import { AppRequest } from 'common/models/AppRequest';
 import { NextFunction, RequestHandler, Response, Router } from 'express';
 import { CLAIM_CHECK_ANSWERS_URL, TESTING_SUPPORT_URL } from 'routes/urls';
-//import { app } from '../../../app';
-//import { calculateExpireTimeForDraftClaimInSeconds } from 'common/utils/dateUtils';
 import { saveDraftClaimToCache } from 'modules/draft-store/draftClaimCache';
 const checkAnswersViewPath = 'features/claim/create-draft';
 
@@ -16,13 +14,13 @@ createDraftClaimController.get(TESTING_SUPPORT_URL, (async (req: AppRequest, res
 }) as RequestHandler);
 
 createDraftClaimController.post(TESTING_SUPPORT_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
-    try {
-      const userId = req.session?.user?.id;
-      await saveDraftClaimToCache(userId);
-      return res.redirect(CLAIM_CHECK_ANSWERS_URL);
-    } catch (error) {
-      next(error);
-    }
-  }) as RequestHandler);
+  try {
+    const userId = req.session?.user?.id;
+    await saveDraftClaimToCache(userId);
+    return res.redirect(CLAIM_CHECK_ANSWERS_URL);
+  } catch (error) {
+    next(error);
+  }
+}) as RequestHandler);
 
   export default createDraftClaimController;
