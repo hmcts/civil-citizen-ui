2129ac3adb85685b89500241aba111424ccd8921
Manish Garg <manish.garg@hmcts.net>
2024-01-25 14:45:07 +0000

    CIV-12428 Fixed Ccj confirmation page

--- src/main/services/translation/claimantResponse/ccdRequestJudgementTranslation.ts
+++ src/main/services/translation/claimantResponse/ccdRequestJudgementTranslation.ts
@@ -1,14 +1,27 @@
-import { YesNo } from 'common/form/models/yesNo';
+import { YesNo, YesNoUpperCamelCase } from 'common/form/models/yesNo';
 import { Claim } from 'common/models/claim';
 import { toCCDYesNo } from '../response/convertToCCDYesNo';
 import { calculateInterestToDate } from 'common/utils/interestUtils';
 import { CCDClaim } from 'common/models/civilClaimResponse';
+import { toCCDDJPaymentOption } from './convertToCCDDJPaymentOption';
+import { convertToPence } from '../claim/moneyConversation';
+import { PaymentOptionType } from 'common/form/models/admission/paymentOption/paymentOptionType';
+import { toCCDDJPaymentFrequency } from '../response/convertToCCDDJPaymentFrequency';
+import { CCDDJPaymentOption } from 'common/models/ccdResponse/ccdDJPaymentOption';
+import { CCDPaymentFrequency } from 'common/models/ccdResponse/ccdPaymentFrequency';
 
 export interface ClaimantResponseRequestDefaultJudgementToCCD extends CCDClaim {
   ccjPaymentPaidSomeOption?: string,
   ccjPaymentPaidSomeAmount?: string,
   ccjJudgmentAmountClaimFee?: string,
   ccjJudgmentLipInterest?: string,
+  partialPayment?: YesNoUpperCamelCase;
+  partialPaymentAmount?: string;
+  paymentTypeSelection?: CCDDJPaymentOption;
+  paymentSetDate?: Date;
+  repaymentDue?: string;
+  repaymentFrequency?: CCDPaymentFrequency;
+  repaymentSuggestion?: string;
 }
 
 export const translateClaimantResponseRequestDefaultJudgementToCCD = (claim: Claim, claimFee: number): ClaimantResponseRequestDefaultJudgementToCCD => {
@@ -20,5 +33,12 @@ export const translateClaimantResponseRequestDefaultJudgementToCCD = (claim: Cla
     ccjPaymentPaidSomeAmount,
     ccjJudgmentAmountClaimFee: claimFee.toString(),
     ccjJudgmentLipInterest: ccjJudgmentLipInterest.toString(),
+    partialPayment: toCCDYesNo(claim.claimantResponse?.ccjRequest?.paidAmount?.option),
+    partialPaymentAmount: claim.claimantResponse?.ccjRequest?.paidAmount?.option === YesNo.YES ? convertToPence(claim.claimantResponse?.ccjRequest?.paidAmount?.amount).toString() : undefined,
+    paymentTypeSelection: toCCDDJPaymentOption( claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type),
+    paymentSetDate: claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type === PaymentOptionType.BY_SET_DATE ? claim.claimantResponse?.ccjRequest?.defendantPaymentDate?.date : undefined,
+    repaymentFrequency: claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type === PaymentOptionType.INSTALMENTS ? toCCDDJPaymentFrequency(claim.claimantResponse?.ccjRequest?.repaymentPlanInstalments?.paymentFrequency) : undefined,
+    repaymentDue:claim.claimantResponse?.ccjRequest?.paidAmount?.option === YesNo.YES ? (claim.claimantResponse?.ccjRequest?.paidAmount?.totalAmount - claim.claimantResponse?.ccjRequest?.paidAmount?.amount).toString() : undefined,
+    repaymentSuggestion: claim.claimantResponse?.ccjRequest?.ccjPaymentOption?.type === PaymentOptionType.INSTALMENTS ? claim.claimantResponse?.ccjRequest?.repaymentPlanInstalments?.amount.toString() : undefined,
   };
 };
--- src/test/unit/services/translation/claimantResponse/ccdRequestJudgementTranslation.test.ts
+++ src/test/unit/services/translation/claimantResponse/ccdRequestJudgementTranslation.test.ts
@@ -1,3 +1,4 @@
+import { PaymentOptionType } from 'common/form/models/admission/paymentOption/paymentOptionType';
 import { CaseState } from 'common/form/models/claimDetails';
 import { YesNo } from 'common/form/models/yesNo';
 import { Claim } from 'common/models/claim';
@@ -13,20 +14,37 @@ describe('Translate claimant ccd request  to ccd version', () => {
   });
 
   it('should translate ccj request for judgment admission into the CCD response', () => {
-    claim.claimantResponse.ccjRequest = {
-      paidAmount: {
-        option: YesNo.YES,
-        amount: 50,
-      },
-    };
+    claim.claimantResponse = {
+      ccjRequest : {
+        paidAmount: {
+          option: YesNo.YES,
+          amount: 50,
+        },
+        ccjPaymentOption: {
+          type: PaymentOptionType.INSTALMENTS,
+        },
+        repaymentPlanInstalments: {
+          amount: 100,
+        },
+    }, 
+  } as any;
+
     const ccdResponse = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
     expect(ccdResponse).toEqual({
       'ccjJudgmentAmountClaimFee': '300',
       'ccjJudgmentLipInterest': '0',
       'ccjPaymentPaidSomeAmount': '5000',
       'ccjPaymentPaidSomeOption': 'Yes',
+      'partialPayment':'Yes',
+      'partialPaymentAmount':'5000',
+      'paymentSetDate':undefined,
+      'paymentTypeSelection':'REPAYMENT_PLAN',
+      'repaymentDue':'NaN',
+      'repaymentFrequency':undefined,
+      'repaymentSuggestion': '100',
     });
   });
+
   it('should translate ccj request for judgment admission into the CCD response without paid amount', () => {
     claim.claimantResponse.ccjRequest = {
       paidAmount: {
@@ -40,6 +58,192 @@ describe('Translate claimant ccd request  to ccd version', () => {
       'ccjJudgmentLipInterest': '0',
       'ccjPaymentPaidSomeAmount': null,
       'ccjPaymentPaidSomeOption': 'No',
+      'partialPayment':'No',
+      'partialPaymentAmount':undefined,
+      'paymentSetDate':undefined,
+      'paymentTypeSelection':'IMMEDIATELY',
+      'repaymentDue':undefined,
+      'repaymentFrequency':undefined,
+      'repaymentSuggestion': undefined,
     });
+  });
+
+  it('should translate the partial payment amount when option is Yes', () => {
+    // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        paidAmount: {
+          option: YesNo.YES,
+          amount: 10,
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.partialPaymentAmount).toEqual('1000');
+  });
+
+  it('should not translate the partial payment amount when option is No', () => {
+    // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        paidAmount: {
+          option: YesNo.NO,
+          amount: 10,
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.partialPaymentAmount).toBeUndefined();
+  });
+
+  it('should translate the payment type selection', () => {
+    // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        ccjPaymentOption: {
+          type: PaymentOptionType.IMMEDIATELY,
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.paymentTypeSelection).toEqual('IMMEDIATELY');
+  });
+
+  it('should translate the payment set date when payment option type is BY_SET_DATE', () => {
+    // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        ccjPaymentOption: {
+          type: PaymentOptionType.BY_SET_DATE,
+        },
+        defendantPaymentDate: {
+          date: '2022-01-01',
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.paymentSetDate).toEqual('2022-01-01');
+  });
+
+  it('should not translate the payment set date when payment option type is not BY_SET_DATE', () => {
+    // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        ccjPaymentOption: {
+          type: PaymentOptionType.IMMEDIATELY,
+        },
+        defendantPaymentDate: {
+          date: '2022-01-01',
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.paymentSetDate).toBeUndefined();
+  });
+
+  it('should translate the repayment due when option is Yes', () => {
+  // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        paidAmount: {
+          option: YesNo.YES,
+          totalAmount: 20,
+          amount: 10,
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    expect(ccdClaim.repaymentDue).toEqual('10');
+  });
+
+  it('should not translate the repayment due when option is No', () => {
+    // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        paidAmount: {
+          option: YesNo.NO,
+          totalAmount: 20,
+          amount: 10,
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.repaymentDue).toBeUndefined();
+  });
+
+  it('should translate the repayment suggestion when payment option type is INSTALMENTS', () => {
+  // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        ccjPaymentOption: {
+          type: PaymentOptionType.INSTALMENTS,
+        },
+        repaymentPlanInstalments: {
+          amount: 100,
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.repaymentSuggestion).toEqual('100');
+  });
+
+  it('should not translate the repayment suggestion when payment option type is not INSTALMENTS', () => {
+    // given
+    const claim = new Claim();
+    claim.claimantResponse = {
+      ccjRequest: {
+        ccjPaymentOption: {
+          type: PaymentOptionType.IMMEDIATELY,
+        },
+        repaymentPlanInstalments: {
+          amount: 100,
+        },
+      },
+    } as any;
+
+    // when
+    const ccdClaim = translateClaimantResponseRequestDefaultJudgementToCCD(claim, 300);
+
+    // then
+    expect(ccdClaim.repaymentSuggestion).toBeUndefined();
   });
 });
