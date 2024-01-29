ebc18d81ba28067018b139e23dd02916e7708f6a
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-01-29 11:27:06 +0100

    Fix: lint

--- src/test/unit/common/form/models/partyDetails.test.ts
+++ src/test/unit/common/form/models/partyDetails.test.ts
@@ -1,27 +1,27 @@
-import { Address } from "common/form/models/address";
-import { PartyDetails } from "common/form/models/partyDetails";
-import { YesNo } from "common/form/models/yesNo";
+import { Address } from 'common/form/models/address';
+import { PartyDetails } from 'common/form/models/partyDetails';
+import { YesNo } from 'common/form/models/yesNo';
 
-describe("PartyDetails", () => {
-  it("should build an instance of a new PartyDetails", () => {
+describe('PartyDetails', () => {
+  it('should build an instance of a new PartyDetails', () => {
     //Given
     const values = {
-      title: "Mr.",
-      lastName: "Doe",
-      firstName: "Joe",
-      soleTraderTradingAs: "Test",
-      partyName: "V1",
-      contactPerson: "Ginny",
+      title: 'Mr.',
+      lastName: 'Doe',
+      firstName: 'Joe',
+      soleTraderTradingAs: 'Test',
+      partyName: 'V1',
+      contactPerson: 'Ginny',
       postToThisAddress: YesNo.YES,
       provideCorrespondenceAddress: YesNo.NO,
-      addressLine1: "test",
-      addressLine2: "test",
-      addressLine3: "test",
-      city: "test",
-      postCode: "test",
+      addressLine1: 'test',
+      addressLine2: 'test',
+      addressLine3: 'test',
+      city: 'test',
+      postCode: 'test',
     };
     const partyDetails: PartyDetails = new PartyDetails(values, false);
-    const primaryAddress = new Address("test", "test", "test", "test", "test");
+    const primaryAddress = new Address('test', 'test', 'test', 'test', 'test');
     //When
     const result = { ...values, primaryAddress, carmEnabled: false };
     //Then
