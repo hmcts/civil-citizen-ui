12382550ad6d9cc8a3ff020f756abdcdc27b89f1
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2024-01-30 13:26:12 +0000

    CIV-12443 Sonar issues

--- src/main/routes/features/response/citizenDetails/postcodeLookupController.ts
+++ src/main/routes/features/response/citizenDetails/postcodeLookupController.ts
@@ -1,28 +1,28 @@
 import {NextFunction, Router} from 'express';
-import { AddressInfoResponse } from '@hmcts/os-places-client';
+import {AddressInfoResponse} from '@hmcts/os-places-client';
 import {POSTCODE_LOOKUP_URL} from '../../../urls';
-import { getOSPlacesClientInstance } from 'modules/ordance-survey-key/ordanceSurveyKey';
+import {getOSPlacesClientInstance} from 'modules/ordance-survey-key/ordanceSurveyKey';
 
 export default Router()
   .get(POSTCODE_LOOKUP_URL, (req, res, next: NextFunction) => {
-    const postcode = <string>req.query?.postcode;
-    if (!postcode || postcode.length===0) {
-      return res.status(400).json({
-        error: {
-          status: 400,
-          message: 'Postcode not provided',
-        },
-      });
-    }
-    const osPlacesClient = getOSPlacesClientInstance();
-    osPlacesClient.lookupByPostcodeAndDataSet(req.query.postcode as string, 'DPA,LPI')
-      .then((addressInfoResponse: AddressInfoResponse) => {
-        if (!addressInfoResponse.isValid) {
-          throw new Error('Invalid post code');
-        }
+      const postcode = <string>req.query?.postcode;
+      if (!postcode || postcode.trim().length === 0) {
+        return res.status(400).json({
+          error: {
+            status: 400,
+            message: 'Postcode not provided',
+          },
+        });
+      }
+      const osPlacesClient = getOSPlacesClientInstance();
+      osPlacesClient.lookupByPostcodeAndDataSet(req.query.postcode as string, 'DPA,LPI')
+        .then((addressInfoResponse: AddressInfoResponse) => {
+          if (!addressInfoResponse.isValid) {
+            throw new Error('Invalid post code');
+          }
 
-        addressInfoResponse.addresses
-          = addressInfoResponse.addresses.filter((addresses, index, self) =>
+          addressInfoResponse.addresses
+            = addressInfoResponse.addresses.filter((addresses, index, self) =>
             index === self.findIndex((t) =>
               (t.formattedAddress === addresses.formattedAddress),
             ));
@@ -33,4 +33,4 @@ export default Router()
         });
     }
   )
-    ;
+;
