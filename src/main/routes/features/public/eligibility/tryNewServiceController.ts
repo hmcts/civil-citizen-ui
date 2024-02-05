6f270e486feb8367df9900f982e82221d76335dc
Manish Garg <manish.garg@hmcts.net>
2024-02-05 15:48:02 +0000

    CIV-12447 fixed  Eligibility journey appears only once if the Claimant not logged in

--- src/main/routes/features/public/eligibility/tryNewServiceController.ts
+++ src/main/routes/features/public/eligibility/tryNewServiceController.ts
@@ -1,9 +1,14 @@
-import {Request, Response, Router} from 'express';
-import {BASE_ELIGIBILITY_URL, ELIGIBILITY_CLAIM_VALUE_URL} from '../../../urls';
+import {Response, Router} from 'express';
+import {BASE_ELIGIBILITY_URL, ELIGIBILITY_CLAIM_VALUE_URL, CLAIM_BILINGUAL_LANGUAGE_PREFERENCE_URL} from '../../../urls';
+import { AppRequest } from 'common/models/AppRequest';
 
 const tryNewServiceController = Router();
 
-tryNewServiceController.get(BASE_ELIGIBILITY_URL, async (req: Request, res: Response) => {
+tryNewServiceController.get(BASE_ELIGIBILITY_URL, async (req: AppRequest, res: Response) => {
+  const userId = req.session?.user?.id;
+  if(req.cookies['eligibilityCompleted'] && userId) {
+    return res.redirect(CLAIM_BILINGUAL_LANGUAGE_PREFERENCE_URL);
+  }
   res.render('features/public/eligibility/try-new-service', {urlNextView: ELIGIBILITY_CLAIM_VALUE_URL});
 });
 
--- src/test/unit/routes/features/public/eligibility/tryNewServiceController.test.ts
+++ src/test/unit/routes/features/public/eligibility/tryNewServiceController.test.ts
@@ -1,18 +1,32 @@
 import request from 'supertest';
 import {app} from '../../../../../../main/app';
-import {BASE_ELIGIBILITY_URL} from 'routes/urls';
+import {BASE_ELIGIBILITY_URL, CLAIM_BILINGUAL_LANGUAGE_PREFERENCE_URL} from 'routes/urls';
 import {t} from 'i18next';
 
 describe('Try the new online service', () => {
+  app.request.cookies = {eligibilityCompleted: false};
 
   describe('on GET', () => {
     it('should return Try the new online service page', async () => {
       await request(app)
         .get(BASE_ELIGIBILITY_URL)
-        .expect((res) => {
+        expect((res: { status: any; text: any; }) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain(t('PAGES.TRY_NEW_SERVICE.TITLE'));
-        });
+         });
+
+    });
+
+    it('should return redirect to bilingual preference page if eligibilty and user session is already present', async () => {
+      app.request.cookies = {eligibilityCompleted:  true};
+      app.request.session = { user : {id: 123}} as any;
+
+      await request(app)
+        .get(BASE_ELIGIBILITY_URL)
+        expect((res: { status: any; redirect: any; }) => {
+          expect(res.status).toBe(302);
+          expect(res.redirect).toBeCalledWith(CLAIM_BILINGUAL_LANGUAGE_PREFERENCE_URL);
+         });
     });
   });
 
