91147f7c17a46761c5a8ec9fa85f7b8907987afa
jeswanth <jeswanth.reddy@hmcts.net>
2024-02-05 18:52:19 +0000

    api to create the claim

--- src/main/routes/features/claim/createDraftClaim.ts
+++ src/main/routes/features/claim/createDraftClaim.ts
@@ -1,9 +1,18 @@
 import { AppRequest } from 'common/models/AppRequest';
 import { NextFunction, RequestHandler, Response, Router } from 'express';
-import { CLAIM_CHECK_ANSWERS_URL, TESTING_SUPPORT_URL } from 'routes/urls';
+import { TESTING_SUPPORT_URL } from 'routes/urls';
+//CLAIM_CHECK_ANSWERS_URL
 import { saveDraftClaimToCache } from 'modules/draft-store/draftClaimCache';
 const createDraftViewPath = 'features/claim/create-draft';
+import jwt_decode from 'jwt-decode';
 
+interface IdTokenJwtPayload {
+  uid: string;
+  sub: string;
+  given_name: string;
+  family_name: string;
+  roles: string[];
+}
 const createDraftClaimController = Router();
 createDraftClaimController.get(TESTING_SUPPORT_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
@@ -13,15 +22,14 @@ createDraftClaimController.get(TESTING_SUPPORT_URL, (async (req: AppRequest, res
   }
 }) as RequestHandler);
 
-createDraftClaimController.post(TESTING_SUPPORT_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
+createDraftClaimController.post(TESTING_SUPPORT_URL, (async (req, res: Response, next: NextFunction) => {
   try {
-    const userId = req.session?.user?.id;
-    if (req.body && Object.keys(req.body).length > 0) {
-      await saveDraftClaimToCache(userId, req.body as any);
-    } else {
-      await saveDraftClaimToCache(userId);
-    }
-    return res.redirect(CLAIM_CHECK_ANSWERS_URL);
+    console.debug(req.body);
+    const jwt: IdTokenJwtPayload = jwt_decode(req.body?.idToken);
+    const caseData = req.body?.caseData ? JSON.parse(req.body?.caseData) : undefined;
+    await saveDraftClaimToCache(jwt.uid, caseData);
+    // return res.redirect(CLAIM_CHECK_ANSWERS_URL);
+    return res.sendStatus(200);
   } catch (error) {
     next(error);
   }
