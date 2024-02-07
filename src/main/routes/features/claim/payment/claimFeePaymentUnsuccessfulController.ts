f6c8229e4759b875e2f8125e67fe89e8b5890dcf
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-02-07 13:52:52 +0100

    CIV-12542 : Change claimFee unsucessful screen
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/routes/features/claim/payment/claimFeePaymentUnsuccessfulController.ts
+++ src/main/routes/features/claim/payment/claimFeePaymentUnsuccessfulController.ts
@@ -1,29 +1,23 @@
 import {NextFunction, RequestHandler, Response, Router} from 'express';
-import {
-  PAY_CLAIM_FEE_UNSUCCESSFUL_URL,
-} from 'routes/urls';
-import {
-  getPaymentUnsuccessfulBodyContent,
-} from 'services/features/claim/payment/claimFeePaymentConfirmationContent';
+import {DASHBOARD_CLAIMANT_URL, PAY_CLAIM_FEE_UNSUCCESSFUL_URL,} from 'routes/urls';
 import {Claim} from 'models/claim';
 import {AppRequest} from 'models/AppRequest';
-import {
-  generateRedisKey,
-  getCaseDataFromStore,
-  saveDraftClaim,
-} from 'modules/draft-store/draftStoreService';
-import {getLng} from 'common/utils/languageToggleUtils';
+import {generateRedisKey, getCaseDataFromStore, saveDraftClaim,} from 'modules/draft-store/draftStoreService';
 import {getFeePaymentRedirectInformation} from 'services/features/feePayment/feePaymentService';
 import {FeeType} from 'form/models/helpWithFees/feeType';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+
 const paymentUnsuccessfulController: Router = Router();
 
-const paymentUnsuccessfulViewPath  = 'features/claim/payment/claim-fee-payment-unsuccessful';
+const paymentUnsuccessfulViewPath  = 'features/caseProgression/hearingFee/payment-unsuccessful';
 
 async function renderView(res: Response, req: AppRequest, claim: Claim, claimId: string) {
-  const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+  const claimNumber = claim.getFormattedCaseReferenceNumber(claimId);
+  const url = claim.claimDetails.claimFeePayment;
   res.render(paymentUnsuccessfulViewPath,
     {
-      paymentUnsuccessfulBody: getPaymentUnsuccessfulBodyContent(claim, getLng(lang), claimId),
+      claimNumber,
+      url,
     });
 }
 
@@ -35,6 +29,14 @@ paymentUnsuccessfulController.get(PAY_CLAIM_FEE_UNSUCCESSFUL_URL, (async (req: A
     claim.claimDetails.claimFeePayment = paymentRedirectInformation;
     await saveDraftClaim(claim.id, claim, true);
     await renderView(res, req, claim, claimId);
+  } catch (error) {
+    next(error);
+  }
+}) as RequestHandler);
+
+paymentUnsuccessfulController.post(PAY_CLAIM_FEE_UNSUCCESSFUL_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
+  try {
+    res.redirect(constructResponseUrlWithIdParams(req.params.id, DASHBOARD_CLAIMANT_URL));
   } catch (error) {
     next(error);
   }
