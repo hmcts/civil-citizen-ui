77e6e90d480c70484c2c330a793a1806675ec952
sankaviv1 <sankavi.mohanraj@version1.com>
2024-02-07 17:38:55 +0000

    add mock claim in awaiting defendant response state

--- src/test/unit/routes/features/mediation/telephoneMediationController.test.ts
+++ src/test/unit/routes/features/mediation/telephoneMediationController.test.ts
@@ -2,7 +2,7 @@ import config from 'config';
 import nock from 'nock';
 import request from 'supertest';
 import {app} from '../../../../../main/app';
-import {RESPONSE_TASK_LIST_URL, TELEPHONE_MEDIATION_URL} from 'routes/urls';
+import {CLAIMANT_RESPONSE_TASK_LIST_URL, RESPONSE_TASK_LIST_URL, TELEPHONE_MEDIATION_URL} from 'routes/urls';
 import {
   mockRedisFailure,
 } from '../../../../utils/mockDraftStore';
@@ -32,7 +32,32 @@ describe('Telephone Mediation Controller', () => {
 
   });
 
-  describe('on POST', () => {
+  describe('on POST - claimant response', () => {
+    it('should redirect to task-list page when the user hit into continue button', async () => {
+      await request(app).post(TELEPHONE_MEDIATION_URL).expect(res => {
+        expect(res.status).toBe(302);
+        expect(res.header.location).toEqual(CLAIMANT_RESPONSE_TASK_LIST_URL);
+      });
+    });
+
+    it('should return status 500 when there is Redis error', async () => {
+      app.locals.draftStoreClient = mockRedisFailure;
+      await request(app).post(TELEPHONE_MEDIATION_URL).expect(res => {
+        expect(res.status).toBe(500);
+        expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+      });
+    });
+
+  });
+
+  describe('on POST - defendant response', () => {
+    beforeAll(() => {
+      nock(idamUrl)
+        .post('/o/token')
+        .reply(200, {id_token: citizenRoleToken});
+      jest.spyOn(draftStoreService, 'generateRedisKey').mockReturnValue('123456789');
+    });
+
     it('should redirect to task-list page when the user hit into continue button', async () => {
       await request(app).post(TELEPHONE_MEDIATION_URL).expect(res => {
         expect(res.status).toBe(302);
