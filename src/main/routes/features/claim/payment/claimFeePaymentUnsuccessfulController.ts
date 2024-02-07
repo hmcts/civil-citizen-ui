61888a0b8aea5f9efef53ed1d06196119bba3814
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-02-07 13:55:28 +0100

    CIV-12542 : Change unsuccessful payment screen
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/routes/features/claim/payment/claimFeePaymentUnsuccessfulController.ts
+++ src/main/routes/features/claim/payment/claimFeePaymentUnsuccessfulController.ts
@@ -1,8 +1,7 @@
 import {NextFunction, RequestHandler, Response, Router} from 'express';
-import {DASHBOARD_CLAIMANT_URL, PAY_CLAIM_FEE_UNSUCCESSFUL_URL,} from 'routes/urls';
-import {Claim} from 'models/claim';
+import {DASHBOARD_CLAIMANT_URL, PAY_CLAIM_FEE_UNSUCCESSFUL_URL} from 'routes/urls';
 import {AppRequest} from 'models/AppRequest';
-import {generateRedisKey, getCaseDataFromStore, saveDraftClaim,} from 'modules/draft-store/draftStoreService';
+import {generateRedisKey, getCaseDataFromStore, saveDraftClaim} from 'modules/draft-store/draftStoreService';
 import {getFeePaymentRedirectInformation} from 'services/features/feePayment/feePaymentService';
 import {FeeType} from 'form/models/helpWithFees/feeType';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
@@ -11,24 +10,18 @@ const paymentUnsuccessfulController: Router = Router();
 
 const paymentUnsuccessfulViewPath  = 'features/caseProgression/hearingFee/payment-unsuccessful';
 
-async function renderView(res: Response, req: AppRequest, claim: Claim, claimId: string) {
-  const claimNumber = claim.getFormattedCaseReferenceNumber(claimId);
-  const url = claim.claimDetails.claimFeePayment;
-  res.render(paymentUnsuccessfulViewPath,
-    {
-      claimNumber,
-      url,
-    });
-}
-
 paymentUnsuccessfulController.get(PAY_CLAIM_FEE_UNSUCCESSFUL_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const paymentRedirectInformation = await getFeePaymentRedirectInformation(claimId, FeeType.CLAIMISSUED, req);
     const claim = await getCaseDataFromStore(generateRedisKey(req));
+    const claimNumber : string = claim.getFormattedCaseReferenceNumber(claimId);
     claim.claimDetails.claimFeePayment = paymentRedirectInformation;
     await saveDraftClaim(claim.id, claim, true);
-    await renderView(res, req, claim, claimId);
+    res.render(paymentUnsuccessfulViewPath, {
+      claimNumber,
+      paymentRedirectInformation,
+    });
   } catch (error) {
     next(error);
   }
