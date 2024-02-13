f6b04ae745ce310aeee5796990cb978cda99e09e
Velican <duzgunce.vd@gmail.com>
2024-02-13 17:22:12 +0000

    merge clean up

--- src/test/functionalTests/codecept.conf.js
+++ src/test/functionalTests/codecept.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     Playwright: {
       url: testConfig.TestUrl,
       browser: 'chromium',
-      show: false,
+      show: process.env.SHOW_BROWSER_WINDOW === 'true' || false,
       waitForTimeout: parseInt(process.env.WAIT_FOR_TIMEOUT_MS || 90000),
       windowSize: '1280x960',
       timeout: 20000,
