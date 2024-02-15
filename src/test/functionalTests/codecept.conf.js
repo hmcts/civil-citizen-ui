8e1ef84d04563de6057859f634954e94b663a261
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2024-02-15 09:54:15 +0000

    Update codecept.conf.js

--- src/test/functionalTests/codecept.conf.js
+++ src/test/functionalTests/codecept.conf.js
@@ -27,7 +27,7 @@ exports.config = {
       windowSize: '1280x960',
       timeout: 30000,
       waitForAction: 500,
-      waitForNavigation: 'load',
+      waitForNavigation: 'networkidle0',
       bypassCSP: true,
       ignoreHTTPSErrors: true,
     },
