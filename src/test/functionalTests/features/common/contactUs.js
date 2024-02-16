aa6e871f531f35b708bf0887040d8fc64b426909
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2024-02-16 09:29:40 +0000

    update

--- src/test/functionalTests/features/common/contactUs.js
+++ src/test/functionalTests/features/common/contactUs.js
@@ -1,15 +1,19 @@
 const I = actor();
+let verified = false;
 
 class ContactUs {
 
   verifyContactUs() {
-    I.click('//span[contains(text(),\'Contact us for help\')]');
-    I.see('Email', 'h3');
-    I.seeElement('//a[.=\'contactocmc@justice.gov.uk\']');
-    I.see('Telephone', 'h3');
-    I.see('0300 123 7050');
-    I.see('Monday to Friday, 8.30am to 5pm.');
-    I.seeElement('[href=\'https://www.gov.uk/call-charges\']');
+    if (!verified)
+      I.click('//span[contains(text(),\'Contact us for help\')]');
+      I.waitForText('Email', 60, 'h3');
+      I.seeElement('//a[.=\'contactocmc@justice.gov.uk\']');
+      I.see('Telephone', 'h3');
+      I.see('0300 123 7050');
+      I.see('Monday to Friday, 8.30am to 5pm.');
+      I.seeElement('[href=\'https://www.gov.uk/call-charges\']');
+      verified = true;
+    }
   }
 }
 
