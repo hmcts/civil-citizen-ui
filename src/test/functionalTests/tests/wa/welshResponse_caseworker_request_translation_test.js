3e2532e42867b1c8169885f2115cd41db42a0d2d
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-02-19 11:25:35 +0000

    Changed feature title of welshResponse_caseworker_request_translation_test git push

--- src/test/functionalTests/tests/wa/welshResponse_caseworker_request_translation_test.js
+++ src/test/functionalTests/tests/wa/welshResponse_caseworker_request_translation_test.js
@@ -29,7 +29,7 @@ Before(async ({api}) => {
   await DashboardSteps.VerifyClaimOnDashboard(claimNumber);
 });
 
-Scenario('Response with PartAdmit-AlreadyPaid @citizenUI @partAdmit @regression @nightly', async () => {
+Scenario('Welsh Response with PartAdmit Then Caseworker Request Transational - SetDate @citizenUI @partAdmit @regression @nightly', async () => {
   await ResponseSteps.RespondToClaim(claimRef, 'cy');
   await ResponseSteps.EnterPersonalDetails(claimRef, false);
   await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
