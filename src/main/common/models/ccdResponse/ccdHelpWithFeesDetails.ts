e0dab42bc57d8c7878e1c319688e20b4d284f727
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-02-21 18:07:35 +0000

    CIV-12752 Set up the Enum

--- src/main/common/models/ccdResponse/ccdHelpWithFeesDetails.ts
+++ src/main/common/models/ccdResponse/ccdHelpWithFeesDetails.ts
@@ -2,5 +2,12 @@ export interface CCDHelpWithFeesDetails {
   remissionAmount?: string;
   outstandingFeeInPounds?: string;
   noRemissionDetails?: string;
-  noRemissionDetailsSummary?: string;
+  noRemissionDetailsSummary?: CCDNoRemissionDetailsSummary;
+}
+
+export enum CCDNoRemissionDetailsSummary {
+  NOT_QUALIFY_FEE_ASSISTANCE = 'NOT_QUALIFY_FEE_ASSISTANCE',
+  INCORRECT_EVIDENCE = 'INCORRECT_EVIDENCE',
+  INSUFFICIENT_EVIDENCE = 'INSUFFICIENT_EVIDENCE',
+  FEES_REQUIREMENT_NOT_MET = 'FEES_REQUIREMENT_NOT_MET',
 }
