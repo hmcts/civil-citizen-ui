9e23667cdd63adfbd7e1301800e92207ca15e1e7
marianadpereira <mariana.pereira@hmcts.net>
2024-02-22 14:04:00 +0000

    resolving lint issues

--- src/test/functionalTests/tests/cp_lipvlip_hearing_fee_tests.js
+++ src/test/functionalTests/tests/cp_lipvlip_hearing_fee_tests.js
@@ -1,14 +1,14 @@
-const config = require('../../config');
-const LoginSteps = require('../features/home/steps/login');
-const HearingFeeSteps = require('../features/caseProgression/steps/hearingFeeSteps');
-const DateUtilsComponent = require('../features/caseProgression/util/DateUtilsComponent');
-const {unAssignAllUsers} = require('./../specClaimHelpers/api/caseRoleAssignmentHelper');
-const {createAccount, deleteAccount} = require('./../specClaimHelpers/api/idamHelper');
-const DashboardSteps = require('../features/dashboard/steps/dashboard');
-
 // eslint-disable-next-line no-unused-vars
-let caseData;
-let claimNumber;
+const config = require('../../config');
+// const LoginSteps = require('../features/home/steps/login');
+// const HearingFeeSteps = require('../features/caseProgression/steps/hearingFeeSteps');
+// const DateUtilsComponent = require('../features/caseProgression/util/DateUtilsComponent');
+// const {unAssignAllUsers} = require('./../specClaimHelpers/api/caseRoleAssignmentHelper');
+// const {createAccount, deleteAccount} = require('./../specClaimHelpers/api/idamHelper');
+// const DashboardSteps = require('../features/dashboard/steps/dashboard');
+
+// let caseData;
+// let claimNumber;
 const claimType = 'FastTrack';
 let claimRef;
 
@@ -16,11 +16,11 @@ Feature('Case progression - Lip v Lip - Hearing Fee journey');
 
 Before(async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    const fourWeeksFromToday = DateUtilsComponent.DateUtilsComponent.rollDateToCertainWeeks(4);
+    // const fourWeeksFromToday = DateUtilsComponent.DateUtilsComponent.rollDateToCertainWeeks(4);
     claimRef = await api.createLiPClaim(config.claimantCitizenUser);
     await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType);
     console.log('THIS IS THE CLAIM REFERENCE ' +  claimRef);
-    await api.viewAndRespondToDefence(config.claimantCitizenUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL', 'FAST_CLAIM');
+    // await api.viewAndRespondToDefence(config.claimantCitizenUser, config.defenceType.rejectAll, 'JUDICIAL_REFERRAL', 'FAST_CLAIM');
 
     // await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef, 'fastTrack');
     // await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef, DateUtilsComponent.DateUtilsComponent.formatDateToYYYYMMDD(fourWeeksFromToday));
