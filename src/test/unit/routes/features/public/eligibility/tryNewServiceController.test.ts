8fc2b9be6663f27f683697b865810d8263a2dde6
Manish Garg <manish.garg@hmcts.net>
2024-02-23 16:38:26 +0000

    CIV-12447 Fixed Eligibility journey appear once if Claimant is not logged in (#3085)
    * CIV-12447 fixed  Eligibility journey appears only once if the Claimant not logged in
    
    * CIV-12447 Fixed Eligibility journey appear once if Claimant is not logged in
    
    * CIV-12447 Fixed Eligibility journey appear once if Claimant is not logged in
    
    * CIV-12447 Fixed Eligibility journey appear once if Claimant is not logged in
    
    * CIV-12447 Fixed Eligibility journey appear once if Claimant is not logged in #3085
    
    * CIV-12447 Fixed Eligibility journey appear once if Claimant is not logged in
    
    * CIV-12447 Fixed Eligibility journey appear once if Claimant is not logged in
    
    * CIV-12247 Updated draft claim url to eligibility
    
    ---------
    
    Co-authored-by: Raja Mani <rajakm@gmail.com>
    Co-authored-by: sankhajuria <sankhajuria@gmail.com>

--- src/test/unit/routes/features/public/eligibility/tryNewServiceController.test.ts
+++ src/test/unit/routes/features/public/eligibility/tryNewServiceController.test.ts
@@ -1,10 +1,11 @@
 import request from 'supertest';
 import {app} from '../../../../../../main/app';
-import { BASE_ELIGIBILITY_URL, MAKE_CLAIM } from 'routes/urls';
+import { BASE_ELIGIBILITY_URL, CLAIM_BILINGUAL_LANGUAGE_PREFERENCE_URL, MAKE_CLAIM } from 'routes/urls';
 import {t} from 'i18next';
 import * as launchDarkly from '../../../../../../main/app/auth/launchdarkly/launchDarklyClient';
 
 describe('Try the new online service', () => {
+  app.request.cookies = {eligibilityCompleted: false};
 
   describe('on GET', () => {
     it.each([
@@ -30,6 +31,21 @@ describe('Try the new online service', () => {
         .expect((res) => {
           expect(res.status).toBe(302);
           expect(res.text).toContain(BASE_ELIGIBILITY_URL);
+        });
+    });
+
+    it.each([
+      [BASE_ELIGIBILITY_URL],
+      [MAKE_CLAIM],
+    ])('should return redirect to bilingual preference page if eligibilty and user session is already present and url is %s', async (url ) => {
+      app.request.cookies = {eligibilityCompleted:  true};
+      app.request.session = { user : {id: 123}} as any;
+      jest.spyOn(launchDarkly, 'isCUIReleaseTwoEnabled').mockResolvedValueOnce(true);
+      await request(app)
+        .get(url)
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.text).toContain(CLAIM_BILINGUAL_LANGUAGE_PREFERENCE_URL);
         });
     });
   });
