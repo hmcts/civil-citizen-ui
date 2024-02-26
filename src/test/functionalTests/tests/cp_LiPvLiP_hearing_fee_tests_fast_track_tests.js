9f0c1e804ee65d2a0a14250b20e03cd8372732e4
marianadpereira <mariana.pereira@hmcts.net>
2024-02-26 08:58:07 +0000

    addressing comments

--- src/test/functionalTests/tests/cp_LiPvLiP_hearing_fee_tests_fast_track_tests.js
+++ src/test/functionalTests/tests/cp_LiPvLiP_hearing_fee_tests_fast_track_tests.js
@@ -3,12 +3,12 @@ const LoginSteps = require('../features/home/steps/login');
 const HearingFeeSteps = require('../features/caseProgression/steps/hearingFeeSteps');
 const DateUtilsComponent = require('../features/caseProgression/util/DateUtilsComponent');
 
-const claimType = 'SmallClaims';
+const claimType = 'FastTrack';
 let claimRef;
 let fiveWeeksFromToday;
 let hearingFeeDueDate;
 
-Feature('Case progression - Lip v Lip - Hearing Fee journey - Small Claims');
+Feature('Case progression - Lip v Lip - Hearing Fee journey - Fast Track');
 
 Before(async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
@@ -17,20 +17,21 @@ Before(async ({api}) => {
     claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
     await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType);
     await api.claimantLipRespondToDefence(config.claimantCitizenUser, claimRef, 'JUDICIAL_REFERRAL');
-    await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef, 'smallClaimsTrack');
+    await api.performCaseProgressedToSDO(config.judgeUserWithRegionId1, claimRef, 'fastTrack');
     await api.performCaseProgressedToHearingInitiated(config.hearingCenterAdminWithRegionId1, claimRef, DateUtilsComponent.DateUtilsComponent.formatDateToYYYYMMDD(fiveWeeksFromToday));
+    await api.waitForFinishedBusinessProcess();
     await LoginSteps.EnterUserCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
   }
 });
 
-Scenario('Apply for Help with Fees Journey - Small Claims', () => {
+Scenario('Apply for Help with Fees Journey - Fast Track', () => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    HearingFeeSteps.initiateApplyForHelpWithFeesJourney(claimRef, '123', hearingFeeDueDate);
+    HearingFeeSteps.initiateApplyForHelpWithFeesJourney(claimRef, '545', hearingFeeDueDate);
   }
 }).tag('@regression-cp');
 
-Scenario('Pay the Hearing Fee Journey - Small Claims', () => {
+Scenario('Pay the Hearing Fee Journey - Fast Track', () => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    HearingFeeSteps.payHearingFeeJourney(claimRef, '123', hearingFeeDueDate);
+    HearingFeeSteps.payHearingFeeJourney(claimRef, '545', hearingFeeDueDate);
   }
 }).tag('@regression-cp');
