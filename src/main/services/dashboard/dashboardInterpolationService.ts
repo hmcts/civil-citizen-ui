4134788ded42822cfd7701648f06b930ddb6921f
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-02-28 11:41:08 +0000

    CIV-12614 adding URLs to the valuesMap

--- src/main/services/dashboard/dashboardInterpolationService.ts
+++ src/main/services/dashboard/dashboardInterpolationService.ts
@@ -1,5 +1,6 @@
 import {Claim} from 'models/claim';
 import {getNumberOfDaysBetweenTwoDays} from 'common/utils/dateUtils';
+import {CLAIM_FEE_BREAKUP, RESPONSE_TASK_LIST_URL} from 'routes/urls';
 
 export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim): string => {
 
@@ -13,8 +14,8 @@ export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim
 
 const setDashboardValues = (claim: Claim): Map<string, string> => {
   const valuesMap: Map<string, string> = new Map<string, string>();
-
-  const daysToRespond = claim?.respondent1ResponseDeadline ? getNumberOfDaysBetweenTwoDays(new Date(), claim.respondent1ResponseDeadline).toString()  :'';
+  const claimId = claim.id;
+  const daysLeftToRespond = claim?.respondent1ResponseDeadline ? getNumberOfDaysBetweenTwoDays(new Date(), claim.respondent1ResponseDeadline).toString()  :'';
 
   valuesMap.set('{VIEW_CLAIM_URL}', '#');
   valuesMap.set('{VIEW_INFO_ABOUT_CLAIMANT}', '#');
@@ -28,8 +29,9 @@ const setDashboardValues = (claim: Claim): Map<string, string> => {
   valuesMap.set('{VIEW_JUDGEMENT}', '#');
   valuesMap.set('{VIEW_APPLICATIONS}', '#');
   valuesMap.set('{DRAFT_CLAIM_TASK_LIST}', '/claim/task-list');
-
-  valuesMap.set('{daysToRespond}', daysToRespond);
+  valuesMap.set('{CLAIM_FEE_URL}', CLAIM_FEE_BREAKUP.replace(':id', claimId));
+  valuesMap.set('{RESPONSE_TASK_LIST_URL}', RESPONSE_TASK_LIST_URL.replace(':id', claimId));
+  valuesMap.set('{daysLeftToRespond}', daysLeftToRespond);
 
   return valuesMap;
 };
