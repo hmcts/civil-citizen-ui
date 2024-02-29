19edec7282c852ca0f3b3bdae022c85d7f079f57
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-02-29 12:59:50 +0000

    fix leap date

--- src/test/utils/dateUtils.ts
+++ src/test/utils/dateUtils.ts
@@ -1,6 +1,10 @@
 export const getNextYearValue = (): number => {
   return new Date().getFullYear() + 1;
 };
+
+export const isLeapYear = (year: number) => {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+};
 export const FIXED_DATE = new Date(2023, 3, 26);
 export const FIXED_TIME_HOUR_MINUTE = '1000';
 
@@ -8,3 +12,4 @@ export const CURRENT_DATE = new Date();
 export const CURRENT_DAY = CURRENT_DATE.getDate();
 export const CURRENT_MONTH = CURRENT_DATE.getMonth() + 1;
 export const CURRENT_YEAR = CURRENT_DATE.getFullYear();
+
