6f71ee7b6ed5a93948943bc16c14fd0151d1fed9
Raja <raja.mani@hmcts.net>
2024-02-29 10:21:47 +0000

    added a LipVLip test FullAdmit set date

--- src/test/functionalTests/specClaimHelpers/fixtures/events/createDefendantResponse.js
+++ src/test/functionalTests/specClaimHelpers/fixtures/events/createDefendantResponse.js
@@ -1,6 +1,6 @@
 module.exports = {
   createDefendantResponse: (totalClaimAmount, responseType) => {
-    if(responseType === 'FD_DISPUTE_ALL_INDIVIDUAL'){
+    if (responseType === 'FD_DISPUTE_ALL_INDIVIDUAL') {
       return {
         event: 'DEFENDANT_RESPONSE_CUI',
         caseDataUpdate: {
@@ -86,14 +86,14 @@ module.exports = {
         },
       };
     }
-    if(responseType === 'FA_PAY_IMMEDIATELY_INDIVIDUAL'){
+    if (responseType === 'FA_PAY_IMMEDIATELY_INDIVIDUAL') {
       return {
         event: 'DEFENDANT_RESPONSE_CUI',
-        caseDataUpdate:{
+        caseDataUpdate: {
           respondent1ClaimResponseTypeForSpec: 'FULL_ADMISSION',
           defenceAdmitPartPaymentTimeRouteRequired: 'IMMEDIATELY',
-          respondToClaimAdmitPartLRspec:{
-            whenWillThisAmountBePaid: '2024-03-06T00:00:00.000Z'
+          respondToClaimAdmitPartLRspec: {
+            whenWillThisAmountBePaid: '2024-03-06T00:00:00.000Z',
           },
           responseClaimMediationSpecRequired: 'No',
           specAoSApplicantCorrespondenceAddressRequired: 'No',
@@ -114,40 +114,214 @@ module.exports = {
             },
             type: 'INDIVIDUAL',
           },
-          respondent1LiPResponse:{
-            respondent1DQExtraDetails:{
+          respondent1LiPResponse: {
+            respondent1DQExtraDetails: {
               whyPhoneOrVideoHearing: '',
               determinationWithoutHearingReason: '',
               considerClaimantDocumentsDetails: '',
-              respondent1DQLiPExpert:{
-                expertCanStillExamineDetails: ''
+              respondent1DQLiPExpert: {
+                expertCanStillExamineDetails: '',
               },
             },
-            respondent1DQHearingSupportLip:{},
-            respondent1ResponseLanguage: 'ENGLISH'
+            respondent1DQHearingSupportLip: {},
+            respondent1ResponseLanguage: 'ENGLISH',
           },
-          respondent1LiPFinancialDetails:{},
+          respondent1LiPFinancialDetails: {},
           specClaimResponseTimelineList: 'MANUAL',
-          specResponseTimelineOfEvents:[],
-          respondent1DQHomeDetails:{},
-          respondent1PartnerAndDependent:{
-            howManyChildrenByAgeGroup:{}
+          specResponseTimelineOfEvents: [],
+          respondent1DQHomeDetails: {},
+          respondent1PartnerAndDependent: {
+            howManyChildrenByAgeGroup: {},
           },
-          specDefendant1SelfEmploymentDetails:{},
-          respondToClaimAdmitPartUnemployedLRspec:{},
-          respondent1DQLanguage:{},
-          respondent1DQVulnerabilityQuestions:{},
-          respondent1DQRequestedCourt:{
+          specDefendant1SelfEmploymentDetails: {},
+          respondToClaimAdmitPartUnemployedLRspec: {},
+          respondent1DQLanguage: {},
+          respondent1DQVulnerabilityQuestions: {},
+          respondent1DQRequestedCourt: {
             otherPartyPreferredSite: '',
             responseCourtCode: '',
-            responseCourtLocations:[],
-            caseLocation:{}
+            responseCourtLocations: [],
+            caseLocation: {},
           },
-          respondent1DQWitnesses:{},
-          respondent1DQHearingSmallClaim:{},
-          respondent1DQExperts:{}
-        }
-      }
+          respondent1DQWitnesses: {},
+          respondent1DQHearingSmallClaim: {},
+          respondent1DQExperts: {},
+        },
+      };
+    }
+    if (responseType === 'FA_PAY_BY_SET_DATE_INDIVIDUAL') {
+      return {
+        event: 'DEFENDANT_RESPONSE_CUI',
+        caseDataUpdate: {
+          respondent1ClaimResponseTypeForSpec: 'FULL_ADMISSION',
+          defenceAdmitPartPaymentTimeRouteRequired: 'BY_SET_DATE',
+          respondToClaimAdmitPartLRspec: {
+            whenWillThisAmountBePaid: '2025-10-01T00:00:00.000Z',
+          },
+          responseClaimMediationSpecRequired: 'No',
+          specAoSApplicantCorrespondenceAddressRequired: 'Yes',
+          totalClaimAmount: '1500',
+          respondent1: {
+            individualDateOfBirth: '1990-01-01',
+            individualFirstName: 'John',
+            individualLastName: 'Doe',
+            individualTitle: 'Sir',
+            partyEmail: 'civilmoneyclaimsdemo@gmail.com',
+            partyPhone: '07800000000',
+            primaryAddress: {
+              AddressLine1: 'TestAddressLine1',
+              AddressLine2: 'TestAddressLine2',
+              AddressLine3: 'TestAddressLine3',
+              PostCode: 'IG61JD',
+              PostTown: 'TestCity',
+            },
+            soleTraderDateOfBirth: null,
+            type: 'INDIVIDUAL',
+          },
+          respondent1LiPResponse: {
+            respondent1DQExtraDetails: {
+              whyPhoneOrVideoHearing: '',
+              determinationWithoutHearingReason: '',
+              considerClaimantDocumentsDetails: '',
+              respondent1DQLiPExpert: {
+                expertCanStillExamineDetails: '',
+              },
+            },
+            respondent1DQHearingSupportLip: {},
+            respondent1ResponseLanguage: 'ENGLISH',
+          },
+          respondent1LiPFinancialDetails: {
+            partnerPensionLiP: 'No',
+            childrenEducationLiP: '1',
+          },
+          specClaimResponseTimelineList: 'MANUAL',
+          specResponseTimelineOfEvents: [],
+          respondent1BankAccountList: [
+            {
+              value: {
+                accountType: 'SAVINGS',
+                jointAccount: 'Yes',
+                balance: '4000',
+              },
+            },
+          ],
+          disabilityPremiumPayments: 'No',
+          respondent1DQHomeDetails: {
+            type: 'PRIVATE_RENTAL',
+            typeOtherDetails: '',
+          },
+          respondent1PartnerAndDependent: {
+            liveWithPartnerRequired: 'Yes',
+            partnerAgedOver: 'Yes',
+            haveAnyChildrenRequired: 'Yes',
+            howManyChildrenByAgeGroup: {
+              numberOfUnderEleven: '1',
+              numberOfElevenToFifteen: '1',
+              numberOfSixteenToNineteen: '1',
+            },
+            receiveDisabilityPayments: 'No',
+            supportedAnyoneFinancialRequired: 'Yes',
+            supportPeopleNumber: '2',
+            supportPeopleDetails: 'Parents',
+          },
+          defenceAdmitPartEmploymentTypeRequired: 'Yes',
+          respondToClaimAdmitPartEmploymentTypeLRspec: ['EMPLOYED'],
+          responseClaimAdmitPartEmployer: {
+            employerDetails: [
+              {
+                value: {
+                  employerName: 'ABC Ltd',
+                  jobTitle: 'Builder',
+                },
+              },
+            ],
+          },
+          specDefendant1SelfEmploymentDetails: {},
+          respondToClaimAdmitPartUnemployedLRspec: {},
+          respondent1CourtOrderPaymentOption: 'No',
+          respondent1CourtOrderDetails: [],
+          respondent1LoanCreditOption: 'Yes',
+          respondent1LoanCreditDetails: [
+            {
+              value: {
+                loanCardDebtDetail: 'HSBC Credit card',
+                totalOwed: '300000',
+                monthlyPayment: '30000',
+              },
+            },
+          ],
+          responseToClaimAdmitPartWhyNotPayLRspec: 'test',
+          respondent1DQCarerAllowanceCreditFullAdmission: 'Yes',
+          specDefendant1Debts: {
+            debtDetails: [
+              {
+                value: {
+                  debtType: 'MORTGAGE',
+                  paymentAmount: '20000',
+                  paymentFrequency: 'ONCE_ONE_MONTH',
+                },
+              },
+            ],
+          },
+          respondent1DQRecurringIncomeFA: [
+            {
+              value: {
+                type: 'JOB',
+                amount: '300000',
+                frequency: 'ONCE_ONE_MONTH',
+              },
+            },
+            {
+              value: {
+                type: 'INCOME_SUPPORT',
+                amount: '100000',
+                frequency: 'ONCE_ONE_MONTH',
+              },
+            },
+          ],
+          respondent1DQRecurringExpensesFA: [
+            {
+              value: {
+                type: 'RENT',
+                amount: '200000',
+                frequency: 'ONCE_ONE_MONTH',
+              },
+            },
+            {
+              value: {
+                type: 'COUNCIL_TAX',
+                amount: '20000',
+                frequency: 'ONCE_ONE_MONTH',
+              },
+            },
+            {
+              value: {
+                type: 'WATER',
+                amount: '2000',
+                frequency: 'ONCE_TWO_WEEKS',
+              },
+            },
+            {
+              value: {
+                type: 'TV',
+                amount: '4000',
+                frequency: 'ONCE_ONE_MONTH',
+              },
+            },
+          ],
+          respondent1DQLanguage: {},
+          respondent1DQVulnerabilityQuestions: {},
+          respondent1DQRequestedCourt: {
+            otherPartyPreferredSite: '',
+            responseCourtCode: '',
+            responseCourtLocations: [],
+            caseLocation: {},
+          },
+          respondent1DQWitnesses: {},
+          respondent1DQHearingSmallClaim: {},
+          respondent1DQExperts: {},
+        },
+      };
     }
   },
 };
---
+++ src/test/functionalTests/tests/createLiPvLiP_FullAdmit_PayImmediately_tests.js
@@ -0,0 +1,31 @@
+const config = require('../../config');
+
+const {unAssignAllUsers} = require('./../specClaimHelpers/api/caseRoleAssignmentHelper');
+const LoginSteps =  require('../features/home/steps/login');
+const DashboardSteps = require('../features/dashboard/steps/dashboard');
+const {createAccount} = require('../specClaimHelpers/api/idamHelper');
+
+const claimType = 'SmallClaims';
+// eslint-disable-next-line no-unused-vars
+let claimRef;
+let caseData;
+let claimNumber;
+
+Feature('Create Lip v Lip claim -  Full Admit and pay Immediately');
+
+Scenario('Create LipvLip claim and defendant response as FullAdmit and pay immediately', async ({api}) => {
+  if (['preview', 'demo'].includes(config.runningEnv)) {
+    await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    claimRef = await api.createLiPClaim(config.claimantEmailsVerificationCitizenUser, claimType);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber = await caseData.legacyCaseReference;
+    await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    await DashboardSteps.VerifyClaimOnDashboard(claimNumber);
+    await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, 'FA_PAY_IMMEDIATELY_INDIVIDUAL');
+  }
+}).tag('@regression-r2');
+
+AfterSuite(async () => {
+  await unAssignAllUsers();
+});
