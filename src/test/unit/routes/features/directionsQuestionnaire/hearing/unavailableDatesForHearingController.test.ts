e3500183b39e0c0a089ce5a841b8a36ca29c3f18
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-03-01 10:49:31 +0000

    CIV-12614 skipping tests

--- src/test/unit/routes/features/directionsQuestionnaire/hearing/unavailableDatesForHearingController.test.ts
+++ src/test/unit/routes/features/directionsQuestionnaire/hearing/unavailableDatesForHearingController.test.ts
@@ -491,7 +491,6 @@ describe('Unavailable dates for hearing Controller', () => {
 
     it('should redirect phone or video hearing page if all information provided and unavailable dates are less than 30 days', async () => {
       const is29Feb = isTwentyNineLeapYear(today);
-      
       await request(app)
         .post(DQ_AVAILABILITY_DATES_FOR_HEARING_URL)
         .send({
