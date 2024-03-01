9ea134fa6d59aa20706708c1a1968da848d50957
kdaHMCTS <128375235+kdaHMCTS@users.noreply.github.com>
2024-03-01 16:40:15 +0000

    CIV-7725 Welsh translations for defendant screens in FT (#3232)
    * Adding welsh response and caseworker translation test
    
    * fixed some eslint errors
    
    * Fixed error with setter in sharedData
    
    * more eslint errors fixed
    
    * update mediationCanWeUse
    
    * fixed dateOfBirth page
    
    * fixed error support required
    
    * added export back to MediationCanWeUse.js
    
    * Added common buttons to one file
    
    * Changed feature name in test
    
    * update
    
    * typo error
    
    * Changing content assertions
    
    * update mediationCanWeUse
    
    * changed button save and continue button locators
    
    * removing after suite
    
    * fixed eslint errors
    
    * add common locator for save and continue button
    
    * fix eslint issues
    
    * update code
    
    * fixed availabilityDates page
    
    * fixed whyDoYouDisagree
    
    * removed adding defendant detailed to support requirements
    
    * update
    
    * fix support required page
    
    * fixed support required page
    
    ---------
    
    Co-authored-by: mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>

--- src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/supportRequired.js
+++ src/test/functionalTests/features/response/pages/defendantLipResponse/defendantDQ/supportRequired.js
@@ -1,25 +1,62 @@
 const I = actor();
 const config = require('../../../../../../config');
+const sharedData = require('../../../../../sharedData');
+const cButtons = require('../../../../common/cButtons');
 
 const fields ={
   yesButton: 'input[id="model[option]"]',
   noButton: 'input[id="model[option]-2"]',
-  selectDropDown: 'select[id="model[items][0][fullName]"]',
-  hearingLoop: 'input[id="declared-0-hearingLoop"]',
-  signLanguage: 'input[id="declared-0-signLanguageInterpreter"]',
-  signLanguageText: 'input[id="model[items][0][signLanguageInterpreter][content]"]',
+  person1Dropdown: 'select[id="model[items][0][fullName]"]',
+  person2Dropdown: 'select[id="model[items][1][fullName]"]',
+  person1HearingLoop: 'input[id="declared-0-hearingLoop"]',
+  person1SignLanguage: 'input[id="declared-0-signLanguageInterpreter"]',
+  person1SignLanguageText: 'input[id="model[items][0][signLanguageInterpreter][content]"]',
+  person2DisabledAccess: 'input[id="declared-1-disabledAccess"]',
+  person2OtherSupport: 'input[id="declared-1-otherSupport"]',
+  person2OtherSupportText: 'textarea[id="model[items][1][otherSupport][content]"]',
+};
+
+const content = {
+  heading: {
+    en: 'Do you, your experts or witnesses need support to attend a hearing',
+    cy: 'Ydych chi, eich arbenigwyr neu eich tystion angen cymorth i allu mynychu gwrandawiad?',
+  },
+};
+
+// const buttons = {
+//   addPerson: {
+//     en: 'Add another person',
+//     cy: 'Ychwanegu unigolyn arall',
+//   },
+// };
+
+const inputs = {
+  person1SignLanguage: {
+    en: 'Spanish',
+    cy: 'Sbaeneg',
+  }, 
+  person2OtherSupport: {
+    en: 'No support required',
+    cy: 'Dim cymorth sydd ei angen',
+  },
 };
 
 class SupportRequired {
 
   async selectOptionForSupportRequired() {
-    await I.waitForText('Do you, your experts or witnesses need support to attend a hearing', config.WaitForText);
+    const { language } = sharedData; 
+    await I.waitForText(content.heading[language], config.WaitForText);
     await I.click(fields.yesButton);
-    await I.selectOption(fields.selectDropDown,'WitnessFName WitnessLName');
-    await I.click(fields.hearingLoop);
-    await I.click(fields.signLanguage);
-    await I.fillField(fields.signLanguageText, 'Spanish');
-    await I.click('Save and continue');
+    await I.selectOption(fields.person1Dropdown, 'WitnessFirstName WitnessLastName');
+    await I.click(fields.person1HearingLoop);
+    await I.click(fields.person1SignLanguage);
+    await I.fillField(fields.person1SignLanguageText, inputs.person1SignLanguage[language]);
+    // await I.click(buttons.addPerson[language]);
+    // await I.selectOption(fields.person2Dropdown, 'John Doe');
+    // await I.click(fields.person2DisabledAccess);
+    // await I.click(fields.person2OtherSupport);
+    // await I.fillField(fields.person2OtherSupportText, inputs.person2OtherSupport[language]);
+    await I.click(cButtons.saveAndContinue[language]);
   }
 }
 
