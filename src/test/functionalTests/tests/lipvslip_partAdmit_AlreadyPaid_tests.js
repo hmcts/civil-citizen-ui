72c66222e3ea37b4c3bf229457a76581c68e7394
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2024-03-02 09:25:25 +0000

    update

---
+++ src/test/functionalTests/tests/lipvslip_partAdmit_AlreadyPaid_tests.js
@@ -0,0 +1,33 @@
+const config = require('../../config');
+const ResponseSteps = require('../features/response/steps/lipDefendantResponseSteps');
+const LoginSteps = require('../features/home/steps/login');
+const DashboardSteps = require('../features/dashboard/steps/dashboard');
+const {createAccount} = require('../specClaimHelpers/api/idamHelper');
+// eslint-disable-next-line no-unused-vars
+const yesIWantMoretime = 'yesIWantMoretime';
+
+let claimRef;
+let caseData;
+let claimNumber;
+let securityCode;
+
+Feature('Response with PartAdmit-AlreadyPaid - Small Claims');
+
+Before(async ({api}) => {
+  if (['preview', 'demo'].includes(config.runningEnv)) {
+    await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    claimRef = await api.createLiPClaim(config.claimantCitizenUser, 'SmallClaims');
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber = await caseData.legacyCaseReference;
+    securityCode = await caseData.respondent1PinToPostLRspec.accessCode;
+    console.log('claim number', claimNumber);
+    console.log('Security code', securityCode);
+    await ResponseSteps.AssignCaseToLip(claimNumber, securityCode);
+    await LoginSteps.EnterUserCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    await DashboardSteps.VerifyClaimOnDashboard(claimNumber);
+  }
+});
+
+Scenario('Response with PartAdmit-AlreadyPaid @citizenUI @partAdmit @nightly - @api', async ({api}) => {
+}).tag('@regression-r2');
