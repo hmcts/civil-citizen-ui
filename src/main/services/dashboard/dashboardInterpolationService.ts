f9387b3c2346233aba949a299a51ed120d345dfb
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-03-07 12:42:00 +0000

    CIV-0000: Fix build
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/services/dashboard/dashboardInterpolationService.ts
+++ src/main/services/dashboard/dashboardInterpolationService.ts
@@ -3,7 +3,7 @@ import {getNumberOfDaysBetweenTwoDays} from 'common/utils/dateUtils';
 import {CLAIM_FEE_BREAKUP, DASHBOARD_NOTIFICATION_REDIRECT, RESPONSE_TASK_LIST_URL} from 'routes/urls';
 import config from 'config';
 
-export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim, notificationId?: number): string => {
+export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim, notificationId?: string): string => {
 
   const valuesMap = setDashboardValues(claim, notificationId);
   valuesMap.forEach((value: string, key: string) => {
@@ -13,7 +13,7 @@ export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim
   return textToReplace;
 };
 
-const setDashboardValues = (claim: Claim, notificationId?: number): Map<string, string> => {
+const setDashboardValues = (claim: Claim, notificationId?: string): Map<string, string> => {
   const valuesMap: Map<string, string> = new Map<string, string>();
   const claimId = claim.id;
   const daysLeftToRespond = claim?.respondent1ResponseDeadline ? getNumberOfDaysBetweenTwoDays(new Date(), claim.respondent1ResponseDeadline).toString()  :'';
@@ -45,7 +45,7 @@ const setDashboardValues = (claim: Claim, notificationId?: number): Map<string, 
     valuesMap.set('{VIEW_DOCUMENT_DRAFT}', DASHBOARD_NOTIFICATION_REDIRECT
       .replace(':id', claimId)
       .replace(':locationName', 'VIEW_DOCUMENT_DRAFT')
-      .replace(':notificationId', notificationId.toString()));
+      .replace(':notificationId', notificationId));
   }
 
   return valuesMap;
