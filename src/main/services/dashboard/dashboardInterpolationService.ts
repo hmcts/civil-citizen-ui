ba99d042e44de43111547d149ba5ec1a7d450ce5
paul-pearson <paul.pearson@version1.com>
2024-03-07 16:27:41 +0000

    CIV-12879 Fix build

--- src/main/services/dashboard/dashboardInterpolationService.ts
+++ src/main/services/dashboard/dashboardInterpolationService.ts
@@ -2,7 +2,7 @@ import {Claim} from 'models/claim';
 import {getNumberOfDaysBetweenTwoDays} from 'common/utils/dateUtils';
 import {CLAIM_FEE_BREAKUP, DASHBOARD_NOTIFICATION_REDIRECT, RESPONSE_TASK_LIST_URL} from 'routes/urls';
 
-export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim, notificationId?: number): string => {
+export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim, notificationId?: string): string => {
 
   const valuesMap = setDashboardValues(claim, notificationId);
   valuesMap.forEach((value: string, key: string) => {
@@ -12,7 +12,7 @@ export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim
   return textToReplace;
 };
 
-const setDashboardValues = (claim: Claim, notificationId?: number): Map<string, string> => {
+const setDashboardValues = (claim: Claim, notificationId?: string): Map<string, string> => {
   const valuesMap: Map<string, string> = new Map<string, string>();
   const claimId = claim.id;
   const daysLeftToRespond = claim?.respondent1ResponseDeadline ? getNumberOfDaysBetweenTwoDays(new Date(), claim.respondent1ResponseDeadline).toString()  :'';
@@ -41,7 +41,7 @@ const setDashboardValues = (claim: Claim, notificationId?: number): Map<string, 
     valuesMap.set('{VIEW_DOCUMENT_DRAFT}', DASHBOARD_NOTIFICATION_REDIRECT
       .replace(':id', claimId)
       .replace(':locationName', 'VIEW_DOCUMENT_DRAFT')
-      .replace(':notificationId', notificationId.toString()));
+      .replace(':notificationId', notificationId));
   }
 
   return valuesMap;
