d70e572f266c039981b505a13cf186f0080fb02e
meulendijksm <max.meulendijks@hmcts.net>
2024-03-08 10:52:59 +0000

    CIV-0000 - keep claimId without redisKey on pay claim fee page

--- src/main/services/dashboard/dashboardInterpolationService.ts
+++ src/main/services/dashboard/dashboardInterpolationService.ts
@@ -2,9 +2,9 @@ import {Claim} from 'models/claim';
 import {getNumberOfDaysBetweenTwoDays} from 'common/utils/dateUtils';
 import {CLAIM_FEE_BREAKUP, DASHBOARD_NOTIFICATION_REDIRECT, RESPONSE_TASK_LIST_URL} from 'routes/urls';
 
-export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim, notificationId?: number): string => {
+export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim, claimId: string, notificationId?: string): string => {
 
-  const valuesMap = setDashboardValues(claim, notificationId);
+  const valuesMap = setDashboardValues(claim, claimId, notificationId);
   valuesMap.forEach((value: string, key: string) => {
     textToReplace = textToReplace?.replace(key, value);
   });
@@ -12,9 +12,8 @@ export const replaceDashboardPlaceholders = (textToReplace: string, claim: Claim
   return textToReplace;
 };
 
-const setDashboardValues = (claim: Claim, notificationId?: number): Map<string, string> => {
+const setDashboardValues = (claim: Claim, claimId: string, notificationId?: string): Map<string, string> => {
   const valuesMap: Map<string, string> = new Map<string, string>();
-  const claimId = claim.id;
   const daysLeftToRespond = claim?.respondent1ResponseDeadline ? getNumberOfDaysBetweenTwoDays(new Date(), claim.respondent1ResponseDeadline).toString()  :'';
 
   valuesMap.set('{VIEW_CLAIM_URL}', '#');
@@ -41,7 +40,7 @@ const setDashboardValues = (claim: Claim, notificationId?: number): Map<string, 
     valuesMap.set('{VIEW_DOCUMENT_DRAFT}', DASHBOARD_NOTIFICATION_REDIRECT
       .replace(':id', claimId)
       .replace(':locationName', 'VIEW_DOCUMENT_DRAFT')
-      .replace(':notificationId', notificationId.toString()));
+      .replace(':notificationId', notificationId));
   }
 
   return valuesMap;
