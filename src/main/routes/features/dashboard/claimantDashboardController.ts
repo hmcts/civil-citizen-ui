4f0cfdbdcb5a74de6c1869c5ae29d563783945be
laarmada <sergiosalmar@yahoo.es>
2024-03-12 11:05:13 +0000

    restore old claimant dashboard version to include the new toggle.

--- src/main/routes/features/dashboard/claimantDashboardController.ts
+++ src/main/routes/features/dashboard/claimantDashboardController.ts
@@ -1,10 +1,12 @@
 import {NextFunction, RequestHandler, Response, Router} from 'express';
-import {DASHBOARD_CLAIMANT_URL} from '../../urls';
+import {DASHBOARD_CLAIMANT_URL,OLD_DASHBOARD_CLAIMANT_URL} from '../../urls';
 import {getDashboardForm, getNotifications} from 'services/dashboard/dashboardService';
 import {Claim} from 'models/claim';
 import {getClaimById} from 'modules/utilityService';
 import {AppRequest} from 'models/AppRequest';
 import {ClaimantOrDefendant} from 'models/partyType';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+import {isDashboardServiceEnabled} from '../../../app/auth/launchdarkly/launchDarklyClient';
 
 const claimantDashboardViewPath = 'features/dashboard/claim-summary-redesign';
 const claimantDashboardController = Router();
@@ -12,24 +14,29 @@ const claimantDashboardController = Router();
 claimantDashboardController.get(DASHBOARD_CLAIMANT_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const claimId =  req.params.id;
-    const lng = req.query.lang ? req.query.lang : req.cookies.lang;
-    let claim: Claim;
-    let caseRole: ClaimantOrDefendant;
-    let dashboardId;
+    if (isDashboardServiceEnabled()){
+      const lng = req.query.lang ? req.query.lang : req.cookies.lang;
+      let claim: Claim;
+      let caseRole: ClaimantOrDefendant;
+      let dashboardId;
 
-    if(claimId == 'draft') {
-      caseRole = ClaimantOrDefendant.CLAIMANT;
-      const userId = (<AppRequest>req)?.session?.user?.id.toString();
-      claim = await getClaimById(userId, req);
-      dashboardId = userId;
+      if(claimId == 'draft') {
+        caseRole = ClaimantOrDefendant.CLAIMANT;
+        const userId = (<AppRequest>req)?.session?.user?.id.toString();
+        claim = await getClaimById(userId, req);
+        dashboardId = userId;
+      } else {
+        claim = await getClaimById(claimId, req, true);
+        caseRole = claim.isClaimant()?ClaimantOrDefendant.CLAIMANT:ClaimantOrDefendant.DEFENDANT;
+        dashboardId = claimId;
+      }
+      const dashboardNotifications = await getNotifications(dashboardId, claim, caseRole, req);
+      const dashboard = await getDashboardForm(caseRole, claim, dashboardId, req);
+      res.render(claimantDashboardViewPath, {claim:claim, claimId, dashboardTaskList:dashboard, dashboardNotifications, lng});
     } else {
-      claim = await getClaimById(claimId, req, true);
-      caseRole = claim.isClaimant()?ClaimantOrDefendant.CLAIMANT:ClaimantOrDefendant.DEFENDANT;
-      dashboardId = claimId;
+
+      res.redirect(constructResponseUrlWithIdParams(claimId, OLD_DASHBOARD_CLAIMANT_URL));
     }
-    const dashboardNotifications = await getNotifications(dashboardId, claim, caseRole, req);
-    const dashboard = await getDashboardForm(caseRole, claim, dashboardId, req);
-    res.render(claimantDashboardViewPath, {claim:claim, claimId, dashboardTaskList:dashboard, dashboardNotifications, lng});
   } catch (error) {
     next(error);
   }
