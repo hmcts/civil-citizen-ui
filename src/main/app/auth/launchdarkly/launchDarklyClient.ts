ad5361dcdadde7d1e72982ee2df730aa7026a2d1
marianadpereira <mariana.pereira@hmcts.net>
2024-03-22 09:59:26 +0000

    pointing to changes

--- src/main/app/auth/launchdarkly/launchDarklyClient.ts
+++ src/main/app/auth/launchdarkly/launchDarklyClient.ts
@@ -40,9 +40,11 @@ export async function isPcqShutterOn(): Promise<boolean> {
 export async function isCUIReleaseTwoEnabled(): Promise<boolean> {
   return await getFlagValue('cuiReleaseTwoEnabled') as boolean;
 }
+
 export async function isDashboardServiceEnabled(): Promise<boolean> {
-  return await getFlagValue('dashboard-service') as boolean;
+  return true;
 }
+
 export async function isCARMEnabled(): Promise<boolean> {
   return await getFlagValue('carm') as boolean;
 }
