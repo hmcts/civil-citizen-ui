836a353bf02382c9cbbd7b6d38df35af2c9185d5
paul-pearson <paul.pearson@version1.com>
2024-03-25 11:10:37 +0000

    CIV-13234 Add missing semicolon

--- src/test/unit/services/features/claimantResponse/defendantResponse/defendantResponseSummaryService.test.ts
+++ src/test/unit/services/features/claimantResponse/defendantResponse/defendantResponseSummaryService.test.ts
@@ -283,7 +283,7 @@ describe("Defendant's response summary service", () => {
       'howMuchHaveYouPaid': howMuchHaveYouPaid,
 
     };
-    claim.respondent1.responseType = ResponseType.FULL_DEFENCE
+    claim.respondent1.responseType = ResponseType.FULL_DEFENCE;
     claim.rejectAllOfClaim.timeline = claim.partialAdmission.timeline;
 
     // When
