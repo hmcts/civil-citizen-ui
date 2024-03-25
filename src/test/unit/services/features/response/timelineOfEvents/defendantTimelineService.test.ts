2ad7dc12dae28f3a1dc0d9241baf19bc65c39548
paul-pearson <paul.pearson@version1.com>
2024-03-25 12:20:50 +0000

    CIV-13234 Add tests for defendantTimelineService

--- src/test/unit/services/features/response/timelineOfEvents/defendantTimelineService.test.ts
+++ src/test/unit/services/features/response/timelineOfEvents/defendantTimelineService.test.ts
@@ -8,13 +8,16 @@ import {
 import * as draftStoreService from '../../../../../../main/modules/draft-store/draftStoreService';
 import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
 import {TimelineRow} from '../../../../../../main/common/form/models/timeLineOfEvents/timelineRow';
+import {RejectAllOfClaim} from 'form/models/rejectAllOfClaim';
+import {Party} from 'models/party';
+import {ResponseType} from 'form/models/responseType';
 
 jest.mock('../../../../../../main/modules/draft-store');
 jest.mock('../../../../../../main/modules/draft-store/draftStoreService');
 
 describe('defendantTimelineService', () => {
   describe('getPartialAdmitTimeline', () => {
-    it('should return defendant timeline from claim when it exists', () => {
+    it('should return defendant timeline from partial admission when it exists', () => {
       //Given
       const claim = new Claim();
       const partialAdmission = new PartialAdmission();
@@ -27,6 +30,20 @@ describe('defendantTimelineService', () => {
       expect(model.rows[0].date).toEqual(partialAdmission.timeline.rows[0].date);
       expect(model.rows[0].description).toEqual(partialAdmission.timeline.rows[0].description);
       expect(model.comment).toEqual(partialAdmission.timeline.comment);
+    });
+    it('should return defendant timeline from full defence when it exists', () => {
+      //Given
+      const claim = new Claim();
+      const rejectAllOfClaim = new RejectAllOfClaim();
+      rejectAllOfClaim.timeline = new DefendantTimeline([new TimelineRow(17, 11, 2021, 'description')], 'comment');
+      claim.rejectAllOfClaim = rejectAllOfClaim;
+      //When
+      const model = getDefendantTimeline(claim);
+      //Then
+      expect(model.rows.length).toBe(4);
+      expect(model.rows[0].date).toEqual(rejectAllOfClaim.timeline.rows[0].date);
+      expect(model.rows[0].description).toEqual(rejectAllOfClaim.timeline.rows[0].description);
+      expect(model.comment).toEqual(rejectAllOfClaim.timeline.comment);
     });
     it('should return empty form when there is no defendant timeline in claim', () => {
       //Given
@@ -46,6 +63,20 @@ describe('defendantTimelineService', () => {
       //Given
       mockGetCaseData.mockImplementation(async () => {
         return new Claim();
+      });
+      const spySave = jest.spyOn(draftStoreService, 'saveDraftClaim');
+      //When
+      await saveDefendantTimeline('123', DefendantTimeline.buildPopulatedForm([new TimelineRow(12, 2, 2020, 'description')], 'comment'));
+      //Then
+      expect(spySave).toBeCalled();
+    });
+    it('should save full defence timeline successfully', async () => {
+      //Given
+      mockGetCaseData.mockImplementation(async () => {
+        const claim: Claim = new Claim();
+        claim.respondent1 = new Party();
+        claim.respondent1.responseType = ResponseType.FULL_DEFENCE;
+        return claim;
       });
       const spySave = jest.spyOn(draftStoreService, 'saveDraftClaim');
       //When
