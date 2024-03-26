96539af52390439894de81d978c6895ab99388e5
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-03-26 07:33:27 +0000

    fixed mores eslint errors

--- src/test/functionalTests/helpers/caseDataHelpers.js
+++ src/test/functionalTests/helpers/caseDataHelpers.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //changes 1711119652839305 to #1711-1196-5283-9305
+  //changes 1711119652839305 to #1711-1196-5283-9305
   formatClaimRef(claimRef) {
     const groups = claimRef.toString().match(/.{1,4}/g);
     const formattedString = '#' + groups.join('-');
