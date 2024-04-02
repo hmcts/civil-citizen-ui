990414c83b1fd0d23a18e235f6ae7bb8b7a240ba
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-04-02 16:16:32 +0100

    CIV-13324 Set up claim fee part

--- src/main/services/features/claim/amount/checkClaimFee.ts
+++ src/main/services/features/claim/amount/checkClaimFee.ts
@@ -3,15 +3,21 @@ import config from 'config';
 import {CivilServiceClient} from 'client/civilServiceClient';
 import {AppRequest} from 'models/AppRequest';
 import {saveClaimFee} from 'services/features/claim/amount/claimFeesService';
-import {generateRedisKey} from 'modules/draft-store/draftStoreService';
+import {calculateInterestToDate} from 'common/utils/interestUtils';
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 export const checkIfClaimFeeHasChanged = async (claimId: string, claim: Claim, req: AppRequest) => {
-  const newClaimFeeData = await civilServiceClient.getClaimFeeData(claim.totalClaimAmount, req);
+  const userId = req.session?.user?.id;
+  let interestToDate = 0;
+  if (claim.hasInterest()) {
+    interestToDate = calculateInterestToDate(claim);
+  }
+
+  const newClaimFeeData = await civilServiceClient.getClaimFeeData(claim.totalClaimAmount + interestToDate, req);
   const oldClaimFee = claim.claimFee?.calculatedAmountInPence;
   if (oldClaimFee !== newClaimFeeData?.calculatedAmountInPence) {
-    await saveClaimFee(generateRedisKey(req), newClaimFeeData);
+    await saveClaimFee(userId, newClaimFeeData);
     return true;
   } else {
     return false;
