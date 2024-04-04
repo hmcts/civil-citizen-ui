a2ddafedb1139e96650bb0e4f335d376be68dc39
paul-pearson <paul.pearson@version1.com>
2024-04-04 12:22:39 +0100

    CIV-13055 Show court decision when favours claimant

--- src/main/services/features/claimantResponse/responseSection/buildCourtDecisionDetailsSection.ts
+++ src/main/services/features/claimantResponse/responseSection/buildCourtDecisionDetailsSection.ts
@@ -3,32 +3,48 @@ import { ClaimantResponse } from 'common/models/claimantResponse';
 import { SummaryRow, summaryRow } from 'common/models/summaryList/summaryList';
 import { SummarySection, summarySection } from 'common/models/summaryList/summarySections';
 import { formatDateToFullDate } from 'common/utils/dateUtils';
-import { getAmount, getFinalPaymentDate, getFirstRepaymentDate, getPaymentAmount, getPaymentDate, getRepaymentFrequency } from 'common/utils/repaymentUtils';
+import {
+  getAmount,
+  getFinalPaymentDate, getFinalPaymentDateForClaimantPlan,
+  getFirstRepaymentDate, getFirstRepaymentDateClaimantPlan,
+  getPaymentAmount,
+  getPaymentAmountClaimantPlan,
+  getPaymentDate,
+  getRepaymentFrequency, getRepaymentFrequencyForClaimantPlan
+} from 'common/utils/repaymentUtils';
 import { t } from 'i18next';
+import {PaymentDate} from 'form/models/admission/fullAdmission/paymentOption/paymentDate';
 
-const buildSummaryCourtDecisionByInstallments = (claim: Claim, lng: string): SummaryRow[] => {
+const buildSummaryCourtDecisionByInstallments = (claim: Claim, isClaimantPlanAccepted: boolean, lng: string): SummaryRow[] => {
   const fullName = claim.getDefendantFullName();
   const amount = getAmount(claim);
-  const instalmentAmount = getPaymentAmount(claim);
-  const frequency = t(`COMMON.PAYMENT_FREQUENCY.${getRepaymentFrequency(claim)}`, { lng })?.toLowerCase();
-  const instalmentDate = formatDateToFullDate(getFirstRepaymentDate(claim), lng);
+  const instalmentAmount = isClaimantPlanAccepted ? getPaymentAmountClaimantPlan(claim) : getPaymentAmount(claim);
+  const paymentFrequency= isClaimantPlanAccepted ? getRepaymentFrequencyForClaimantPlan(claim) : getRepaymentFrequency(claim);
+  const frequency = t(`COMMON.PAYMENT_FREQUENCY.${paymentFrequency}`, { lng })?.toLowerCase();
+
+  const instalmentDate = isClaimantPlanAccepted ? formatDateToFullDate(getFirstRepaymentDateClaimantPlan(claim), lng) : formatDateToFullDate(getFirstRepaymentDate(claim), lng);
   return [
 
     summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COURT_REPAYMENT_PLAN', { lng }), t('PAGES.CHECK_YOUR_ANSWER.WILL_REPAY_IN_INSTALLMENTS', { lng, fullName, amount, instalmentAmount, instalmentDate, frequency })),
-    summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA', { lng }), `${formatDateToFullDate(getFinalPaymentDate(claim), lng)}`),
+    summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA', { lng }), `${isClaimantPlanAccepted?formatDateToFullDate(getFinalPaymentDateForClaimantPlan(claim),lng):formatDateToFullDate(getFinalPaymentDate(claim), lng)}`),
   ];
 };
 
-const buildSummaryCourtDecisionBySetDate = (claim: Claim, lng: string) => {
-  const paymentDate = formatDateToFullDate(getPaymentDate(claim), lng);
+const buildSummaryCourtDecisionBySetDate = (claim: Claim, isClaimantPlanAccepted: boolean, lng: string) => {
+  const date = claim.claimantResponse?.suggestedPaymentIntention?.paymentDate as unknown as PaymentDate;
+  const paymentDate = t(formatDateToFullDate(isClaimantPlanAccepted ? date.date : getPaymentDate(claim), lng));
   return [
     summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COURT_REPAYMENT_PLAN', { lng }), t('PAGES.CHECK_YOUR_ANSWER.REPAYMENT_IN_FULL', { lng, paymentDate })),
     summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COMPLETION_DATE_CYA', { lng }), `${paymentDate}`),
   ];
 };
 
-const buildSummaryCourtDecisionStatement = (lng: string) => {
-  return summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COURT_DECISION_ROW', { lng }), t('PAGES.CHECK_YOUR_ANSWER.COURT_REJECTED_YOUR_REPAYMENT_PLAN', { lng }));
+const buildSummaryCourtDecisionStatement = (claimantResponse: ClaimantResponse, lng: string) => {
+  if (claimantResponse.isCourtDecisionInFavourOfClaimant) {
+    return summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COURT_DECISION_ROW', { lng }), t('PAGES.CHECK_YOUR_ANSWER.COURT_ACCEPTED_YOUR_REPAYMENT_PLAN', { lng }));
+  } else {
+    return summaryRow(t('PAGES.CHECK_YOUR_ANSWER.COURT_DECISION_ROW', { lng }), t('PAGES.CHECK_YOUR_ANSWER.COURT_REJECTED_YOUR_REPAYMENT_PLAN', { lng }));
+  }
 };
 
 export const buildSummaryForCourtDecisionDetails = (claim: Claim, lng: string): SummarySection => {
@@ -37,17 +53,19 @@ export const buildSummaryForCourtDecisionDetails = (claim: Claim, lng: string): 
     title: t('PAGES.CHECK_YOUR_ANSWER.COURT_DECISION_SECTION', { lng }),
     summaryRows: [],
   });
-  const isPayByInstallment = claimantResponse.isCourtDecisionInFavourOfDefendant && (claim.isPAPaymentOptionInstallments() || claim.isFAPaymentOptionInstallments());
-  const isPayBySetDate = claimantResponse.isCourtDecisionInFavourOfDefendant && (claim.isPAPaymentOptionByDate() || claim.isFAPaymentOptionBySetDate());
+  const isPayByInstallment = (claimantResponse.isCourtDecisionInFavourOfDefendant && (claim.isPAPaymentOptionInstallments() || claim.isFAPaymentOptionInstallments()))
+    || (claimantResponse.isCourtDecisionInFavourOfClaimant && claimantResponse.isClaimantSuggestedPayByInstalments);
+  const isPayBySetDate = (claimantResponse.isCourtDecisionInFavourOfDefendant && (claim.isPAPaymentOptionByDate() || claim.isFAPaymentOptionBySetDate()))
+    || (claimantResponse.isCourtDecisionInFavourOfClaimant && claimantResponse.isClaimantSuggestedPayByDate);
   if (isPayByInstallment) {
-    courtDecision.summaryList.rows.push(buildSummaryCourtDecisionStatement(lng));
-    courtDecision.summaryList.rows.push(...buildSummaryCourtDecisionByInstallments(claim, lng));
+    courtDecision.summaryList.rows.push(buildSummaryCourtDecisionStatement(claimantResponse, lng));
+    courtDecision.summaryList.rows.push(...buildSummaryCourtDecisionByInstallments(claim, claimantResponse.isCourtDecisionInFavourOfClaimant, lng));
   }
 
   if (isPayBySetDate) {
-    courtDecision.summaryList.rows.push(buildSummaryCourtDecisionStatement(lng));
-    courtDecision.summaryList.rows.push(...buildSummaryCourtDecisionBySetDate(claim, lng));
+    courtDecision.summaryList.rows.push(buildSummaryCourtDecisionStatement(claimantResponse, lng));
+    courtDecision.summaryList.rows.push(...buildSummaryCourtDecisionBySetDate(claim, claimantResponse.isCourtDecisionInFavourOfClaimant, lng));
   }
 
-  if (isPayByInstallment || isPayBySetDate) return courtDecision;
+  if (claimantResponse.isCourtDecisionInFavourOfClaimant || claimantResponse.isCourtDecisionInFavourOfDefendant) return courtDecision;
 };
