ef151e4976c74b519952b3510770561d1164cb68
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-04-09 08:15:16 +0100

    create divider and page Information components

--- src/main/routes/features/document/mediation/viewSettlementAgreementDocumentController.ts
+++ src/main/routes/features/document/mediation/viewSettlementAgreementDocumentController.ts
@@ -4,21 +4,27 @@ import {CivilServiceClient} from 'client/civilServiceClient';
 import {VIEW_MEDIATION_SETTLEMENT_AGREEMENT_DOCUMENT} from 'routes/urls';
 import {AppRequest} from 'models/AppRequest';
 import {FileSectionBuilder} from 'models/commons/fileSectionBuilder';
+import {caseNumberPrettify} from "common/utils/stringUtils";
+import {Claim} from "models/claim";
 
 const viewMediationSettlementAgreementDocument = Router();
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl, true);
 const viewDocuments = 'features/common/viewDocuments';
 
-const renderView = (res: Response): void => {
+const renderView = (res: Response, claimId: string, claim: Claim): void => {
   const element = new FileSectionBuilder()
     .addRow('test', 'test')
     .addRow('test', 'test')
     .addRow('test', 'test')
     .build();
+
   res.render(viewDocuments, {
     items: element,
+    pageCaption: 'pageCaption',
     pageTitle: 'pageTitle',
+    claimId: caseNumberPrettify(claimId),
+    claimAmount: 1500, //TODO see why totalClaimAmount is empty
   });
 };
 
@@ -28,7 +34,7 @@ viewMediationSettlementAgreementDocument.get(VIEW_MEDIATION_SETTLEMENT_AGREEMENT
     //const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
     console.log(claim);
-    renderView(res);
+    renderView(res, claimId, claim);
   } catch (error) {
     next(error);
   }
