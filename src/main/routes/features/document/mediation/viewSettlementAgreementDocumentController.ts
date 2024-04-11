400157b997591fd9ff559a13b2e1c72c612eb1b2
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-04-11 09:02:35 +0100

    create mapper class

--- src/main/routes/features/document/mediation/viewSettlementAgreementDocumentController.ts
+++ src/main/routes/features/document/mediation/viewSettlementAgreementDocumentController.ts
@@ -1,12 +1,12 @@
 import {NextFunction, Request, RequestHandler, Response, Router} from 'express';
 import {DASHBOARD_CLAIMANT_URL, DEFENDANT_SUMMARY_URL, VIEW_MEDIATION_SETTLEMENT_AGREEMENT_DOCUMENT} from 'routes/urls';
-import {ViewDocumentsSection} from 'models/commons/viewDocumentsSectionBuilder';
 import {caseNumberPrettify} from 'common/utils/stringUtils';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {Claim} from 'models/claim';
 import {AppRequest} from 'models/AppRequest';
 import config from 'config';
 import {CivilServiceClient} from 'client/civilServiceClient';
+import {mapperMediationAgreementToDocumentView} from 'common/mappers/documentViewMapper';
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
@@ -14,9 +14,9 @@ const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServi
 const viewMediationSettlementAgreementDocument = Router();
 const viewDocuments = 'features/common/viewDocuments';
 
-const renderView = (res: Response, claimId: string, claimAmount: number, isClaimant: boolean, claim: Claim, lang: string): void => {
+const renderView = (res: Response, claimId: string, claim: Claim, lang: string): void => {
 
-  const documentsSection = new ViewDocumentsSection(
+  const documentsSection = mapperMediationAgreementToDocumentView(
     'PAGES.VIEW_MEDIATION_SETTLEMENT_AGREEMENT_DOCUMENT.DOCUMENT_TABLE_TITLE',
     claim.mediationAgreement, claim.mediationSettlementAgreedAt,
     claimId,
@@ -27,8 +27,8 @@ const renderView = (res: Response, claimId: string, claimAmount: number, isClaim
     pageCaption: 'COMMON.MEDIATION',
     pageTitle: 'PAGES.VIEW_MEDIATION_SETTLEMENT_AGREEMENT_DOCUMENT.PAGE_TITLE',
     claimId: caseNumberPrettify(claimId),
-    claimAmount: claimAmount,
-    dashboardUrl: constructResponseUrlWithIdParams(claimId, isClaimant ? DASHBOARD_CLAIMANT_URL : DEFENDANT_SUMMARY_URL),
+    claimAmount: claim.totalClaimAmount,
+    dashboardUrl: constructResponseUrlWithIdParams(claimId, claim.isClaimant() ? DASHBOARD_CLAIMANT_URL : DEFENDANT_SUMMARY_URL),
   });
 };
 
@@ -37,7 +37,7 @@ viewMediationSettlementAgreementDocument.get(VIEW_MEDIATION_SETTLEMENT_AGREEMENT
     const claimId = req.params.id;
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
-    renderView(res, claimId, claim.totalClaimAmount, claim.isClaimant(), claim, lang);
+    renderView(res, claimId, claim, lang);
   } catch (error) {
     next(error);
   }
