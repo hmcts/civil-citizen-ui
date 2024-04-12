01967c4cfdc8084de5cc21394923810c61810058
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-04-12 15:11:25 +0100

    fix colour issue

--- src/main/common/models/dashboard/taskList/dashboardTaskStatus.ts
+++ src/main/common/models/dashboard/taskList/dashboardTaskStatus.ts
@@ -1,7 +1,6 @@
 export enum DashboardTaskStatus {
   ACTION_NEEDED = 'Action needed',
   COMPLETE = 'Complete',
-  INCOMPLETE = 'Incomplete',
   DONE = 'Done',
   READY_TO_VIEW = 'Ready to r',
   NOT_AVAILABLE_YET = 'Not available yet',
@@ -13,7 +12,6 @@ export enum DashboardTaskStatus {
 export const TaskStatusColor: Record<DashboardTaskStatus, string> = {
   [DashboardTaskStatus.ACTION_NEEDED]:'govuk-tag--red',
   [DashboardTaskStatus.COMPLETE]: '',
-  [DashboardTaskStatus.INCOMPLETE]:'govuk-tag--grey',
   [DashboardTaskStatus.DONE]:'govuk-tag--green',
   [DashboardTaskStatus.READY_TO_VIEW]:'govuk-tag--red',
   [DashboardTaskStatus.NOT_AVAILABLE_YET]:'govuk-tag--grey',
