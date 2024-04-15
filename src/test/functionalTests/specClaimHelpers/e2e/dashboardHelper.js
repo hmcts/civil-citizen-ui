fa9b4f5148dcfdae93e12f1353083cb5c216777f
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2024-04-15 15:35:29 +0100

    update

--- src/test/functionalTests/specClaimHelpers/e2e/dashboardHelper.js
+++ src/test/functionalTests/specClaimHelpers/e2e/dashboardHelper.js
@@ -2,7 +2,7 @@ const I = actor();
 
 const selectors = {
   titleClass: '.govuk-notification-banner__title',
-  contentClass: '.govuk-notification-banner__content',
+  contentClass: 'div.govuk-notification-banner__content',
 };
 
 module.exports = {
@@ -13,6 +13,13 @@ module.exports = {
     }
     await I.waitForContent(title);
     await I.see(title, selectors.titleClass);
-    await I.see(content, selectors.contentClass);
+    if (Array.isArray(content)) {
+      for (let i = 0; i < content.length; i++) {
+        console.log('value verified is ..', content[i]);
+        await I.see(content[i], selectors.contentClass);
+      }
+    } else {
+      await I.see(content, selectors.contentClass);
+    }
   },
 };
