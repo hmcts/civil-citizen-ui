b4c72e09ddc749a06a3a8d39bf754b3bbe25d039
jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
2024-04-23 17:29:47 +0100

    Update routes.ts

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -281,7 +281,7 @@ import mediationConfirmationController from 'routes/features/mediation/uploadDoc
 import notificationRedirectController from 'routes/features/dashboard/notificationRedirectController';
 import claimantClaimSummaryController from 'routes/features/dashboard/claimantClaimSummaryController';
 import bundlesController from 'routes/features/caseProgression/bundlesController';
-import viewMediationSettlementAgreementDocument from 'routes/features/document/mediation/viewSettlementAgreementDocumentController'
+import viewMediationSettlementAgreementDocument from 'routes/features/document/mediation/viewSettlementAgreementDocumentController';
 import viewDefendantInformationController from './features/dashboard/viewDefendantInformationController';
 import viewClaimantInformationController from './features/dashboard/viewClaimantInformationController';
 import cancelController from 'routes/common/cancelController';
