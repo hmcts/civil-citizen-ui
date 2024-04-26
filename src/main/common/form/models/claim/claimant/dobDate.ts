162999aed48b4bfbb02986d30e9a6ede381b2d0d
jeswanth <jeswanth.reddy@hmcts.net>
2024-04-26 16:07:00 +0100

    code for incorrect error message

---
+++ src/main/common/form/models/claim/claimant/dobDate.ts
@@ -0,0 +1,17 @@
+import { IsDate, MaxDate, Validate, ValidateIf } from 'class-validator';
+import { CitizenDate } from './citizenDate';
+import { OptionalDateNotInFutureValidator } from 'common/form/validators/optionalDateNotInFutureValidator';
+import { getDOBforAgeFromCurrentTime } from 'common/utils/dateUtils';
+
+
+export class DOBDate extends CitizenDate {
+    @ValidateIf(o => (o.day < 32 && o.month < 13 && o.year > 999))
+    @IsDate({ message: 'ERRORS.VALID_DATE' })
+    @MaxDate(() => getDOBforAgeFromCurrentTime(18), { message: 'ERRORS.VALID_ENTER_A_DATE_BEFORE' })
+    date?: Date;
+
+    constructor(day?: string, month?: string, year?: string) {
+        super(day, month, year);
+        this.date = new Date(new Date(year + '-' + month + '-' + day).setHours(0, 0, 0, 0));
+    }
+}
