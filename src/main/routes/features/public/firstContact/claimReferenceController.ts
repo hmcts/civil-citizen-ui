32b7ec74e065d27ed8f903ac91386bce1122ceb5
sunilrochani <sunil.rochani@hotmail.com>
2024-04-26 14:00:43 +0100

    DTSCCI-208: Multiple Incidents: Defendant Can't link claim

--- src/main/routes/features/public/firstContact/claimReferenceController.ts
+++ src/main/routes/features/public/firstContact/claimReferenceController.ts
@@ -3,13 +3,18 @@ import {GenericForm} from 'form/models/genericForm';
 import {
   FIRST_CONTACT_PIN_URL,
   FIRST_CONTACT_CLAIM_REFERENCE_URL,
+  FIRST_CONTACT_CLAIM_SUMMARY_URL,
 } from '../../../../routes/urls';
 import {ClaimReference} from '../../../../common/models/firstContact/claimReference';
 import { AppSession } from 'common/models/AppRequest';
 import { saveFirstContactData } from 'services/firstcontact/firstcontactService';
+import {CivilServiceClient} from 'client/civilServiceClient';
+import config from 'config';
 
 const claimReferenceController = Router();
 const claimReferenceViewPath = 'features/public/firstContact/claim-reference';
+const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
+const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
 claimReferenceController.get(FIRST_CONTACT_CLAIM_REFERENCE_URL, (req: Request, res: Response) => {
   res.render(claimReferenceViewPath, { form: new GenericForm(new ClaimReference(req.body.claimReferenceValue)) });
@@ -23,7 +28,16 @@ claimReferenceController.post(FIRST_CONTACT_CLAIM_REFERENCE_URL, (async (req: Re
     res.render(claimReferenceViewPath, {form});
   } else {
     req.session = saveFirstContactData(req.session as AppSession, { claimReference: req.body.claimReferenceValue });
-    res.redirect(FIRST_CONTACT_PIN_URL);
+    if (req.body.claimReferenceValue?.includes('MC')) {
+      const linked: boolean = await civilServiceClient.isOcmcDefendantLinked(req.body.claimReferenceValue);
+      if (linked) {
+        res.redirect(FIRST_CONTACT_CLAIM_SUMMARY_URL);
+      } else {
+        res.redirect(FIRST_CONTACT_PIN_URL);
+      }
+    } else {
+      res.redirect(FIRST_CONTACT_PIN_URL);
+    }
   }
 }) as RequestHandler);
 
