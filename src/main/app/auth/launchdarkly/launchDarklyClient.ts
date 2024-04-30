98b26853ccb9a5e236b5989cabc7cf228646778e
AhsanZX97 <ahsan.zia1@hmcts.net>
2024-04-30 16:08:53 +0100

    Update launchDarklyClient.ts

--- src/main/app/auth/launchdarkly/launchDarklyClient.ts
+++ src/main/app/auth/launchdarkly/launchDarklyClient.ts
@@ -38,7 +38,7 @@ export async function isPcqShutterOn(): Promise<boolean> {
 }
 
 export async function isCUIReleaseTwoEnabled(): Promise<boolean> {
-  return await getFlagValue('cuiReleaseTwoEnabled') as boolean;
+  return true;
 }
 export async function isDashboardServiceEnabled(): Promise<boolean> {
   return await getFlagValue('dashboard-service') as boolean;
