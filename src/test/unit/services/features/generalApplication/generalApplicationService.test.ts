739b737ee415f40c985dde04216af019e364c4a2
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-05-01 12:47:56 +0100

    CIV-13398 checkstyle fixed

--- src/test/unit/services/features/generalApplication/generalApplicationService.test.ts
+++ src/test/unit/services/features/generalApplication/generalApplicationService.test.ts
@@ -1,12 +1,18 @@
 import * as draftStoreService from '../../../../../main/modules/draft-store/draftStoreService';
 import {Claim} from 'models/claim';
-import {saveApplicationCosts, saveApplicationType } from 'services/features/generalApplication/generalApplicationService';
+import {getCancelUrl, saveApplicationCosts, saveApplicationType} from 'services/features/generalApplication/generalApplicationService';
 import {ApplicationType, ApplicationTypeOption} from 'common/models/generalApplication/applicationType';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
 import {YesNo} from 'form/models/yesNo';
+import {isDashboardServiceEnabled} from '../../../../../main/app/auth/launchdarkly/launchDarklyClient';
+import {DASHBOARD_CLAIMANT_URL, DEFENDANT_SUMMARY_URL, OLD_DASHBOARD_CLAIMANT_URL} from 'routes/urls';
+import {GeneralApplication} from 'models/generalApplication/GeneralApplication';
+import {CaseRole} from 'form/models/caseRoles';
 
 jest.mock('../../../../../main/modules/draft-store');
 jest.mock('../../../../../main/modules/draft-store/draftStoreService');
+jest.mock('../../../../../main/app/auth/launchdarkly/launchDarklyClient');
+
 const mockGetCaseData = draftStoreService.getCaseDataFromStore as jest.Mock;
 
 describe('General Application service', () => {
@@ -41,18 +47,18 @@ describe('General Application service', () => {
     it('should save application costs selected', async () => {
       //Given
       mockGetCaseData.mockImplementation(async () => {
-        return new Claim();
+        const claim = new Claim();
+        claim.generalApplication = new GeneralApplication();
+        return claim;
       });
       const spy = jest.spyOn(draftStoreService, 'saveDraftClaim');
       const mockSaveClaim = draftStoreService.saveDraftClaim as jest.Mock;
-      mockSaveClaim.mockResolvedValue(() => {});
-
+      mockSaveClaim.mockResolvedValue(() => { return new Claim(); });
       //When
       await saveApplicationCosts('123', YesNo.YES);
       //Then
       expect(spy).toBeCalled();
     });
-
     it('should throw error when draft store throws error', async () => {
       //Given
       mockGetCaseData.mockImplementation(async () => {
@@ -65,6 +71,42 @@ describe('General Application service', () => {
       });
       //Then
       await expect(saveApplicationCosts('123', YesNo.NO)).rejects.toThrow(TestMessages.REDIS_FAILURE);
+    });
+  });
+
+  describe('Get cancel button url', () => {
+    it('should return claimant new dashboard url when user is claimant and dashboard feature flag is enabled', async () => {
+      //Given
+      (isDashboardServiceEnabled as jest.Mock).mockReturnValueOnce(true);
+      const claim = new Claim();
+      claim.caseRole = CaseRole.CLAIMANT;
+      claim.generalApplication = new GeneralApplication();
+      //When
+      const cancelUrl = await getCancelUrl('123',claim);
+      //Then
+      expect(cancelUrl).toEqual(DASHBOARD_CLAIMANT_URL.replace(':id','123'));
+    });
+    it('should return claimant old dashboard url when user is claimant and dashboard feature flag is disabled', async () => {
+      //Given
+      (isDashboardServiceEnabled as jest.Mock).mockReturnValueOnce(false);
+      const claim = new Claim();
+      claim.caseRole = CaseRole.CLAIMANT;
+      claim.generalApplication = new GeneralApplication();
+      //When
+      const cancelUrl = await getCancelUrl('123',claim);
+      //Then
+      expect(cancelUrl).toEqual(OLD_DASHBOARD_CLAIMANT_URL.replace(':id','123'));
+    });
+    it('should return defendant dashboard url when user is defendent', async () => {
+      //Given
+      (isDashboardServiceEnabled as jest.Mock).mockReturnValueOnce(false);
+      const claim = new Claim();
+      claim.caseRole = CaseRole.DEFENDANT;
+      claim.generalApplication = new GeneralApplication();
+      //When
+      const cancelUrl = await getCancelUrl('123',claim);
+      //Then
+      expect(cancelUrl).toEqual(DEFENDANT_SUMMARY_URL.replace(':id','123'));
     });
   });
 });
