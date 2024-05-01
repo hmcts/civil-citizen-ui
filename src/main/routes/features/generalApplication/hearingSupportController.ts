41b7deeddf2ddf12af2441486942ef36b4520065
paul-pearson <paul.pearson@version1.com>
2024-05-01 16:06:36 +0100

    CIV-13455 Tidy up

--- src/main/routes/features/generalApplication/hearingSupportController.ts
+++ src/main/routes/features/generalApplication/hearingSupportController.ts
@@ -8,20 +8,25 @@ import {generateRedisKey} from 'modules/draft-store/draftStoreService';
 import {getClaimById} from 'modules/utilityService';
 import {t} from 'i18next';
 import {HearingSupport} from 'models/generalApplication/hearingSupport';
+import {Claim} from 'models/claim';
 
 const hearingSupportController = Router();
 const viewPath = 'features/generalApplication/hearing-support';
 const backLinkUrl = 'test'; // TODO: add url
 
+async function renderView(claimId: string, claim: Claim, form: GenericForm<HearingSupport>, res: Response): Promise<void> {
+  const applicationType = selectedApplicationType[claim.generalApplication?.applicationType?.option];
+  const cancelUrl = await getCancelUrl(claimId, claim);
+  res.render(viewPath, { form, cancelUrl, backLinkUrl, applicationType, headingTitle: t('PAGES.GENERAL_APPLICATION.HEARING_SUPPORT.TITLE') });
+}
+
 hearingSupportController.get(HEARING_SUPPORT_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
     const claim = await getClaimById(claimId, req, true);
-    const cancelUrl = await getCancelUrl(claimId, claim);
-    const applicationType = selectedApplicationType[claim.generalApplication?.applicationType?.option];
     const hearingSupport = claim.generalApplication?.hearingSupport || new HearingSupport([]);
     const form = new GenericForm(hearingSupport);
-    res.render(viewPath, { form, cancelUrl, backLinkUrl, applicationType, headingTitle: t('PAGES.GENERAL_APPLICATION.HEARING_SUPPORT.TITLE') });
+    await renderView(claimId, claim, form, res);
   } catch (error) {
     next(error);
   }
@@ -31,8 +36,6 @@ hearingSupportController.post(HEARING_SUPPORT_URL, (async (req: AppRequest | Req
   try {
     const claimId = req.params.id;
     const claim = await getClaimById(claimId, req, true);
-    const cancelUrl = await getCancelUrl(claimId, claim);
-    const applicationType = selectedApplicationType[claim.generalApplication?.applicationType?.option];
     const redisKey = generateRedisKey(<AppRequest>req);
     const hearingSupport: HearingSupport = new HearingSupport(HearingSupport.convertToArray(req.body.requiredSupport),
       req.body.signLanguageContent, req.body.languageContent, req.body.otherContent);
@@ -40,7 +43,7 @@ hearingSupportController.post(HEARING_SUPPORT_URL, (async (req: AppRequest | Req
     const form = new GenericForm(hearingSupport);
     await form.validate();
     if (form.hasErrors()) {
-      res.render(viewPath, { form, cancelUrl, backLinkUrl, applicationType, headingTitle: t('PAGES.GENERAL_APPLICATION.HEARING_SUPPORT.TITLE') });
+      await renderView(claimId, claim, form, res);
     } else {
       await saveHearingSupport(redisKey, hearingSupport);
       res.redirect('test'); // TODO: add url
