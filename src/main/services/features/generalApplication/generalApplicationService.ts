67123da15eda7912615e54cdc92bb6951e85dd3d
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-05-02 15:51:45 +0200

    CIV-13551 : Service changes
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/services/features/generalApplication/generalApplicationService.ts
+++ src/main/services/features/generalApplication/generalApplicationService.ts
@@ -1,6 +1,11 @@
 import {getCaseDataFromStore, saveDraftClaim} from 'modules/draft-store/draftStoreService';
 import {GeneralApplication} from 'common/models/generalApplication/GeneralApplication';
 import {ApplicationType} from 'common/models/generalApplication/applicationType';
+import {isDashboardServiceEnabled} from 'app/auth/launchdarkly/launchDarklyClient';
+import {DASHBOARD_CLAIMANT_URL, DEFENDANT_SUMMARY_URL, OLD_DASHBOARD_CLAIMANT_URL} from '../../../routes/urls';
+import {Claim} from 'common/models/claim';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+import {YesNo} from 'form/models/yesNo';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('claimantResponseService');
@@ -10,6 +15,28 @@ export const saveApplicationType = async (claimId: string, applicationType: Appl
     const claim = await getCaseDataFromStore(claimId, true);
     claim.generalApplication = Object.assign(new GeneralApplication(), claim.generalApplication);
     claim.generalApplication.applicationType = applicationType;
+    await saveDraftClaim(claimId, claim);
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+};
+
+export const getCancelUrl = async (claimId: string, claim: Claim): Promise<string> => {
+  if (claim.isClaimant()) {
+    const isDashboardEnabled = await isDashboardServiceEnabled();
+    if (isDashboardEnabled) {
+      return constructResponseUrlWithIdParams(claimId, DASHBOARD_CLAIMANT_URL);
+    }
+    return constructResponseUrlWithIdParams(claimId, OLD_DASHBOARD_CLAIMANT_URL);
+  }
+  return constructResponseUrlWithIdParams(claimId, DEFENDANT_SUMMARY_URL);
+};
+
+export const saveRespondentAgreeToOrder = async (claimId: string, claim: Claim, respondentAgreeToOrder: YesNo): Promise<void> => {
+  try {
+    claim.generalApplication = Object.assign(new GeneralApplication(), claim.generalApplication);
+    claim.generalApplication.respondentAgreeToOrder = respondentAgreeToOrder;
     await saveDraftClaim(claimId, claim);
   } catch (error) {
     logger.error(error);
