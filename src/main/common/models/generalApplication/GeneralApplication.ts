be42be6d5baa16dcbffbbb89fe5dd97e45393598
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-05-02 15:50:33 +0200

    CIV-13551 : Model
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/common/models/generalApplication/GeneralApplication.ts
+++ src/main/common/models/generalApplication/GeneralApplication.ts
@@ -1,10 +1,13 @@
 import {ApplicationType} from './applicationType';
+import {YesNo} from 'form/models/yesNo';
 
 export class GeneralApplication {
- 
-  applicationType?: ApplicationType;
 
-  constructor(applicationType?: ApplicationType) {
+  applicationType?: ApplicationType;
+  respondentAgreeToOrder?: YesNo;
+
+  constructor(applicationType?: ApplicationType, agreeToOrder?: YesNo) {
     this.applicationType = applicationType;
+    this.respondentAgreeToOrder = agreeToOrder;
   }
 }
