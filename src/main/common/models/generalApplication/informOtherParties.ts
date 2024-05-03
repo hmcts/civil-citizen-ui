07c80db2b95b491de23a1fdf66aa4cfe442b6785
jeswanth <jeswanth.reddy@hmcts.net>
2024-05-03 16:02:44 +0100

    added validation for text area

--- src/main/common/models/generalApplication/informOtherParties.ts
+++ src/main/common/models/generalApplication/informOtherParties.ts
@@ -1,8 +1,13 @@
+import { IsDefined, IsNotEmpty, ValidateIf } from 'class-validator';
 import { GenericYesNo } from 'common/form/models/genericYesNo';
+import { YesNo } from 'common/form/models/yesNo';
 
 export class InformOtherParties extends GenericYesNo {
 
-  reasonForCourtNotInformingOtherParties?: string;
+  @ValidateIf(o => o.option == YesNo.NO)
+  @IsDefined({ message: 'ERRORS.GENERAL_APPLICATION.EXPLAIN_DO_NOT_WANT_COURT' })
+  @IsNotEmpty({ message: 'ERRORS.GENERAL_APPLICATION.EXPLAIN_DO_NOT_WANT_COURT' })
+    reasonForCourtNotInformingOtherParties?: string;
 
   constructor(option?: string, reasonForCourtNotInformingOtherParties?: string) {
     super(option, 'ERRORS.GENERAL_APPLICATION.NEED_TO_TELL');
