bfa6f0d3b544191718d8fe4d93353172db2f72e2
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-05-08 12:17:41 +0200

    CIV-13407 : Url
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/routes/urls.ts
+++ src/main/routes/urls.ts
@@ -306,3 +306,4 @@ export const VIEW_RESPONSE_TO_CLAIM = `${BASE_CASE_URL}/view-response-to-claim`;
 export const VIEW_MEDIATION_DOCUMENTS = `${MEDIATION_URL}/view-mediation-documents`;
 export const HEARING_SUPPORT_URL = `${BASE_GENERAL_APPLICATION_URL}/hearing-support`;
 export const GA_AGREEMENT_FROM_OTHER_PARTY = `${BASE_GENERAL_APPLICATION_URL}/agreement-from-other-party`;
+export const GA_REQUESTING_REASON_URL = `${BASE_GENERAL_APPLICATION_URL}/requesting-reason`;
