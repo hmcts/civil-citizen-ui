cf1cb05e083df8c06c2a795e028a4f6d2345caca
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2024-05-09 17:39:46 +0100

    update

--- src/test/functionalTests/tests/nonprod/LipvLip_Api_FullAdmit_PaySetDate_SSA_tests.js
+++ src/test/functionalTests/tests/nonprod/LipvLip_Api_FullAdmit_PaySetDate_SSA_tests.js
@@ -24,6 +24,7 @@ Scenario('Create LipvLip claim and defendant response as FullAdmit pay by set da
     caseData = await api.retrieveCaseData(config.adminUser, claimRef);
     claimNumber = await caseData.legacyCaseReference;
     const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+    console.log('isDashboardServiceEnabled..', isDashboardServiceEnabled);
     await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.admitAllPayBySetDateWithIndividual);
     await api.waitForFinishedBusinessProcess();
     await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
