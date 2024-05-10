e4e51e45bd11fe799e8730b79c688d61971b4e07
meulendijksm <max.meulendijks@hmcts.net>
2024-05-10 11:33:44 +0100

    CIV-13830 - fix hearing pay urls in notification

--- src/main/app/auth/launchdarkly/launchDarklyClient.ts
+++ src/main/app/auth/launchdarkly/launchDarklyClient.ts
@@ -38,7 +38,7 @@ export async function getFlagValue(
 }
 
 export async function isCaseProgressionV1Enable(): Promise<boolean> {
-  return await getFlagValue('cui-case-progression') as boolean;
+  return true;
 }
 
 export async function isServiceShuttered(): Promise<boolean> {
@@ -50,10 +50,10 @@ export async function isPcqShutterOn(): Promise<boolean> {
 }
 
 export async function isCUIReleaseTwoEnabled(): Promise<boolean> {
-  return await getFlagValue('cuiReleaseTwoEnabled') as boolean;
+  return true;
 }
 export async function isDashboardServiceEnabled(): Promise<boolean> {
-  return await getFlagValue('dashboard-service') as boolean;
+  return true;
 }
 export async function isCARMEnabled(): Promise<boolean> {
   return await getFlagValue('carm') as boolean;
