e9ba82a01904cf86d267126782b5b7b714ff9b7e
paul-pearson <paul.pearson@version1.com>
2024-05-13 16:50:53 +0100

    CIV-9442 Fix test after merge

--- src/test/unit/routes/features/claim/generalApplication/applicationCostsController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/applicationCostsController.test.ts
@@ -3,14 +3,43 @@ import request from 'supertest';
 import {GA_APPLICATION_COSTS_URL} from 'routes/urls';
 import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
 import {t} from 'i18next';
-import {mockCivilClaim, mockRedisFailure} from '../../../../../utils/mockDraftStore';
 import {getApplicationCostsContent} from 'services/features/generalApplication/applicationCostsService';
 import {PageSectionBuilder} from 'common/utils/pageSectionBuilder';
+import {Claim} from 'models/claim';
+import {GeneralApplication} from 'models/generalApplication/GeneralApplication';
+import {ApplicationType, ApplicationTypeOption} from 'models/generalApplication/applicationType';
+import * as utilityService from 'modules/utilityService';
+import config from 'config';
+import nock from 'nock';
+import {isGaForLipsEnabled} from '../../../../../../main/app/auth/launchdarkly/launchDarklyClient';
 
 jest.mock('../../../../../../main/modules/oidc');
+jest.mock('../../../../../../main/modules/draft-store/draftStoreService');
+jest.mock('../../../../../../main/modules/utilityService');
+jest.mock('../../../../../../main/app/auth/launchdarkly/launchDarklyClient');
 jest.mock('../../../../../../main/services/features/generalApplication/applicationCostsService');
 
+const mockGetClaim = utilityService.getClaimById as jest.Mock;
+
 describe('General Application - Application costs', () => {
+  const citizenRoleToken: string = config.get('citizenRoleToken');
+  const idamUrl: string = config.get('idamUrl');
+
+  beforeAll(() => {
+    nock(idamUrl)
+      .post('/o/token')
+      .reply(200, {id_token: citizenRoleToken});
+    (isGaForLipsEnabled as jest.Mock).mockResolvedValue(true);
+  });
+
+  beforeEach(() => {
+    mockGetClaim.mockImplementation(() => {
+      const claim = new Claim();
+      claim.generalApplication = new GeneralApplication(new ApplicationType(ApplicationTypeOption.SETTLE_BY_CONSENT));
+      return claim;
+    });
+  });
+
   const mockContent = getApplicationCostsContent as jest.Mock;
   mockContent.mockImplementation(() => {
     return new PageSectionBuilder().build();
@@ -18,8 +47,6 @@ describe('General Application - Application costs', () => {
 
   describe('on GET', () => {
     it('should return page', async () => {
-      app.locals.draftStoreClient = mockCivilClaim;
-
       await request(app)
         .get(GA_APPLICATION_COSTS_URL)
         .expect((res) => {
@@ -29,7 +56,9 @@ describe('General Application - Application costs', () => {
     });
 
     it('should return http 500 when has error in the get method', async () => {
-      app.locals.draftStoreClient = mockRedisFailure;
+      mockGetClaim.mockImplementation(() => {
+        throw new Error(TestMessages.REDIS_FAILURE);
+      });
       await request(app)
         .get(GA_APPLICATION_COSTS_URL)
         .expect((res) => {
