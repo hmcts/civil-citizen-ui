b58c027d1e1418d1a1537d72e25ae72c7aa07841
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-05-14 17:18:49 +0100

    fixed lint issues

--- src/test/functionalTests/specClaimHelpers/fixtures/cookies/exuiCookies.js
+++ src/test/functionalTests/specClaimHelpers/fixtures/cookies/exuiCookies.js
@@ -2,10 +2,10 @@ const config = require('../../../../config');
 const getDomain = config.getDomain;
 
 module.exports = (userId) => [
-    {
-      name: `hmcts-exui-cookies-${userId}-mc-accepted`,
-      value: 'true',
-      domain: getDomain(config.url.manageCase),
-      path: '/',
-    },
+  {
+    name: `hmcts-exui-cookies-${userId}-mc-accepted`,
+    value: 'true',
+    domain: getDomain(config.url.manageCase),
+    path: '/',
+  },
 ];
