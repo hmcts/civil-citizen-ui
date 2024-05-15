12f14e129e5518a637e0446d05a1efb477fb91a6
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-05-15 10:30:30 +0100

    add view mediation documents

--- src/main/routes/features/mediation/uploadDocuments/confirmationController.ts
+++ src/main/routes/features/mediation/uploadDocuments/confirmationController.ts
@@ -1,7 +1,7 @@
 import {RequestHandler, Response, Router} from 'express';
 import {
   MEDIATION_UPLOAD_DOCUMENTS_CONFIRMATION,
-  START_MEDIATION_UPLOAD_FILES,
+  VIEW_MEDIATION_DOCUMENTS,
 } from 'routes/urls';
 
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
@@ -15,7 +15,7 @@ const checkAnswersViewPath = 'features/common/confirmation-page.njk';
 const mediationConfirmationController = Router();
 const MEDIATION_CONFIRMATION_PAGE = 'PAGES.MEDIATION.CONFIRMATION_PAGE.';
 export const getContent = (claimId: string): ClaimSummarySection[] => {
-  const startMediationUploadUrl = constructResponseUrlWithIdParams(claimId,START_MEDIATION_UPLOAD_FILES);
+  const startMediationUploadUrl = constructResponseUrlWithIdParams(claimId,VIEW_MEDIATION_DOCUMENTS);
   return new PageSectionBuilder()
     .addLink(`${MEDIATION_CONFIRMATION_PAGE}LINK_TEXT`,
       startMediationUploadUrl,
