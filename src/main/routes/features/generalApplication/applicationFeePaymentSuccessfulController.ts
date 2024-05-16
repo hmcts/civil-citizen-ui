29c385644c4830587b0c9abe132db06439a4e35f
jeswanth <jeswanth.reddy@hmcts.net>
2024-05-16 09:58:02 +0100

    code chnages for adjourn hearing application type

--- src/main/routes/features/generalApplication/applicationFeePaymentSuccessfulController.ts
+++ src/main/routes/features/generalApplication/applicationFeePaymentSuccessfulController.ts
@@ -1,31 +1,30 @@
-import {NextFunction, RequestHandler, Response, Router} from 'express';
-import { DASHBOARD_URL, GA_PAYMENT_SUCCESSFUL} from 'routes/urls';
-import {AppRequest} from 'models/AppRequest';
-import { CivilServiceClient } from 'client/civilServiceClient';
-import config from 'config';
+import { NextFunction, RequestHandler, Response, Router } from 'express';
+import { GA_PAYMENT_SUCCESSFUL_URL } from 'routes/urls';
+import { AppRequest } from 'models/AppRequest';
 import { getGaPaymentSuccessfulBodyContent, getGaPaymentSuccessfulButtonContent, getGaPaymentSuccessfulPanelContent } from 'services/features/generalApplication/applicationFeePaymentConfirmationContent';
+import { getCancelUrl } from 'services/features/generalApplication/generalApplicationService';
+import { generateRedisKey, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
 const applicationFeePaymentSuccessfulController: Router = Router();
 
-const paymentSuccessfulViewPath  = 'features/generalApplication/payment-successful';
-const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
-const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
+const paymentSuccessfulViewPath = 'features/generalApplication/payment-successful';
 
-async function renderView(res: Response, req: AppRequest, claimId: string, redirectUrl: string) {
-  const claim = await civilServiceClient.retrieveClaimDetails(claimId, req);
+async function renderView(res: Response, req: AppRequest, claimId: string) {
+  const redisKey = generateRedisKey(req);
+  const claim = await getCaseDataFromStore(redisKey);
   const lng = req.query.lang ? req.query.lang : req.cookies.lang;
   res.render(paymentSuccessfulViewPath,
     {
       gaPaymentSuccessfulPanel: getGaPaymentSuccessfulPanelContent(claim, lng),
       gaPaymentSuccessfulBody: getGaPaymentSuccessfulBodyContent(claim, lng),
-      gaPaymentSuccessfulButton: getGaPaymentSuccessfulButtonContent(redirectUrl),
+      gaPaymentSuccessfulButton: getGaPaymentSuccessfulButtonContent(await getCancelUrl(claimId, claim)),
     });
 }
 
-applicationFeePaymentSuccessfulController.get(GA_PAYMENT_SUCCESSFUL, (async (req: AppRequest, res: Response, next: NextFunction) => {
+applicationFeePaymentSuccessfulController.get(GA_PAYMENT_SUCCESSFUL_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
-    await renderView(res, req, claimId, DASHBOARD_URL);
-  }catch (error) {
+    await renderView(res, req, claimId);
+  } catch (error) {
     next(error);
   }
 }) as RequestHandler);
--- src/test/unit/routes/features/claim/generalApplication/applicationFeePaymentSuccessfulController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/applicationFeePaymentSuccessfulController.test.ts
@@ -2,49 +2,74 @@ import request from 'supertest';
 import {app} from '../../../../../../main/app';
 import nock from 'nock';
 import config from 'config';
-import {GA_PAYMENT_SUCCESSFUL} from 'routes/urls';
-import * as draftStoreService from 'modules/draft-store/draftStoreService';
-import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
-import { CivilServiceClient } from 'client/civilServiceClient';
+import { GA_PAYMENT_SUCCESSFUL_URL } from 'routes/urls';
 import { Claim } from 'common/models/claim';
-import { ClaimDetails } from 'common/form/models/claim/details/claimDetails';
+import { isGaForLipsEnabled } from 'app/auth/launchdarkly/launchDarklyClient';
+import * as draftService from 'modules/draft-store/draftStoreService';
+import { GeneralApplication } from 'common/models/generalApplication/GeneralApplication';
+import { ApplicationType, ApplicationTypeOption } from 'common/models/generalApplication/applicationType';
+import { CaseProgressionHearing } from 'common/models/caseProgression/caseProgressionHearing';
+import { t } from 'i18next';
+import { TestMessages } from '../../../../../utils/errorMessageTestConstants';
 
 jest.mock('../../../../../../main/modules/oidc');
-jest.mock('../../../../../../main/modules/draft-store');
-const spyDel = jest.spyOn(draftStoreService, 'deleteDraftClaimFromStore');
+jest.mock('../../../../../../main/modules/draft-store/draftStoreService');
+jest.mock('../../../../../../main/app/auth/launchdarkly/launchDarklyClient');
 
 describe('Claim fee payment confirmation', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
-
+  const mockDataFromStore = jest.spyOn(draftService, 'getCaseDataFromStore');
+  let claim: Claim;
   beforeAll(() => {
     nock(idamUrl)
       .post('/o/token')
-      .reply(200, {id_token: citizenRoleToken});
+      .reply(200, { id_token: citizenRoleToken });
+    (isGaForLipsEnabled as jest.Mock).mockResolvedValue(true);
+  });
+
+  beforeEach(() => {
+    claim = new Claim();
+    claim.generalApplication = new GeneralApplication();
+    claim.caseProgressionHearing = new CaseProgressionHearing();
   });
 
   describe('on GET', () => {
     it('should return resolving successful payment page', async () => {
-      const claim = new Claim();
-      claim.claimDetails = new ClaimDetails();
-      jest
-        .spyOn(CivilServiceClient.prototype, 'retrieveClaimDetails').mockResolvedValueOnce(claim);
+      claim.generalApplication.applicationType = new ApplicationType(ApplicationTypeOption.STAY_THE_CLAIM);
+      mockDataFromStore.mockResolvedValueOnce(claim);
       await request(app)
-        .get(GA_PAYMENT_SUCCESSFUL)
+        .get(GA_PAYMENT_SUCCESSFUL_URL)
         .expect((res) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain('Application fee');
           expect(res.text).toContain('REF-123-123');
+          expect(res.text).toContain(t('PAGES.GENERAL_APPLICATION.GA_PAYMENT_SUCCESSFUL.WHAT_HAPPENS_NEXT'));
+          expect(res.text).toContain(t('PAGES.GENERAL_APPLICATION.GA_PAYMENT_SUCCESSFUL.WHAT_HAPPENS_NEXT_PARA_1'));
+          expect(res.text).toContain(t('PAGES.GENERAL_APPLICATION.GA_PAYMENT_SUCCESSFUL.CHOOSEN_NOT_TO_INFORM_OTHER_PARTY_PARA_2'));
         });
+
+    });
+    it('should return resolving successful application submitted', async () => {
+      claim.generalApplication.applicationType = new ApplicationType(ApplicationTypeOption.ADJOURN_HEARING);
+      claim.caseProgressionHearing.hearingDate = new Date('2026-01-01');
+      mockDataFromStore.mockResolvedValueOnce(claim);
+      await request(app)
+        .get(GA_PAYMENT_SUCCESSFUL_URL)
+        .expect((res) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain('Application fee');
+          expect(res.text).toContain('Application Submitted');
+          expect(res.text).toContain(t('PAGES.GENERAL_APPLICATION.GA_PAYMENT_SUCCESSFUL.WHAT_HAPPENS_NEXT'));
+          expect(res.text).toContain(t('PAGES.GENERAL_APPLICATION.GA_PAYMENT_SUCCESSFUL.WHAT_HAPPENS_NEXT_PARA_1'));
+        });
+
     });
 
     it('should return error if there is no claim fee data', async () => {
-      jest.spyOn(CivilServiceClient.prototype, 'retrieveClaimDetails').mockRejectedValueOnce(new Error(TestMessages.SOMETHING_WENT_WRONG));
-
-      spyDel.mockImplementation(() => {return null;});
-
+      mockDataFromStore.mockRejectedValueOnce(new Error(TestMessages.SOMETHING_WENT_WRONG));
       await request(app)
-        .get(GA_PAYMENT_SUCCESSFUL)
+        .get(GA_PAYMENT_SUCCESSFUL_URL)
         .expect((res) => {
           expect(res.status).toBe(500);
           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
