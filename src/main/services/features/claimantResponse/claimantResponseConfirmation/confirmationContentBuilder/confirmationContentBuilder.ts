74a6349abc22a047ab08379b43ee88e189443876
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-05-20 17:17:43 +0100

    CIV-13683 changes

--- src/main/services/features/claimantResponse/claimantResponseConfirmation/confirmationContentBuilder/confirmationContentBuilder.ts
+++ src/main/services/features/claimantResponse/claimantResponseConfirmation/confirmationContentBuilder/confirmationContentBuilder.ts
@@ -44,12 +44,12 @@ export function buildClaimantResponseSection(claim: Claim, lang: string): ClaimS
   return getClaimantResponseStatus(claim, claimantResponseStatusTitle, lang);
 }
 
-export function buildNextStepsSection(claim: Claim, lang: string, carmApplicable: boolean): ClaimSummarySection[] {
+export function buildNextStepsSection(claim: Claim, lang: string, carmApplicable: boolean, respondToSettlementAgreementDeadLine?: Date): ClaimSummarySection[] {
   const claimantResponse = Object.assign(new ClaimantResponse(), claim.claimantResponse);
   const RCDisputeNotContinueNextSteps = getRCDisputeNotContinueNextSteps(claim, lang);
   const PAPayImmediatelyAcceptedNextSteps = getPAPayImmediatelyAcceptedNextSteps(claim, lang);
   const ccjNextSteps = getCCJNextSteps(claim, lang);
-  const SignSettlementAgreementNextSteps = getSignSettlementAgreementNextSteps(claim, lang);
+  const SignSettlementAgreementNextSteps = getSignSettlementAgreementNextSteps(claim, lang, respondToSettlementAgreementDeadLine);
   const RejectedResponseNoMediationNextSteps = getRejectedResponseNoMediationNextSteps(lang);
   const RejectedResponseYesMediationNextSteps = getRejectedResponseYesMediationNextSteps(lang);
   const RejectedResponseCarmMediationNextSteps = getMediationCarmNextSteps(lang);
