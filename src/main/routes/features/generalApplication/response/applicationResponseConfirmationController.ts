771036fd82c9c602a6296e7825955c5ab8f7d02b
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-05-20 08:59:28 +0200

    CIV-13864 : Controller
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

---
+++ src/main/routes/features/generalApplication/response/applicationResponseConfirmationController.ts
@@ -0,0 +1,18 @@
+import {NextFunction, RequestHandler, Router} from 'express';
+import {DEFENDANT_SUMMARY_URL, GA_RESPONSE_CONFIRMATION_URL,} from 'routes/urls';
+
+const submitGeneralApplicationConfirmationViewPath = 'features/generalApplication/response/application-response-confirmation';
+const applicationResponseConfirmationController = Router();
+
+applicationResponseConfirmationController.get(GA_RESPONSE_CONFIRMATION_URL, (async (req, res, next: NextFunction) => {
+  try {
+    const claimId = req.params.id;
+    res.render(submitGeneralApplicationConfirmationViewPath, {
+      dashboardUrl : DEFENDANT_SUMMARY_URL.replace(':id', claimId),
+    });
+  }catch (error) {
+    next(error);
+  }
+}) as RequestHandler);
+
+export default applicationResponseConfirmationController;
