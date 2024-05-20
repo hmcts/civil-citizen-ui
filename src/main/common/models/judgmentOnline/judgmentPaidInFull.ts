6cb49d972c81e4ad3a0282ab772a227664813713
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2024-05-20 23:18:39 +0100

    CIV-10455 update variable type to match ccd

--- src/main/common/models/judgmentOnline/judgmentPaidInFull.ts
+++ src/main/common/models/judgmentOnline/judgmentPaidInFull.ts
@@ -1,5 +1,6 @@
+
 export class JudgmentPaidInFull{
 
   dateOfFullPaymentMade: Date;
-  confirmFullPaymentMade: string;
+  confirmFullPaymentMade: string[];
 }
--- src/main/services/translation/judgmentOnline/convertToCCDjudgmentPaidInFull.ts
+++ src/main/services/translation/judgmentOnline/convertToCCDjudgmentPaidInFull.ts
@@ -4,6 +4,6 @@ import {Claim} from 'models/claim';
 export const toCCDjudgmentPaidInFull = (claim: Claim): JudgmentPaidInFull => {
   return {
     dateOfFullPaymentMade: claim.judgmentPaidInFull?.dateOfFullPaymentMade,
-    confirmFullPaymentMade: claim.judgmentPaidInFull.confirmFullPaymentMade,
+    confirmFullPaymentMade: claim.judgmentPaidInFull.confirmFullPaymentMade ? ['CONFIRMED'] : null,
   };
 };
