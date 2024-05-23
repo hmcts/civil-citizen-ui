43c5cfe35158c424183bdde34d5b41ae1dfc5a87
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-05-23 15:32:00 +0200

    CIV-9476 : Model
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/common/models/generalApplication/response/response.ts
+++ src/main/common/models/generalApplication/response/response.ts
@@ -1,15 +1,18 @@
 import {HearingArrangement} from 'models/generalApplication/hearingArrangement';
 import {HearingContactDetails} from 'models/generalApplication/hearingContactDetails';
 import {YesNo} from 'form/models/yesNo';
+import {HearingSupport} from 'models/generalApplication/hearingSupport';
 
 export class Response {
   hearingArrangement?: HearingArrangement;
   hearingContactDetails?: HearingContactDetails;
+  hearingSupport?: HearingSupport;
   agreeToOrder?: YesNo;
 
-  constructor(hearingArrangement?: HearingArrangement, hearingContactDetails?: HearingContactDetails, agreeToOrder?: YesNo) {
+  constructor(hearingArrangement?: HearingArrangement, hearingContactDetails?: HearingContactDetails, agreeToOrder?: YesNo, hearingSupport?: HearingSupport) {
     this.hearingArrangement = hearingArrangement;
     this.hearingContactDetails = hearingContactDetails;
     this.agreeToOrder = agreeToOrder;
+    this.hearingSupport = hearingSupport;
   }
 }
