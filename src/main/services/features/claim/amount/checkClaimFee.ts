7d3d8a22b4edd0acaa5d3c191e58f462b0cc1996
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-05-24 13:48:06 +0200

    CIV-13738 : Stop storing the fee when the fee has changed to prevent re-access the pay the claim fee screen
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/services/features/claim/amount/checkClaimFee.ts
+++ src/main/services/features/claim/amount/checkClaimFee.ts
@@ -1,9 +1,7 @@
-import { Claim } from 'common/models/claim';
+import {Claim} from 'common/models/claim';
 import config from 'config';
 import {CivilServiceClient} from 'client/civilServiceClient';
 import {AppRequest} from 'models/AppRequest';
-import {saveClaimFee} from 'services/features/claim/amount/claimFeesService';
-import {generateRedisKey} from 'modules/draft-store/draftStoreService';
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
@@ -11,7 +9,6 @@ export const checkIfClaimFeeHasChanged = async (claimId: string, claim: Claim, r
   const newClaimFeeData = await civilServiceClient.getClaimFeeData(claim.totalClaimAmount, req);
   const oldClaimFee = claim.claimFee?.calculatedAmountInPence;
   if (oldClaimFee !== newClaimFeeData?.calculatedAmountInPence) {
-    await saveClaimFee(generateRedisKey(req), newClaimFeeData);
     return true;
   } else {
     return false;
