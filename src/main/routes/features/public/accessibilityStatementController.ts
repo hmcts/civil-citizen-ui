9fd17e0709fef7f41269c75b8fe164224f8baa32
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-05-24 13:38:46 +0200

    Fix: controller and test

--- src/main/routes/features/public/accessibilityStatementController.ts
+++ src/main/routes/features/public/accessibilityStatementController.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
-import {ACCESSIBILITY_STATEMENT_URL} from '../../../routes/urls';
+import {ACCESSIBILITY_STATEMENT_URL, DASHBOARD_URL} from '../../../routes/urls';
 
 const accessibilityStatementController = Router();
+const accessibilityStatementViewPath = 'features/public/accessibility-statement';
 
 accessibilityStatementController.get(ACCESSIBILITY_STATEMENT_URL, (req, res) => {
-  res.render('features/public/accessibility-statement');
+  res.render(
+    accessibilityStatementViewPath,
+    { dashboardUrl: DASHBOARD_URL }
+  );
 });
 
 export default accessibilityStatementController;
