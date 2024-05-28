5ad6cc9c5c53efdd3bdd5979d31a9dbaf7961730
LeonardoPalmeiro <93268657+LeonardoPalmeiro@users.noreply.github.com>
2024-05-28 09:46:44 +0100

    CIV-12675 status changes for mediation document upload (#3732)
    * add mediation variable and unit test for uploadMediation dashboard
    
    * updated cs image
    
    * fix pipeline
    
    * point to latest
    
    ---------
    
    Co-authored-by: Madhan Mahadevan <madhan.mahadevan@hmcts.net>
    Co-authored-by: amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
    Co-authored-by: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>

--- src/main/services/dashboard/dashboardInterpolationService.ts
+++ src/main/services/dashboard/dashboardInterpolationService.ts
@@ -27,6 +27,7 @@ import {
   UPLOAD_YOUR_DOCUMENTS_URL,
   VIEW_ORDERS_AND_NOTICES_URL,
   EVIDENCE_UPLOAD_DOCUMENTS_URL,
+  VIEW_MEDIATION_DOCUMENTS,
 } from 'routes/urls';
 import config from 'config';
 import {getTotalAmountWithInterestAndFees} from 'modules/claimDetailsService';
@@ -106,6 +107,7 @@ const setDashboardValues = (claim: Claim, claimId: string, notification?: Dashbo
   valuesMap.set('{VIEW_MEDIATION_SETTLEMENT_AGREEMENT}', VIEW_MEDIATION_SETTLEMENT_AGREEMENT_DOCUMENT.replace(':id', claimId));
   valuesMap.set('{UPLOAD_MEDIATION_DOCUMENTS}', START_MEDIATION_UPLOAD_FILES.replace(':id', claimId));
   valuesMap.set('{VIEW_EVIDENCE_UPLOAD_DOCUMENTS}', EVIDENCE_UPLOAD_DOCUMENTS_URL.replace(':id', claimId));
+  valuesMap.set('{VIEW_MEDIATION_DOCUMENTS}', VIEW_MEDIATION_DOCUMENTS.replace(':id', claimId));
 
   if (claimantRequirements) {
     valuesMap.set('{VIEW_CLAIMANT_HEARING_REQS_SIZE}', displayDocumentSizeInKB(claimantRequirements.documentSize));
