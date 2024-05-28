09f8b6b2875281ec0de855ef1d0cd6f64c90824c
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-05-28 13:15:22 +0100

    CIV-13546 Duplicated url

--- src/main/services/dashboard/dashboardInterpolationService.ts
+++ src/main/services/dashboard/dashboardInterpolationService.ts
@@ -110,7 +110,6 @@ const setDashboardValues = (claim: Claim, claimId: string, notification?: Dashbo
   valuesMap.set('{VIEW_EVIDENCE_UPLOAD_DOCUMENTS}', EVIDENCE_UPLOAD_DOCUMENTS_URL.replace(':id', claimId));
   valuesMap.set('{GENERAL_APPLICATIONS_INITIATION_PAGE_URL}', '#');
   valuesMap.set('{VIEW_MEDIATION_DOCUMENTS}', VIEW_MEDIATION_DOCUMENTS.replace(':id', claimId));
-  valuesMap.set('{GENERAL_APPLICATIONS_INITIATION_PAGE_URL}', '#');
   valuesMap.set('{CONFIRM_YOU_HAVE_BEEN_PAID_URL}', CONFIRM_YOU_HAVE_BEEN_PAID_URL.replace(':id', claimId));
 
   if (claimantRequirements) {
