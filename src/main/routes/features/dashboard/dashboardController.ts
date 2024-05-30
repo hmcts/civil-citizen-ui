f97148c2b868e47c4d2191752b33cc9e90fbc00d
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-05-30 11:01:17 +0100

    CIV-14001 Add GA Endpoint

--- src/main/routes/features/dashboard/dashboardController.ts
+++ src/main/routes/features/dashboard/dashboardController.ts
@@ -8,13 +8,17 @@ import {CivilServiceClient} from 'client/civilServiceClient';
 import {DraftClaimData, getDraftClaimData} from 'services/dashboard/draftClaimService';
 import { buildPagination } from 'services/features/dashboard/claimPaginationService';
 import { DashboardClaimantResponse, DashboardDefendantResponse } from 'common/models/dashboard/dashboarddefendantresponse';
+import {GeneralApplicationClient} from 'client/generalApplicationClient';
+import {ApplicationResponse} from 'models/generalApplication/applicationResponse';
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
+const generalApplicationApiBaseUrl = config.get<string>('services.generalApplication.url');
+const generalApplicationClient: GeneralApplicationClient = new GeneralApplicationClient(generalApplicationApiBaseUrl);
 
 function renderPage(res: Response, claimsAsClaimant: DashboardClaimantItem[], claimDraftSaved: DashboardClaimantItem,
   claimsAsDefendant: DashboardDefendantItem[], responseDraftSaved: boolean, draftClaimUrl: string,
-  paginationArgumentClaimant: object, paginationArgumentDefendant: object, lang: string ): void {
+  paginationArgumentClaimant: object, paginationArgumentDefendant: object, lang: string, applications: ApplicationResponse[] ): void {
 
   res.render('features/dashboard/dashboard', {
     claimsAsClaimant,
@@ -25,6 +29,7 @@ function renderPage(res: Response, claimsAsClaimant: DashboardClaimantItem[], cl
     paginationArgumentDefendant,
     lang,
     newOcmcClaimUrl: draftClaimUrl,
+    applications,
   });
 }
 
@@ -45,7 +50,8 @@ dashboardController.get(DASHBOARD_URL, (async function (req, res, next) {
     const responseDraftSaved = false;
     const paginationArgumentClaimant = buildPagination(claimsAsClaimant.totalPages, req.query?.claimantPage as string, lang, 'claimantPage', defendantPage);
     const draftClaimUrl = draftClaimData?.claimCreationUrl;
-    renderPage(res, claimsAsClaimant.claims, claimDraftSaved, claimsAsDefendant.claims, responseDraftSaved, draftClaimUrl, paginationArgumentClaimant, claimsAsDefendantPaginationList, lang);
+    const applications: ApplicationResponse[] = await generalApplicationClient.getApplications(appRequest);
+    renderPage(res, claimsAsClaimant.claims, claimDraftSaved, claimsAsDefendant.claims, responseDraftSaved, draftClaimUrl, paginationArgumentClaimant, claimsAsDefendantPaginationList, lang, applications);
   }catch(error){
     next(error);
   }
