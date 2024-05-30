be592bb1570c5bbff083fe78fe9cdf63a6f8815c
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-05-30 10:13:07 +0200

    Fix: test coverage

--- src/test/unit/services/features/claim/checkAnswers/detailsSection/buildTheirDetailsSection.test.ts
+++ src/test/unit/services/features/claim/checkAnswers/detailsSection/buildTheirDetailsSection.test.ts
@@ -41,6 +41,36 @@ const CLAIM_ID = 'claimId';
 const INDEX_THEIRDETAILS_SECTION = 1;
 
 describe('Citizen Details Section', () => {
+  it('should return your Company details summary sections wehen delayed flight Yes', async () => {
+    //Given
+    const claim = createClaimWithIndividualDetails();
+    claim.respondent1.type = PartyType.COMPANY;
+    claim.delayedFlight = new GenericYesNo(YesNo.YES);
+    claim.flightDetails = new FlightDetails('test', '123', '2023', '1', '1');
+    claim.respondent1.partyDetails.contactPerson = CONTACT_PERSON;
+    //When
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'en');
+    //Then
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[0].value.html).toBe('Nice organisation');
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[1].value.html).toBe('The Post Man');
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[4].value.html).toBe('COMMON.VARIATION.YES');
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[5].value.html).toBe('test');
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[6].value.html).toBe('123');
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[7].value.html).toBe('1 January 2023');
+  });
+  it('should return your Company details summary sections wehen delayed flight No', async () => {
+    //Given
+    const claim = createClaimWithIndividualDetails();
+    claim.respondent1.type = PartyType.COMPANY;
+    claim.delayedFlight = new GenericYesNo(YesNo.NO);
+    claim.respondent1.partyDetails.contactPerson = CONTACT_PERSON;
+    //When
+    const summarySections = await getSummarySections(CLAIM_ID, claim, 'en');
+    //Then
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[0].value.html).toBe('Nice organisation');
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[1].value.html).toBe('The Post Man');
+    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[4].value.html).toBe('COMMON.VARIATION.NO');
+  });
   it('should build Their Details Section', async () => {
     //Given
     const claim = createClaimWithIndividualDetails();
@@ -148,24 +178,6 @@ describe('Citizen Details Section', () => {
     //Then
     expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[1].value.html).toBe('Business name');
   });
-  it('should return your Company details summary sections', async () => {
-    //Given
-    const claim = createClaimWithIndividualDetails();
-    claim.delayedFlight = new GenericYesNo(YesNo.YES);
-    claim.flightDetails = new FlightDetails('test', '123', '2023', '1', '1');
-    claim.respondent1.type = PartyType.COMPANY;
-    claim.respondent1.partyDetails.contactPerson = CONTACT_PERSON;
-    //When
-    const summarySections = await getSummarySections(CLAIM_ID, claim, 'en');
-    //Then
-    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[0].value.html).toBe('Nice organisation');
-    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[1].value.html).toBe('The Post Man');
-    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[4].value.html).toBe('COMMON.VARIATION.YES');
-    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[5].value.html).toBe('test');
-    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[6].value.html).toBe('123');
-    expect(summarySections.sections[INDEX_THEIRDETAILS_SECTION].summaryList.rows[7].value.html).toBe('1 January 2023');
-  });
-  
   it('should return your Organisation details summary sections', async () => {
     //Given
     const claim = createClaimWithIndividualDetails();
