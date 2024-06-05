bdfc2e0ca8547cfc1f8df68f06451114fb5b4dca
TurkingtonL <lillie.turkington@HMCTS.net>
2024-06-05 13:04:26 +0100

    fix: tests

--- src/test/unit/routes/features/public/eligibility/tryNewServiceController.test.ts
+++ src/test/unit/routes/features/public/eligibility/tryNewServiceController.test.ts
@@ -63,7 +63,7 @@ describe('Try the new online service', () => {
       await request(app)
         .get(url)
         .expect((res) => {
-          expect(res.status).toBe(200);
+          expect(res.status).toBe(302);
           expect(res.text).toContain(ELIGIBILITY_KNOWN_CLAIM_AMOUNT_URL);
         });
     });
@@ -77,7 +77,7 @@ describe('Try the new online service', () => {
       await request(app)
         .get(url)
         .expect((res) => {
-          expect(res.status).toBe(200);
+          expect(res.status).toBe(302);
           expect(res.text).toContain(ELIGIBILITY_CLAIM_VALUE_URL);
         });
     });
