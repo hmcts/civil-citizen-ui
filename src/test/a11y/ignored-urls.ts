423a8d840d37f5f89e3608a36b1c22c05e44440e
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-06-07 12:09:27 +0100

    fix build and a11y tests

--- src/test/a11y/ignored-urls.ts
+++ src/test/a11y/ignored-urls.ts
@@ -70,4 +70,5 @@ export const IGNORED_URLS = [
   urls.CONFIRM_YOU_HAVE_BEEN_PAID_URL, //TODO: remove this once finished the page
   urls.VIEW_THE_JUDGMENT_URL, //TODO: remove this once finished the page
   urls.REQUEST_FOR_CONSIDERATION, //TODO: remove this once finished the page
+  urls.DQ_MULTITRACK_CLAIMANT_DOCUMENTS_TO_BE_CONSIDERED_URL, //TODO: remove this once finished the page
 ];
