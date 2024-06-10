13e698004c134427ac6043a6edfd496fb91ad8f0
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-06-10 16:30:19 +0200

    Update: application summary (WIP)

--- src/main/app/client/generalApplicationClient.ts
+++ src/main/app/client/generalApplicationClient.ts
@@ -46,7 +46,7 @@ export class GeneralApplicationClient {
       //   return new ApplicationResponse(application.id, caseData, application.state, application.last_modified);
       // });
       // return applications;
-      return [mock1, mock2]
+      return [test, mock1, mock2]
       // return [
       //   new ApplicationResponse('123', {legacyCaseReference: 'ABC'}, CaseState.CASE_ISSUED, new Date()),
       //   new ApplicationResponse('456', {legacyCaseReference: 'XYZ'}, CaseState.AWAITING_APPLICANT_INTENTION, new Date())
@@ -59,256 +59,150 @@ export class GeneralApplicationClient {
 }
 
 const mock1 = {
-  'id': '1716993568476294', 
-  'createdDate': '2024-05-29T14:39:28.483971', 
-  'state': 'APPLICATION_SUBMITTED_AWAITING_JUDICIAL_DECISION', 
-  'applicationTypes': 'Adjourn a hearing', 
+  id: '123123123123123', 
+  createdDate: '2024-05-29T14:39:28.483971', 
+  state: 'AWAITING_RESPONDENT_RESPONSE', 
+  data: {
+    applicationTypes: 'Adjourn a hearing', 
+  },
 };
 
 const mock2 = {
-  'id': '1716993568476294', 
-  'createdDate': '2024-05-29T14:39:28.483971', 
-  'state': 'APPLICATION_SUBMITTED_AWAITING_JUDICIAL_DECISION', 
-  'applicationTypes': 'Adjourn a hearing', 
+  id: '4564564564564564', 
+  createdDate: '2024-05-29T14:39:28.483971', 
+  state: 'AWAITING_APPLICATION_PAYMENT', 
+  data:{
+    applicationTypes: 'Adjourn a hearing', 
+  },
 };
 
-// const mock = {
-//   "isMultiParty": "No",
-//   "locationName": "County Court Money Claims Centre",
-//   "generalAppType": {
-//     "types": [
-//       "ADJOURN_HEARING",
-//       "EXTEND_TIME"
-//     ]
-//   },
-//   "businessProcess": {
-//     "status": "FINISHED",
-//     "camundaEvent": "INITIATE_GENERAL_APPLICATION"
-//   },
-//   "isCcmccLocation": "Yes",
-//   "applicationTypes": "Adjourn a hearing",
-//   "isGaApplicantLip": "Yes",
-//   "isDocumentVisible": "Yes",
-//   "CaseAccessCategory": "SPEC_CLAIM",
-//   "claimant1PartyName": "Mr Claimant person",
-//   "defendant1PartyName": "mr defendant person",
-//   "parentCaseReference": "1717675354750680",
-//   "generalAppPBADetails": {
-//     "fee": {
-//       "code": "FEE0443",
-//       "version": "2",
-//       "calculatedAmountInPence": "10800"
-//     },
-//     "serviceRequestReference": "2022-1655915218557"
-//   },
-//   "isGaRespondentOneLip": "Yes",
-//   "isGaRespondentTwoLip": "No",
-//   "civilServiceUserRoles": {
-//     "id": "74ad03f0-8bf5-4a87-90b3-f299652d01fc",
-//     "email": "civilmoneyclaimsdemo@gmail.com"
-//   },
-//   "caseManagementCategory": {
-//     "value": {
-//       "code": "Civil",
-//       "label": "Civil"
-//     },
-//     "list_items": [
-//       {
-//         "id": "ec8c9fbf-59c5-4842-909f-1a49ebe471c0",
-//         "value": {
-//           "code": "Civil",
-//           "label": "Civil"
-//         }
-//       }
-//     ]
-//   },
-//   "caseManagementLocation": {
-//     "region": "4",
-//     "address": "88 Alfred Street, RandonCity",
-//     "postcode": "AA5 4RR",
-//     "siteName": "County Court Money Claims Centre",
-//     "baseLocation": "992288"
-//   },
-//   "gaApplicantDisplayName": "null - Defendant",
-//   "generalAppConsentOrder": "No",
-//   "generalAppApplnSolicitor": {
-//     "id": "74ad03f0-8bf5-4a87-90b3-f299652d01fc",
-//     "email": "civilmoneyclaimsdemo@gmail.com",
-//     "surname": "Citizen",
-//     "forename": "civilmoneyclaimsdemo@gmail.com"
-//   },
-//   "generalAppDetailsOfOrder": "The hearing arranged for [enter date] be moved to the first available date after [enter date], avoiding [enter dates to avoid].",
-//   "generalAppHearingDetails": {
-//     "HearingDetailsEmailID": "civilmoneyclaimsdemo@gmail.com",
-//     "HearingPreferredLocation": {
-//       "value": {
-//         "code": "aaddfb55-cd28-40f0-8a63-19900ce9b88c",
-//         "label": "Central London County Court - THOMAS MORE BUILDING, ROYAL COURTS OF JUSTICE, STRAND, LONDON - WC2A 2LL"
-//       },
-//       "list_items": [
-//         {
-//           "code": "aaddfb55-cd28-40f0-8a63-19900ce9b88c",
-//           "label": "Central London County Court - THOMAS MORE BUILDING, ROYAL COURTS OF JUSTICE, STRAND, LONDON - WC2A 2LL"
-//         }
-//       ]
-//     },
-//     "HearingDetailsTelephoneNumber": "01632960001",
-//     "ReasonForPreferredHearingType": "asdf",
-//     "HearingPreferencesPreferredType": "IN_PERSON",
-//     "unavailableTrialRequiredYesOrNo": "No"
-//   },
-//   "generalAppParentCaseLink": {
-//     "CaseReference": "1717675354750680"
-//   },
-//   "generalAppReasonsOfOrder": "asdf",
-//   "generalAppSuperClaimType": "SPEC_CLAIM",
-//   "parentClaimantIsApplicant": "No",
-//   "generalAppInformOtherParty": {
-//     "isWithNotice": "Yes"
-//   },
-//   "generalAppStatementOfTruth": {
-//     "name": "pablo"
-//   },
-//   "applicant1OrganisationPolicy": {
-//     "OrgPolicyCaseAssignedRole": "[APPLICANTSOLICITORONE]"
-//   },
-//   "generalAppRespondentAgreement": {
-//     "hasAgreed": "Yes"
-//   },
-//   "generalAppNotificationDeadlineDate": "2024-06-12T16:00:00"
-// };
-
-// const mock2 = {
-// 	"isMultiParty": "No",
-// 	"locationName": "County Court Money Claims Centre",
-// 	"generalAppType": {
-// 		"types": [
-// 			"EXTEND_TIME"
-// 		]
-// 	},
-// 	"businessProcess": {
-// 		"status": "FINISHED",
-// 		"camundaEvent": "INITIATE_GENERAL_APPLICATION"
-// 	},
-// 	"isCcmccLocation": "Yes",
-// 	"applicationTypes": "Extend time",
-// 	"isGaApplicantLip": "Yes",
-// 	"isDocumentVisible": "Yes",
-// 	"CaseAccessCategory": "SPEC_CLAIM",
-// 	"applicantPartyName": "lipf lipl",
-// 	"claimant1PartyName": "org",
-// 	"defendant1PartyName": "lipf lipl",
-// 	"parentCaseReference": "1716900200663759",
-// 	"generalAppPBADetails": {
-// 		"fee": {
-// 			"code": "FEE0443",
-// 			"version": "2",
-// 			"calculatedAmountInPence": "10800"
-// 		},
-// 		"serviceRequestReference": "2022-1655915218557"
-// 	},
-// 	"isGaRespondentOneLip": "No",
-// 	"isGaRespondentTwoLip": "No",
-// 	"civilServiceUserRoles": {
-// 		"id": "39dce7c1-c9d6-49eb-aa57-b7322461e146",
-// 		"email": "civilmoneyclaimsdemo@gmail.com"
-// 	},
-// 	"caseManagementCategory": {
-// 		"value": {
-// 			"code": "Civil",
-// 			"label": "Civil"
-// 		},
-// 		"list_items": [
-// 			{
-// 				"id": "736a21c3-8c71-4e76-a441-05322b12f7f5",
-// 				"value": {
-// 					"code": "Civil",
-// 					"label": "Civil"
-// 				}
-// 			}
-// 		]
-// 	},
-// 	"caseManagementLocation": {
-// 		"region": "4",
-// 		"address": "88 Alfred Street, RandonCity",
-// 		"postcode": "AA5 4RR",
-// 		"siteName": "County Court Money Claims Centre",
-// 		"baseLocation": "992288"
-// 	},
-// 	"gaApplicantDisplayName": "lipf lipl - Defendant",
-// 	"generalAppConsentOrder": "No",
-// 	"generalAppApplnSolicitor": {
-// 		"id": "39dce7c1-c9d6-49eb-aa57-b7322461e146",
-// 		"email": "civilmoneyclaimsdemo@gmail.com",
-// 		"surname": "Citizen",
-// 		"forename": "civilmoneyclaimsdemo@gmail.com"
-// 	},
-// 	"generalAppDetailsOfOrder": "test",
-// 	"generalAppHearingDetails": {
-// 		"SupportRequirement": [
-// 			"LANGUAGE_INTERPRETER"
-// 		],
-// 		"HearingDetailsEmailID": "a@b.com",
-// 		"HearingPreferredLocation": {
-// 			"value": {
-// 				"code": "f8e66ca0-df59-4ef2-8a9b-0ffe05c57aa9",
-// 				"label": "Barnet Civil and Family Centre - St Mary's Court, Regents Park Road - N3 1BQ"
-// 			},
-// 			"list_items": [
-// 				{
-// 					"code": "f8e66ca0-df59-4ef2-8a9b-0ffe05c57aa9",
-// 					"label": "Barnet Civil and Family Centre - St Mary's Court, Regents Park Road - N3 1BQ"
-// 				}
-// 			]
-// 		},
-// 		"generalAppUnavailableDates": [
-// 			{
-// 				"id": "5cc89b8e-4690-466f-8f49-73beb0995a4f",
-// 				"value": {
-// 					"unavailableTrialDateFrom": "2024-07-01"
-// 				}
-// 			},
-// 			{
-// 				"id": "134df13d-7bb3-4659-b425-10867229e38d",
-// 				"value": {
-// 					"unavailableTrialDateTo": "2024-07-08",
-// 					"unavailableTrialDateFrom": "2024-07-05"
-// 				}
-// 			}
-// 		],
-// 		"HearingDetailsTelephoneNumber": "01234123123",
-// 		"ReasonForPreferredHearingType": "test",
-// 		"HearingPreferencesPreferredType": "VIDEO",
-// 		"unavailableTrialRequiredYesOrNo": "Yes",
-// 		"SupportRequirementLanguageInterpreter": "Spanish"
-// 	},
-// 	"generalAppParentCaseLink": {
-// 		"CaseReference": "1716900200663759"
-// 	},
-// 	"generalAppReasonsOfOrder": "test",
-// 	"generalAppSuperClaimType": "SPEC_CLAIM",
-// 	"parentClaimantIsApplicant": "No",
-// 	"generalAppInformOtherParty": {
-// 		"isWithNotice": "Yes"
-// 	},
-// 	"generalAppStatementOfTruth": {
-// 		"name": "Paul Pearson"
-// 	},
-// 	"applicant1OrganisationPolicy": {
-// 		"OrgPolicyCaseAssignedRole": "[APPLICANTSOLICITORONE]"
-// 	},
-// 	"generalAppRespondentAgreement": {
-// 		"hasAgreed": "Yes"
-// 	},
-// 	"generalAppRespondentSolicitors": [
-// 		{
-// 			"id": "3e247e2b-9f7d-48df-af42-0ba1a72e2786",
-// 			"value": {
-// 				"id": "dd2416f8-2029-4f5c-970b-1a2cf1678de3",
-// 				"email": "hmcts.civil+organisation.1.solicitor.1@gmail.com",
-// 				"organisationIdentifier": "Q1KOKP2"
-// 			}
-// 		}
-// 	],
-// 	"generalAppNotificationDeadlineDate": "2024-06-12T16:00:00"
-// };
+const test = {
+  id: 1716993568476294,
+  jurisdiction: "CIVIL",
+  caseTypeId: "GENERALAPPLICATION",
+  createdDate: "2024-05-29T14:39:28.483971",
+  lastModified: "2024-06-07T09:59:44.868006",
+  state: "APPLICATION_SUBMITTED_AWAITING_JUDICIAL_DECISION",
+  lockedBy: "null",
+  securityLevel: "null",
+  data: {
+    generalAppUrgencyRequirement: { generalAppUrgency: "No" },
+    generalAppNotificationDeadlineDate: "2024-06-03T16:00:00",
+    civilServiceUserRoles: {
+      id: "dd2416f8-2029-4f5c-970b-1a2cf1678de3",
+      email: "hmcts.civil+organisation.1.solicitor.1@gmail.com",
+    },
+    generalAppHearingDate: {
+      hearingScheduledDate: "2025-01-01",
+      hearingScheduledPreferenceYesNo: "Yes",
+    },
+    generalAppStatementOfTruth: {
+      name: "sd",
+      role: "ds",
+    },
+    caseManagementLocation: {
+      region: "4",
+      address: "88 Alfred Street, RandonCity",
+      postcode: "AA5 4RR",
+      siteName: "County Court Money Claims Centre",
+      baseLocation: "992288",
+    },
+    generalAppApplnSolicitor: {
+      id: "dd2416f8-2029-4f5c-970b-1a2cf1678de3",
+      email: "hmcts.civil+organisation.1.solicitor.1@gmail.com",
+      surname: "Claimant-solicitor",
+      forename: "hmcts.civil+organisation.1.solicitor.1@gmail.com",
+      organisationIdentifier: "Q1KOKP2",
+    },
+    parentClaimantIsApplicant: "Yes",
+    isGaApplicantLip: "No",
+    caseManagementCategory: {
+      value: { code: "Civil", label: "Civil" },
+      list_items: [
+        {
+          id: "38ef1e46-5324-4d8b-a9f3-9dfe76de8bb2",
+          value: { code: "Civil", label: "Civil" },
+        },
+      ],
+    },
+    CaseAccessCategory: "SPEC_CLAIM",
+    parentCaseReference: "1716900200663759",
+    generalAppRespondentSolicitors: [
+      {
+        id: "a13e197e-6b5d-4a92-9910-a02c3794756a",
+        value: {
+          id: "39dce7c1-c9d6-49eb-aa57-b7322461e146",
+          email: "civilmoneyclaimsdemo@gmail.com",
+          surname: "lipl",
+          forename: "lipf",
+        },
+      },
+    ],
+    isDocumentVisible: "Yes",
+    generalAppParentCaseLink: { CaseReference: 1716900200663759 },
+    generalAppRespondentAgreement: { hasAgreed: "Yes" },
+    applicantPartyName: "org",
+    generalAppConsentOrder: "No",
+    generalAppSuperClaimType: "SPEC_CLAIM",
+    generalAppPBADetails: {
+      fee: { code: "FREE", version: 1, calculatedAmountInPence: 0 },
+      paymentDetails: {
+        status: "SUCCESS",
+        reference: "FREE",
+        customerReference: "FREE",
+      },
+      paymentSuccessfulDate: "2024-05-29T15:39:41.5588713",
+      serviceRequestReference: "FREE",
+    },
+    generalAppDetailsOfOrder: "ds",
+    isGaRespondentTwoLip: "No",
+    locationName: "County Court Money Claims Centre",
+    claimant1PartyName: "org",
+    respondent1OrganisationPolicy: { OrgPolicyCaseAssignedRole: ["DEFENDANT"] },
+    generalAppHearingDetails: {
+      hearingYesorNo: "No",
+      HearingDuration: "MINUTES_30",
+      judgeRequiredYesOrNo: "No",
+      trialRequiredYesOrNo: "No",
+      HearingDetailsEmailID: "sd@do.com",
+      HearingPreferredLocation: {
+        value: {
+          code: "20a1b82d-6883-4910-b83a-4d4e3771d9b3",
+          label:
+            "Barnet Civil and Family Centre - St Mary's Court, Regents Park Road - N3 1BQ",
+        },
+        list_items: [
+          {
+            code: "20a1b82d-6883-4910-b83a-4d4e3771d9b3",
+            label:
+              "Barnet Civil and Family Centre - St Mary's Court, Regents Park Road - N3 1BQ",
+          },
+        ],
+      },
+      HearingDetailsTelephoneNumber: 123,
+      ReasonForPreferredHearingType: "ds",
+      vulnerabilityQuestionsYesOrNo: "No",
+      HearingPreferencesPreferredType: "VIDEO",
+      unavailableTrialRequiredYesOrNo: "No",
+    },
+    isGaRespondentOneLip: "Yes",
+    businessProcess: {
+      status: "FINISHED",
+      camundaEvent: "INITIATE_GENERAL_APPLICATION",
+    },
+    defendant1PartyName: "lipf lipl",
+    generalAppInformOtherParty: { isWithNotice: "Yes" },
+    generalAppType: { types: ["ADJOURN_HEARING"] },
+    generalAppReasonsOfOrder: "sd",
+    applicationTypes: "Adjourn a hearing",
+    applicant1OrganisationPolicy: {
+      Organisation: { OrganisationID: "Q1KOKP2" },
+      OrgPolicyCaseAssignedRole: ["APPLICANTSOLICITORONE"],
+    },
+    isMultiParty: "No",
+    isCcmccLocation: "Yes",
+    gaApplicantDisplayName: "org - Claimant",
+  },
+  securityClassification: "PUBLIC",
+  callbackResponseStatus: "null",
+};
--- src/main/routes/features/generalApplication/applicationSummaryController.ts
+++ src/main/routes/features/generalApplication/applicationSummaryController.ts
@@ -2,11 +2,12 @@ import config from 'config';
 import { NextFunction, Response, Router } from 'express';
 import { AppRequest } from 'common/models/AppRequest';
 import { generateRedisKey, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
-import { GA_APPLICATION_SUMMARY_URL } from 'routes/urls';
+import { GA_APPLICATION_SUMMARY_URL, GA_VIEW_APPLICATION_URL } from 'routes/urls';
 import { getCancelUrl } from 'services/features/generalApplication/generalApplicationService';
 import { GeneralApplicationClient } from 'client/generalApplicationClient';
-// import { t } from 'i18next';
-// import { ApplicationTypeOption, selectedApplicationType } from 'common/models/generalApplication/applicationType';
+import { t } from 'i18next';
+import { ApplicationState, ApplicationStatus, ApplicationSummary, StatusColor } from 'common/models/generalApplication/applicationSummary';
+import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
 
 const applicationSummaryController = Router();
 const viewPath = 'features/generalApplication/applications-summary';
@@ -17,39 +18,48 @@ const civilServiceClient: GeneralApplicationClient = new GeneralApplicationClien
 applicationSummaryController.get(GA_APPLICATION_SUMMARY_URL, async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
-    // const lng = req.query.lang || req.cookies.lang;
+    const lng = req.query.lang || req.cookies.lang;
     const redisKey = generateRedisKey(req);
     const claim = await getCaseDataFromStore(redisKey);
     const cancelUrl = await getCancelUrl(claimId, claim);
-    const backLinkUrl = 'test'; // TODO: add url
     const applications = await civilServiceClient.getApplications(req);
     console.log('applications: ', applications);
 
-    const applicationsRows: any[] = [];
+    const applicationsRows: ApplicationSummary[] = [];
     applications.forEach(application => {
-      
-      // const types = application.generalAppType.types.map((type: ApplicationTypeOption) => {
-      //     return t(selectedApplicationType[type], {lng})
-      // });
-      // console.log('types: ', types);
-
-
+      const status = getApplicationStatus(application.state);
       applicationsRows.push({
-        status: application.state,
-        types: application.applicationTypes,
+        state: t(`PAGES.GENERAL_APPLICATION.SUMMARY.${application.state}`, {lng}),
+        status: t(`PAGES.GENERAL_APPLICATION.SUMMARY.${status}`, {lng}),
+        statusColor: StatusColor[status],
+        types: application.data.applicationTypes,
         id: application.id,
         createdDate: application.createdDate,
+        applicationUrl: `${constructResponseUrlWithIdParams(application.id, GA_VIEW_APPLICATION_URL)}?applicationId=${application.id}` 
       })
     });
+    console.log('applicationsRows: ', applicationsRows);
     
     res.render(viewPath, {
       cancelUrl,
-      backLinkUrl,
       applicationsRows,
+      dashboardUrl: getCancelUrl(claimId, claim),
+      pageTitle: 'PAGES.GENERAL_APPLICATION.SUMMARY.MY_APPLICATIONS',
     });
   } catch (error) {
     next(error);
   }
 });
+
+const getApplicationStatus = (status: ApplicationState): ApplicationStatus => {
+  switch (status) {
+    case ApplicationState.APPLICATION_SUBMITTED_AWAITING_JUDICIAL_DECISION:
+      return ApplicationStatus.IN_PROGRESS;
+    case ApplicationState.AWAITING_RESPONDENT_RESPONSE:
+      return ApplicationStatus.IN_PROGRESS;
+    case ApplicationState.AWAITING_APPLICATION_PAYMENT:
+      return ApplicationStatus.TO_DO;
+  }
+};
 
 export default applicationSummaryController;
