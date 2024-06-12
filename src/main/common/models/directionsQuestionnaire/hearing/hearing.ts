035786cffb16903aceb369fbd5e60657d92a24d0
sankaviv1 <95748224+sankaviv1@users.noreply.github.com>
2024-06-12 09:53:48 +0100

    CIV-13087 Claimant documents to be considered  (#3975)
    * CIV-13807 Claimant documents considered details sreen
    
    * checkstyle fix
    
    * change file name
    
    * fix welsh translation for CIV-13386

--- src/main/common/models/directionsQuestionnaire/hearing/hearing.ts
+++ src/main/common/models/directionsQuestionnaire/hearing/hearing.ts
@@ -24,4 +24,5 @@ export class Hearing {
   disclosureOfDocuments?: DisclosureOfDocuments;
   disclosureNonElectronicDocument?: string;
   disclosureOfElectronicDocumentsIssues?: string;
+  claimantDocumentsConsideredDetails?: string;
 }
