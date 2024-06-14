ccac1ebb5d52b05063dcdb14ac6d33f4f838a314
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-06-14 10:12:53 +0200

    Revert "Merge branch 'master' of https://github.com/hmcts/civil-citizen-ui"
    This reverts commit 8d419a43a8b6a7953ebb0bf4ff9d5dd68f684671, reversing
    changes made to 11c7b54f1f6e329d912b54c5eb9c76b534553757.

--- src/test/unit/routes/features/dashboard/claimantDashboardController.test.ts
+++ src/test/unit/routes/features/dashboard/claimantDashboardController.test.ts
@@ -195,7 +195,6 @@ describe('claimant Dashboard Controller', () => {
     it('should show support links for claimant', async () => {
 
       const claim = new Claim();
-      claim.caseRole = CaseRole.CLAIMANT;
       claim.ccdState = CaseState.AWAITING_RESPONDENT_ACKNOWLEDGEMENT;
       jest
         .spyOn(CivilServiceClient.prototype, 'retrieveClaimDetails')
@@ -220,7 +219,6 @@ describe('claimant Dashboard Controller', () => {
     it('should show support links for claimant whit links hidden', async () => {
 
       const claim = new Claim();
-      claim.caseRole = CaseRole.CLAIMANT;
       claim.ccdState = CaseState.CASE_ISSUED;
       jest
         .spyOn(CivilServiceClient.prototype, 'retrieveClaimDetails')
