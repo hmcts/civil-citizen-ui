e5baf2989f4c9b0d733641e084c9d6f290c50ff8
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-06-17 11:15:40 +0200

    Fix: endpoint

--- src/main/app/client/generalApplicationClient.ts
+++ src/main/app/client/generalApplicationClient.ts
@@ -6,8 +6,7 @@ import {
 } from './generalApplicationUrls';
 import {ApplicationResponse} from 'models/generalApplication/applicationResponse';
 import {Application} from 'models/generalApplication/application';
-// import { ApplicationState } from 'common/models/generalApplication/applicationSummary';
-import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('generalApplicationClient');
@@ -56,59 +55,12 @@ export class GeneralApplicationClient {
 
   async getApplicationsByCaseId(caseId: string, req: AppRequest): Promise<ApplicationResponse[]> {
     const config = this.getConfig(req);
-    // let applications: ApplicationResponse[] = [];
     try {
-      const response = await this.client.post(constructResponseUrlWithIdParams(caseId, CIVIL_GENERAL_APPLICATIONS_BY_CASE_URL), {match_all: {}}, config);
-      // applications = response.data?.cases?.map((application: ApplicationResponse) => {
-      //   const caseData = Object.assign(new Application(), application.case_data);
-      //   return new ApplicationResponse(application.id, caseData, application.state, application.last_modified, application.created_date);
-      // });
-      // console.log(response.data);
-
-      return response.data;
-      // return [mock1]
+      const response = await this.client.get(constructResponseUrlWithIdParams(caseId, CIVIL_GENERAL_APPLICATIONS_BY_CASE_URL), config);
+      return response.data.cases;
     } catch (err) {
-      logger.error('Error when getApplicationsByCaseId');
+      logger.error('Error when getApplicationsByCaseId', err);
       throw err;
     }
   }
 }
-
-// const mock1 = {
-//     id: '1718283169493783',
-//     jurisdiction: 'CIVIL',
-//     state: ApplicationState.AWAITING_APPLICATION_PAYMENT,
-//     case_type_id: 'GENERALAPPLICATION',
-//     created_date: '2024-06-13T12:52:49.448',
-//     last_modified: '2024-06-13T12:53:04.909',
-//     case_data: {
-//         applicationTypes: 'Extend time'
-//     },
-//     security_classification: 'PUBLIC'
-// }
-  // id: '1234567890',
-  // created_date: '2024-06-12 09:52:16.492',
-  // last_modified: '2024-05-29T14:39:28.483971',
-  // state: ApplicationState.AWAITING_RESPONDENT_RESPONSE,
-  // case_data: {
-  //   applicationTypes: 'Adjourn a hearing', 
-  // },
-// };
-// const mock2 = {
-//   id: '4564564564564564', 
-//   created_date: '2024-05-29T14:39:28.483971',
-//   last_modified: '2024-05-29T14:39:28.483971',
-//   state: ApplicationState.AWAITING_APPLICATION_PAYMENT, 
-//   case_data:{
-//     applicationTypes: 'Adjourn a hearing', 
-//   },
-// };
-// const test = {
-//   id: '1716993568476294',
-//   created_date: "2024-05-29T14:39:28.483971",
-//   last_modified: '2024-05-29T14:39:28.483971',
-//   state: ApplicationState.APPLICATION_SUBMITTED_AWAITING_JUDICIAL_DECISION,
-//   case_data: {
-//     applicationTypes: "Adjourn a hearing",
-//   },
-// };
