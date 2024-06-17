9456e6511dfd09dcc235150dc67ae30d2da04398
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-06-17 16:20:39 +0200

    CIV-9458 : Controller
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -291,12 +291,12 @@ import respondentAgreementController from './features/generalApplication/respons
 import viewTheHearingController from 'routes/features/caseProgression/viewTheHearingController';
 import viewResponseToClaimController from 'routes/features/dashboard/viewResponseToClaimController';
 import viewTheJudgementController from 'routes/features/caseProgression/viewTheJudgementController';
-import claimFeeMakePaymentAgainController
-  from 'routes/features/claim/payment/claimFeeMakePaymentAgainController';
+import claimFeeMakePaymentAgainController from 'routes/features/claim/payment/claimFeeMakePaymentAgainController';
 import informOtherPartiesController from './features/generalApplication/informOtherPartiesController';
 import hearingSupportController from './features/generalApplication/hearingSupportController';
 import agreementFromOtherPartyController from './features/generalApplication/agreementFromOtherPartyController';
-import applicationFeePaymentSuccessfulController from './features/generalApplication/applicationFeePaymentSuccessfulController';
+import applicationFeePaymentSuccessfulController
+  from './features/generalApplication/applicationFeePaymentSuccessfulController';
 import claimApplicationCostController from './features/generalApplication/claimApplicationCostController';
 import respondentAgreeToOrderController from 'routes/features/generalApplication/respondentAgreeToOrderController';
 import payingForApplicationController from './features/generalApplication/payingForApplicationController';
@@ -310,7 +310,8 @@ import unavailableHearingDatesController from 'routes/features/generalApplicatio
 import hearingArrangementController from './features/generalApplication/hearingArrangementController';
 import hearingContactDetailsController from './features/generalApplication/hearingContactDetailsController';
 import gaCheckAnswersController from 'routes/features/generalApplication/checkAnswersController';
-import applicationPaymentUnsuccessfulController from './features/generalApplication/applicationPaymentUnsuccessfulController';
+import applicationPaymentUnsuccessfulController
+  from './features/generalApplication/applicationPaymentUnsuccessfulController';
 import applicationResponseConfirmationController
   from 'routes/features/generalApplication/response/applicationResponseConfirmationController';
 import flightDetailsController from './features/claim/airlines/flightDetailsController';
@@ -318,13 +319,16 @@ import delayedFlightController from './features/claim/airlines/delayedFlightCont
 import respondentRequestChangeInformationController
   from 'routes/features/generalApplication/respondentRequestChangeInformationController';
 import accessibilityStatementController from './features/public/accessibilityStatementController';
-import hearingArrangementsGuidanceController from 'routes/features/generalApplication/hearingArrangementsGuidanceController';
+import hearingArrangementsGuidanceController
+  from 'routes/features/generalApplication/hearingArrangementsGuidanceController';
 import wantToUploadDocumentsController from 'routes/features/generalApplication/wantToUploadDocuments';
-import uploadEvidenceDocumentsForApplicationController from 'routes/features/generalApplication/uploadEvidenceDocumentForApplication';
+import uploadEvidenceDocumentsForApplicationController
+  from 'routes/features/generalApplication/uploadEvidenceDocumentForApplication';
 import confirmYouHaveBeenPaidController from 'routes/features/judgmentOnline/confirmYouHaveBeenPaidController';
 import confirmYouHaveBeenPaidConfirmController
   from 'routes/features/judgmentOnline/confirmYouHaveBeenPaidConfirmationController';
-import gaCheckAnswersResponseController from 'routes/features/generalApplication/response/checkAnswersResponseController';
+import gaCheckAnswersResponseController
+  from 'routes/features/generalApplication/response/checkAnswersResponseController';
 import hearingArrangementResponseController
   from 'routes/features/generalApplication/response/hearingArrangementResponseController';
 import hearingContactDetailsResponseController
@@ -346,7 +350,7 @@ import payApplicationFeeConfirmationController
 import submitGeneralApplicationConfirmationController
   from 'routes/features/generalApplication/submitGeneralApplicationConfirmationController';
 import acceptDefendantOfferController from './features/generalApplication/response/acceptDefendantOfferController';
-import viewMyApplicationsController from 'routes/features/generalApplication/viewMyApplicationsController';
+import viewApplicationController from 'routes/features/generalApplication/viewApplicationController';
 
 export default [
   homeController,
@@ -640,5 +644,5 @@ export default [
   helpWithApplicationFeeReferenceController,
   helpWithApplicationFeeContinueController,
   payApplicationFeeConfirmationController,
-  viewMyApplicationsController,
+  viewApplicationController,
 ];
