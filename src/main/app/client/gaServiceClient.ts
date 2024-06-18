19fd7642ded3182aa42661ac0e00ea60a3320726
kenneth-hmcts <91327278+kenneth-hmcts@users.noreply.github.com>
2024-06-18 18:02:38 +0100

    CIV-9465 LiP Pay for Application (#4002)
    * CIV-14008 changes
    
    * checkStyle fixed
    
    * checkstyle fixed
    
    * test case updated
    
    * checkstyle updated
    
    * Update ccdTranslation.ts
    
    * test case updated
    
    * new model class added
    
    * checkstyle fixed
    
    * checkstyle fixed
    
    * Update cy.json
    
    * CIV-9477 response check answers screen
    
    * added missing routes, removed backLinkUrl declaration in controller
    
    * removed bracket
    
    * removed duplicated entries
    
    * CIV-14041 cy.json fix
    
    * CIV-9477 Update check you answer response statement of truth content
    
    * CIV-14140 preview testing
    
    * CIV-9447 Updated content
    
    * Update checkAnswersController.ts
    
    fixed check style error.
    
    * Update checkAnswersController.ts
    
    * CIV-9465 Set up page
    
    * CIV-9465 Update lint
    
    * CIV-9465 By pass unit test at this moment
    
    * CIV-9465 Update unit test
    
    * CIV-9465 Update unit test
    
    * CIV-9465 Update unit test
    
    * CIV-9465 Update unit test
    
    * CIV-9465 Update for local test
    
    * CIV-9465 update preivew
    
    * CIV-9465 update preivew
    
    * CIV-9465 By pass sonar for preview test
    
    * CIV-9465 Update preview for GA changes
    
    * CIV-9465 Roll back from preview testing
    
    * CIV-9465 Roll back from preview testing
    
    * CIV-9465 Roll back from preview testing
    
    * CIV-9465 Set up unit test
    
    * CIV-9465 Set up unit test
    
    * CIV-9465 Fix lint
    
    * CIV-9465 Remove from review
    
    * CIV-9465 Update from master
    
    * Update values.enableNotifyEmails.preview.template.yaml
    
    ---------
    
    Co-authored-by: kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
    Co-authored-by: kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
    Co-authored-by: Raja Mani <rajakm@gmail.com>
    Co-authored-by: jarekPierchala <jaroslaw.pierchala@hmcts.net>
    Co-authored-by: David Jeronimo <david.jeronimo@jmcts.net>
    Co-authored-by: sankhajuria <sankhajuria@gmail.com>
    Co-authored-by: paul-pearson <paul.pearson@version1.com>
    Co-authored-by: Manish Garg <manish.garg@hmcts.net>
    Co-authored-by: jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
    Co-authored-by: neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>

--- src/main/app/client/gaServiceClient.ts
+++ src/main/app/client/gaServiceClient.ts
@@ -1,11 +1,13 @@
-import Axios, {AxiosInstance} from 'axios';
+import Axios, {AxiosInstance, AxiosResponse} from 'axios';
 import {AppRequest} from 'common/models/AppRequest';
 import {EventDto} from 'models/gaEvents/eventDto';
 import {CCDGeneralApplication} from 'models/gaEvents/eventDto';
 import {ApplicationEvent} from 'models/gaEvents/applicationEvent';
 import {GeneralApplicationResponse} from 'models/generalApplicationResponse';
 import {Application} from 'models/application';
-import {GA_SERVICE_SUBMIT_EVENT} from 'client/gaServiceUrls';
+import {GA_FEES_PAYMENT_STATUS_URL, GA_FEES_PAYMENT_URL, GA_SERVICE_SUBMIT_EVENT} from 'client/gaServiceUrls';
+import {PaymentInformation} from 'models/feePayment/paymentInformation';
+import {plainToInstance} from 'class-transformer';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('gaServiceClient');
@@ -48,6 +50,29 @@ export class GaServiceClient {
       return convertResponseToApplication(appResponse);
     } catch (err: unknown) {
       logger.error(`Error when submitting event ${event}`);
+      throw err;
+    }
+  }
+
+  async getGaFeePaymentRedirectInformation(claimId: string, req: AppRequest): Promise<PaymentInformation> {
+    const config = this.getConfig(req);
+    try {
+      const response = await this.client.post(GA_FEES_PAYMENT_URL.replace(':claimId', claimId),'', config);
+      return plainToInstance(PaymentInformation, response.data);
+    } catch (err: unknown) {
+      logger.error('Error when getting fee payment redirect information');
+      throw err;
+    }
+  }
+
+  async getGaFeePaymentStatus(claimId: string, paymentReference: string, req: AppRequest): Promise<PaymentInformation> {
+    const config = this.getConfig(req);
+    try {
+      const response: AxiosResponse<object> = await this.client.get(GA_FEES_PAYMENT_STATUS_URL.replace(':claimId', claimId).replace(':paymentReference', paymentReference), config);
+
+      return plainToInstance(PaymentInformation, response.data);
+    } catch (err: unknown) {
+      logger.error('Error when getting fee payment status');
       throw err;
     }
   }
