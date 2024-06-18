9f3f14988d5b3f4b5d0a1571d01351495aebe2a7
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-06-18 11:21:57 +0200

    Revert "Fix: master error"
    This reverts commit 10758b147dfac4db6aaa05ade02ac208c72f6401.

--- src/test/unit/app/client/civilServiceClient.test.ts
+++ src/test/unit/app/client/civilServiceClient.test.ts
@@ -480,13 +480,10 @@ describe('Civil Service Client', () => {
     data.issueDate = date;
     data.respondent1ResponseDeadline = date;
     const claimUpdate:ClaimUpdate =  {issueDate: date, respondent1ResponseDeadline: date};
-    const ccdGApp:CCDGeneralApplication =  {
-      generalAppInformOtherParty: undefined, generalAppAskForCosts: undefined,
-      generalAppType: undefined, generalAppRespondentAgreement: undefined, generalAppDetailsOfOrder: undefined,
-      generalAppReasonsOfOrder: undefined, generalAppHearingDetails: undefined, generalAppStatementOfTruth: undefined,
-      generalAppEvidenceDocument: undefined,
-      generalAppHelpWithFees: undefined
-    };
+    const ccdGApp:CCDGeneralApplication =  {generalAppInformOtherParty: undefined, generalAppAskForCosts: undefined
+      , generalAppType: undefined, generalAppRespondentAgreement: undefined, generalAppDetailsOfOrder: undefined
+      , generalAppReasonsOfOrder: undefined, generalAppHearingDetails: undefined, generalAppStatementOfTruth: undefined
+      , generalAppEvidenceDocument: undefined};
 
     const mockResponse = new CivilClaimResponse();
     mockResponse.id = '1';
