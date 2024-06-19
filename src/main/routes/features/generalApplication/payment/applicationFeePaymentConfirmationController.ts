0cdfec043e84e9d06db0539df51e9395fca505a3
Manish Garg <manish.garg@hmcts.net>
2024-06-19 15:31:04 +0100

    CIV-13767 Fixed formatting

--- src/main/routes/features/generalApplication/payment/applicationFeePaymentConfirmationController.ts
+++ src/main/routes/features/generalApplication/payment/applicationFeePaymentConfirmationController.ts
@@ -1,3 +1,4 @@
+import { AppRequest } from 'common/models/AppRequest';
 import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
 import {NextFunction,RequestHandler, Response, Router} from 'express';
 import {APPLICATION_FEE_PAYMENT_CONFIRMATION_URL, APPLICATION_FEE_PAYMENT_CONFIRMATION_URL_WITH_UNIQUE_ID} from 'routes/urls';
