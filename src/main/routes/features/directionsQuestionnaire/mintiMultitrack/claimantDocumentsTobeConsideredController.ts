1c5c4c5600b6bfdabc63c17574070d3e54391f97
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-06-20 09:17:18 +0100

    add the translation

--- src/main/routes/features/directionsQuestionnaire/mintiMultitrack/claimantDocumentsTobeConsideredController.ts
+++ src/main/routes/features/directionsQuestionnaire/mintiMultitrack/claimantDocumentsTobeConsideredController.ts
@@ -13,18 +13,21 @@ import {AppRequest} from 'models/AppRequest';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {GenericYesNo} from 'form/models/genericYesNo';
 import {YesNo} from 'form/models/yesNo';
+import {t} from 'i18next';
 
 const claimantDocumentsTobeConsideredController = Router();
 const hasClaimantDocumentsToBeConsideredViewPath = 'features/directionsQuestionnaire/mintiMultiTrack/claimant-documents-to-be-considered';
 const CLAIMANT_DOCS_FOR_DISCLOSURE_PAGE = 'PAGES.CLAIMANT_DOCS_FOR_DISCLOSURE.';
 
-function renderView(hasClaimantDocumentsToBeConsidered: GenericForm<GenericYesNo>, claimId: string, res: Response): void {
+function renderView(hasClaimantDocumentsToBeConsidered: GenericForm<GenericYesNo>, claimId: string, isClaimant: boolean, lng: string, res: Response): void {
   const form = hasClaimantDocumentsToBeConsidered;
+  const userRoleKey = isClaimant ? 'DEFENDANT': 'CLAIMANT';
+  const userRole = t(`COMMON.${userRoleKey}`, {lng});
   res.render(hasClaimantDocumentsToBeConsideredViewPath, {
     form,
-    pageTitle: `${CLAIMANT_DOCS_FOR_DISCLOSURE_PAGE}PAGE_TITLE`,
-    title: `${CLAIMANT_DOCS_FOR_DISCLOSURE_PAGE}TITLE`,
-    componentText: `${CLAIMANT_DOCS_FOR_DISCLOSURE_PAGE}PAGE_TITLE`,
+    pageTitle: t(`${CLAIMANT_DOCS_FOR_DISCLOSURE_PAGE}PAGE_TITLE`, {lng, userRole}),
+    title: t(`${CLAIMANT_DOCS_FOR_DISCLOSURE_PAGE}TITLE`, {lng, userRole}),
+    componentText: t(`${CLAIMANT_DOCS_FOR_DISCLOSURE_PAGE}PAGE_TITLE`, {lng, userRole}),
     backLinkUrl: BACK_URL,
   });
 }
@@ -32,10 +35,11 @@ function renderView(hasClaimantDocumentsToBeConsidered: GenericForm<GenericYesNo
 claimantDocumentsTobeConsideredController.get(DQ_MULTITRACK_CLAIMANT_DOCUMENTS_TO_BE_CONSIDERED_URL, (async (req, res, next: NextFunction) => {
   try {
     const claimId = req.params.id;
+    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const directionQuestionnaire = await getDirectionQuestionnaire(generateRedisKey(<AppRequest>req));
     const hasClaimantDocumentsToBeConsidered = directionQuestionnaire.hearing?.hasClaimantDocumentsToBeConsidered ?
       new GenericYesNo(directionQuestionnaire.hearing?.hasClaimantDocumentsToBeConsidered?.option) : new GenericYesNo();
-    renderView(new GenericForm(hasClaimantDocumentsToBeConsidered), claimId , res);
+    renderView(new GenericForm(hasClaimantDocumentsToBeConsidered), claimId, false, lang, res);
   } catch (error) {
     next(error);
   }
@@ -44,10 +48,12 @@ claimantDocumentsTobeConsideredController.get(DQ_MULTITRACK_CLAIMANT_DOCUMENTS_T
 claimantDocumentsTobeConsideredController.post(DQ_MULTITRACK_CLAIMANT_DOCUMENTS_TO_BE_CONSIDERED_URL, (async (req: Request, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
+    const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+
     const hasClaimantDocumentsToBeConsideredForm = new GenericForm(new GenericYesNo(req.body.option, 'ERRORS.CLAIMANT_DOCS_FOR_DISCLOSURE'));
     hasClaimantDocumentsToBeConsideredForm.validateSync();
     if (hasClaimantDocumentsToBeConsideredForm.hasErrors()) {
-      renderView(hasClaimantDocumentsToBeConsideredForm, claimId, res);
+      renderView(hasClaimantDocumentsToBeConsideredForm, claimId,true, lang, res);
     } else {
       await saveDirectionQuestionnaire(
         generateRedisKey(<AppRequest>req),
