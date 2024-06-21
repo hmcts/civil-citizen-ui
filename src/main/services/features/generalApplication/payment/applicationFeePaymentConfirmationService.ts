31bdec8a2be4566e79d0d918b8550a05600d9a72
Manish Garg <manish.garg@hmcts.net>
2024-06-21 14:03:41 +0100

    CIV-13767 Temp changes for test skip and getting case data

--- src/main/services/features/generalApplication/payment/applicationFeePaymentConfirmationService.ts
+++ src/main/services/features/generalApplication/payment/applicationFeePaymentConfirmationService.ts
@@ -2,21 +2,24 @@ import {AppRequest} from 'models/AppRequest';
 import {
   DASHBOARD_URL, GA_PAYMENT_SUCCESSFUL_URL, GA_PAYMENT_UNSUCCESSFUL_URL,
 } from 'routes/urls';
-import { deleteDraftClaimFromStore, generateRedisKey, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
-import {Claim} from 'models/claim';
+import { deleteDraftClaimFromStore, generateRedisKey} from 'modules/draft-store/draftStoreService';
+import config from 'config';
 import { ClaimBilingualLanguagePreference } from 'common/models/claimBilingualLanguagePreference';
 import { getGaFeePaymentStatus } from '../applicationFee/generalApplicationFeePaymentService';
+import { CivilServiceClient } from 'client/civilServiceClient';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('claimFeePaymentConfirmationService');
 
 const success = 'Success';
 const paymentCancelledByUser = 'Payment was cancelled by the user';
+const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
+const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
 export const getRedirectUrl = async (claimId: string, req: AppRequest): Promise<string> => {
   try {
     const redisClaimId = generateRedisKey(req);
-    const claim: Claim = await getCaseDataFromStore(redisClaimId);
+    const claim = await civilServiceClient.retrieveClaimDetails(claimId, <AppRequest>req);
     const paymentInfo = claim.generalApplication?.applicationFeePaymentDetails;
     const paymentStatus = await getGaFeePaymentStatus(claimId, paymentInfo?.paymentReference, req);
 
