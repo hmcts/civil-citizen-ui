05f1bb3c883ef02230788ca49ac331b47acd25a5
Manish Garg <manish.garg@hmcts.net>
2024-06-25 13:06:07 +0100

    CIV-13767 Updated amount on payment successful screen application

--- src/main/services/features/generalApplication/generalApplicationService.ts
+++ src/main/services/features/generalApplication/generalApplicationService.ts
@@ -23,15 +23,10 @@ import {UploadGAFiles} from 'models/generalApplication/uploadGAFiles';
 import {GaHelpWithFees} from 'models/generalApplication/gaHelpWithFees';
 import {AcceptDefendantOffer, ProposedPaymentPlanOption} from 'common/models/generalApplication/response/acceptDefendantOffer';
 import {GaResponse} from 'common/models/generalApplication/response/gaResponse';
-import config from 'config';
-import { GeneralApplicationClient } from 'client/generalApplicationClient';
-import { ApplicationResponse } from 'common/models/generalApplication/applicationResponse';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('claimantResponseService');
 
-const baseUrl: string = config.get<string>('services.generalApplication.url');
-const generalApplicationClient = new GeneralApplicationClient(baseUrl);
 export const saveApplicationType = async (claimId: string, applicationType: ApplicationType, index?: number): Promise<void> => {
   try {
     const claim = await getCaseDataFromStore(claimId, true);
@@ -342,3 +337,4 @@ export const saveHelpWithFeesDetails = async (claimId: string, value: any, hwfPr
     throw error;
   }
 };
+
