004ae47b696a8861d768f0da4bbf13abc1439b15
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-06-25 09:30:28 +0200

    Update: view application (WIP)

--- src/main/routes/features/generalApplication/viewApplicationController.ts
+++ src/main/routes/features/generalApplication/viewApplicationController.ts
@@ -2,6 +2,9 @@ import {NextFunction, RequestHandler, Response, Router} from 'express';
 import {GA_VIEW_APPLICATION_URL} from 'routes/urls';
 import {AppRequest} from 'common/models/AppRequest';
 import {getApplicationSections} from 'services/features/generalApplication/viewApplication/viewApplicationService';
+import { ApplicationResponse } from 'common/models/generalApplication/applicationResponse';
+import { getApplicationFromGAService } from 'services/features/generalApplication/generalApplicationService';
+import { CaseState } from 'common/form/models/claimDetails';
 
 const viewApplicationController = Router();
 const viewPath = 'features/generalApplication/view-applications';
@@ -13,7 +16,10 @@ viewApplicationController.get(GA_VIEW_APPLICATION_URL, (async (req: AppRequest, 
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const summaryRows = await getApplicationSections(req, applicationId, lang);
     const pageTitle = 'PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.PAGE_TITLE';
-    res.render(viewPath, {backLinkUrl, summaryRows, pageTitle });
+    const applicationResponse: ApplicationResponse = await getApplicationFromGAService(req, applicationId);
+    const isResponseFromCourt = applicationResponse.state === CaseState.JUDICIAL_REFERRAL ? true : false;
+
+    res.render(viewPath, {backLinkUrl, summaryRows, pageTitle, isResponseFromCourt });
   } catch (error) {
     next(error);
   }
