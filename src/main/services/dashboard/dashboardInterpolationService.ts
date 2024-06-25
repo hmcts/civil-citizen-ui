faf19d4ca0e90103d490ea2cfba4eacbe9adaa2e
meulendijksm <max.meulendijks@hmcts.net>
2024-06-25 16:57:15 +0100

    CIV-13717 - add dashboard status & notification redirect

--- src/main/services/dashboard/dashboardInterpolationService.ts
+++ src/main/services/dashboard/dashboardInterpolationService.ts
@@ -115,7 +115,6 @@ const setDashboardValues = (claim: Claim, claimId: string, notification?: Dashbo
   valuesMap.set('{GENERAL_APPLICATIONS_INITIATION_PAGE_URL}', '#');
   valuesMap.set('{VIEW_MEDIATION_DOCUMENTS}', VIEW_MEDIATION_DOCUMENTS.replace(':id', claimId));
   valuesMap.set('{CONFIRM_YOU_HAVE_BEEN_PAID_URL}', CONFIRM_YOU_HAVE_BEEN_PAID_URL.replace(':id', claimId));
-  valuesMap.set('{VIEW_DECISION_RECONSIDERATION}', CASE_DOCUMENT_VIEW_URL.replace(':id', claimId).replace(':documentId', getSystemGeneratedCaseDocumentIdByType(claim.systemGeneratedCaseDocuments, DocumentType.DECISION_MADE_ON_APPLICATIONS)));
 
   if (claimantRequirements) {
     valuesMap.set('{VIEW_CLAIMANT_HEARING_REQS_SIZE}', displayDocumentSizeInKB(claimantRequirements.documentSize));
@@ -149,6 +148,10 @@ const setDashboardValues = (claim: Claim, claimId: string, notification?: Dashbo
       .replace(':notificationId', notificationId)
       .replace(':documentId', documentIdExtractor(documentId)));
 
+    valuesMap.set('{VIEW_DECISION_RECONSIDERATION}', DASHBOARD_NOTIFICATION_REDIRECT
+      .replace(':id', claimId)
+      .replace(':locationName', 'VIEW_DECISION_RECONSIDERATION')
+      .replace(':notificationId', notificationId));
   }
 
   return valuesMap;
