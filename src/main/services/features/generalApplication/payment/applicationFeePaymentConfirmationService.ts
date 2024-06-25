53f53025babad0021894651908fbc7c792aa51a4
Manish Garg <manish.garg@hmcts.net>
2024-06-25 14:48:35 +0100

    CIV-13767 Removed dangled comma

--- src/main/services/features/generalApplication/payment/applicationFeePaymentConfirmationService.ts
+++ src/main/services/features/generalApplication/payment/applicationFeePaymentConfirmationService.ts
@@ -1,5 +1,5 @@
 import {AppRequest} from 'models/AppRequest';
-import {GA_APPLY_HELP_WITH_FEE_SELECTION, GA_PAYMENT_SUCCESSFUL_URL, GA_PAYMENT_UNSUCCESSFUL_URL,} from 'routes/urls';
+import {GA_APPLY_HELP_WITH_FEE_SELECTION, GA_PAYMENT_SUCCESSFUL_URL, GA_PAYMENT_UNSUCCESSFUL_URL} from 'routes/urls';
 import { getGaFeePaymentStatus } from '../applicationFee/generalApplicationFeePaymentService';
 import { getClaimById } from 'modules/utilityService';
 
