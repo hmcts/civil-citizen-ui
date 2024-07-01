732cefb07329506d6eab816ae7f76e83c7ff202d
hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>
2024-07-01 14:10:55 +0200

    CIV-9458 : Add index param
    Signed-off-by: hmcts-version1-pablo <107927784+hmcts-version1-pablo@users.noreply.github.com>

--- src/main/routes/features/generalApplication/viewApplicationController.ts
+++ src/main/routes/features/generalApplication/viewApplicationController.ts
@@ -2,6 +2,7 @@ import {NextFunction, RequestHandler, Response, Router} from 'express';
 import {DASHBOARD_URL, GA_VIEW_APPLICATION_URL} from 'routes/urls';
 import {AppRequest} from 'common/models/AppRequest';
 import {getApplicationSections} from 'services/features/generalApplication/viewApplication/viewApplicationService';
+import {queryParamNumber} from 'common/utils/requestUtils';
 
 const viewApplicationController = Router();
 const viewPath = 'features/generalApplication/view-applications';
@@ -10,10 +11,11 @@ const backLinkUrl = 'test'; // TODO: add url
 viewApplicationController.get(GA_VIEW_APPLICATION_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const applicationId = req.query.applicationId ? String(req.query.applicationId) : null;
+    const applicationIndex = queryParamNumber(req, 'index');
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const summaryRows = await getApplicationSections(req, applicationId, lang);
     const pageTitle = 'PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.PAGE_TITLE';
-    res.render(viewPath, {backLinkUrl, summaryRows, pageTitle, dashboardUrl: DASHBOARD_URL });
+    res.render(viewPath, {backLinkUrl, summaryRows, pageTitle, dashboardUrl: DASHBOARD_URL, applicationIndex });
   } catch (error) {
     next(error);
   }
