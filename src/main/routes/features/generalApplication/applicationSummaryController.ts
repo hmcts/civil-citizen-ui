535f47f4292fb76bbfb2a68d20d2d8b9b50b2db0
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-07-03 09:28:12 +0200

    Fix: index

--- src/main/routes/features/generalApplication/applicationSummaryController.ts
+++ src/main/routes/features/generalApplication/applicationSummaryController.ts
@@ -34,7 +34,7 @@ applicationSummaryController.get(GA_APPLICATION_SUMMARY_URL, async (req: AppRequ
         types: application.case_data?.applicationTypes,
         id: application.id,
         createdDate: dateTimeFormat(application.created_date, lng),
-        applicationUrl: `${constructResponseUrlWithIdParams(application.id, GA_VIEW_APPLICATION_URL)}?applicationId=${application.id}&index${index}`,
+        applicationUrl: `${constructResponseUrlWithIdParams(application.id, GA_VIEW_APPLICATION_URL)}?applicationId=${application.id}&index=${index}`,
       });
     });
 
