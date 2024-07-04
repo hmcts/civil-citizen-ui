d56c11952f30640a02598379d01588173519e996
TurkingtonL <lillie.turkington@HMCTS.net>
2024-07-04 10:07:58 +0100

    formatting

--- src/main/app/auth/launchdarkly/launchDarklyClient.ts
+++ src/main/app/auth/launchdarkly/launchDarklyClient.ts
@@ -76,7 +76,7 @@ export async function  isDashboardEnabledForCase(date: Date): Promise<boolean> {
   return await getFlagValue('is-dashboard-enabled-for-case', epoch) as boolean;
 }
 
-export async function  isCarmEnabledForCase(date: Date): Promise<boolean> {
+export async function isCarmEnabledForCase(date: Date): Promise<boolean> {
   const { DateTime } = require('luxon');
   const systemTimeZone = DateTime.local().zoneName;
   const epoch = DateTime.fromISO(date, { zone: systemTimeZone }).toSeconds();
