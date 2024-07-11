c54d72ed33b52271bc531cf3d14e0064789953e3
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2024-07-11 17:16:56 +0100

    CIV-14162 Claim intention full admit Welsh translations (#4122)
    * CIV-14162 added mentioned welsh translations
    
    * fixed lint issues
    
    * fixed build issues
    
    * fixed unit tests
    
    * chnaged to common name
    
    * fixed unit tests
    
    * fixed text issues
    
    ---------
    
    Co-authored-by: Manish Garg <manish.garg@hmcts.net>
    Co-authored-by: Johnny Vineall <vineall@live.co.uk>

--- src/main/common/form/models/claimantResponse/courtProposedDate.ts
+++ src/main/common/form/models/claimantResponse/courtProposedDate.ts
@@ -1,7 +1,7 @@
 import {IsNotEmpty} from 'class-validator';
 
 export class CourtProposedDate {
-  @IsNotEmpty({message: 'ERRORS.VALID_YES_NO_SELECTION'})
+  @IsNotEmpty({ message: 'ERRORS.VALID_YES_NO_SELECTION_VARIATION1' })
     decision?: CourtProposedDateOptions;
 
   constructor(decision?: CourtProposedDateOptions) {
