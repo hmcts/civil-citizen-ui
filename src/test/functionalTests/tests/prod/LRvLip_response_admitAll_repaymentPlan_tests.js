906866345064301c06243bdc4f77dd8dd22345ec
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2024-07-15 13:42:02 +0100

    Update LRvLip_response_admitAll_repaymentPlan_tests.js

--- src/test/functionalTests/tests/prod/LRvLip_response_admitAll_repaymentPlan_tests.js
+++ src/test/functionalTests/tests/prod/LRvLip_response_admitAll_repaymentPlan_tests.js
@@ -13,7 +13,7 @@ let caseData;
 let claimNumber;
 let securityCode;
 
-Feature('Response with AdmitAll and Repayment plan @vjrtest');
+Feature('Response with AdmitAll and Repayment plan');
 
 Before(async ({api}) => {
   await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
@@ -29,19 +29,19 @@ Before(async ({api}) => {
   await CitizenDashboardSteps.VerifyClaimOnDashboard(claimNumber);
 });
 
-// Scenario('Response with AdmitAll and Repayment plan @citizenUI @admitAll @nightly', async ({api}) => {
-//   console.log('Response with AdmitAll claimRef --> ' + claimRef);
-//   await ResponseSteps.RespondToClaim(claimRef);
-//   await ResponseSteps.EnterPersonalDetails(claimRef);
-//   await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
-//   await ResponseSteps.EnterResponseToClaim(claimRef, admitAll);
-//   await ResponseSteps.EnterPaymentOption(claimRef, admitAll, repaymentPlan);
-//   await ResponseSteps.EnterFinancialDetails(claimRef);
-//   await ResponseSteps.EnterRepaymentPlan(claimRef);
-//   await ResponseSteps.CheckAndSubmit(claimRef, admitAll);
-//   // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-9655
-//   // await api.enterBreathingSpace(config.applicantSolicitorUser);
-//   // await api.liftBreathingSpace(config.applicantSolicitorUser);
-//   await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.admitAllPayByInstallment, config.claimState.PROCEEDS_IN_HERITAGE_SYSTEM);
-// }).tag('@regression-cui-r1');
+Scenario('Response with AdmitAll and Repayment plan @citizenUI @admitAll @nightly', async ({api}) => {
+  console.log('Response with AdmitAll claimRef --> ' + claimRef);
+  await ResponseSteps.RespondToClaim(claimRef);
+  await ResponseSteps.EnterPersonalDetails(claimRef);
+  await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
+  await ResponseSteps.EnterResponseToClaim(claimRef, admitAll);
+  await ResponseSteps.EnterPaymentOption(claimRef, admitAll, repaymentPlan);
+  await ResponseSteps.EnterFinancialDetails(claimRef);
+  await ResponseSteps.EnterRepaymentPlan(claimRef);
+  await ResponseSteps.CheckAndSubmit(claimRef, admitAll);
+  // commenting until this is fixed https://tools.hmcts.net/jira/browse/CIV-9655
+  // await api.enterBreathingSpace(config.applicantSolicitorUser);
+  // await api.liftBreathingSpace(config.applicantSolicitorUser);
+  await api.viewAndRespondToDefence(config.applicantSolicitorUser, config.defenceType.admitAllPayByInstallment, config.claimState.PROCEEDS_IN_HERITAGE_SYSTEM);
+}).tag('@regression-cui-r1');
 
