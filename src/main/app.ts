fcfd0e04dc8fbdadd2ba6a6fb93425e347ddad7e
jarekPierchala <jaroslaw.pierchala@hmcts.net>
2024-07-15 17:27:12 +0100

    missing comma

--- src/main/app.ts
+++ src/main/app.ts
@@ -138,7 +138,7 @@ app.use((_req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
     '*',
-  )
+  );
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
