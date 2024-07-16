ddbd837f5e7833b9c4d06d7fc4fe4a227e9be61d
Manish Garg <manish.garg@hmcts.net>
2024-07-16 13:59:57 +0100

    CIV-14636 Added redis logic for claimantIntent guard

--- src/main/routes/guards/claimantIntentGuard.ts
+++ src/main/routes/guards/claimantIntentGuard.ts
@@ -3,6 +3,9 @@ import { Claim } from '../../common/models/claim';
 import { constructResponseUrlWithIdParams } from '../../common/utils/urlFormatter';
 import { DASHBOARD_CLAIMANT_URL } from '../../routes/urls';
 import { getClaimById } from 'modules/utilityService';
+import { deleteDraftClaimFromStore, generateRedisKey, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
+import { AppRequest } from 'common/models/AppRequest';
+
 export const claimantIntentGuard = (
   req: Request,
   res: Response,
@@ -10,7 +13,16 @@ export const claimantIntentGuard = (
 ) => {
   (async () => {
     try {
-      const caseData: Claim = await getClaimById(req.params.id, req, true);
+      const redisClaimId = generateRedisKey(req as AppRequest);
+      let caseData: Claim = await getCaseDataFromStore(redisClaimId, true);
+
+      // Delete cache from redis if case state is not AWAITING_APPLICANT_INTENTION.
+      // Giving a chance to reload data from ccd database.
+      if(!caseData.isEmpty() && !caseData.isClaimantIntentionPending()) {
+        await deleteDraftClaimFromStore(redisClaimId);
+      }
+
+      caseData = await getClaimById(req.params.id, req, true);
       if (caseData.isClaimantIntentionPending() || req.originalUrl.includes('claimant-response/confirmation')) {
         next();
       } else {
--- src/test/unit/routes/guards/claimantIntentGuard.test.ts
+++ src/test/unit/routes/guards/claimantIntentGuard.test.ts
@@ -4,42 +4,59 @@ import { Claim } from 'common/models/claim';
 import { getClaimById } from 'modules/utilityService';
 import { constructResponseUrlWithIdParams } from 'common/utils/urlFormatter';
 import { CLAIMANT_RESPONSE_CONFIRMATION_URL, DASHBOARD_CLAIMANT_URL } from 'routes/urls';
+import * as draftStoreService from '../../../../main/modules/draft-store/draftStoreService';
+
+jest.mock('../../../../main/modules/draft-store');
+jest.mock('../../../../main/modules/draft-store/draftStoreService');
 jest.mock('modules/utilityService', () => ({
   getClaimById: jest.fn(),
+  getRedisStoreForSession: jest.fn(),
+  deleteDraftClaimFromStore: jest.fn(),
 }));
+
 describe('claimantIntentGuard', () => {
   let req: Partial<Request>;
   let res: Partial<Response> & { redirect: jest.Mock };
   let next: jest.Mock;
+
+  const mockGetCaseData = draftStoreService.getCaseDataFromStore as jest.Mock;
+  const caseStoreData = new Claim();
+ 
   beforeEach(() => {
     req = {params: {id: '123'}, originalUrl: 'test' };
     res = { redirect: jest.fn() };
     next = jest.fn();
+    mockGetCaseData.mockImplementation(async () => {
+      return caseStoreData;
+    });
   });
+
   it('should call next if isClaimantIntentionPending returns true', async () => {
     const claim: Partial<Claim> = {
       isClaimantIntentionPending: jest.fn().mockReturnValue(true),
     };
-    (getClaimById as jest.Mock).mockResolvedValueOnce(claim as Claim);
+    (getClaimById as jest.Mock).mockResolvedValue(claim as Claim);
     await claimantIntentGuard(req as Request, res as Response, next);
     expect(next).toHaveBeenCalled();
     expect(res.redirect).not.toHaveBeenCalled();
   });
+
   it('should call next if isClaimantIntentionPending returns false but it`s claimant response confirmation page', async () => {
     const claim: Partial<Claim> = {
       isClaimantIntentionPending: jest.fn().mockReturnValue(false),
     };
-    (getClaimById as jest.Mock).mockResolvedValueOnce(claim as Claim);
+    (getClaimById as jest.Mock).mockResolvedValue(claim as Claim);
     req.originalUrl = CLAIMANT_RESPONSE_CONFIRMATION_URL;
     await claimantIntentGuard(req as Request, res as Response, next);
     expect(next).toHaveBeenCalled();
     expect(res.redirect).not.toHaveBeenCalled();
   });
+
   it('should redirect if isClaimantIntentionPending returns false', async () => {
-    const claim = {
+    const claim : Partial<Claim> = {
       isClaimantIntentionPending: jest.fn().mockReturnValue(false),
     };
-    (getClaimById as jest.Mock).mockResolvedValueOnce(claim);
+    (getClaimById as jest.Mock).mockResolvedValue(claim as Claim);
     const redirectUrl = constructResponseUrlWithIdParams(
       req.params.id,
       DASHBOARD_CLAIMANT_URL,
@@ -47,9 +64,10 @@ describe('claimantIntentGuard', () => {
     await claimantIntentGuard(req as Request, res as Response, next);
     expect(res.redirect).toHaveBeenCalledWith(redirectUrl);
   });
+
   it('should pass the error to next if there is an exception', async () => {
     const error = new Error('Test error');
-    (getClaimById as jest.Mock).mockRejectedValueOnce(error);
+    (getClaimById as jest.Mock).mockResolvedValue(error);
     await claimantIntentGuard(req as Request, res as Response, next);
     expect(next).toHaveBeenCalledWith(error);
   });
