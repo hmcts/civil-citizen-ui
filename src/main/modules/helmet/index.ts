e7f3168f617241c70df6e9b021134ce6bb183973
jarekPierchala <jaroslaw.pierchala@hmcts.net>
2024-07-22 13:30:11 +0100

    copy gov-uk manifest to project assets

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -78,16 +78,6 @@ export class Helmet {
 
     // include default helmet functions
     app.use(helmet(this.config));
-    app.use('/assets/manifest.json', function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-      );
-      next();
-    });
     this.setContentSecurityPolicy(app);
   }
 
