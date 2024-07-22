49ed0b87e695012bbff2cf7d5757749d99ba6221
jarekPierchala <jaroslaw.pierchala@hmcts.net>
2024-07-22 13:58:39 +0100

    changed copy from file to dir

--- webpack/govukFrontend.js
+++ webpack/govukFrontend.js
@@ -5,7 +5,7 @@ const copyGovukTemplateAssets = new CopyWebpackPlugin({
   patterns: [
     { from: path.join(__dirname, '../node_modules/govuk-frontend/dist/govuk/assets/images'), to: 'assets/images' },
     { from: path.join(__dirname, '../node_modules/govuk-frontend/dist/govuk/assets/fonts'), to: 'assets/fonts' },
-    { from: path.join(__filename, '../node_modules/govuk-frontend/dist/govuk/assets/manifest.json'), to: 'assets/manifest.json' },
+    { from: path.join(__dirname, '../node_modules/govuk-frontend/dist/govuk/assets/manifest.json'), to: 'assets' },
   ],
 });
 
