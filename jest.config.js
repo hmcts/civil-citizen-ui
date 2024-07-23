146051d7897a222ba3157455440838f47b0cda9a
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-07-23 10:37:04 +0100

    add jest config

--- jest.config.js
+++ jest.config.js
@@ -30,4 +30,12 @@ module.exports = {
     "src/**/*.js",
     "!src/**/*.test.js",
   ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  }
 };
