5cc7cb76d827e386d20064db98ee8f3bcb859004
Manish Garg <manish.garg@hmcts.net>
2024-07-31 15:12:31 +0100

    CIV-14326 Fixed liniting issues

--- src/main/services/features/generalApplication/viewApplication/viewApplicationService.ts
+++ src/main/services/features/generalApplication/viewApplication/viewApplicationService.ts
@@ -91,13 +91,12 @@ const getHearingOrder = (applicationResponse: ApplicationResponse, lang: string)
   const hearingOrderDocs = applicationResponse?.case_data?.hearingOrderDocument;
   let hearingOrderDocInfoArray : DocumentInformation[] = [];
   if(hearingOrderDocs) {
-  hearingOrderDocInfoArray = hearingOrderDocs.sort((item1,item2) => {
-   return new Date(item2?.value?.createdDatetime).getTime() - new Date(item1?.value?.createdDatetime).getTime()
-  })
-  .map(hearingOrder => {
-    return setUpDocumentLinkObject(hearingOrder?.value?.documentLink, hearingOrder?.value?.createdDatetime, applicationResponse?.id, lang, 'PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.HEARING_ORDER');
-  });
- }
+    hearingOrderDocInfoArray = hearingOrderDocs.sort((item1,item2) => {
+      return new Date(item2?.value?.createdDatetime).getTime() - new Date(item1?.value?.createdDatetime).getTime();
+    }).map(hearingOrder => {
+      return setUpDocumentLinkObject(hearingOrder?.value?.documentLink, hearingOrder?.value?.createdDatetime, applicationResponse?.id, lang, 'PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.HEARING_ORDER');
+    });
+  }
   return hearingOrderDocInfoArray;
 };
 
