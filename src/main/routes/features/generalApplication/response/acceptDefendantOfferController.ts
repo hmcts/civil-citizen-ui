3569ab633b6c57a4c0b15b9ccc66c8ff253cf629
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2024-08-05 17:02:20 +0100

    CIV-14720 Refactor GA to Allow storing GA Response progress in multiple applications (#4262)
    * CIV-14720 added redis changes for GA respondent response
    
    * CIV-14720 resolved lint issues
    
    * fixed build issues
    
    * fixed unit tests in the pipeline
    
    * CIV-14720 fixed pipeline issues
    
    * CIV-14720 fixed build issues
    
    * fixed build issues
    
    * CIV-14720 build issues
    
    * CIV-14720 fixed accessbility issues
    
    * CIV-14720 fixed build issues
    
    * CIV-14720 fixed lint issues
    
    ---------
    
    Co-authored-by: neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
    Co-authored-by: Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
    Co-authored-by: sankhajuria <sankhajuria@gmail.com>

--- src/main/routes/features/generalApplication/response/acceptDefendantOfferController.ts
+++ src/main/routes/features/generalApplication/response/acceptDefendantOfferController.ts
@@ -1,22 +1,23 @@
 import { NextFunction, RequestHandler, Response, Router } from 'express';
 import { AppRequest } from 'common/models/AppRequest';
 import { GenericForm } from 'common/form/models/genericForm';
-import { generateRedisKey, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
+import { generateRedisKey, generateRedisKeyForGA, getCaseDataFromStore } from 'modules/draft-store/draftStoreService';
 import { GA_ACCEPT_DEFENDANT_OFFER_URL } from 'routes/urls';
 import { getCancelUrl, saveAcceptDefendantOffer } from 'services/features/generalApplication/generalApplicationService';
 import { Claim } from 'common/models/claim';
 import { AcceptDefendantOffer } from 'common/models/generalApplication/response/acceptDefendantOffer';
 import { getRespondToApplicationCaption } from 'services/features/generalApplication/response/generalApplicationResponseService';
+import { getDraftGARespondentResponse } from 'services/features/generalApplication/response/generalApplicationResponseStoreService';
 
 const acceptDefendantOfferController = Router();
 const viewPath = 'features/generalApplication/response/accept-defendant-offer';
 
-const renderView = async (claimId: string, claim: Claim, form: GenericForm<AcceptDefendantOffer>, lng: string, res: Response): Promise<void> => {
+const renderView = async (claimId: string, claim: Claim, form: GenericForm<AcceptDefendantOffer>, lng: string, appId: string, res: Response): Promise<void> => {
   const cancelUrl = await getCancelUrl(claimId, claim);
   const backLinkUrl = 'test'; // TODO: add url
   res.render(viewPath, {
     cancelUrl,
-    caption: getRespondToApplicationCaption(claim, lng),
+    caption: getRespondToApplicationCaption(claim, appId, lng),
     backLinkUrl,
     form,
   });
@@ -28,9 +29,10 @@ acceptDefendantOfferController.get(GA_ACCEPT_DEFENDANT_OFFER_URL, async (req: Ap
     const lang = req.query.lang || req.cookies.lang;
     const redisKey = generateRedisKey(req);
     const claim = await getCaseDataFromStore(redisKey);
-    const acceptDefendantOffer = claim.generalApplication?.response?.acceptDefendantOffer || new AcceptDefendantOffer();
+    const gaResponse = await getDraftGARespondentResponse(generateRedisKeyForGA(req));
+    const acceptDefendantOffer = gaResponse?.acceptDefendantOffer || new AcceptDefendantOffer();
     const form = new GenericForm(acceptDefendantOffer);
-    renderView(claimId, claim, form, lang, res);
+    renderView(claimId, claim, form, lang, req.params.appId, res);
   } catch (error) {
     next(error);
   }
@@ -55,9 +57,9 @@ acceptDefendantOfferController.post(GA_ACCEPT_DEFENDANT_OFFER_URL, (async (req: 
       const redisKey = generateRedisKey(req);
       const claim = await getCaseDataFromStore(redisKey);
       const lang = req.query.lang || req.cookies.lang;
-      return await renderView(claimId, claim, form, lang, res);
+      return await renderView(claimId, claim, form, lang, req.params.appId, res);
     }
-    await saveAcceptDefendantOffer(generateRedisKey(req), acceptDefendantOffer);
+    await saveAcceptDefendantOffer(generateRedisKeyForGA(req), acceptDefendantOffer);
     res.redirect('test'); // TODO: add url
   } catch (error) {
     next(error);
