4a6d098fb62dbd55146028d9b81a12e0ad18242a
Pijian Liao <Pijian.Liao@HMCTS.NET>
2024-08-08 16:19:48 +0100

    CIV-14276 validate input

--- src/main/routes/features/generalApplication/additionalInfoUpload/respondController.ts
+++ src/main/routes/features/generalApplication/additionalInfoUpload/respondController.ts
@@ -19,11 +19,14 @@ const viewPath = 'features/generalApplication/additionalInfoUpload/additional-in
 respondAddInfoController.get(GA_RESPOND_ADDITIONAL_INFO_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const { appId, id: claimId } = req.params;
+    const currentUrl = constructResponseUrlWithIdAndAppIdParams(claimId, appId, GA_RESPOND_ADDITIONAL_INFO_URL);
     const claimIdPrettified = caseNumberPrettify(claimId);
     const claim = await getClaimById(claimId, req, true);
     const cancelUrl = await getCancelUrl(claimId, claim);
     const backLinkUrl = constructResponseUrlWithIdAndAppIdParams(claimId, appId, GA_RESPOND_ADDITIONAL_INFO_URL);
-    res.render(viewPath, { backLinkUrl, cancelUrl, claimIdPrettified, claim});
+    const respondAddInfo = new RespondAddInfo();
+    const form = new GenericForm(respondAddInfo);
+    res.render(viewPath, { currentUrl, backLinkUrl, cancelUrl, claimIdPrettified, claim, form});
   } catch (error) {
     next(error);
   }
@@ -33,22 +36,21 @@ respondAddInfoController.post(GA_RESPOND_ADDITIONAL_INFO_URL, (async (req: AppRe
   try {
     const option = req.body.option;
     const text = req.body.additionalText;
-
+    const { appId, id:claimId } = req.params;
+    const currentUrl = constructResponseUrlWithIdAndAppIdParams(claimId, appId, GA_RESPOND_ADDITIONAL_INFO_URL);
     const respondAddInfo = new RespondAddInfo(
       option,
       text,
     );
     const form = new GenericForm(respondAddInfo);
     await form.validate();
-    form.validateSync();
-
     if (form.hasErrors()) {
       const { appId, id: claimId } = req.params;
       const claimIdPrettified = caseNumberPrettify(claimId);
       const claim = await getClaimById(claimId, req, true);
       const cancelUrl = await getCancelUrl(claimId, claim);
       const backLinkUrl = constructResponseUrlWithIdAndAppIdParams(claimId, appId, GA_RESPOND_ADDITIONAL_INFO_URL);
-      res.render(viewPath, { backLinkUrl, cancelUrl, claimIdPrettified, claim});
+      return await res.render(viewPath, { currentUrl, backLinkUrl, cancelUrl, claimIdPrettified, claim, form});
     }
     //await saveAcceptDefendantOffer(generateRedisKeyForGA(req), acceptDefendantOffer);
     res.redirect('test'); // TODO: add url
