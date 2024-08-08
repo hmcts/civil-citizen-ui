7e66a13107e7a21da569dd1550bcf9989cfc087c
David Jeronimo <david.jeronimo@jmcts.net>
2024-08-08 13:11:34 +0200

    CIV-14011 ga response submitted (2)

--- src/main/services/features/generalApplication/response/submitApplicationResponse.ts
+++ src/main/services/features/generalApplication/response/submitApplicationResponse.ts
@@ -3,7 +3,7 @@ import config from 'config';
 import {Application} from 'models/application';
 import {GaServiceClient} from 'client/gaServiceClient';
 import { toCcdGeneralApplicationWithResponse } from 'services/translation/generalApplication/ccdTranslation';
-import { getDraftGARespondentResponse } from './generalApplicationResponseStoreService';
+import { deleteDraftGARespondentResponseFromStore, getDraftGARespondentResponse } from './generalApplicationResponseStoreService';
 import { generateRedisKeyForGA } from 'modules/draft-store/draftStoreService';
 
 const {Logger} = require('@hmcts/nodejs-logging');
@@ -15,11 +15,12 @@ const gaServiceClient: GaServiceClient = new GaServiceClient(gaServiceApiBaseUrl
 export const submitApplicationResponse = async (req: AppRequest): Promise<Application> => {
   try {
     const applicationId = req.params.appId;
-    const applicationResponse = await gaServiceClient.getApplication(req, `${applicationId}`);
     const gaRedisKey = generateRedisKeyForGA(req);
     const gaRespondentResponse = await getDraftGARespondentResponse(gaRedisKey);
-    const generalApplication = toCcdGeneralApplicationWithResponse(applicationResponse?.case_data, gaRespondentResponse);
-    return await gaServiceClient.submitRespondToApplicationEvent(applicationResponse.id, generalApplication, req);
+    const generalApplication = toCcdGeneralApplicationWithResponse(gaRespondentResponse);
+    const application = await gaServiceClient.submitRespondToApplicationEvent(applicationId, generalApplication, req);
+    await deleteDraftGARespondentResponseFromStore(gaRedisKey);
+    return application;
   } catch (err) {
     logger.error(err);
     throw err;
