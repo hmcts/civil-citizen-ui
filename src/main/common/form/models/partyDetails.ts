a7f42bddede4dbf0dccb873d2fb0bb98f555c77b
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2024-08-09 12:03:00 +0100

    CIV-14714 Fix error overlap for name

--- src/main/common/form/models/partyDetails.ts
+++ src/main/common/form/models/partyDetails.ts
@@ -7,7 +7,6 @@ import {SpecialCharValidator} from 'form/validators/specialCharValidator';
 export class PartyDetails {
 
   @ValidateIf(o => o.title !== undefined)
-  @MaxLength(35, {message: 'ERRORS.ENTER_VALID_TITLE'})
   @Validate(SpecialCharValidator)
   @Validate(FullNameValidator,['nameLength', ' '])
     title?: string;
@@ -15,7 +14,6 @@ export class PartyDetails {
   @ValidateIf(o => o.firstName !== undefined)
   @IsDefined({message: 'ERRORS.ENTER_FIRST_NAME'})
   @IsNotEmpty({message: 'ERRORS.ENTER_FIRST_NAME'})
-  @MaxLength(255, {message: 'ERRORS.TEXT_TOO_MANY'})
   @Validate(SpecialCharValidator)
   @Validate(FullNameValidator,['nameLength', ' '])
     firstName?: string;
@@ -23,7 +21,6 @@ export class PartyDetails {
   @ValidateIf(o => o.lastName !== undefined)
   @IsDefined({message: 'ERRORS.ENTER_LAST_NAME'})
   @IsNotEmpty({message: 'ERRORS.ENTER_LAST_NAME'})
-  @MaxLength(255, {message: 'ERRORS.TEXT_TOO_MANY'})
   @Validate(SpecialCharValidator)
   @Validate(FullNameValidator,['nameLength', 'ERRORS.TEXT_TOO_MANY'])
     lastName?: string;
--- src/main/common/form/validators/fullNameValidator.ts
+++ src/main/common/form/validators/fullNameValidator.ts
@@ -7,28 +7,45 @@ import {isJudgmentOnlineLive} from '../../../app/auth/launchdarkly/launchDarklyC
 @ValidatorConstraint({name: 'fullNameValidator', async: true})
 export class FullNameValidator implements ValidatorConstraintInterface {
   errorMessage: string[] = [];
-
+  isJudgmentOnlineLiveFlagOff: boolean;
   async getJudgmentOnlineFlag() {
     return await isJudgmentOnlineLive();
   }
 
   async validate(fieldText: string, validationArguments?: ValidationArguments) {
-    if (!await this.getJudgmentOnlineFlag()) { // Do not validate in case isJudgmentOnlineLive flag is off
+    if (!await this.getJudgmentOnlineFlag()) { // Old validation in case isJudgmentOnlineLive flag is off
+      this.isJudgmentOnlineLiveFlagOff = true;
+      if (validationArguments.property == 'title' && validationArguments.value.length > 35) {
+        this.errorMessage.push('ERRORS.ENTER_VALID_TITLE');
+        return false;
+      } else if ((validationArguments.property == 'firstName' || validationArguments.property == 'lastName')
+        && validationArguments.value.length > 255) {
+        this.errorMessage.push('ERRORS.TEXT_TOO_MANY');
+        return false;
+      }
       return true;
     }
 
+    this.isJudgmentOnlineLiveFlagOff = false;
     if (validationArguments.constraints && validationArguments.constraints.length > 0) {
       const property = validationArguments.constraints[0];
       if (validationArguments.value.length > 0) {
-        this.errorMessage.push(validationArguments.constraints[1]);
         const value = (validationArguments.object as never)[property];
-        return value <= 70;
+        if (value > 70) {
+          this.errorMessage.push(validationArguments.constraints[1]);
+          return false;
+        }
+        return true;
       }
     }
     return true;
   }
 
   defaultMessage() {
-    return this.errorMessage.pop();
+    if (this.isJudgmentOnlineLiveFlagOff) {
+      return this.errorMessage.shift();
+    } else {
+      return this.errorMessage.pop();
+    }
   }
 }
