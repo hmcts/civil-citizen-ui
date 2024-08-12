5b009a9d0e7c7c0e20a7cb6887f2c067b4051c87
laarmada <sergiosalmar@yahoo.es>
2024-08-12 09:02:53 +0100

    fix test

--- src/test/unit/app/client/pcq/savePcqIdClaim.test.ts
+++ src/test/unit/app/client/pcq/savePcqIdClaim.test.ts
@@ -2,12 +2,14 @@ import * as draftStoreService from 'modules/draft-store/draftStoreService';
 import {Claim} from 'common/models/claim';
 import {savePcqIdClaim} from 'client/pcq/savePcqIdClaim';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
+import {AppRequest} from 'models/AppRequest';
 
 jest.mock('../../../../../main/modules/draft-store');
 jest.mock('../../../../../main/modules/draft-store/draftStoreService');
 
 describe('Save PCQ ID Service', () => {
   const mockGetCaseData = draftStoreService.getCaseDataFromStore as jest.Mock;
+  const mockRequest = { params: { id: '12345' } } as unknown as AppRequest;
 
   it('should save PCQ id', async () => {
     //Given
@@ -16,7 +18,7 @@ describe('Save PCQ ID Service', () => {
     });
     const spySave = jest.spyOn(draftStoreService, 'saveDraftClaim');
     //When
-    await savePcqIdClaim('pcqId', 'claimId');
+    await savePcqIdClaim('pcqId', mockRequest,'claimId');
     //Then
     expect(spySave).toBeCalled();
   });
@@ -28,7 +30,7 @@ describe('Save PCQ ID Service', () => {
       throw new Error(TestMessages.REDIS_FAILURE);
     });
     //Then
-    await expect(savePcqIdClaim('pcqId', 'claimId')).rejects.toThrow(
+    await expect(savePcqIdClaim('pcqId', mockRequest,'claimId')).rejects.toThrow(
       TestMessages.REDIS_FAILURE,
     );
   });
