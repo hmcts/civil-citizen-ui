113c765ff5d13c45f1f65bd228b8afd678b1b7ce
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-08-13 12:15:15 +0100

    CIV-14088 Update unit test

--- src/test/unit/routes/features/claim/generalApplication/response/respondentUploadEvidenceDocumentsController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/response/respondentUploadEvidenceDocumentsController.test.ts
@@ -175,6 +175,15 @@ describe('General Application - Respondent GA upload evidence documents ', () =>
         });
     });
 
+    it('should throw the error if user click continue button without uploading a file', async () => {
+      await request(app)
+        .post(constructResponseUrlWithIdAndAppIdParams('123', '345', GA_RESPONDENT_UPLOAD_DOCUMENT_URL))
+        .expect((res) => {
+          expect(res.status).toBe(302);
+          expect(res.text).toContain(constructResponseUrlWithIdAndAppIdParams('123', '345', GA_RESPONDENT_UPLOAD_DOCUMENT_URL));
+        });
+    });
+
     it('should save the file and display', async () => {
       jest.spyOn(CivilServiceClient.prototype, 'uploadDocument').mockResolvedValueOnce(mockCaseDocument);
       jest.spyOn(gaStoreResponseService, 'getDraftGARespondentResponse').mockResolvedValue(gaResponse);
