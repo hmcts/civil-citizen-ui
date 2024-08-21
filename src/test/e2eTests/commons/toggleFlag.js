c599a488a2b70f3f6bf17a3b5cb503f4da618deb
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-08-21 07:14:20 +0100

    fix get launch darkly from env. variables

--- src/test/e2eTests/commons/toggleFlag.js
+++ src/test/e2eTests/commons/toggleFlag.js
@@ -1,6 +1,9 @@
 const I = actor();
 
-const toggleFlag = (flagKey, flagValue) => I.amOnPage(`/testing-support/toggleflag/${flagKey}/${flagValue}`);
+const toggleFlag = (flagKey, flagValue) => {
+  I.amOnPage(`/testing-support/toggleflag/${flagKey}/${flagValue}`);
+  I.see('Flag updated successfully');
+};
 
 module.exports = {
   toggleFlag,
