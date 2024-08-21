8b666a56ae2c301cfc5b6af98c7e05a43ae65fff
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2024-08-21 22:23:12 +0100

    CIV-14353 corrected error messages according to ACs

--- src/main/common/models/generalApplication/response/respondAddInfo.ts
+++ src/main/common/models/generalApplication/response/respondAddInfo.ts
@@ -2,10 +2,10 @@ import { YesNo } from 'common/form/models/yesNo';
 import {IsNotEmpty, ValidateIf} from 'class-validator';
 
 export class RespondAddInfo {
-  @ValidateIf(o => !o.option && o.additionalText> 1)
+
   @IsNotEmpty({ message: 'ERRORS.GENERAL_APPLICATION.RESPONDENT_UPLOAD_OPTION.EMPTY_OPTION' })
     option?: YesNo;
-
+  @ValidateIf(o =>  o.option === YesNo.NO || o.additionalText === null)
   @IsNotEmpty({ message: 'ERRORS.GENERAL_APPLICATION.RESPONDENT_UPLOAD_OPTION.ERROR_INPUT' })
     additionalText?: string;
 
