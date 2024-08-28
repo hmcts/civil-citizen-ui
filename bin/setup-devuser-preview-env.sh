bbca80f667a404e36a984ab41a3ab87599b04e11
Mike Lemos <gobii@me.com>
2024-08-28 15:18:39 +0100

    DTSCCI-812 adding scripts to run a preview dev environment

---
+++ bin/setup-devuser-preview-env.sh
@@ -0,0 +1,23 @@
+ccdBranch=${1:-master}
+camundaBranch=${2:-master}
+generalAppCCDBranch=${3:-master}
+dmnBranch=${4:-master}
+waStandaloneBranch=${5:-master}
+
+echo "Loading Environment Variables"
+source ./bin/variables/load-dev-user-preview-environment-variables.sh
+
+echo "Importing Roles to the CCD pod"
+./bin/add-roles.sh
+
+echo "Importing Camunda definitions"
+./bin/pull-latest-camunda-files.sh ${camundaBranch}
+./bin/pull-latest-dmn-files.sh ${dmnBranch}
+./bin/pull-latest-camunda-wa-files.sh ${waStandaloneBranch}
+
+echo "Importing CCD definitions"
+./bin/import-ccd-definition.sh "-e *-prod.json,*-shuttered.json" ${ccdBranch}
+#./bin/import-ga-ccd-definition.sh "-e *-prod.json" ${generalAppCCDBranch}
+
+echo "ENV variables set for devuser-preview environment."
+echo "CDAM_REDIRECT_URL: $CCD_IDAM_REDIRECT_URL"
