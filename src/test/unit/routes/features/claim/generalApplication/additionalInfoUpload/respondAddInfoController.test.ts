029691e9efbcbb3a713c8aa5af7aa4a195734b46
LiaoP <pijian.liao@hmcts.net>
2024-08-29 18:39:08 +0100

    CIV-14276 unit test

--- src/test/unit/routes/features/claim/generalApplication/additionalInfoUpload/respondAddInfoController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/additionalInfoUpload/respondAddInfoController.test.ts
@@ -74,7 +74,7 @@ describe('General Application - uploadDocumentsForRequestMoreInfoController', ()
       .send({option: null, additionalText: null})
       .expect((res) => {
         expect(res.status).toBe(200);
-        expect(res.text).toContain('You need to tell us if you want to upload documents to support your response. Choose option: Yes or No.');
+        expect(res.text).toContain('You need to either enter the information requested in the box or select Yes to upload documents to support your response.');
       });
   });
 
