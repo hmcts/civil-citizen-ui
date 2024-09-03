b22dc1822835a55c91d5e33f72fd81796ce26571
jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
2024-09-03 14:14:08 +0100

    Update viewApplicationController.ts

--- src/main/routes/features/generalApplication/response/viewApplicationController.ts
+++ src/main/routes/features/generalApplication/response/viewApplicationController.ts
@@ -24,13 +24,11 @@ viewApplicationToRespondentController.get(GA_RESPONSE_VIEW_APPLICATION_URL, (asy
   try {
     const claimId = req.params.id;
     const applicationId = req.params.appId ? String(req.params.appId) : null;
-    const claimId = req.params.id;
     const applicationIndex = queryParamNumber(req, 'index') ? queryParamNumber(req, 'index') : '1';
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const summaryRows = await getApplicationSections(req, applicationId, lang);
     const backLinkUrl = constructResponseUrlWithIdAndAppIdParams(req.params.id, applicationId, GA_RESPONDENT_INFORMATION_URL);
     const applicationResponse: ApplicationResponse = await getApplicationFromGAService(req, applicationId);
-    const redirectUrl = await getRedirectUrl(applicationResponse, applicationId, claimId);
     const pageTitle = 'PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.PAGE_TITLE';
     const applicantDocuments: DocumentsViewComponent = getApplicantDocuments(applicationResponse, lang);
     const courtDocuments: DocumentsViewComponent = getCourtDocuments(applicationResponse, lang);
