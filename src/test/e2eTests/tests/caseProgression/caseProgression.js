1fa879ae4576586c8494687e3bf5263cf7635034
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-09-05 09:10:48 +0100

    add case progression pay and fees journey

--- src/test/e2eTests/tests/caseProgression/caseProgression.js
+++ src/test/e2eTests/tests/caseProgression/caseProgression.js
@@ -2,16 +2,27 @@ const config = require('../../../config');
 const CaseProgressionSteps = require('../../caseProgression/caseProgressionSteps');
 
 const {toggleFlag} = require('../../commons/toggleFlag');
+const HelpWithFees = require('../../caseProgression/helpWithFees');
 
-Feature('Case Progression journey').tag('@leo');
+Feature('Case Progression journey').tag('@e2e');
 
-Scenario.only('upload documents claimant', () => {
+Scenario('upload documents', () => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     toggleFlag('cui-case-progression', true);
     CaseProgressionSteps.start('0000000000000001');
     CaseProgressionSteps.typeOfDocument();
     CaseProgressionSteps.uploadDocuments();
     CaseProgressionSteps.checkAndSend();
+    toggleFlag('cui-case-progression', false);
+  }
+});
+
+Scenario('help with fees', () => {
+  if (['preview', 'demo'].includes(config.runningEnv)) {
+    toggleFlag('cui-case-progression', true);
+    HelpWithFees.applyHelpWithFees('0000000000000002');
+    HelpWithFees.start();
+    HelpWithFees.referenceNumber();
     toggleFlag('cui-case-progression', false);
   }
 });
