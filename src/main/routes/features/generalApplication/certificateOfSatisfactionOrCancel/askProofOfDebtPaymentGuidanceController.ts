408726d71e2ea1139185a39f64a70cb6c1a1dd9b
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-09-18 15:08:38 +0100

    CIV-15073 changes

---
+++ src/main/routes/features/generalApplication/certificateOfSatisfactionOrCancel/askProofOfDebtPaymentGuidanceController.ts
@@ -0,0 +1,28 @@
+import {NextFunction, RequestHandler, Response, Router} from 'express';
+import {
+  APPLICATION_TYPE_URL,
+  GA_ASK_PROOF_OF_DEBT_PAYMENT_GUIDANCE_URL
+} from 'routes/urls';
+import {AppRequest} from 'common/models/AppRequest';
+import {getClaimById} from 'modules/utilityService';
+import {getCancelUrl} from 'services/features/generalApplication/generalApplicationService';
+import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
+
+const askProofOfDebtPaymentGuidanceController = Router();
+const viewPath = 'features/generalApplication/certificateOfSatisfactionOrCancel/ask-proof-debtPayment-guidance';
+
+askProofOfDebtPaymentGuidanceController.get(GA_ASK_PROOF_OF_DEBT_PAYMENT_GUIDANCE_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
+  try {
+    const claimId = req.params.id;
+    const claim = await getClaimById(claimId, req, true);
+    const cancelUrl = await getCancelUrl(claimId, claim);
+    const backLinkUrl = constructResponseUrlWithIdParams(req.params.id, APPLICATION_TYPE_URL);
+    const nextPageUrl = 'test';
+    res.render(viewPath, { cancelUrl, backLinkUrl, nextPageUrl});
+
+  } catch (error) {
+    next(error);
+  }
+}) as RequestHandler);
+
+export default askProofOfDebtPaymentGuidanceController;
--- src/main/routes/urls.ts
+++ src/main/routes/urls.ts
@@ -393,3 +393,4 @@ export const GA_UPLOAD_WRITTEN_REPRESENTATION_DOCS_URL = `${BASE_GENERAL_APPLICA
 export const GA_UPLOAD_WRITTEN_REPRESENTATION_DOCS_CYA_URL = `${BASE_GENERAL_APPLICATION_URL}/:appId/upload-written-representation-docs/check-and-send`;
 export const GA_UPLOAD_WRITTEN_REPRESENTATION_DOCS_SUBMITTED_URL = `${BASE_GENERAL_APPLICATION_URL}/:appId/upload-written-representation-docs/submitted`;
 export const TEST_SUPPORT_TOGGLE_FLAG_ENDPOINT = '/testing-support/toggleFlag/:key/:value';
+export const GA_ASK_PROOF_OF_DEBT_PAYMENT_GUIDANCE_URL =`${BASE_GENERAL_APPLICATION_URL}/ask-proof-of-debt-payment-guidance`;
