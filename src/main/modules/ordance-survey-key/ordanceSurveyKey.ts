9bab381f9c685394e239d67075d6d92aa0e61b7b
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-09-20 10:54:52 +0100

    create api call

--- src/main/modules/ordance-survey-key/ordanceSurveyKey.ts
+++ src/main/modules/ordance-survey-key/ordanceSurveyKey.ts
@@ -1,18 +1,67 @@
-import { OSPlacesClient } from '@hmcts/os-places-client';
 import config from 'config';
+import axios from 'axios';
 
-let osPlacesClientInstance: OSPlacesClient | null = null;
+export class Point {
+  type: string;
+  coordinates: number[];
 
-export const createOSPlacesClientInstance = () => {
+  constructor(type: string, coordinates: number[]) {
+    this.type = type;
+    this.coordinates = coordinates;
+  }
+}
+
+export class Address {
+  readonly uprn: string;
+  readonly organisationName: string | undefined;
+  readonly departmentName: string | undefined;
+  readonly poBoxNumber: string | undefined;
+  readonly buildingName: string | undefined;
+  readonly subBuildingName: string | undefined;
+  readonly buildingNumber: number | undefined;
+  readonly thoroughfareName: string | undefined;
+  readonly dependentThoroughfareName: string | undefined;
+  readonly dependentLocality: string | undefined;
+  readonly doubleDependentLocality: string | undefined;
+  readonly postTown: string;
+  readonly postcode: string;
+  readonly postcodeType: string;
+  readonly formattedAddress: string;
+  readonly point: Point;
+  readonly udprn: string | undefined;
+
+  constructor(uprn: string, organisationName: string | undefined, departmentName: string | undefined, poBoxNumber: string | undefined, buildingName: string | undefined, subBuildingName: string | undefined, buildingNumber: number | undefined, thoroughfareName: string | undefined, dependentThoroughfareName: string | undefined, dependentLocality: string | undefined, doubleDependentLocality: string | undefined, postTown: string, postcode: string, postcodeType: string, formattedAddress: string, point: Point, udprn: string | undefined) {
+    this.uprn = uprn;
+    this.organisationName = organisationName;
+    this.departmentName = departmentName;
+    this.poBoxNumber = poBoxNumber;
+    this.buildingName = buildingName;
+    this.subBuildingName = subBuildingName;
+    this.buildingNumber = buildingNumber;
+    this.thoroughfareName = thoroughfareName;
+    this.dependentThoroughfareName = dependentThoroughfareName;
+    this.dependentLocality = dependentLocality;
+    this.doubleDependentLocality = doubleDependentLocality;
+    this.postTown = postTown;
+    this.postcode = postcode;
+    this.postcodeType = postcodeType;
+    this.formattedAddress = formattedAddress;
+    this.point = point;
+    this.udprn = udprn;
+  }
+}
+
+export async function lookupByPostcodeAndDataSet(postCode: string): Promise<any> {
+  console.log(config.get<string>('services.postcodeLookup.ordnanceSurveyApiKey'));
   const apiKey = config.get<string>('services.postcodeLookup.ordnanceSurveyApiKey');
-  if (apiKey && !osPlacesClientInstance) {
-    osPlacesClientInstance = new OSPlacesClient(apiKey);
-  }
-};
-
-export const getOSPlacesClientInstance = (): OSPlacesClient => {
-  if (!osPlacesClientInstance) {
-    createOSPlacesClientInstance();
-  }
-  return osPlacesClientInstance;
-};
+  const response = await axios.get(`https://api.os.uk/search/places/v1/postcode?dataset=DPA,LPI&postcode=${postCode}"&key=` + apiKey);
+  const addreses = response.data.results.map((jsonAddress: any) => {
+    if (!jsonAddress.DPA) {
+      return new Address(jsonAddress.LPI.UPRN, jsonAddress.LPI.ORGANISATION, jsonAddress.LPI.DEPARTMENT_NAME, jsonAddress.LPI.PO_BOX_NUMBER, jsonAddress.LPI.PAO_TEXT, jsonAddress.LPI.SAO_TEXT, jsonAddress.LPI.BUILDING_NUMBER, jsonAddress.LPI.STREET_DESCRIPTION, jsonAddress.LPI.DEPENDENT_THOROUGHFARE_NAME, jsonAddress.LPI.DEPENDENT_LOCALITY, jsonAddress.LPI.DOUBLE_DEPENDENT_LOCALITY, jsonAddress.LPI.TOWN_NAME, jsonAddress.LPI.POSTCODE_LOCATOR, jsonAddress.LPI.POSTAL_ADDRESS_CODE, jsonAddress.LPI.ADDRESS, new Point('Point', [jsonAddress.LPI.X_COORDINATE, jsonAddress.LPI.Y_COORDINATE]), jsonAddress.LPI.USRN);
+    }
+    else {
+      return new Address(jsonAddress.DPA.UPRN, jsonAddress.DPA.ORGANISATION_NAME, jsonAddress.DPA.DEPARTMENT_NAME, jsonAddress.DPA.PO_BOX_NUMBER, jsonAddress.DPA.BUILDING_NAME, jsonAddress.DPA.SUB_BUILDING_NAME, jsonAddress.DPA.BUILDING_NUMBER, jsonAddress.DPA.THOROUGHFARE_NAME, jsonAddress.DPA.DEPENDENT_THOROUGHFARE_NAME, jsonAddress.DPA.DEPENDENT_LOCALITY, jsonAddress.DPA.DOUBLE_DEPENDENT_LOCALITY, jsonAddress.DPA.POST_TOWN, jsonAddress.DPA.POSTCODE, jsonAddress.DPA.POSTAL_ADDRESS_CODE, jsonAddress.DPA.ADDRESS, new Point('Point', [jsonAddress.DPA.X_COORDINATE, jsonAddress.DPA.Y_COORDINATE]), jsonAddress.DPA.UDPRN);
+    }
+  });
+  return addreses;
+}
--- src/main/routes/features/response/citizenDetails/postcodeLookupController.ts
+++ src/main/routes/features/response/citizenDetails/postcodeLookupController.ts
@@ -1,11 +1,10 @@
 import {NextFunction, Router} from 'express';
-import {AddressInfoResponse} from '@hmcts/os-places-client';
 import {POSTCODE_LOOKUP_URL} from 'routes/urls';
-import {getOSPlacesClientInstance} from 'modules/ordance-survey-key/ordanceSurveyKey';
+import {lookupByPostcodeAndDataSet} from 'modules/ordance-survey-key/ordanceSurveyKey';
 
 export default Router()
-  .get(POSTCODE_LOOKUP_URL, (req, res, next: NextFunction) => {
-    const postcode = <string>req.query?.postcode;
+  .get(POSTCODE_LOOKUP_URL, async (req, res, next: NextFunction) => {
+/*    const postcode = <string>req.query?.postcode;
     if (!postcode || postcode.trim().length === 0) {
       return res.status(400).json({
         error: {
@@ -13,22 +12,20 @@ export default Router()
           message: 'Postcode not provided',
         },
       });
-    }
-    const osPlacesClient = getOSPlacesClientInstance();
-    osPlacesClient.lookupByPostcodeAndDataSet(req.query.postcode as string, 'DPA,LPI')
-      .then((addressInfoResponse: AddressInfoResponse) => {
-        if (!addressInfoResponse.isValid) {
-          throw new Error('Invalid post code');
-        }
+    }*/
+    //const osPlacesClient = getOSPlacesClientInstance();
+    //osPlacesClient.lookupByPostcodeAndDataSet(req.query.postcode as string, 'DPA,LPI')
+    try {
+      const response = await lookupByPostcodeAndDataSet('W1J 7NT');  // Call the service
 
-        addressInfoResponse.addresses
-          = addressInfoResponse.addresses.filter((addresses, index, self) =>
-            index === self.findIndex((t) =>
-              (t.formattedAddress === addresses.formattedAddress),
-            ));
-        res.json(addressInfoResponse);
-      })
-      .catch((err: Error) => {
-        next(err);
+      res.status(200).json(response);
+    }  catch (error) {
+      res.status(error.response.status).json({
+        error: {
+          status: error.response.status,
+          message: error.message,
+        },
       });
+    }
   });
+
