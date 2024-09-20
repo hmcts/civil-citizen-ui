54f5e0edb54c6f114ab4b1b1ff6dedf4a0a4851b
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-09-20 13:04:37 +0100

    reformat

--- src/main/routes/features/response/citizenDetails/postcodeLookupController.ts
+++ src/main/routes/features/response/citizenDetails/postcodeLookupController.ts
@@ -4,19 +4,20 @@ import {lookupByPostcodeAndDataSet} from 'modules/ordance-survey-key/ordanceSurv
 
 export default Router()
   .get(POSTCODE_LOOKUP_URL, async (req, res, next: NextFunction) => {
-/*    const postcode = <string>req.query?.postcode;
-    if (!postcode || postcode.trim().length === 0) {
-      return res.status(400).json({
-        error: {
-          status: 400,
-          message: 'Postcode not provided',
-        },
-      });
-    }*/
-    //const osPlacesClient = getOSPlacesClientInstance();
-    //osPlacesClient.lookupByPostcodeAndDataSet(req.query.postcode as string, 'DPA,LPI')
+
     try {
-      const response = await lookupByPostcodeAndDataSet('W1J 7NT');  // Call the service
+      const postcode = <string>req.query?.postcode;
+      if (!postcode || postcode.trim().length === 0) {
+        return res.status(400).json({
+          error: {
+            status: 400,
+            message: 'Postcode not provided',
+          },
+        });
+      }
+      //const osPlacesClient = getOSPlacesClientInstance();
+      //osPlacesClient.lookupByPostcodeAndDataSet(req.query.postcode as string, 'DPA,LPI')
+      const response = await lookupByPostcodeAndDataSet(req.query.postcode as string);  // Call the service
 
       res.status(200).json(response);
     }  catch (error) {
