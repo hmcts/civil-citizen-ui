324e00c469f145e7fb508496a786d5f2b78b88b0
marianadpereira <mariana.pereira@HMCTS.NET>
2024-09-23 12:26:00 +0100

    amending FT

--- src/test/functionalTests/tests/common/LRvLip_response_rejectAll_DisputeAll_tests.js
+++ src/test/functionalTests/tests/common/LRvLip_response_rejectAll_DisputeAll_tests.js
@@ -13,7 +13,7 @@ let caseData;
 let claimNumber;
 let securityCode;
 
-Feature('Response with RejectAll and DisputeAll @testing');
+Feature('Response with RejectAll and DisputeAll');
 
 Before(async ({api}) => {
   await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
@@ -29,7 +29,7 @@ Before(async ({api}) => {
   await CitizenDashboardSteps.VerifyClaimOnDashboard(claimNumber);
 });
 
-Scenario('Response with RejectAll and DisputeAll @citizenUI @rejectAll @nightly', async ({api, I}) => {
+Scenario('Response with RejectAll and DisputeAll - GA (Ask for more time) @citizenUI @rejectAll @nightly', async ({api, I}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     console.log('Creating GA app as claimant');
     await I.amOnPage('/dashboard');
