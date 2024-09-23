9e59458042b92d93b3ea2e44b5357f15c446221a
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-09-23 14:28:26 +0100

    CIV-15155 update button text

--- src/main/routes/features/caseProgression/hearingFee/payHearingFeeConfirmationController.ts
+++ src/main/routes/features/caseProgression/hearingFee/payHearingFeeConfirmationController.ts
@@ -17,7 +17,7 @@ const getHearingFeeConfirmationContent = (claimId: string, lng: string) => {
   return new PageSectionBuilder()
     .addTitle('PAGES.PAY_HEARING_FEE.CONFIRMATION_PAGE.WHAT_HAPPENS_NEXT')
     .addParagraph('PAGES.PAY_HEARING_FEE.CONFIRMATION_PAGE.YOU_WILL_RECEIVE')
-    .addButton(t('COMMON.BUTTONS.CLOSE_AND_RETURN_TO_CASE_OVERVIEW', {lng}), constructResponseUrlWithIdParams(claimId, DASHBOARD_URL)).build();
+    .addButton(t('COMMON.BUTTONS.GO_TO_ACCOUNT', {lng}), constructResponseUrlWithIdParams(claimId, DASHBOARD_URL)).build();
 };
 
 payHearingFeeConfirmationController.get(HEARING_FEE_CONFIRMATION_URL, (async (req, res, next: NextFunction) => {
