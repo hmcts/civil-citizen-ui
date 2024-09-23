209111897d2b04ddb7eeb3294b07b7a37ba38560
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-09-23 09:53:08 +0200

    Fix: back link

--- src/main/routes/features/generalApplication/response/respondentWantToUploadDocumentsController.ts
+++ src/main/routes/features/generalApplication/response/respondentWantToUploadDocumentsController.ts
@@ -4,7 +4,8 @@ import {
   GA_RESPONDENT_AGREEMENT_URL,
   GA_RESPONDENT_HEARING_PREFERENCE_URL,
   GA_RESPONDENT_UPLOAD_DOCUMENT_URL,
-  GA_RESPONDENT_WANT_TO_UPLOAD_DOCUMENT_URL, GA_RESPONSE_VIEW_APPLICATION_URL,
+  GA_RESPONDENT_WANT_TO_UPLOAD_DOCUMENT_URL,
+  GA_ACCEPT_DEFENDANT_OFFER_URL,
 } from 'routes/urls';
 import {GenericForm} from 'form/models/genericForm';
 import {AppRequest} from 'models/AppRequest';
@@ -83,8 +84,12 @@ respondentWantToUploadDocumentsController.post(GA_RESPONDENT_WANT_TO_UPLOAD_DOCU
 
 function getBackLinkUrl(claimId: string, applicationId: string, gaResponse: GaResponse) {
   if (gaResponse.generalApplicationType.length === 1 && gaResponse.generalApplicationType.includes(ApplicationTypeOption.VARY_PAYMENT_TERMS_OF_JUDGMENT)) {
-    return constructResponseUrlWithIdAndAppIdParams(claimId, applicationId, GA_RESPONSE_VIEW_APPLICATION_URL);
+    return constructResponseUrlWithIdAndAppIdParams(claimId, applicationId, GA_ACCEPT_DEFENDANT_OFFER_URL);
   }
-  return !gaResponse.respondentAgreement ? constructResponseUrlWithIdAndAppIdParams(claimId, applicationId, GA_AGREE_TO_ORDER_URL) : constructResponseUrlWithIdAndAppIdParams(claimId, applicationId, GA_RESPONDENT_AGREEMENT_URL);
+  
+  return !gaResponse.respondentAgreement 
+    ? constructResponseUrlWithIdAndAppIdParams(claimId, applicationId, GA_AGREE_TO_ORDER_URL) 
+    : constructResponseUrlWithIdAndAppIdParams(claimId, applicationId, GA_RESPONDENT_AGREEMENT_URL);
 }
+
 export default respondentWantToUploadDocumentsController;
