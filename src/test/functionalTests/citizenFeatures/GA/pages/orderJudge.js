2240b1283f8e6aa818acf6d65d8a656f74f52555
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2024-09-25 13:14:03 +0100

    CIV-0000 Add GA tests (#4248)
    * update
    
    * update
    
    * update
    
    * update
    
    * Update LRvLip_response_rejectAll_DisputeAll_tests.js
    
    * small changes
    
    * amending FT
    
    * Update CompanyVsIndividual.js
    
    * Update config.js
    
    * adding defendant user creation
    
    * adding wait for payment
    
    * Update createGAAppSteps.js
    
    * adding page objects for GA
    
    * Update createGAAppSteps.js
    
    ---------
    
    Co-authored-by: Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
    Co-authored-by: marianadpereira <71711509+marianadpereira@users.noreply.github.com>
    Co-authored-by: Johnny Vineall <vineall@live.co.uk>
    Co-authored-by: marianadpereira <mariana.pereira@HMCTS.NET>

---
+++ src/test/functionalTests/citizenFeatures/GA/pages/orderJudge.js
@@ -0,0 +1,48 @@
+const ContactUs = require('../../common/contactUs');
+const I = actor();
+
+const contactUs = new ContactUs();
+
+class OrderJudge {
+
+  async checkPageFullyLoaded () {
+    await I.waitForElement('//a[.=\'Cancel\']');
+  }
+
+  nextAction (nextAction) {
+    I.click(nextAction);
+  }
+
+  async verifyPageContent() {
+    this.checkPageFullyLoaded();
+    this.verifyBreadcrumbs();
+    this.verifyHeadingDetails();
+    await this.verifyPageText();
+    contactUs.verifyContactUs();
+  }
+
+  verifyBreadcrumbs() {
+    I.see('Back', '//a[@class="govuk-back-link"]');
+  }
+
+  verifyHeadingDetails() {
+    I.see('More time to do what is required by a court order', 'h1');
+    I.see('What order do you want the judge to make?', 'h1');
+  }
+
+  async verifyPageText() {
+    I.see('A judge will consider your application and make an order. The order that a judge would usually make to give more time to do what\'s required by a court order is in the box below.');
+    I.see('You\'ll need to add information to the box, like what you\'re asking for more time to do and a new deadline.');
+    I.see('You can also alter any of the other text if you need to.');
+    I.see('You\'ll be able to explain your reasons on the next screen.');
+    I.see('The contents of this box will be seen by the other parties.');
+    I.see('Enter the order that you want the judge to make', 'h1');
+    await I.seeElement('//*[@id="text"]');
+  }
+
+  async fillTextBox(orderText) {
+    await I.fillField('#text', orderText);
+  }
+}
+
+module.exports = OrderJudge;
