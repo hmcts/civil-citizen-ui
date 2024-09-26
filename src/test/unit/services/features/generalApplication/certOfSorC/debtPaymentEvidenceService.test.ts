0f9b30643d0eebe969ee5f85bc24e28f6e5fc0f4
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-09-26 12:29:48 +0100

    add service tests

---
+++ src/test/unit/services/features/generalApplication/certOfSorC/debtPaymentEvidenceService.test.ts
@@ -0,0 +1,21 @@
+import * as draftStoreService from 'modules/draft-store/draftStoreService';
+import {Claim} from 'models/claim';
+import {debtPaymentEvidenceService} from "services/features/generalApplication/certOfSorC/debtPaymentEvidenceService";
+import {debtPaymentOptions} from "models/generalApplication/debtPaymentOptions";
+
+jest.mock('modules/draft-store');
+jest.mock('modules/draft-store/draftStoreService');
+const mockGetCaseDataFromDraftStore = draftStoreService.getCaseDataFromStore as jest.Mock;
+let claim = new Claim();
+
+describe('DebtPaymentEvidenceService', () => {
+  it('save debt payment evidence', async () => {
+    mockGetCaseDataFromDraftStore.mockImplementation(async () => {
+      return claim;
+    });
+    const spyGetCaseDataFromStore = jest.spyOn(draftStoreService, 'getCaseDataFromStore');
+
+    await debtPaymentEvidenceService.saveDebtPaymentEvidence('id', {evidence: debtPaymentOptions.UPLOAD_EVIDENCE});
+    expect(spyGetCaseDataFromStore).toBeCalled();
+  });
+});
