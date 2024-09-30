4639e53891915e02427f843b9ff32b3fd55ffb8f
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-09-30 14:24:38 +0100

    fix unit test

--- src/test/unit/routes/features/claim/generalApplication/certOfSorC/askProofOfDebtPaymentGuidanceController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/certOfSorC/askProofOfDebtPaymentGuidanceController.test.ts
@@ -47,8 +47,8 @@ describe('General Application - ask proof of debt payment guidance', () => {
 
           const decodedText = res.text.replace(/&#39;/g, "'");
 
-          expect(decodedText).toContain(t('PAGES.GENERAL_APPLICATION.ASK_FOR_PROOF_OF_DEBT_PAYMENT.PAGE_TITLE'));
-          expect(decodedText).toContain(t('PAGES.GENERAL_APPLICATION.ASK_FOR_PROOF_OF_DEBT_PAYMENT.TITLE'));
+          expect(decodedText).toContain(t('COMMON.ASK_FOR_PROOF_OF_DEBT_PAYMENT'));
+          expect(decodedText).toContain(t('COMMON.ASK_FOR_PROOF_OF_DEBT_PAYMENT'));
           expect(decodedText).toContain(t('PAGES.GENERAL_APPLICATION.ASK_FOR_PROOF_OF_DEBT_PAYMENT.PARA_1'));
           expect(decodedText).toContain(t('PAGES.GENERAL_APPLICATION.ASK_FOR_PROOF_OF_DEBT_PAYMENT.PARA_TITLE_1'));
           expect(decodedText).toContain(t('PAGES.GENERAL_APPLICATION.ASK_FOR_PROOF_OF_DEBT_PAYMENT.PARA_3'));
--- src/test/unit/routes/features/claim/generalApplication/certOfSorC/debtPaymentEvidenceController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/certOfSorC/debtPaymentEvidenceController.test.ts
@@ -2,22 +2,24 @@ import {app} from '../../../../../../../main/app';
 import nock from 'nock';
 import config from 'config';
 import request from 'supertest';
-import {GA_CHECK_YOUR_ANSWERS_COSC_URL, GA_DEBT_PAYMENT_EVIDENCE_URL, GA_UPLOAD_DOCUMENT_COSC_URL} from 'routes/urls';
+import {
+  GA_CHECK_YOUR_ANSWERS_COSC_URL,
+  GA_DEBT_PAYMENT_EVIDENCE_COSC_URL, GA_UPLOAD_DOCUMENTS_COSC_URL,
+
+} from 'routes/urls';
 import {Claim} from 'models/claim';
-import {isGaForLipsEnabled} from '../../../../../../../main/app/auth/launchdarkly/launchDarklyClient';
-import * as utilityService from 'modules/utilityService';
 import {t} from 'i18next';
 import {debtPaymentOptions} from 'models/generalApplication/debtPaymentOptions';
-import {debtPaymentEvidenceService} from 'services/features/generalApplication/certOfSorC/debtPaymentEvidenceService';
 import {TestMessages} from '../../../../../../utils/errorMessageTestConstants';
+import {
+  getCertificateOfSatisfactionOrCanceled,
+} from 'services/features/generalApplication/certOfSorC/certificateOfSatisfactionOrCanceledService';
+import * as launchDarkly from '../../../../../../../main/app/auth/launchdarkly/launchDarklyClient';
 
 jest.mock('modules/oidc');
 jest.mock('modules/draft-store/draftStoreService');
-jest.mock('../../../../../../../main/app/auth/launchdarkly/launchDarklyClient');
-jest.mock('modules/utilityService');
-
-const mockGetClaimById = utilityService.getClaimById as jest.Mock;
-const mockIsGaForLipsEnabled = isGaForLipsEnabled as jest.Mock;
+jest.mock('services/features/generalApplication/certOfSorC/certificateOfSatisfactionOrCanceledService');
+const mockGetCertificateOfSatisfactionOrCanceled = getCertificateOfSatisfactionOrCanceled as jest.Mock;
 
 describe('General Application - CoSorC - debt payment evidence controller', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
@@ -28,19 +30,20 @@ describe('General Application - CoSorC - debt payment evidence controller', () =
     nock(idamServiceUrl)
       .post('/o/token')
       .reply(200, {id_token: citizenRoleToken});
-    mockIsGaForLipsEnabled.mockResolvedValue(true);
+    jest.spyOn(launchDarkly, 'isCUIReleaseTwoEnabled').mockResolvedValueOnce(true);
+    jest.spyOn(launchDarkly, 'isGaForLipsEnabled').mockResolvedValue(true);
 
   });
 
   beforeEach(() => {
     claim.id = 'id';
-    mockGetClaimById.mockReturnValue(claim);
+    mockGetCertificateOfSatisfactionOrCanceled.mockReturnValue(claim);
   });
 
   describe('on GET', () => {
     it('should return page', async () => {
       await request(app)
-        .get(GA_DEBT_PAYMENT_EVIDENCE_URL.replace(':id', claim.id))
+        .get(GA_DEBT_PAYMENT_EVIDENCE_COSC_URL.replace(':id', claim.id))
         .expect((res) => {
           expect(res.status).toBe(200);
         });
@@ -50,7 +53,7 @@ describe('General Application - CoSorC - debt payment evidence controller', () =
   describe('on POST', () => {
     it('should return error on page for no evidence chosen', async () => {
       await request(app)
-        .post(GA_DEBT_PAYMENT_EVIDENCE_URL.replace(':id', claim.id))
+        .post(GA_DEBT_PAYMENT_EVIDENCE_COSC_URL.replace(':id', claim.id))
         .send({evidence: null})
         .expect((res) => {
           expect(res.status).toBe(200);
@@ -60,7 +63,7 @@ describe('General Application - CoSorC - debt payment evidence controller', () =
 
     it('should return error on page for no details written', async () => {
       await request(app)
-        .post(GA_DEBT_PAYMENT_EVIDENCE_URL.replace(':id', claim.id))
+        .post(GA_DEBT_PAYMENT_EVIDENCE_COSC_URL.replace(':id', claim.id))
         .send({evidence: debtPaymentOptions.NO_EVIDENCE})
         .expect((res) => {
           expect(res.status).toBe(200);
@@ -69,22 +72,20 @@ describe('General Application - CoSorC - debt payment evidence controller', () =
     });
 
     it('should return upload docs page', async () => {
-      jest.spyOn(debtPaymentEvidenceService,'saveDebtPaymentEvidence').mockReturnValue(Promise.resolve(null));
 
       await request(app)
-        .post(GA_DEBT_PAYMENT_EVIDENCE_URL.replace(':id', claim.id))
+        .post(GA_DEBT_PAYMENT_EVIDENCE_COSC_URL.replace(':id', claim.id))
         .send({evidence: debtPaymentOptions.UPLOAD_EVIDENCE})
         .expect((res) => {
           expect(res.status).toBe(302);
-          expect(res.header.location).toEqual(GA_UPLOAD_DOCUMENT_COSC_URL.replace(':id', claim.id));
+          expect(res.header.location).toEqual(GA_UPLOAD_DOCUMENTS_COSC_URL.replace(':id', claim.id));
         });
     });
 
     it('should return check your answers page', async () => {
-      jest.spyOn(debtPaymentEvidenceService,'saveDebtPaymentEvidence').mockReturnValue(Promise.resolve(null));
 
       await request(app)
-        .post(GA_DEBT_PAYMENT_EVIDENCE_URL.replace(':id', claim.id))
+        .post(GA_DEBT_PAYMENT_EVIDENCE_COSC_URL.replace(':id', claim.id))
         .send({evidence: debtPaymentOptions.NO_EVIDENCE, provideDetails: 'some evidence'})
         .expect((res) => {
           expect(res.status).toBe(302);
