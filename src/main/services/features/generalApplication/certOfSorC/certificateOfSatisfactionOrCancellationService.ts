e238471ee7b58560816f03228e7cc49561d39a47
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-10-08 19:08:20 +0100

    testing issue fixed

--- src/main/services/features/generalApplication/certOfSorC/certificateOfSatisfactionOrCancellationService.ts
+++ src/main/services/features/generalApplication/certOfSorC/certificateOfSatisfactionOrCancellationService.ts
@@ -4,9 +4,8 @@ import {Request} from 'express';
 import {getClaimById} from 'modules/utilityService';
 import {AppRequest} from 'models/AppRequest';
 import {GeneralApplication} from 'models/generalApplication/GeneralApplication';
-import {Claim} from 'models/claim';
 const {Logger} = require('@hmcts/nodejs-logging');
-const logger = Logger.getLogger('defendantFinalPaymentDateService');
+const logger = Logger.getLogger('certificateOfSatisfactionOrCancellationService');
 
 export const getCertificateOfSatisfactionOrCancellation = async (req: Request): Promise<CertificateOfSatisfactionOrCancellation> => {
   try {
@@ -31,22 +30,12 @@ export const saveCertificateOfSatisfactionOrCancellation = async (req: Request, 
     if (!claim.generalApplication.certificateOfSatisfactionOrCancellation) {
       claim.generalApplication.certificateOfSatisfactionOrCancellation = new CertificateOfSatisfactionOrCancellation();
     }
-    const resetClaim = resetPaymentEvidenceData(claim,propertyName);
-    resetClaim.generalApplication.certificateOfSatisfactionOrCancellation[propertyName] = value;
+    claim.generalApplication.certificateOfSatisfactionOrCancellation[propertyName] = value;
 
-    await saveDraftClaim(redisKey, resetClaim);
+    await saveDraftClaim(redisKey, claim);
   } catch (error) {
     logger.error(error);
     throw error;
   }
 };
 
-function resetPaymentEvidenceData(claim: Claim, propertyName: keyof CertificateOfSatisfactionOrCancellation) {
-  if (propertyName === 'debtPaymentEvidence') {
-    delete claim.generalApplication.certificateOfSatisfactionOrCancellation?.debtPaymentEvidence;
-    if(claim.generalApplication.uploadEvidenceForApplication) {
-      claim.generalApplication.uploadEvidenceForApplication = [];
-    }
-  }
-  return claim;
-}
