360259cccacbf6d12c8c0b96227c4d42a0a017d3
sankaviv1 <sankavi.mohanraj@version1.com>
2024-10-11 10:29:42 +0100

    CIV-14462 Accessibility statement carm

--- src/main/routes/features/public/accessibilityStatementController.ts
+++ src/main/routes/features/public/accessibilityStatementController.ts
@@ -1,13 +1,18 @@
 import {Router} from 'express';
 import {ACCESSIBILITY_STATEMENT_URL, DASHBOARD_URL} from '../../../routes/urls';
+import {isCARMEnabled} from '../../../app/auth/launchdarkly/launchDarklyClient';
 
 const accessibilityStatementController = Router();
 const accessibilityStatementViewPath = 'features/public/accessibility-statement';
 
-accessibilityStatementController.get(ACCESSIBILITY_STATEMENT_URL, (req, res) => {
+accessibilityStatementController.get(ACCESSIBILITY_STATEMENT_URL, async (req, res) => {
+  const carmEnabled = await isCARMEnabled();
   res.render(
     accessibilityStatementViewPath,
-    { dashboardUrl: DASHBOARD_URL },
+    {
+      dashboardUrl: DASHBOARD_URL,
+      carmEnabled: carmEnabled
+    },
   );
 });
 
