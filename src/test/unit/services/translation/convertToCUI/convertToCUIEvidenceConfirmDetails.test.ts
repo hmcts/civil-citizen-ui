526853e04d900513939c6a1b8a4cfd7c391ce726
sankaviv1 <sankavi.mohanraj@version1.com>
2024-10-15 12:05:02 +0100

    add test

--- src/test/unit/services/translation/convertToCUI/convertToCUIEvidenceConfirmDetails.test.ts
+++ src/test/unit/services/translation/convertToCUI/convertToCUIEvidenceConfirmDetails.test.ts
@@ -3,6 +3,24 @@ import {CCDClaim} from 'models/civilClaimResponse';
 import {ConfirmYourDetailsEvidence} from 'form/models/confirmYourDetailsEvidence';
 
 describe('translate CCD data to CUI DQ Evidence confirm details', () => {
+  it('should return undefined when no lip response', () => {
+    // Given
+    const ccdClaim: CCDClaim = {};
+
+    const expected: ConfirmYourDetailsEvidence = {
+      firstName: undefined,
+      lastName: undefined,
+      phoneNumber: undefined,
+      emailAddress: undefined,
+      jobTitle: undefined,
+    };
+
+    // When
+    const result = convertToCUIEvidenceConfirmDetails(ccdClaim);
+    // Then
+    expect(result).toEqual(expected);
+  });
+
   it('should return undefined', () => {
     // Given
     const ccdClaim: CCDClaim = {
