528f2f348ed1921bdcc87db8ec5e870f55278716
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-10-23 09:11:53 +0100

    add first-contact

--- src/main/modules/csrf/index.ts
+++ src/main/modules/csrf/index.ts
@@ -5,7 +5,7 @@ import { isTestingSupportDraftUrl } from 'modules/oidc';
 export class CSRFToken {
   public enableFor(app: Application): void {
     app.use((req, res, next) => {
-      if (req.path.startsWith('/eligibility') || isTestingSupportDraftUrl(req.originalUrl)) {
+      if (req.path.startsWith('/eligibility') || req.path.startsWith('/first-contact') || isTestingSupportDraftUrl(req.originalUrl)) {
         return next();
       }
       return csurf()(req, res, next);
