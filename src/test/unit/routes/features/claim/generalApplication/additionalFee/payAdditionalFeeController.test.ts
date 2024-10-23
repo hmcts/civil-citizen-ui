08c7ad6800603d1c1bf56f74ca74d093acf3af05
GorilaEspacial <woodworkerdeveloper@gmail.com>
2024-10-23 16:14:03 +0200

    Fix: tests

--- src/test/unit/routes/features/claim/generalApplication/additionalFee/payAdditionalFeeController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/additionalFee/payAdditionalFeeController.test.ts
@@ -105,7 +105,7 @@ describe('General Application - Pay additional fee Page', () => {
         .send({option: null})
         .expect((res) => {
           expect(res.status).toBe(200);
-          expect(res.text).toContain(t('ERRORS.VALID_YES_NO_SELECTION_UPPER'));
+          expect(res.text).toContain(t('ERRORS.GENERAL_APPLICATION.APPLY_HELP_WITH_FEES'));
         });
     });
 
