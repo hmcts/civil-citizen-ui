9678d83aa96f25bcd83bcfca0d5acb7448607a04
kannan-v-hmcts <148557022+kannan-v-hmcts@users.noreply.github.com>
2024-10-23 16:37:49 +0100

    Update SystemDocumentInfoExtractor.ts

--- src/main/services/features/caseProgression/SystemDocumentInfoExtractor.ts
+++ src/main/services/features/caseProgression/SystemDocumentInfoExtractor.ts
@@ -4,7 +4,10 @@ import {DocumentType} from 'models/document/documentType';
 
 export class CaseDocumentInfoExtractor {
   static getSystemGeneratedCaseDocumentIdByType = ((systemGeneratedCaseDocuments: SystemGeneratedCaseDocuments[], documentType: DocumentType, defendantOrClaimant?: string) => {
-    const sealedDocument = systemGeneratedCaseDocuments.find((document) => {
+   const sortedDocuments = systemGeneratedCaseDocuments.sort((a, b) => {
+      return new Date(b.value.createdDatetime).getTime() - new Date(a.value.createdDatetime).getTime();
+    });
+    const sealedDocument = sortedDocuments.find((document) => {
       if (documentType == DocumentType.DIRECTIONS_QUESTIONNAIRE) {
         return document.value.documentType === documentType && document.value.documentName.startsWith(defendantOrClaimant);
       }
