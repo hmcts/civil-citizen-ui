8f88042628e076fd1ab14b218113e6be1e24a180
marianadpereira <71711509+marianadpereira@users.noreply.github.com>
2024-10-28 09:10:52 +0000

    Update run-crossbrowser-tests.sh

--- src/test/crossbrowser/run-crossbrowser-tests.sh
+++ src/test/crossbrowser/run-crossbrowser-tests.sh
@@ -6,10 +6,10 @@ export E2E_FRONTEND_URL=${TEST_URL}
 export IGNORE_SESSION_VALIDATION=true
 
 EXIT_STATUS=0
-BROWSER_GROUP=microsoftIE11 yarn test-crossbrowser-fullfunctional || EXIT_STATUS=$?
-BROWSER_GROUP=microsoftEdge yarn test-crossbrowser-fullfunctional || EXIT_STATUS=$?
-BROWSER_GROUP=chrome yarn test-crossbrowser-fullfunctional || EXIT_STATUS=$?
-BROWSER_GROUP=firefox yarn test-crossbrowser-fullfunctional || EXIT_STATUS=$?
-BROWSER_GROUP=safari yarn test-crossbrowser-fullfunctional || EXIT_STATUS=$?
+BROWSER_GROUP=microsoftIE11 yarn test:crossbrowser:fullfunctional || EXIT_STATUS=$?
+BROWSER_GROUP=microsoftEdge yarn test:crossbrowser:fullfunctional || EXIT_STATUS=$?
+BROWSER_GROUP=chrome yarn test:crossbrowser:fullfunctional || EXIT_STATUS=$?
+BROWSER_GROUP=firefox yarn test:crossbrowser:fullfunctional || EXIT_STATUS=$?
+BROWSER_GROUP=safari yarn test:crossbrowser:fullfunctional || EXIT_STATUS=$?
 echo EXIT_STATUS: $EXIT_STATUS
 exit $EXIT_STATUS
