29d947cad9fa8886d3ddd57a0bdf0fac4c66b081
marianadpereira <71711509+marianadpereira@users.noreply.github.com>
2024-10-29 12:27:52 +0000

    Update SystemDocumentInfoExtractor.ts

--- src/main/services/features/caseProgression/SystemDocumentInfoExtractor.ts
+++ src/main/services/features/caseProgression/SystemDocumentInfoExtractor.ts
@@ -2,15 +2,12 @@ import {documentIdExtractor} from 'common/utils/stringUtils';
 import {SystemGeneratedCaseDocuments} from 'models/document/systemGeneratedCaseDocuments';
 import {DocumentType} from 'models/document/documentType';
 
-export class CaseDocumentInfoExtractor {
-  static getSystemGeneratedCaseDocumentIdByType = ((systemGeneratedCaseDocuments: SystemGeneratedCaseDocuments[], documentType: DocumentType, defendantOrClaimant?: string) => {
-    const sortedDocuments = systemGeneratedCaseDocuments.sort((a, b) => {
-      return a.value.createdDatetime.getTime() - b.value.createdDatetime.getTime();
-    });
-    const sealedDocument = sortedDocuments.find((document) => {
-      if (documentType == DocumentType.DIRECTIONS_QUESTIONNAIRE) {
-        return document.value.documentType === documentType && document.value.documentName.startsWith(defendantOrClaimant);
-      }
+ export class CaseDocumentInfoExtractor {
+   static getSystemGeneratedCaseDocumentIdByType = ((systemGeneratedCaseDocuments: SystemGeneratedCaseDocuments[], documentType: DocumentType, defendantOrClaimant?: string) => {
+     const sealedDocument = systemGeneratedCaseDocuments.find((document) => {
+       if (documentType == DocumentType.DIRECTIONS_QUESTIONNAIRE) {
+         return document.value.documentType === documentType && document.value.documentName.startsWith(defendantOrClaimant);
+       }
       return document.value.documentType === documentType;
     });
     return sealedDocument ? documentIdExtractor(sealedDocument.value?.documentLink?.document_binary_url) : null;
