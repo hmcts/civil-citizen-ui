2776f3a10df7c96aa134588f28a409651213f401
sankaviv1 <sankavi.mohanraj@version1.com>
2024-10-30 08:55:49 +0000

    skip prod test

--- src/test/functionalTests/tests/prod/LRvLip_response_partAdmit_dateToPayOn_tests.js
+++ src/test/functionalTests/tests/prod/LRvLip_response_partAdmit_dateToPayOn_tests.js
@@ -30,7 +30,7 @@ Before(async ({api}) => {
   await CitizenDashboardSteps.VerifyClaimOnDashboard(claimNumber);
 });
 
-Scenario('Response with PartAdmit and Date to PayOn @citizenUI @partAdmit @nightly', async ({api}) => {
+Scenario.skip('Response with PartAdmit and Date to PayOn @citizenUI @partAdmit @nightly', async ({api}) => {
   await ResponseSteps.RespondToClaim(claimRef);
   await ResponseSteps.EnterPersonalDetails(claimRef);
   await ResponseSteps.EnterYourOptionsForDeadline(claimRef, dontWantMoreTime);
