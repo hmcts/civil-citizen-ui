abf2fbd11c38190309a7c747e9544a1007280527
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-11-05 11:28:45 +0000

    add index

--- src/main/common/utils/urlFormatter.ts
+++ src/main/common/utils/urlFormatter.ts
@@ -1,7 +1,15 @@
-import {NotEligibleReason} from '../../common/form/models/eligibility/NotEligibleReason';
+import {NotEligibleReason} from 'form/models/eligibility/NotEligibleReason';
 
-export function constructResponseUrlWithIdParams(id: string, path: string): string{
-  return path.replace(/(:id)/i, id);
+export function constructResponseUrlWithIdParams(id: string, path: string, index?: number | string): string {
+  // Replace :id in the path with the provided id
+  let url = path.replace(/(:id)/i, id);
+
+  // Append ?index=${index} if index is defined
+  if (index !== undefined) {
+    url += `?index=${index}`;
+  }
+
+  return url;
 }
 
 export function constructResponseUrlWithIdAndAppIdParams(id: string, appId: string, path: string): string {
