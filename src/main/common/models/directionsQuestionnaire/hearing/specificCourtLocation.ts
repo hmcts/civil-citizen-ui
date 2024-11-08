c1f2c8bca102d010342597f922aed68ad04577dd
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-11-08 14:13:20 +0000

    remove yes and no and add new paragraph

--- src/main/common/models/directionsQuestionnaire/hearing/specificCourtLocation.ts
+++ src/main/common/models/directionsQuestionnaire/hearing/specificCourtLocation.ts
@@ -1,36 +1,22 @@
-import {IsDefined, IsNotEmpty, ValidateIf} from 'class-validator';
-import {YesNo} from 'common/form/models/yesNo';
+import {IsDefined, IsNotEmpty} from 'class-validator';
 
 export class SpecificCourtLocation {
 
-  @IsDefined({message: 'ERRORS.SPECIFIC_COURT.SELECT_YES_NO'})
-    option: string;
-  @ValidateIf(o =>  o.isSpecificCourtRequired())
   @IsDefined({message: 'ERRORS.SPECIFIC_COURT.SELECT_COURT_LOCATION'})
   @IsNotEmpty({message: 'ERRORS.SPECIFIC_COURT.SELECT_COURT_LOCATION'})
     courtLocation: string;
-  @ValidateIf(o => o.isSpecificCourtRequired())
+
   @IsDefined({message: 'PAGES.SPECIFIC_COURT.REASON'})
   @IsNotEmpty({message: 'PAGES.SPECIFIC_COURT.REASON'})
     reason: string;
 
-  constructor(option?: string, courtLocation?: string, reason?: string) {
-    this.option = option;
-    if(this.isSpecificCourtRequired()){
-      this.courtLocation = courtLocation;
-      this.reason = reason;
-    }else{
-      this.courtLocation = undefined;
-      this.reason = undefined;
-    }
-  }
-
-  isSpecificCourtRequired(){
-    return this.option === YesNo.YES;
+  constructor(courtLocation?: string, reason?: string) {
+    this.courtLocation = courtLocation;
+    this.reason = reason;
   }
 
   public static fromObject(record: Record<string, string>){
-    return new SpecificCourtLocation(record?.option, record?.courtLocation, record?.reason);
+    return new SpecificCourtLocation(record?.courtLocation, record?.reason);
   }
 
 }
