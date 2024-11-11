bc6e40b13df78d429540549af15d86e735b6e59b
LiaoP <pijian.liao@hmcts.net>
2024-11-11 14:14:19 +0000

    CIV-15649 update test

--- src/test/unit/routes/features/claim/generalApplication/response/acceptDefendantOfferController.test.ts
+++ src/test/unit/routes/features/claim/generalApplication/response/acceptDefendantOfferController.test.ts
@@ -15,6 +15,8 @@ import { YesNo } from 'common/form/models/yesNo';
 import { ProposedPaymentPlanOption } from 'common/models/generalApplication/response/acceptDefendantOffer';
 import { decode } from 'punycode';
 import { constructResponseUrlWithIdAndAppIdParams } from 'common/utils/urlFormatter';
+import * as generalApplicationService from 'services/features/generalApplication/generalApplicationService';
+import {ApplicationResponse} from 'models/generalApplication/applicationResponse';
 
 jest.mock('../../../../../../../main/modules/oidc');
 jest.mock('../../../../../../../main/modules/draft-store/draftStoreService');
@@ -29,6 +31,7 @@ describe('General Application - Accept defendant offer', () => {
   const idamUrl: string = config.get('idamUrl');
   const mockDataFromStore = jest.spyOn(draftService, 'getCaseDataFromStore');
   let claim: Claim;
+  let applicationResponse: ApplicationResponse;
   beforeAll(() => {
     nock(idamUrl)
       .post('/o/token')
@@ -44,6 +47,42 @@ describe('General Application - Accept defendant offer', () => {
     mockDataFromStore.mockResolvedValue(claim);
 
     jest.spyOn(gaStoreResponseService, 'getDraftGARespondentResponse').mockResolvedValueOnce(gaResponse);
+    jest.spyOn(generalApplicationService, 'getApplicationFromGAService').mockResolvedValueOnce(applicationResponse);
+
+    applicationResponse = {
+      case_data: {
+        applicationTypes: undefined,
+        generalAppType: undefined,
+        generalAppRespondentAgreement: undefined,
+        generalAppInformOtherParty: undefined,
+        generalAppAskForCosts: undefined,
+        generalAppDetailsOfOrder: undefined,
+        generalAppReasonsOfOrder: undefined,
+        generalAppEvidenceDocument: undefined,
+        gaAddlDoc: undefined,
+        generalAppHearingDetails: undefined,
+        generalAppStatementOfTruth: undefined,
+        generalAppPBADetails: {
+          fee: undefined,
+          paymentDetails: {
+            status: 'SUCCESS',
+            reference: 'REF-123-123',
+          },
+          additionalPaymentDetails: {
+            status: 'SUCCESS',
+            reference: undefined,
+          },
+          serviceRequestReference: undefined,
+        },
+        applicationFeeAmountInPence: undefined,
+        parentClaimantIsApplicant: undefined,
+        judicialDecision: undefined,
+      },
+      created_date: '',
+      id: '',
+      last_modified: '',
+      state: undefined,
+    };
   });
 
   afterEach(
@@ -85,10 +124,10 @@ describe('General Application - Accept defendant offer', () => {
     it('should save the value when No and ACCEPT_INSTALMENTS and redirect', async () => {
       await request(app)
         .post(constructResponseUrlWithIdAndAppIdParams('123', '345', GA_ACCEPT_DEFENDANT_OFFER_URL))
-        .send({ 
-          option: YesNo.NO, 
-          type: ProposedPaymentPlanOption.ACCEPT_INSTALMENTS, 
-          amountPerMonth: 10, 
+        .send({
+          option: YesNo.NO,
+          type: ProposedPaymentPlanOption.ACCEPT_INSTALMENTS,
+          amountPerMonth: 10,
           reasonProposedInstalment: 'test',
         })
         .expect((res) => {
@@ -98,14 +137,14 @@ describe('General Application - Accept defendant offer', () => {
     it('should save the value when No and PROPOSE_BY_SET_DATE and redirect', async () => {
       await request(app)
         .post(constructResponseUrlWithIdAndAppIdParams('123', '345', GA_ACCEPT_DEFENDANT_OFFER_URL))
-        .send({ 
-          option: YesNo.NO, 
-          type: ProposedPaymentPlanOption.PROPOSE_BY_SET_DATE, 
-          day: '1', 
-          month: '1', 
-          year: '2040', 
+        .send({
+          option: YesNo.NO,
+          type: ProposedPaymentPlanOption.PROPOSE_BY_SET_DATE,
+          day: '1',
+          month: '1',
+          year: '2040',
           reasonProposedSetDate: 'test',
-        })        
+        })
         .expect((res) => {
           expect(res.status).toBe(302);
         });
@@ -155,7 +194,7 @@ describe('General Application - Accept defendant offer', () => {
     it('should return http 500 when has error in the post method', async () => {
       jest.spyOn(gaStoreResponseService, 'saveDraftGARespondentResponse').mockImplementation(() => {
         throw new Error(TestMessages.REDIS_FAILURE);
-      });      
+      });
       await request(app)
         .post(GA_ACCEPT_DEFENDANT_OFFER_URL)
         .send({ option: YesNo.YES })
