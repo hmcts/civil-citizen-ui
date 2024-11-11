b45a2a62b422df48cdeeb79af2901640352298db
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-11-11 10:24:50 +0000

    fix unit test

--- src/test/unit/services/translation/claimantResponse/ccdTransaltion.test.ts
+++ src/test/unit/services/translation/claimantResponse/ccdTransaltion.test.ts
@@ -123,7 +123,6 @@ describe('Translate claimant response to ccd version', () => {
     expect(ccdClaim.applicant1DQLanguage.court).toBe('WELSH');
     expect(ccdClaim.applicant1DQLanguage.documents).toBe('ENGLISH');
     expect(ccdClaim.applicant1DQVulnerabilityQuestions.vulnerabilityAdjustmentsRequired).toBe(YesNoUpperCamelCase.NO);
-    expect(ccdClaim.applicant1DQRequestedCourt.requestHearingAtSpecificCourt).toBe(YesNoUpperCamelCase.NO);
     expect(ccdClaim.applicant1DQWitnesses.witnessesToAppear).toBe(YesNoUpperCamelCase.NO);
     expect(ccdClaim.applicant1ClaimExpertSpecRequired).toBe(YesNoUpperCamelCase.YES);
     expect(ccdClaim.applicant1DQExperts.expertRequired).toBe(YesNoUpperCamelCase.YES);
