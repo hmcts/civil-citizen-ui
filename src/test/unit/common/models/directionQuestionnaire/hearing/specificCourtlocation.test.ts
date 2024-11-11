b9b8d027ec9ec9df5aaa90aff3a752e6cd9bb458
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2024-11-11 07:13:40 +0000

    fix unit test

--- src/test/unit/common/models/directionQuestionnaire/hearing/specificCourtlocation.test.ts
+++ src/test/unit/common/models/directionQuestionnaire/hearing/specificCourtlocation.test.ts
@@ -2,13 +2,6 @@ import {SpecificCourtLocation} from 'models/directionsQuestionnaire/hearing/spec
 
 describe('SpecificCourtLocation', ()=>{
 
-  it('should remove court location and reason when court location is not required', ()=>{
-    //When
-    const specificCourtLocation = new SpecificCourtLocation('location', 'reason');
-    //Then
-    expect(specificCourtLocation.reason).toBeUndefined();
-    expect(specificCourtLocation.courtLocation).toBeUndefined();
-  });
   it('should not remove court location and reason when court location is required', ()=>{
     //When
     const specificCourtLocation = new SpecificCourtLocation('location', 'reason');
