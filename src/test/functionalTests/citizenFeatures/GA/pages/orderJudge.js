23619a9072feac2d1e55760c89f0bacd475a491d
Johnny Vineall <vineall@live.co.uk>
2024-11-12 12:13:23 +0000

    adding missing semicolons

--- src/test/functionalTests/citizenFeatures/GA/pages/orderJudge.js
+++ src/test/functionalTests/citizenFeatures/GA/pages/orderJudge.js
@@ -34,7 +34,7 @@ class OrderJudge {
     switch(applicationType) {
       case 'Reconsider an order':
         I.see('A judge will consider your application and make an order');
-        I.see('You should explain which order and which parts of it you\'d like to be reconsidered. You\'ll be able to explain your reasons on the next screen.')
+        I.see('You should explain which order and which parts of it you\'d like to be reconsidered. You\'ll be able to explain your reasons on the next screen.');
         I.see('The information you enter on this page will be seen by the other parties.');
         break;
       case 'More time to do what is required by a court order':
@@ -46,7 +46,7 @@ class OrderJudge {
         break;
       case 'Change a hearing date':
         I.see('A judge will consider your application and make an order. The order that a judge would usually make to change a hearing date is in the box below.');
-        I.see('You\'ll need to add information to the box, like when your hearing is currently arranged for, after what date you\'d like to change it to and any dates to avoid.')
+        I.see('You\'ll need to add information to the box, like when your hearing is currently arranged for, after what date you\'d like to change it to and any dates to avoid.');
         I.see('You can also alter any of the other text if you need to.');
         I.see('You\'ll be able to explain your reasons on the next screen.');
         I.see('The contents of this box will be seen by the other parties.');
