364e3a15015eddbff427bd7002d8c9f891affec0
jeswanth <jeswanth.reddy@hmcts.net>
2024-11-25 16:13:09 +0000

    CIV-15472 added steps for settle  claim

--- src/test/e2eTests/tests/generalApplication/LipvsLipSettleClaim.js
+++ src/test/e2eTests/tests/generalApplication/LipvsLipSettleClaim.js
@@ -1,27 +1,23 @@
 const {toggleFlag} = require('../../commons/toggleFlag');
 const createGAApplication = require('../../genralApplication/createGAApplication');
-const RespondentResponse = require('../../genralApplication/respondentResponse');
-const responseFromCourt = require('../../genralApplication/responseFromCourt');
 const config = require('../../../config');
+const RespondentResponse = require("../../genralApplication/respondentResponse");
+const responseApplicationSummary = require("../../genralApplication/responseApplicationSummary");
 
-Feature('Lip V Lip Strike Out Without Notice @galip').tag('@e2e');
-
-Scenario('Claimant GA Application and respond to response for strike out without notice to notice ', async () => {
+Feature('Lip V Lip settle claim with consent @settleclaim').tag('@e2e');
+Scenario('Claimant GA Application and respond to response for settle claim with consent ', () => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-
     toggleFlag('cuiReleaseTwoEnabled', true);
     toggleFlag('GaForLips', true);
-    const claimID = 1732192997720139;
-    const appId = 1732194111758649;
+    const claimID = 1732290567986310;
+    const appId = 1732292100554027;
     createGAApplication.start(claimID);
-    createGAApplication.selectApplicationType('Other applications', 'Ask the court to strike out all or part of the other parties\' case without a trial');
-    createGAApplication.selectAgreementFromOtherParty('No');
-    createGAApplication.InformOtherParties('No');
-    createGAApplication.applicationCosts(claimID, 'Court to strike out all or part of the other parties\' case without a trial', 'To apply to the court to strike out all or part of the other parties’ case, the application fee is £108');
+    createGAApplication.selectApplicationType('Other applications', 'Ask the court to make an order settling the claim by consent');
+    createGAApplication.selectAgreementFromOtherParty('Yes');
+    createGAApplication.applicationCosts(claimID, 'Court to make an order settling the claim by consent', 'To apply to the court to make an order settling the claim by consent, the application fee is £108');
     createGAApplication.claimCosts(claimID, 'Yes');
-    createGAApplication.orderJudge(claimID, 'strike out the application');
+    createGAApplication.orderJudge(claimID, 'settle the claim');
     createGAApplication.requestingReason(claimID);
-    createGAApplication.addAnotherApp(claimID, 'No');
     createGAApplication.wantToUploadDocs(claimID, 'No');
     createGAApplication.hearingArrangementsInfo(claimID);
     createGAApplication.hearingArrangements(claimID, 'In person at the court');
@@ -33,12 +29,7 @@ Scenario('Claimant GA Application and respond to response for strike out without
     createGAApplication.submitConfirmation(claimID, 108);
     createGAApplication.selectFeeType(claimID);
     createGAApplication.verifyPaymentSuccessfullPage();
-    responseFromCourt.viewApplication(claimID, appId);
-    responseFromCourt.checkResponseFromCourtSection('Direction to make the application with notice', 'Pay the additional fee');
-    responseFromCourt.additionalFeePage(275);
-    responseFromCourt.feeSelectionPage('No');
-    responseFromCourt.verifyPaymentSuccessfullPage();
-    RespondentResponse.respondentAgreement(claimID, appId, 'Respond to an application to court to strike out all or part of the other parties\' case without a trial', 'Yes');
+    RespondentResponse.agreeToOrder('Yes', 'Respond to an application to court to make an order settling the claim by consent', claimID, appId);
     RespondentResponse.wantToUploadDocuments(claimID, appId, 'No');
     RespondentResponse.hearingPreference(claimID, appId);
     RespondentResponse.hearingArrangement(claimID, appId, 'In person at the court');
@@ -47,10 +38,8 @@ Scenario('Claimant GA Application and respond to response for strike out without
     RespondentResponse.hearingSupport(claimID, appId);
     RespondentResponse.submitApplication(claimID, appId);
     RespondentResponse.confirmationPage(claimID, appId);
+    responseApplicationSummary.viewResponseApplicationSummary(claimID, appId, 'Order made');
     toggleFlag('cuiReleaseTwoEnabled', false);
     toggleFlag('GaForLips', false);
   }
-});
-AfterSuite(async () => {
-  await createGAApplication.resetWiremockScenario();
-});
+})
