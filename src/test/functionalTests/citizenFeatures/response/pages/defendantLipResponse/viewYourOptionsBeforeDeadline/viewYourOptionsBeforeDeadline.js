355015891f6ff0f2114d7150304f849d030de012
Matthew Fallon <matthew.fallon@hmcts.net>
2024-11-29 15:41:06 +0000

    renaming more variables

--- src/test/functionalTests/citizenFeatures/response/pages/defendantLipResponse/viewYourOptionsBeforeDeadline/viewYourOptionsBeforeDeadline.js
+++ src/test/functionalTests/citizenFeatures/response/pages/defendantLipResponse/viewYourOptionsBeforeDeadline/viewYourOptionsBeforeDeadline.js
@@ -42,21 +42,21 @@ const content = {
 
 var targetDate = new Date();
 targetDate.setDate(targetDate.getDate() + 40);
-const extendedDay = targetDate.getDate();
-const extendedMonth = targetDate.getMonth() + 1;
-const extendedYear = targetDate.getFullYear();
+const targetDay = targetDate.getDate();
+const targetMonth = targetDate.getMonth() + 1;
+const targetYear = targetDate.getFullYear();
 
-var targetDateFuture = new Date();
-targetDateFuture.setDate(targetDateFuture.getDate() + 60);
-const futureDay = targetDateFuture.getDate();
-const futureMonth = targetDateFuture.getMonth() + 1;
-const futureYear = targetDateFuture.getFullYear();
+var futureDate = new Date();
+futureDate.setDate(futureDate.getDate() + 60);
+const futureDay = futureDate.getDate();
+const futureMonth = futureDate.getMonth() + 1;
+const futureYear = futureDate.getFullYear();
 
-var targetDatePast = new Date();
-targetDatePast.setDate(targetDatePast.getDate() - 1);
-const pastDay = targetDatePast.getDate();
-const pastMonth = targetDatePast.getMonth() + 1;
-const pastYear = targetDatePast.getFullYear();
+var pastDate = new Date();
+pastDate.setDate(pastDate.getDate() - 1);
+const pastDay = pastDate.getDate();
+const pastMonth = pastDate.getMonth() + 1;
+const pastYear = pastDate.getFullYear();
 
 class ViewYourOptionsBeforeDeadline {
 
@@ -79,9 +79,9 @@ class ViewYourOptionsBeforeDeadline {
         await I.click('Save and continue');
         await I.see('You have already agreed to more time to respond','h1');
         await I.see('Enter the respond date you have agreed with the other party or their legal representative');
-        await I.fillField(fields.day, extendedDay);
-        await I.fillField(fields.month, extendedMonth);
-        await I.fillField(fields.year, extendedYear);
+        await I.fillField(fields.day, targetDay);
+        await I.fillField(fields.month, targetMonth);
+        await I.fillField(fields.year, targetYear);
         await I.click('Save and continue');
         await I.see('New response deadline','h1');
         await I.click('Continue');
