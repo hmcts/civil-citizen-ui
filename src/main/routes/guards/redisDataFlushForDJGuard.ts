f08483a8215db93d8d8438077fe62a96d3781083
jeswanth <jeswanth.reddy@hmcts.net>
2024-12-06 13:52:24 +0000

    CIV-16227 refresh data in redis For DJ

---
+++ src/main/routes/guards/redisDataFlushForDJGuard.ts
@@ -0,0 +1,24 @@
+import {AppRequest} from 'models/AppRequest';
+import {NextFunction, Response} from 'express';
+import {
+  deleteDraftClaimFromStore,
+  generateRedisKey,
+  getCaseDataFromStore,
+  saveDraftClaim
+} from 'modules/draft-store/draftStoreService';
+import {getClaimById} from 'modules/utilityService';
+
+export const redisDataFlushForDJ = async (req: AppRequest, res: Response, next: NextFunction) => {
+  try {
+    let claim = await getCaseDataFromStore(generateRedisKey(req));
+    if (claim.refreshDataForDJ) {
+      await deleteDraftClaimFromStore(generateRedisKey(req));
+      claim = await getClaimById(req.params.id, req, true);
+      claim.refreshDataForDJ = false;
+      await saveDraftClaim(generateRedisKey(req), claim);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
