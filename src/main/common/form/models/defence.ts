6fbc07ba5ecf231309bb9afbaf05abc8e8f7c9ed
rishisharma <rishi.sharma@hmcts.net>
2024-12-20 11:31:53 +0000

    DTSCCI-1369 defence summary char limit changed 400 to 800

--- src/main/common/form/models/defence.ts
+++ src/main/common/form/models/defence.ts
@@ -3,7 +3,7 @@ import {IsDefined, IsNotEmpty, MaxLength} from 'class-validator';
 export class Defence {
   @IsDefined({message: 'ERRORS.DEFENCE_REQUIRED'})
   @IsNotEmpty({message: 'ERRORS.DEFENCE_REQUIRED'})
-  @MaxLength(400, {message: 'ERRORS.TEXT400_TOO_LONG'})
+  @MaxLength(800, {message: 'ERRORS.TEXT800_TOO_LONG'})
     text?: string;
 
   constructor(text?: string) {
