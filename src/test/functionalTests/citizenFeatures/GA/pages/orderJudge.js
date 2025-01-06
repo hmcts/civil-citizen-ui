e541e1c90e81cd1f18dbd9724b53a7bdc2aec7bc
Johnny Vineall <johnny.vineall@HMCTS.NET>
2025-01-06 11:47:35 +0000

    CIV-15429 and CIV-15430 (#5022)
    * Re-ordering GAs to align with user journey
    
    * Ask to set aside judgement GA
    
    * checkAndSend page file fix
    
    * Adding new app type to switches
    
    * vary a judgement GA
    
    * Adding with notice/consent logic
    
    * lint fixes
    
    * lint
    
    * With consent working for other app types
    
    * Adding defendant creating functionality for vary judgement GA
    
    * Pulling GA ID from url and returning
    
    * Make a change GA
    
    * Summary judgment GA
    
    * GA strike out order
    
    * pause claim GA
    
    * GA ask court to sanction
    
    * GA settle by consent
    
    * something not on the list GA
    
    * adding some GA notifications
    
    * lint fix
    
    * creating new GA tests file
    
    * adding regression tag
    
    * fixing test name
    
    * moving case creation to before
    
    * removing partadmit tag
    
    * creating claim with wrong user...
    
    * using wrong api
    
    * more fixes
    
    * adding all GA types
    
    * fixes
    
    * more fixes
    
    * splitting defendant tests into new test, and fixing function name
    
    * moving defendant tests to vs LR
    
    * removing old file
    
    * fixing case creation api
    
    * removing tag
    
    * removing unnecessary navigation
    
    * lint fix
    
    * adding context to log messages, and removing extra steps
    
    * moving ga creation file, and creating GA response file
    
    * lint fixes
    
    * fixing test name
    
    * fixing file name
    
    * filename fix
    
    * Adding complete GA response journey
    
    * lint fix
    
    * judge create order API steps creation
    
    * temporarily turning off LR v Lip GA tests
    
    * Disabling some tests in pipeline and adding notification check
    
    * lint fix
    
    * adding defendant notification check
    
    * cya page text fix

--- src/test/functionalTests/citizenFeatures/GA/pages/orderJudge.js
+++ src/test/functionalTests/citizenFeatures/GA/pages/orderJudge.js
@@ -32,17 +32,16 @@ class OrderJudge {
 
   async verifyPageText(applicationType) {
     switch(applicationType) {
+      case 'Set aside (remove) a judgment':
+        I.see('A judge will consider your application to make an order. The order that a judge would usually make to set aside (remove) a judgment is in the box below.');
+        I.see('You can add or alter any of the text if you need to.');
+        I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The contents of this box will be seen by the other parties.');
+        break;
       case 'Reconsider an order':
         I.see('A judge will consider your application and make an order');
         I.see('You should explain which order and which parts of it you\'d like to be reconsidered. You\'ll be able to explain your reasons on the next screen.');
         I.see('The information you enter on this page will be seen by the other parties.');
-        break;
-      case 'More time to do what is required by a court order':
-        I.see('A judge will consider your application and make an order. The order that a judge would usually make to give more time to do what\'s required by a court order is in the box below.');
-        I.see('You\'ll need to add information to the box, like what you\'re asking for more time to do and a new deadline.');
-        I.see('You can also alter any of the other text if you need to.');
-        I.see('You\'ll be able to explain your reasons on the next screen.');
-        I.see('The contents of this box will be seen by the other parties.');
         break;
       case 'Change a hearing date':
         I.see('A judge will consider your application and make an order. The order that a judge would usually make to change a hearing date is in the box below.');
@@ -51,10 +50,68 @@ class OrderJudge {
         I.see('You\'ll be able to explain your reasons on the next screen.');
         I.see('The contents of this box will be seen by the other parties.');
         break;
+      case 'More time to do what is required by a court order':
+        I.see('A judge will consider your application and make an order. The order that a judge would usually make to give more time to do what\'s required by a court order is in the box below.');
+        I.see('You\'ll need to add information to the box, like what you\'re asking for more time to do and a new deadline.');
+        I.see('You can also alter any of the other text if you need to.');
+        I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The contents of this box will be seen by the other parties.');
+        break;
       case 'Relief from a penalty you\'ve been given by the court':
         I.see('A judge will consider your application and make an order');
         I.see('You should explain what penalty you\'re asking for relief from. For example, if you missed a deadline to provide documents to the court, or if the court made an order giving you a penalty.');
         I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The information you enter on this page will be seen by the other parties.');
+        break;
+      case 'Ask to make a change to your claim or defence that you\'ve submitted':
+        I.see('A judge will consider your application and make an order. The order that a judge would usually make to allow you to change your claim or defence is in the box below.');
+        I.see('You can:');
+        I.see('explain which document you want to amend');
+        I.see('upload a new version of this document with any amends underlined. We\'ll ask if you want to upload documents to support your application later on');
+        I.see('You can also alter any of the other text if you need to.');
+        I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The contents of this box will be seen by the other parties.');
+        break;
+      case 'Court to make a summary judgment on a case':
+        I.see('A judge will consider your application to make an order. The order that a judge would usually make for a summary judgment is in the box below.');
+        I.see('You can add or alter any of the text if you need to.');
+        I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The contents of this box will be seen by the other parties.');
+        break;
+      case 'Court to strike out all or part of the other parties\' case without a trial':
+        I.see('A judge will consider your application and make an order.');
+        I.see('You should:');
+        I.see('explain which document you want to be dismissed');
+        I.see('mention the date that the document was submitted');
+        I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The information you enter on this page will be seen by the other parties.');
+        break;
+      case 'Court to pause a claim':
+        I.see('A judge will consider your application and make an order. The order that a judge would usually make to pause a claim is in the box below.');
+        I.see('You\'ll need to add information to the box, like when you\'d like the claim to be paused until.');
+        I.see('You can also alter any of the other text if you need to.');
+        I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The contents of this box will be seen by the other parties.');
+        break;
+      case 'Court to impose a sanction on the other parties unless they do a specific action':
+        I.see('A judge will consider your application and make an order.');
+        I.see('You should:');
+        I.see('explain the action you want the other parties to take and by which date they should do this action');
+        I.see('explain that you\'d like the court to give an appropriate sanction to the other parties');
+        I.see('You\'ll be able to explain your reasons on the next screen.');
+        I.see('The information you enter on this page will be seen by the other parties.');
+        break;
+      case 'Court to make an order settling the claim by consent':
+        I.see('A judge will consider your application and make an order.');
+        I.see('You should:');
+        I.see('explain that you and all the parties involved in the case should be settled');
+        I.see('include the name of all the parties involved');
+        I.see('explain the terms of settlement');
+        I.see('The information you enter on this page will be seen by the other parties.');
+        break;
+      case 'Court to do something that\'s not on this list':
+        I.see('A judge will consider your application and make an order.');
+        I.see('You should explain what order you want the judge to make. You\'ll be able to explain your reasons on the next screen.');
         I.see('The information you enter on this page will be seen by the other parties.');
         break;
     }
