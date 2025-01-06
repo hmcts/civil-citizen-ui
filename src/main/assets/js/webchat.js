0d5b1b9a00f1414da6fb0d4a56a32d25dba757a5
Velican <duzgunce.vd@gmail.com>
2025-01-06 17:28:14 +0000

    implement check for prod to load correct cnbc webchat

--- src/main/assets/js/webchat.js
+++ src/main/assets/js/webchat.js
@@ -1,13 +1,28 @@
-window.__8x8Chat = {
+const isProd = process.env.NODE_ENV === 'production' || false;
+
+const webchatCNBCProd = {
   uuid: 'script_1191773643670e666a982030.90013235',
   tenant: 'aG1jdHN4MTAx',
   channel: 'NBC_CNBC_Web_Chat',
   channelUuid: 'YFOOn2KASGSaEd96HY8-BA',
+  buttonContainerId: '__8x8-chat-button-container-script_1191773643670e666a982030.90013235',
+};
+
+const webchatCNBCNonProd = {
+  uuid: 'script_35780229675c3e23dcdd66.11858382',
+  tenant: 'aG1jdHNzdGFnaW5nMDE',
+  channel: 'CNBC Webchat STAGING',
+  channelUuid: 'QG-Cq97KTguzQz5H7T_n6g',
+  buttonContainerId: '__8x8-chat-button-container-script_35780229675c3e23dcdd66.11858382',
+};
+
+window.__8x8Chat = {
   domain: 'https://vcc-eu4.8x8.com',
   path: '/.',
-  buttonContainerId: '__8x8-chat-button-container-script_1191773643670e666a982030.90013235',
   align: 'right',
+  ...(isProd ? webchatCNBCProd : webchatCNBCNonProd),
 };
+
 (function() {
   var se = document.createElement('script');
   se.type = 'text/javascript';
--- src/main/routes/features/contact/contactCNBCController.ts
+++ src/main/routes/features/contact/contactCNBCController.ts
@@ -5,7 +5,8 @@ const contactCNBCController = Router();
 const contactUsViewPath = 'features/public/contact-cnbc';
 
 contactCNBCController.get(CONTACT_CNBC_URL, (req, res) => {
-  res.render(contactUsViewPath, { pageTitle: 'Civil National Business Centre'});
+  const isProd = process.env.NODE_ENV === 'production' || false;
+  res.render(contactUsViewPath, { pageTitle: 'Civil National Business Centre', isProd });
 });
 
 export default contactCNBCController;
