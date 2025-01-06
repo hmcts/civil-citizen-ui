9a6cf4b148ec5eb5bd027e1ef0569ab842a1ecc0
sankaviv1 <sankavi.mohanraj@version1.com>
2025-01-06 17:47:02 +0000

    CIV-16258 Minti lip confirmation screens

--- src/main/routes/features/claimantResponse/claimantResponseConfirmationController.ts
+++ src/main/routes/features/claimantResponse/claimantResponseConfirmationController.ts
@@ -6,8 +6,9 @@ import {getLng} from 'common/utils/languageToggleUtils';
 import {claimantResponseConfirmationGuard} from 'routes/guards/claimantResponseConfirmationGuard';
 import {AppRequest} from 'common/models/AppRequest';
 import {deleteDraftClaimFromStore, generateRedisKey} from 'modules/draft-store/draftStoreService';
-import {isCarmEnabledForCase} from '../../../app/auth/launchdarkly/launchDarklyClient';
+import {isCarmEnabledForCase, isMintiEnabledForCase} from '../../../app/auth/launchdarkly/launchDarklyClient';
 import {getRespondToSettlementAgreementDeadline} from 'services/features/claimantResponse/signSettlmentAgreementService';
+import {isIntermediateTrack, isMultiTrack} from 'form/models/claimType';
 
 const claimantResponseConfirmationController = Router();
 
@@ -17,8 +18,10 @@ claimantResponseConfirmationController.get(CLAIMANT_RESPONSE_CONFIRMATION_URL, c
     await deleteDraftClaimFromStore(generateRedisKey(req));
     const claim = await getClaimById(req.params.id, req, true);
     const carmApplicable = await isCarmEnabledForCase(claim.submittedDate);
+    const mintiEnabled = await isMintiEnabledForCase(claim.submittedDate);
+    const isMultiOrIntermediateTrack = isIntermediateTrack(claim.totalClaimAmount, mintiEnabled) || isMultiTrack(claim.totalClaimAmount, mintiEnabled)
     const respondToSettlementAgreementDeadLine = await getRespondToSettlementAgreementDeadline(req, claim);
-    const claimantResponseConfirmationContent = getClaimantResponseConfirmationContent(claim, getLng(lang), carmApplicable, respondToSettlementAgreementDeadLine);
+    const claimantResponseConfirmationContent = getClaimantResponseConfirmationContent(claim, getLng(lang), carmApplicable, isMultiOrIntermediateTrack, respondToSettlementAgreementDeadLine);
     res.render('features/claimantResponse/claimant-response-confirmation', {
       claimantResponseConfirmationContent,
       pageTitle: 'PAGES.CLAIMANT_RESPONSE_CONFIRMATION.PAGE_TITLE',
