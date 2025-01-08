8f62bdc9646de8a786ee3599bebf9413664e96d4
Velican <duzgunce.vd@gmail.com>
2025-01-08 17:32:00 +0000

    clean up

--- src/main/routes/features/contact/contactCNBCController.ts
+++ src/main/routes/features/contact/contactCNBCController.ts
@@ -5,8 +5,16 @@ import config from 'config';
 const contactCNBCController = Router();
 const contactUsViewPath = 'features/public/contact-cnbc';
 
+type WebChat = {
+  uuid: string;
+  tenant: string;
+  channel: string;
+  channelUuid: string;
+  buttonContainerId: string;
+};
+
 contactCNBCController.get(CONTACT_CNBC_URL, (req, res) => {
-  const webChat = config.get('webChat.cnbc');
+  const webChat = config.get<WebChat>('webChat.cnbc');
   res.render(contactUsViewPath, { pageTitle: 'Civil National Business Centre', webChat });
 });
 
