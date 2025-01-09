ede43f6625ff91344f766de3a44efe3d97bd9aa9
Velican <duzgunce.vd@gmail.com>
2025-01-09 18:02:12 +0000

    clean up unit test

--- src/test/unit/routes/features/contact/contactMediationController.test.ts
+++ src/test/unit/routes/features/contact/contactMediationController.test.ts
@@ -1,21 +1,8 @@
 import request from 'supertest';
 import { app } from '../../../../../main/app';
 import { CONTACT_MEDIATION_URL } from 'routes/urls';
-import config from 'config';
-import nock from 'nock';
-
-jest.mock('../../../../../main/modules/oidc');
 
 describe('Contact Mediation page', () => {
-  const citizenRoleToken: string = config.get('citizenRoleToken');
-  const idamUrl: string = config.get('idamUrl');
-
-  beforeAll(() => {
-    nock(idamUrl)
-      .post('/o/token')
-      .reply(200, { id_token: citizenRoleToken });
-  });
-
   describe('on GET', () => {
     it('should display Contact Mediation page', async () => {
       const res = await request(app).get(CONTACT_MEDIATION_URL);
