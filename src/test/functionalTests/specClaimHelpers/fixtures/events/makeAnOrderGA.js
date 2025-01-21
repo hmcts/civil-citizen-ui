b26e6532f067fff574436b9d17eff4f604b73260
Raja <raja.mani@hmcts.net>
2025-01-21 10:55:19 +0000

    added tests

--- src/test/functionalTests/specClaimHelpers/fixtures/events/makeAnOrderGA.js
+++ src/test/functionalTests/specClaimHelpers/fixtures/events/makeAnOrderGA.js
@@ -43,4 +43,92 @@ module.exports = {
       },
     };
   },
+  dismissAnOrderGA: (document) => {
+    return {
+      event: 'MAKE_AN_ORDER',
+      caseDataUpdate: {
+        judicialDecision: {
+          decision: 'MAKE_AN_ORDER',
+        },
+        judgeTitle: 'judgeb birmin',
+        bilingualHint: 'No',
+        judicialDecisionMakeOrder: {
+          makeAnOrder: 'DISMISS_THE_APPLICATION',
+          judgeRecitalText: 'The Judge considered the application of the claimant dated 5 December 2024',
+          orderText: 'Test order',
+          judgeApproveEditOptionDoc: null,
+          judgeApproveEditOptionDate: null,
+          judgeApproveEditOptionDateForUnlessOrder: null,
+          dismissalOrderText: 'This application is dismissed.',
+          directionsText: null,
+          directionsResponseByDate: null,
+          judicialByCourtsInitiative: 'OPTION_1',
+          orderCourtOwnInitiative: 'As this order was made on the court\'s own initiative, any party affected by the order may apply to set aside, vary, or stay the order. Any such application must be made by 4pm on',
+          orderCourtOwnInitiativeDate: '2024-12-12',
+          orderWithoutNotice: 'If you were not notified of the application before this order was made, you may apply to set aside, vary, or stay the order. Any such application must be made by 4pm on',
+          orderWithoutNoticeDate: '2024-12-12',
+          showReasonForDecision: 'No',
+          reasonForDecisionText: null,
+          displayjudgeApproveEditOptionDate: 'No',
+          displayjudgeApproveEditOptionDoc: 'Yes',
+          isOrderProcessedByStayScheduler: 'No',
+          isOrderProcessedByUnlessScheduler: 'No',
+          displayjudgeApproveEditOptionDateForUnlessOrder: 'No',
+          showJudgeRecitalText: [
+            'SHOW',
+          ],
+        },
+        judicialMakeOrderDocPreview: {
+          document_url: document.document_url,
+          document_binary_url: document.document_binary_url,
+          document_filename: document.document_filename,
+          document_hash: document.document_hash,
+        },
+      },
+    };
+  },
+  giveDirections: (document) => {
+    return {
+      event: 'MAKE_AN_ORDER',
+      caseDataUpdate: {
+        judicialDecision: {
+          decision: 'MAKE_AN_ORDER',
+        },
+        judgeTitle: 'judgeb birmin',
+        bilingualHint: 'No',
+        judicialDecisionMakeOrder: {
+          makeAnOrder: 'GIVE_DIRECTIONS_WITHOUT_HEARING',
+          judgeRecitalText: 'The Judge considered the application of the claimant dated 5 December 2024',
+          orderText: 'Test order',
+          judgeApproveEditOptionDoc: null,
+          judgeApproveEditOptionDate: null,
+          judgeApproveEditOptionDateForUnlessOrder: null,
+          dismissalOrderText: 'This application is dismissed.',
+          directionsText: null,
+          directionsResponseByDate: null,
+          judicialByCourtsInitiative: 'OPTION_1',
+          orderCourtOwnInitiative: 'As this order was made on the court\'s own initiative, any party affected by the order may apply to set aside, vary, or stay the order. Any such application must be made by 4pm on',
+          orderCourtOwnInitiativeDate: '2024-12-12',
+          orderWithoutNotice: 'If you were not notified of the application before this order was made, you may apply to set aside, vary, or stay the order. Any such application must be made by 4pm on',
+          orderWithoutNoticeDate: '2024-12-12',
+          showReasonForDecision: 'No',
+          reasonForDecisionText: null,
+          displayjudgeApproveEditOptionDate: 'No',
+          displayjudgeApproveEditOptionDoc: 'Yes',
+          isOrderProcessedByStayScheduler: 'No',
+          isOrderProcessedByUnlessScheduler: 'No',
+          displayjudgeApproveEditOptionDateForUnlessOrder: 'No',
+          showJudgeRecitalText: [
+            'SHOW',
+          ],
+        },
+        judicialMakeOrderDocPreview: {
+          document_url: document.document_url,
+          document_binary_url: document.document_binary_url,
+          document_filename: document.document_filename,
+          document_hash: document.document_hash,
+        },
+      },
+    };
+  },
 };
--- src/test/functionalTests/tests/nonprod/LiPvLiP_GA_e2e_tests.js
+++ src/test/functionalTests/tests/nonprod/LiPvLiP_GA_e2e_tests.js
@@ -8,7 +8,7 @@ const {isDashboardServiceToggleEnabled} = require('../../specClaimHelpers/api/te
 const {verifyNotificationTitleAndContent} = require('../../specClaimHelpers/e2e/dashboardHelper');
 const {orderMadeGA} = require('../../specClaimHelpers/dashboardNotificationConstants');
 
-let claimRef, claimType, caseData, claimNumber, gaID;
+let claimRef, claimType, caseData, claimNumber, gaID, courtResponseType;
 
 Feature('Lip v Lip GA e2e Tests');
 
@@ -16,7 +16,7 @@ Before(async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
     await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
-    
+
     claimType = 'FastTrack';
     claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
     caseData = await api.retrieveCaseData(config.adminUser, claimRef);
@@ -27,7 +27,8 @@ Before(async ({api}) => {
   }
 });
 
-Scenario('LipvLip Applicant GA creation e2e tests @citizenUI @nightly - @api @ga @regression', async ({I, api}) => {
+Scenario('LipvLip Applicant GA creation e2e tests - Make an Order @citizenUI - @api @ga @regression', async ({I, api}) => {
+  courtResponseType = 'approveOrEdit';
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
 
@@ -41,10 +42,10 @@ Scenario('LipvLip Applicant GA creation e2e tests @citizenUI @nightly - @api @ga
     await LoginSteps.EnterCitizenCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
     await I.amOnPage('/dashboard');
     await I.click(claimNumber);
-    
+
     await respondGASteps.respondToGA(claimRef, gaID, 'Respond to an application to more time to do what is required by a court order', 'Miss Jane Doe v Sir John Doe');
 
-    await api.makeOrderGA(gaID);
+    await api.makeOrderGA(gaID, courtResponseType);
 
     const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
 
@@ -69,5 +70,57 @@ Scenario('LipvLip Applicant GA creation e2e tests @citizenUI @nightly - @api @ga
     }
     //await I.amOnPage(`/case/${gaID}/general-application/summary`);
 
+  }
+});
+
+Scenario('LipvLip Applicant GA creation e2e tests - Dismiss an Order @citizenUI - @api @ga @nightly', async ({I, api}) => {
+  courtResponseType = 'dismissAnOrder';
+  if (['preview', 'demo'].includes(config.runningEnv)) {
+    await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+
+    console.log('Creating an Adjourn Hearing Order GA app as claimant');
+    await I.amOnPage('/dashboard');
+    await I.click(claimNumber);
+    gaID = await createGASteps.askToChangeHearingDateGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'withoutnotice');
+
+    await api.makeOrderGA(gaID, courtResponseType);
+
+    const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+
+    await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await I.amOnPage('/dashboard');
+    await I.click(claimNumber);
+
+    if (isDashboardServiceEnabled) {
+      const notification = orderMadeGA();
+      await verifyNotificationTitleAndContent(claimNumber, notification.title, notification.content);
+      await I.click(notification.nextSteps);
+    }
+  }
+});
+
+Scenario('LipvLip Applicant GA creation e2e tests - Give directions without listing @citizenUI - @api @ga @nightly @raja', async ({I, api}) => {
+  courtResponseType = 'giveDirections';
+  if (['preview', 'demo'].includes(config.runningEnv)) {
+    await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+
+    console.log('Creating an Adjourn Hearing Order GA app as claimant');
+    await I.amOnPage('/dashboard');
+    await I.click(claimNumber);
+    gaID = await createGASteps.askToChangeHearingDateGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'withoutnotice');
+
+    await api.makeOrderGA(gaID, courtResponseType);
+
+    const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+
+    await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await I.amOnPage('/dashboard');
+    await I.click(claimNumber);
+
+    if (isDashboardServiceEnabled) {
+      const notification = orderMadeGA();
+      await verifyNotificationTitleAndContent(claimNumber, notification.title, notification.content);
+      await I.click(notification.nextSteps);
+    }
   }
 });
