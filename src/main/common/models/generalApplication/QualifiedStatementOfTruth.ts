4729b83e6c95a4c2c077319371ee0840c5a09a77
jeswanth <jeswanth.reddy@hmcts.net>
2025-02-12 17:36:26 +0000

    WIP: 82e025d81ee Update: welsh texts (#4273)

--- src/main/common/models/generalApplication/QualifiedStatementOfTruth.ts
+++ src/main/common/models/generalApplication/QualifiedStatementOfTruth.ts
@@ -4,14 +4,14 @@ import {SIGNER_NAME_MAX_LENGTH} from 'form/validators/validationConstraints';
 export class QualifiedStatementOfTruth {
 
   @IsNotEmpty({message: 'ERRORS.STATEMENT_OF_TRUTH_REQUIRED_MESSAGE'})
-  signed?: boolean;
+    signed?: boolean;
 
   @MaxLength(SIGNER_NAME_MAX_LENGTH, {message: 'ERRORS.FULL_NAME_TOO_LONG'})
   @IsNotEmpty({message: 'ERRORS.GENERAL_APPLICATION.QUALIFIED_SOT.ERROR_FULL_NAME'})
-  name?: string;
+    name?: string;
   @MaxLength(SIGNER_NAME_MAX_LENGTH, {message: 'ERRORS.FULL_NAME_TOO_LONG'})
   @IsNotEmpty({message: 'ERRORS.GENERAL_APPLICATION.QUALIFIED_SOT.ERROR_ROLE'})
-  title?: string;
+    title?: string;
 
   constructor(signed?: boolean, name?: string, title?: string) {
     this.signed = signed;
