ffb45abd6703dd0c526ef4c20e4426fe7fa94996
AbuSalam22 <136581242+AbuSalam22@users.noreply.github.com>
2025-02-21 14:26:49 +0000

    DTSCCI-1484

--- src/main/common/utils/taskList/taskListBuilder.ts
+++ src/main/common/utils/taskList/taskListBuilder.ts
@@ -152,7 +152,7 @@ const buildSubmitSection = (caseData: Claim, claimId: string, lang: string): Tas
   const checkAndSubmitYourResponseTask = getCheckAndSubmitYourResponseTask(claimId, lang);
 
   if (isRejectAllAndCounterClaim(caseData)) {
-    checkAndSubmitYourResponseTask.status = TaskStatus.NOT_AVAILABLE_YET;
+    checkAndSubmitYourResponseTask.status = TaskStatus.UNAVAILABLE;
   }
 
   tasks.push(checkAndSubmitYourResponseTask);
