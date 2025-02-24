6f9ca554f05c0b230f33d37ff5a8b4758dabc2cc
Velican <duzgunce.vd@gmail.com>
2025-02-24 20:40:01 +0000

    check if claim has interest before calling interest calculation.

--- src/main/routes/features/claim/payment/claimFeeBreakDownController.ts
+++ src/main/routes/features/claim/payment/claimFeeBreakDownController.ts
@@ -31,7 +31,7 @@ claimFeeBreakDownController.get(CLAIM_FEE_BREAKUP, claimFeePaymentGuard, (async 
     }
     const claimFee = convertToPoundsFilter(claim.claimFee?.calculatedAmountInPence);
     const hasInterest = claim.claimInterest === YesNo.YES;
-    const interestAmount = await calculateInterestToDate(claim);
+    const interestAmount = hasInterest ? await calculateInterestToDate(claim) : 0;
     const totalAmount = hasInterest ? (claim.totalClaimAmount + interestAmount + claimFee) : (claim.totalClaimAmount + claimFee);
     return res.render(viewPath, {
       totalClaimAmount: claim.totalClaimAmount?.toFixed(2),
