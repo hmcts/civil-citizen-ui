18245d518241558b4aa312f640bc0d1a4f767271
Velican <duzgunce.vd@gmail.com>
2025-03-04 11:58:04 +0000

    use scenario skip instead of commenting out.

--- src/test/functionalTests/tests/prod/LipvLip_Api_partAdmit_PayBySetDate_tests.js
+++ src/test/functionalTests/tests/prod/LipvLip_Api_partAdmit_PayBySetDate_tests.js
@@ -13,41 +13,41 @@ let claimRef, claimType, claimNumber;
 Feature('Response with PartAdmit-PayBySetDate - Small Claims & Fast Track ');
 
 // TODO undo this once the stop from choosing settlement agreement is removed
-// Scenario('Response with PartAdmit-PayBySetDate Small claims @citizenUI @partAdmit @nightly - @api', async ({api}) => {
-//   await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-//   await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
-//   claimType = 'SmallClaims';
-//   claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
-//   await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.partAdmitWithPartPaymentOnSpecificDateWithIndividual);
-//   await api.waitForFinishedBusinessProcess();
-//
-//   //Claimant response below here
-//   let caseData = await api.retrieveCaseData(config.adminUser, claimRef);
-//   claimNumber = await caseData.legacyCaseReference;
-//   const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
-//   console.log('isDashboardServiceEnabled..', isDashboardServiceEnabled);
-//   await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-//   await ResponseToDefenceLipVsLipSteps.claimantAcceptForDefRespPartAdmitPayBySetDate(claimRef, '456', claimNumber);
-//   await api.waitForFinishedBusinessProcess();
-// });
+Scenario.skip('Response with PartAdmit-PayBySetDate Small claims @citizenUI @partAdmit @nightly - @api', async ({api}) => {
+  await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+  await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+  claimType = 'SmallClaims';
+  claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
+  await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.partAdmitWithPartPaymentOnSpecificDateWithIndividual);
+  await api.waitForFinishedBusinessProcess();
+
+  //Claimant response below here
+  let caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+  claimNumber = await caseData.legacyCaseReference;
+  const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+  console.log('isDashboardServiceEnabled..', isDashboardServiceEnabled);
+  await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+  await ResponseToDefenceLipVsLipSteps.claimantAcceptForDefRespPartAdmitPayBySetDate(claimRef, '456', claimNumber);
+  await api.waitForFinishedBusinessProcess();
+});
 
 // TODO undo this once the stop from choosing settlement agreement is removed
-// Scenario('Response with PartAdmit-PayBySetDate Fast Track @citizenUI @partAdmit @nightly - @api', async ({api}) => {
-//   await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-//   await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
-//   claimType = 'FastTrack';
-//   claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
-//   await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.partAdmitWithPartPaymentOnSpecificDateWithIndividual);
-//   await api.waitForFinishedBusinessProcess();
-//
-//   //Claimant response below here
-//   let caseData = await api.retrieveCaseData(config.adminUser, claimRef);
-//   claimNumber = await caseData.legacyCaseReference;
-//
-//   await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-//   await ResponseToDefenceLipVsLipSteps.claimantAcceptForDefRespPartAdmitPayBySetDate(claimRef, '3456', claimNumber);
-//   await api.waitForFinishedBusinessProcess();
-// });
+Scenario.skip('Response with PartAdmit-PayBySetDate Fast Track @citizenUI @partAdmit @nightly - @api', async ({api}) => {
+  await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+  await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+  claimType = 'FastTrack';
+  claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
+  await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.partAdmitWithPartPaymentOnSpecificDateWithIndividual);
+  await api.waitForFinishedBusinessProcess();
+
+  //Claimant response below here
+  let caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+  claimNumber = await caseData.legacyCaseReference;
+
+  await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+  await ResponseToDefenceLipVsLipSteps.claimantAcceptForDefRespPartAdmitPayBySetDate(claimRef, '3456', claimNumber);
+  await api.waitForFinishedBusinessProcess();
+});
 
 Scenario('Response with PartAdmit-PayBySetDate Small claims Reject repayment plan Request CCJ @citizenUI @partAdmit @nightly - @api', async ({
   I,
