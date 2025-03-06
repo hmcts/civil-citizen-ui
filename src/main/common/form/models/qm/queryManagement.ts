f8f2698a3c1179005e78fbd4051d1fb82f247321
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-03-06 15:58:11 +0000

    add controller

--- src/main/common/form/models/qm/queryManagement.ts
+++ src/main/common/form/models/qm/queryManagement.ts
@@ -1,4 +1,6 @@
 import {IsDefined} from 'class-validator';
+import {ValidationArgs} from "form/models/genericForm";
+import {GenericYesNoCarmEmailConfirmation} from "form/models/genericYesNoCarmEmailConfirmation";
 
 export class Hint {
   text: string;
@@ -34,7 +36,43 @@ export class WhatDoYouWantToDo {
   }
 }
 
+const withMessage = (buildErrorFn: (messageName: string) => string) => {
+  return (args: ValidationArgs<GenericYesNoCarmEmailConfirmation>): string => {
+    return buildErrorFn(args.object.messageName);
+  };
+};
+
+const generateErrorMessage = (messageName: string): string => {
+  return messageName ? messageName : 'ERRORS.VALID_YES_NO_OPTION_GALLAI_NA_ALLAI';
+};
+
+export class QualifyingQuestion {
+
+  @IsDefined({message: withMessage(generateErrorMessage)})
+    option?: QualifyingQuestionTypeOption;
+  items: radioButtonItems[] ;
+  messageName?: string;
+
+  constructor(option: QualifyingQuestionTypeOption, radioButtonItems?: radioButtonItems[], messageName?: string ) {
+    this.option = option;
+    this.items = radioButtonItems;
+    this.messageName = messageName;
+  }
+}
+
 export enum WhatToDoTypeOption {
+  CHANGE_CASE = 'CHANGE_CASE',
+  GET_UPDATE = 'GET_UPDATE',
+  SEND_UPDATE = 'SEND_UPDATE',
+  SEND_DOCUMENTS = 'SEND_DOCUMENTS',
+  SOLVE_PROBLEM = 'SOLVE_PROBLEM',
+  MANAGE_HEARING = 'MANAGE_HEARING',
+  GET_SUPPORT = 'GET_SUPPORT',
+  FOLLOW_UP = 'FOLLOW_UP',
+  SOMETHING_ELSE = 'SOMETHING_ELSE',
+}
+
+export enum QualifyingQuestionTypeOption {
   CHANGE_CASE = 'CHANGE_CASE',
   GET_UPDATE = 'GET_UPDATE',
   SEND_UPDATE = 'SEND_UPDATE',
@@ -48,5 +86,5 @@ export enum WhatToDoTypeOption {
 
 export class QueryManagement {
   whatDoYouWantToDo: WhatDoYouWantToDo;
-
+  qualifyingQuestion : WhatDoYouWantToDo;
 }
