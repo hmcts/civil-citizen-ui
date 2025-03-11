fca8feaa0b870ec263a8e748e2916fdf46ce0993
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-11 19:44:48 +0000

    update

--- src/test/functionalTests/codecept.conf.js
+++ src/test/functionalTests/codecept.conf.js
@@ -2,15 +2,15 @@
 const testConfig = require('../config.js');
 const {unAssignAllUsers} = require('./specClaimHelpers/api/caseRoleAssignmentHelper');
 const {deleteAllIdamTestUsers} = require('./specClaimHelpers/api/idamHelper');
-const {mkDirTestTimeDir, deleteTestTimesFile} = require('./helpers/test_time_helper.js');
+const {mkDirTestTimeDir, deleteTestTimesFolder, appendTestTimesToReport} = require('./helpers/test_time_helper.js');
 
 //const testHeadlessBrowser = process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true;
 process.env.PLAYWRIGHT_SERVICE_RUN_ID = process.env.PLAYWRIGHT_SERVICE_RUN_ID || new Date().toISOString();
 let startTime;
 exports.config = {
   bootstrapAll: async () => {
-    await deleteTestTimesFile();
-    // await mkDirTestTimeDir()
+    deleteTestTimesFolder();
+    mkDirTestTimeDir();
     startTime = new Date();
     console.log(`Starting the tests at ${startTime}`);
   },
@@ -19,6 +19,7 @@ exports.config = {
     const executionTime = (endTime - startTime) / 1000; // in seconds
     console.log(`Finished the tests at ${endTime}`);
     console.log(`Total execution time: ${executionTime} seconds`);
+    appendTestTimesToReport();
   },
   async teardown() {
     console.log('Current worker has finished running tests so we should clean up the user roles');
--- src/test/functionalTests/helpers/test_time_helper.js
+++ src/test/functionalTests/helpers/test_time_helper.js
@@ -1,5 +1,6 @@
+const testTimeReportFolderPath = 'test-results/functional/test-times/';
 const testTimeReportFilePath = 'test-results/functional/test-times-report.json';
-const fs = require('fs/promises');
+const fs = require('fs');
 const fsSync = require('fs');
 const path = require('path');
 
@@ -14,37 +15,70 @@ module.exports = {
   addTestStartTime: async (testName) => {
     let data;
     try {
-      data = JSON.parse(await fs.readFile(testTimeReportFilePath));
+      data = JSON.parse(await fs.readFile(`${testTimeReportFolderPath}/${testName}.json`));
     } catch(e) {
       data = {};
     }
     data[testName] = {...data[testName], startTime: getTime()};
-    await fs.writeFile(testTimeReportFilePath, JSON.stringify(data, null, 2));
+    await fs.writeFile(`${testTimeReportFolderPath}/${testName}.json`, JSON.stringify(data, null, 2));
   },
 
   addTestEndTime: async (testName) => {
     let data;
     try {
-      data = JSON.parse(await fs.readFile(testTimeReportFilePath));
+      data = JSON.parse(await fs.readFile(`${testTimeReportFolderPath}/${testName}.json`));
     } catch(e) {
       data = {};
     }
     data[testName] = {...data[testName], endTime: getTime()};
-    await fs.writeFile(testTimeReportFilePath, JSON.stringify(data, null, 2));
+    await fs.writeFile(`${testTimeReportFolderPath}/${testName}.json`, JSON.stringify(data, null, 2));
   },
 
   mkDirTestTimeDir: async () => {
-    const dirPath = path.dirname(testTimeReportFilePath);
-    if (!fsSync.existsSync(testTimeReportFilePath)) {
+    const dirPath = path.dirname(testTimeReportFolderPath);
+    if (!fsSync.existsSync(testTimeReportFolderPath)) {
       await fs.readFile(dirPath, { recursive: true });
     }
   },
 
-  deleteTestTimesFile: async () => {
+  deleteTestTimesFolder: async () => {
     try {
-      await fs.unlink(testTimeReportFilePath);
+       fs.unlink(tes);
     } catch(error) {
       console.log(error);
     }
   },
+
+  appendTestTimesToReport: async () => {
+    await fs.readdir(testTimeReportFolderPath, (err, jsonFiles) => {
+      jsonFiles.forEach((file, index) => {
+      const filePath = path.join(directoryPath, file);
+      
+      // Read the content of the JSON file
+      fs.readFile(filePath, 'utf8', (err, data) => {
+
+        // Parse the JSON data
+        try {
+          const jsonData = JSON.parse(data);
+
+          // Merge the current file's data with the overall data
+          mergedData = { ...mergedData, ...jsonData };
+
+          // If it's the last file, save the merged data to the output file
+          if (index === jsonFiles.length - 1) {
+              fs.writeFile(outputFilePath, JSON.stringify(mergedData, null, 2), 'utf8', (err) => {
+              if (err) {
+                console.error('Error writing to output file:', err);
+              } else {
+                console.log('Data successfully merged and saved to', outputFilePath);
+              }
+            });
+          }
+        } catch (parseErr) {
+          console.error(`Error parsing JSON in file ${file}:`, parseErr);
+        }
+      });
+    });
+  });
+  }
 };
