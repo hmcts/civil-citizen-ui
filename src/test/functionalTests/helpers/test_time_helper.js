4f9376abdc9714575a95376a84f9f5fce8719683
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-12 08:31:06 +0000

    update

--- src/test/functionalTests/helpers/test_time_helper.js
+++ src/test/functionalTests/helpers/test_time_helper.js
@@ -1,3 +1,4 @@
+const testTimeReportFolderPath = 'test-results/functional/test-times/';
 const testTimeReportFilePath = 'test-results/functional/test-times-report.json';
 const fs = require('fs/promises');
 const fsSync = require('fs');
@@ -10,41 +11,73 @@ const getTime = () => {
   return `${hours}:${minutes}`;
 };
 
+const hyphenate = (filePath) => {
+  return filePath.replace(/\s+/g, '-');
+};
+
 module.exports = {
   addTestStartTime: async (testName) => {
-    let data;
+    const filePath = `${testTimeReportFolderPath}${hyphenate(testName)}.json`;
+    let data = {};
     try {
-      data = JSON.parse(await fs.readFile(testTimeReportFilePath));
+      data = JSON.parse(await fs.readFile(filePath));
     } catch(e) {
       data = {};
     }
     data[testName] = {...data[testName], startTime: getTime()};
-    await fs.writeFile(testTimeReportFilePath, JSON.stringify(data, null, 2));
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2));
   },
 
   addTestEndTime: async (testName) => {
+    const filePath = `${testTimeReportFolderPath}${hyphenate(testName)}.json`;
     let data;
     try {
-      data = JSON.parse(await fs.readFile(testTimeReportFilePath));
+      data = JSON.parse(await fs.readFile(filePath));
     } catch(e) {
       data = {};
     }
     data[testName] = {...data[testName], endTime: getTime()};
-    await fs.writeFile(testTimeReportFilePath, JSON.stringify(data, null, 2));
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2));
   },
 
   mkDirTestTimeDir: async () => {
-    const dirPath = path.dirname(testTimeReportFilePath);
-    if (!fsSync.existsSync(testTimeReportFilePath)) {
-      await fs.readFile(dirPath, { recursive: true });
+    try {
+      await fs.access(testTimeReportFolderPath);
+    } catch (e) {
+      await fs.mkdir(testTimeReportFolderPath, { recursive: true });
     }
   },
 
-  deleteTestTimesFile: async () => {
+  deleteTestTimes: () => {
     try {
-      await fs.unlink(testTimeReportFilePath);
+      fsSync.rmSync(testTimeReportFolderPath, { recursive: true, force: true });
+      fsSync.unlinkSync(testTimeReportFilePath);
     } catch(error) {
       console.log(error);
+    }
+  },
+
+  appendTestTimesToReport: async () => {
+    try {
+      const files = await fs.readdir(testTimeReportFolderPath);
+      
+      const jsonFiles = files.filter(file => path.extname(file) === '.json');
+      
+      let mergedData = {};
+  
+      for (const file of jsonFiles) {
+        const filePath = path.join(testTimeReportFolderPath, file);
+        
+        const fileData = await fs.readFile(filePath, 'utf-8');
+        const parsedData = JSON.parse(fileData);
+        
+        mergedData = { ...mergedData, ...parsedData };
+      }
+  
+      await fs.writeFile(testTimeReportFilePath, JSON.stringify(mergedData, null, 2));
+      console.log('Data successfully merged and saved to:', testTimeReportFilePath);
+    } catch (error) {
+      console.error('Error processing files:', error);
     }
   },
 };
