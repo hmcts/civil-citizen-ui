1bb92f07f65170b45ac02c4df2542044994cd213
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-03-12 13:45:15 +0000

    DTSCCI-1372 format to two decimal places applicant1SuggestInstalmentsPaymentAmountForDefendantSpec

--- src/main/services/translation/claim/moneyConversation.ts
+++ src/main/services/translation/claim/moneyConversation.ts
@@ -16,7 +16,7 @@ export const convertToPound = (amount: number): number => {
 };
 
 export const formatAmountTwoDecimalPlaces = (amount: number): number => {
-  return amount ? Number(new Intl.NumberFormat('en-UK', { minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: false }).format(amount)) : undefined;
+  return amount ? Number(Number(new Intl.NumberFormat('en-UK', { minimumFractionDigits: 2, maximumFractionDigits: 2, useGrouping: false }).format(amount)).toFixed(2)) : undefined;
 };
 
 export const convertToPoundInStringFormat = (amount: number): string => {
