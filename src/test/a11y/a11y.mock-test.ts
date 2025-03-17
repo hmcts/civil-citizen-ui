5ec4a536b700800e4a13f6701fdc35bae787d396
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-17 08:58:37 +0000

    Run accessibility tests on multiple threads

--- src/test/a11y/a11y.mock-test.ts
+++ src/test/a11y/a11y.mock-test.ts
@@ -5,7 +5,7 @@ import {fail} from 'assert';
 import supertest from 'supertest';
 import {translateUrlToFilePath} from '../utils/mocks/a11y/urlToFileName';
 
-const urlsList = Object.values(urls).filter(url => !IGNORED_URLS.includes(url));
+const urlsList = getChunkAtIndex(Object.values(urls).filter(url => !IGNORED_URLS.includes(url)), parseInt(process.env.A11Y_CHUNKS), parseInt(process.env.A11Y_CHUNKS_INDEX));
 const pa11y = require('pa11y');
 import {retry} from '../functionalTests/specClaimHelpers/api/retryHelper.js';
 
@@ -17,7 +17,7 @@ console.log(networkInterfaces);
 
 // Create a Pa11y test server with Node.js and Express
 const express = require('express');
-const port = 3000;
+const port = 3000 + process.env.A11Y_CHUNKS_INDEX;
 const app = express();
 
 class PallyIssue {
@@ -48,6 +48,7 @@ describe('Accessibility', async () => {
   const options = {
     hideElements: '#logo, .logo, .copyright, link[rel=mask-icon]',
     standard:'WCAG2AA',
+    reportName: '',
     includeWarnings: true,
     log: {
       debug: console.log,
@@ -77,3 +78,18 @@ describe('Accessibility', async () => {
   }
   server.close();
 });
+
+function getChunkAtIndex(array: string[], numChunks: number, index: number) {
+  if (!Array.isArray(array) || numChunks <= 0 || index < 0 || index >= numChunks) {
+      throw new Error("Invalid input: Provide a valid array, a positive number of chunks, and a valid index.");
+  }
+
+  const chunkSize = Math.floor(array.length / numChunks);
+  const remainder = array.length % numChunks; 
+
+  const start = index * chunkSize + Math.min(index, remainder);
+
+  const end = start + chunkSize + (index < remainder ? 1 : 0);
+
+  return array.slice(start, end);
+}
