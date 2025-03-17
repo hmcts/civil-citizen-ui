0691d9f61eef27f650dbccd1dfbc78a5ab6e6332
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-17 11:53:22 +0000

    give permission for run-parallel-a11y-tests.sh

--- src/test/functionalTests/run-parallel-a11y-tests.sh
+++ src/test/functionalTests/run-parallel-a11y-tests.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-if [[ -z "$CHUNKS" || ! "$CHUNKS" =~ ^[0-9]+$ || "$CHUNKS" -le 0 ]]; then
-  echo "Error: CHUNKS must be a positive integer."
+if [[ -z "$A11Y_CHUNKS" || ! "$A11Y_CHUNKS" =~ ^[0-9]+$ || "$A11Y_CHUNKS" -le 0 ]]; then
+  echo "Error: A11Y_CHUNKS must be a positive integer."
   exit 1
 fi
 
 COMMANDS=()
-for i in $(seq 0 $((CHUNKS - 1))); do
+for i in $(seq 0 $((A11Y_CHUNKS - 1))); do
   COMMANDS+=("A11Y_CHUNKS_INDEX=$i yarn tests:a11y --reporter-options reportFilename=a11y-$((i + 1))")
 done
 
