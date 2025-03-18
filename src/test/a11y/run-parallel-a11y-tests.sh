32496fb108b1ba166dcb757c11931dfff42740c9
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-18 08:59:44 +0000

    Make accessibility tests run on multiple threads

---
+++ src/test/a11y/run-parallel-a11y-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+if [[ -z "$A11Y_CHUNKS" || ! "$A11Y_CHUNKS" =~ ^[0-9]+$ || "$A11Y_CHUNKS" -le 0 ]]; then
+  echo "Error: A11Y_CHUNKS must be a positive integer."
+  exit 1
+fi
+
+COMMANDS=()
+for i in $(seq 0 $((A11Y_CHUNKS - 1))); do
+  COMMANDS+=("A11Y_CHUNKS_INDEX=$i yarn tests:a11y --reporter-options reportFilename=a11y-$((i + 1))")
+done
+
+yarn concurrently "${COMMANDS[@]}"
