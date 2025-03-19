b0b2f0e6c1be150168110e0e74480edb17eb5404
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-19 08:31:04 +0000

    remove concurrently

--- src/test/a11y/run-parallel-a11y-tests.sh
+++ src/test/a11y/run-parallel-a11y-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -ex
 
 if [[ -z "$A11Y_CHUNKS" || ! "$A11Y_CHUNKS" =~ ^[0-9]+$ || "$A11Y_CHUNKS" -le 0 ]]; then
@@ -6,9 +7,9 @@ if [[ -z "$A11Y_CHUNKS" || ! "$A11Y_CHUNKS" =~ ^[0-9]+$ || "$A11Y_CHUNKS" -le 0 
   exit 1
 fi
 
-COMMANDS=()
 for i in $(seq 0 $((A11Y_CHUNKS - 1))); do
-  COMMANDS+=("A11Y_CHUNKS_INDEX=$i yarn tests:a11y --reporter-options reportFilename=a11y-$((i + 1))")
+  A11Y_CHUNKS_INDEX=$i yarn tests:a11y --reporter-options reportFilename=a11y-$((i + 1)) &
 done
 
-yarn concurrently "${COMMANDS[@]}"
+wait
+echo "All accessibility tests completed."
