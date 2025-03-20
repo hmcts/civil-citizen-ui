7d361316526c0b6467c2ab6c6b7d0047206afdd4
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-03-20 11:33:19 +0000

    DTSCCI-1720 fix test

--- src/test/unit/services/features/claim/amount/checkClaimFee.test.ts
+++ src/test/unit/services/features/claim/amount/checkClaimFee.test.ts
@@ -1,9 +1,21 @@
 import {CivilServiceClient} from 'client/civilServiceClient';
 import {checkIfClaimFeeHasChanged} from 'services/features/claim/amount/checkClaimFee';
 import {mockClaim} from '../../../../../utils/mockClaim';
+import nock from 'nock';
+import config from 'config';
 
 jest.mock('../../../../../../main/services/features/claim/amount/claimFeesService');
+const civilServiceUrl = config.get<string>('services.civilService.url');
 describe('Check claim fee is changed service', () => {
+  beforeEach(() => {
+    nock(civilServiceUrl)
+      .post('/fees/claim/calculate-interest')
+      .reply(200, '100');
+    nock(civilServiceUrl)
+      .post('/fees/claim/interest')
+      .reply(200, '100');
+  });
+
   it('Should return status true if claim fee is changed ', async () => {
     //Given
     const mockClaimFee = {
