0ab6e9f7c37dca1df91606afc323667099da1f92
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2025-03-26 15:16:58 +0000

    update

--- src/test/functionalTests/tests/common/cp_LiPvLiP_hearing_fee_tests_small_claims_tests.js
+++ src/test/functionalTests/tests/common/cp_LiPvLiP_hearing_fee_tests_small_claims_tests.js
@@ -62,7 +62,7 @@ Scenario('Apply for Help with Fees Journey - Small Claims', async ({I, api}) => 
     taskListItem = payTheHearingFee(hearingFeeDueDate);
     await verifyTasklistLinkAndState(taskListItem.title, taskListItem.locator, 'In progress', false, true, taskListItem.deadline);
   }
-}).tag('@regression-cp');
+});
 
 Scenario('Pay the Hearing Fee Journey - Small Claims', async ({I, api}) => {
   const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
@@ -81,4 +81,4 @@ Scenario('Pay the Hearing Fee Journey - Small Claims', async ({I, api}) => {
     taskListItem = payTheHearingFee(hearingFeeDueDate);
     await verifyTasklistLinkAndState(taskListItem.title, taskListItem.locator, 'Done', false, false);
   }
-}).tag('@regression-cp');
+});
