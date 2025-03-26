daf0eed6e956ce208efae22c812ae055eabcf593
Neeta Shinde <neeta.shinde@hmcts.net>
2025-03-26 12:19:20 +0000

    fixed checkstyle.

--- src/main/routes/features/dashboard/claimantClaimSummaryController.ts
+++ src/main/routes/features/dashboard/claimantClaimSummaryController.ts
@@ -6,8 +6,8 @@ import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 const claimantClaimSummaryController = Router();
 
 claimantClaimSummaryController.get(OLD_DASHBOARD_CLAIMANT_URL, (async (req:AppRequest, res:Response, next: NextFunction) => {
-   const claimId = req.params.id;
-   res.redirect(constructResponseUrlWithIdParams(claimId, DASHBOARD_CLAIMANT_URL));
+  const claimId = req.params.id;
+  res.redirect(constructResponseUrlWithIdParams(claimId, DASHBOARD_CLAIMANT_URL));
 })as RequestHandler);
 
 export default claimantClaimSummaryController;
