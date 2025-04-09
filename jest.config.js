d1e25efb729f9e550cfae4355f10e24643303d32
Ruban <ruban72@hotmail.com>
2025-04-09 09:29:24 +0100

    Revert "DTSCCI-1585: Add optimisations to jest for unit tests"
    This reverts commit 32e06426a5ca10a56a3cea43e1f86545822df9e5.

--- jest.config.js
+++ jest.config.js
@@ -23,9 +23,5 @@ module.exports = {
   },
   setupFilesAfterEnv: ['./jest.setup.redis-mock.js'],
   coverageProvider: 'v8',
-  workerIdleMemoryLimit: '1024MB',
-  maxWorkers: "50%",
-  resetMocks: true,
-  restoreMocks: true,
-  clearMocks: true
+  workerIdleMemoryLimit: '1024MB'
 };
