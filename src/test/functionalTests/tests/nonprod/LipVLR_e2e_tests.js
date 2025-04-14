c6f817a8067f8bd904af6223d785e8f46f9ce7c1
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-04-14 09:53:05 +0100

    add test times for LipVLR e2e tests

--- src/test/functionalTests/tests/nonprod/LipVLR_e2e_tests.js
+++ src/test/functionalTests/tests/nonprod/LipVLR_e2e_tests.js
@@ -5,6 +5,7 @@ const nocSteps = require('../../lrFeatures/noc/steps/nocSteps.js');
 const steps = require('../../citizenFeatures/createClaim/steps/createLipvLipClaimSteps');
 const {verifyNotificationTitleAndContent} = require('../../specClaimHelpers/e2e/dashboardHelper');
 const {isDashboardServiceToggleEnabled} = require('../../specClaimHelpers/api/testingSupport');
+const testTimeHelper = require('../../helpers/test_time_helper');
 const {
   payClaimFee,
   nocForLip,
@@ -14,8 +15,41 @@ let claimRef, caseData, selectedHWF, legacyCaseReference, defendantName, isDashb
 
 Feature('Lip v LR e2e Tests').tag('@api @noc @regression');
 
-Before(async ({I, api}) => {
+// Before(async ({I, api}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     selectedHWF = false;
+//     const defaultClaimFee = 455;
+//     const defaultClaimAmount = 9000;
+//     isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+//     await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+//     await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+//     await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+//     await steps.createClaimDraftViaTestingSupport();
+//     claimRef = await steps.checkAndSubmit(selectedHWF);
+//     caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+//     legacyCaseReference = await caseData.legacyCaseReference;
+//     defendantName = await caseData.respondent1.partyName;
+//     await api.setCaseId(claimRef);
+//     await api.waitForFinishedBusinessProcess();
+//     if (isDashboardServiceEnabled) {
+//       const notification = payClaimFee(defaultClaimFee);
+//       await verifyNotificationTitleAndContent(legacyCaseReference, notification.title, notification.content);
+//       await I.click(notification.nextSteps);
+//     } else {
+//       await steps.clickPayClaimFee();
+//     }
+//     await steps.verifyAndPayClaimFee(defaultClaimAmount, defaultClaimFee);
+//     await api.waitForFinishedBusinessProcess();
+//     await api.assignToLipDefendant(claimRef);
+//   }
+// });
+
+Scenario('LipVLR - NoC and DefendantLR respond as DefenceAll @citizenUI', async ({
+  I,
+  api,
+}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
+    await testTimeHelper.addTestStartTime('LipVLR - NoC and DefendantLR respond as DefenceAll');
     selectedHWF = false;
     const defaultClaimFee = 455;
     const defaultClaimAmount = 9000;
@@ -40,14 +74,6 @@ Before(async ({I, api}) => {
     await steps.verifyAndPayClaimFee(defaultClaimAmount, defaultClaimFee);
     await api.waitForFinishedBusinessProcess();
     await api.assignToLipDefendant(claimRef);
-  }
-});
-
-Scenario('LipVLR - NoC and DefendantLR respond as DefenceAll @citizenUI', async ({
-  I,
-  api,
-}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
     await nocSteps.requestNoticeOfChangeForRespondent1Solicitor(claimRef, defendantName, config.defendantSolicitorUser);
     await api.checkUserCaseAccess(config.defendantCitizenUser, false);
     await api.checkUserCaseAccess(config.defendantSolicitorUser, true);
@@ -65,6 +91,7 @@ Scenario('LipVLR - NoC and DefendantLR respond as DefenceAll @citizenUI', async 
     camundaEvent = 'APPLY_NOC_DECISION_DEFENDANT_LIP';
     expectedState = 'AWAITING_APPLICANT_INTENTION';
     await api.defendantLRResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', camundaEvent, expectedState);
+    await testTimeHelper.addTestEndTime('LipVLR - NoC and DefendantLR respond as DefenceAll');
   }
 });
 
@@ -73,6 +100,31 @@ Scenario('LipVLR - NoC and DefendantLR respond as AdmitAll @citizenUI', async ({
   api,
 }) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
+    await testTimeHelper.addTestStartTime('LipVLR - NoC and DefendantLR respond as AdmitAll');
+    selectedHWF = false;
+    const defaultClaimFee = 455;
+    const defaultClaimAmount = 9000;
+    isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+    await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await steps.createClaimDraftViaTestingSupport();
+    claimRef = await steps.checkAndSubmit(selectedHWF);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    legacyCaseReference = await caseData.legacyCaseReference;
+    defendantName = await caseData.respondent1.partyName;
+    await api.setCaseId(claimRef);
+    await api.waitForFinishedBusinessProcess();
+    if (isDashboardServiceEnabled) {
+      const notification = payClaimFee(defaultClaimFee);
+      await verifyNotificationTitleAndContent(legacyCaseReference, notification.title, notification.content);
+      await I.click(notification.nextSteps);
+    } else {
+      await steps.clickPayClaimFee();
+    }
+    await steps.verifyAndPayClaimFee(defaultClaimAmount, defaultClaimFee);
+    await api.waitForFinishedBusinessProcess();
+    await api.assignToLipDefendant(claimRef);
     await nocSteps.requestNoticeOfChangeForRespondent1Solicitor(claimRef, defendantName, config.defendantSolicitorUser);
     await api.checkUserCaseAccess(config.defendantCitizenUser, false);
     await api.checkUserCaseAccess(config.defendantSolicitorUser, true);
@@ -90,6 +142,7 @@ Scenario('LipVLR - NoC and DefendantLR respond as AdmitAll @citizenUI', async ({
     camundaEvent = 'APPLY_NOC_DECISION_DEFENDANT_LIP';
     expectedState = 'PROCEEDS_IN_HERITAGE_SYSTEM';
     await api.defendantLRResponse(config.defendantSolicitorUser, 'FULL_ADMISSION', camundaEvent, expectedState);
+    await testTimeHelper.addTestEndTime('LipVLR - NoC and DefendantLR respond as AdmitAll');
   }
 });
 
@@ -98,6 +151,31 @@ Scenario('LipVLR - NoC and DefendantLR respond as PartAdmit @citizenUI - @api @n
   api,
 }) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
+    await testTimeHelper.addTestStartTime('LipVLR - NoC and DefendantLR respond as PartAdmit');
+    selectedHWF = false;
+    const defaultClaimFee = 455;
+    const defaultClaimAmount = 9000;
+    isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+    await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await steps.createClaimDraftViaTestingSupport();
+    claimRef = await steps.checkAndSubmit(selectedHWF);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    legacyCaseReference = await caseData.legacyCaseReference;
+    defendantName = await caseData.respondent1.partyName;
+    await api.setCaseId(claimRef);
+    await api.waitForFinishedBusinessProcess();
+    if (isDashboardServiceEnabled) {
+      const notification = payClaimFee(defaultClaimFee);
+      await verifyNotificationTitleAndContent(legacyCaseReference, notification.title, notification.content);
+      await I.click(notification.nextSteps);
+    } else {
+      await steps.clickPayClaimFee();
+    }
+    await steps.verifyAndPayClaimFee(defaultClaimAmount, defaultClaimFee);
+    await api.waitForFinishedBusinessProcess();
+    await api.assignToLipDefendant(claimRef);
     await nocSteps.requestNoticeOfChangeForRespondent1Solicitor(claimRef, defendantName, config.defendantSolicitorUser);
     await api.checkUserCaseAccess(config.defendantCitizenUser, false);
     await api.checkUserCaseAccess(config.defendantSolicitorUser, true);
@@ -115,6 +193,7 @@ Scenario('LipVLR - NoC and DefendantLR respond as PartAdmit @citizenUI - @api @n
     camundaEvent = 'APPLY_NOC_DECISION_DEFENDANT_LIP';
     expectedState = 'PROCEEDS_IN_HERITAGE_SYSTEM';
     await api.defendantLRResponse(config.defendantSolicitorUser, 'PART_ADMISSION', camundaEvent, expectedState);
+    await testTimeHelper.addTestEndTime('LipVLR - NoC and DefendantLR respond as PartAdmit');
   }
 });
 
@@ -123,6 +202,31 @@ Scenario('LipVLR - NoC and DefendantLR respond as CounterClaim @citizenUI - @api
   api,
 }) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
+    await testTimeHelper.addTestStartTime('LipVLR - NoC and DefendantLR respond as CounterClaim');
+    selectedHWF = false;
+    const defaultClaimFee = 455;
+    const defaultClaimAmount = 9000;
+    isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
+    await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+    await steps.createClaimDraftViaTestingSupport();
+    claimRef = await steps.checkAndSubmit(selectedHWF);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    legacyCaseReference = await caseData.legacyCaseReference;
+    defendantName = await caseData.respondent1.partyName;
+    await api.setCaseId(claimRef);
+    await api.waitForFinishedBusinessProcess();
+    if (isDashboardServiceEnabled) {
+      const notification = payClaimFee(defaultClaimFee);
+      await verifyNotificationTitleAndContent(legacyCaseReference, notification.title, notification.content);
+      await I.click(notification.nextSteps);
+    } else {
+      await steps.clickPayClaimFee();
+    }
+    await steps.verifyAndPayClaimFee(defaultClaimAmount, defaultClaimFee);
+    await api.waitForFinishedBusinessProcess();
+    await api.assignToLipDefendant(claimRef);
     await nocSteps.requestNoticeOfChangeForRespondent1Solicitor(claimRef, defendantName, config.defendantSolicitorUser);
     await api.checkUserCaseAccess(config.defendantCitizenUser, false);
     await api.checkUserCaseAccess(config.defendantSolicitorUser, true);
@@ -140,5 +244,6 @@ Scenario('LipVLR - NoC and DefendantLR respond as CounterClaim @citizenUI - @api
     camundaEvent = 'APPLY_NOC_DECISION_DEFENDANT_LIP';
     expectedState = 'PROCEEDS_IN_HERITAGE_SYSTEM';
     await api.defendantLRResponse(config.defendantSolicitorUser, 'COUNTER_CLAIM', camundaEvent, expectedState);
+    await testTimeHelper.addTestEndTime('LipVLR - NoC and DefendantLR respond as CounterClaim');
   }
 });
