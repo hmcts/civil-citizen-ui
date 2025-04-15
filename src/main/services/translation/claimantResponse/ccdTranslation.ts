8859dc22412ad9829e54264ba42ee986aba48428
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-04-15 13:26:15 +0100

    DTSCCI-1796 changes

--- src/main/services/translation/claimantResponse/ccdTranslation.ts
+++ src/main/services/translation/claimantResponse/ccdTranslation.ts
@@ -11,6 +11,12 @@ import {convertToPoundsFilter} from 'common/utils/currencyFormat';
 
 export const translateClaimantResponseDJToCCD = async (claim: Claim): Promise<CCDClaim> => {
   const summaryDetails = await getJudgmentAmountSummary(claim, convertToPoundsFilter(claim.claimFee?.calculatedAmountInPence), 'en');
+  const interest = Number(summaryDetails.interestToDate || 0);
+  let repaymentSummary = `The judgment will order the defendants to pay £${summaryDetails.total}, including the claim fee and interest, if applicable, as shown:\n### Claim amount \n £${claim.totalClaimAmount}\n`;
+  if(interest === 0) {
+    repaymentSummary = repaymentSummary + ` ### Claim interest amount \n £${interest} \n`;
+  }
+  repaymentSummary= repaymentSummary + ` £${summaryDetails.claimFeeAmount}\n ## Subtotal \n £${summaryDetails.subTotal}\n ## Total still owed \n £${summaryDetails.total}`;
   return {
     applicant1: toCCDParty(claim.applicant1),
     respondent1: toCCDParty(claim.respondent1),
@@ -26,6 +32,6 @@ export const translateClaimantResponseDJToCCD = async (claim: Claim): Promise<CC
     repaymentDue:claim.hasDefendantPaid() ? (claim.getCCJTotalAmount() - claim.getDefendantPaidAmount()).toString() : undefined,
     repaymentSuggestion: claim.getCCJPaymentOption() === PaymentOptionType.INSTALMENTS ? convertToPence(claim.getCCJRepaymentPlanAmount()).toString() : undefined,
     repaymentDate: claim.getCCJPaymentOption() === PaymentOptionType.INSTALMENTS ? claim.getCCJRepaymentPlanDate() : undefined,
-    repaymentSummaryObject: `The judgment will order the defendants to pay £${summaryDetails.total}, including the claim fee and interest, if applicable, as shown:\n### Claim amount \n £${claim.totalClaimAmount}\n ### Claim interest amount \n £${Number(summaryDetails.interestToDate || 0)} \n ### Claim fee amount \n £${summaryDetails.claimFeeAmount}\n ## Subtotal \n £${summaryDetails.subTotal}\n\n ## Total still owed \n £${summaryDetails.total}`,
+    repaymentSummaryObject: repaymentSummary,
   };
 };
