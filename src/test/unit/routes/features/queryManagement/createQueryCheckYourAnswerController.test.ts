1454f02277ea9a7d14da8a6f664b1d6a42764096
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2025-04-17 18:24:28 +0100

    Update createQueryCheckYourAnswerController.test.ts

--- src/test/unit/routes/features/queryManagement/createQueryCheckYourAnswerController.test.ts
+++ src/test/unit/routes/features/queryManagement/createQueryCheckYourAnswerController.test.ts
@@ -34,6 +34,7 @@ describe('create query check your answer controller', () => {
   });
   afterAll(() => {
     jest.clearAllMocks();
+    delete require.cache[require.resolve('../../../../../main/app')];
   });
   describe('GET', () => {
     it('should render query page', async () => {
