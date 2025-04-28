408b8651b4d0ccc99f05d4109d7cf3b223118d59
drummondjm <mark.drummond@version.com>
2025-04-28 18:15:55 +0100

    initial view query details screen

--- src/main/routes/features/queryManagement/qmViewQueryDetailsController.ts
+++ src/main/routes/features/queryManagement/qmViewQueryDetailsController.ts
@@ -1,13 +1,14 @@
 import {NextFunction, Request, RequestHandler, Response, Router} from 'express';
 import {
   QM_QUERY_DETAILS_URL,
-  BACK_URL
+  BACK_URL, QM_FOLLOW_UP_URL
 } from 'routes/urls';
 import { Claim } from 'models/claim';
 import { AppRequest } from 'models/AppRequest';
 import config from 'config';
 import { CivilServiceClient } from 'client/civilServiceClient';
 import {QueryListItem, ViewQueriesService} from 'services/features/queryManagement/viewQueriesService';
+import {formatDocumentViewURL} from 'common/utils/formatDocumentURL';
 
 const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
 const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
@@ -17,11 +18,21 @@ const viewQueriesPath = 'features/queryManagement/qm-view-query-details-template
 
 const renderView = async (res: Response, claimId: string, claim: Claim, lang: string, selectedQueryItem: QueryListItem): Promise<void> => {
   const backLinkUrl = BACK_URL;
+  // url to be updated once we have CIV-16951 send follow up message
+  const followUpScreen = QM_FOLLOW_UP_URL.replace(':id', claimId);
+  const firstAttachment = selectedQueryItem.attachments?.[0];
+
+  let documentLink: string | undefined;
+  if (firstAttachment?.value?.document_binary_url) {
+     documentLink = formatDocumentViewURL(firstAttachment.value.document_filename, claimId, firstAttachment.value.document_binary_url);
+  }
 
   res.render(viewQueriesPath, {
-    claimId,
+    pageTitle: 'PAGES.QM.VIEW_QUERY_DETAILS.PAGE_TITLE',
     selectedQueryItem,
-    pageTitle: 'PAGES.QM.VIEW_QUERY.PAGE_TITLE',
+    documentLink,
+    followUpScreen,
+    claimId,
     backLinkUrl
   });
 };
