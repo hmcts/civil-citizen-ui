7fbf61586145fbce9061a020bbbf70a0e20c48bf
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-05-07 14:12:51 +0100

    fix issue preview

--- src/main/services/features/queryManagement/queryManagementService.ts
+++ src/main/services/features/queryManagement/queryManagementService.ts
@@ -106,7 +106,8 @@ const saveDocumentToUploaded = async (req: AppRequest, file: UploadQMAdditionalF
 export const getSummaryList = async (formattedSummary: SummarySection, req: AppRequest, isFollowUp = false): Promise<void> => {
   const claim = await getClaimById(req.params.id, req, true);
   const queryManagement = claim.queryManagement;
-  const query = isFollowUp ? queryManagement.sendFollowUpQuery : queryManagement.createQuery;
+  delete claim.queryManagement.sendFollowUpQuery;
+  const query = isFollowUp ? queryManagement?.sendFollowUpQuery : queryManagement.createQuery;
 
   if (query) {
     const uploadedFiles = query.uploadedFiles;
