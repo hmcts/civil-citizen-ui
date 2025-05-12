81cb01d4aeaa328b888e8b60194f583f5c8bc1ec
Velican <duzgunce.vd@gmail.com>
2025-05-12 14:41:17 +0100

    remove id_token due to provider test not including it

--- src/test/contract/consumers/Oidc.test.ts
+++ src/test/contract/consumers/Oidc.test.ts
@@ -27,7 +27,6 @@ describe('Odic Pact Test', () => {
 
   describe('get Oidc response', () => {
     const odicResponse = {
-      id_token: 'someIdToken',
       access_token: 'someAccessToken',
     };
 
