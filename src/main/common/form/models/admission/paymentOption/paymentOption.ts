8c0d7f56a8daf5881f2fe2affbfe4ff717690bfe
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-05-20 10:57:26 +0100

    DTSCCI-1588 Def Response - Amendment to Payment Type Screen (FA)

--- src/main/common/form/models/admission/paymentOption/paymentOption.ts
+++ src/main/common/form/models/admission/paymentOption/paymentOption.ts
@@ -4,9 +4,11 @@ import {IsIn} from 'class-validator';
 export class PaymentOption {
   @IsIn(Object.values(PaymentOptionType), {message: 'ERRORS.VALID_PAYMENT_OPTION'})
     paymentType: PaymentOptionType;
+  totalClaimAmount?: number;
 
-  constructor(paymentType?: PaymentOptionType) {
+  constructor(paymentType?: PaymentOptionType, totalClaimAmount?: number) {
     this.paymentType = paymentType;
+    this.totalClaimAmount = totalClaimAmount;
   }
 
   paymentOptionBySetDateSelected() {
