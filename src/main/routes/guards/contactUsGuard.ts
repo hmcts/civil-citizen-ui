1e7c1b70b01d5840998c1bdfc4df8a56d995b1e4
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-05-26 13:43:27 +0100

    add the code to trigger send message link

--- src/main/routes/guards/contactUsGuard.ts
+++ src/main/routes/guards/contactUsGuard.ts
@@ -5,21 +5,27 @@ import {NextFunction, Request, Response} from 'express';
 import {generateRedisKey, getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {QM_START_URL} from 'routes/urls';
 import {AppRequest} from 'common/models/AppRequest';
+import {CaseState} from 'form/models/claimDetails';
 
 export const contactUsGuard = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ): Promise<void> => {
-  const requestId = req.path.split('/').filter(Boolean)[1] || undefined;
+  const isClaimOffLine = [CaseState.PENDING_CASE_ISSUED, CaseState.CASE_DISMISSED, CaseState.PROCEEDS_IN_HERITAGE_SYSTEM];
+  const requestId = req.path.split('/').filter(Boolean)[1] || undefined; // get claim id
   if(requestId){
     req.params.id = requestId;
     const redisKey = generateRedisKey(<AppRequest>req);
     const caseData: Claim = await getCaseDataFromStore(redisKey);
     const isQMFlagEnabled = await isQueryManagementEnabled(caseData.submittedDate);
 
-    res.locals.qmStartUrl = constructResponseUrlWithIdParams(req.params.id, QM_START_URL)+'?linkFrom=start';
-    res.locals.isQMFlagEnabled = isQMFlagEnabled;
+    if(isQMFlagEnabled && !isClaimOffLine.includes(caseData.ccdState)) {
+      res.locals.showCreateQuery = true;
+      res.locals.isQMFlagEnabled = true;
+      res.locals.disableSendMessage = true;
+      res.locals.qmStartUrl = constructResponseUrlWithIdParams(req.params.id, QM_START_URL)+'?linkFrom=start';
+    }
   }
   next();
 };
