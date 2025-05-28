53f9e4ee94fdc4af7c0d3098e71eb9073b5d8591
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-05-28 11:48:15 +0100

    add returns

--- src/main/services/commons/generalApplicationHelper.ts
+++ src/main/services/commons/generalApplicationHelper.ts
@@ -13,25 +13,31 @@ export const isGaOnline = (claim: Claim, isEaCourt: boolean, isWelshGaEnabled: b
   if (isSettledOrDiscontinued) { // if the claim is settled or discontinued
     if (!claim.previousCCDState) { // if the claim is settled or discontinued and previous CCD state is undefined
       gaInformation.isGaOnline = false;
+      return gaInformation;
     } else {
       gaInformation.isSettledOrDiscontinuedWithPreviousCCDState = true; // in the case that all the application's tasklist are inactive
     }
   }
   if (claim.isCaseIssuedPending()){ // if the claim is not yet issued
     gaInformation.isGaOnline = false;
+    return gaInformation;
   } else if ((claim.defendantUserDetails === undefined ||
       (claim.isLRDefendant() && claim.respondentSolicitorDetails === undefined)) // if the claim is not yet assigned to the defendant and not settled or discontinued
     && !isSettledOrDiscontinued) {
     gaInformation.isGaOnline = false;
+    return gaInformation;
   } else if (claim.hasClaimTakenOffline() ||
       claim.hasClaimBeenDismissed()) { // not show the ga link if claim is taken offline or dismissed
     gaInformation.isGaOnline = false;
+    return gaInformation;
   } else if (!isEaCourt) { // if the claim is not in EA court, then GA is not online
     gaInformation.isGaOnline = false;
+    return gaInformation;
   } else { //is in EA court
     if (claim.isAnyPartyBilingual() && !isWelshGaEnabled) { // if the claim is in EA court and any party is bilingual
       gaInformation.isGaOnline = false;
       gaInformation.isGAWelsh = true;
+      return gaInformation;
     }
   }
   return gaInformation;
