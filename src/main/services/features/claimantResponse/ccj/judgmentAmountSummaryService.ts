cfee1e65e2ee63c28dce7a554f4424cd88d4eb7c
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-05-30 15:02:53 +0100

    DTSCCI-1585 changes

--- src/main/services/features/claimantResponse/ccj/judgmentAmountSummaryService.ts
+++ src/main/services/features/claimantResponse/ccj/judgmentAmountSummaryService.ts
@@ -1,18 +1,24 @@
 import {Claim} from 'models/claim';
+import {getInterestData} from 'common/utils/interestUtils';
 
 export const getJudgmentAmountSummary = async (claim: Claim, claimFee: number, lang: string) => {
   const hasDefendantAlreadyPaid = claim.hasDefendantPaid();
   const alreadyPaidAmount = hasDefendantAlreadyPaid ? claim.getDefendantPaidAmount().toFixed(2) : 0;
+  const fullAdmission = claim.isFullAdmission();
+  const claimHasInterest = claim.hasInterest();
+  const interestDetails = fullAdmission && claimHasInterest ? await getInterestData(claim, lang) : undefined;
   const claimFeeAmount = claim.helpWithFees?.helpWithFeesReferenceNumber ? Number(claim.claimIssuedHwfDetails.outstandingFeeInPounds) : claimFee;
   const claimAmountAccepted : number = claim.hasClaimantAcceptedDefendantAdmittedAmount() ? claim.partialAdmissionPaymentAmount() : claim.totalClaimAmount;
-  const claimSubTotal = claimAmountAccepted + claimFeeAmount;
+  const claimSubTotal = claimAmountAccepted + claimFeeAmount + (interestDetails ? Number(interestDetails.interestToDate) : 0);
   const total = (claimSubTotal - Number(alreadyPaidAmount)).toFixed(2);
   const subTotal = claimSubTotal.toFixed(2);
   return {
     hasDefendantAlreadyPaid,
     alreadyPaidAmount,
     claimFeeAmount,
+    claimHasInterest,
     subTotal,
     total,
+    ...interestDetails,
   };
 };
