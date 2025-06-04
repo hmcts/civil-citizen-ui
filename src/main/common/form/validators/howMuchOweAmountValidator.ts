a7428ada88e3020af17c1d03e3057974fa2a3517
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-06-04 11:27:13 +0100

    DTSCCI-1585 changes

---
+++ src/main/common/form/validators/howMuchOweAmountValidator.ts
@@ -0,0 +1,31 @@
+import {ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface} from 'class-validator';
+
+/**
+ * Validates a number against a value of an object property. Returns true if the number is equal to or less than the property value number.
+ * Will not validate (return true) if the property value is not numeric or is not defined
+ */
+@ValidatorConstraint({name: 'howMuchOweAmountValidator'})
+export class HowMuchOweAmountValidator implements ValidatorConstraintInterface {
+  defaultMessage(): string {
+    return 'ERRORS.VALID_NUMBER_FOR_PREVIOUS_PAGE';
+  }
+
+  /**
+   * Requires property definition in validation arguments. So that the value is validated against the value of that property For example {property}
+   * @param value
+   * @param validationArguments {constraints:[string]} where string in the array would be a property name of the compared field
+   * For example @Validate(NumberEqualToPropertyValueOrLessValidator, ['maxValue']
+   */
+  validate(value: number, validationArguments?: ValidationArguments): Promise<boolean> | boolean {
+    if (validationArguments.constraints && validationArguments.constraints.length > 0) {
+      const property = validationArguments.constraints[0];
+      const propertyValue = (validationArguments.object as any)[property];
+      if (propertyValue === undefined || isNaN(propertyValue) || !value || isNaN(value)) {
+        return true;
+      }
+
+      return Number(value) <= (propertyValue - 0.01);
+    }
+    return true;
+  }
+}
