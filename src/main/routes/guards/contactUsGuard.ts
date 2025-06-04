985b6a73ead2183f5b0b02ee4b1b27012de165a2
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-06-04 12:26:15 +0100

    check on civil-service if the claim exists

--- src/main/routes/guards/contactUsGuard.ts
+++ src/main/routes/guards/contactUsGuard.ts
@@ -2,10 +2,14 @@ import {isQueryManagementEnabled} from '../../app/auth/launchdarkly/launchDarkly
 import {Claim} from 'common/models/claim';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {NextFunction, Request, Response} from 'express';
-import {generateRedisKey, getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {QM_START_URL} from 'routes/urls';
 import {AppRequest} from 'common/models/AppRequest';
 import {CaseState} from 'form/models/claimDetails';
+import config from 'config';
+import {CivilServiceClient} from 'client/civilServiceClient';
+
+const civilServiceApiBaseUrl = config.get<string>('services.civilService.url');
+const civilServiceClient: CivilServiceClient = new CivilServiceClient(civilServiceApiBaseUrl);
 
 export const contactUsGuard = async (
   req: Request,
@@ -25,15 +29,16 @@ export const contactUsGuard = async (
 
   if(requestId){
     req.params.id = requestId; // ensure req.params.id is set for further processing
-    const redisKey = generateRedisKey(<AppRequest>req);
-    const caseData: Claim = await getCaseDataFromStore(redisKey);
-    const isQMFlagEnabled = await isQueryManagementEnabled(caseData?.submittedDate);
-    delete req.params.id ; // remove params.id to avoid confusion in the next middleware
-    if(isQMFlagEnabled && !isClaimOffLine.includes(caseData.ccdState)) {
-      res.locals.showCreateQuery = true;
-      res.locals.isQMFlagEnabled = true;
-      res.locals.disableSendMessage = true;
-      res.locals.qmStartUrl = constructResponseUrlWithIdParams(requestId, QM_START_URL)+'?linkFrom=start';
+    const caseData: Claim = await civilServiceClient.retrieveClaimDetails(requestId, <AppRequest>req);
+    if (caseData || !caseData.isEmpty()) {
+      const isQMFlagEnabled = await isQueryManagementEnabled(caseData?.submittedDate);
+      delete req.params.id ; // remove params.id to avoid confusion in the next middleware
+      if(isQMFlagEnabled && !isClaimOffLine.includes(caseData.ccdState)) {
+        res.locals.showCreateQuery = true;
+        res.locals.isQMFlagEnabled = true;
+        res.locals.disableSendMessage = true;
+        res.locals.qmStartUrl = constructResponseUrlWithIdParams(requestId, QM_START_URL)+'?linkFrom=start';
+      }
     }
   }
   next();
