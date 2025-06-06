b60939fa47bcffc973170024824b80905a8098e8
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-06-06 13:44:42 +0100

    add the unit test of the guard.

--- src/main/routes/guards/mediationClaimantPhoneRedirectionGuard.ts
+++ src/main/routes/guards/mediationClaimantPhoneRedirectionGuard.ts
@@ -2,12 +2,12 @@ import {NextFunction, RequestHandler, Response} from 'express';
 import {constructResponseUrlWithIdParams} from 'common/utils/urlFormatter';
 import {MEDIATION_CLAIMANT_PHONE_URL} from 'routes/urls';
 import {AppRequest} from 'models/AppRequest';
-import {getClaimById} from 'modules/utilityService';
+import {generateRedisKey, getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 
-export const mediationPhoneRedirectionGuard = (async (req: AppRequest, res: Response, next: NextFunction) => {
+export const mediationClaimantPhoneRedirectionGuard = (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const claimId = req.params.id;
-    const claim = await getClaimById(claimId, req, true);
+    const claim = await getCaseDataFromStore(generateRedisKey(<AppRequest>req));
     if (req.method ==='GET' && claim.applicant1.partyPhone === undefined && claim.isClaimant()) {
       res.redirect(constructResponseUrlWithIdParams(claimId, MEDIATION_CLAIMANT_PHONE_URL));
     } else {
