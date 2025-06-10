0768fe99230a0d5a061be164c027c3ee47b9d45f
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2025-06-10 15:29:43 +0100

    aded beforeSuite

--- src/test/functionalTests/tests/nonprod/qm__LiPvLiP_ApplyToCourt_tests.js
+++ src/test/functionalTests/tests/nonprod/qm__LiPvLiP_ApplyToCourt_tests.js
@@ -3,42 +3,43 @@ const { createAccount } = require('../../specClaimHelpers/api/idamHelper');
 const LoginSteps = require('../../commonFeatures/home/steps/login');
 const ResponseSteps = require('../../citizenFeatures/response/steps/lipDefendantResponseSteps');
 
-async function setupClaimAndLogin({ api, I, userType = 'claimant', flowType = 'courtUpdate' }) {
-  const user = userType === 'claimant' ? config.claimantCitizenUser : config.defendantCitizenUser;
-  await createAccount(user.email, user.password);
+let claimRef, caseData, claimNumber;
 
-  const claimRef = await api.createLiPClaim(config.claimantCitizenUser, 'Multi', true);
+async function claimSetup(api) {
+  await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+  await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
 
+  claimRef = await api.createLiPClaim(config.claimantCitizenUser, 'Multi', true);
   await api.setCaseId(claimRef);
   await api.waitForFinishedBusinessProcess();
 
-  const caseData = await api.retrieveCaseData(config.adminUser, claimRef);
-  const claimNumber = caseData.legacyCaseReference;
+  caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+  claimNumber = caseData.legacyCaseReference;
+}
 
+async function loginAndSelectClaim({ I, user }) {
   await LoginSteps.EnterCitizenCredentials(user.email, user.password);
   await I.amOnPage('/dashboard');
   await I.click(claimNumber);
-  return { claimRef, caseData, claimNumber };
 }
 
 Feature('QM - LIP - Apply to Court scenarios @qm @123');
 
-Before(async () => {
-  await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-  await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+BeforeSuite(async ({ api, I }) => {
+  await claimSetup(api, I);
 });
 
-Scenario('Claimant verify Court Update Options flow', async ({ api, I }) => {
-  await setupClaimAndLogin({ api, I, userType: 'claimant', flowType: 'courtUpdate' });
+Scenario('Claimant verify Court Update Options flow', async ({ I }) => {
+  await loginAndSelectClaim({ I, user: config.claimantCitizenUser });
   await ResponseSteps.verifyCourtUpdateOptionsFlow();
 }).tag('@qm').tag('@nightly');
 
-Scenario('Claimant verify Send an Update to the court Options flow', async ({ api, I }) => {
-  await setupClaimAndLogin({ api, I, userType: 'claimant', flowType: 'sendUpdate' });
+Scenario('Claimant verify Send an Update to the court Options flow', async ({ I }) => {
+  await loginAndSelectClaim({ I, user: config.claimantCitizenUser });
   await ResponseSteps.verifySendUpdateToCourtFlow();
 }).tag('@qm').tag('@nightly');
 
-Scenario('Defendant Send documents to the court flow', async ({ api, I }) => {
-  await setupClaimAndLogin({ api, I, userType: 'defendant', flowType: 'sendDocuments' });
+Scenario('Defendant Send documents to the court flow', async ({ I }) => {
+  await loginAndSelectClaim({ I, user: config.defendantCitizenUser });
   await ResponseSteps.sendDocuments();
 }).tag('@qm').tag('@nightly');
