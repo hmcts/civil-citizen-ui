a629ee7de06a09f3a2aacb38c192d1882576c845
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2025-06-17 17:35:08 +0100

    Cleaned up

--- src/main/common/form/models/queryManagement/viewQuery.ts
+++ src/main/common/form/models/queryManagement/viewQuery.ts
@@ -37,16 +37,14 @@ export class QueryListItem {
   documents: FormattedDocument[];
   sentBy: string;
   sentOn: string;
-  usersQuery: boolean;
 
-  constructor(messageDetails: string, isHearingRelated: YesNoUpperCamelCase, documents: FormattedDocument[], sentBy: string, sentOn: string, hearingDate?: string, usersQuery?: boolean) {
+  constructor(messageDetails: string, isHearingRelated: YesNoUpperCamelCase, documents: FormattedDocument[], sentBy: string, sentOn: string, hearingDate?: string ) {
     this.messageDetails = messageDetails;
     this.isHearingRelated = isHearingRelated;
     this.hearingDate = hearingDate;
     this.documents = documents;
     this.sentBy = sentBy;
     this.sentOn = sentOn;
-    this.usersQuery = usersQuery;
   }
 }
 
