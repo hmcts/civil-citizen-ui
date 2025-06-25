a0d5d9a4b61aeeb4c32d260cbf917bfa70212feb
LeonardoPalmeiro <leonardo.palmeiro@hmcts.net>
2025-06-25 10:53:52 +0100

    fix

--- src/test/a11y/run-parallel-a11y-tests.sh
+++ src/test/a11y/run-parallel-a11y-tests.sh
@@ -9,9 +9,10 @@ if [[ -z "$A11Y_CHUNKS" || ! "$A11Y_CHUNKS" =~ ^[0-9]+$ || "$A11Y_CHUNKS" -le 0 
   exit 1
 fi
 
+pids=()
 for i in $(seq 0 $((A11Y_CHUNKS - 1))); do
   A11Y_CHUNKS_INDEX=$i yarn tests:a11y --reporter-options reportFilename=a11y-$((i + 1)) &
-  PIDS+=($!)
+  pids+=($!)
 done
 
 
@@ -21,15 +22,10 @@ for pid in "${pids[@]}"; do
   wait "$pid" || exit_code=1
 done
 
-# Wait for all and track failures
-exit_code=0
-for pid in "${pids[@]}"; do
-  wait "$pid" || exit_code=1
-done
-
 if [[ $exit_code -ne 0 ]]; then
   echo "One or more accessibility tests failed."
   HAS_FAILURE=1
 fi
+
 echo $HAS_FAILURE > a11y_failures.boolean
 echo "All accessibility tests completed successfully."
