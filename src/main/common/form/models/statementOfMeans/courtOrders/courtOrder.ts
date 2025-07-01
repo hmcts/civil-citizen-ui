543dc31035dc969de9bce62886fa7954d8abd10c
Mike Lemos <gobii@me.com>
2025-07-01 13:53:39 +0100

    DTSCCI-2089 fix for spaces on claim number

--- src/main/common/form/models/statementOfMeans/courtOrders/courtOrder.ts
+++ src/main/common/form/models/statementOfMeans/courtOrders/courtOrder.ts
@@ -1,20 +1,20 @@
-import {IsNotEmpty, IsNumber, Min, ValidateIf} from 'class-validator';
-import {toNumberOrUndefined} from 'common/utils/numberConverter';
+import { IsNotEmpty, IsNumber, Min, ValidateIf } from 'class-validator';
+import { toNumberOrUndefined } from 'common/utils/numberConverter';
 
 export class CourtOrder {
   @ValidateIf(o => o.isAtLeastOneFieldPopulated())
-  @IsNumber({maxDecimalPlaces: 2}, {message: 'ERRORS.VALID_TWO_DECIMAL_NUMBER'})
-  @Min(0, {message: 'ERRORS.VALID_STRICTLY_POSITIVE_NUMBER'})
-    instalmentAmount?: number;
+  @IsNumber({ maxDecimalPlaces: 2 }, { message: 'ERRORS.VALID_TWO_DECIMAL_NUMBER' })
+  @Min(0, { message: 'ERRORS.VALID_STRICTLY_POSITIVE_NUMBER' })
+  instalmentAmount?: number;
 
   @ValidateIf(o => o.isAtLeastOneFieldPopulated())
-  @IsNumber({maxDecimalPlaces: 2}, {message: 'ERRORS.VALID_TWO_DECIMAL_NUMBER'})
-  @Min(1.00, {message: 'ERRORS.AMOUNT_REQUIRED'})
-    amount?: number;
+  @IsNumber({ maxDecimalPlaces: 2 }, { message: 'ERRORS.VALID_TWO_DECIMAL_NUMBER' })
+  @Min(1.00, { message: 'ERRORS.AMOUNT_REQUIRED' })
+  amount?: number;
 
   @ValidateIf(o => o.isAtLeastOneFieldPopulated())
-  @IsNotEmpty({message: 'ERRORS.VALID_CLAIM_NUMBER'})
-    claimNumber?: string;
+  @IsNotEmpty({ message: 'ERRORS.VALID_CLAIM_NUMBER' })
+  claimNumber?: string;
 
   constructor(amount?: number, instalmentAmount?: number, claimNumber?: string) {
     this.instalmentAmount = instalmentAmount;
@@ -22,19 +22,23 @@ export class CourtOrder {
     this.claimNumber = claimNumber;
   }
 
-  static fromObject(value?: Record<string,string>): CourtOrder {
+  static fromObject(value?: Record<string, string>): CourtOrder {
     const instalmentAmount: number = toNumberOrUndefined(value.instalmentAmount);
     const amount: number = toNumberOrUndefined(value.amount);
-    const claimNumber: string = value.claimNumber || undefined;
+    const claimNumber: string = value.claimNumber.trim() || undefined;
 
     return new CourtOrder(amount, instalmentAmount, claimNumber);
   }
 
-  public isEmpty (): boolean {
-    return Object.values(this).every(value => value === undefined || value === '' || value?.length === 0);
+  public isEmpty(): boolean {
+    return Object.values(this).every(value =>
+      value === undefined ||
+      value === '' ||
+      (typeof value === 'string' && value.trim().length === 0)
+    )
   }
 
-  isAtLeastOneFieldPopulated (): boolean {
+  isAtLeastOneFieldPopulated(): boolean {
     return !this.isEmpty();
   }
 }
