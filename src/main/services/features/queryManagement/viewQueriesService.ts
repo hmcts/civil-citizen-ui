9ab55935f99c5283aa41ba9c835c8fb7bbcc1b30
sankaviv1 <sankavi.mohanraj@version1.com>
2025-07-02 17:05:49 +0100

    update logic

--- src/main/services/features/queryManagement/viewQueriesService.ts
+++ src/main/services/features/queryManagement/viewQueriesService.ts
@@ -50,7 +50,7 @@ export class ViewQueriesService {
     const children = queries.caseMessages.filter(query => query.value.parentId === queryId);
     const combined = [parent, ...children];
     console.log(combined);
-    const closedQuery = combined.filter(message => message.value.isClosed && message.value.isClosed.option === 'Yes').length > 0;
+    const closedQuery = combined.some(message => message.value.isClosed && message.value.isClosed.option === 'Yes');
     console.log(closedQuery);
     const lastStatus = closedQuery ? 'PAGES.QM.VIEW_QUERY.STATUS_CLOSED'
       : combined.length % 2 === 0 ? 'PAGES.QM.VIEW_QUERY.STATUS_RECEIVED' : 'PAGES.QM.VIEW_QUERY.STATUS_SENT' ;
