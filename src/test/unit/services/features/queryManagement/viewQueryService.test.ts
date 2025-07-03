1c4dfabdb10a684610a0b816adf4b5405805478f
sankaviv1 <sankavi.mohanraj@version1.com>
2025-07-03 12:13:10 +0100

    fix unit test

--- src/test/unit/services/features/queryManagement/viewQueryService.test.ts
+++ src/test/unit/services/features/queryManagement/viewQueryService.test.ts
@@ -316,7 +316,7 @@ describe('ViewQueriesService', () => {
         'a71d3791-b58d-4a57-957f-78dc48a12462',
         'formatted-2025-02-20T12:00:00Z-en',
         'fullDate-Sat May 10 2025 00:00:00 GMT+0000 (Coordinated Universal Time)-en',
-      )), true, 'fullDate-Thu Feb 20 2025 12:00:00 GMT+0000 (Greenwich Mean Time)-en',
+      )), true, 'fullDate-Thu Feb 20 2025 12:00:00 GMT+0000 (Coordinated Universal Time)-en',
     );
     expect(result.items.length).toBe(1);
     expect(result).toEqual(expected);
