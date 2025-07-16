dc993aba66a24a2901657779f9310be1af96351f
jeswanth <jeswanth.reddy@hmcts.net>
2025-07-16 15:27:07 +0100

    CIV-17393 removed toggles which is not needed

--- src/test/functionalTests/tests/mainClaimWelsh/LipVsLip_claimant_as_welsh.js
+++ src/test/functionalTests/tests/mainClaimWelsh/LipVsLip_claimant_as_welsh.js
@@ -2,7 +2,6 @@ const config = require('../../../config');
 const LoginSteps = require('../../commonFeatures/home/steps/login');
 const CitizenDashboardSteps = require('../../citizenFeatures/citizenDashboard/steps/citizenDashboard');
 const {createAccount} = require('../../specClaimHelpers/api/idamHelper');
-const {isDashboardServiceToggleEnabled} = require('../../specClaimHelpers/api/testingSupport');
 const {verifyNotificationTitleAndContent} = require('../../specClaimHelpers/e2e/dashboardHelper');
 const {
   respondToClaim,
@@ -22,26 +21,20 @@ Scenario('Create LipvLip claim and defendant response as FullAdmit and pay immed
   claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType, false, 'Individual', 'BOTH');
   caseData = await api.retrieveCaseData(config.adminUser, claimRef);
   claimNumber = await caseData.legacyCaseReference;
-  const isDashboardServiceEnabled = await isDashboardServiceToggleEnabled();
-  console.log('isDashboardServiceEnabled..', isDashboardServiceEnabled);
   await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
   await CitizenDashboardSteps.VerifyClaimOnDashboard(claimNumber);
   await CitizenDashboardSteps.VerifyStatusOnDashboard('The documents are being translated.', '//*[@id="main-content"]/div[1]/div/table/tbody/tr[1]/td[4]');
   await api.submitUploadTranslatedDoc('CLAIM_ISSUE');
   await LoginSteps.EnterCitizenCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
   await CitizenDashboardSteps.VerifyClaimOnDashboard(claimNumber);
-  if (isDashboardServiceEnabled) {
-    const notification = respondToClaim();
-    await verifyNotificationTitleAndContent(claimNumber, notification.title, notification.content);
-    await I.click(notification.nextSteps);
-  }
+  const notification = respondToClaim();
+  await verifyNotificationTitleAndContent(claimNumber, notification.title, notification.content);
+  await I.click(notification.nextSteps);
   await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.admitAllPayImmediateWithIndividual);
   await api.waitForFinishedBusinessProcess();
   await api.submitUploadTranslatedDoc('DEFENDANT_RESPONSE');
-  if (isDashboardServiceEnabled) {
-    const notification = defendantResponseFullAdmitPayImmediately(claimTotalAmount, deadline);
-    await verifyNotificationTitleAndContent(claimNumber, notification.title, notification.content);
-    await I.click(notification.nextSteps);
-  }
+  const claimantNotification = defendantResponseFullAdmitPayImmediately(claimTotalAmount, deadline);
+  await verifyNotificationTitleAndContent(claimNumber, claimantNotification.title, claimantNotification.content);
+  await I.click(claimantNotification.nextSteps);
   await I.click('Sign out');
 }).tag('@cui-welsh');
