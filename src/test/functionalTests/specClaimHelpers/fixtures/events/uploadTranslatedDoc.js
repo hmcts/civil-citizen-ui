a1f1d5350f232291699c8864cca62b2c1f0eef7b
jeswanth <jeswanth.reddy@hmcts.net>
2025-07-16 13:17:22 +0100

    CIV-17393 added changes wels functional tests

--- src/test/functionalTests/specClaimHelpers/fixtures/events/uploadTranslatedDoc.js
+++ src/test/functionalTests/specClaimHelpers/fixtures/events/uploadTranslatedDoc.js
@@ -2,24 +2,23 @@ const uploadTranslatedDoc = (document, translationDocType) => {
   const userData = {
     userInput: {
       UploadTranslatedDocument: {
-        "translatedDocuments": [
+        'translatedDocuments': [
           {
-            "value": {
-              "documentType": translationDocType,
-              "file": {
+            'value': {
+              'documentType': translationDocType,
+              'file': {
                 document_url: document.document_url,
                 document_binary_url: document.document_binary_url,
-                "document_filename": "notice_of_discontinuance_000MC022.pdf",
-              }
+                'document_filename': 'notice_of_discontinuance_000MC022.pdf',
+              },
             },
-            "id": null
-          }
-        ]
-      }
-    }
-
-  }
-  return userData
+            'id': null,
+          },
+        ],
+      },
+    },
+  };
+  return userData;
 };
 
 module.exports = uploadTranslatedDoc;
