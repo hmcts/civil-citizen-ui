e7207add5fc12b996dedfa6ab651783a174b0e89
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2025-07-30 13:38:40 +0100

    Update viewApplicationService.ts

--- src/main/services/features/generalApplication/viewApplication/viewApplicationService.ts
+++ src/main/services/features/generalApplication/viewApplication/viewApplicationService.ts
@@ -263,8 +263,8 @@ export const getHearingOrder = (applicationResponse: ApplicationResponse, lang: 
       return new Date(item2?.value?.createdDatetime).getTime() - new Date(item1?.value?.createdDatetime).getTime();
     }).map(hearingOrder => {
       const documentLabel = hearingOrder.value.documentName.indexOf('Translated') !== -1
-        ? 'PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.TRANSLATED_HEARING_ORDER'
-        : 'PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.HEARING_ORDER';
+        ? t('PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.TRANSLATED_HEARING_ORDER', {lang})
+        : t('PAGES.GENERAL_APPLICATION.VIEW_APPLICATION.HEARING_ORDER', {lang});
       return setUpDocumentLinkObject(hearingOrder.value?.documentLink, hearingOrder.value?.createdDatetime, applicationResponse?.id, lang, documentLabel, hearingOrder.value.documentName);
     });
   }
