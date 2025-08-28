dea97ac49de20205878efdbd9d68b2634c29057c
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-08-28 09:24:44 +0100

    update non-prod functional test tags

--- src/test/functionalTests/tests/nonprod/LipVLR_NoC_e2e_tests.js
+++ src/test/functionalTests/tests/nonprod/LipVLR_NoC_e2e_tests.js
@@ -12,7 +12,7 @@ const {
 
 let claimRef, caseData, selectedHWF, legacyCaseReference, defendantName, isDashboardServiceEnabled, camundaEvent, expectedState;
 
-Feature('Lip v LR e2e Tests').tag('@api @noc @regression');
+Feature('Lip v LR e2e Tests').tag('@noc @regression');
 
 Before(async ({I, api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
