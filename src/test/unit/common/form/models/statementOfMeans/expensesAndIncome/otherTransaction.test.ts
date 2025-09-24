e9b7741dac47bb1280775f3fb4a98c0082c54ceb
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-09-24 16:49:35 +0100

    DTSCCI-2633 changes

--- src/test/unit/common/form/models/statementOfMeans/expensesAndIncome/otherTransaction.test.ts
+++ src/test/unit/common/form/models/statementOfMeans/expensesAndIncome/otherTransaction.test.ts
@@ -67,7 +67,15 @@ describe('OtherTransaction.buildPopulatedForm', () => {
       // Then
       expect(result.declared).toBe(true);
       expect(result.transactionSources).toHaveLength(2);
-      expect(result.transactionSources[0]).toEqual(expect.objectContaining({"amount": undefined, "isIncome": true, "name": "InvalidTransaction", "nameRequired": true, "schedule": undefined}));
+      expect(result.transactionSources[0]).toEqual(
+        expect.objectContaining(
+          {
+            'amount': undefined,
+            'isIncome': true,
+            'name': 'InvalidTransaction',
+            'nameRequired': true,
+            'schedule': undefined
+          }));
     });
   });
 });
