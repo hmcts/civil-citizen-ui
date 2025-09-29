cbef187d27589b627fc008d1eb521f72ccc30161
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2025-09-29 14:40:45 +0100

    CIV-17691 removed unnecessary condition

--- src/main/services/features/generalApplication/response/generalApplicationResponseService.ts
+++ src/main/services/features/generalApplication/response/generalApplicationResponseService.ts
@@ -140,8 +140,7 @@ export const saveRespondentStatementOfTruth = async (redisKey: string, statement
 
 export const isApplicationVisibleToRespondent = (application: ApplicationResponse): boolean => {
   return isApplicationFullyVisibleToRespondent(application)
-    || (application.case_data?.judicialDecision && (application.case_data?.applicationIsCloaked === YesNoUpperCamelCase.YES
-    && application.state !== ApplicationState.APPLICATION_ADD_PAYMENT));
+    || (application.case_data?.judicialDecision && (application.state !== ApplicationState.APPLICATION_ADD_PAYMENT));
 };
 
 export const isApplicationFullyVisibleToRespondent = (application: ApplicationResponse): boolean => {
@@ -160,8 +159,7 @@ export const isApplicationFullyVisibleToRespondent = (application: ApplicationRe
 
 export const isApplicationVisibleToRespondentForClaimant = (application: ApplicationResponse): boolean => {
   return isApplicationFullyVisibleToRespondentForClaimant(application)
-  || (application.case_data?.judicialDecision && (application.case_data?.applicationIsCloaked === YesNoUpperCamelCase.YES
-     && application.state !== ApplicationState.APPLICATION_ADD_PAYMENT));
+  || (application.case_data?.judicialDecision && (application.state !== ApplicationState.APPLICATION_ADD_PAYMENT));
 };
 
 export const isApplicationFullyVisibleToRespondentForClaimant = (application: ApplicationResponse): boolean => {
