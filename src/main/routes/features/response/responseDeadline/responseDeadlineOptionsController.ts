8df8176eb16cf0a6b6c768caa61442116c4c2258
Neeta Shinde <neeta.shinde@hmcts.net>
2025-09-30 11:18:13 +0100

    fixed conflict

--- src/main/routes/features/response/responseDeadline/responseDeadlineOptionsController.ts
+++ src/main/routes/features/response/responseDeadline/responseDeadlineOptionsController.ts
@@ -19,7 +19,6 @@ const responseDeadlineOptionsController = Router();
 const responseDeadlineOptionsViewPath = 'features/response/response-deadline-options';
 const responseDeadlineService = new ResponseDeadlineService();
 
-async function renderView(res: Response, form: GenericForm<ResponseDeadline>, claim: Claim, language: string): Promise<void> {
 async function renderView(res: Response, form: GenericForm<ResponseDeadline>, claim: Claim, language: string, claimId: string): Promise<void> {
   const isGaNroEnabled = await isCuiGaNroEnabled();
   res.render(responseDeadlineOptionsViewPath, {
@@ -32,56 +31,56 @@ async function renderView(res: Response, form: GenericForm<ResponseDeadline>, cl
 }
 
 responseDeadlineOptionsController.get(RESPONSE_DEADLINE_OPTIONS_URL, deadLineGuard,
-  (async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const claim = await getCaseDataFromStore(generateRedisKey(<AppRequest>req));
-      const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-      renderView(res, new GenericForm(new ResponseDeadline(claim.responseDeadline?.option)), claim, lang, req.params.id);
-    } catch (error) {
-      next(error);
-    }
-  }) as RequestHandler);
+    (async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const claim = await getCaseDataFromStore(generateRedisKey(<AppRequest>req));
+        const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+        renderView(res, new GenericForm(new ResponseDeadline(claim.responseDeadline?.option)), claim, lang, req.params.id);
+      } catch (error) {
+        next(error);
+      }
+    }) as RequestHandler);
 
 responseDeadlineOptionsController.post(RESPONSE_DEADLINE_OPTIONS_URL, deadLineGuard,
-  ( async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      let responseOption;
-      let redirectUrl;
-      const claimId = req.params.id;
-      const redisKey = generateRedisKey(<AppRequest>req);
-      const lang = req.query.lang ? req.query.lang : req.cookies.lang;
-      const claim = await getCaseDataFromStore(redisKey);
-      switch (req.body['option']) {
-        case 'already-agreed':
-          responseOption = ResponseOptions.ALREADY_AGREED;
-          redirectUrl = constructResponseUrlWithIdParams(claimId, AGREED_TO_MORE_TIME_URL);
-          break;
-        case 'no':
-          responseOption = ResponseOptions.NO;
-          redirectUrl = constructResponseUrlWithIdParams(claimId, RESPONSE_TASK_LIST_URL);
-          break;
-        case 'request-refused':
-          responseOption = ResponseOptions.REQUEST_REFUSED;
-          redirectUrl = constructResponseUrlWithIdParams(claimId, RESPONSE_TASK_LIST_URL);
-          break;
-        case 'yes':
-          responseOption = ResponseOptions.YES;
-          redirectUrl = constructResponseUrlWithIdParams(claimId, REQUEST_MORE_TIME_URL);
-          break;
-        default:
-          responseOption = undefined;
+    (async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        let responseOption;
+        let redirectUrl;
+        const claimId = req.params.id;
+        const redisKey = generateRedisKey(<AppRequest>req);
+        const lang = req.query.lang ? req.query.lang : req.cookies.lang;
+        const claim = await getCaseDataFromStore(redisKey);
+        switch (req.body['option']) {
+          case 'already-agreed':
+            responseOption = ResponseOptions.ALREADY_AGREED;
+            redirectUrl = constructResponseUrlWithIdParams(claimId, AGREED_TO_MORE_TIME_URL);
+            break;
+          case 'no':
+            responseOption = ResponseOptions.NO;
+            redirectUrl = constructResponseUrlWithIdParams(claimId, RESPONSE_TASK_LIST_URL);
+            break;
+          case 'request-refused':
+            responseOption = ResponseOptions.REQUEST_REFUSED;
+            redirectUrl = constructResponseUrlWithIdParams(claimId, RESPONSE_TASK_LIST_URL);
+            break;
+          case 'yes':
+            responseOption = ResponseOptions.YES;
+            redirectUrl = constructResponseUrlWithIdParams(claimId, REQUEST_MORE_TIME_URL);
+            break;
+          default:
+            responseOption = undefined;
+        }
+        const form = new GenericForm(new ResponseDeadline(responseOption));
+        await form.validate();
+        if (form.hasErrors()) {
+          renderView(res, form, claim, lang, claimId);
+        } else {
+          await responseDeadlineService.saveDeadlineResponse(redisKey, responseOption);
+          res.redirect(redirectUrl);
+        }
+      } catch (error) {
+        next(error);
       }
-      const form = new GenericForm(new ResponseDeadline(responseOption));
-      await form.validate();
-      if (form.hasErrors()) {
-        renderView(res, form, claim, lang, claimId);
-      } else {
-        await responseDeadlineService.saveDeadlineResponse(redisKey, responseOption);
-        res.redirect(redirectUrl);
-      }
-    } catch (error) {
-      next(error);
-    }
-  }) as RequestHandler);
+    }) as RequestHandler);
 
 export default responseDeadlineOptionsController;
