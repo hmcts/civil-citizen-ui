c6d18925dc157b68b68f13cd74876912c781672e
matt-goodwin-solirius <matt.goodwin@solirius.com>
2025-10-23 14:18:52 +0100

    Further tests fixed

--- src/test/functionalTests/citizenFeatures/GA/pages/informOtherParties.js
+++ src/test/functionalTests/citizenFeatures/GA/pages/informOtherParties.js
@@ -27,20 +27,20 @@ class InformOtherParties {
   }
 
   verifyHeadingDetails(applicationType) {
-    I.see(applicationType, 'h1');
+    I.see(applicationType, '.govuk-caption-l');
     I.see('Informing the other parties', 'h1');
   }
 
   async verifyPageText() {
     I.see('The court will usually send the other parties a copy of your application to give them the opportunity to respond to it.');
     I.see('In some circumstances a judge may make an order without sending your application to the other parties first.');
-    I.see('If you do not want the court to inform the other parties about this application', 'h1');
+    I.see('If you do not want the court to inform the other parties about this application', 'h2');
     I.see('You must provide a reason which will be considered by a judge.');
     await I.see('If the judge orders that the other parties should be informed, you’ll be notified and you\'ll need to pay an additional fee. Your application may also be delayed.');
   }
 
   async verifyOptions() {
-    I.see('Should the court inform the other parties about this application?', 'h1');
+    I.see('Should the court inform the other parties about this application?');
     I.see('If you’ll be selecting multiple applications, this answer will apply to all of them.');
     I.see('Yes');
     await I.see('No');
