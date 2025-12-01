3cc7a2f00dfc6c6c5ac5e2d188da707c3612055a
Shaun Francis <shaun.francis@hmcts.net>
2025-12-01 11:16:45 +0000

    Fix GA_BY_CASE_URL constant

--- src/main/app/client/gaServiceUrls.ts
+++ src/main/app/client/gaServiceUrls.ts
@@ -4,4 +4,4 @@ export const GA_SERVICE_CASE_URL = '/cases/:caseId';
 export const GA_SERVICE_SUBMIT_EVENT = `${GA_SERVICE_CASES_URL}:caseId/ga/citizen/:submitterId/event`;
 export const GA_FEES_PAYMENT_URL= '/fees/case/:claimId/ga/payment';
 export const GA_FEES_PAYMENT_STATUS_URL= '/fees/case/:claimId/ga/payment/:paymentReference/status';
-export const GA_BY_CASE_URL = '${GA_SERVICE_CASES_URL}:id/ga/applications';
+export const GA_BY_CASE_URL = '/cases/:id/ga/applications';
