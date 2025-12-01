f314cd173e195353b29455be357daebbcbd05ee3
Shaun Francis <shaun.francis@hmcts.net>
2025-12-01 10:16:39 +0000

    Update general applications URLs to use civil-service

--- src/main/app/client/gaServiceUrls.ts
+++ src/main/app/client/gaServiceUrls.ts
@@ -1,7 +1,7 @@
 export const GA_SERVICE_CASES_URL = '/cases/';
 export const GA_GET_APPLICATION_URL= `${GA_SERVICE_CASES_URL}:caseId`;
 export const GA_SERVICE_CASE_URL = '/cases/:caseId';
-export const GA_SERVICE_SUBMIT_EVENT = `${GA_SERVICE_CASES_URL}:caseId/citizen/:submitterId/event`;
-export const GA_FEES_PAYMENT_URL= '/fees/case/:claimId/payment';
-export const GA_FEES_PAYMENT_STATUS_URL= '/fees/case/:claimId/payment/:paymentReference/status';
-export const GA_BY_CASE_URL = '/cases/:id/applications';
+export const GA_SERVICE_SUBMIT_EVENT = `${GA_SERVICE_CASES_URL}:caseId/ga/citizen/:submitterId/event`;
+export const GA_FEES_PAYMENT_URL= '/fees/case/:claimId/ga/payment';
+export const GA_FEES_PAYMENT_STATUS_URL= '/fees/case/:claimId/ga/payment/:paymentReference/status';
+export const GA_BY_CASE_URL = '${GA_SERVICE_CASES_URL}:id/ga/applications';
