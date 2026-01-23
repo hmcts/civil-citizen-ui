9eca841c3f52bb2b156ae535520e220a9636931f
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2026-01-23 12:54:24 +0000

    DTSCCI-3717: adding the file upload error message and it's translation

--- src/main/common/utils/fileUploadUtils.ts
+++ src/main/common/utils/fileUploadUtils.ts
@@ -134,6 +134,7 @@ export const uploadAndValidateFile = async (
       try {
         if (categoryModel && categoryModel[+index]) {
           categoryModel[+index].caseDocument = await civilServiceClient.uploadDocument(req, fileUpload);
+          logger.info(`[SAVE FILE] File upload response: ${JSON.stringify(categoryModel[+index].caseDocument)}`);
           categoryModel[+index].caseDocument = undefined;
           if (!categoryModel[+index].caseDocument?.documentLink) {
             logger.error(`[SAVE FILE] File upload response missing documentLink: ${JSON.stringify(categoryModel[+index].caseDocument)}`);
