2529fa0a9532b08a0d89d458fc11e41ad0526576
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2026-03-05 15:15:02 +0000

    DTSCCI-3494 log claimant response

--- src/main/routes/guards/redisDataFlushForDJGuard.ts
+++ src/main/routes/guards/redisDataFlushForDJGuard.ts
@@ -6,13 +6,19 @@ import {
   saveDraftClaim,
 } from 'modules/draft-store/draftStoreService';
 import {refreshDraftStoreClaimFrom} from 'modules/utilityService';
+const {Logger} = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('redisDataFlushForDJGuard');
 
 export const redisDataFlushForDJ = async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
-    let claim = await getCaseDataFromStore(generateRedisKey(req));
+    const redisKey = generateRedisKey(req);
+    let claim = await getCaseDataFromStore(redisKey);
+    logger.info(`Before refresh claim : claim.refreshDataForDJ: ${claim.refreshDataForDJ} redisKey: ${redisKey} ${claim.claimantResponse}`);
     if (claim.refreshDataForDJ) {
+      logger.info(`Refreshing claim : redisKey: ${redisKey} ${claim.claimantResponse}`);
       claim = await refreshDraftStoreClaimFrom(req, true);
       claim.refreshDataForDJ = false;
+      logger.info(`After refresh claim : redisKey: ${redisKey} ${claim.claimantResponse}`);
       await saveDraftClaim(generateRedisKey(req), claim);
     }
     next();
