4790d5239c8daa6f0a4e18dd68d38578ae6c47b3
Johnny Vineall <vineall@live.co.uk>
2026-03-10 12:31:36 +0000

    another fix

--- src/main/modules/csrf/index.ts
+++ src/main/modules/csrf/index.ts
@@ -1,5 +1,5 @@
 import csurf from 'csurf';
-import type {Application, Request, Response, NextFunction} from 'express';
+import type {Application} from 'express';
 import { isTestingSupportDraftUrl } from 'modules/oidc';
 
 export class CSRFToken {
@@ -8,7 +8,8 @@ export class CSRFToken {
       if (req.path.startsWith('/eligibility') || req.path.startsWith('/first-contact') || isTestingSupportDraftUrl(req.originalUrl)) {
         return next();
       }
-      return csurf()(req as unknown as Request, res as unknown as Response, next as NextFunction);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return csurf()(req as any, res as any, next);
     });
     app.use((req, res, next) => {
       if (!(req.path.startsWith('/eligibility') || req.path.startsWith('/first-contact')) && !isTestingSupportDraftUrl(req.originalUrl)) {
