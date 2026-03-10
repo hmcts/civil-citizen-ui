4ce8b28fdc06926d1df56ab7704c4d571dbc193a
rishisharma <rishi.sharma@hmcts.net>
2026-03-10 14:48:16 +0000

    DTSCCI-3779 TEST PR RKS

--- src/test/functionalTests/specClaimHelpers/dashboardNotificationConstants.js
+++ src/test/functionalTests/specClaimHelpers/dashboardNotificationConstants.js
@@ -151,10 +151,10 @@ module.exports = {
   },
 
   //Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Defendant
-  defendantResponseFullAdmitPayBySetDateDefendant: () => {
+  defendantResponseFullAdmitPayBySetDateDefendant: (amount, deadline) => {
     return {
-      title: 'The claimant has been paid the full amount that they were owed',
-      content: ['The claim is now settled and there’s no further action any party needs to take.'],
+      title: 'Response to the claim',
+      content: [`You have offered to pay £${amount}`, `by ${deadline}.`, 'We will contact you when the claimant responds to your offer.'],
       nextSteps: 'View your response',
     };
   },
@@ -162,10 +162,10 @@ module.exports = {
   //Notice.AAA6.DefResponse.FullOrPartAdmit.PayBySetDate.Claimant
   //This might not be strictly correct ^
   //Deadline logic needs adding
-  defendantResponseFullAdmitPayBySetDateClaimant: () => {
+  defendantResponseFullAdmitPayBySetDateClaimant: (amount, deadline) => {
     return {
-      title: 'The claimant has been paid the full amount that they were owed',
-      content: ['The claim is now settled and there’s no further action any party needs to take.'],
+      title: 'Response to the claim',
+      content: [`Sir John Doe has offered to pay you £${amount}`, `by ${deadline}`],
       nextSteps: 'View and respond',
     };
   },
