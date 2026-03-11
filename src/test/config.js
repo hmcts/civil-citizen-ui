794d058b87dc251673403e30865d74cb5c8ed900
jameskr <james.skriczka@hmcts.net>
2026-03-11 11:26:23 +0000

    amend path

--- src/test/config.js
+++ src/test/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({path: '.env.tests.local'});
 const PropertiesVolume = require('./secretsConfig');
 
 const defaultPassword = process.env.CITIZEN_PASSWORD;
