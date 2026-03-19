e9bd3723462ef02672a39a4dd022ed57457e66a6
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2026-03-19 16:48:33 +0000

    DTSCCI-3494 fix Integration Test

--- src/test/e2eTests/defendantResponse/defendantResponseSteps.js
+++ src/test/e2eTests/defendantResponse/defendantResponseSteps.js
@@ -11,7 +11,7 @@ class DefendantResponseSteps {
   }
 
   partAdmitPayBySetDateDefendantSignsSettlementAgreementConfirmation() {
-    I.see('You\'ve both signed a settlement agreement');
+    I.see('You\'ve both signed a settlement agreement', 60);
     I.see('The agreement says you\'ll repay by');
     I.see('The claimant can\'t request a County Court Judgment against you unless you break the terms.');
     I.see('Contact Miss Jane Doe if you need their payment details.');
