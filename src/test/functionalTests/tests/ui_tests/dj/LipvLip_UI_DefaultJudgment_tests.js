ba0603c7f59ce91df05f6cc17dd7ebd283cdef55
Douglas Rice <douglas.rice@hmcts.net>
2026-03-31 15:14:58 +0100

    test

--- src/test/functionalTests/tests/ui_tests/dj/LipvLip_UI_DefaultJudgment_tests.js
+++ src/test/functionalTests/tests/ui_tests/dj/LipvLip_UI_DefaultJudgment_tests.js
@@ -8,7 +8,7 @@ const claimType = 'SmallClaims';
 // eslint-disable-next-line no-unused-vars
 let claimRef;
 
-Feature('Create Lip v Lip claim -  Default Judgment').tag('@ui-dj');
+Feature('Create Lip v Lip claim -  Default Judgment').tag('@ui-dj @debug');
 
 Scenario('Create LipvLip claim and defendant not responded by deadline and Claimant raise Default Judgment', async ({api}) => {
   await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
--- src/test/functionalTests/tests/ui_tests/ga/LipvLip_GA_creation_tests.js
+++ src/test/functionalTests/tests/ui_tests/ga/LipvLip_GA_creation_tests.js
@@ -11,46 +11,46 @@ Before(async ({api}) => {
   await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
   await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
 
-  claimType = 'FastTrack';
-  claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
-  caseData = await api.retrieveCaseData(config.adminUser, claimRef);
-  claimNumber = await caseData.legacyCaseReference;
-
-  await api.assignToLipDefendant(claimRef);
-  await api.waitForFinishedBusinessProcess();
+  // claimType = 'FastTrack';
+  // claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
+  // caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+  // claimNumber = await caseData.legacyCaseReference;
+  //
+  // await api.assignToLipDefendant(claimRef);
+  // await api.waitForFinishedBusinessProcess();
 });
 
 Scenario('LipvLip Applicant GA creation tests', async ({I}) => {
-  await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-
-  console.log('Creating set aside GA app as claimant');
-  await I.amOnPage('/dashboard');
-  await I.click(claimNumber);
-  await createGASteps.askToSetAsideJudgementGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
-
-  console.log('Creating vary a judgment GA app as claimant');
-  await createGASteps.askToVaryAJudgementGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
-
-  console.log('Creating court to reconsider an order GA app as claimant');
-  await createGASteps.askCourtToReconsiderAnOrderGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
-
-  console.log('Creating change hearing date GA app as claimant');
-  await createGASteps.askToChangeHearingDateGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'notice');
-
-  console.log('Creating more time to do order GA app as claimant');
-  await createGASteps.askForMoreTimeCourtOrderGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'notice');
-
-  console.log('Creating relief from penalty GA app as claimant');
-  await createGASteps.askForReliefFromAPenaltyGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'withoutnotice');
-
-  console.log('Creating change claim or defence GA app as claimant');
-  await createGASteps.askToChangeSubmittedGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'withoutnotice');
-
-  console.log('Creating multiple applications as claimant');
-  await createGASteps.createMultipleApplications(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
+   await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
+//
+//   console.log('Creating set aside GA app as claimant');
+//   await I.amOnPage('/dashboard');
+//   await I.click(claimNumber);
+//   await createGASteps.askToSetAsideJudgementGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
+//
+//   console.log('Creating vary a judgment GA app as claimant');
+//   await createGASteps.askToVaryAJudgementGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
+//
+//   console.log('Creating court to reconsider an order GA app as claimant');
+//   await createGASteps.askCourtToReconsiderAnOrderGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
+//
+//   console.log('Creating change hearing date GA app as claimant');
+//   await createGASteps.askToChangeHearingDateGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'notice');
+//
+//   console.log('Creating more time to do order GA app as claimant');
+//   await createGASteps.askForMoreTimeCourtOrderGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'notice');
+//
+//   console.log('Creating relief from penalty GA app as claimant');
+//   await createGASteps.askForReliefFromAPenaltyGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'withoutnotice');
+//
+//   console.log('Creating change claim or defence GA app as claimant');
+//   await createGASteps.askToChangeSubmittedGA(claimRef, 'Miss Jane Doe v Sir John Doe', 'withoutnotice');
+//
+//   console.log('Creating multiple applications as claimant');
+//   await createGASteps.createMultipleApplications(claimRef, 'Miss Jane Doe v Sir John Doe', 'consent');
 });
 
-Scenario('LipvLip Defendant GA creation tests', async ({I}) => {
+Scenario.skip('LipvLip Defendant GA creation tests', async ({I}) => {
   await LoginSteps.EnterCitizenCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
 
   console.log('Creating summary judgment GA app as defendant');
