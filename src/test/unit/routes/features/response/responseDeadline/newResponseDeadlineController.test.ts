525d383ddd50973f9c6f159186549dce81505ab9
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2026-04-17 11:12:50 +0100

    DTSCCI-4249: update the unit test

--- src/test/unit/routes/features/response/responseDeadline/newResponseDeadlineController.test.ts
+++ src/test/unit/routes/features/response/responseDeadline/newResponseDeadlineController.test.ts
@@ -6,7 +6,6 @@ import {Claim} from '../../../../../../main/common/models/claim';
 import {RESPONSE_TASK_LIST_URL, NEW_RESPONSE_DEADLINE_URL} from '../../../../../../main/routes/urls';
 import {PartyType} from '../../../../../../main/common/models/partyType';
 import {TestMessages} from '../../../../../utils/errorMessageTestConstants';
-import {AppSession, UserDetails} from 'models/AppRequest';
 import {
   getClaimWithExtendedResponseDeadline,
   submitExtendedResponseDeadline,
@@ -23,6 +22,9 @@ const mockSubmitExtendedResponseDeadline = submitExtendedResponseDeadline as jes
 describe('Response - New response deadline', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamServiceUrl: string = config.get('services.idam.url');
+  const claimId = '1234';
+  const newResponseDeadlineUrl = NEW_RESPONSE_DEADLINE_URL.replace(':id', claimId);
+  const responseTaskListUrl = RESPONSE_TASK_LIST_URL.replace(':id', claimId);
   const extendedDate = new Date(2022, 9, 31);
   const claim = new Claim();
   claim.applicant1 = {
@@ -47,17 +49,17 @@ describe('Response - New response deadline', () => {
       const expectedDate = '31 October 2022';
 
       mockGetClaimWithExtendedResponseDeadline.mockResolvedValue(claim);
-      await request(app).get(NEW_RESPONSE_DEADLINE_URL)
+      await request(app).get(newResponseDeadlineUrl)
         .expect((res) => {
           expect(res.status).toBe(200);
           expect(res.text).toContain('New response deadline');
           expect(res.text).toContain(expectedDate);
+          expect(res.text).toContain(claim.getClaimantFullName());
         });
     });
     it('should show error when proposed extended deadline does not exist', async () => {
-      claim.responseDeadline.agreedResponseDeadline = undefined;
       mockGetClaimWithExtendedResponseDeadline.mockRejectedValue(new Error('No extended response deadline found'));
-      await request(app).get(NEW_RESPONSE_DEADLINE_URL)
+      await request(app).get(newResponseDeadlineUrl)
         .expect((res) => {
           expect(res.status).toBe(500);
           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
@@ -65,7 +67,7 @@ describe('Response - New response deadline', () => {
     });
     it('should show error when draft store throws error', async () => {
       mockGetClaimWithExtendedResponseDeadline.mockRejectedValue(new Error(TestMessages.REDIS_FAILURE));
-      await request(app).get(NEW_RESPONSE_DEADLINE_URL)
+      await request(app).get(newResponseDeadlineUrl)
         .expect((res) => {
           expect(res.status).toBe(500);
           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
@@ -75,18 +77,16 @@ describe('Response - New response deadline', () => {
   describe('On Post', () => {
     it('should redirect to task list successfully', async () => {
       mockSubmitExtendedResponseDeadline.mockResolvedValue(undefined);
-      app.request.session = <AppSession>{user: <UserDetails>{id: '1234'}};
 
-      await request(app).post(NEW_RESPONSE_DEADLINE_URL)
+      await request(app).post(newResponseDeadlineUrl)
         .expect((res) => {
           expect(res.status).toBe(302);
-          expect(res.header.location).toBe(RESPONSE_TASK_LIST_URL);
+          expect(res.header.location).toBe(responseTaskListUrl);
         });
     });
     it('should show error when there is error with the call to submit event', async () => {
       mockSubmitExtendedResponseDeadline.mockRejectedValue(new Error('error'));
-      app.request.session = <AppSession>{user: <UserDetails>{id: '1234'}};
-      await request(app).post(NEW_RESPONSE_DEADLINE_URL)
+      await request(app).post(newResponseDeadlineUrl)
         .expect((res) => {
           expect(res.status).toBe(500);
           expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
