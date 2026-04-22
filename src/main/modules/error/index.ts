f690b9792c52945d0226fafb5420f8b2bf110348
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2026-04-22 10:23:30 +0100

    DTSCCI-5220: fix the build

--- src/main/modules/error/index.ts
+++ src/main/modules/error/index.ts
@@ -30,7 +30,7 @@ export class ErrorHandler {
         });
         appInsightsClient?.flush({
           isAppCrashing: false,
-          callback: () => undefined,
+          callback: (): void => {},
         });
       }
 
