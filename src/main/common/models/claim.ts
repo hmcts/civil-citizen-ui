f13273a10fe8f5369979b8e92c32256aebaf5327
douglasrice <Douglas.rice@hmcts.net>
2026-04-28 13:45:14 +0100

    Update claim.ts

--- src/main/common/models/claim.ts
+++ src/main/common/models/claim.ts
@@ -822,7 +822,7 @@ export class Claim {
   private getSoeTraderPartyName(party: Party): string {
     const partyName = this.getIndividualPartyName(party);
     if (party.partyDetails?.soleTraderTradingAs) {
-      return `${partyName} ` + 'T/A' + `${party.partyDetails?.soleTraderTradingAs}` ;
+      return `${partyName} ` + 'T\/A' + `${party.partyDetails?.soleTraderTradingAs}` ;
     }
     return partyName;
   }
