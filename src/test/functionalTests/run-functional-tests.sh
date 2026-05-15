fb9ec47dea011b72a235edb804b5a056b2beb3fb
Pavithran Vangiti <pavithran.vangiti@hmcts.net>
2026-05-15 13:12:30 +0100

    Add provision to skip functional tests using git label on PRs

--- src/test/functionalTests/run-functional-tests.sh
+++ src/test/functionalTests/run-functional-tests.sh
@@ -80,6 +80,13 @@ run_failed_not_executed_functional_tests() {
 TEST_FILES_REPORT="test-results/functional/testFilesReport.json"
 PREV_TEST_FILES_REPORT="test-results/functional/prevTestFilesReport.json"
 
+# Check if SKIP_FUNCTIONAL_TESTS is set to true
+if [ "$SKIP_FUNCTIONAL_TESTS" = "true" ]; then
+  echo "The label 'pr-values: skip-functional-tests' exists on the PR."
+  echo "Skipping functional tests."
+  exit 0
+fi
+
 #Check if RUN_ALL_FUNCTIONAL_TESTS is set to true
 if [ "$RUN_ALL_FUNCTIONAL_TESTS" = "true" ]; then
   echo "The label 'runAllFunctionalTests' exists on the PR."
