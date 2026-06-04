fb8028e2449cc1a8148ac7e46bc758dabe358aec
Shaun Francis <shaun.francis@hmcts.net>
2026-06-04 11:40:32 +0100

    Fix pull-latest-camunda-files.sh to clone camunda processes from civil-service repo

--- bin/pull-latest-camunda-files.sh
+++ bin/pull-latest-camunda-files.sh
@@ -2,19 +2,19 @@
 
 branchName=$1
 
-#Checkout specific branch pf  civil camunda bpmn definition 
-git clone https://github.com/hmcts/civil-camunda-bpmn-definition.git
+#Checkout specific branch pf  civil camunda bpmn definition
+git clone https://github.com/hmcts/civil-service.git
 cd civil-camunda-bpmn-definition
 
-echo "Switch to ${branchName} branch on civil-camunda-bpmn-definition"
+echo "Switch to ${branchName} branch on civil-service"
 git checkout ${branchName}
 cd ..
 mkdir civil-bpmn
 
 #Copy camunda folder to civil-ccd-def which contians bpmn files
-cp -r ./civil-camunda-bpmn-definition/src/main/resources/camunda ./civil-bpmn/.
-rm -rf ./civil-camunda-bpmn-definition
+cp -r ./civil-service/src/main/resources/camunda ./civil-bpmn/.
+rm -rf ./civil-service
 
-#upload bpmn files to environment      
+#upload bpmn files to environment
 ./bin/import-bpmn-diagram.sh ./civil-bpmn/.
 rm -rf ./civil-bpmn.
