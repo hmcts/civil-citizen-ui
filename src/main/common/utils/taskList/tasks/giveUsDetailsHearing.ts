1ea9967baa4a2b0598d01dff2781acfc4d176ac2
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2026-06-08 09:07:35 +0100

    DTSCCI-129 changes

--- src/main/common/utils/taskList/tasks/giveUsDetailsHearing.ts
+++ src/main/common/utils/taskList/tasks/giveUsDetailsHearing.ts
@@ -10,6 +10,7 @@ import {
   DQ_TRIED_TO_SETTLE_CLAIM_URL,
 } from 'routes/urls';
 import {isIntermediateTrack, isMultiTrack} from 'form/models/claimType';
+import {Hearing} from 'models/directionsQuestionnaire/hearing/hearing';
 
 const checkTaskCompleteForTrack = (claim: Claim, defendantDQ: DirectionQuestionnaire, mintiApplicable: boolean): boolean => {
   if (claim.isSmallClaimsTrackDQ) {
@@ -24,6 +25,7 @@ const checkTaskCompleteForTrack = (claim: Claim, defendantDQ: DirectionQuestionn
 
 export const getGiveUsDetailsHearingTask = (claim: Claim, claimId: string, lang: string, mintiApplicable: boolean): Task => {
   const defendantDQ = Object.assign(new DirectionQuestionnaire(), claim.directionQuestionnaire);
+  defendantDQ.hearing = Object.assign(new Hearing(), claim.directionQuestionnaire?.hearing);
   const linkUrl = !claim.isSmallClaimsTrackDQ ? DQ_TRIED_TO_SETTLE_CLAIM_URL : DETERMINATION_WITHOUT_HEARING_URL;
   const isTaskCompleted = checkTaskCompleteForTrack(claim, defendantDQ, mintiApplicable);
   return {
