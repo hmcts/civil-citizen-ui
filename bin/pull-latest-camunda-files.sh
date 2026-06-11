af9d24b038a6fbd488a7b76f333d4b57c33060a2
Mike Lemos <michael.lemos1@hmcts.net>
2026-06-11 16:32:44 +0100

    Revert "Temporarily revert PR changes for vanilla pipeline run"
    This reverts commit 54f6177246804c5f5a8b8f1f912fe21277c516a3.

--- bin/pull-latest-camunda-files.sh
+++ bin/pull-latest-camunda-files.sh
@@ -3,17 +3,17 @@
 branchName=$1
 
 #Checkout specific branch pf  civil camunda bpmn definition
-git clone https://github.com/hmcts/civil-service.git
-cd civil-service
+git clone https://github.com/hmcts/civil-camunda-bpmn-definition.git
+cd civil-camunda-bpmn-definition
 
-echo "Switch to ${branchName} branch on civil-service"
+echo "Switch to ${branchName} branch on civil-camunda-bpmn-definition"
 git checkout ${branchName}
 cd ..
 mkdir civil-bpmn
 
 #Copy camunda folder to civil-ccd-def which contians bpmn files
-cp -r ./civil-service/src/main/resources/camunda ./civil-bpmn/.
-rm -rf ./civil-service
+cp -r ./civil-camunda-bpmn-definition/src/main/resources/camunda ./civil-bpmn/.
+rm -rf ./civil-camunda-bpmn-definition
 
 #upload bpmn files to environment
 ./bin/import-bpmn-diagram.sh ./civil-bpmn/.
