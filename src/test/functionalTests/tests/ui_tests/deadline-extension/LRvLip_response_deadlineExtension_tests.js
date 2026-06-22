a4a39d678f5a524911890fb8ce62b52a7ec42715
MaharukhPatel <maharukh.patel@hmcts.net>
2026-06-22 15:23:00 +0100

    DTSCCI-5429: Add notification assertions for DefResponse.MoretimeRequested & DefResponse.FullDefence.AlreadyPaid.

--- src/test/functionalTests/tests/ui_tests/deadline-extension/LRvLip_response_deadlineExtension_tests.js
+++ src/test/functionalTests/tests/ui_tests/deadline-extension/LRvLip_response_deadlineExtension_tests.js
@@ -1,35 +1,102 @@
-const config = require('../../../../config');
-const ResponseSteps = require('../../../citizenFeatures/response/steps/lipDefendantResponseSteps');
-const LoginSteps = require('../../../commonFeatures/home/steps/login');
-const CitizenDashboardSteps = require('../../../citizenFeatures/citizenDashboard/steps/citizenDashboard');
-const {createAccount} = require('../../../specClaimHelpers/api/idamHelper');
-
-const iHaveAlreadyAgreedMoretime = 'iHaveAlreadyAgreedMoretime';
-
+const config = require("../../../../config");
+const ResponseSteps = require("../../../citizenFeatures/response/steps/lipDefendantResponseSteps");
+const LoginSteps = require("../../../commonFeatures/home/steps/login");
+const CitizenDashboardSteps = require("../../../citizenFeatures/citizenDashboard/steps/citizenDashboard");
+const { createAccount } = require("../../../specClaimHelpers/api/idamHelper");
+const {
+  verifyNotificationTitleAndContent,
+} = require("../../../specClaimHelpers/e2e/dashboardHelper");
+const I = actor();
+const iHaveAlreadyAgreedMoretime = "iHaveAlreadyAgreedMoretime";
 let claimRef;
 let caseData;
 let claimNumber;
 let securityCode;
+const {
+  defendantNotificationMoreTimeRequested,
+} = require("../../../specClaimHelpers/dashboardNotificationConstants");
 
-Feature('Extended Response Time').tag('@civil-citizen-nightly @ui-deadline-extension');
+Feature("Extended Response Time").tag(
+  "@civil-citizen-nightly @ui-deadline-extension",
+);
 
-Before(async ({api}) => {
-  await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+Before(async ({ api }) => {
+  await createAccount(
+    config.defendantCitizenUser.email,
+    config.defendantCitizenUser.password,
+  );
   claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser);
-  console.log('Claim has been created Successfully    <===>  ', claimRef);
+  console.log("Claim has been created Successfully    <===>  ", claimRef);
   caseData = await api.retrieveCaseData(config.adminUser, claimRef);
   claimNumber = await caseData.legacyCaseReference;
   securityCode = await caseData.respondent1PinToPostLRspec.accessCode;
-  console.log('claim number', claimNumber);
-  console.log('Security code', securityCode);
+  console.log("claim number", claimNumber);
+  console.log("Security code", securityCode);
   await ResponseSteps.AssignCaseToLip(claimNumber, securityCode);
-  await LoginSteps.EnterCitizenCredentials(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+  await LoginSteps.EnterCitizenCredentials(
+    config.defendantCitizenUser.email,
+    config.defendantCitizenUser.password,
+  );
   await CitizenDashboardSteps.VerifyClaimOnDashboard(claimNumber);
 });
 
 //Skipped dependant on the release DTSCCI-2558
-Scenario.skip('No response submitted, date agreed upon request time', async () => {
+Scenario("No response submitted, date agreed upon request time", async () => {
   await ResponseSteps.RespondToClaim(claimRef);
-  await ResponseSteps.EnterYourOptionsForDeadline(claimRef, iHaveAlreadyAgreedMoretime);
+  await ResponseSteps.EnterYourOptionsForDeadline(
+    claimRef,
+    iHaveAlreadyAgreedMoretime,
+  );
   await ResponseSteps.DefendantSummaryPage(claimRef);
+
+  // notification verification
+  await I.refreshPage();
+  await I.waitForElement(".dashboard-notification", 10);
+
+  const moreTimeBanner = locate(".govuk-notification-banner").withText(
+    "More time requested",
+  );
+  await I.waitForElement(moreTimeBanner, 10);
+  const notificationText = await I.grabTextFrom(moreTimeBanner);
+  const normalisedNotificationText = notificationText
+    .replace(/\s+/g, " ")
+    .trim();
+  const match = normalisedNotificationText.match(
+    /The response deadline is now (.+?) on (.+?)\. There are (\d+) days remaining/,
+  );
+  if (!match) {
+    throw new Error(
+      `Could not extract deadline details from notification text: ${normalisedNotificationText}`,
+    );
+  }
+
+  const deadlineTime = match[1];
+  const deadlineDate = match[2];
+  const daysToRespond = Number(match[3]);
+
+  console.log("Extracted deadline values:", {
+    deadlineTime,
+    deadlineDate,
+    daysToRespond,
+  });
+
+  const defendantNotification = defendantNotificationMoreTimeRequested(
+    deadlineTime,
+    deadlineDate,
+    daysToRespond,
+  );
+
+  console.log(
+    "Defendant NotificationdefendantNotification.title:",
+    defendantNotification.title,
+  );
+  console.log(
+    "Defendant NotificationdefendantNotification.content:",
+    defendantNotification.content,
+  );
+  await verifyNotificationTitleAndContent(
+    claimNumber,
+    defendantNotification.title,
+    defendantNotification.content,
+  );
 });
--- src/test/functionalTests/tests/ui_tests/reject-all/LipvLip_RejectAll_AlreadyPaid_InFull_tests.js
+++ src/test/functionalTests/tests/ui_tests/reject-all/LipvLip_RejectAll_AlreadyPaid_InFull_tests.js
@@ -1,56 +1,207 @@
-const config = require('../../../../config');
-const { createAccount } = require('../../../specClaimHelpers/api/idamHelper');
-const LoginSteps = require('../../../commonFeatures/home/steps/login');
-const ResponseToDefenceLipVsLipSteps = require('../../../citizenFeatures/response/steps/responseToDefenceLipvLipSteps');
-const nocSteps = require('../../../lrFeatures/noc/steps/nocSteps');
-const { nocForLip } = require('../../../specClaimHelpers/dashboardNotificationConstants');
-const { verifyNotificationTitleAndContent } = require('../../../specClaimHelpers/e2e/dashboardHelper');
+const config = require("../../../../config");
+const { createAccount } = require("../../../specClaimHelpers/api/idamHelper");
+const LoginSteps = require("../../../commonFeatures/home/steps/login");
+const ResponseToDefenceLipVsLipSteps = require("../../../citizenFeatures/response/steps/responseToDefenceLipvLipSteps");
+const nocSteps = require("../../../lrFeatures/noc/steps/nocSteps");
+const {
+  nocForLip,
+} = require("../../../specClaimHelpers/dashboardNotificationConstants");
+const {
+  verifyNotificationTitleAndContent,
+} = require("../../../specClaimHelpers/e2e/dashboardHelper");
 // eslint-disable-next-line no-unused-vars
-const yesIWantMoretime = 'yesIWantMoretime';
-
+const yesIWantMoretime = "yesIWantMoretime";
+const {
+  defendantResponseFullDefenceAlreadyPaid,
+} = require("../../../specClaimHelpers/dashboardNotificationConstants");
 let claimRef, claimType;
 let caseData;
 let claimNumber, defendantName;
 
-Feature('Response with RejectAll-AlreadyPaid-InFull - Small Claims & Fast Track').tag('@civil-citizen-nightly @ui-reject-all');
+Feature(
+  "Response with RejectAll-AlreadyPaid-InFull - Small Claims & Fast Track",
+).tag("@civil-citizen-nightly @ui-reject-all");
 
-Scenario('Response with RejectAll-AlreadyPaid-InFull Small claims and Claimant settle', async ({ api }) => {
-  await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-  await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
-  claimType = 'SmallClaims';
-  claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
-  caseData = await api.retrieveCaseData(config.adminUser, claimRef);
-  claimNumber = await caseData.legacyCaseReference;
-  await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.rejectAllAlreadyPaidInFullWithIndividual);
-  await api.waitForFinishedBusinessProcess();
-  await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-  // One of the step in the below method is commented until https://tools.hmcts.net/jira/browse/CIV-13496 is fixed
-  await ResponseToDefenceLipVsLipSteps.ResponseToDefenceStepsAsAnAcceptanceOfFullDefenceAlreadyPaidInFull(claimRef, claimNumber);
-  await api.waitForFinishedBusinessProcess();
-});
+Scenario(
+  "Response with RejectAll-AlreadyPaid-InFull Small claims and Claimant settle",
+  async ({ I, api }) => {
+    await createAccount(
+      config.claimantCitizenUser.email,
+      config.claimantCitizenUser.password,
+    );
+    await createAccount(
+      config.defendantCitizenUser.email,
+      config.defendantCitizenUser.password,
+    );
+    claimType = "SmallClaims";
+    claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber = await caseData.legacyCaseReference;
+    await api.performCitizenResponse(
+      config.defendantCitizenUser,
+      claimRef,
+      claimType,
+      config.defenceType.rejectAllAlreadyPaidInFullWithIndividual,
+    );
+    await api.waitForFinishedBusinessProcess();
+    await LoginSteps.EnterCitizenCredentials(
+      config.claimantCitizenUser.email,
+      config.claimantCitizenUser.password,
+    );
+    // check notification
+    await I.amOnPage("/dashboard");
+    await I.click(claimNumber);
+    await I.amOnPage("/dashboard/" + claimRef + "/claimantNewDesign");
+    await I.refreshPage();
+    await I.waitForElement(".dashboard-notification", 10);
+    const notificationBanner = locate(".govuk-notification-banner").withText(
+      "Response to the claim",
+    );
+    await I.waitForElement(notificationBanner, 10);
+    const notificationText = await I.grabTextFrom(notificationBanner);
+    const normalisedNotificationText = notificationText
+      .replace(/\s+/g, " ")
+      .trim();
+    const match = normalisedNotificationText.match(
+      /The defendant has said they already paid (.+?) on (.+?)\. You can confirm payment and settle, or proceed with the claim\. You need to respond by (.+?) on (.+?) or the claim will not continue\./,
+    );
+    if (!match) {
+      throw new Error(
+        `Could not extract payment and deadline details from notification text: ${normalisedNotificationText}`,
+      );
+    }
+    const claimSettledAmount = match[1];
+    const claimSettledDateEn = match[2];
+    const defaultRespondTime = match[3];
+    const applicant1ResponseDeadlineEn = match[4];
+    console.log("Extracted notification values:", {
+      claimSettledAmount,
+      claimSettledDateEn,
+      defaultRespondTime,
+      applicant1ResponseDeadlineEn,
+    });
+    const defendantNotification = defendantResponseFullDefenceAlreadyPaid(
+      claimSettledAmount,
+      claimSettledDateEn,
+      defaultRespondTime,
+      applicant1ResponseDeadlineEn,
+    );
+    console.log("title:", defendantNotification.title);
+    console.log("content:", defendantNotification.content);
+    await verifyNotificationTitleAndContent(
+      claimNumber,
+      defendantNotification.title,
+      defendantNotification.content,
+    );
+    // One of the step in the below method is commented until https://tools.hmcts.net/jira/browse/CIV-13496 is fixed
+    await ResponseToDefenceLipVsLipSteps.ResponseToDefenceStepsAsAnAcceptanceOfFullDefenceAlreadyPaidInFull(
+      claimRef,
+      claimNumber,
+    );
+    await api.waitForFinishedBusinessProcess();
+  },
+);
 
-Scenario('Response with RejectAll-AlreadyPaid-InFull Fast Track and Claimant proceeds', async ({ I, api }) => {
-  await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-  await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
-  claimType = 'FastTrack';
-  claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
-  caseData = await api.retrieveCaseData(config.adminUser, claimRef);
-  claimNumber = await caseData.legacyCaseReference;
-  defendantName = await caseData.respondent1.partyName;
-  await api.performCitizenResponse(config.defendantCitizenUser, claimRef, claimType, config.defenceType.rejectAllAlreadyPaidInFullWithIndividual);
-  await api.waitForFinishedBusinessProcess();
-  await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-  await ResponseToDefenceLipVsLipSteps.ResponseToDefenceStepsAsAnRejectionOfFullDefenceAlreadyPaidInFull(claimRef, claimNumber);
-  await api.waitForFinishedBusinessProcess();
-  await nocSteps.requestNoticeOfChangeForRespondent1Solicitor(claimRef, defendantName, config.defendantSolicitorUser);
-  await api.checkUserCaseAccess(config.defendantCitizenUser, false);
-  await api.checkUserCaseAccess(config.defendantSolicitorUser, true);
+Scenario(
+  "Response with RejectAll-AlreadyPaid-InFull Fast Track and Claimant proceeds",
+  async ({ I, api }) => {
+    await createAccount(
+      config.claimantCitizenUser.email,
+      config.claimantCitizenUser.password,
+    );
+    await createAccount(
+      config.defendantCitizenUser.email,
+      config.defendantCitizenUser.password,
+    );
+    claimType = "FastTrack";
+    claimRef = await api.createLiPClaim(config.claimantCitizenUser, claimType);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber = await caseData.legacyCaseReference;
+    defendantName = await caseData.respondent1.partyName;
+    await api.performCitizenResponse(
+      config.defendantCitizenUser,
+      claimRef,
+      claimType,
+      config.defenceType.rejectAllAlreadyPaidInFullWithIndividual,
+    );
+    await api.waitForFinishedBusinessProcess();
+    await LoginSteps.EnterCitizenCredentials(
+      config.claimantCitizenUser.email,
+      config.claimantCitizenUser.password,
+    );
 
-  await LoginSteps.EnterCitizenCredentials(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
-  await I.amOnPage('/dashboard');
-  await I.click(claimNumber);
+    // check notification
+    await I.amOnPage("/dashboard");
+    await I.click(claimNumber);
+    await I.amOnPage("/dashboard/" + claimRef + "/claimantNewDesign");
+    await I.refreshPage();
+    await I.waitForElement(".dashboard-notification", 10);
+    const notificationBanner = locate(".govuk-notification-banner").withText(
+      "Response to the claim",
+    );
+    await I.waitForElement(notificationBanner, 10);
+    const notificationText = await I.grabTextFrom(notificationBanner);
+    const normalisedNotificationText = notificationText
+      .replace(/\s+/g, " ")
+      .trim();
+    const match = normalisedNotificationText.match(
+      /The defendant has said they already paid (.+?) on (.+?)\. You can confirm payment and settle, or proceed with the claim\. You need to respond by (.+?) on (.+?) or the claim will not continue\./,
+    );
+    if (!match) {
+      throw new Error(
+        `Could not extract payment and deadline details from notification text: ${normalisedNotificationText}`,
+      );
+    }
+    const claimSettledAmount = match[1];
+    const claimSettledDateEn = match[2];
+    const defaultRespondTime = match[3];
+    const applicant1ResponseDeadlineEn = match[4];
+    console.log("Extracted notification values:", {
+      claimSettledAmount,
+      claimSettledDateEn,
+      defaultRespondTime,
+      applicant1ResponseDeadlineEn,
+    });
+    const defendantNotification = defendantResponseFullDefenceAlreadyPaid(
+      claimSettledAmount,
+      claimSettledDateEn,
+      defaultRespondTime,
+      applicant1ResponseDeadlineEn,
+    );
+    console.log("title:", defendantNotification.title);
+    console.log("content:", defendantNotification.content);
+    await verifyNotificationTitleAndContent(
+      claimNumber,
+      defendantNotification.title,
+      defendantNotification.content,
+    );
 
-  const nocForLipNotif = nocForLip(defendantName);
-  await verifyNotificationTitleAndContent(claimNumber, nocForLipNotif.title, nocForLipNotif.content);
-  await I.click(nocForLipNotif.nextSteps);
-});
+    await ResponseToDefenceLipVsLipSteps.ResponseToDefenceStepsAsAnRejectionOfFullDefenceAlreadyPaidInFull(
+      claimRef,
+      claimNumber,
+    );
+    await api.waitForFinishedBusinessProcess();
+    await nocSteps.requestNoticeOfChangeForRespondent1Solicitor(
+      claimRef,
+      defendantName,
+      config.defendantSolicitorUser,
+    );
+    await api.checkUserCaseAccess(config.defendantCitizenUser, false);
+    await api.checkUserCaseAccess(config.defendantSolicitorUser, true);
+
+    await LoginSteps.EnterCitizenCredentials(
+      config.claimantCitizenUser.email,
+      config.claimantCitizenUser.password,
+    );
+    await I.amOnPage("/dashboard");
+    await I.click(claimNumber);
+
+    const nocForLipNotif = nocForLip(defendantName);
+    await verifyNotificationTitleAndContent(
+      claimNumber,
+      nocForLipNotif.title,
+      nocForLipNotif.content,
+    );
+    await I.click(nocForLipNotif.nextSteps);
+  },
+);
