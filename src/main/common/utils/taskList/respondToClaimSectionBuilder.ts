998486728fa788211b8f35beb363beeeb710d830
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2026-07-09 14:00:37 +0100

    DTSCCI3843: update the tasklistBuilder and mov ethe respond to claim

---
+++ src/main/common/utils/taskList/respondToClaimSectionBuilder.ts
@@ -0,0 +1,164 @@
+import {Claim} from 'models/claim';
+import {Task} from 'models/taskList/task';
+import {TaskStatus} from 'models/taskList/TaskStatus';
+import {ResponseType} from 'common/form/models/responseType';
+import {YesNo} from 'common/form/models/yesNo';
+import {PaymentOptionType} from 'common/form/models/admission/paymentOption/paymentOptionType';
+import {RejectAllOfClaimType} from 'form/models/rejectAllOfClaimType';
+import {isNotPayImmediatelyResponse} from './tasks/taskListHelpers';
+import {getChooseAResponseTask} from './tasks/chooseAResponse';
+import {getDecideHowYouPayTask} from './tasks/decideHowYouPay';
+import {getShareFinancialDetailsTask} from './tasks/shareFinancialDetails';
+import {getRepaymentPlanTask} from './tasks/repaymentPlan';
+import {getHowMuchHaveYouPaidTask} from './tasks/howMuchHaveYouPaid';
+import {getHowMuchMoneyAdmitOweTask} from './tasks/howMuchMoneyAdmitOwe';
+import {getWhenWillYouPayTask} from './tasks/whenWillYouPay';
+import {getTellUsHowMuchYouHavePaidTask} from './tasks/tellUsHowMuchYouHavePaid';
+import {getTellUsWhyDisagreeWithClaimTask} from './tasks/tellUsWhyDisagreeWithClaim';
+import {getWhyDisagreeWithAmountClaimedTask} from './tasks/whyDisagreeWithAmountClaimed';
+
+type RespondToClaimTasks = {
+  chooseAResponseTask: Task;
+  decideHowYouPayTask: Task;
+  shareFinancialDetailsTask: Task;
+  repaymentPlanTask: Task;
+  howMuchHaveYouPaidTask: Task;
+  howMuchMoneyAdmitOweTask: Task;
+  whenWillYouPayTask: Task;
+  tellUsHowMuchYouHavePaidTask: Task;
+  tellUsWhyDisagreeWithClaimTask: Task;
+};
+
+const getRespondToClaimTasks = (caseData: Claim, claimId: string, lang: string): RespondToClaimTasks => ({
+  chooseAResponseTask: getChooseAResponseTask(caseData, claimId, lang),
+  decideHowYouPayTask: getDecideHowYouPayTask(caseData, claimId, lang),
+  shareFinancialDetailsTask: getShareFinancialDetailsTask(caseData, claimId, lang),
+  repaymentPlanTask: getRepaymentPlanTask(caseData, claimId, lang),
+  howMuchHaveYouPaidTask: getHowMuchHaveYouPaidTask(caseData, claimId, lang),
+  howMuchMoneyAdmitOweTask: getHowMuchMoneyAdmitOweTask(caseData, claimId, lang),
+  whenWillYouPayTask: getWhenWillYouPayTask(caseData, claimId, lang),
+  tellUsHowMuchYouHavePaidTask: getTellUsHowMuchYouHavePaidTask(caseData, claimId, lang),
+  tellUsWhyDisagreeWithClaimTask: getTellUsWhyDisagreeWithClaimTask(caseData, claimId, lang),
+});
+
+const isRejectAllAndCounterClaim = (caseData: Claim): boolean => {
+  return caseData.rejectAllOfClaim?.option === RejectAllOfClaimType.COUNTER_CLAIM;
+};
+
+const isChooseAResponseComplete = (task: Task): boolean => {
+  return task.status === TaskStatus.COMPLETE;
+};
+
+const hasPartialAdmissionAlreadyPaid = (caseData: Claim): boolean => {
+  return caseData.partialAdmission?.alreadyPaid?.option === YesNo.YES;
+};
+
+const hasPartialAdmissionNotPaid = (caseData: Claim): boolean => {
+  return caseData.partialAdmission?.alreadyPaid?.option === YesNo.NO;
+};
+
+const hasPartialAdmissionAmountOwed = (caseData: Claim): boolean => {
+  return !!caseData.partialAdmission?.howMuchDoYouOwe?.amount;
+};
+
+const isPartialAdmissionBySetDate = (caseData: Claim): boolean => {
+  return caseData.partialAdmission?.paymentIntention?.paymentOption === PaymentOptionType.BY_SET_DATE
+    && !!caseData.partialAdmission?.paymentIntention?.paymentDate;
+};
+
+const isPartialAdmissionInstallments = (caseData: Claim): boolean => {
+  return caseData.partialAdmission?.paymentIntention?.paymentOption === PaymentOptionType.INSTALMENTS;
+};
+
+const shouldShowFullDefenceWhyDisagreeAmount = (caseData: Claim): boolean => {
+  return caseData.rejectAllOfClaim?.howMuchHaveYouPaid?.amount < caseData.totalClaimAmount;
+};
+
+const applyCounterClaimRule = (caseData: Claim, chooseAResponseTask: Task): void => {
+  if (isRejectAllAndCounterClaim(caseData)) {
+    chooseAResponseTask.status = TaskStatus.INCOMPLETE;
+  }
+};
+
+const buildFullAdmissionTasks = (caseData: Claim, tasks: RespondToClaimTasks): Task[] => {
+  const fullAdmissionTasks: Task[] = [tasks.decideHowYouPayTask];
+
+  if (tasks.decideHowYouPayTask.status === TaskStatus.COMPLETE && isNotPayImmediatelyResponse(caseData)) {
+    fullAdmissionTasks.push(tasks.shareFinancialDetailsTask);
+    if (caseData.isFAPaymentOptionInstallments()) {
+      fullAdmissionTasks.push(tasks.repaymentPlanTask);
+    }
+  }
+
+  return fullAdmissionTasks;
+};
+
+const buildPartialAdmissionTasks = (caseData: Claim, claimId: string, lang: string, tasks: RespondToClaimTasks): Task[] => {
+  const partialAdmissionTasks: Task[] = [];
+  const whyDisagreeWithAmountClaimedTask = getWhyDisagreeWithAmountClaimedTask(caseData, claimId, ResponseType.PART_ADMISSION, lang);
+
+  if (hasPartialAdmissionAlreadyPaid(caseData)) {
+    partialAdmissionTasks.push(tasks.howMuchHaveYouPaidTask);
+  } else if (hasPartialAdmissionNotPaid(caseData)) {
+    partialAdmissionTasks.push(tasks.howMuchMoneyAdmitOweTask);
+    if (hasPartialAdmissionAmountOwed(caseData)) {
+      partialAdmissionTasks.push(tasks.whenWillYouPayTask);
+    }
+  }
+
+  if (isPartialAdmissionBySetDate(caseData)) {
+    partialAdmissionTasks.push(tasks.shareFinancialDetailsTask);
+  }
+
+  if (isPartialAdmissionInstallments(caseData)) {
+    partialAdmissionTasks.push(tasks.shareFinancialDetailsTask);
+    partialAdmissionTasks.push(tasks.repaymentPlanTask);
+  }
+
+  // Keep original ordering semantics from splice(2, 0, ...)
+  partialAdmissionTasks.splice(1, 0, whyDisagreeWithAmountClaimedTask);
+  return partialAdmissionTasks;
+};
+
+const buildFullDefenceTasks = (caseData: Claim, claimId: string, lang: string, tasks: RespondToClaimTasks): Task[] => {
+  const fullDefenceTasks: Task[] = [];
+
+  if (caseData.hasConfirmedAlreadyPaid()) {
+    fullDefenceTasks.push(tasks.tellUsHowMuchYouHavePaidTask);
+    if (shouldShowFullDefenceWhyDisagreeAmount(caseData)) {
+      fullDefenceTasks.push(getWhyDisagreeWithAmountClaimedTask(caseData, claimId, ResponseType.FULL_DEFENCE, lang));
+    }
+  } else if (caseData.isRejectAllOfClaimDispute()) {
+    fullDefenceTasks.push(tasks.tellUsWhyDisagreeWithClaimTask);
+  }
+
+  return fullDefenceTasks;
+};
+
+export const buildRespondToClaimTasks = (caseData: Claim, claimId: string, lang: string): Task[] => {
+  const tasks = getRespondToClaimTasks(caseData, claimId, lang);
+  applyCounterClaimRule(caseData, tasks.chooseAResponseTask);
+
+  const sectionTasks: Task[] = [tasks.chooseAResponseTask];
+  if (!isChooseAResponseComplete(tasks.chooseAResponseTask)) {
+    return sectionTasks;
+  }
+
+  if (caseData.isFullAdmission()) {
+    sectionTasks.push(...buildFullAdmissionTasks(caseData, tasks));
+  }
+
+  if (caseData.isPartialAdmission()) {
+    sectionTasks.push(...buildPartialAdmissionTasks(caseData, claimId, lang, tasks));
+  }
+
+  if (caseData.isFullDefence()) {
+    sectionTasks.push(...buildFullDefenceTasks(caseData, claimId, lang, tasks));
+  }
+
+  return sectionTasks;
+};
+
+export {
+  isRejectAllAndCounterClaim,
+};
--- src/main/common/utils/taskList/taskListBuilder.ts
+++ src/main/common/utils/taskList/taskListBuilder.ts
@@ -4,29 +4,18 @@ import {Claim} from 'models/claim';
 import {TaskStatus} from 'models/taskList/TaskStatus';
 import {getConfirmYourDetailsTask} from './tasks/confirmYourDetails';
 import {getViewOptionsBeforeDeadlineTask} from './tasks/viewOptionsBeforeDeadline';
-import {getChooseAResponseTask} from './tasks/chooseAResponse';
 import {getCheckAndSubmitYourResponseTask} from './tasks/checkAndSubmitYourResponse';
 import {isPastDeadline} from '../dateUtils';
-import {getDecideHowYouPayTask} from './tasks/decideHowYouPay';
-import {getShareFinancialDetailsTask} from './tasks/shareFinancialDetails';
-import {getRepaymentPlanTask} from './tasks/repaymentPlan';
-import {isFullDefenceAndNotCounterClaim, isNotPayImmediatelyResponse} from './tasks/taskListHelpers';
+import {isFullDefenceAndNotCounterClaim} from './tasks/taskListHelpers';
 import {ResponseType} from 'common/form/models/responseType';
-import {YesNo} from 'common/form/models/yesNo';
-import {getHowMuchHaveYouPaidTask} from './tasks/howMuchHaveYouPaid';
 import {getWhyDisagreeWithAmountClaimedTask} from './tasks/whyDisagreeWithAmountClaimed';
 import {getGiveUsDetailsHearingTask} from './tasks/giveUsDetailsHearing';
-import {getHowMuchMoneyAdmitOweTask} from './tasks/howMuchMoneyAdmitOwe';
 import {getFreeTelephoneMediationTask} from './tasks/freeTelephoneMediation';
-import {getWhenWillYouPayTask} from './tasks/whenWillYouPay';
-import {PaymentOptionType} from 'common/form/models/admission/paymentOption/paymentOptionType';
 import {getLng} from 'common/utils/languageToggleUtils';
 import {t} from 'i18next';
-import {getTellUsHowMuchYouHavePaidTask} from './tasks/tellUsHowMuchYouHavePaid';
-import {getTellUsWhyDisagreeWithClaimTask} from './tasks/tellUsWhyDisagreeWithClaim';
 import {getTelephoneMediationTask} from 'common/utils/taskList/tasks/telephoneMediation';
 import {getAvailabilityForMediationTask} from 'common/utils/taskList/tasks/availabilityForMediation';
-import {RejectAllOfClaimType} from 'form/models/rejectAllOfClaimType';
+import {buildRespondToClaimTasks, isRejectAllAndCounterClaim} from './respondToClaimSectionBuilder';
 
 const buildPrepareYourResponseSection = (caseData: Claim, claimId: string, lang: string, carmApplicable = false): TaskList => {
   const tasks: Task[] = [];
@@ -43,79 +32,8 @@ const buildPrepareYourResponseSection = (caseData: Claim, claimId: string, lang:
   return {title: t('TASK_LIST.PREPARE_YOUR_RESPONSE.TITLE', {lng: getLng(lang)}), tasks};
 };
 
-const isRejectAllAndCounterClaim = (caseData: Claim): boolean => {
-  return caseData.rejectAllOfClaim?.option === RejectAllOfClaimType.COUNTER_CLAIM;
-};
-
 const buildRespondToClaimSection = (caseData: Claim, claimId: string, lang: string): TaskList => {
-  const tasks: Task[] = [];
-  const chooseAResponseTask = getChooseAResponseTask(caseData, claimId, lang);
-  const decideHowYouPayTask = getDecideHowYouPayTask(caseData, claimId, lang);
-  const shareFinancialDetailsTask = getShareFinancialDetailsTask(caseData, claimId, lang);
-  const repaymentPlanTask = getRepaymentPlanTask(caseData, claimId, lang);
-  const howMuchHaveYouPaidTask = getHowMuchHaveYouPaidTask(caseData, claimId, lang);
-  const howMuchMoneyAdmitOweTask = getHowMuchMoneyAdmitOweTask(caseData, claimId, lang);
-  const whenWillYouPayTask = getWhenWillYouPayTask(caseData, claimId, lang);
-  const tellUsHowMuchYouHavePaidTask = getTellUsHowMuchYouHavePaidTask(caseData, claimId, lang);
-  const tellUsWhyDisagreeWithClaimTask = getTellUsWhyDisagreeWithClaimTask(caseData, claimId, lang);
-
-  if (isRejectAllAndCounterClaim(caseData)) {
-    chooseAResponseTask.status = TaskStatus.INCOMPLETE;
-  }
-
-  tasks.push(chooseAResponseTask);
-
-  if (chooseAResponseTask.status === TaskStatus.COMPLETE) {
-
-    if (caseData.isFullAdmission()) {
-      tasks.push(decideHowYouPayTask);
-
-      if (decideHowYouPayTask.status === TaskStatus.COMPLETE && isNotPayImmediatelyResponse(caseData)) {
-        tasks.push(shareFinancialDetailsTask);
-
-        if(caseData.isFAPaymentOptionInstallments()) {
-          tasks.push(repaymentPlanTask);
-        }
-      }
-    }
-
-    if (caseData.isPartialAdmission()) {
-      const whyDisagreeWithAmountClaimedTask = getWhyDisagreeWithAmountClaimedTask(caseData, claimId, ResponseType.PART_ADMISSION, lang);
-
-      if (caseData.partialAdmission?.alreadyPaid?.option === YesNo.YES) {
-        tasks.push(howMuchHaveYouPaidTask);
-      } else if (caseData.partialAdmission?.alreadyPaid?.option === YesNo.NO) {
-        tasks.push(howMuchMoneyAdmitOweTask);
-        if (caseData.partialAdmission?.howMuchDoYouOwe?.amount) {
-          tasks.push(whenWillYouPayTask);
-        }
-      }
-
-      if (caseData.partialAdmission?.paymentIntention?.paymentOption === PaymentOptionType.BY_SET_DATE && caseData.partialAdmission?.paymentIntention?.paymentDate) {
-        tasks.push(shareFinancialDetailsTask);
-      }
-
-      if (caseData.partialAdmission?.paymentIntention?.paymentOption === PaymentOptionType.INSTALMENTS) {
-        tasks.push(shareFinancialDetailsTask);
-        tasks.push(repaymentPlanTask);
-      }
-
-      tasks.splice(2, 0, whyDisagreeWithAmountClaimedTask);
-    }
-
-    if (caseData.isFullDefence()) {
-      if (caseData.hasConfirmedAlreadyPaid()) {
-        tasks.push(tellUsHowMuchYouHavePaidTask);
-        if (caseData.rejectAllOfClaim?.howMuchHaveYouPaid?.amount < caseData.totalClaimAmount) {
-          const whyDisagreeWithAmountClaimedTask = getWhyDisagreeWithAmountClaimedTask(caseData, claimId, ResponseType.FULL_DEFENCE, lang);
-          tasks.push(whyDisagreeWithAmountClaimedTask);
-        }
-      } else if (caseData.isRejectAllOfClaimDispute()) {
-        tasks.push(tellUsWhyDisagreeWithClaimTask);
-      }
-    }
-  }
-
+  const tasks = buildRespondToClaimTasks(caseData, claimId, lang);
   return {title: t('TASK_LIST.RESPOND_TO_CLAIM.TITLE', {lng: getLng(lang)}), tasks};
 };
 
