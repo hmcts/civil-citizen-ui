127b435e9c3e73e3f84cfd01c57298c31058b2f2
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-07-27 14:37:12 +0100

    Corrected test to ensure failure for testing

--- src/test/functionalTests/tests/ui_tests/diagnostics/DTSCCI5976_controlled_failure_tests.js
+++ src/test/functionalTests/tests/ui_tests/diagnostics/DTSCCI5976_controlled_failure_tests.js
@@ -3,5 +3,5 @@ Feature('DTSCCI-5976 controlled functional diagnostics failure')
 
 Scenario('DTSCCI-5976 controlled browser assertion failure for diagnostic evidence', async ({ I }) => {
   await I.amOnPage('/');
-  await I.waitForContent('DTSCCI-5976 intentionally missing content', 1);
+  throw new Error('DTSCCI-5976 controlled functional diagnostics failure');
 });
