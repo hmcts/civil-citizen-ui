89f2f047ffa850cafdb51569b765d089ff567540
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2026-07-28 13:56:13 +0100

    DTSCCI-5475 changes

--- src/main/common/form/models/statementOfMeans/employment/employers.ts
+++ src/main/common/form/models/statementOfMeans/employment/employers.ts
@@ -1,9 +1,7 @@
-import {Validate, ValidateNested} from 'class-validator';
+import {ValidateNested} from 'class-validator';
 import {Employer} from './employer';
-import {AtLeastOneEmployerValidator} from '../../../../../common/form/validators/atLeastOneEmployerValidator';
 
 export class Employers {
-  @Validate(AtLeastOneEmployerValidator, { message: 'ERRORS.VALID_ENTER_AT_LEAST_ONE_EMPLOYER' })
   @ValidateNested()
     rows: Employer[];
 
