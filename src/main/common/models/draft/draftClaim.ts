9675b91362770dbb99400ed5bee9e16a6ea18f97
Amy Breslin <amy.breslin@solirius.com>
2026-08-06 12:42:54 +0100

    DTSCCI-5549 - fixes for separating creating and saving or loading drafts. Returing 201/200 response for POST. Implemented check for expiresAt and implemented unit tests for manager service.

--- src/main/common/models/draft/draftClaim.ts
+++ src/main/common/models/draft/draftClaim.ts
@@ -1,3 +1,5 @@
+import {CivilClaimResponse} from 'models/civilClaimResponse';
+
 export interface DraftClaimRequest {
   caseId?: string;
   payload: Record<string, unknown>;
@@ -7,6 +9,15 @@ export interface DraftClaimResponse {
   draftId: string;
   caseId?: string;
   payload: Record<string, unknown>;
+  createdAt: string;
+  updatedAt: string;
+  expiresAt: string;
+}
+
+export interface DraftClaimManagerResult {
+  claimResponse: CivilClaimResponse;
+  rawResponse: DraftClaimResponse;
+  isNew?: boolean;
   createdAt: string;
   updatedAt: string;
   expiresAt: string;
