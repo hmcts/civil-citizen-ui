680cc359b1d3f6aeeea7d51d2e3702172aece65b
krishnanuthalapati <32389208+krishnanuthalapati@users.noreply.github.com>
2026-08-07 10:47:14 +0100

    Temporarily skipped due to inconsistent behaviour between Preview and Master (#8034)

--- src/test/functionalTests/tests/ui_tests/defendant-linking/defendantLinkingThroughCUI_tests.js
+++ src/test/functionalTests/tests/ui_tests/defendant-linking/defendantLinkingThroughCUI_tests.js
@@ -27,7 +27,7 @@ Scenario.skip('CTSC admin links a defendant to a LiP claim through Manage Case',
   await completeFullAdmitPayImmediatelyJourney(I, api, claimRef, claimNumber, defendantUser);
 });
 
-Scenario('Defendant links a LiP claim using claim number and security code through CUI', async ({I, api}) => {
+Scenario.skip('Defendant links a LiP claim using claim number and security code through CUI', async ({I, api}) => {
   const {claimantUser, defendantUser} = await createScenarioUsers();
 
   const claimRef = await api.createLiPClaim(claimantUser, claimType, false, 'Individual', undefined, false, false);
