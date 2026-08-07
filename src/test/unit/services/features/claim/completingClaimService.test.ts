c1b527a4be5fab86c145eb1b92bb6a9ebd73966d
AbuSalamHMCTS <abu.salam@HMCTS.NET>
2026-08-07 16:37:02 +0100

    DTSCCI-5549 lint

--- src/test/unit/services/features/claim/completingClaimService.test.ts
+++ src/test/unit/services/features/claim/completingClaimService.test.ts
@@ -1,9 +1,9 @@
 import * as draftStoreService from '../../../../../main/modules/draft-store/draftStoreService';
-import * as draftStoreManagerService from '../../../../../main/modules/draft-store/draftStoreManagerService'
-import {Claim} from '../../../../../main/common/models/claim';
+import * as draftStoreManagerService from '../../../../../main/modules/draft-store/draftStoreManagerService';
+import {Claim} from 'models/claim';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
 import {saveCompletingClaim} from 'services/features/claim/completingClaimService';
-import {AppRequest} from '../../../../../main/common/models/AppRequest';
+import {AppRequest} from 'models/AppRequest';
 
 jest.mock('../../../../../main/modules/draft-store');
 jest.mock('../../../../../main/modules/draft-store/draftStoreService');
@@ -17,7 +17,7 @@ describe('Resolving Dispute Service', () => {
       },
       draftId: 'draft-123',
     },
-  } as unknown as AppRequest
+  } as unknown as AppRequest;
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -33,13 +33,15 @@ describe('Resolving Dispute Service', () => {
     await saveCompletingClaim(mockReq);
     // Then
     expect(spyGetCaseDataFromStore).toHaveBeenCalledWith('123');
-    expect(spyUpdateDraftClaim).toHaveBeenCalledWith( mockReq, expect.objectContaining({completingClaimConfirmed: true}),
+    expect(spyUpdateDraftClaim).toHaveBeenCalledWith(
+      mockReq,
+      expect.objectContaining({completingClaimConfirmed: true}),
       'draft-123',
-      );
-    });
+    );
+  });
 
-    //Given
   it('should throw an error when draftStore fails', async () => {
+    //Given
     (draftStoreService.getCaseDataFromStore as jest.Mock).mockRejectedValue(
       new Error(TestMessages.REDIS_FAILURE),
     );
