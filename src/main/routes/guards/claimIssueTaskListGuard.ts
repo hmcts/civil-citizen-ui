439a6a52db35c107a54b6a14c4abe9b86d624bf4
Amy Breslin <amy.breslin@solirius.com>
2026-08-13 09:09:45 +0100

    DTSCCI-5549 - refactored existing code to use new draftStoreManagerService updateDraftClaim and getDraftClaim

--- src/main/routes/guards/claimIssueTaskListGuard.ts
+++ src/main/routes/guards/claimIssueTaskListGuard.ts
@@ -1,7 +1,5 @@
-import {NextFunction, Request, Response, RequestHandler} from 'express';
 import {Claim} from 'common/models/claim';
 import {getDraftClaim} from 'modules/draft-store/draftStoreManagerService';
-import {getCaseDataFromStore} from 'modules/draft-store/draftStoreService';
 import {AppRequest} from 'models/AppRequest';
 import {BASE_ELIGIBILITY_URL} from 'routes/urls';
 import {stashClaimOnRequest} from 'common/utils/claimRequestLocals';
@@ -9,10 +7,10 @@ import {stashClaimOnRequest} from 'common/utils/claimRequestLocals';
 export const claimIssueTaskListGuard = (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
     const appReq: AppRequest = <AppRequest>req;
-    const userId = appReq.session?.user?.id;
 
-    let caseData : Claim;
+    let caseData: CLaim = new CLaim();
     const draftResult = await getDraftClaim(appReq);
+
     if (draftResult) {
       appReq.session.draftId = draftResult.rawResponse.draftId;
       caseData = Object.assign(new Claim(), draftResult.claimResponse.case_data);
@@ -20,12 +18,11 @@ export const claimIssueTaskListGuard = (async (req: AppRequest, res: Response, n
       if (!caseData.draftClaimCreatedAt && draftResult.createdAt) {
         caseData.draftClaimCreatedAt = new Date(draftResult.createdAt);
       }
-    } else {
-      caseData = await getCaseDataFromStore(userId, true);
     }
 
     stashClaimOnRequest(req, caseData);
     const excludeUrlList = ['/confirmation', '/fee', '/fee-change', '/pay-fees'].some(endpoint => req.originalUrl.includes(endpoint));
+
     if (!caseData?.isDraftClaim()
       && !req.cookies['eligibilityCompleted']
       && !excludeUrlList) {
