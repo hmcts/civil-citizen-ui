e4f7bd0d71721e20579c32a4ba9aa6dabdedba12
Amy Breslin <amy.breslin@solirius.com>
2026-08-14 17:45:39 +0100

    DTSCCI-5549 - refactored existing code to use new draftStoreManagerService updateDraftClaim and updated corresponding tests

--- src/main/routes/guards/claimIssueTaskListGuard.ts
+++ src/main/routes/guards/claimIssueTaskListGuard.ts
@@ -1,3 +1,4 @@
+import {NextFunction, Response, RequestHandler} from 'express';
 import {Claim} from 'common/models/claim';
 import {getDraftClaim} from 'modules/draft-store/draftStoreManagerService';
 import {AppRequest} from 'models/AppRequest';
@@ -8,11 +9,12 @@ export const claimIssueTaskListGuard = (async (req: AppRequest, res: Response, n
   try {
     const appReq: AppRequest = <AppRequest>req;
 
-    let caseData: CLaim = new CLaim();
+    let caseData : Claim = new Claim();
     const draftResult = await getDraftClaim(appReq);
-
     if (draftResult) {
-      appReq.session.draftId = draftResult.rawResponse.draftId;
+      if (appReq.session) {
+        appReq.session.draftId = draftResult.rawResponse.draftId;
+      }
       caseData = Object.assign(new Claim(), draftResult.claimResponse.case_data);
 
       if (!caseData.draftClaimCreatedAt && draftResult.createdAt) {
@@ -22,7 +24,6 @@ export const claimIssueTaskListGuard = (async (req: AppRequest, res: Response, n
 
     stashClaimOnRequest(req, caseData);
     const excludeUrlList = ['/confirmation', '/fee', '/fee-change', '/pay-fees'].some(endpoint => req.originalUrl.includes(endpoint));
-
     if (!caseData?.isDraftClaim()
       && !req.cookies['eligibilityCompleted']
       && !excludeUrlList) {
