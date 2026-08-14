cd75953eaf803ba55cd08d9e0cf67818866d8d48
Amy Breslin <amy.breslin@solirius.com>
2026-08-14 17:45:39 +0100

    DTSCCI-5549 - refactored existing code to use new draftStoreManagerService updateDraftClaim and updated corresponding tests

--- src/main/services/features/claim/completingClaimService.ts
+++ src/main/services/features/claim/completingClaimService.ts
@@ -1,6 +1,6 @@
 import {AppRequest} from 'common/models/AppRequest';
+import {updateDraftClaim, getDraftClaim} from 'modules/draft-store/draftStoreManagerService';
 import {Claim} from 'common/models/claim';
-import {getDraftClaim, updateDraftClaim} from 'modules/draft-store/draftStoreManagerService';
 
 const {Logger} = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('completingClaimService');
@@ -8,17 +8,27 @@ const logger = Logger.getLogger('completingClaimService');
 export const  saveCompletingClaim = async (req: AppRequest): Promise<void> => {
   try {
     const draftId = req.session?.draftId;
+    if (!draftId) {
+      throw new Error('[completingClaimService] draftId is missing from session');
+    }
 
     const draftResult = await getDraftClaim(req);
-    const claim: Claim = draftResult?.claimResponse?.case_data
-    ? Object.assign(new Claim(), draftResult.claimResponse.case_data)
-      : new Claim();
+    if (!draftResult) {
+      throw new Error('[completingClaimService] no draft claim found for user');
+    }
+
+    const claim = Object.assign<Claim, Record<string, any>>(
+      new Claim(),
+      draftResult.claimResponse.case_data,
+    );
+    if (!claim.draftClaimCreatedAt && draftResult.createdAt) {
+      claim.draftClaimCreatedAt = new Date(draftResult.createdAt);
+    }
 
     claim.completingClaimConfirmed = true;
-
     await updateDraftClaim(req, claim, draftId);
   } catch (error) {
-    logger.error(error);
+    logger.error(`[completingClaimService] failed to save completing claim: ${error}`);
     throw error;
   }
 };
