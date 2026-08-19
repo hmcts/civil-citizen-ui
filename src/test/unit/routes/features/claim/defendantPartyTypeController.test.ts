74024e28924867555862df81332d3498c5d6affa
Amy Breslin <amy.breslin@solirius.com>
2026-08-19 15:01:58 +0100

    DTSCCI-5549 - fixed build errors for tests that were modified

--- src/test/unit/routes/features/claim/defendantPartyTypeController.test.ts
+++ src/test/unit/routes/features/claim/defendantPartyTypeController.test.ts
@@ -1,19 +1,20 @@
-import config from 'config';
-import nock from 'nock';
 import request from 'supertest';
 import {app} from '../../../../../main/app';
-import {PartyType} from 'models/partyType';
+import nock from 'nock';
+import config from 'config';
 import {
-  DELAYED_FLIGHT_URL,
-  CLAIM_DEFENDANT_INDIVIDUAL_DETAILS_URL,
-  CLAIM_DEFENDANT_ORGANISATION_DETAILS_URL,
   CLAIM_DEFENDANT_PARTY_TYPE_URL,
+  CLAIM_DEFENDANT_INDIVIDUAL_DETAILS_URL,
   CLAIM_DEFENDANT_SOLE_TRADER_DETAILS_URL,
+  CLAIM_DEFENDANT_COMPANY_DETAILS_URL,
+  CLAIM_DEFENDANT_ORGANISATION_DETAILS_URL,
 } from 'routes/urls';
 import {TestMessages} from '../../../../utils/errorMessageTestConstants';
-import {Claim} from 'models/claim';
+import {t} from 'i18next';
+import {PartyType} from 'models/partyType';
 import {getDraftClaim, updateDraftClaim} from 'modules/draft-store/draftStoreManagerService';
 import * as draftStoreService from 'modules/draft-store/draftStoreService';
+import {Claim} from 'models/claim';
 import {CivilClaimResponse} from 'models/civilClaimResponse';
 import {DraftClaimManagerResult} from 'models/draft/draftClaim';
 
@@ -33,7 +34,7 @@ const createMockManagerResult = (claim: Claim): DraftClaimManagerResult => ({
   claimResponse: {
     id: '123',
     case_data: claim as unknown as Claim,
-  } as CivilClaimResponse,
+  } as unknown as CivilClaimResponse,
   rawResponse: {
     draftId: '123',
     payload: claim,
@@ -43,10 +44,9 @@ const createMockManagerResult = (claim: Claim): DraftClaimManagerResult => ({
   expiresAt: '2026-09-01T10:00:00.000Z',
 });
 
-describe('Defendant party type controller', () => {
+describe('Defendant Party Type Controller', () => {
   const citizenRoleToken: string = config.get('citizenRoleToken');
   const idamUrl: string = config.get('idamUrl');
-  app.request.cookies = {eligibilityCompleted: true};
 
   beforeAll(() => {
     nock(idamUrl)
@@ -59,17 +59,19 @@ describe('Defendant party type controller', () => {
   });
 
   describe('on GET', () => {
-    it('should display defendant party type page', async () => {
-      const mockClaim = new Claim();
-      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockGetCaseDataFromStore.mockResolvedValue(mockClaim);
+    it('should return party type page', async () => {
+      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockGetCaseDataFromStore.mockResolvedValue(new Claim());
 
-      const response = await request(app).get(CLAIM_DEFENDANT_PARTY_TYPE_URL);
-      expect(response.status).toBe(200);
-      expect(response.text).toContain('Who are you making the claim against?');
+      await request(app)
+        .get(CLAIM_DEFENDANT_PARTY_TYPE_URL)
+        .expect((res: request.Response) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain(t('PAGES.PARTY_TYPE.DEFENDANT_TITLE'));
+        });
     });
 
-    it('should return status 500 when error is thrown', async () => {
+    it('should return http 500 when has error in the get method', async () => {
       mockGetDraftClaim.mockRejectedValue(new Error(TestMessages.REDIS_FAILURE));
       mockGetCaseDataFromStore.mockRejectedValue(new Error(TestMessages.REDIS_FAILURE));
 
@@ -83,87 +85,80 @@ describe('Defendant party type controller', () => {
   });
 
   describe('on POST', () => {
-    it('should display defendant party type page if there is no selection', async () => {
-      const mockClaim = new Claim();
-      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockGetCaseDataFromStore.mockResolvedValue(mockClaim);
-
-      const response = await request(app).post(CLAIM_DEFENDANT_PARTY_TYPE_URL);
-      expect(response.status).toBe(200);
-      expect(response.text).toContain('Who are you making the claim against?');
-    });
-
-    it('should redirect to the defendant individual details if individual radio is selected', async () => {
-      const mockClaim = new Claim();
-      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockGetCaseDataFromStore.mockResolvedValue(mockClaim);
-      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
+    it('should redirect to individual details page when INDIVIDUAL selected', async () => {
+      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockGetCaseDataFromStore.mockResolvedValue(new Claim());
+      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
       mockSaveDraftClaim.mockResolvedValue(undefined);
-
-      const response = await request(app)
-        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
-        .send({option: PartyType.INDIVIDUAL});
-      expect(response.status).toBe(302);
-      expect(response.header.location).toBe(CLAIM_DEFENDANT_INDIVIDUAL_DETAILS_URL);
-    });
-
-    it('should redirect to the defendant company details if company radio is selected', async () => {
-      const mockClaim = new Claim();
-      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockGetCaseDataFromStore.mockResolvedValue(mockClaim);
-      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockSaveDraftClaim.mockResolvedValue(undefined);
-
-      const response = await request(app)
-        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
-        .send({option: PartyType.COMPANY});
-      expect(response.status).toBe(302);
-      expect(response.header.location).toBe(DELAYED_FLIGHT_URL);
-    });
-
-    it('should redirect to the sole trader details if sole trader radio is selected', async () => {
-      const mockClaim = new Claim();
-      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockGetCaseDataFromStore.mockResolvedValue(mockClaim);
-      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockSaveDraftClaim.mockResolvedValue(undefined);
-
-      const response = await request(app)
-        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
-        .send({option: PartyType.SOLE_TRADER});
-      expect(response.status).toBe(302);
-      expect(response.header.location).toBe(CLAIM_DEFENDANT_SOLE_TRADER_DETAILS_URL);
-    });
-
-    it('should redirect to the organisation details if organisation radio is selected', async () => {
-      const mockClaim = new Claim();
-      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockGetCaseDataFromStore.mockResolvedValue(mockClaim);
-      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockSaveDraftClaim.mockResolvedValue(undefined);
-
-      const response = await request(app)
-        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
-        .send({option: PartyType.ORGANISATION});
-      expect(response.status).toBe(302);
-      expect(response.header.location).toBe(CLAIM_DEFENDANT_ORGANISATION_DETAILS_URL);
-    });
-
-    it('should render page if non-existent party type is provided', async () => {
-      const mockClaim = new Claim();
-      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(mockClaim));
-      mockGetCaseDataFromStore.mockResolvedValue(mockClaim);
 
       await request(app)
         .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
-        .send({foo: 'blah'})
-        .expect((response: request.Response) => {
-          expect(response.status).toBe(200);
-          expect(response.text).toContain(TestMessages.DEFENDANT_PARTY_TYPE_REQUIRED);
+        .send({option: PartyType.INDIVIDUAL})
+        .expect((res: request.Response) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toBe(CLAIM_DEFENDANT_INDIVIDUAL_DETAILS_URL);
         });
     });
 
-    it('should return something went wrong page if redis failure occurs', async () => {
+    it('should redirect to sole trader details page when SOLE_TRADER selected', async () => {
+      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockGetCaseDataFromStore.mockResolvedValue(new Claim());
+      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockSaveDraftClaim.mockResolvedValue(undefined);
+
+      await request(app)
+        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
+        .send({option: PartyType.SOLE_TRADER})
+        .expect((res: request.Response) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toBe(CLAIM_DEFENDANT_SOLE_TRADER_DETAILS_URL);
+        });
+    });
+
+    it('should redirect to company details page when COMPANY selected', async () => {
+      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockGetCaseDataFromStore.mockResolvedValue(new Claim());
+      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockSaveDraftClaim.mockResolvedValue(undefined);
+
+      await request(app)
+        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
+        .send({option: PartyType.COMPANY})
+        .expect((res: request.Response) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toBe(CLAIM_DEFENDANT_COMPANY_DETAILS_URL);
+        });
+    });
+
+    it('should redirect to organisation details page when ORGANISATION selected', async () => {
+      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockGetCaseDataFromStore.mockResolvedValue(new Claim());
+      mockUpdateDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockSaveDraftClaim.mockResolvedValue(undefined);
+
+      await request(app)
+        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
+        .send({option: PartyType.ORGANISATION})
+        .expect((res: request.Response) => {
+          expect(res.status).toBe(302);
+          expect(res.header.location).toBe(CLAIM_DEFENDANT_ORGANISATION_DETAILS_URL);
+        });
+    });
+
+    it('should return errors on empty option', async () => {
+      mockGetDraftClaim.mockResolvedValue(createMockManagerResult(new Claim()));
+      mockGetCaseDataFromStore.mockResolvedValue(new Claim());
+
+      await request(app)
+        .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
+        .send({option: ''})
+        .expect((res: request.Response) => {
+          expect(res.status).toBe(200);
+          expect(res.text).toContain(t('ERRORS.VALID_CHOOSE_DEFENDANT_TYPE'));
+        });
+    });
+
+    it('should return http 500 when has error in the post method', async () => {
       mockGetDraftClaim.mockRejectedValue(new Error(TestMessages.REDIS_FAILURE));
       mockGetCaseDataFromStore.mockRejectedValue(new Error(TestMessages.REDIS_FAILURE));
       mockUpdateDraftClaim.mockRejectedValue(new Error(TestMessages.REDIS_FAILURE));
@@ -171,10 +166,10 @@ describe('Defendant party type controller', () => {
 
       await request(app)
         .post(CLAIM_DEFENDANT_PARTY_TYPE_URL)
-        .send({option: PartyType.ORGANISATION})
-        .expect((response: request.Response) => {
-          expect(response.status).toBe(500);
-          expect(response.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
+        .send({option: PartyType.INDIVIDUAL})
+        .expect((res: request.Response) => {
+          expect(res.status).toBe(500);
+          expect(res.text).toContain(TestMessages.SOMETHING_WENT_WRONG);
         });
     });
   });
