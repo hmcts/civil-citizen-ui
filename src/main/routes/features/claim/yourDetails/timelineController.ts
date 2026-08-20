482745321bd39b8170092bef1d2acd5891f0e001
Amy Breslin <amy.breslin@solirius.com>
2026-08-20 15:29:32 +0100

    DTSCCI-6162 - migrate claim issue services to durable draft manager and update claim-details controllers to pass req. Also updated corresponding tests

--- src/main/routes/features/claim/yourDetails/timelineController.ts
+++ src/main/routes/features/claim/yourDetails/timelineController.ts
@@ -16,8 +16,7 @@ const pageTitle= 'PAGES.TIMELINE.TITLE';
 
 timelineController.get(CLAIM_TIMELINE_URL, (async (req: AppRequest, res: Response, next: NextFunction) => {
   try {
-    const userId = req.session?.user?.id;
-    const form = new GenericForm(getTimeline(await getClaimDetails(userId)));
+    const form = new GenericForm(getTimeline(await getClaimDetails(req)));
     const lang = req.query.lang ? req.query.lang : req.cookies.lang;
     const dates = [
       getDateInThePast(lang, 90),
