ad5a7c2cbece4b93069489a922aad71719fe6258
Amy Breslin <amy.breslin@solirius.com>
2026-09-02 17:22:02 +0100

    DTSCCI-6162 - remove leftover draft-store mocks from interest controller tests so webpack can compile

--- src/test/unit/routes/features/claim/iterest/claimTotalInterestController.test.ts
+++ src/test/unit/routes/features/claim/iterest/claimTotalInterestController.test.ts
@@ -77,10 +77,6 @@ describe('Claim Total Interest Controller', () => {
     });
 
     it('should render page with error when negative interest amount is provided', async () => {
-      getInterestMock.mockImplementationOnce(async () => {
-        return new Claim();
-      });
-
       await request(app).post(CLAIM_INTEREST_TOTAL_URL).send({amount: '-5', reason: '99 reasons'}).expect((res) => {
         expect(res.status).toBe(200);
         expect(res.text).toContain(TestMessages.VALID_INTEREST_AMOUNT);
