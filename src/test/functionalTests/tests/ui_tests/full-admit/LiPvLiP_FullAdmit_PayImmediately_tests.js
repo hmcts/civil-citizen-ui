5b92ae279899fcc3f194e87d705cadab7560a765
Mike Lemos <michael.lemos1@hmcts.net>
2026-09-23 11:33:37 +0100

    DTSCCI-6157 Run defendant responses on thin client

--- src/test/functionalTests/tests/ui_tests/full-admit/LiPvLiP_FullAdmit_PayImmediately_tests.js
+++ src/test/functionalTests/tests/ui_tests/full-admit/LiPvLiP_FullAdmit_PayImmediately_tests.js
@@ -13,7 +13,7 @@ const claimType = 'SmallClaims';
 let caseData, claimNumber, claimRef, claimAmount = 1500, claimFee = 80, deadline = '6 March 2024';
 let claimTotalAmount = claimAmount + claimFee;
 
-Feature('Create Lip v Lip claim -  Full Admit and pay Immediately').tag('@civil-citizen-master @civil-citizen-pr @civil-citizen-nightly @ui-full-admit');
+Feature('Create Lip v Lip claim -  Full Admit and pay Immediately').tag('@civil-citizen-master @civil-citizen-pr @civil-citizen-nightly @ui-full-admit @thin-full-stack');
 
 Scenario('Create LipvLip claim and defendant response as FullAdmit and pay immediately', async ({I, api}) => {
   await createAccount(config.claimantCitizenUser.email, config.claimantCitizenUser.password);
